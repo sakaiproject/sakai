c5500aeb31fc9fb14c95b708f722d880549ec8fc
Ed Smiley <esmiley@stanford.edu>
2005-06-24 00:09:01 +0000

    SAK-1374: updated copyright, Sun Microsystems, modifications copyright Sakai
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@227 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/src/com/corejsf/util/Tags.java
+++ samigo/src/com/corejsf/util/Tags.java
@@ -1,3 +1,30 @@
+/**********************************************************************************
+* $HeadURL$
+* $Id$
+***********************************************************************************
+* Copyright (c) 2004 Sun Microsystems from the Java Series, Core Java ServerFaces
+* source freely distributable.
+* see http://www.sun.com/books/java_series.html
+ ***********************************************************************************
+* Modifications Copyright (c) 2005
+* The Regents of the University of Michigan, Trustees of Indiana University,
+* Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+*
+* Licensed under the Educational Community License Version 1.0 (the "License");
+* By obtaining, using and/or copying this Original Work, you agree that you have read,
+* understand, and will comply with the terms and conditions of the Educational Community License.
+* You may obtain a copy of the License at:
+*
+*      http://cvs.sakaiproject.org/licenses/license_1_0.html
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+**********************************************************************************/
+
 package com.corejsf.util;
 
 import java.io.Serializable;
@@ -23,45 +50,45 @@ public class Tags {
          component.getAttributes().put(attributeName, attributeValue);
    }
 
-   public static void setInteger(UIComponent component, 
+   public static void setInteger(UIComponent component,
          String attributeName, String attributeValue) {
       if (attributeValue == null) return;
       if (UIComponentTag.isValueReference(attributeValue))
          setValueBinding(component, attributeName, attributeValue);
-      else 
-         component.getAttributes().put(attributeName, 
+      else
+         component.getAttributes().put(attributeName,
                new Integer(attributeValue));
    }
 
-   public static void setDouble(UIComponent component, 
+   public static void setDouble(UIComponent component,
          String attributeName, String attributeValue) {
       if (attributeValue == null) return;
       if (UIComponentTag.isValueReference(attributeValue))
          setValueBinding(component, attributeName, attributeValue);
-      else 
-         component.getAttributes().put(attributeName, 
+      else
+         component.getAttributes().put(attributeName,
                new Double(attributeValue));
    }
 
-   public static void setBoolean(UIComponent component, 
+   public static void setBoolean(UIComponent component,
          String attributeName, String attributeValue) {
       if (attributeValue == null) return;
       if (UIComponentTag.isValueReference(attributeValue))
          setValueBinding(component, attributeName, attributeValue);
-      else 
-         component.getAttributes().put(attributeName, 
+      else
+         component.getAttributes().put(attributeName,
                new Boolean(attributeValue));
    }
-   
+
    public static void setStrings(UIComponent component, Map map) {
       Iterator iter = map.entrySet().iterator();
       while (iter.hasNext()) {
          Map.Entry entry = (Map.Entry) iter.next();
-         setString(component, (String) entry.getKey(), 
+         setString(component, (String) entry.getKey(),
                (String) entry.getValue());
       }
-   }         
-   
+   }
+
    public static void setValueBinding(UIComponent component, String attributeName,
          String attributeValue) {
       FacesContext context = FacesContext.getCurrentInstance();
@@ -72,34 +99,34 @@ public class Tags {
 
    public static void setActionListener(UIComponent component, String attributeValue) {
       setMethodBinding(component, "actionListener", attributeValue,
-            new Class[] { ActionEvent.class });      
+            new Class[] { ActionEvent.class });
    }
 
-   public static void setValueChangeListener(UIComponent component, 
+   public static void setValueChangeListener(UIComponent component,
          String attributeValue) {
       setMethodBinding(component, "valueChangeListener", attributeValue,
-            new Class[] { ValueChangeEvent.class });      
+            new Class[] { ValueChangeEvent.class });
    }
 
-   public static void setValidator(UIComponent component, 
+   public static void setValidator(UIComponent component,
          String attributeValue) {
       setMethodBinding(component, "validator", attributeValue,
-            new Class[] { FacesContext.class, UIComponent.class, Object.class });      
+            new Class[] { FacesContext.class, UIComponent.class, Object.class });
    }
 
    public static void setAction(UIComponent component, String attributeValue) {
       if (attributeValue == null) return;
-      if (UIComponentTag.isValueReference(attributeValue)) 
+      if (UIComponentTag.isValueReference(attributeValue))
          setMethodBinding(component, "action", attributeValue,
                new Class[] {});
       else {
          FacesContext context = FacesContext.getCurrentInstance();
          Application app = context.getApplication();
          MethodBinding mb = new ActionMethodBinding(attributeValue);
-         component.getAttributes().put("action", mb);         
+         component.getAttributes().put("action", mb);
       }
    }
-      
+
    public static void setMethodBinding(UIComponent component, String attributeName,
          String attributeValue, Class[] paramTypes) {
       if (attributeValue == null)
@@ -110,62 +137,62 @@ public class Tags {
          MethodBinding mb = app.createMethodBinding(attributeValue, paramTypes);
          component.getAttributes().put(attributeName, mb);
       }
-   }     
-   
+   }
+
    public static String eval(String expression) {
       if (expression == null) return null;
       if (UIComponentTag.isValueReference(expression)) {
          FacesContext context = FacesContext.getCurrentInstance();
          Application app = context.getApplication();
-         return "" + app.createValueBinding(expression).getValue(context); 
+         return "" + app.createValueBinding(expression).getValue(context);
       }
-      else return expression;      
-   }   
+      else return expression;
+   }
 
    public static Integer evalInteger(String expression) {
       if (expression == null) return null;
       if (UIComponentTag.isValueReference(expression)) {
          FacesContext context = FacesContext.getCurrentInstance();
          Application app = context.getApplication();
-         Object r = app.createValueBinding(expression).getValue(context); 
+         Object r = app.createValueBinding(expression).getValue(context);
          if (r == null) return null;
          else if (r instanceof Integer) return (Integer) r;
          else return new Integer(r.toString());
       }
-      else return new Integer(expression);      
-   }   
-   
+      else return new Integer(expression);
+   }
+
    public static Double evalDouble(String expression) {
       if (expression == null) return null;
       if (UIComponentTag.isValueReference(expression)) {
          FacesContext context = FacesContext.getCurrentInstance();
          Application app = context.getApplication();
-         Object r = app.createValueBinding(expression).getValue(context); 
+         Object r = app.createValueBinding(expression).getValue(context);
          if (r == null) return null;
          else if (r instanceof Double) return (Double) r;
          else return new Double(r.toString());
       }
-      else return new Double(expression);      
-   }   
-   
+      else return new Double(expression);
+   }
+
    public static Boolean evalBoolean(String expression) {
       if (expression == null) return null;
       if (UIComponentTag.isValueReference(expression)) {
          FacesContext context = FacesContext.getCurrentInstance();
          Application app = context.getApplication();
-         Object r = app.createValueBinding(expression).getValue(context); 
+         Object r = app.createValueBinding(expression).getValue(context);
          if (r == null) return null;
          else if (r instanceof Boolean) return (Boolean) r;
          else return new Boolean(r.toString());
       }
-      else return new Boolean(expression);      
-   }   
+      else return new Boolean(expression);
+   }
 
-   private static class ActionMethodBinding 
-         extends MethodBinding implements Serializable {      
+   private static class ActionMethodBinding
+         extends MethodBinding implements Serializable {
       private String result;
-   
-      public ActionMethodBinding(String result) { this.result = result; }      
+
+      public ActionMethodBinding(String result) { this.result = result; }
       public Object invoke(FacesContext context, Object params[]) {
          return result;
       }
