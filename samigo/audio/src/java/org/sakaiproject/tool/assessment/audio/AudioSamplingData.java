2c1e39cf4ceea7d6070d10e1fed81139bb48e536
Ed Smiley <esmiley@stanford.edu>
2005-08-18 01:44:53 +0000

    (Task SAM-376) refactored out sampling display from AudioRecorder.java into new classes
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@1260 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ samigo/audio/src/java/org/sakaiproject/tool/assessment/audio/AudioSamplingData.java
@@ -0,0 +1,126 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/svn/trunk/sakai/sam/src/org/sakaiproject/tool/assessment/services/qti/QTIService.java $
+ * $Id: QTIService.java 632 2005-07-14 21:22:50Z janderse@umich.edu $
+ ***********************************************************************************
+ *
+ * Portions copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
+ *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ *
+   * Licensed under the Educational Community License Version 1.0 (the "License");
+ * By obtaining, using and/or copying this Original Work, you agree that you have read,
+ * understand, and will comply with the terms and conditions of the Educational Community License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **********************************************************************************/
+package org.sakaiproject.tool.assessment.audio;
+
+import java.io.Serializable;
+import java.util.Vector;
+import javax.sound.sampled.AudioInputStream;
+
+public class AudioSamplingData
+  implements Serializable
+{
+  private String reportStatus;
+  private String errStr;
+  private String fileName;
+  private double seconds;
+  private double duration;
+  private Runnable capture;
+  private Thread captureThread;
+  private java.util.Vector line;
+  private javax.sound.sampled.AudioInputStream audioInputStream;
+  public String getReportStatus()
+  {
+    return reportStatus;
+  }
+
+  public void setReportStatus(String reportStatus)
+  {
+    this.reportStatus = reportStatus;
+  }
+
+  public String getErrStr()
+  {
+    return errStr;
+  }
+
+  public void setErrStr(String errStr)
+  {
+    this.errStr = errStr;
+  }
+
+  public String getFileName()
+  {
+    return fileName;
+  }
+
+  public void setFileName(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public double getSeconds()
+  {
+    return seconds;
+  }
+
+  public void setSeconds(double seconds)
+  {
+    this.seconds = seconds;
+  }
+
+  public double getDuration()
+  {
+    return duration;
+  }
+
+  public void setDuration(double duration)
+  {
+    this.duration = duration;
+  }
+
+  public Runnable getCapture()
+  {
+    return capture;
+  }
+
+  public void setCapture(Runnable capture)
+  {
+    this.capture = capture;
+  }
+
+  public Thread getCaptureThread()
+  {
+    return captureThread;
+  }
+
+  public void setCaptureThread(Thread captureThread)
+  {
+    this.captureThread = captureThread;
+  }
+  public Vector getLine()
+  {
+    return line;
+  }
+  public void setLine(java.util.Vector line)
+  {
+    this.line = line;
+  }
+  public AudioInputStream getAudioInputStream()
+  {
+    return audioInputStream;
+  }
+  public void setAudioInputStream(javax.sound.sampled.AudioInputStream audioInputStream)
+  {
+    this.audioInputStream = audioInputStream;
+  }
+}
