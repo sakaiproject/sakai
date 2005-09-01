edf9a1213913b58be2f0dd46d560f2b0c39e6afd
Ed Smiley <esmiley@stanford.edu>
2005-09-01 20:21:55 +0000

    SAM-513: intial test build of API (dummy)
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@1490 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ samigo/api/src/java/org/sakaiproject/tool/dummy/ifc/DummyIfc.java
@@ -0,0 +1,11 @@
+package org.sakaiproject.tool.dummy.ifc;
+
+import java.io.Serializable;
+
+/**
+ * Merely a dummy to test build.
+ * Remove later.
+ */
+public class DummyIfc implements Serializable
+{
+}
