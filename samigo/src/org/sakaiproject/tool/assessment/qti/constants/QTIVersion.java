10d74130ea85dc11810be3c02864a1eba3e23003
Ed Smiley <esmiley@stanford.edu>
2005-09-14 23:36:24 +0000

    SAK-1927: moved qti service api package dependencies
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@1724 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ samigo/src/org/sakaiproject/tool/assessment/qti/constants/QTIVersion.java
@@ -0,0 +1,56 @@
+/**********************************************************************************
+* $URL$
+* $Id$
+***********************************************************************************
+*
+* Copyright (c) 2003-2005 The Regents of the University of Michigan, Trustees of Indiana University,
+*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+*
+* Licensed under the Educational Community License Version 1.0 (the "License");
+* By obtaining, using and/or copying this Original Work, you agree that you have read,
+* understand, and will comply with the terms and conditions of the Educational Community License.
+* You may obtain a copy of the License at:
+*
+*      http://cvs.sakaiproject.org/licenses/license_1_0.html
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+**********************************************************************************/
+
+package org.sakaiproject.tool.assessment.qti.constants;
+
+/**
+ * <p>Supported QTI Versions.</p>
+ * <p> </p>
+ * <p>Copyright: Copyright (c) 2005 Sakai</p>
+ * <p> </p>
+ * @author Ed Smiley esmiley@stanford.edu
+ * @version $Id$
+ */
+
+public class QTIVersion
+{
+  public static final int VERSION_1_2 = 1;
+  public static final int VERSION_2_0 = 2;
+
+  public QTIVersion()
+  {
+  }
+
+  /**
+   * @param q
+   * @return true if
+   * OK (QTI_VERSION_1_2 or QTI_VERSION_2_0)
+   */
+  public static boolean isValid(int q)
+  {
+    return (q == VERSION_1_2 || q == VERSION_2_0) ? true : false;
+  }
+
+}
+
+
