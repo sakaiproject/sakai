8c30f499230e727d215ea88ee8f04d4862a17077
Lance Speelmon <lance@rsmart.com>
2005-09-20 14:39:51 +0000

    SAK-320 http://bugs.sakaiproject.org/jira/browse/SAK-320
    Freshly generated DDL from Hibernate with new attachment table.
    
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/syllabus@1857 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- syllabus/syllabus-app/src/sql/mysql/sakai_syllabus.sql
+++ syllabus/syllabus-app/src/sql/mysql/sakai_syllabus.sql
@@ -1,6 +1,8 @@
 alter table SAKAI_SYLLABUS_DATA drop foreign key FK3BC123AA4FDCE067
+alter table SAKAI_SYLLABUS_ATTACH drop foreign key FK4BF41E45A09831E0
 drop table if exists SAKAI_SYLLABUS_DATA
 drop table if exists SAKAI_SYLLABUS_ITEM
+drop table if exists SAKAI_SYLLABUS_ATTACH
 create table SAKAI_SYLLABUS_DATA (
    id bigint not null auto_increment,
    lockId integer not null,
@@ -22,7 +24,21 @@ create table SAKAI_SYLLABUS_ITEM (
    primary key (id),
    unique (userId, contextId)
 )
+create table SAKAI_SYLLABUS_ATTACH (
+   syllabusAttachId bigint not null auto_increment,
+   lockId integer not null,
+   attachmentId text not null,
+   syllabusAttachName text not null,
+   syllabusAttachSize text,
+   syllabusAttachType text,
+   createdBy text,
+   syllabusAttachUrl text not null,
+   lastModifiedBy text,
+   syllabusId bigint,
+   primary key (syllabusAttachId)
+)
 create index syllabus_position on SAKAI_SYLLABUS_DATA (position)
 alter table SAKAI_SYLLABUS_DATA add index FK3BC123AA4FDCE067 (surrogateKey), add constraint FK3BC123AA4FDCE067 foreign key (surrogateKey) references SAKAI_SYLLABUS_ITEM (id)
 create index syllabus_userId on SAKAI_SYLLABUS_ITEM (userId)
 create index syllabus_contextId on SAKAI_SYLLABUS_ITEM (contextId)
+alter table SAKAI_SYLLABUS_ATTACH add index FK4BF41E45A09831E0 (syllabusId), add constraint FK4BF41E45A09831E0 foreign key (syllabusId) references SAKAI_SYLLABUS_DATA (id)
