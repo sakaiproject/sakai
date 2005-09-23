fd470892e5984acd935448e661a75e4e6323563c
Qingru Zhang <zqingru@stanford.edu>
2005-09-23 00:06:20 +0000

    SAK-1939
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@1989 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tool/src/java/org/sakaiproject/tool/assessment/facade/ItemGradingFacade.java
+++ samigo/tool/src/java/org/sakaiproject/tool/assessment/facade/ItemGradingFacade.java
@@ -108,28 +108,13 @@ public class ItemGradingFacade
   public void setSubmittedDate(Date submittedDate) {
     this.submittedDate = submittedDate;
   }
-  public Float getAutoScore() {
-   // Round to the nearest 1/10th.
-   if (autoScore != null) {
-    float alignment = autoScore.floatValue();
-    int tmp = Math.round(alignment * 10.0f);
-    alignment = (float)tmp / 10.0f;
-    autoScore = new Float(alignment);
-   }
+  public Float getAutoScore() { 
     return autoScore;
   }
   public void setAutoScore(Float autoScore) {
     this.autoScore = autoScore;
   }
-  public Float getOverrideScore() {
-
-   // Round to the nearest 1/10th.
-   if (overrideScore != null) {
-    float alignment = overrideScore.floatValue();
-    int tmp = Math.round(alignment * 10.0f);
-    alignment = (float)tmp / 10.0f;
-    overrideScore = new Float(alignment);
-   }
+  public Float getOverrideScore() {  
     return overrideScore;
   }
   public void setOverrideScore(Float overrideScore) {
