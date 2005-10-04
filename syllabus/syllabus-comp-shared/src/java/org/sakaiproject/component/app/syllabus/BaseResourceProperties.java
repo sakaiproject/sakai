8b43dee7919a691207f7ccc610bc626397f2ff2a
Glenn R. Golden <ggolden@etudes.org>
2005-10-04 19:56:31 +0000

    SAK-2132 - removed edit and locks from the Site API; updated client code
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/syllabus@2294 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- syllabus/syllabus-comp-shared/src/java/org/sakaiproject/component/app/syllabus/BaseResourceProperties.java
+++ syllabus/syllabus-comp-shared/src/java/org/sakaiproject/component/app/syllabus/BaseResourceProperties.java
@@ -1,25 +1,27 @@
 /**********************************************************************************
-* $URL$
-* $Id$
-***********************************************************************************
-*
-* Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
-*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
-* 
-* Licensed under the Educational Community License Version 1.0 (the "License");
-* By obtaining, using and/or copying this Original Work, you agree that you have read,
-* understand, and will comply with the terms and conditions of the Educational Community License.
-* You may obtain a copy of the License at:
-* 
-*      http://cvs.sakaiproject.org/licenses/license_1_0.html
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-**********************************************************************************/
+ * $URL: https://source.sakaiproject.org/svn/trunk/sakai/legacy/util/src/java/org/sakaiproject/util/resource/BaseResourceProperties.java $
+ * $Id: BaseResourceProperties.java 1060 2005-08-09 17:47:12Z jimeng@umich.edu $
+ **********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005 The Regents of the University of Michigan, Trustees of Indiana University,
+ *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ * 
+ * Licensed under the Educational Community License Version 1.0 (the "License");
+ * By obtaining, using and/or copying this Original Work, you agree that you have read,
+ * understand, and will comply with the terms and conditions of the Educational Community License.
+ * You may obtain a copy of the License at:
+ * 
+ *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **********************************************************************************/
+
+// package
 package org.sakaiproject.component.app.syllabus;
 
 // imports
@@ -27,6 +29,7 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 import java.util.Stack;
 import java.util.Vector;
 
@@ -53,8 +56,7 @@ import org.w3c.dom.NodeList;
  * BaseResourceProperties is the base class for ResourceProperties implementations.
  * </p>
  * 
- * @author University of Michigan, Sakai Software Development Team
- * @version $Revision: 20 $
+ * @author Sakai Software Development Team
  */
 public class BaseResourceProperties implements ResourceProperties
 {
@@ -785,6 +787,143 @@ public class BaseResourceProperties implements ResourceProperties
 		return PROP_STRUCTOBJ_TYPE;
 		
 	}	// getNamePropStructObjType
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setLazy(boolean lazy)
+	{
+		m_lazy = lazy;
+	}
+
+	/**
+	* Add a single valued property.
+	* @param name The property name.
+	* @param value The property value.
+	*/
+	public void addProperty(String name, String value)
+	{
+		// protect against a null put
+		if (value == null) value = "";
+
+		m_props.put(name, value);
+
+	}   // addProperty
+
+	/**
+	* Add a value to a multi-valued property.
+	* @param name The property name.
+	* @param value The property value.
+	*/
+	public void addPropertyToList(String name, String value)
+	{
+		// protect against a null put
+		if (value == null) value = "";
+
+		// accumulate multiple values in a list
+		Object current = m_props.get(name);
+		
+		// if we don't have a value yet, make a list to hold this one
+		if (current == null)
+		{
+			List values = new Vector();
+			m_props.put(name, values);
+			values.add(value);
+		}
+		
+		// if we do and it's a list, add this one
+		else if (current instanceof List)
+		{
+			((List) current).add(value);
+		}
+		
+		// if it's not a list, it's wrong!
+		else
+		{
+			Logger.warn(this + "addPropertyToList() value set not a list: " + name);
+		}
+
+	}	// addPropertyToList
+
+	/**
+	* Add all the properties from the other ResourceProperties object.
+	* @param other The ResourceProperties to add.
+	*/
+	public void addAll(ResourceProperties other)
+	{
+		// if there's a list, it must be deep copied
+		for (Iterator iNames = other.getPropertyNames(); iNames.hasNext();)
+		{
+			String name = (String) iNames.next();
+			Object value = ((BaseResourceProperties) other).m_props.get(name);
+			if (value instanceof List)
+			{
+				List list = new Vector();
+				list.addAll((List) value);
+				m_props.put(name, list);
+			}
+			else
+			{
+				m_props.put(name, value);
+			}
+		}
+
+	}	// addAll
+
+	/**
+	* Add all the properties from the Properties object.
+	* @param props The Properties to add.
+	*/
+	public void addAll(Properties props)
+	{
+		// if there's a list, it must be deep copied
+		for (Enumeration e = props.propertyNames(); e.hasMoreElements();)
+		{
+			String name = (String) e.nextElement();
+			Object value = props.get(name);
+			if (value instanceof List)
+			{
+				List list = new Vector();
+				list.addAll((List) value);
+				m_props.put(name, list);
+			}
+			else
+			{
+				m_props.put(name, value);
+			}
+		}
+
+	}	// addAll
+
+	/**
+	* Remove all properties.
+	*/
+	public void clear()
+	{
+		m_props.clear();
+
+	}	// clear
+
+	/**
+	* Remove a property.
+	* @param name The property name.
+	*/
+	public void removeProperty(String name)
+	{
+		m_props.remove(name);
+
+	}   // removeProperty
+
+	/**
+	* Take all values from this object.
+	* @param user The ResourceProperties object to take values from.
+	*/
+	public void set(ResourceProperties props)
+	{
+		clear();
+		addAll(props);
+
+	}	// set
 
 } // BaseResourceProperties
 
--- syllabus/syllabus-comp-shared/src/java/org/sakaiproject/component/app/syllabus/BaseResourcePropertiesEdit.java
+++ syllabus/syllabus-comp-shared/src/java/org/sakaiproject/component/app/syllabus/BaseResourcePropertiesEdit.java
@@ -1,9 +1,9 @@
 /**********************************************************************************
-* $URL$
-* $Id$
+* $URL: https://source.sakaiproject.org/svn/trunk/sakai/legacy/util/src/java/org/sakaiproject/util/resource/BaseResourcePropertiesEdit.java $
+* $Id: BaseResourcePropertiesEdit.java 632 2005-07-14 21:22:50Z janderse@umich.edu $
 ***********************************************************************************
 *
-* Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
+* Copyright (c) 2003, 2004 The Regents of the University of Michigan, Trustees of Indiana University,
 *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
 * 
 * Licensed under the Educational Community License Version 1.0 (the "License");
@@ -20,17 +20,11 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *
 **********************************************************************************/
+
+// package
 package org.sakaiproject.component.app.syllabus;
 
 // imports
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
-import java.util.Vector;
-
-import org.sakaiproject.service.framework.log.cover.Logger;
-import org.sakaiproject.service.legacy.resource.ResourceProperties;
 import org.sakaiproject.service.legacy.resource.ResourcePropertiesEdit;
 import org.w3c.dom.Element;
 
@@ -38,7 +32,7 @@ import org.w3c.dom.Element;
 * <p>BaseResourceProperties is the base class for ResourcePropertiesEdit implementations.</p>
 * 
 * @author University of Michigan, CHEF Software Development Team
-* @version $Revision: 20 $
+* @version $Revision: 632 $
 * @see org.chefproject.core.ResourceProperties
 */
 public class BaseResourcePropertiesEdit
@@ -63,143 +57,6 @@ public class BaseResourcePropertiesEdit
 		super(el);
 
 	}	// BaseResourcePropertiesEdit
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setLazy(boolean lazy)
-	{
-		m_lazy = lazy;
-	}
-
-	/**
-	* Add a single valued property.
-	* @param name The property name.
-	* @param value The property value.
-	*/
-	public void addProperty(String name, String value)
-	{
-		// protect against a null put
-		if (value == null) value = "";
-
-		m_props.put(name, value);
-
-	}   // addProperty
-
-	/**
-	* Add a value to a multi-valued property.
-	* @param name The property name.
-	* @param value The property value.
-	*/
-	public void addPropertyToList(String name, String value)
-	{
-		// protect against a null put
-		if (value == null) value = "";
-
-		// accumulate multiple values in a list
-		Object current = m_props.get(name);
-		
-		// if we don't have a value yet, make a list to hold this one
-		if (current == null)
-		{
-			List values = new Vector();
-			m_props.put(name, values);
-			values.add(value);
-		}
-		
-		// if we do and it's a list, add this one
-		else if (current instanceof List)
-		{
-			((List) current).add(value);
-		}
-		
-		// if it's not a list, it's wrong!
-		else
-		{
-			Logger.warn(this + "addPropertyToList() value set not a list: " + name);
-		}
-
-	}	// addPropertyToList
-
-	/**
-	* Add all the properties from the other ResourceProperties object.
-	* @param other The ResourceProperties to add.
-	*/
-	public void addAll(ResourceProperties other)
-	{
-		// if there's a list, it must be deep copied
-		for (Iterator iNames = other.getPropertyNames(); iNames.hasNext();)
-		{
-			String name = (String) iNames.next();
-			Object value = ((BaseResourceProperties) other).m_props.get(name);
-			if (value instanceof List)
-			{
-				List list = new Vector();
-				list.addAll((List) value);
-				m_props.put(name, list);
-			}
-			else
-			{
-				m_props.put(name, value);
-			}
-		}
-
-	}	// addAll
-
-	/**
-	* Add all the properties from the Properties object.
-	* @param props The Properties to add.
-	*/
-	public void addAll(Properties props)
-	{
-		// if there's a list, it must be deep copied
-		for (Enumeration e = props.propertyNames(); e.hasMoreElements();)
-		{
-			String name = (String) e.nextElement();
-			Object value = props.get(name);
-			if (value instanceof List)
-			{
-				List list = new Vector();
-				list.addAll((List) value);
-				m_props.put(name, list);
-			}
-			else
-			{
-				m_props.put(name, value);
-			}
-		}
-
-	}	// addAll
-
-	/**
-	* Remove all properties.
-	*/
-	public void clear()
-	{
-		m_props.clear();
-
-	}	// clear
-
-	/**
-	* Remove a property.
-	* @param name The property name.
-	*/
-	public void removeProperty(String name)
-	{
-		m_props.remove(name);
-
-	}   // removeProperty
-
-	/**
-	* Take all values from this object.
-	* @param user The ResourceProperties object to take values from.
-	*/
-	public void set(ResourceProperties props)
-	{
-		clear();
-		addAll(props);
-
-	}	// set
 
 }   // BaseResourcePropertiesEdit
 
