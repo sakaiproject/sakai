55481f31baeffad8916a29585cb155e930618a5c
Ed Smiley <esmiley@stanford.edu>
2005-10-06 02:05:20 +0000

    SAK-1927: uncommented out some code, added licenses
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@2362 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tests/src/java/org/sakaiproject/tool/assessment/test/integration/context/TestIntCtxtFactoryMethods.java
+++ samigo/tests/src/java/org/sakaiproject/tool/assessment/test/integration/context/TestIntCtxtFactoryMethods.java
@@ -1,3 +1,25 @@
+/**********************************************************************************
+ * $URL: $
+ * $Id: $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2004-2005 The Regents of the University of Michigan, Trustees of Indiana University,
+ *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ *
+   * Licensed under the Educational Community License Version 1.0 (the "License");
+ * By obtaining, using and/or copying this Original Work, you agree that you have read,
+ * understand, and will comply with the terms and conditions of the Educational Community License.
+ * You may obtain a copy of the License at:
+ *
+ *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ **********************************************************************************/
 package org.sakaiproject.tool.assessment.test.integration.context;
 
 import junit.framework.*;
@@ -97,22 +119,20 @@ public void testGetAuthzHelper() {
   testAuthz.testGetAssessments();
   System.out.println("testGetAssessmentsByAgentAndFunction()");
   testAuthz.testGetAssessmentsByAgentAndFunction();
-  // these seem to be a problem in standalone
-//  System.out.println("testGetAuthorizationByAgentAndFunction()");
-//  testAuthz.testGetAuthorizationByAgentAndFunction();
-//  System.out.println("testGetAuthorizationByFunctionAndQualifier()");
-//  testAuthz.testGetAuthorizationByFunctionAndQualifier();
-//  System.out.println("testGetAuthorizationToViewAssessments()");
-//  testAuthz.testGetAuthorizationToViewAssessments();
+  System.out.println("testGetAuthorizationByAgentAndFunction()");
+  testAuthz.testGetAuthorizationByAgentAndFunction();
+  System.out.println("testGetAuthorizationByFunctionAndQualifier()");
+  testAuthz.testGetAuthorizationByFunctionAndQualifier();
+  System.out.println("testGetAuthorizationToViewAssessments()");
+  testAuthz.testGetAuthorizationToViewAssessments();
+  System.out.println("testRemoveAuthorizationByQualifier()");
+  testAuthz.testRemoveAuthorizationByQualifier();
   System.out.println("testIsAuthorized()");
   testAuthz.testIsAuthorized();
-//  System.out.println("testRemoveAuthorizationByQualifier()");
-//  testAuthz.testRemoveAuthorizationByQualifier();
 
   System.out.println("    *** complete: testing gradebook helper methods");
 }
 
-//todo
 public void testGetGradeBookServiceHelper() {
   GradebookServiceHelper actualReturn = integrationContextFactory.getGradebookServiceHelper();
   assertNotNull(actualReturn);
