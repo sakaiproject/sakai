cd10af62875bd55ce6c34a416a9bdc65e149f287
Ed Smiley <esmiley@stanford.edu>
2005-10-11 23:09:34 +0000

    SAK-1927: reduced pakage dependencies, moved util.PagingUtilQueries* to facade
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@2518 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tool/src/java/org/sakaiproject/tool/assessment/facade/util/PagingUtilQueries.java
+++ samigo/tool/src/java/org/sakaiproject/tool/assessment/facade/util/PagingUtilQueries.java
@@ -20,7 +20,7 @@
 * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *
 **********************************************************************************/
-package org.sakaiproject.tool.assessment.util;
+package org.sakaiproject.tool.assessment.facade.util;
 
 import java.util.ArrayList;
 import java.util.List;
