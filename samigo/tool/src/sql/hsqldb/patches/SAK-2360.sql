fc683c15b9ba09ec5ec1236e5a0ca10c2148083d
Daisy Flemming <daisyf@stanford.edu>
2005-10-31 16:53:58 +0000

    SAK-1037
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@3258 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ samigo/tool/src/sql/hsqldb/patches/SAK-2360.sql
@@ -0,0 +1,5 @@
+alter table SAM_ASSESSFEEDBACK_T
+add column  (SHOWSTUDENTQUESTIONSCORE integer);
+alter table SAM_PUBLISHEDFEEDBACK_T
+add column  (SHOWSTUDENTQUESTIONSCORE integer);
+
---
+++ samigo/tool/src/sql/hsqldb/sakai_samigo.sql
@@ -0,0 +1,337 @@
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (1 , 'stanford.edu' ,'assessment.item' ,'Multiple Choice' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (2 , 'stanford.edu' ,'assessment.item' ,'Multiple Correct' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (3 , 'stanford.edu' ,'assessment.item' ,'Multiple Choice Survey' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (4 , 'stanford.edu' ,'assessment.item' ,'True - False' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (5 , 'stanford.edu' ,'assessment.item' ,'Short Answer/Essay' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (6 , 'stanford.edu' ,'assessment.item' ,'File Upload' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (7 , 'stanford.edu' ,'assessment.item' ,'Audio Recording' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (8 , 'stanford.edu' ,'assessment.item' ,'Fill in Blank' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (9 , 'stanford.edu' ,'assessment.item' ,'Matching' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (21 , 'stanford.edu' ,'assessment.section' ,'Default' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (22 , 'stanford.edu' ,'assessment.section' ,'Normal' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (41 , 'stanford.edu' ,'assessment.template' ,'Quiz' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (42 , 'stanford.edu' ,'assessment.template' ,'Homework' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (43 , 'stanford.edu' ,'assessment.template' ,'Mid Term' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (44 , 'stanford.edu' ,'assessment.template' ,'Final' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (61 , 'stanford.edu' ,'assessment' ,'Quiz' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (62 , 'stanford.edu' ,'assessment' ,'Homework' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (63 , 'stanford.edu' ,'assessment' ,'Mid Term' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (64 , 'stanford.edu' ,'assessment' ,'Final' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (0 , 'stanford.edu' ,'assessment.questionpool' ,'Default' ,
+    'Stanford Question Pool',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (30 , 'stanford.edu' ,'assessment.questionpool.access' ,'Access Denied' ,
+    'Access Denied',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (31 , 'stanford.edu' ,'assessment.questionpool.access' ,'Read Only' ,
+    'Read Only',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (32 , 'stanford.edu' ,'assessment.questionpool.access' ,'Read and Copy' ,
+    'Read and Copy',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (33 , 'stanford.edu' ,'assessment.questionpool.access' ,'Read/Write' ,
+    'Read/Write',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (34 , 'stanford.edu' ,'assessment.questionpool.access' ,'Administration' ,
+    'Adminstration',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (81 , 'stanford.edu' ,'assessment.taking' ,'Taking Assessment' ,
+    'Taking Assessment',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (101 , 'stanford.edu' ,'assessment.published' ,'A Published Assessment' ,
+    'A Published Assessment',1 ,1 , SYSDATE ,1 ,SYSDATE);
+
+
+INSERT INTO SAM_ASSESSMENTBASE_T ("ID" ,"ISTEMPLATE" ,
+    "PARENTID" ,"TITLE" ,"DESCRIPTION" ,"COMMENTS" ,
+    "ASSESSMENTTEMPLATEID" ,"TYPEID" ,"INSTRUCTORNOTIFICATION" ,
+    "TESTEENOTIFICATION" ,"MULTIPARTALLOWED" ,"STATUS" ,
+    "CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (1,1 ,0 ,'Default Template' ,'' ,'comments' , NULL
+    ,'42' ,1 ,1 ,1 ,1 ,'admin' ,SYSDATE ,'admin' ,SYSDATE  )
+;
+INSERT INTO SAM_ASSESSEVALUATION_T ("ASSESSMENTID" ,
+    "EVALUATIONCOMPONENTS" ,"SCORINGTYPE" ,"NUMERICMODELID" ,
+    "FIXEDTOTALSCORE" ,"GRADEAVAILABLE" ,"ISSTUDENTIDPUBLIC" ,
+    "ANONYMOUSGRADING" ,"AUTOSCORING" ,"TOGRADEBOOK" )
+    VALUES (1 ,'' ,1 ,'' , NULL , NULL , NULL ,1 , NULL ,1  )
+;
+INSERT INTO SAM_ASSESSFEEDBACK_T ("ASSESSMENTID" ,
+    "FEEDBACKDELIVERY" ,"SHOWQUESTIONTEXT" ,"SHOWSTUDENTRESPONSE"
+    ,"SHOWCORRECTRESPONSE" ,"SHOWSTUDENTSCORE" , "SHOWSTUDENTQUESTIONSCORE",
+    "SHOWQUESTIONLEVELFEEDBACK" ,"SHOWSELECTIONLEVELFEEDBACK" ,
+    "SHOWGRADERCOMMENTS" ,"SHOWSTATISTICS" ,"EDITCOMPONENTS" )
+    VALUES (1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,0 ,1 ,1 ,1  )
+;
+INSERT INTO SAM_ASSESSACCESSCONTROL_T ("ASSESSMENTID" ,
+    "SUBMISSIONSALLOWED" ,"SUBMISSIONSSAVED" ,"ASSESSMENTFORMAT" ,
+    "BOOKMARKINGITEM" ,"TIMELIMIT" ,"TIMEDASSESSMENT" ,
+    "RETRYALLOWED" ,"LATEHANDLING" ,"STARTDATE" ,"DUEDATE" ,
+    "SCOREDATE" ,"FEEDBACKDATE" ,"RETRACTDATE" ,"AUTOSUBMIT" ,
+    "ITEMNAVIGATION" ,"ITEMNUMBERING" ,"SUBMISSIONMESSAGE" ,
+    "RELEASETO" ,"USERNAME" ,"PASSWORD" ,"FINALPAGEURL" ,
+    "UNLIMITEDSUBMISSIONS" )
+    VALUES (1 ,NULL,1 ,1 , NULL , NULL , NULL , NULL ,1 ,
+    NULL, NULL, NULL, NULL, NULL,
+    1 ,2 ,1 ,'' ,'' ,'' ,'' ,'' ,
+    1  )
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(1, 1, 'finalPageURL_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(2, 1, 'anonymousRelease_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(3, 1, 'dueDate_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(4, 1, 'description_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(5, 1, 'metadataQuestions_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(6, 1, 'bgImage_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(7, 1, 'feedbackComponents_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(8, 1, 'retractDate_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(9, 1, 'feedbackType_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(10, 1, 'timedAssessmentAutoSubmit_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(11, 1, 'toGradebook_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(12, 1, 'displayChunking_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(13, 1, 'recordedScore_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(14, 1, 'authenticatedRelease_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(15, 1, 'displayNumbering_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(16, 1, 'submissionMessage_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(17, 1, 'releaseDate_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(18, 1, 'assessmentAuthor_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(19, 1, 'passwordRequired_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(20, 1, 'author', '')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(21, 1, 'submissionModel_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(22, 1, 'ipAccessType_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(23, 1, 'timedAssessment_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(24, 1, 'metadataAssess_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(25, 1, 'bgColor_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(26, 1, 'testeeIdentity_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(27, 1, 'templateInfo_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(28, 1, 'itemAccessType_isInstructorEditable', 'true')
+;
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(29, 1, 'lateHandling_isInstructorEditable', 'true')
+;
+
+
+insert into SAM_FUNCTIONDATA_T
+        values (1, 'take.published.assessment',
+       'Take Assessment','Take Assessment', '81');
+insert into SAM_FUNCTIONDATA_T
+        values (2, 'view.published.assessment',
+        'View Assessment','View Assessment', '81');
+insert into SAM_FUNCTIONDATA_T
+        values (3, 'submit.assessment',
+       'Submit Assessment','Submit Assessment', '81');
+insert into SAM_FUNCTIONDATA_T
+        values (4, 'submit.assessment.forgrade',
+        'Submit Assessment For Grade','Submit Assessment For Grade', '81');
