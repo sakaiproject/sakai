3dacd14ceb6eba711b3c9b78b339bf932cb91681
Glenn R. Golden <ggolden@etudes.org>
2005-11-10 20:32:58 +0000

    eclipse warning cleanup
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/gradebook@3759 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/tool/src/java/org/sakaiproject/tool/gradebook/ui/StudentViewBean.java
+++ gradebook/tool/src/java/org/sakaiproject/tool/gradebook/ui/StudentViewBean.java
@@ -27,7 +27,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
