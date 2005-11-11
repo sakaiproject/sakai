b039e8652d3b4f3017cd6063d7c8bc2651f7071e
Daisy Flemming <daisyf@stanford.edu>
2005-11-11 03:33:28 +0000

    change to log.debug instead
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@3775 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tool/src/java/org/sakaiproject/tool/assessment/ui/bean/authz/AuthorizationBean.java
+++ samigo/tool/src/java/org/sakaiproject/tool/assessment/ui/bean/authz/AuthorizationBean.java
@@ -225,14 +225,14 @@ public class AuthorizationBean implements Serializable {
      String functionName=(String)ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.facade.authz.resource.AuthzPermissions", functionKey);
      boolean privilege = PersistenceService.getInstance().getAuthzQueriesFacade().hasPrivilege(functionName);
      map.put(functionName+"_"+siteId, new Boolean(privilege));
-     log.info(functionName+"_"+siteId+"="+privilege);
+     log.debug(functionName+"_"+siteId+"="+privilege);
      return privilege;
   }
 
   public void addAdminPrivilege(boolean privilege, String functionKey, String siteId){
      String functionName=(String)ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.facade.authz.resource.AuthzPermissions", functionKey);
      map.put(functionName+"_"+siteId, new Boolean(privilege));
-     log.info(functionName+"_"+siteId+"="+privilege);
+     log.debug(functionName+"_"+siteId+"="+privilege);
   }
 
   public boolean getTakeAssessment(){
@@ -313,7 +313,7 @@ public class AuthorizationBean implements Serializable {
     Object o = map.get(functionName+"_"+siteId);
     if (o!=null)
       privilege = ((Boolean)o).booleanValue();
-    //log.info("**** authzBean:"+functionName+"_"+siteId+"="+privilege);
+    //log.debug("**** authzBean:"+functionName+"_"+siteId+"="+privilege);
     return privilege;
   }
 
