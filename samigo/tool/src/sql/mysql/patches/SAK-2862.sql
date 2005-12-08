914aacb89178e8876a77670b2521ccc612202989
Lydia Li <lydial@stanford.edu>
2005-12-08 06:47:57 +0000

    SAK-2862 please do not include this in maint branch
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@4500 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tool/src/sql/mysql/patches/SAK-2862.sql
+++ samigo/tool/src/sql/mysql/patches/SAK-2862.sql
@@ -3,3 +3,7 @@ add column  FEEDBACKAUTHORING integer;
 alter table SAM_PUBLISHEDFEEDBACK_T
 add column  FEEDBACKAUTHORING integer;
 
+INSERT INTO SAM_ASSESSMETADATA_T (ASSESSMENTMETADATAID, ASSESSMENTID, LABEL,
+    ENTRY)
+    VALUES(30, 1, 'feedbackAuthoring_isInstructorEditable', 'true')
+;
--- samigo/tool/src/sql/oracle/patches/SAK-2862.sql
+++ samigo/tool/src/sql/oracle/patches/SAK-2862.sql
@@ -3,3 +3,6 @@ add (FEEDBACKAUTHORING integer);
 alter table SAM_PUBLISHEDFEEDBACK_T
 add (FEEDBACKAUTHORING integer);
 
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL", "ENTRY")
+    VALUES(sam_assessMetaData_id_s.nextVal, 1, 'feedbackAuthoring_isInstructorEditable', 'true');
+
