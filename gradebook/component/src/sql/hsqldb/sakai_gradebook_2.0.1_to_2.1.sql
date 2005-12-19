b7d3ad6c753147580b09e980aa59d52d40f4a13b
Ray Davis <raydavis@berkeley.edu>
2005-12-19 16:57:27 +0000

    SAK-3187 Move the DB defense against duplicate grade records out of the 2.01-to-2.1 conversion script and into a 2.1 patching script
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/gradebook@4787 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/component/src/sql/hsqldb/sakai_gradebook_2.0.1_to_2.1.sql
+++ gradebook/component/src/sql/hsqldb/sakai_gradebook_2.0.1_to_2.1.sql
@@ -1,7 +1,2 @@
 -- Gradebook related tables changes needed between Sakai 2.01 and 2.1
 alter table GB_GRADABLE_OBJECT_T add column (NOT_COUNTED bit);
-
--- Guard against any bugs that might result in duplicate grade records.
-alter table GB_GRADE_RECORD_T add unique (GRADABLE_OBJECT_ID, STUDENT_ID);
-create index GB_GRADE_RECORD_G_O_IDX on GB_GRADE_RECORD_T (GRADABLE_OBJECT_ID);
-create index GB_GRADE_RECORD_STUDENT_ID_IDX on GB_GRADE_RECORD_T (STUDENT_ID);
