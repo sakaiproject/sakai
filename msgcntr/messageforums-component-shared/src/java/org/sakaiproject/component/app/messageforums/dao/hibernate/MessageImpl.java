804c6f88687bea62bbc75862b745b997b00fc9d8
Rashmi Shastri <rshastri@iupui.edu>
2005-12-19 21:34:39 +0000

    initial comparators
    git-svn-id: https://source.sakaiproject.org/svn/branches/forums-dev@4800 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-component-shared/src/java/org/sakaiproject/component/app/messageforums/dao/hibernate/MessageImpl.java
+++ msgcntr/messageforums-component-shared/src/java/org/sakaiproject/component/app/messageforums/dao/hibernate/MessageImpl.java
@@ -1,29 +1,23 @@
-/**********************************************************************************
- * $URL$
- * $Id$
- ***********************************************************************************
- *
- * Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
- *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
- * 
- * Licensed under the Educational Community License Version 1.0 (the "License");
- * By obtaining, using and/or copying this Original Work, you agree that you have read,
- * understand, and will comply with the terms and conditions of the Educational Community License.
- * You may obtain a copy of the License at:
- * 
- *      http://cvs.sakaiproject.org/licenses/license_1_0.html
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
- * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- **********************************************************************************/
+/*******************************************************************************************************
+ * $URL$ $Id$ **********************************************************************************
+ * Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University, Board
+ * of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation Licensed under the
+ * Educational Community License Version 1.0 (the "License"); By obtaining, using and/or copying this
+ * Original Work, you agree that you have read, understand, and will comply with the terms and
+ * conditions of the Educational Community License. You may obtain a copy of the License at:
+ * http://cvs.sakaiproject.org/licenses/license_1_0.html THE SOFTWARE IS PROVIDED "AS IS", WITHOUT
+ * WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************************************/
 
 package org.sakaiproject.component.app.messageforums.dao.hibernate;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
@@ -32,171 +26,260 @@ import org.sakaiproject.api.app.messageforums.Attachment;
 import org.sakaiproject.api.app.messageforums.Message;
 import org.sakaiproject.api.app.messageforums.Topic;
 import org.sakaiproject.api.app.messageforums.UniqueArrayList;
- 
-public class MessageImpl extends MutableEntityImpl implements Message {
 
-    private static final Log LOG = LogFactory.getLog(MessageImpl.class);
+public class MessageImpl extends MutableEntityImpl implements Message
+{
 
-    private String title;
-    private String body;
-    private String author;
-    private List attachments = new UniqueArrayList();
-    private String label;
-    private Message inReplyTo;
-    private String gradebook;
-    private String gradebookAssignment;
-    private String typeUuid;
-    private Boolean approved;
-    private Boolean draft;
-    private Topic topic;
+  private static final Log LOG = LogFactory.getLog(MessageImpl.class);
+
+  private String title;
+  private String body;
+  private String author;
+  private List attachments = new UniqueArrayList();
+  private String label;
+  private Message inReplyTo;
+  private String gradebook;
+  private String gradebookAssignment;
+  private String typeUuid;
+  private Boolean approved;
+  private Boolean draft;
+  private Topic topic;
+
+  public static Comparator ATTACHMENT_COMPARATOR;
+  public static Comparator SUBJECT_COMPARATOR;
+  public static Comparator DATE_COMPARATOR;
+  public static Comparator LABEL_COMPARATOR;
+  public static Comparator AUTHORED_BY_COMPARATOR;
+
+  // indecies for hibernate
+  private int tindex;
+
+  public Topic getTopic()
+  {
+    return topic;
+  }
+
+  public void setTopic(Topic topic)
+  {
+    this.topic = topic;
+  }
+
+  public MessageImpl()
+  {
+    attachments = new ArrayList();
+  }
+
+  public Boolean getDraft()
+  {
+    return draft;
+  }
+
+  public void setDraft(Boolean draft)
+  {
+    this.draft = draft;
+  }
+
+  public Boolean getApproved()
+  {
+    return approved;
+  }
+
+  public void setApproved(Boolean approved)
+  {
+    this.approved = approved;
+  }
+
+  public List getAttachments()
+  {
+    return attachments;
+  }
+
+  public void setAttachments(List attachments)
+  {
+    this.attachments = attachments;
+  }
+
+  public String getAuthor()
+  {
+    return author;
+  }
+
+  public void setAuthor(String author)
+  {
+    this.author = author;
+  }
+
+  public String getBody()
+  {
+    return body;
+  }
+
+  public void setBody(String body)
+  {
+    this.body = body;
+  }
+
+  public String getGradebook()
+  {
+    return gradebook;
+  }
+
+  public void setGradebook(String gradebook)
+  {
+    this.gradebook = gradebook;
+  }
+
+  public String getGradebookAssignment()
+  {
+    return gradebookAssignment;
+  }
+
+  public void setGradebookAssignment(String gradebookAssignment)
+  {
+    this.gradebookAssignment = gradebookAssignment;
+  }
+
+  public Message getInReplyTo()
+  {
+    return inReplyTo;
+  }
+
+  public void setInReplyTo(Message inReplyTo)
+  {
+    this.inReplyTo = inReplyTo;
+  }
+
+  public String getLabel()
+  {
+    return label;
+  }
+
+  public void setLabel(String label)
+  {
+    this.label = label;
+  }
+
+  public String getTitle()
+  {
+    return title;
+  }
+
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
+
+  public String getTypeUuid()
+  {
+    return typeUuid;
+  }
+
+  public void setTypeUuid(String typeUuid)
+  {
+    this.typeUuid = typeUuid;
+  }
+
+  public String toString()
+  {
+    return "Message.id:" + id;
+  }
+
+  // //////////////////////////////////////////////////////////////////////
+  // helper methods for collections
+  // //////////////////////////////////////////////////////////////////////
+
+  public void addAttachment(Attachment attachment)
+  {
+    if (LOG.isDebugEnabled())
+    {
+      LOG.debug("addAttachment(Attachment " + attachment + ")");
+    }
+
+    if (attachment == null)
+    {
+      throw new IllegalArgumentException("attachment == null");
+    }
+
+    attachment.setMessage(this);
+    attachments.add(attachment);
+  }
+
+  public void removeAttachment(Attachment attachment)
+  {
+    if (LOG.isDebugEnabled())
+    {
+      LOG.debug("removeAttachment(Attachment " + attachment + ")");
+    }
+
+    if (attachment == null)
+    {
+      throw new IllegalArgumentException("Illegal attachment argument passed!");
+    }
+
+    attachment.setMessage(null);
+    attachments.remove(attachment);
+  }
+
+  public int getTindex()
+  {
+    try
+    {
+      return getTopic().getMessages().indexOf(this);
+    }
+    catch (Exception e)
+    {
+      return tindex;
+    }
+  }
+
+  public void setTindex(int tindex)
+  {
+    this.tindex = tindex;
+  }
+
+  // ============================================
+  static
+  {
+    SUBJECT_COMPARATOR = new Comparator()
+    {
+      public int compare(Object message, Object otherMessage)
+      {
+        String msg = ((Message) message).getTitle();
+        String msg2 = ((Message) otherMessage).getTitle();
+        return msg.compareTo(msg2);
+
+      }
+    };
     
-    // indecies for hibernate
-    private int tindex;    
+    DATE_COMPARATOR = new Comparator()
+    {
+      public int compare(Object message, Object otherMessage)
+      {
+        Date msg = ((Message) message).getCreated();
+        Date msg2 = ((Message) otherMessage).getCreated();
+        return msg.compareTo(msg2);
+      }
+    };
+
+    AUTHORED_BY_COMPARATOR = new Comparator()
+    {
+      public int compare(Object message, Object otherMessage)
+      {
+        String msg = ((Message) message).getAuthor();
+        String msg2 = ((Message) otherMessage).getAuthor();
+        return msg.compareTo(msg2);
+      }
+    };
     
-    public Topic getTopic() {
-        return topic;
-    }
-
-    public void setTopic(Topic topic) {
-        this.topic = topic;
-    }
-
-    public MessageImpl() {
-        attachments = new ArrayList();
-    }
-
-    public Boolean getDraft() {
-        return draft;
-    }
-
-    public void setDraft(Boolean draft) {
-        this.draft = draft;
-    }
-
-    public Boolean getApproved() {
-        return approved;
-    }
-
-    public void setApproved(Boolean approved) {
-        this.approved = approved;
-    }
-
-    public List getAttachments() {
-        return attachments;
-    }
-
-    public void setAttachments(List attachments) {
-        this.attachments = attachments;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getGradebook() {
-        return gradebook;
-    }
-
-    public void setGradebook(String gradebook) {
-        this.gradebook = gradebook;
-    }
-
-    public String getGradebookAssignment() {
-        return gradebookAssignment;
-    }
-
-    public void setGradebookAssignment(String gradebookAssignment) {
-        this.gradebookAssignment = gradebookAssignment;
-    }
-
-    public Message getInReplyTo() {
-        return inReplyTo;
-    }
-
-    public void setInReplyTo(Message inReplyTo) {
-        this.inReplyTo = inReplyTo;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getTypeUuid() {
-        return typeUuid;
-    }
-
-    public void setTypeUuid(String typeUuid) {
-        this.typeUuid = typeUuid;
-    }
+    LABEL_COMPARATOR = new Comparator()
+    {
+      public int compare(Object message, Object otherMessage)
+      {
+        String msg = ((Message) message).getLabel();
+        String msg2 = ((Message) otherMessage).getLabel();
+        return msg.compareTo(msg2);
+      }
+    };
     
-    public String toString() {
-        return "Message.id:" + id;
-    }
-
-    ////////////////////////////////////////////////////////////////////////
-    // helper methods for collections
-    ////////////////////////////////////////////////////////////////////////
-    
-    public void addAttachment(Attachment attachment) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("addAttachment(Attachment " + attachment + ")");
-        }
-        
-        if (attachment == null) {
-            throw new IllegalArgumentException("attachment == null");
-        }
-        
-        attachment.setMessage(this);
-        attachments.add(attachment);
-    }
-
-    public void removeAttachment(Attachment attachment) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("removeAttachment(Attachment " + attachment + ")");
-        }
-        
-        if (attachment == null) {
-            throw new IllegalArgumentException("Illegal attachment argument passed!");
-        }
-        
-        attachment.setMessage(null);
-        attachments.remove(attachment);
-    }
-
-    public int getTindex() {
-        try {
-            return getTopic().getMessages().indexOf(this);
-        } catch (Exception e) {
-            return tindex;
-        }
-    }
-
-    public void setTindex(int tindex) {
-        this.tindex = tindex;
-    }
-    
+  }
 }
