f2816acbf3e75e991ee96597bc5970f40d4bc234
Daisy Flemming <daisyf@stanford.edu>
2006-01-11 04:36:19 +0000

    SAK-3436
    git-svn-id: https://source.sakaiproject.org/svn/trunk/sakai/sam@5242 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/tool/src/java/org/sakaiproject/tool/assessment/ui/listener/select/SelectActionListener.java
+++ samigo/tool/src/java/org/sakaiproject/tool/assessment/ui/listener/select/SelectActionListener.java
@@ -184,13 +184,7 @@ public class SelectActionListener
           delivery.setRawScore(g.getFinalScore().toString()); // Bug 318 fix. It seems raw score should also be based on final score.
 	  delivery.setRaw(g.getFinalScore().longValue());
         }
-        System.out.println("***g.finalScore ="+delivery.getAssessmentId()+":"+g.getFinalScore());
-        System.out.println("***raw score ="+delivery.getAssessmentId()+":"+delivery.getRawScore());
 
-        //if (g.getTotalAutoScore() != null) {
-	//    delivery.setRawScore(g.getTotalAutoScore().toString());
-	//    delivery.setRaw(g.getTotalAutoScore().longValue());
-        //}
         delivery.setTimeElapse(getTimeElapsed(g.getTimeElapsed()));
         delivery.setSubmissionDate(g.getSubmittedDate());
         if (g.getSubmittedDate() != null && g.getAttemptDate() != null) {
