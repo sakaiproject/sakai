36ae5f4ad2853a517e1cfae4bca6c17545f66b62
Glenn R. Golden <ggolden@etudes.org>
2006-03-27 20:56:40 +0000

    memory api
    git-svn-id: https://source.sakaiproject.org/svn/memory/trunk@7073 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ memory/memory/api/src/java/org/sakaiproject/memory/api/MultiRefCache.java
@@ -0,0 +1,48 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.memory.api;
+
+import java.util.Collection;
+
+/**
+ * <p>
+ * MultiRefCache is a cache that holds objects and a set of references that the cached entry are dependent on - if any change, the entry is invalidated.
+ * </p>
+ */
+public interface MultiRefCache extends Cache
+{
+	/**
+	 * Cache an object
+	 * 
+	 * @param key
+	 *        The key with which to find the object.
+	 * @param payload
+	 *        The object to cache.
+	 * @param duration
+	 *        The time to cache the object (seconds).
+	 * @param ref
+	 *        One entity reference that, if changed, will invalidate this entry.
+	 * @param azgIds
+	 *        AuthzGroup ids that, if the changed, will invalidate this entry.
+	 */
+	void put(Object key, Object payload, int duration, String ref, Collection azgIds);
+}
