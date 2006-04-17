9ac668ecda467cf21bf9d6addfb1243b068f3670
Glenn R. Golden <ggolden@etudes.org>
2006-04-17 13:06:02 +0000

    migrating presence
    git-svn-id: https://source.sakaiproject.org/svn/presence/trunk@7844 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- presence/presence-api/api/src/java/org/sakaiproject/presence/cover/PresenceService.java
+++ presence/presence-api/api/src/java/org/sakaiproject/presence/cover/PresenceService.java
@@ -1,142 +1,133 @@
 /**********************************************************************************
-* $URL$
-* $Id$
-***********************************************************************************
-*
-* Copyright (c) 2003, 2004 The Regents of the University of Michigan, Trustees of Indiana University,
-*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
-* 
-* Licensed under the Educational Community License Version 1.0 (the "License");
-* By obtaining, using and/or copying this Original Work, you agree that you have read,
-* understand, and will comply with the terms and conditions of the Educational Community License.
-* You may obtain a copy of the License at:
-* 
-*      http://cvs.sakaiproject.org/licenses/license_1_0.html
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-**********************************************************************************/
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 204, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
-package org.sakaiproject.service.legacy.presence.cover;
+package org.sakaiproject.presence.cover;
 
-import org.sakaiproject.service.framework.component.cover.ComponentManager;
+import org.sakaiproject.component.cover.ComponentManager;
 
 /**
-* <p>PresenceService is a static Cover for the {@link org.sakaiproject.service.legacy.presence.PresenceService PresenceService};
-* see that interface for usage details.</p>
-* 
-* @author University of Michigan, Sakai Software Development Team
-* @version $Revision$
-*/
+ * <p>
+ * PresenceService is a static Cover for the {@link org.sakaiproject.presence.api.PresenceService PresenceService}; see that interface for usage details.
+ * </p>
+ */
 public class PresenceService
 {
 	/**
 	 * Access the component instance: special cover only method.
+	 * 
 	 * @return the component instance.
 	 */
-	public static org.sakaiproject.service.legacy.presence.PresenceService getInstance()
+	public static org.sakaiproject.presence.api.PresenceService getInstance()
 	{
-		if (ComponentManager.CACHE_SINGLETONS)
+		if (ComponentManager.CACHE_COMPONENTS)
 		{
-			if (m_instance == null) m_instance = (org.sakaiproject.service.legacy.presence.PresenceService) ComponentManager.get(org.sakaiproject.service.legacy.presence.PresenceService.class);
+			if (m_instance == null)
+				m_instance = (org.sakaiproject.presence.api.PresenceService) ComponentManager
+						.get(org.sakaiproject.presence.api.PresenceService.class);
 			return m_instance;
 		}
 		else
 		{
-			return (org.sakaiproject.service.legacy.presence.PresenceService) ComponentManager.get(org.sakaiproject.service.legacy.presence.PresenceService.class);
+			return (org.sakaiproject.presence.api.PresenceService) ComponentManager
+					.get(org.sakaiproject.presence.api.PresenceService.class);
 		}
 	}
-	private static org.sakaiproject.service.legacy.presence.PresenceService m_instance = null;
 
-	public static java.lang.String REFERENCE_ROOT = org.sakaiproject.service.legacy.presence.PresenceService.REFERENCE_ROOT;
-	public static java.lang.String EVENT_PRESENCE = org.sakaiproject.service.legacy.presence.PresenceService.EVENT_PRESENCE;
-	public static java.lang.String EVENT_ABSENCE = org.sakaiproject.service.legacy.presence.PresenceService.EVENT_ABSENCE;
+	private static org.sakaiproject.presence.api.PresenceService m_instance = null;
+
+	public static java.lang.String REFERENCE_ROOT = org.sakaiproject.presence.api.PresenceService.REFERENCE_ROOT;
+
+	public static java.lang.String EVENT_PRESENCE = org.sakaiproject.presence.api.PresenceService.EVENT_PRESENCE;
+
+	public static java.lang.String EVENT_ABSENCE = org.sakaiproject.presence.api.PresenceService.EVENT_ABSENCE;
 
 	public static java.lang.String presenceReference(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.presenceReference(param0);
 	}
 
 	public static java.lang.String locationId(java.lang.String param0, java.lang.String param1, java.lang.String param2)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.locationId(param0, param1, param2);
 	}
 
 	public static java.lang.String getLocationDescription(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.getLocationDescription(param0);
 	}
 
 	public static void setPresence(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return;
 
 		service.setPresence(param0);
 	}
 
 	public static void removePresence(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return;
 
 		service.removePresence(param0);
 	}
 
 	public static java.util.List getPresence(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.getPresence(param0);
 	}
 
 	public static java.util.List getPresentUsers(java.lang.String param0)
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.getPresentUsers(param0);
 	}
 
 	public static java.util.List getLocations()
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return null;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return null;
 
 		return service.getLocations();
 	}
 
 	public static int getTimeout()
 	{
-		org.sakaiproject.service.legacy.presence.PresenceService service = getInstance();
-		if (service == null)
-			return 0;
+		org.sakaiproject.presence.api.PresenceService service = getInstance();
+		if (service == null) return 0;
 
 		return service.getTimeout();
 	}
 }
-
-
-
