1f31b70ae69f98bcfe08215cbf57725e76a5b940
Stephen Marquard <stephen.marquard@uct.ac.za>
2006-04-23 14:51:38 +0000

    SAK-4418 Add DOCTYPE to frameset page, and remove scrolling=yes
    git-svn-id: https://source.sakaiproject.org/svn/help/trunk@8172 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- help/help-tool/src/java/org/sakaiproject/jsf/help/HelpFrameSetRender.java
+++ help/help-tool/src/java/org/sakaiproject/jsf/help/HelpFrameSetRender.java
@@ -72,25 +72,24 @@ public class HelpFrameSetRender extends Renderer
     
     helpWindowTitle = ServerConfigurationService.getString("ui.service") + " Help";
     
-    
-    
-    writer.write("<html><head><title>" + helpWindowTitle + "</title></head>");
+    writer.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
+    writer.write("<html><head><title>" + helpWindowTitle + "</title></head>\n");
     writer.write("<FRAMESET cols=\"30%, 70%\"><FRAMESET rows=\"250, 350\">");
     writer.write("<FRAME src=" + searchToolUrl + " name=\"search\"/>");
     writer.write("<FRAME src=" + tocToolUrl + " name=\"toc\"/>");
-    writer.write("</FRAMESET>");
+    writer.write("</FRAMESET>\n");
     
     Application app = context.getApplication();
     ValueBinding binding = app.createValueBinding("#{Components['org.sakaiproject.api.app.help.HelpManager']}");
     HelpManager manager  = (HelpManager) binding.getValue(context);    
                   
     if(manager.getWelcomePage() == null) {
-    	writer.write("<FRAME src=\"html/help.html\" name=\"content\" scrolling=\"yes\">");
+    	writer.write("<FRAME src=\"html/help.html\" name=\"content\"/>");
     }
     else {
-      writer.write("<FRAME src=\"content.hlp?docId=" + manager.getWelcomePage() + "\" name=\"content\" scrolling=\"yes\">");             
+      writer.write("<FRAME src=\"content.hlp?docId=" + manager.getWelcomePage() + "\" name=\"content\"/>");             
     }
                                         
-    writer.write("</FRAMESET></html>");
+    writer.write("</FRAMESET></html>\n");
   }
 }
