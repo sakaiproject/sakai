a94160e1d7f84ec886de3acfc28519b656f3e986
Andrew Thornton <andrew@caret.cam.ac.uk>
2006-05-09 21:28:45 +0000

    fixes broken test case...
    git-svn-id: https://source.sakaiproject.org/svn/rwiki/trunk@9210 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- rwiki/rwiki-util/util/src/test/uk/ac/cam/caret/sakai/rwiki/utils/test/NameHelperTest.java
+++ rwiki/rwiki-util/util/src/test/uk/ac/cam/caret/sakai/rwiki/utils/test/NameHelperTest.java
@@ -51,7 +51,7 @@ public class NameHelperTest extends TestCase
 	private final String similarLocalName = "/site/Testa";
 
 	private final String defaultName = "/site/test"
-			+ NameHelper.SPACE_SEPARATOR + NameHelper.getDefaultPage();
+			+ NameHelper.SPACE_SEPARATOR + NameHelper.DEFAULT_PAGE;
 
 	public final String testBundleName = "/uk/ac/cam/caret/sakai/rwiki/utils/test/NameHelperTest.test";
 
