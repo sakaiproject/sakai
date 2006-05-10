896486256fab9ae7351cc659b09da94846305629
Daisy Flemming <daisyf@stanford.edu>
2006-05-10 20:01:43 +0000

    fix license
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@9263 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/api/src/java/org/sakaiproject/tool/assessment/shared/api/qti/QTIServiceAPI.java
+++ samigo/api/src/java/org/sakaiproject/tool/assessment/shared/api/qti/QTIServiceAPI.java
@@ -1,25 +1,24 @@
 /**********************************************************************************
-* $URL: https://source.sakaiproject.org/svn/trunk/sakai/sam/src/org/sakaiproject/tool/assessment/services/qti/QTIService.java $
-* $Id: QTIService.java 632 2005-07-14 21:22:50Z janderse@umich.edu $
-***********************************************************************************
-*
-* Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
-*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
-*
-* Licensed under the Educational Community License Version 1.0 (the "License");
-* By obtaining, using and/or copying this Original Work, you agree that you have read,
-* understand, and will comply with the terms and conditions of the Educational Community License.
-* You may obtain a copy of the License at:
-*
-*      http://cvs.sakaiproject.org/licenses/license_1_0.html
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
-* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-**********************************************************************************/
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 
 package org.sakaiproject.tool.assessment.shared.api.qti;
 
@@ -33,7 +32,7 @@ public interface QTIServiceAPI
   /**
    * Import an assessment XML document in QTI format, extract & persist the data.
    * @param document the assessment XML document in QTI format
-   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0;
+   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0
    * @return a persisted assessment
    */
   public AssessmentIfc createImportedAssessment(Document document,
@@ -42,7 +41,7 @@ public interface QTIServiceAPI
   /**
    * Import an item XML document in QTI format, extract & persist the data.
    * @param document the item XML document in QTI format
-   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0;
+   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0
    * @return a persisted item
    */
   public ItemDataIfc createImportedItem(Document document, int qtiVersion);
@@ -53,7 +52,7 @@ public interface QTIServiceAPI
    * Note:  this service requires a Faces context.
    *
    * @param assessmentId the assessment's Id
-   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0;
+   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0
    * @return the Document with the assessment data
    */
   public Document getExportedAssessment(String assessmentId, int qtiVersion);
@@ -64,7 +63,7 @@ public interface QTIServiceAPI
    * Note:  this service requires a Faces context.
    *
    * @param itemId the item's Id
-   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0;
+   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0
    * @return the Document with the assessment data
    */
   public Document getExportedItem(String itemId, int qtiVersion);
@@ -75,7 +74,7 @@ public interface QTIServiceAPI
    * Note:  this service requires a Faces context.
    *
    * @param itemIds an array of item ids
-   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0;
+   * @param qtiVersion either 1=QTI VERSION 1.2  or 2=QTI Version 2.0
    * @return the Document with the item bank
    */
   public Document getExportedItemBank(String itemIds[], int qtiVersion);
