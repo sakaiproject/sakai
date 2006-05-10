cecc51fa1ef59419780f73e4aa46e7160d329674
Glenn R. Golden <ggolden@etudes.org>
2006-05-10 17:05:25 +0000

    added snoop to main display (implemented in Web, not here), and fixed license header
    git-svn-id: https://source.sakaiproject.org/svn/portal/trunk@9236 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-mercury/mercury/src/java/org/sakaiproject/portal/tool/MercuryPortal.java
+++ portal/portal-mercury/mercury/src/java/org/sakaiproject/portal/tool/MercuryPortal.java
@@ -1,23 +1,21 @@
 /**********************************************************************************
  * $URL$
  * $Id$
- **********************************************************************************
+ ***********************************************************************************
  *
- * Copyright (c) 2005 The Regents of the University of Michigan, Trustees of Indiana University,
- *                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+ * Copyright (c) 2005, 2006 The Sakai Foundation.
  * 
- * Licensed under the Educational Community License Version 1.0 (the "License");
- * By obtaining, using and/or copying this Original Work, you agree that you have read,
- * understand, and will comply with the terms and conditions of the Educational Community License.
- * You may obtain a copy of the License at:
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
  * 
- *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ *      http://www.opensource.org/licenses/ecl1.php
  * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
- * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
  *
  **********************************************************************************/
 
@@ -331,8 +329,8 @@ public class MercuryPortal extends HttpServlet
 		}
 		out.println("</table>");
 
-		// out.println("<H2>Snoop</H2>");
-		// snoop(out, true, getServletConfig(), req);
+		out.println("<H2>Snoop</H2>");
+		Web.snoop(out, true, getServletConfig(), req);
 
 		// close the response
 		out.println("</body></html>");
@@ -394,7 +392,7 @@ public class MercuryPortal extends HttpServlet
 		showSession(out, true);
 
 		out.println("<H2>Unknown Request</H2>");
-		snoop(out, true, getServletConfig(), req);
+		Web.snoop(out, true, getServletConfig(), req);
 
 		// close the response
 		out.println("</body></html>");
@@ -655,152 +653,6 @@ public class MercuryPortal extends HttpServlet
 		}
 
 		return buf.toString();
-	}
-
-	protected void snoop(PrintWriter out, boolean html, ServletConfig config, HttpServletRequest req)
-	{
-		Enumeration e = config.getInitParameterNames();
-		if (e != null)
-		{
-			boolean first = true;
-			while (e.hasMoreElements())
-			{
-				if (first)
-				{
-					out.println("<h1>Init Parameters</h1>");
-					out.println("<pre>");
-					first = false;
-				}
-				String param = (String) e.nextElement();
-				out.println(" " + param + ": " + config.getInitParameter(param));
-			}
-			out.println("</pre>");
-		}
-
-		out.println("<h1>Request information:</h1>");
-		out.println("<pre>");
-
-		print(out, "Request method", req.getMethod());
-		String requestUri = req.getRequestURI();
-		print(out, "Request URI", requestUri);
-		displayStringChars(out, requestUri);
-		print(out, "Request protocol", req.getProtocol());
-		String servletPath = req.getServletPath();
-		print(out, "Servlet path", servletPath);
-		displayStringChars(out, servletPath);
-		String contextPath = req.getContextPath();
-		print(out, "Context path", contextPath);
-		displayStringChars(out, contextPath);
-		String pathInfo = req.getPathInfo();
-		print(out, "Path info", pathInfo);
-		displayStringChars(out, pathInfo);
-		print(out, "Path translated", req.getPathTranslated());
-		print(out, "Query string", req.getQueryString());
-		print(out, "Content length", req.getContentLength());
-		print(out, "Content type", req.getContentType());
-		print(out, "Server name", req.getServerName());
-		print(out, "Server port", req.getServerPort());
-		print(out, "Remote user", req.getRemoteUser());
-		print(out, "Remote address", req.getRemoteAddr());
-		// print(out, "Remote host", req.getRemoteHost());
-		print(out, "Authorization scheme", req.getAuthType());
-
-		out.println("</pre>");
-
-		e = req.getHeaderNames();
-		if (e.hasMoreElements())
-		{
-			out.println("<h1>Request headers:</h1>");
-			out.println("<pre>");
-			while (e.hasMoreElements())
-			{
-				String name = (String) e.nextElement();
-				out.println(" " + name + ": " + req.getHeader(name));
-			}
-			out.println("</pre>");
-		}
-
-		e = req.getParameterNames();
-		if (e.hasMoreElements())
-		{
-			out.println("<h1>Servlet parameters (Single Value style):</h1>");
-			out.println("<pre>");
-			while (e.hasMoreElements())
-			{
-				String name = (String) e.nextElement();
-				out.println(" " + name + " = " + req.getParameter(name));
-			}
-			out.println("</pre>");
-		}
-
-		e = req.getParameterNames();
-		if (e.hasMoreElements())
-		{
-			out.println("<h1>Servlet parameters (Multiple Value style):</h1>");
-			out.println("<pre>");
-			while (e.hasMoreElements())
-			{
-				String name = (String) e.nextElement();
-				String vals[] = (String[]) req.getParameterValues(name);
-				if (vals != null)
-				{
-					out.print("<b> " + name + " = </b>");
-					out.println(vals[0]);
-					for (int i = 1; i < vals.length; i++)
-						out.println("           " + vals[i]);
-				}
-				out.println("<p>");
-			}
-			out.println("</pre>");
-		}
-
-		e = req.getAttributeNames();
-		if (e.hasMoreElements())
-		{
-			out.println("<h1>Request attributes:</h1>");
-			out.println("<pre>");
-			while (e.hasMoreElements())
-			{
-				String name = (String) e.nextElement();
-				out.println(" " + name + ": " + req.getAttribute(name));
-			}
-			out.println("</pre>");
-		}
-
-	}
-
-	protected static void print(PrintWriter out, String name, String value)
-	{
-		out.print(" " + name + ": ");
-		out.println(value == null ? "&lt;none&gt;" : value);
-	}
-
-	protected static void print(PrintWriter out, String name, int value)
-	{
-		out.print(" " + name + ": ");
-		if (value == -1)
-		{
-			out.println("&lt;none&gt;");
-		}
-		else
-		{
-			out.println(value);
-		}
-	}
-
-	protected static void displayStringChars(PrintWriter out, String str)
-	{
-		if (str == null)
-		{
-			out.print("null");
-		}
-		else
-			for (int i = 0; i < str.length(); i++)
-			{
-				int c = (int) str.charAt(i);
-				out.print(Integer.toHexString(c) + " ");
-			}
-		out.println();
 	}
 }
 
