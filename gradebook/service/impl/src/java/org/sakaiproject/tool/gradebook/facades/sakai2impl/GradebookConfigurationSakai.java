642952369ce8962977d5a89362b21d81498d40cc
Ray Davis <raydavis@berkeley.edu>
2006-05-19 19:22:22 +0000

    SAK-4535 When auto.ddl is set in Sakai, explicitly add indexes since Hibernate SchemaUpdate won't do it
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/trunk@9704 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ gradebook/service/impl/src/java/org/sakaiproject/tool/gradebook/facades/sakai2impl/GradebookConfigurationSakai.java
@@ -0,0 +1,62 @@
+/**********************************************************************************
+*
+* $Id$
+*
+***********************************************************************************
+*
+* Copyright (c) 2006 The Regents of the University of California, The MIT Corporation
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
+package org.sakaiproject.tool.gradebook.facades.sakai2impl;
+
+import java.util.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.sakaiproject.db.api.SqlService;
+
+import org.sakaiproject.service.gradebook.shared.GradebookConfiguration;
+
+/**
+ * Sakai-specific add-on to the vanilla configuration bean.
+ * The main addition is to handle automatic database upgrades which lie outside
+ * the scope of Hibernate's normal SchemaUpdate. In versions of Hibernate through
+ * at least 3.1.3, this includes any index definitions. (See Hibernate JIRA
+ * issue HHH-1012.)
+ */
+public class GradebookConfigurationSakai extends GradebookConfiguration {
+    private static final Log log = LogFactory.getLog(GradebookConfigurationSakai.class);
+
+    private SqlService sqlService;
+    private boolean autoDdl;
+
+	public void init() {
+		String sqlUpdateScriptName = "sakai_gradebook_post_schemaupdate";
+		if (autoDdl && (sqlService != null)) {
+			if (log.isInfoEnabled()) log.info("About to call sqlService.ddl with " + sqlUpdateScriptName);
+			sqlService.ddl(this.getClass().getClassLoader(), sqlUpdateScriptName);
+		}
+		super.init();
+	}
+
+	public void setSqlService(SqlService sqlService) {
+		this.sqlService = sqlService;
+	}
+
+	public void setAutoDdl(boolean autoDdl) {
+		this.autoDdl = autoDdl;
+	}
+}
