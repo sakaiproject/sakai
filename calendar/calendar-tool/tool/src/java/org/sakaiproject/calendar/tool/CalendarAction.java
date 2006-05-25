4cfe4a56fc76f80c266fe4d0c3a0ef5943afbf28
Glenn R. Golden <ggolden@etudes.org>
2006-05-25 20:08:59 +0000

    SAK-4682 - adjust resources tool and permissions helper to support folder level permissions editing
    git-svn-id: https://source.sakaiproject.org/svn/calendar/trunk@9987 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
+++ calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
@@ -7156,7 +7156,7 @@ extends VelocityPortletStateAction
 		String siteRef = SiteService.siteReference(calendarRef.getContext());
 
 		// setup for editing the permissions of the site for this tool, using the roles of this site, too
-		state.setAttribute(PermissionsHelper.SITE_REF, siteRef);
+		state.setAttribute(PermissionsHelper.TARGET_REF, siteRef);
 
 		// ... with this description
 		state.setAttribute(PermissionsHelper.DESCRIPTION, rb.getString("java.set")
