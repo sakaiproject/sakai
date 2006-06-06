c3a5671bbde43361023dc5286a169024611efd3c
Ian Boston <ieb@tfd.co.uk>
2006-06-06 12:20:34 +0000

    Removed SQL files, as we not use HBM to create the tables
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@10223 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/sql/hsqldb/search_cluster.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE search_segments
-(
-	name_ varchar(255) not null,
-	version_ BIGINT not null,
-	size_ BIGINT not null,
-	packet_ BINARY,
-    CONSTRAINT search_segments_index UNIQUE (name_)
-);
