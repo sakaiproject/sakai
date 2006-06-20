119e37f2a951649bdfbb75a58582719d728fb931
Lydia Li <lydial@stanford.edu>
2006-06-20 21:34:13 +0000

    SAK-5084
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@10937 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/integration/helper/integrated/AgentHelperImpl.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/integration/helper/integrated/AgentHelperImpl.java
@@ -86,6 +86,7 @@ public class AgentHelperImpl implements AgentHelper
    * @return the agent string.
    */
   public String getAgentString(String agentString){
+//log.info("getAgentString(string) = " + agentString);
     String agentS="";
     // this is anonymous user sign 'cos sakai doesn't know about them-daisyf
     try
@@ -99,7 +100,9 @@ public class AgentHelperImpl implements AgentHelper
       }
       else
       {
-        agentS = user.getId();
+//log.info("userdisplayid = " + user.getDisplayId());
+//log.info("userdisplay name = " + user.getDisplayName());
+        agentS = user.getDisplayId();
       }
     }
     catch (Exception ex)
