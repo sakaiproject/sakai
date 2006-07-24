d3d0bb899af0420a3c0740b9c16dc3e3edaa3187
Gonzalo Silverio <gsilver@umich.edu>
2006-07-24 20:12:00 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-5665
    git-svn-id: https://source.sakaiproject.org/svn/site/trunk@12745 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site/site-impl/impl/src/java/org/sakaiproject/site/impl/BaseSiteService.java
+++ site/site-impl/impl/src/java/org/sakaiproject/site/impl/BaseSiteService.java
@@ -1540,8 +1540,8 @@ public abstract class BaseSiteService implements SiteService, StorageUser
 					out
 							.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
 					out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
-					out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
 					out.println("<head>");
+					out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
 					out.println("<meta http-equiv=\"Content-Style-Type\" content=\"text/css\" />");
 					out.println("<link href=\"" + skinRepo
 							+ "/tool_base.css\" type=\"text/css\" rel=\"stylesheet\" media=\"all\" />");
