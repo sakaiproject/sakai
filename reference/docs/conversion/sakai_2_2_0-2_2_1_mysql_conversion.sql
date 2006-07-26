071a77ef0d02902c9036eca33602eccce8185a67
Lance Speelmon <lance@rsmart.com>
2006-07-26 17:18:23 +0000

    OSP-1607 http://bugs.osportfolio.org/jira/browse/OSP-1607
    Increasing the size of fields that hold a site id to 99.  Some were not specifying a length and would result in a length of 255.  I'm leaving those alone for now.
    Created mysql and oracle conversion scripts for these field size changes.
    
    
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@12990 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ reference/docs/conversion/sakai_2_2_0-2_2_1_mysql_conversion.sql
@@ -0,0 +1,8 @@
+alter table osp_guidance modify column site_id varchar(99);
+alter table osp_review modify column site_id varchar(99);
+alter table osp_style modify column site_id varchar(99);
+alter table osp_site_tool modify column site_id varchar(99);
+alter table osp_presentation_template modify column site_id varchar(99);
+alter table osp_presentation modify column site_id varchar(99);
+alter table osp_presentation_layout modify column site_id varchar(99);
+alter table osp_wizard modify column site_id varchar(99);
---
+++ reference/docs/conversion/sakai_2_2_0-2_2_1_oracle_conversion.sql
@@ -0,0 +1,8 @@
+alter table osp_guidance modify site_id varchar2(99);
+alter table osp_review modify site_id varchar2(99);
+alter table osp_style modify site_id varchar2(99);
+alter table osp_site_tool modify site_id varchar2(99);
+alter table osp_presentation_template modify site_id varchar2(99);
+alter table osp_presentation modify site_id varchar2(99);
+alter table osp_presentation_layout modify site_id varchar2(99);
+alter table osp_wizard modify site_id varchar2(99);
