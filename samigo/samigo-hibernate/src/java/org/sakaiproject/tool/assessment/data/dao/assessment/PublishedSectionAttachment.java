92b5e9cbd75050138c3c02b8f54eba38ab3c1762
Daisy Flemming <daisyf@stanford.edu>
2006-09-12 16:39:10 +0000

    SAK-4396
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@14414 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ samigo/samigo-hibernate/src/java/org/sakaiproject/tool/assessment/data/dao/assessment/PublishedSectionAttachment.java
@@ -0,0 +1,77 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/svn/sam/trunk/samigo-hibernate/src/java/org/sakaiproject/tool/assessment/data/dao/grading/MediaData.java $
+ * $Id: MediaData.java 11438 2006-06-30 20:06:03Z daisyf@stanford.edu $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2004, 2005, 2006 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.tool.assessment.data.dao.assessment;
+
+import org.sakaiproject.tool.assessment.data.ifc.assessment.SectionAttachmentIfc;
+import org.sakaiproject.tool.assessment.data.ifc.assessment.SectionDataIfc;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class PublishedSectionAttachment
+    extends PublishedAttachmentData
+    implements java.io.Serializable, SectionAttachmentIfc
+{
+  private SectionDataIfc section;
+
+  public PublishedSectionAttachment()  { }
+
+  public PublishedSectionAttachment(Long attachmentId, SectionDataIfc section, String resourceId,
+                   String filename, String mimeType,
+                   Long fileSize, String description, String location,
+                   Boolean isLink, Integer status, String createdBy, Date createdDate,
+                   String lastModifiedBy, Date lastModifiedDate){
+    super(attachmentId, resourceId, SectionAttachmentIfc.SECTION_ATTACHMENT,
+          filename, mimeType,
+          fileSize, description, location, isLink, status,
+          createdBy, createdDate, lastModifiedBy, lastModifiedDate);
+    this.section = section;
+  }
+
+  public PublishedSectionAttachment(Long attachmentId, String resourceId,
+                   String filename, String mimeType,
+                   Long fileSize, String description, String location,
+                   Boolean isLink, Integer status, String createdBy, Date createdDate,
+                   String lastModifiedBy, Date lastModifiedDate){
+    super(attachmentId, resourceId, SectionAttachmentIfc.SECTION_ATTACHMENT,
+          filename, mimeType,
+          fileSize, description, location, isLink, status,
+          createdBy, createdDate, lastModifiedBy, lastModifiedDate);
+  }
+
+  public SectionDataIfc getSection() {
+    return section;
+  }
+
+  public void setSection(SectionDataIfc section) {
+    this.section = section;
+  }
+
+  public Long getAttachmentType() {
+    return SectionAttachmentIfc.SECTION_ATTACHMENT;
+  }
+
+  public void setAttachmentType(Long attachmentType) {
+  }
+
+
+}
