c6b79a9f250ce0a22586bab6b46fc2655a9e2b21
Louis Majanja <louis@media.berkeley.edu>
2006-09-15 23:54:58 +0000

    SAK-6255 added a local custom tag and converter hat overides the JSF spec buy using the the local time instead of GMT
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/trunk@14753 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/jsf/convertDateTime/ConvertDateTimeTag.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2006 The Regents of the University of California, The MIT Corporation
+ *
+ *  Licensed under the Educational Community License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ecl1.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+
+package org.sakaiproject.tool.gradebook.jsf.convertDateTime;
+
+import javax.faces.webapp.ConverterTag;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * Simple tag that overrides the spec ConvertDateTimeTag and uses TimeZone.getDefault() as the
+ * base timezone, rather than GMT.
+ * *
+ * Author:Louis Majanja <louis@media.berkeley.edu>
+ * Date: Sep 15, 2006
+ * Time: 12:27:22 PM
+ */
+public class ConvertDateTimeTag extends ConverterTag {
+
+        /**
+     * serial version id for correct serialisation versioning
+     */
+    private static final long serialVersionUID = 1L;
+
+
+    public ConvertDateTimeTag()
+    {
+        setConverterId(org.sakaiproject.tool.gradebook.jsf.convertDateTime.DateTimeConverter.CONVERTER_ID);
+    }
+
+
+    public void setPageContext(PageContext context)
+    {
+        super.setPageContext(context);
+        setConverterId(DateTimeConverter.CONVERTER_ID);
+    }
+}
---
+++ gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/jsf/convertDateTime/DateTimeConverter.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2006 The Regents of the University of California, The MIT Corporation
+ *
+ *  Licensed under the Educational Community License, Version 1.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ecl1.php
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+
+package org.sakaiproject.tool.gradebook.jsf.convertDateTime;
+
+import java.util.TimeZone;
+
+
+/**
+ * Simple converter that overrides the spec DateTimeConverter and uses TimeZone.getDefault() as the
+ * base timezone, rather than GMT.
+ *
+ * Author:Louis Majanja <louis@media.berkeley.edu>
+ * Date: Sep 15, 2006
+ * Time: 12:16:10 PM
+ */
+public class DateTimeConverter extends javax.faces.convert.DateTimeConverter {
+
+    public static final String CONVERTER_ID =  org.sakaiproject.tool.gradebook.jsf.convertDateTime.DateTimeConverter.class.getName();
+
+    public DateTimeConverter()
+    {
+        setTimeZone(TimeZone.getDefault());
+    }
+
+}
