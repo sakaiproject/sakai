3b97fd7aa49b7093c059fbab939229500e89eb83
Charles Hedrick <hedrick@rutgers.edu>
2006-09-24 15:19:04 +0000

    add more details
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@15178 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-api/api/src/java/org/sakaiproject/content/api/DavManager.java
+++ content/content-api/api/src/java/org/sakaiproject/content/api/DavManager.java
@@ -21,9 +21,50 @@ import org.sakaiproject.exception.TypeException;
  */
 public interface DavManager 
 {
+        /* General question on locks:
+	 * DAV has a locking scheme. Currently it is implemented entirely within DavServlet.
+	 * Since Sakai also has locking, we end up with two locking systems that
+	 * don't talk. If we do things like copying collections in Content, it also
+	 * makes it nearly impossible to do what the protocol suggests and copy
+	 * all but the locked items, returning a list of what was and was not
+	 * copied. At the moment we simply give an error for the whole operation.
+	 * I believe that is legal. I am also skeptical about the ability of clients
+	 * to cope with partial success. However to produce a maximally compliant DAV,
+	 * we would probably need to move DAV locking into Content.
+	 */
+
+        /* General comment on error handling:
+	 * DAV has the possibility to return an XML structure listing items and error codes. 
+	 * If an operation on a collection fails for some items in the collection, the spec requires
+	 * us to return a list of all items indicating where it succeeded and where it failed.
+	 * Operations here must either succeed in their entirety, fail and back out of all 
+	 * changes, or return a list of items and status. It would clearly be easier not to
+	 * return such a list. If that is the intent, care must be taken to avoid partial
+	 * failure. E.g. in copying collections where an existing one will be deleted, it
+	 * would be prudent to copy to a new name, verify that the copy worked, and then
+	 * delete the old one and rename the new one.
+	 */
+
+        /* General comment on return values:
+	 * These operations all return boolean. The reason is that DAV has two different
+	 * success codes: 201 Created and 204 No content. We must distinguish between
+	 * cases where new content was created and where it was not. E.g. the spec for
+	 * copy says that 201 is returned if a new resource is created, while
+	 * 204 is returned if the resource existed. Operations are expected to
+	 * return true for Created and false for No Content. Some operations will
+	 * always return the same value. This is noted in the descriptions.
+	 * At the HTTP level, 201 returns a URI for the new resources. This allows for
+	 * the possibility that we could use a different name than the requested one.
+	 * If we wanted to do that, then these functions would return null or a
+	 * String. However because clients will typically use these functions to
+	 * model a Unix file system, I strongly recommend against doing this.
+	 */
+
 	/**
 	 * Copies a resource or collection along with all attributes and properties.  
-	 * If the parameter overwrite is true, replace existing entity (or entities).  
+	 * If the parameter overwrite is true, delete existing entity (or entities).  
+	 * Otherwise update them. For collections, preexisting items from newId that
+	 * do not have corresponding items in oldId will remain.
 	 * If the parameter oldId identifies a collection and the parameter recursive
 	 * is true, copy the entire hierarchy of collections and resources rooted at
 	 * oldId. If the parameter oldId identifies a collection and the parameter 
@@ -33,18 +74,17 @@ public interface DavManager
 	 *  <li>no entity exists with the identifier oldId</li>
 	 *  <li>the user does not have permission to access the entity oldId</li>
 	 *  <li>the user does not have permission to create the entity newId</li>
-	 *  <li>overwrite is true, the entity newId exists, and the user does not have permission to modify/delete oldId</li>
-	 *  <li>overwrite is true, the entity newId exists, and the entity newId (or one of its members) is in use by another user</li>
-	 *  <li>overwrite is false and the entity newId already exists</li>
+	 *  <li>the entity newId exists, and the user does not have permission to modify/delete oldId</li>
+	 *  <li>the entity newId exists, and the entity newId (or one of its members) is in use by another user</li>
+	 *  <li>overwrite is false and one of newId, oldId is a resource and the other a collection</li>
 	 *  <li>the containing collection for newId does not exist</li>
 	 *  <li>newId is too long to be used as a resource-id or collection-id</li>
-	 *  <li>either oldId or newId identifies a collection and the other identifies a resource</li>
 	 *  <li>the server is configured to write the resource body to the filesystem and an attempt to save the resource body fails</li>
+	 *  <li>the source and destination objects are the same
+	 *  <li>writing the new resource puts the user over quota
 	 * </ul>
-	 * TODO: if overwrite is true, recursive is false and newId exists, is it an error if newId is not empty? 
-	 * what should happen to items contained by newId if recursive is false or if there are not entities with 
-	 * corresponding id's in oldId?
-	 * TODO: when replacing an existing entity, does user need permission to 'modify' or 'delete' the existing entity?
+	 * If overwrite is set, the nature of any existing destination does not matter. It will be deleted.
+	 * Return: true if a new collection or resource was created, i.e. newId did not exist previously
 	 * 
 	 * @param oldId
 	 * @param newId
@@ -52,34 +92,47 @@ public interface DavManager
 	 * @param recursive
 	 * 
 	 * @throws PermissionException
-	 *            if the user does not have permission to delete 
+	 *            if the user does not have permission to access the old entity, delete 
 	 *            the existing entity or create the new entity.
-	 * @throws IdUsedException
-	 *            if overwrite is false and the newId is already in use.
 	 * @throws IdUnusedException
 	 *            if oldIdd does not exist.
 	 * @throws InconsistentException
 	 *            if the containing collection (for newId) does not exist.
+         * @throws TypeException
+         *            if overwrite is false, and 
+	 *            newId and oldId do not identify the same kind of entities 
+         *            (must both be identifiers for collections OR must both
+         *            be identifiers for collections)
 	 * @throws IdLengthException
 	 *            if the newId is too long.
 	 * @throws InUseException
 	 *            if the entity specified by oldId is currently in use 
 	 *            by another user or, if overwrite is true, the entity 
 	 *            identified by newId exists and is in use by another user.
-	 * @throws TypeException
-	 *            if newId and oldId do not identify the same kind of entities 
-	 *            (must both be identifiers for collections OR must both
-	 *            be identifiers for collections)
 	 * @throws ServerOverloadException
 	 *            if the server is configured to write the resource body to the 
 	 *            filesystem and an attempt to save the resource body fails. 
+	 * @throws OverQuotaException
+	 *            if writing the new resource puts the user over quota
 	 */
-	public void davCopy(String oldId, String newId, boolean overwrite, boolean recursive)  
+	public boolean davCopy(String oldId, String newId, boolean overwrite, boolean recursive)  
 			throws PermissionException, InconsistentException, IdLengthException, 
 					InUseException, ServerOverloadException;
 	
 	/**
 	 * 
+	 * Make a new collection.
+	 * The new collection is empty.
+         * The operation succeeds unless one of the following occurs:
+	 * <ul>
+	 *  <li>the user does not have permission to create the entity newId</li>
+	 *  <li>an entity newId exists
+	 *  <li>newId is too long to be used as a resource-id or collection-id</li>
+	 *  <li>the server is configured to write the resource body to the filesystem and an attempt to save the resource body fails</li>
+	 *  <li>writing the new collection puts the user over quota
+	 * </ul>
+	 * If successful, always returns true, because a new collection is always created
+	 *
 	 * @param newId
 	 *            
 	 * @throws PermissionException
@@ -92,26 +145,44 @@ public interface DavManager
 	 *            if the newId is too long.
 	 * @throws IdInvalidException
 	 *            if the resource id is invalid.
+	 * @throws ServerOverloadException
+	 *            if the server is configured to write the collection to the
+	 *            filesystem and it fails
+	 * @throws OverQuotaException
+	 *            if writing the new collection puts the user over quota
 	 */
-	public void davCreateCollection(String newId) 
+	public boolean davCreateCollection(String newId) 
 			throws PermissionException, IdUsedException, InconsistentException, 
 					IdLengthException, IdInvalidException;
 	
 	/**
-	 * Create the entity or update it if it exists.
+	 * Create the resource or update it if it exists.
 	 * if newid exists and is resource, update its contents, retaining properities; 
 	 * however delete followed by create is  OK as long as it preserves the  old
-	 * properties
+	 * properties.
+	 * If newid did not exist, use specified content type, if any. 
+	 * If contenttype is null, look at the extension. If no extension or unknown, use text/plain
+	 * The operation succeeds unless one of the following is true:
+	 * <ul>
+	 *  <li>newid exists and is a collection
+	 *  <li>user does not have permission to create or write the new object
+	 *  <li>the containing collection does not exist
+	 *  <li>newId is too long to be used as a resource-id or collection-id</li>
+	 *  <li>the server is configured to write the resource body to the filesystem and an attempt to save the resource body fails</li>
+	 *  <li>writing the new resource puts the user over quota
+	 * </ul>
+	 * Returns true if a new resource is created, i.e. if newId did not exist
 	 * 
 	 * @param newId
 	 * @param content
 	 * @param contenttype
 	 * 
 	 * @throws PermissionException
-	 *            if the user does not have permission to create this entity.
+	 *            if the user does not have permission to create or write to this entity.
 	 * @throws TypeException
 	 *            if the id is already in use and identifies a collection. 
 	 *            TODO: Is this the right exception in that case?
+	 *            [I don't care what exception you use, as long as it is documented.]
 	 * @throws InconsistentException
 	 *            if the containing collection does not exist.
 	 * @throws IdLengthException
@@ -121,14 +192,23 @@ public interface DavManager
 	 * @throws ServerOverloadException
 	 *            if the server is configured to write the resource body to the 
 	 *            filesystem and an attempt to save the resource body fails.
+	 * @throws OverQuotaException
+	 *            if writing the new collection puts the user over quota
 	 */
-	public void davCreateResource(String newId, InputStream content, String contentType) 
+	public boolean davCreateResource(String newId, InputStream content, String contentType) 
 			throws PermissionException, TypeException, InconsistentException, IdLengthException, 
 					InUseException, ServerOverloadException;
 	
 	/**
 	 * Delete a collection or resource. If entity is a non-empty collection,
 	 * remove it and everything it contains.
+	 * Fails if
+	 * <ul>
+	 *  <li>no entity exists with the identifier entityId</li>
+	 *  <li>the user does not have permission to delete the entity</li>
+	 *  <li>the server is configured to write the resource body to the filesystem and an attempt to delete it fails</li>
+	 * </ul>
+	 * Always returns false.
 	 * 
 	 * @param entityId
 	 * @throws PermissionException
@@ -136,8 +216,13 @@ public interface DavManager
 	 * @throws InUseException
 	 *            if the entity (or one of its members) is currently in use
 	 *            by another user.
+	 * @throws IdUnusedException
+	 *            if entityId does not exist.
+	 * @throws ServerOverloadException
+	 *            if the server is configured to write the resource body to the 
+	 *            filesystem and an attempt to delete it fails. 
 	 */
-	public void davDelete(String entityId) 
+	public boolean davDelete(String entityId) 
 			throws PermissionException, IdUnusedException, InUseException;
 	
 	/**
@@ -186,11 +271,10 @@ public interface DavManager
 			throws PermissionException, IdUnusedException;
 
 	/**
-	 * Moves a resource or collection along with all attributes and properties,
-	 * unless the entity identified by newId exists, in which case certain 
-	 * properties and attributes are retained for existing entities that are 
-	 * being replaced.   
-	 * If the parameter overwrite is true, replace existing entity (or entities).  
+	 * Moves a resource or collection along with all attributes and properties.
+	 * No properties are taken from any existing entity.
+	 * If the parameter overwrite is true, delete existing entities before
+	 *  the move; otherwise fail if the new entity exists.
 	 * If the parameter oldId identifies a collection, move the entire hierarchy 
 	 * of collections and resources rooted at oldId. The copy succeeds unless 
 	 * one of these errors occurs:
@@ -198,18 +282,17 @@ public interface DavManager
 	 *  <li>no entity exists with the identifier oldId</li>
 	 *  <li>the user does not have permission to access the entity oldId</li>
 	 *  <li>the user does not have permission to create the entity newId</li>
-	 *  <li>overwrite is true, the entity newId exists, and the user does not have permission to modify/delete oldId</li>
+	 *  <li>the user does not have permission to delete oldId</li>
+	 *  <li>overwrite is true, the entity newId exists, and the user does not have permission to delete newId</li>
 	 *  <li>overwrite is true, the entity newId exists, and the entity newId (or one of its members) is in use by another user</li>
 	 *  <li>overwrite is false and the entity newId already exists</li>
 	 *  <li>the containing collection for newId does not exist</li>
 	 *  <li>newId is too long to be used as a resource-id or collection-id</li>
-	 *  <li>either oldId or newId identifies a collection and the other identifies a resource</li>
 	 *  <li>the server is configured to write the resource body to the filesystem and an attempt to save the resource body fails</li>
+	 *  <li>the source and destination objects are the same
+	 *  <li>writing the new resource puts the user over quota
 	 * </ul>
-	 * TODO: if overwrite is true and newId exists, is it an error if newId is not empty? 
-	 * what should happen to items contained by newId if there are not entities with 
-	 * corresponding id's in oldId?
-	 * TODO: when replacing an existing entity, does user need permission to 'modify' or 'delete' the existing entity?
+	 * returns true if a new entity is created, i.e. newId did not exist
 	 * 
 	 * @param oldId
 	 * @param newId
@@ -217,7 +300,7 @@ public interface DavManager
 	 * 
 	 * @throws PermissionException
 	 *            if the user does not have permission to delete 
-	 *            the existing entity or create the new entity.
+	 *            the existing entity, or delete/create the new entity.
 	 * @throws IdUsedException
 	 *            if overwrite is false and the newId is already in use.
 	 * @throws IdUnusedException
@@ -230,15 +313,13 @@ public interface DavManager
 	 *            if the entity specified by oldId is currently in use 
 	 *            by another user or, if overwrite is true, the entity 
 	 *            identified by newId exists and is in use by another user.
-	 * @throws TypeException
-	 *            if newId and oldId do not identify the same kind of entities 
-	 *            (must both be identifiers for collections OR must both
-	 *            be identifiers for collections)
 	 * @throws ServerOverloadException
 	 *            if the server is configured to write the resource body to the 
 	 *            filesystem and an attempt to save the resource body fails. 
+	 * @throws OverQuotaException
+	 *            if writing the new resource puts the user over quota
 	 */
-	public void davRename(String oldId, String newId, boolean overwrite) 
+	public boolean davRename(String oldId, String newId, boolean overwrite) 
 			throws PermissionException, IdUsedException, InconsistentException, IdLengthException, 
 					InUseException, TypeException, ServerOverloadException;
 	
