75105df23b5c5a0bfa5aff39f2508bbe35904ed9
Glenn R. Golden <ggolden@etudes.org>
2006-09-28 18:27:14 +0000

    SAK-6567 - fixed mysql conversion as per this JIRA
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@15393 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_2_1-2_3_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_2_1-2_3_0_mysql_conversion.sql
@@ -198,14 +198,10 @@ alter table CM_OFFICIAL_INSTRUCTORS_T add index FK470F8ACCC28CC1AD (ENROLLMENT_S
 ----------------------------------------------------------------------------------------------------------------------------------------
 -- SAM (SAK-4396)
 
-alter table SAM_ATTACHMENT_T drop foreign key FK99FA8CB8CAC2365B;
-alter table SAM_ATTACHMENT_T drop foreign key FK99FA8CB83288DBBD;
-alter table SAM_ATTACHMENT_T drop foreign key FK99FA8CB870CE2BD;
-alter table SAM_PUBLISHEDATTACHMENT_T drop foreign key FK270998869482C945;
-alter table SAM_PUBLISHEDATTACHMENT_T drop foreign key FK2709988631446627;
-alter table SAM_PUBLISHEDATTACHMENT_T drop foreign key FK27099886895D4813;
+SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists SAM_ATTACHMENT_T;
 drop table if exists SAM_PUBLISHEDATTACHMENT_T;
+SET FOREIGN_KEY_CHECKS = 1;
 create table SAM_ATTACHMENT_T (ATTACHMENTID bigint not null auto_increment, ATTACHMENTTYPE varchar(255) not null, RESOURCEID varchar(255), FILENAME varchar(255), MIMETYPE varchar(80), FILESIZE integer, DESCRIPTION varchar(4000), LOCATION varchar(4000), ISLINK integer, STATUS integer not null, CREATEDBY varchar(36) not null, CREATEDDATE datetime not null, LASTMODIFIEDBY varchar(36) not null, LASTMODIFIEDDATE datetime not null, ASSESSMENTID bigint, SECTIONID bigint, ITEMID bigint, primary key (ATTACHMENTID));
 create table SAM_PUBLISHEDATTACHMENT_T (ATTACHMENTID bigint not null auto_increment, ATTACHMENTTYPE varchar(255) not null, RESOURCEID varchar(255), FILENAME varchar(255), MIMETYPE varchar(80), FILESIZE integer, DESCRIPTION varchar(4000), LOCATION varchar(4000), ISLINK integer, STATUS integer not null, CREATEDBY varchar(36) not null, CREATEDDATE datetime not null, LASTMODIFIEDBY varchar(36) not null, LASTMODIFIEDDATE datetime not null, ASSESSMENTID bigint, SECTIONID bigint, ITEMID bigint, primary key (ATTACHMENTID));
 alter table SAM_ATTACHMENT_T add index FK99FA8CB8CAC2365B (ASSESSMENTID), add constraint FK99FA8CB8CAC2365B foreign key (ASSESSMENTID) references SAM_ASSESSMENTBASE_T (ID);
