ac76dad8c8547aa2835d3924fe48c16b543b4d86
Glenn R. Golden <ggolden@etudes.org>
2006-11-01 14:00:04 +0000

    fixed copyright in java files for archive
    git-svn-id: https://source.sakaiproject.org/svn/archive/trunk@17722 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- archive/import-impl/src/java/org/sakaiproject/importer/impl/importables/Announcement.java
+++ archive/import-impl/src/java/org/sakaiproject/importer/impl/importables/Announcement.java
@@ -1,16 +1,23 @@
-/* *****************************************************************************
- * Announcement.java - created by aaronz
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/svn/access/trunk/access-impl/impl/src/java/org/sakaiproject/access/tool/AccessServlet.java $
+ * $Id: AccessServlet.java 17063 2006-10-11 19:48:42Z jimeng@umich.edu $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006 The Sakai Foundation.
  * 
- * Copyright (c) 2006 Virginia Polytechnic Institute and State University
- * Licensed under the Educational Community License version 1.0
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
  * 
- * A copy of the Educational Community License has been included in this 
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ *      http://www.opensource.org/licenses/ecl1.php
  * 
- * Contributors:
- * Aaron Zeckoski (aaronz@vt.edu)
- * 
- * ****************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.importer.impl.importables;
 
