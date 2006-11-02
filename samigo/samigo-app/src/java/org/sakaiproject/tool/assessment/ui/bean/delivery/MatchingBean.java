03ba8688040bac1940a53ad087a5157ef333d44b
Karen Tsao <ktsao@stanford.edu>
2006-11-02 21:11:50 +0000

    SAK-5535
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@17859 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/MatchingBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/MatchingBean.java
@@ -94,6 +94,13 @@ public class MatchingBean
       items.add(data);
       parent.setItemGradingDataArray(items);
     }
+    
+    // Fixed for SAK-5535
+    // If we don't reset published answer id to null, the previous selected value will remain in the bean
+    // That is, the "select" selection in dropdown will never be set
+    if (newresp.equals("0")) {
+    	data.setPublishedAnswerId(null);
+    }
     Iterator iter = itemText.getAnswerSet().iterator();
     while (iter.hasNext())
     {
