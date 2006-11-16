808c034b365d722298fc9f825803c5a4f0e192f4
Daisy Flemming <daisyf@stanford.edu>
2006-11-16 17:23:35 +0000

    SAK-6790
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@18184 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_3_0-2_4_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_3_0-2_4_0_mysql_conversion.sql
@@ -11,3 +11,24 @@
 -- OSP conversion
 alter table osp_presentation_template add column propertyFormType varchar(36);
 alter table osp_presentation add column property_form varchar(36);
+
+-- SAMIGO SAK-6790 conversion
+alter table SAM_ASSESSMENTBASE_T MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_SECTION_T MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_PUBLISHEDASSESSMENT_T MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_PUBLISHEDSECTION_T MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_ITEM_T MODIFY  ITEMIDSTRING varchar(255), MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_ITEMFEEDBACK_T MODIFY TYPEID varchar(255) not null;
+alter table SAM_ANSWERFEEDBACK_T MODIFY TYPEID varchar(255);
+alter table SAM_ATTACHMENT_T MODIFY RESOURCEID TEXT,  MODIFY CREATEDBY varchar(255) not null, MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_PUBLISHEDITEM_T MODIFY  ITEMIDSTRING varchar(255), MODIFY  CREATEDBY varchar(255) not null,  MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_PUBLISHEDITEMFEEDBACK_T MODIFY TYPEID varchar(255) not null;
+alter table SAM_PUBLISHEDANSWERFEEDBACK_T MODIFY TYPEID varchar(255);
+alter table SAM_PUBLISHEDATTACHMENT_T  MODIFY RESOURCEID TEXT, MODIFY CREATEDBY varchar(255) not null, MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_AUTHZDATA_T MODIFY AGENTID varchar(255) not null, MODIFY FUNCTIONID varchar(36) not null, MODIFY QUALIFIERID varchar(36) not null;
+alter table SAM_AUTHZDATA_T MODIFY LASTMODIFIEDBY varchar(255) not null;
+alter table SAM_ASSESSMENTGRADING_T MODIFY AGENTID varchar(255) not null, MODIFY GRADEDBY varchar(255);
+alter table SAM_ITEMGRADING_T MODIFY AGENTID varchar(255) not null, MODIFY GRADEDBY varchar(255);
+alter table SAM_GRADINGSUMMARY_T MODIFY AGENTID varchar(255) not null;
+alter table SAM_MEDIA_T MODIFY CREATEDBY varchar(255), MODIFY LASTMODIFIEDBY varchar(255);
+alter table SAM_TYPE_T MODIFY CREATEDBY varchar(255) not null, MODIFY LASTMODIFIEDBY varchar(255) not null;
