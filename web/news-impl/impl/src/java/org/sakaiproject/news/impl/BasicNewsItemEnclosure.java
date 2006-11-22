70ebb4b76ba2db53b58162767765c53b7792620f
Joshua Ryan <josh@jryan.net>
2006-11-22 04:22:52 +0000

    SAK-7217 factor out inner classes + a little clean up
    git-svn-id: https://source.sakaiproject.org/svn/web/trunk@18367 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- web/news-impl/impl/src/java/org/sakaiproject/news/impl/BasicNewsItemEnclosure.java
+++ web/news-impl/impl/src/java/org/sakaiproject/news/impl/BasicNewsItemEnclosure.java
@@ -1,3 +1,24 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/svn/web/trunk/news-impl/impl/src/java/org/sakaiproject/news/impl/BasicNewsItemEnclosure.java $
+ * $Id: BasicNewsItemEnclosure.java 18196 2006-11-17 02:25:14Z joshua.ryan@asu.edu $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.news.impl;
 
 import org.sakaiproject.news.api.NewsItemEnclosure;
