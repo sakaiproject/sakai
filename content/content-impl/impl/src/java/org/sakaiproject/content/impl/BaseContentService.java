8aa21b6168a8cab235d358f49459a7207bc91532
Jim Eng <jimeng@umich.edu>
2006-12-02 18:43:58 +0000

    SAK-7239 Eliminate use of getCollectionSize in creating new Resources.
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@18596 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
+++ content/content-impl/impl/src/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -4584,7 +4584,7 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 		
 		if(this.m_prioritySortEnabled)
 		{
-			((BasicGroupAwareEdit) edit).setPriority();
+			// ((BasicGroupAwareEdit) edit).setPriority();
 		}
 		
 		// update the properties for update
@@ -8471,7 +8471,7 @@ public abstract class BaseContentService implements ContentHostingService, Cache
 					m_properties = new BaseResourcePropertiesEdit(element);
 					if(m_prioritySortEnabled)
 					{
-						setPriority();
+						// setPriority();
 					}
 				}
 				// look for groups 
