333d44426d55bf1a5b30c1c24be971aa3926d4d1
Josh Holtzman <jmholtzman@gmail.com>
2006-12-15 22:36:01 +0000

    SAK-7408 -- renamed all org.apache.foo xml api imports to com.sun.org.apache.foo.internal and removed maven dependencies on xalan and xerces.
    git-svn-id: https://source.sakaiproject.org/svn/syllabus/trunk@19623 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- syllabus/syllabus-impl/src/java/org/sakaiproject/component/app/syllabus/SyllabusServiceImpl.java
+++ syllabus/syllabus-impl/src/java/org/sakaiproject/component/app/syllabus/SyllabusServiceImpl.java
@@ -32,7 +32,7 @@ import java.util.Vector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.xerces.impl.dv.util.Base64;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import org.sakaiproject.api.app.syllabus.GatewaySyllabus;
 import org.sakaiproject.api.app.syllabus.SyllabusAttachment;
 import org.sakaiproject.api.app.syllabus.SyllabusData;
