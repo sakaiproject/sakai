8b53239b690c03389da011c1fda2a4c9df8a236c
Ian Boston <ieb@tfd.co.uk>
2006-12-20 01:17:40 +0000

    Replecated Josh H's compat work for Maven 2 Removed Base64 dep on xerces in podcast Added valid rights web.xml in to allow M2 to build and deploy Ok Removed Xerces dep in webservices (needs validating)
    git-svn-id: https://source.sakaiproject.org/svn/podcasts/trunk@19785 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- podcasts/podcasts/src/java/org/sakaiproject/tool/podcasts/RSSPodfeedServlet.java
+++ podcasts/podcasts/src/java/org/sakaiproject/tool/podcasts/RSSPodfeedServlet.java
@@ -31,7 +31,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.xerces.impl.dv.util.Base64;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import org.sakaiproject.api.app.podcasts.PodfeedService;
 import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.content.cover.ContentHostingService;
