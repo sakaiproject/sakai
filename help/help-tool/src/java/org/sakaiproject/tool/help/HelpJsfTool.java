dcd3e35455a3baddd9cbfe72caf5d410cfdbad23
josrodri <josrodri@iupui.edu>
2007-01-12 19:33:41 +0000

    SAK-7619: Allow redirection to external Help webapp.
    git-svn-id: https://source.sakaiproject.org/svn/help/trunk@20312 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- help/help-tool/src/java/org/sakaiproject/tool/help/HelpJsfTool.java
+++ help/help-tool/src/java/org/sakaiproject/tool/help/HelpJsfTool.java
@@ -29,6 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.jsf.util.JsfTool;
 import org.sakaiproject.util.Web;
 
@@ -53,13 +54,31 @@ public class HelpJsfTool extends JsfTool
   private static final String TOC_ATTRIBUTE = "tocURL";
   private static final String SEARCH_ATTRIBUTE = "searchURL";
   private static final String HELP_ATTRIBUTE = "helpURL";
-
+  
+  /** To determine if an external webapp handles help and if so, the base url */
+  private static final String USE_EXTERNAL_WEBAPP = "help.extern.webapp";
+  private static final String EXTERNAL_WEBAPP_URL_BASE = "help.extern.webapp.urlbase";
+  
   /**
    * @see org.sakaiproject.jsf.util.JsfTool#dispatch(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
    */
   protected void dispatch(HttpServletRequest req, HttpServletResponse res)
       throws ServletException, IOException
   {
+	    // if magic switch turned on, go to external webapp
+	    if ("true".equals(ServerConfigurationService.getString(USE_EXTERNAL_WEBAPP))) {
+	       String docId = req.getParameter("help");
+	       
+	       String extUrl = ServerConfigurationService.getString(EXTERNAL_WEBAPP_URL_BASE);
+	       
+	       if (docId != null && ! "".equals("docId")) {
+	    	   extUrl += docId;
+	       }
+	    	
+	       res.sendRedirect(extUrl);
+		   return;
+	    }
+
     req.setAttribute(TOC_ATTRIBUTE, Web.returnUrl(req, TOC_PATH));
     req.setAttribute(SEARCH_ATTRIBUTE, Web.returnUrl(req, SEARCH_PATH));
     req.setAttribute(HELP_ATTRIBUTE, Web.returnUrl(req, HELP_PATH));
