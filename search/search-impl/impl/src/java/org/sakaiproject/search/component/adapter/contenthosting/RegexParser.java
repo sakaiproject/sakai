d0634aee8c99d45b605012a4947f8e42b1139bea
Ian Boston <ieb@tfd.co.uk>
2007-02-10 02:09:51 +0000

    Optimised parser for HTML (2x speed of previous 1) and no object creation in parsing
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@21284 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/java/org/sakaiproject/search/component/adapter/contenthosting/RegexParser.java
+++ search/search-impl/impl/src/java/org/sakaiproject/search/component/adapter/contenthosting/RegexParser.java
@@ -21,10 +21,9 @@
 
 package org.sakaiproject.search.component.adapter.contenthosting;
 
+import java.io.InputStream;
 import java.util.Iterator;
-import java.util.Stack;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -35,83 +34,263 @@ import org.apache.commons.logging.LogFactory;
 public class RegexParser implements Iterator<String>
 {
 	private static Log log = LogFactory.getLog(RegexParser.class);
-	private static final String IGNORE_TAGS = "::script::head::style::";
-	
-	private Stack<String> elementStack = new Stack<String>();
-	private Pattern tags = Pattern.compile("(.*?)<(.*?)>",Pattern.MULTILINE);
-	private Matcher matcher;
-	private int ignore = 100;
 
-	private boolean notxml = true;
-	
-	
+	private static final char[][] IGNORE_TAGS = new char[][] { "script".toCharArray(), "head".toCharArray(), "style".toCharArray() };
+
+	private static final String PAD = "                                                                                                                   ";
+
+	private static final Properties entities = new Properties();
+
+	static
+	{
+		try
+		{
+			InputStream is = RegexParser.class.getResourceAsStream("/org/sakaiproject/search/component/bundle/htmlentities.properties");
+			entities.load(is);
+			is.close();
+		}
+		catch (Exception ex)
+		{
+			log.error("Unable to load HTML Entities", ex);
+		}
+	};
+
+	private int[] elementStack = new int[1024];
+
+	private int ignore = elementStack.length;
+
+	private boolean notxml = false;
+
+	private char[] cbuf;
+
+	private int current = 0;
+
+	private int clen = 0;;
+
+	private int endstack = 0;
+
+	private int last = 0;
+
 	public RegexParser(String content)
 	{
-		    matcher = tags.matcher(content);
+		cbuf = content.toCharArray();
+		current = 0;
+		clen = cbuf.length;
 	}
-    
-    
-    public String getTagName (String tag, int start) {
-     tag = tag.substring(start);
-	 char[] c= tag.toCharArray();
-	 String[] words = tag.split("\\s",2);
-	 if  ( words != null && words.length != 0 ) {
-		 return words[0];
-	 } else {
-		 return tag;
-	 }
-    }
 
+	public String getTagName(String tag, int start)
+	{
+		tag = tag.substring(start);
+		char[] c = tag.toCharArray();
+		String[] words = tag.split("\\s", 2);
+		if (words != null && words.length != 0)
+		{
+			return words[0];
+		}
+		else
+		{
+			return tag;
+		}
+	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.Iterator#hasNext()
 	 */
 	public boolean hasNext()
 	{
-		return matcher.find();
+		if (current >= clen)
+		{
+			return false;
+		}
+		for (int i = current; i < clen; i++)
+		{
+			if (cbuf[i] == '<')
+			{
+				current = i;
+				return true;
+			}
+		}
+		current = clen - 1;
+		return true;
 	}
 
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.Iterator#next()
 	 */
 	public String next()
 	{
-		String before = matcher.group(1);
-		String tag = matcher.group(2);
-		if ( tag.endsWith("/") ) {
-		} else if ( tag.startsWith("!--") ) {
-		} else if ( tag.startsWith("/") ) {
-			tag = getTagName(tag,1);
-			if ( elementStack.size() > 0 && tag.equals(elementStack.peek()) ) {
-				elementStack.pop();
-			} else {
-				notxml = true;
+		int tagend = clen - 1;
+		int elend = -1;
+		int tagstart = current + 1;
+		boolean ignoreBefore = !(ignore > endstack);
+		boolean ignoreAfter = ignoreBefore;
+
+		for (int i = current; i < clen; i++)
+		{
+			if (elend == -1 && Character.isWhitespace(cbuf[i]))
+			{
+				elend = i;
 			}
-		} else {
-			tag = getTagName(tag,0);
-			elementStack.push(tag);
-			if ( ignore >= elementStack.size() ) {
-				if ( IGNORE_TAGS.indexOf(":"+tag+":") != -1 ) {
-			        ignore = elementStack.size();
-				} else {
-			        ignore = elementStack.size()+1;
+			if (cbuf[i] == '>')
+			{
+				tagend = i;
+				if (elend == -1)
+				{
+					elend = tagend;
 				}
-			} 
+				break;
+			}
 		}
-		
-		if ( notxml || ignore > elementStack.size()  )  {
-		    return before.trim();
-		} 
-		return "";
+		if (tagstart < clen)
+		{
+			if (cbuf[tagend - 1] == '/')
+			{
+			}
+			else if (tagstart + 2 < clen && cbuf[tagstart] == '!' && cbuf[tagstart + 1] == '-' && cbuf[tagstart + 2] == '-')
+			{
+			}
+			else if (cbuf[tagstart] == '/')
+			{
+				tagstart++;
+				if (!notxml)
+				{
+					boolean match = true;
+					if ((elend - tagstart) == (elementStack[endstack - 1] - elementStack[endstack - 2]))
+					{
+						int j = elementStack[endstack - 2];
+						for (int i = 0; i < (elend - tagstart); i++)
+						{
+							if (Character.toLowerCase(cbuf[tagstart + i]) != Character.toLowerCase(cbuf[j + i]))
+							{
+								match = false;
+								break;
+							}
+						}
+					}
+
+					if (match)
+					{
+						endstack -= 2;
+						ignoreAfter = !(ignore > endstack);
+					}
+					else
+					{
+						notxml = true;
+					}
+				}
+			}
+			else
+			{
+				if (!notxml)
+				{
+					elementStack[endstack] = tagstart;
+					elementStack[endstack + 1] = elend;
+					endstack += 2;
+					if (!ignoreAfter)
+					{
+						for (int i = 0; i < IGNORE_TAGS.length; i++)
+						{
+							if (IGNORE_TAGS[i].length == (elend - tagstart))
+							{
+								ignoreAfter = true;
+								for (int j = 0; j < IGNORE_TAGS[i].length; j++)
+								{
+									if (IGNORE_TAGS[i][j] != Character.toLowerCase(cbuf[tagstart + j]))
+									{
+										ignoreAfter = false;
+										break;
+									}
+								}
+								if (ignoreAfter)
+								{
+									break;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		String t = "";
+		if (notxml || !ignoreBefore)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = last; i < current; i++)
+			{
+				if (cbuf[i] == '&')
+				{
+					if (cbuf[i + 1] == '#')
+					{
+						for (int j = i; j < current; j++)
+						{
+							if (cbuf[j] == ';')
+							{
+								String entity = new String(cbuf, i + 2, j - (i + 2));
+								sb.append((char) Integer.decode(entity).intValue());
+								i = j;
+								break;
+							}
+						}
+					}
+					else
+					{
+						for (int j = i; j < current; j++)
+						{
+							if (cbuf[j] == ';')
+							{
+
+								String entity = new String(cbuf, i, j - i + 1);
+								String s = (String) entities.get(entity);
+								if (s == null)
+								{
+									s = entity;
+								}
+								else if (s.length() > 1)
+								{
+									sb.append(s.charAt(1));
+								}
+								i = j;
+								break;
+							}
+						}
+					}
+				}
+				else
+				{
+					sb.append(cbuf[i]);
+				}
+			}
+			t = sb.toString();
+		}
+		last = tagend + 1;
+		current = last;
+
+		if (ignoreAfter)
+		{
+			if (!ignoreBefore)
+			{
+				ignore = endstack;
+			}
+		}
+		else
+		{
+			ignore = endstack + 2;
+		}
+		return t;
 	}
 
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.util.Iterator#remove()
 	 */
 	public void remove()
 	{
 	}
-       
+
 }
