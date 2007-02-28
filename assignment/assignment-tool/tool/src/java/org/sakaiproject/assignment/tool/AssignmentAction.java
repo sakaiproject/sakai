137c95701374003f33b049de52831f18bca9c694
Zhen Qian <zqian@umich.edu>
2007-02-28 17:48:27 +0000

    Fix to SAK-7329:Grading action buttons incorrect
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@22003 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -2130,6 +2130,20 @@ public class AssignmentAction extends PagedResourceActionII
 		state.setAttribute(STATE_MODE, MODE_INSTRUCTOR_GRADE_SUBMISSION);
 
 	} // doCancel_preview_grade_submission
+	
+	/**
+	 * Action is to cancel the preview grade process
+	 */
+	public void doCancel_preview_to_list_submission(RunData data)
+	{
+		SessionState state = ((JetspeedRunData) data).getPortletSessionState(((JetspeedRunData) data).getJs_peid());
+
+		// back to the instructor view of grading a submission
+		state.setAttribute(STATE_MODE, MODE_INSTRUCTOR_GRADE_ASSIGNMENT);
+
+	} // doCancel_preview_to_list_submission
+	
+	
 
 	/**
 	 * Action is to return to the view of list assignments
