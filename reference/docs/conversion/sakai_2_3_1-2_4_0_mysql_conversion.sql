435454331cf37c34788119f080ac413d5cb1e543
Michelle Wagner <wagnermr@iu.edu>
2007-03-14 12:38:46 +0000

    SAK-8632 http://bugs.sakaiproject.org/jira/browse/SAK-8632 Moderator functionality in Message Center - updating conversion scripts for 2.4
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@22591 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_3_1-2_4_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_3_1-2_4_0_mysql_conversion.sql
@@ -162,3 +162,22 @@ ALTER TABLE SAKAI_POSTEM_GRADEBOOK MODIFY title VARCHAR(255) not null;
 
 -- SAK-8213
 ALTER TABLE SAKAI_REALM_ROLE_DESC ADD (PROVIDER_ONLY CHAR(1) NULL);
+
+----------------------------------------------------------------------------------------------------------------------------------------
+-- Add Moderator functionality to Message Center (SAK-8632)
+----------------------------------------------------------------------------------------------------------------------------------------
+-- add column to allow Moderated as template setting
+alter table MFR_AREA_T add column (MODERATED bit);
+update MFR_AREA_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_AREA_T modify column MODERATED bit not null;
+
+-- change APPROVED column to allow null values to represent pending approvals
+alter table MFR_MESSAGE_T modify APPROVED bit null;
+
+-- change MODERATED column in MFR_OPEN_FORUM_T to not null
+update MFR_OPEN_FORUM_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_OPEN_FORUM_T modify column MODERATED bit not null;
+
+-- change MODERATED column in MFR_TOPIC_T to not null
+update MFR_TOPIC_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_TOPIC_T modify column MODERATED bit not null;
--- reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
@@ -161,3 +161,22 @@ ALTER TABLE SAKAI_POSTEM_GRADEBOOK MODIFY title VARCHAR2 (255);
 
 -- SAK-8213
 ALTER TABLE SAKAI_REALM_ROLE_DESC ADD (PROVIDER_ONLY CHAR(1) NULL);
+
+----------------------------------------------------------------------------------------------------------------------------------------
+-- Add Moderator functionality to Message Center (SAK-8632)
+----------------------------------------------------------------------------------------------------------------------------------------
+-- add column to allow Moderator as template setting
+alter table MFR_AREA_T add (MODERATED NUMBER(1,0));
+update MFR_AREA_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_AREA_T modify (MODERATED NUMBER(1,0) not null);
+
+-- change APPROVED column to allow null values to represent pending approvals
+alter table MFR_MESSAGE_T modify (APPROVED NUMBER(1,0) null);
+
+-- change MODERATED column in MFR_OPEN_FORUM_T to not null
+update MFR_OPEN_FORUM_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_OPEN_FORUM_T modify (MODERATED NUMBER(1,0) not null);
+
+-- change MODERATED column in MFR_TOPIC_T to not null
+update MFR_TOPIC_T set MODERATED=0 where MODERATED is NULL;
+alter table MFR_TOPIC_T modify (MODERATED NUMBER(1,0) not null);
