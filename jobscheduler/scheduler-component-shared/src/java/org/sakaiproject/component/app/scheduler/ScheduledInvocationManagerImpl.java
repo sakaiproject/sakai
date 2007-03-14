c7a356297bc1876f3943bab09412b4c9f8989da2
Andrew Poland <andrew@poland.cx>
2007-03-14 22:11:44 +0000

    SAK-8702 Implement Scheduled Invocation API
    git-svn-id: https://source.sakaiproject.org/svn/jobscheduler/trunk@22644 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- jobscheduler/scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/ScheduledInvocationManagerImpl.java
+++ jobscheduler/scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/ScheduledInvocationManagerImpl.java
@@ -1,23 +1,82 @@
 package org.sakaiproject.component.app.scheduler;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.api.app.scheduler.ScheduledInvocationManager;
+import org.sakaiproject.db.api.SqlService;
+import org.sakaiproject.id.api.IdManager;
 import org.sakaiproject.time.api.Time;
 
 public class ScheduledInvocationManagerImpl implements
 		ScheduledInvocationManager {
-
+	
+	
+	private static final Log LOG = LogFactory.getLog(ScheduledInvocationManagerImpl.class);
+	
+	/** Dependency: IdManager */
+	protected IdManager m_idManager = null;
+	
+	public void setIdManager(IdManager service)
+	{
+		m_idManager = service;
+	}
+	
+	/** Dependency: SqlService */
+	protected SqlService m_sqlService = null;
+	
+	public void setSqlService(SqlService service)
+	{
+		m_sqlService = service;
+	}
+	
+	public void init() {
+		
+		LOG.info("init()");
+		
+	}
+	
 	public String createDelayedInvocation(Time time, String componentId,
 			String opaqueContext) {
-		String uuid = null;
-		// String uuid = IdManager.generateGUID();
-		// DbWrite(INSERT INTO JOBS VALUES(uuid, TIME, componentId, opaqueContext);
 		
-		return uuid;
+		String[] fields = null;
+		
+		String uuid = m_idManager.createUuid();
+		
+		LOG.info("Creating new Delayed Invocation: " + uuid);
+		String sql = "INSERT INTO SCHEDULER_DELAYED_INVOCATION SET INVOCATION_ID = ?, INVOCATION_TIME = ?, COMPONENT_ID = ?, CONTEXT = ?";
+		
+		fields[0]=uuid;
+		//TODO: FORMAT DATE
+		fields[1]=time.toString();
+		fields[2]=componentId;
+		fields[3]=opaqueContext;
+		
+		LOG.info("SQL: "+sql);
+		if(m_sqlService.dbWrite(sql, fields)) {
+		 
+		  return uuid;
+		
+		} else {
+		
+		  return null;
+		  
+		}
 
 	}
 
 	public void deleteDelayedInvocation(String uuid) {
-		// TODO Auto-generated method stub
+		
+		String[] fields = null;
+		
+		LOG.info("Removing Delayed Invocation: " + uuid);
+		String sql = "DELETE FROM SCHEDULER_DELAYED_INVOCATION WHERE INVOCATION_ID = ?";
+		
+		fields[0]=uuid;
+		
+		LOG.info("SQL: "+sql);
+		m_sqlService.dbWrite(sql, fields);
 
 	}
 
--- jobscheduler/scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/ScheduledInvocationRunner.java
+++ jobscheduler/scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/jobs/ScheduledInvocationRunner.java
@@ -1,22 +1,47 @@
 package org.sakaiproject.component.app.scheduler.jobs;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.sakaiproject.api.app.scheduler.ScheduledInvocationCommand;
+import org.sakaiproject.db.api.SqlService;
 
 public class ScheduledInvocationRunner implements Job {
 
+	private static final Log LOG = LogFactory.getLog(ScheduledInvocationRunner.class);
+
+	/** Dependency: SqlService */
+	protected SqlService m_sqlService = null;
+	
+	public void setSqlService(SqlService service)
+	{
+		m_sqlService = service;
+	}
+	
+	
+	
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		
-		// DbService.dbRead("SELECT JOB_ID FROM JOBS WHERE DATE < NOW()");
+		String sql = "SELECT INVOCATION_ID, INVOCATION_TIME, COMPONENT, CONTEXT FROM SCHEDULER_DELAYED_INVOCATION WHERE TIME < NOW()";
+		
+		m_sqlService.dbRead(sql);
+		
+		//foreach row:
+		
+		//String uuid;
+		//String component;
+		
+		// DbService.dbWrite("DELETE FROM SCHEDULER_DELAYED_INVOCATION WHERE UUID = uuid");
 		
 		//ScheduledInvocationCommand command = ComponentManager.get(JOB_ID);
-		// command.execute();
+		// command.execute(CONTEXT);
 		
-		// DbService.dbWrite("DELETE ROM JOBS WHERE JOBS_ID = jobid");
+		//done!
 		
 
 	}
+
+	
 
 }
