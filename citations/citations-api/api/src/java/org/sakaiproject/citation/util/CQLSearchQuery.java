6e6c473ab479bf266721298905e83789fc69f916
Gaurav Bhatnagar <gbhatnag@umich.edu>
2007-03-15 16:39:40 +0000

    SAK-8613 incorporating apis from citations-util-api and modifiying Eclipse files
    git-svn-id: https://source.sakaiproject.org/svn/citations/trunk@22669 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ citations/citations-api/api/src/java/org/sakaiproject/citation/util/CQLSearchQuery.java
@@ -0,0 +1,14 @@
+package org.sakaiproject.citation.util;
+
+import org.sakaibrary.common.search.api.SearchQuery;
+
+public interface CQLSearchQuery {
+
+	/**
+	 * Gets a CQL-formatted search query string by converting searchQuery.
+	 * 
+	 * @param searchQuery SearchQuery object to convert.
+	 * @return the search query in CQL format or null if searchQuery is null.
+	 */
+	public String getCQLSearchQueryString( SearchQuery searchQuery );
+}
