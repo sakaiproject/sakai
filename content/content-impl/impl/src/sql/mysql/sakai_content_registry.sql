8fa7ff89c701d8cfebb3f72f27a117f0fe00096e
Jim Eng <jimeng@umich.edu>
2007-03-22 14:42:13 +0000

    SAK-8892 Revisions to the way types are filtered based on site.
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@27609 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ content/content-impl/impl/src/sql/mysql/sakai_content_registry.sql
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------
+-- CONTENT_TYPE_REGISTRY
+-----------------------------------------------------------------------------
+
+CREATE TABLE CONTENT_TYPE_REGISTRY
+(
+    CONTEXT_ID VARCHAR (99) NOT NULL,
+	RESOURCE_TYPE_ID VARCHAR (255),
+    ENABLED BOOLEAN
+ );
+
+-- index by context_id and resource_type_id 
