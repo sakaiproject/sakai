729a6e6f8cbe529b9df34b8698fd80f2a997eac4
Chris Maurer <maurercw@gmail.com>
2007-03-30 17:53:58 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-9130 Fixed the oracle conversion.  Apparently clob fields prefer "like" to "=" in a where clause.
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@28206 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
@@ -19,7 +19,7 @@ update osp_list_config set selected_columns = replace(selected_columns, 'name', 
 update osp_list_config set selected_columns = replace(selected_columns, 'siteName', 'site.title') where selected_columns like '%siteName%';
 
 --Updating for a change to the synoptic view for portfolio worksites
-update sakai_site_tool_property set name='siteTypeList', value='portfolio,PortfolioAdmin' where value='portfolioWorksites';
+update sakai_site_tool_property set name='siteTypeList', value='portfolio,PortfolioAdmin' where value like 'portfolioWorksites';
 
 --making sure these fields allow nulls
 ALTER TABLE osp_scaffolding MODIFY ( readyColor VARCHAR2(7) NULL );
