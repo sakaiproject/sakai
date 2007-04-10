a74a3afdf8b789e02576d9863dceead61a82e26f
Glenn R. Golden <ggolden@etudes.org>
2007-04-10 13:15:37 +0000

    SAK-9140
    git-svn-id: https://source.sakaiproject.org/svn/providers/trunk@28577 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- providers/sample/src/java/org/sakaiproject/provider/user/SampleUserDirectoryProvider.java
+++ providers/sample/src/java/org/sakaiproject/provider/user/SampleUserDirectoryProvider.java
@@ -3,7 +3,7 @@
  * $Id$
  ***********************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006, 2006 The Sakai Foundation.
+ * Copyright (c) 2003, 2004, 2005, 2006, 2006, 2007 The Sakai Foundation.
  * 
  * Licensed under the Educational Community License, Version 1.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -329,8 +329,7 @@ public class SampleUserDirectoryProvider implements UserDirectoryProvider, Users
 	 */
 	public String getDisplayId(User user)
 	{
-		// just a sample of what fun you can have here!
-		return "*" + user.getEid() + "*";
+		return user.getEid();
 	}
 
 	/**
