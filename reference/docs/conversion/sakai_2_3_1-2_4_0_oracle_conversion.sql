63d921b7f96a114ebf2a64d8575c11fe286e717a
Andrew Poland <andrew@poland.cx>
2007-04-12 18:30:07 +0000

    SAK-9459 Citation tool schema
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@28861 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_3_1-2_4_0_oracle_conversion.sql
@@ -1019,3 +1019,53 @@ from
 drop table PERMISSIONS_TEMP;
 drop table PERMISSIONS_SRC_TEMP;
 
+-----------------------------------------------------------------------------
+-- CITATION_COLLECTION
+-----------------------------------------------------------------------------
+
+CREATE TABLE CITATION_COLLECTION
+(
+   COLLECTION_ID VARCHAR2 (36) NOT NULL,
+		PROPERTY_NAME VARCHAR2 (255),
+		PROPERTY_VALUE LONG
+---    CONSTRAINT CITATION_COLLECTION_INDEX (COLLECTION_ID)
+);
+
+-----------------------------------------------------------------------------
+-- CITATION_CITATION
+-----------------------------------------------------------------------------
+
+CREATE TABLE CITATION_CITATION
+(
+   CITATION_ID VARCHAR2 (36) NOT NULL,
+		PROPERTY_NAME VARCHAR2 (255),
+		PROPERTY_VALUE LONG
+---    CONSTRAINT CITATION_CITATION_INDEX (CITATION_ID)
+);
+
+-----------------------------------------------------------------------------
+-- CITATION_SCHEMA
+-----------------------------------------------------------------------------
+
+CREATE TABLE CITATION_SCHEMA
+(
+   SCHEMA_ID VARCHAR2 (36) NOT NULL,
+		PROPERTY_NAME VARCHAR2 (255),
+		PROPERTY_VALUE LONG
+---    CONSTRAINT CITATION_SCHEMA_INDEX (SCHEMA_ID)
+);
+
+-----------------------------------------------------------------------------
+-- CITATION_SCHEMA_FIELD
+-----------------------------------------------------------------------------
+
+CREATE TABLE CITATION_SCHEMA_FIELD
+(
+   SCHEMA_ID VARCHAR2 (36) NOT NULL,
+   FIELD_ID VARCHAR2 (36) NOT NULL,
+		PROPERTY_NAME VARCHAR2 (255),
+		PROPERTY_VALUE LONG
+---    CONSTRAINT CITATION_SCHEMA_INDEX (SCHEMA_ID, FIELD_ID)
+);
+
+
