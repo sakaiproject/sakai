742e7ec57ee540df41f9fb9e6443b9a9911a11d5
Zhen Qian <zqian@umich.edu>
2007-04-18 14:23:10 +0000

    Fix to SAK-9478:A warning message "Assignment xxx has submissions." is shown even when there are no submissions for the assignment
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@29060 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -4687,13 +4687,25 @@ public class AssignmentAction extends PagedResourceActionII
 				Iterator submissions = AssignmentService.getSubmissions(a).iterator();
 				if (submissions.hasNext())
 				{
+					// any submitted?
 					boolean anySubmitted = false;
 					for (;submissions.hasNext() && !anySubmitted;)
 					{
 						AssignmentSubmission s = (AssignmentSubmission) submissions.next();
-						if (s.getSubmitted())
+						if (s.getSubmitted() && s.getTimeSubmitted() != null)
 						{
 							anySubmitted = true;
+						}
+					}
+					
+					// any draft submission
+					boolean anyDraft = false;
+					for (;submissions.hasNext() && !anyDraft;)
+					{
+						AssignmentSubmission s = (AssignmentSubmission) submissions.next();
+						if (!s.getSubmitted())
+						{
+							anyDraft = true;
 						}
 					}
 					if (anySubmitted)
@@ -4701,7 +4713,8 @@ public class AssignmentAction extends PagedResourceActionII
 						// if there is any submitted submission to this assignment, show alert
 						addAlert(state, rb.getString("assig1") + " " + a.getTitle() + " " + rb.getString("hassum"));
 					}
-					else
+					
+					if (anyDraft)
 					{
 						// otherwise, show alert about someone has started working on the assignment, not necessarily submitted
 						addAlert(state, rb.getString("hasDraftSum"));
