de8bce0a832d7ea16ea8f44edb43afcddaef8e85
jlrenfro@ucdavis.edu <jlrenfro@ucdavis.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-05-18 22:05:19 +0000

    SCORM New files. This and the previous revision include the refactoring to Wicket 1.3

--- scormplayer/tool/src/java/org/sakaiproject/scorm/helper/ScormHelper.java
+++ scormplayer/tool/src/java/org/sakaiproject/scorm/helper/ScormHelper.java
@@ -1,22 +1,10 @@
 package org.sakaiproject.scorm.helper;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.wicket.extensions.ajax.markup.html.form.upload.UploadWebRequest;
-import org.apache.wicket.markup.html.AjaxServerAndClientTimeFilter;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
-import org.apache.wicket.util.io.IObjectStreamFactory;
-import org.apache.wicket.util.lang.Objects;
-import org.sakaiproject.scorm.client.api.ScormClientFacade;
-import org.sakaiproject.scorm.helper.pages.UploadPage;
-import org.sakaiproject.scorm.helper.pages.UploadContentPackage;
 import org.apache.wicket.util.file.Folder;
-
-import org.apache.wicket.settings.ISecuritySettings;
-import org.apache.wicket.util.crypt.ClassCryptFactory;
-import org.apache.wicket.util.crypt.NoCrypt;
+import org.sakaiproject.scorm.client.api.ScormClientFacade;
+import org.sakaiproject.scorm.helper.pages.UploadContentPackage;
 
 public class ScormHelper extends WebApplication {
 
