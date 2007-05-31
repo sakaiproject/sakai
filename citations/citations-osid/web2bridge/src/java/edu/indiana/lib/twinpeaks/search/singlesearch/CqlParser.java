2f5ca4d2048f43a9785f0273946ee3942394bc2d
Steve Smail <ssmail@indiana.edu>
2007-05-31 17:02:57 +0000

    SAK-10143: Phrase searchs are now handled properly
    git-svn-id: https://source.sakaiproject.org/svn/citations/trunk@31251 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations/citations-osid/web2bridge/src/java/edu/indiana/lib/twinpeaks/search/singlesearch/CqlParser.java
+++ citations/citations-osid/web2bridge/src/java/edu/indiana/lib/twinpeaks/search/singlesearch/CqlParser.java
@@ -13,31 +13,47 @@ import edu.indiana.lib.twinpeaks.util.*;
 public class CqlParser extends org.xml.sax.helpers.DefaultHandler {
 
 private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.class);
-
-	// index mappings (CQL -> Sirsi)
+  //
+	// Index mappings (CQL -> Sirsi)
+	//
 	private static final java.util.Map INDEX_MAP = new java.util.HashMap();
 	static {
-		INDEX_MAP.put( "keyword", " " );
-		INDEX_MAP.put( "title",   ":TITLE" );
-		INDEX_MAP.put( "author",  ":CREATOR" );
-		INDEX_MAP.put( "subject", ":SUBJECT" );
-		INDEX_MAP.put( "year",    ":DATE" );
+		INDEX_MAP.put("keyword",  " ");
+		INDEX_MAP.put("title",    ":TITLE");
+		INDEX_MAP.put("author",   ":CREATOR");
+		INDEX_MAP.put("subject",  ":SUBJECT");
+		INDEX_MAP.put("year",     ":DATE");
 	}
-
-	// boolean mappings (CQL -> Sirsi)
+  //
+	// Boolean mappings (CQL -> Sirsi)
+	//
 	private static final java.util.Map BOOL_RELATION_MAP = new java.util.HashMap();
-	static {
-		BOOL_RELATION_MAP.put( "and", " AND " );
-		BOOL_RELATION_MAP.put( "or",  " OR " );
+	static
+	{
+		BOOL_RELATION_MAP.put("and", " AND ");
+		BOOL_RELATION_MAP.put("or",  " OR ");
 	}
-
-	// for SAX Parsing
-	SAXParser saxParser;
-	StringBuffer textBuffer;
-	StringBuffer searchClause;
-	boolean inSearchClause;
+  //
+	// SAX Parsing
+	//
+	SAXParser       saxParser;
+	StringBuffer    textBuffer;
+	StringBuffer    searchClause;
+	boolean         inSearchClause;
 	java.util.Stack cqlStack;
+  //
+  // Treat all non-keyword fields as phrases?
+  //
+	final static boolean TREAT_ALL_FIELDS_AS_PHRASE = true;
+	//
+	// Are we currently parsing a keyword field?
+	//
+	boolean inKeyword;
 
+
+  /**
+   * Constructor
+   */
 	public CqlParser()
 	{
 		// initialize stack
@@ -47,42 +63,55 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 		SAXParserFactory factory;
 
 		factory = SAXParserFactory.newInstance();
-		factory.setNamespaceAware( true );
-		try {
+		factory.setNamespaceAware(true);
+
+		try
+		{
 			saxParser = factory.newSAXParser();
-		} catch( org.xml.sax.SAXException e ) {
-			e.printStackTrace();
-		} catch( ParserConfigurationException e ) {
-			e.printStackTrace();
+		}
+		catch (org.xml.sax.SAXException e)
+		{
+			_log.error("SAX exception: " + e);
+		}
+		catch (ParserConfigurationException e)
+		{
+			_log.error("Parse failed: " + e);
 		}
 	}
 
 	/**
-	 * Converts a CQL-formatted search query into a format that the X-Server
+	 * Converts a CQL-formatted search query into a format that the Web2 Bridge
 	 * can understand.  Uses org.z3950.zing.cql.CQLNode.toXCQL() and SAX Parsing
-	 * to convert the cqlSearchQuery into an X-Server find_command.
+	 * to convert the cqlSearchQuery into a find_command.
 	 *
 	 * @param cqlSearchQuery CQL-formatted search query.
 	 * @return X-Server find_command or null if cqlSearchQuery is null or empty.
 	 * @see org.z3950.zing.cql.CQLNode.toXCQL()
 	 */
-	public String doCQL2MetasearchCommand( String cqlSearchQuery ) {
-
-		if( cqlSearchQuery == null || cqlSearchQuery.equals( "" ) ) {
+	public String doCQL2MetasearchCommand( String cqlSearchQuery )
+	{
+		if ( cqlSearchQuery == null || cqlSearchQuery.equals( "" ) )
+		{
 			return null;
 		}
 
 		org.z3950.zing.cql.CQLParser parser = new org.z3950.zing.cql.CQLParser();
 		org.z3950.zing.cql.CQLNode root = null;
 
-		try {
+		try
+		{
 			// parse the criteria
 			root = parser.parse( cqlSearchQuery );
-		} catch( java.io.IOException ioe ) {
-			ioe.printStackTrace();
-		} catch( org.z3950.zing.cql.CQLParseException e ) {
-			e.printStackTrace();
 		}
+		catch( java.io.IOException ioe )
+		{
+			_log.error("CQL parse exception: " + ioe);
+		}
+		catch( org.z3950.zing.cql.CQLParseException e )
+		{
+			_log.error("CQL parse exception: " + e);
+		}
+
 		String cqlXml = root.toXCQL( 0 );
 
 		_log.debug("CQL XML:");
@@ -90,24 +119,32 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 
 		// get cqlXml as a stream
 		java.io.ByteArrayInputStream byteInputStream = null;
-		try {
-			byteInputStream = new java.io.ByteArrayInputStream(
-					cqlXml.getBytes( "UTF8" ) );
-		} catch( java.io.UnsupportedEncodingException uee ) {
-			uee.printStackTrace();
+
+		try
+		{
+			byteInputStream = new java.io.ByteArrayInputStream(cqlXml.getBytes( "UTF8" ));
+		}
+		catch( java.io.UnsupportedEncodingException uee )
+		{
+			_log.error("Encoding exception: " + uee);
 		}
 
 		// clear the stack
 		cqlStack.removeAllElements();
 
 		// run the parser
-		try {
+		try
+		{
 			saxParser.parse( byteInputStream, this );
 			byteInputStream.close();
-		} catch( java.io.IOException ioe ) {
-			ioe.printStackTrace();
-		} catch( org.xml.sax.SAXException spe ) {
-			spe.printStackTrace();
+		}
+		catch( java.io.IOException ioe )
+		{
+			_log.error("IO exception: " + ioe);
+		}
+		catch( org.xml.sax.SAXException spe )
+		{
+			_log.error("SAX exception: " + spe);
 		}
 
 		String cqlResult = ( String ) cqlStack.pop();
@@ -123,11 +160,16 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 	 *
 	 * @see org.xml.sax.helpers.DefaultHandler
 	 */
-	public void startElement( String namespaceURI, String sName,
-			String qName, Attributes attrs ) throws SAXException {
+	public void startElement( String namespaceURI,
+	                          String sName,
+	                          String qName,
+	                          Attributes attrs ) throws SAXException
+	{
 		// set flags to avoid overwriting duplicate tag data
-		if( qName.equals( "searchClause" ) ) {
+		if( qName.equals( "searchClause" ) )
+		{
 			inSearchClause = true;
+			inKeyword = false;
 		}
 	}
 
@@ -137,13 +179,16 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 	 * @see org.xml.sax.helpers.DefaultHandler
 	 */
 	public void endElement( String namespaceURI, String sName, String qName )
-	throws SAXException {
+	                                                    throws SAXException
+	{
 		// extract data
 		extractDataFromText( qName );
 
 		// clear flags
-		if( qName.equals( "searchClause" ) ) {
+		if( qName.equals( "searchClause" ) )
+		{
 			inSearchClause = false;
+			inKeyword = false;
 		}
 	}
 
@@ -153,7 +198,8 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 	 * @see org.xml.sax.helpers.DefaultHandler
 	 */
 	public void characters( char[] buf, int offset, int len )
-	throws SAXException {
+	                                        throws SAXException
+	{
 		// store character data
 		String text = new String( buf, offset, len );
 
@@ -169,26 +215,32 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
 	// PRIVATE HELPER METHODS -
 	//-------------------------
 
-	private void extractDataFromText( String element ) {
-		if( textBuffer == null ) {
+	private void extractDataFromText(String element)
+	{
+		if (textBuffer == null)
+		{
 			return;
 		}
 
 		String text = textBuffer.toString().trim();
-		if( text.equals( "" ) && !element.equals( "triple" ) ) {
+		if (text.equals("") && !element.equals("triple"))
+		{
 			return;
 		}
     //
 		// check for a boolean relation value
 		//
-		if( !inSearchClause && element.equals( "value" ) ) {
-			cqlStack.push( text );
+		if (!inSearchClause && element.equals( "value" ))
+		{
+			cqlStack.push(text);
 		}
     //
 		// Construct a search clause
 		//
-		if( inSearchClause ) {
-			if( searchClause == null ) {
+		if (inSearchClause)
+		{
+			if (searchClause == null)
+			{
 				searchClause = new StringBuffer();
 			}
       //
@@ -196,69 +248,101 @@ private static org.apache.commons.logging.Log	_log = LogUtils.getLog(CqlParser.c
       //
       //    (title is the index, = is the value, macbeth is the term)
       //
-			if( element.equals( "index" ) ) {
-				searchClause.append( translateIndex( text ) );
+			if (element.equals("index"))
+			{
+			  String field = translateIndex(text);
 
-			} else if( element.equals( "value" ) ) {
+			  inKeyword = ((String) INDEX_MAP.get("keyword")).equals(field);
+				searchClause.append(field);
+			}
+			else if (element.equals("value"))
+			{
 			  //
-				// The relation value is always supplied as '='.
-				// We don't need it.  Just use a space for Web2
+				// The relation value is always supplied as '='.  The Muse syntax employed
+				// by the Web2 bridge doesn't need it.  Use a space instead.
 				//
-				searchClause.append( ' ' );
+				searchClause.append(' ');
+			}
+			else if (element.equals("term"))
+			{
+			  //
+			  // Search term processing:
+			  //
+				// * Honor '+' encoding for embedded spaces
+				// * Add double quotes
+			//
+				// Example:
+				//      aa+bb+cc  <becomes>  "aa bb cc"
+				//
+				if (TREAT_ALL_FIELDS_AS_PHRASE || inKeyword)
+				{
+				  searchClause.append('"');
+				}
+			  searchClause.append(text.replaceAll("\\+", " ").trim());
 
-			} else if( element.equals( "term" ) ) {
-				// Discard '+' encoding for embedded spaces (should we url decode?).
-				//
-				// Unless our caller provides enclosing quotes (\"), this will
-				// produce a series of keywords, not a phrase.
-				//
-			  searchClause.append( text.replaceAll("\\+", " ").trim() );
-				cqlStack.push( searchClause.toString().trim() );
+				if (TREAT_ALL_FIELDS_AS_PHRASE || inKeyword)
+				{
+				  searchClause.append('"');
+        }
+				cqlStack.push(searchClause.toString().trim());
 				searchClause = null;
 			}
 		}
     //
 		// evaluate expression so far if we hit a </triple>
 		//
-		if( element.equals( "triple" ) ) {
+		if( element.equals( "triple" ) )
+		{
 			String rightOperand    = ( String ) cqlStack.pop();
 			String leftOperand     = ( String ) cqlStack.pop();
 			String booleanRelation = ( String ) cqlStack.pop();
 
-			cqlStack.push( leftOperand +
-					translateBooleanRelation( booleanRelation ) +
-					rightOperand );
+			cqlStack.push(leftOperand.replaceAll("\\+", " ").trim()
+                +   translateBooleanRelation(booleanRelation)
+					      +   rightOperand.replaceAll("\\+", " ").trim());
 		}
 
 		textBuffer = null;
 	}
 
-	private String translateIndex( String cqlIndex ) {
-		String xserverIndex = ( String ) INDEX_MAP.get( cqlIndex );
+  /**
+   * Translate a CQL index to the appropriate Sirsi/Muse field name
+   * @param cqlIndex CQL index name
+   * @return Sirsi/Muse field name
+   */
+	private String translateIndex(String cqlIndex)
+	{
+		String sirsiIndex = ( String ) INDEX_MAP.get(cqlIndex);
 
-		if( xserverIndex == null || xserverIndex.equals( "" ) ) {
-			_log.error( "\nERROR (CQL2XServerFindCommand." +
-					"translateIndex()): bad index" );
-			// default to keyword
-			xserverIndex = " ";
+		if (sirsiIndex == null || sirsiIndex.equals( "" ))
+		{
+			_log.error("translateIndex(): bad index, using KEYWORD");
+			sirsiIndex = (String) INDEX_MAP.get("keyword");
 		}
 
-		return xserverIndex;
+		return sirsiIndex;
 	}
 
-	private String translateBooleanRelation( String booleanRelation ) {
-		String xserverBoolean = ( String ) BOOL_RELATION_MAP.get( booleanRelation );
+  /**
+   * Translate a CQL boolean term to the appropriate Sirsi/Muse syntax
+   * @param booleanRelation CQL boolean term
+   * @return Sirsi/Muse boolean operation
+   */
+	private String translateBooleanRelation(String booleanRelation)
+	{
+		String sirsiBoolean = (String) BOOL_RELATION_MAP.get(booleanRelation);
 
-		if( xserverBoolean == null || xserverBoolean.equals( "" ) ) {
-			_log.error( "\nERROR (CQL2XServerFindCommand." +
-					"translateBooleanRelation()): bad boolean relation" );
-			// default to and
-			xserverBoolean = " AND ";
+		if (sirsiBoolean == null || sirsiBoolean.equals( "" ))
+		{
+			_log.error("translateBooleanRelation(): bad boolean relation, using AND" );
+			sirsiBoolean = (String) BOOL_RELATION_MAP.get("and");
 		}
-
-		return xserverBoolean;
+		return sirsiBoolean;
 	}
 
+  /**
+   * Main(): test
+   */
   public static void main(String[] args)
   {
     CqlParser parser = new CqlParser();
