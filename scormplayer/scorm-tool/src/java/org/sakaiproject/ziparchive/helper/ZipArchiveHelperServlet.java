2dd15776e6ab92d2c88a0320adc1512d0ca613b2
jlrenfro@ucdavis.edu <jlrenfro@ucdavis.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-06-07 02:21:36 +0000

    SCORM Adding some important stuff I forgot.

---
+++ scormplayer/scorm-tool/src/java/org/sakaiproject/ziparchive/helper/ZipArchiveHelperServlet.java
@@ -0,0 +1,17 @@
+package org.sakaiproject.ziparchive.helper;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ZipArchiveHelperServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse reponse) {
+		
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse reponse) {
+		
+	}
+	
+}
