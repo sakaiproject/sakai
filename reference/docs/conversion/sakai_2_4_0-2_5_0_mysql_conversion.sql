26ae34f97b34e8e9fe3dc9277d45205ac6c4a70a
Michelle Wagner <wagnermr@iu.edu>
2007-06-20 17:28:18 +0000

    SAK-9808, SAK-10423 Implement ability to delete Forums message Updated conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@31691 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_4_0-2_5_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_mysql_conversion.sql
@@ -18,3 +18,7 @@ ALTER TABLE SAKAI_POSTEM_HEADINGS MODIFY heading VARCHAR(500);
 alter table searchbuilderitem add itemscope integer;
 alter table searchbuilderitem add index isearchbuilderitem_sco(itemscope);
 
+
+-- SAK-9808: Implement ability to delete threaded messages within Forums
+alter table MFR_MESSAGE_T add DELETED bit not null default false;
+create index MFR_MESSAGE_DELETED_I on MFR_MESSAGE_T (DELETED);
--- reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
@@ -18,3 +18,8 @@ ALTER TABLE SAKAI_POSTEM_HEADINGS MODIFY heading VARCHAR2 (500);
 alter table searchbuilderitem add itemscope integer;
 alter table searchbuilderitem add index isearchbuilderitem_sco(itemscope);
 
+-- SAK-9808: Implement ability to delete threaded messages within Forums
+alter table MFR_MESSAGE_T add DELETED number(1, 0) not null;
+update MFR_MESSAGE_T set DELETED=0 where DELETED is NULL;
+create index MFR_MESSAGE_DELETED_I on MFR_MESSAGE_T (DELETED);
+
