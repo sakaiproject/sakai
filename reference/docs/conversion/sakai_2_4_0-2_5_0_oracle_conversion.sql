f519398c80d1728c06805477ad45f69e7388f3bc
Andrew Poland <andrew@poland.cx>
2007-07-19 18:58:56 +0000

    SAK-10423 Default value for Messages/Forums 'DELETED' column
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@32899 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
@@ -18,6 +18,9 @@ ALTER TABLE SAKAI_POSTEM_HEADINGS MODIFY heading VARCHAR2 (500);
 alter table searchbuilderitem add itemscope integer;
 alter table searchbuilderitem add index isearchbuilderitem_sco(itemscope);
 
--- SAK-10423
-alter table MFR_MESSAGE_T DELETED number(1, 0) default '0' not null;
+-- SAK-9808: Implement ability to delete threaded messages within Forums
+alter table MFR_MESSAGE_T add DELETED number(1, 0) default '0' not null;
+
+
+
 
