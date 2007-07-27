858f83e4a7d0813180b7e5471fe4bc494be2bca2
Jim Eng <jimeng@umich.edu>
2007-07-27 00:39:55 +0000

    SAK-10900 Better logic for setting or clearing pub-view after a change.
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@33223 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
@@ -2663,6 +2663,10 @@ public class ListItem
 			{
 				edit.clearGroupAccess();
 			}
+			else if(ContentHostingService.isPubView(this.id) && ! this.isPubview)
+			{
+				edit.clearPublicAccess();
+			}
 		} 
 		catch (InconsistentException e) 
 		{
