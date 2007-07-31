3c25c88c1493902131ddcaec8da12c6ff9630b75
Zhen Qian <zqian@umich.edu>
2007-07-31 18:23:07 +0000

    fix to SAK-10963:Assignment 'accept until' date error
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@33333 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -401,6 +401,8 @@ public class AssignmentAction extends PagedResourceActionII
 	
 	private static final String NEW_ASSIGNMENT_DUEDATE_CALENDAR_ASSIGNMENT_ID = "new_assignment_duedate_calendar_assignment_id";
 
+	private static final String NEW_ASSIGNMENT_PAST_DUE_DATE = "new_assignment_past_due_date";
+	
 	// close date
 	private static final String NEW_ASSIGNMENT_ENABLECLOSEDATE = "new_assignment_enableclosedate";
 
@@ -457,7 +459,7 @@ public class AssignmentAction extends PagedResourceActionII
 	private static final String ALLOW_RESUBMIT_CLOSEHOUR = "allow_resubmit_closeHour";
 	private static final String ALLOW_RESUBMIT_CLOSEMIN = "allow_resubmit_closeMin";
 	private static final String ALLOW_RESUBMIT_CLOSEAMPM = "allow_resubmit_closeAMPM";
-
+	
 	private static final String ATTACHMENTS_MODIFIED = "attachments_modified";
 
 	/** **************************** instructor's view student submission ***************** */
@@ -3623,11 +3625,22 @@ public class AssignmentAction extends PagedResourceActionII
 			dueHour = 0;
 		}
 		Time dueTime = TimeService.newTimeLocal(dueYear, dueMonth, dueDay, dueHour, dueMin, 0, 0);
-		// validate date
-		if (dueTime.before(TimeService.newTime()))
+		
+		// show alert message when due date is in past. Remove it after user confirms the choice.
+		if (dueTime.before(TimeService.newTime()) && state.getAttribute(NEW_ASSIGNMENT_PAST_DUE_DATE) == null)
+		{
+			state.setAttribute(NEW_ASSIGNMENT_PAST_DUE_DATE, Boolean.TRUE);
+		}
+		else
+		{
+			// clean the attribute after user confirm
+			state.removeAttribute(NEW_ASSIGNMENT_PAST_DUE_DATE);
+		}
+		if (state.getAttribute(NEW_ASSIGNMENT_PAST_DUE_DATE) != null)
 		{
 			addAlert(state, rb.getString("assig4"));
 		}
+		
 		if (!dueTime.after(openTime))
 		{
 			addAlert(state, rb.getString("assig3"));
