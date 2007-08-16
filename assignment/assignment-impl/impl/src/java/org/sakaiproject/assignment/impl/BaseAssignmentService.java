957a3a766eefe68e5e487406cd81d6981c1efdac
David Horwitz <dhorwitz@gmail.com>
2007-08-16 08:20:57 +0000

    SAK-11156 catch queue exceptions to prevent stack traces being thrown for queue errors
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@34051 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
+++ assignment/assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
@@ -6976,7 +6976,12 @@ public abstract class BaseAssignmentService implements AssignmentService, Entity
 						{
 							String contentId = ((Reference) m_submittedAttachments.get(0)).getId();
 							String userId = (String)this.getSubmitterIds().get(0);
-							contentReviewService.queueContent(userId, null, getAssignment().getReference(), contentId);
+							try {
+								contentReviewService.queueContent(userId, null, getAssignment().getReference(), contentId);
+							}
+							catch (QueueException qe) {
+								M_log.warn("Unable to queue content with content review Service: " + qe.getMessage());
+							}
 						}
 					} catch (Exception e) {
 						e.printStackTrace();
@@ -8286,7 +8291,8 @@ public abstract class BaseAssignmentService implements AssignmentService, Entity
 			try {
 				Assignment ass = this.getAssignment();
 				contentReviewService.queueContent(null, null, ass.getReference(), attachment.getId());
-				
+			} catch (QueueException qe) {
+				M_log.warn("Unable to add content to Content Review queue: " + qe.getMessage());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
