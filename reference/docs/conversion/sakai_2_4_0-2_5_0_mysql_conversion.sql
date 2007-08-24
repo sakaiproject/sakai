14dca05d67458eae642861313c572ad5e8d0353f
Michelle Wagner <wagnermr@iu.edu>
2007-08-24 12:55:07 +0000

    SAK-10543 http://bugs.sakaiproject.org/jira/browse/SAK-10543 Add GB post-2.4 conversions to 2.4 > 2.5 upgrade script
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@34341 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_4_0-2_5_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_mysql_conversion.sql
@@ -48,3 +48,52 @@ alter table SAKAI_PERSON_T add column dateOfBirth date;
 -- having the same value.
 alter table SAKAI_SESSION add column SESSION_ACTIVE tinyint(1);
 create index SESSION_ACTIVE_IE on SAKAI_SESSION (SESSION_ACTIVE);
+
+--Add categories to the gradebook
+create table GB_CATEGORY_T (ID bigint not null auto_increment, VERSION integer not null, GRADEBOOK_ID bigint not null, NAME varchar(255) not null, WEIGHT double precision, DROP_LOWEST integer, REMOVED bit, primary key (ID));
+alter table GB_GRADABLE_OBJECT_T add CATEGORY_ID bigint;
+alter table GB_GRADEBOOK_T add GRADE_TYPE integer not null;
+alter table GB_GRADEBOOK_T add CATEGORY_TYPE integer not null;
+alter table GB_CATEGORY_T add index FKCD333737325D7986 (GRADEBOOK_ID), add constraint FKCD333737325D7986 foreign key (GRADEBOOK_ID) references GB_GRADEBOOK_T (ID);
+alter table GB_GRADABLE_OBJECT_T add index FK759996A7F09DEFAE (CATEGORY_ID), add constraint FK759996A7F09DEFAE foreign key (CATEGORY_ID) references GB_CATEGORY_T (ID);
+create index GB_CATEGORY_GB_IDX on GB_CATEGORY_T (GRADEBOOK_ID);
+create index GB_GRADABLE_OBJ_CT_IDX on GB_GRADABLE_OBJECT_T (CATEGORY_ID);
+update GB_GRADEBOOK_T set GRADE_TYPE = 1, CATEGORY_TYPE = 1;
+
+--Gradebook SAK-10427
+alter table GB_GRADABLE_OBJECT_T add column UNGRADED bit;
+update GB_GRADABLE_OBJECT_T set UNGRADED = false;
+
+--Gradebook SAK-10571
+drop table if exists GB_LETTERGRADE_PERCENT_MAPPING;
+drop table if exists GB_LETTERGRADE_MAPPING;
+create table GB_LETTERGRADE_MAPPING (LG_MAPPING_ID bigint not null, value double precision, GRADE varchar(255) not null, primary key (LG_MAPPING_ID, GRADE));
+create table GB_LETTERGRADE_PERCENT_MAPPING (LGP_MAPPING_ID bigint not null auto_increment, VERSION integer not null, MAPPING_TYPE integer not null, GRADEBOOK_ID bigint, primary key (LGP_MAPPING_ID), unique (MAPPING_TYPE, GRADEBOOK_ID));
+alter table GB_LETTERGRADE_MAPPING add index FKC8CDDC5CE7F3A761 (LG_MAPPING_ID), add constraint FKC8CDDC5CE7F3A761 foreign key (LG_MAPPING_ID) references GB_LETTERGRADE_PERCENT_MAPPING (LGP_MAPPING_ID);
+
+insert into GB_LETTERGRADE_PERCENT_MAPPING values (null,  0, 1, null);
+insert into GB_LETTERGRADE_MAPPING values (1, 100.0, 'A+');
+insert into GB_LETTERGRADE_MAPPING values (1, 95.0, 'A');
+insert into GB_LETTERGRADE_MAPPING values (1, 90.0, 'A-');
+insert into GB_LETTERGRADE_MAPPING values (1, 87.0, 'B+');
+insert into GB_LETTERGRADE_MAPPING values (1, 83.0, 'B');
+insert into GB_LETTERGRADE_MAPPING values (1, 80.0, 'B-');
+insert into GB_LETTERGRADE_MAPPING values (1, 77.0, 'C+');
+insert into GB_LETTERGRADE_MAPPING values (1, 73.0, 'C');
+insert into GB_LETTERGRADE_MAPPING values (1, 70.0, 'C-');
+insert into GB_LETTERGRADE_MAPPING values (1, 67.0, 'D+');
+insert into GB_LETTERGRADE_MAPPING values (1, 63.0, 'D');
+insert into GB_LETTERGRADE_MAPPING values (1, 60.0, 'D-');
+insert into GB_LETTERGRADE_MAPPING values (1, 0.0, 'F');
+
+--Gradebook SAK-10835
+CREATE TABLE GB_PERMISSION_T ( 
+    GB_PERMISSION_ID bigint AUTO_INCREMENT NOT NULL,
+    VERSION     	 integer NOT NULL,
+    GRADEBOOK_ID	 bigint NOT NULL,
+    USER_ID     	 varchar(99) NOT NULL,
+    FUNCTION_NAME  	 varchar(5) NOT NULL,
+    CATEGORY_ID 	 bigint NULL,
+    GROUP_ID    	 varchar(255) NULL,
+    PRIMARY KEY(GB_PERMISSION_ID)
+);
--- reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
@@ -49,3 +49,56 @@ alter table SAKAI_PERSON_T add (dateOfBirth date);
 -- having the same value.
 alter table SAKAI_SESSION add column SESSION_ACTIVE number(1,0);
 create index SESSION_ACTIVE_IE on SAKAI_SESSION (SESSION_ACTIVE);
+
+--Add categories to gradebook
+create table GB_CATEGORY_T (ID number(19,0) not null, VERSION number(10,0) not null, GRADEBOOK_ID number(19,0) not null, NAME varchar2(255 char) not null, WEIGHT double precision, DROP_LOWEST number(10,0), REMOVED number(1,0), primary key (ID));
+alter table GB_GRADABLE_OBJECT_T add CATEGORY_ID number(19,0);
+alter table GB_GRADEBOOK_T add GRADE_TYPE number(10,0);
+alter table GB_GRADEBOOK_T add CATEGORY_TYPE number(10,0);
+alter table GB_CATEGORY_T add constraint FKCD333737325D7986 foreign key (GRADEBOOK_ID) references GB_GRADEBOOK_T;
+alter table GB_GRADABLE_OBJECT_T add constraint FK759996A7F09DEFAE foreign key (CATEGORY_ID) references GB_CATEGORY_T;
+create sequence GB_CATEGORY_S;
+create index GB_CATEGORY_GB_IDX on GB_CATEGORY_T (GRADEBOOK_ID);
+create index GB_GRADABLE_OBJ_CT_IDX on GB_GRADABLE_OBJECT_T (CATEGORY_ID);
+update GB_GRADEBOOK_T set GRADE_TYPE = 1, CATEGORY_TYPE = 1;
+alter table GB_GRADEBOOK_T modify ( GRADE_TYPE number(10,0) not null, CATEGORY_TYPE number(10,0) not null );
+
+--Gradebook SAK-10427
+alter table GB_GRADABLE_OBJECT_T add (UNGRADED number(1,0));
+update GB_GRADABLE_OBJECT_T set UNGRADED = 0;
+
+--Gradebook SAK-10571
+create table GB_LETTERGRADE_MAPPING (LG_MAPPING_ID number(19,0) not null, VALUE double precision, GRADE varchar2(255 char) not null, primary key (LG_MAPPING_ID, GRADE));
+create table GB_LETTERGRADE_PERCENT_MAPPING (LGP_MAPPING_ID number(19,0) not null, VERSION number(10,0) not null, MAPPING_TYPE number(10,0) not null, GRADEBOOK_ID number(19,0), primary key (LGP_MAPPING_ID), unique (MAPPING_TYPE, GRADEBOOK_ID));
+alter table GB_LETTERGRADE_MAPPING add constraint FKC8CDDC5CE7F3A761 foreign key (LG_MAPPING_ID) references GB_LETTERGRADE_PERCENT_MAPPING (LGP_MAPPING_ID);
+create sequence GB_LETTER_MAPPING_S;
+
+insert into GB_LETTERGRADE_PERCENT_MAPPING values (GB_LETTER_MAPPING_S.NEXTVAL, 0, 1, null);
+insert into GB_LETTERGRADE_MAPPING values (1, 100.0, 'A+');
+insert into GB_LETTERGRADE_MAPPING values (1, 95.0, 'A');
+insert into GB_LETTERGRADE_MAPPING values (1, 90.0, 'A-');
+insert into GB_LETTERGRADE_MAPPING values (1, 87.0, 'B+');
+insert into GB_LETTERGRADE_MAPPING values (1, 83.0, 'B');
+insert into GB_LETTERGRADE_MAPPING values (1, 80.0, 'B-');
+insert into GB_LETTERGRADE_MAPPING values (1, 77.0, 'C+');
+insert into GB_LETTERGRADE_MAPPING values (1, 73.0, 'C');
+insert into GB_LETTERGRADE_MAPPING values (1, 70.0, 'C-');
+insert into GB_LETTERGRADE_MAPPING values (1, 67.0, 'D+');
+insert into GB_LETTERGRADE_MAPPING values (1, 63.0, 'D');
+insert into GB_LETTERGRADE_MAPPING values (1, 60.0, 'D-');
+insert into GB_LETTERGRADE_MAPPING values (1, 0.0, 'F');
+
+--Gradebook SAK-10835
+CREATE TABLE GB_PERMISSION_T ( 
+    GB_PERMISSION_ID number(19,0) not null,
+    VERSION     	 number(10,0) not null,
+    GRADEBOOK_ID	 number(19,0) not null,
+    USER_ID     	 varchar2(99) not null,
+    FUNCTION_NAME 	 varchar2(5) not null,
+    CATEGORY_ID 	 number(19,0) null,
+    GROUP_ID    	 varchar2(255) null,
+    PRIMARY KEY(GB_PERMISSION_ID)
+);
+
+create sequence GB_PERMISSION_S;
+
