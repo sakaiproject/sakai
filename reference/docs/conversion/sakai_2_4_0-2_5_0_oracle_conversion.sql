378b7f2066debe11d933c305eacfe1fdf52f4bce
Chris Maurer <maurercw@gmail.com>
2007-09-19 15:38:41 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-11579 Fixing the syntax for adding a column to SAKAI_SESSION
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@35425 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
@@ -47,7 +47,7 @@ alter table SAKAI_PERSON_T add (dateOfBirth date);
 -- SAK-8780, SAK-7452 - Add SESSION_ACTIVE flag to explicitly indicate when
 -- a session is active rather than relying on SESSION_START and SESSION_END
 -- having the same value.
-alter table SAKAI_SESSION add column SESSION_ACTIVE number(1,0);
+alter table SAKAI_SESSION add SESSION_ACTIVE number(1,0);
 create index SESSION_ACTIVE_IE on SAKAI_SESSION (SESSION_ACTIVE);
 
 --Add categories to gradebook
