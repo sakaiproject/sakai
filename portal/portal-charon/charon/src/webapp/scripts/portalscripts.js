2cc03e80796a0412966961d34fbe4142f67d72ee
Colin Clark <colinbdclark@gmail.com>
2007-10-17 22:46:37 +0000

    SAK-11891
    Portal DHTML More Sites: patch from Eli Cochran to fix strange behaviour when resizing the browser window in IE 6. This patch makes some minor improvements to the factoring of the more sites code in portalscripts.js and fixes the window bug nicely.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/portal/trunk@37098 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-charon/charon/src/webapp/scripts/portalscripts.js
+++ portal/portal-charon/charon/src/webapp/scripts/portalscripts.js
@@ -1,40 +1,40 @@
 /* Script for pop-up dhtml more tabs implementation 
  * uses jQuery library
  */
-function dhtml_more_tabs() {
-	if ($('#selectNav').css('display') == 'none' ) {
-		// if the selectNav element is not a child of siteNav then we need to init the page
-		if ($('#selectNav').parent().attr('id') != 'siteNav') {
-			// make the selectNav div a child of the linkNav element for better positioning
-			$('div#selectNav').appendTo('#linkNav').addClass('dhtml_more_tabs');
+
+var dhtml_more_tabs = function() {
+	//initialize the more tabs area then reset the function to just the show/hide behavior
+	$('div#selectNav').appendTo('#linkNav').addClass('dhtml_more_tabs');
+	$('div#selectNav').css('top',$('#linkNav').height() - 3); // adjust the vertical position
+	$('div#selectNav').width($('#linkNav').width()*0.75); // fixes an IE6 bug
+	dhtml_more_tabs = function() {
+		if ($('#selectNav').css('display') == 'none' ) {
+			$('div#selectNav').show();
+			// highlight the more tab
+			$('.more-tab').addClass('more-active');
+			// dim the current tab
+			$('.selectedTab').addClass('tab-dim');
+			// mask the rest of the page
+			createDHTMLMask() ;
+			$('.more-tab').css('z-index',9800);
+			$('#selectNav').css('z-index',9900);
+			$('.selectedTab').bind('click',function(){dhtml_more_tabs();return false;});
+		} else {
+			// unhighlight the more tab
+			$('.more-tab').removeClass('more-active');
+			// hide the dropdown
+			$('div#selectNav').hide(); // hide the box
+			//undim the currently selected tab
+			$('.selectedTab').removeClass('tab-dim');
+			removeDHTMLMask()
+			$('.selectedTab').unbind('click');
 		}
-		// show the dropdown
-		$('div#selectNav').css('top',$('#linkNav').height() - 3); // adjust the vertical position
-		$('div#selectNav').show();
-		// highlight the more tab
-		$('.more-tab').addClass('more-active');
-		// dim the current tab
-		$('.selectedTab').addClass('tab-dim');
-		// mask the rest of the page
-		createDHTMLMask() ;
-		$('.more-tab').css('z-index',9800);
-		$('#selectNav').css('z-index',9900);
-		$('.selectedTab').bind('click',function(){dhtml_more_tabs();return false;});
-	} else {
-		// unhighlight the more tab
-		$('.more-tab').removeClass('more-active');
-		// hide the dropdown
-		$('div#selectNav').hide(); // hide the box
-		//undim the currently selected tab
-		$('.selectedTab').removeClass('tab-dim');
-		removeDHTMLMask()
-		$('.selectedTab').unbind('click');
 	}
-	return false;
+	dhtml_more_tabs();
 }
 
 function createDHTMLMask() {
-	$('#container').append('<div id="portalMask">&nbsp;</div>');
+	$('body').append('<div id="portalMask">&nbsp;</div>');
 	$('#portalMask').css('height',browserSafeDocHeight()).css('width','100%').css('z-index',1000).bind("click",function(event){
 		dhtml_more_tabs();
 		return false;
