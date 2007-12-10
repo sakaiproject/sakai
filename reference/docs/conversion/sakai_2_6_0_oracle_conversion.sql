0f3458482abb07e28725571283a181bb816be341
Ying Wang <wang58@iupui.edu>
2007-12-10 20:23:14 +0000

    SAK-12176 Messages-send cc to recipients' email address(es)
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@39091 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
@@ -2,7 +2,7 @@
 
 -- add column sendEmailOut to table MFR_AREA_T
 alter table MFR_AREA_T add (SENDEMAILOUT NUMBER(1,0));
-update MFR_AREA_T set SENDEMAILOUT=0 where SENDEMAILOUT is NULL;
+update MFR_AREA_T set SENDEMAILOUT=1 where SENDEMAILOUT is NULL;
 alter table MFR_AREA_T modify (SENDEMAILOUT NUMBER(1,0) not null);
 
 --new msg.emailout permission 
