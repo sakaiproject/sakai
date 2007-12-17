af840bff6a8d6933e51d8390145fa17b93eba809
Ian Boston <ieb@tfd.co.uk>
2007-12-17 10:20:01 +0000

    SAK-12447
    Added Unit Test to memory service to check invalidation process,
    no errors found in test.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/memory/trunk@39352 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ memory/memory-impl/impl/src/test/org/sakai/memory/impl/test/MemoryServiceTest.java
@@ -0,0 +1,101 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakai.memory.impl.test;
+
+import java.util.Random;
+
+import junit.framework.TestCase;
+import net.sf.ehcache.CacheManager;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.authz.api.SecurityService;
+import org.sakaiproject.event.api.EventTrackingService;
+import org.sakaiproject.event.api.UsageSessionService;
+import org.sakaiproject.memory.api.Cache;
+import org.sakaiproject.memory.impl.BasicMemoryService;
+
+/**
+ * @author ieb
+ *
+ */
+public class MemoryServiceTest extends TestCase
+{
+
+	private static Log log = LogFactory.getLog(MemoryServiceTest.class);
+	private EventTrackingService eventTrackingService;
+	private UsageSessionService usageSessionService;
+	private SecurityService securityService;
+	private BasicMemoryService basicMemoryService;
+	private CacheManager cacheManager;
+
+	/**
+	 * @param name
+	 */
+	public MemoryServiceTest(String name)
+	{
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		
+		eventTrackingService = new MockEventTrackingService();
+		securityService = new MockSecurityService();
+		usageSessionService = new MockUsageSessionService();
+		basicMemoryService = new MockBasicMemoryService(eventTrackingService, securityService, usageSessionService );
+		cacheManager = new CacheManager(this.getClass().getResourceAsStream("ehcache.xml"));
+		basicMemoryService.setCacheManager(cacheManager);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+	}
+	
+	public void testCreate() {
+		Cache cache = basicMemoryService.newCache("org.sakaiproject.alias.api.AliasService.callCache","");
+		Random r = new Random();
+		int hit = 0;
+		int miss = 0;
+		for ( int i = 0; i < 10000000; i++) {
+			int k = r.nextInt(1000);
+			if ( cache.containsKey(k)) {
+				Object k2 = cache.get(k);
+				hit++;
+				assertNotNull(k2);
+			} else {
+				cache.put(k,k);
+				miss++;
+			}
+		}
+		log.info("Hits ="+hit+" Misses="+miss);
+	}
+
+}
