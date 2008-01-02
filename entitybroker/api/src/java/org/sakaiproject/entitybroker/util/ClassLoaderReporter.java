7bb22d98a718b926cdc63fe8f4e04b76bb93e55a
Aaron Zeckoski <azeckoski@vt.edu>
2008-01-02 14:10:34 +0000

    SAK-12408: Added comment to the EB interface to make it more clear
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/trunk@39677 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- entitybroker/api/src/java/org/sakaiproject/entitybroker/util/ClassLoaderReporter.java
+++ entitybroker/api/src/java/org/sakaiproject/entitybroker/util/ClassLoaderReporter.java
@@ -4,12 +4,17 @@
 
 package org.sakaiproject.entitybroker.util;
 
+import org.sakaiproject.entitybroker.access.HttpServletAccessProvider;
+
 /**
  * Allows a bean to report the classloader that is appropriate for it and will be used
  * for dispatching into this beans environment, this is only needed in advanced
  * cases and should not normally be implemented<br/>
  * The primary use case here is to allow someone to set their classloader when they
- * are using a proxied bean or the implementation class is in the wrong classloader
+ * are using a proxied bean or the implementation class is in the wrong classloader<br/>
+ * This is primarily used in the case of the {@link HttpServletAccessProvider} and the
+ * implementation of that should also implement this interface to be able to specify the 
+ * classloader
  * 
  * @author Aaron Zeckoski (aaron@caret.cam.ac.uk)
  */
