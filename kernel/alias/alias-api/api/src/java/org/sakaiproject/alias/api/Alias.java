54ab806fddcbe439062d8b51b86817f1b4e64fbb
Ian Boston <ieb@tfd.co.uk>
2008-02-05 09:26:08 +0000

    NOJIRA Pulling alias into kernel space
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@40904 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ kernel/alias/alias-api/api/src/java/org/sakaiproject/alias/api/Alias.java
@@ -0,0 +1,68 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.alias.api;
+
+import org.sakaiproject.entity.api.Entity;
+import org.sakaiproject.time.api.Time;
+import org.sakaiproject.user.api.User;
+
+/**
+ * <p>
+ * Alias ...
+ * </p>
+ */
+public interface Alias extends Entity, Comparable
+{
+	/**
+	 * @return the user who created this.
+	 */
+	User getCreatedBy();
+
+	/**
+	 * @return the user who last modified this.
+	 */
+	User getModifiedBy();
+
+	/**
+	 * @return the time created.
+	 */
+	Time getCreatedTime();
+
+	/**
+	 * @return the time last modified.
+	 */
+	Time getModifiedTime();
+
+	// TODO:
+
+	/**
+	 * Access the alias target.
+	 * 
+	 * @return The alias target.
+	 */
+	String getTarget();
+
+	/**
+	 * @return a description of the item this alias's target applies to.
+	 */
+	String getDescription();
+}
