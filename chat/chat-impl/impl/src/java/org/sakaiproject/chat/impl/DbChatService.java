ca20989f254e67bcec033e747243f2767dfe84b0
Charles Severance <csev@umich.edu>
2008-02-13 23:08:21 +0000

    SAK-12837
    Add methods in the Db service to call the corresponding methods in the Base DB service.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/chat/trunk@41192 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- chat/chat-impl/impl/src/java/org/sakaiproject/chat/impl/DbChatService.java
+++ chat/chat-impl/impl/src/java/org/sakaiproject/chat/impl/DbChatService.java
@@ -37,6 +37,8 @@ import org.sakaiproject.time.api.Time;
 import org.sakaiproject.util.BaseDbDoubleStorage;
 import org.sakaiproject.util.StorageUser;
 import org.sakaiproject.util.Xml;
+import org.sakaiproject.javax.Filter;
+import org.sakaiproject.javax.PagingPosition;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -297,6 +299,21 @@ public class DbChatService extends BaseChatService
 		{
 			super.removeResource(channel, edit);
 		}
+
+		public int getCount(MessageChannel channel)
+		{
+		        return super.getCount(channel);
+		}
+
+		public int getCount(MessageChannel channel, Filter filter)
+		{
+			return super.getCount(channel, filter);
+		}
+
+                public List getMessages(MessageChannel channel, Filter filter, boolean asc, PagingPosition pager)
+                {       
+                        return super.getMessages(channel, filter, asc, pager);
+                }
 
 		public List getMessages(MessageChannel channel, Time afterDate, int limitedToLatest, String draftsForId, boolean pubViewOnly)
 		{
