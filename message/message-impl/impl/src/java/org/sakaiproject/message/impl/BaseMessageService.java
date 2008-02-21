9f6d8402f035bffc7213a9595b198e52b5298a6a
Lance Speelmon <lance@rsmart.com>
2008-02-21 20:03:06 +0000

    SAK-13032 http://jira.sakaiproject.org/jira/browse/SAK-13032  Generic Jira: Code can generate nulpointer exceptions (Static code review)
    git-svn-id: https://source.sakaiproject.org/svn/message/trunk@41500 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- message/message-impl/impl/src/java/org/sakaiproject/message/impl/BaseMessageService.java
+++ message/message-impl/impl/src/java/org/sakaiproject/message/impl/BaseMessageService.java
@@ -4110,7 +4110,7 @@ public abstract class BaseMessageService implements MessageService, StorageUser,
 			// there should not be a case where there's no message or a message with no channel... -ggolden
 			if ((m_message == null) || ((BaseMessageEdit) m_message).m_channel == null)
 			{
-				M_log.warn("setGroupAccess() called with null message: " + m_message.toString() + " or channel: " + ((m_message == null) ? "" : ((BaseMessageEdit) m_message).m_channel.toString()));
+				M_log.warn("setGroupAccess() called with null message: " + ((m_message == null) ? "null" : ((BaseMessageEdit) m_message).toString()) + " or channel: " + ((m_message == null) ? "" : ((BaseMessageEdit) m_message).m_channel.toString()));
 				throw new PermissionException(m_sessionManager.getCurrentSessionUserId(), "access:channel", ((m_message == null) ? "" : ((BaseMessageEdit) m_message).getReference()));
 			}
 
@@ -4174,7 +4174,7 @@ public abstract class BaseMessageService implements MessageService, StorageUser,
 			// there should not be a case where there's no message or a message with no channel... -ggolden
 			if ((m_message == null) || ((BaseMessageEdit) m_message).m_channel == null)
 			{
-				M_log.warn("clearGroupAccess() called with null message: " + m_message.toString() + " or channel: " + ((m_message == null) ? "" : ((BaseMessageEdit) m_message).m_channel.toString()));
+				M_log.warn("clearGroupAccess() called with null message: " + ((m_message == null) ? "null" : ((BaseMessageEdit) m_message).toString()) + " or channel: " + ((m_message == null) ? "" : ((BaseMessageEdit) m_message).m_channel.toString()));
 				throw new PermissionException(m_sessionManager.getCurrentSessionUserId(), "access:channel", ((m_message == null) ? "" : ((BaseMessageEdit) m_message).getReference()));
 			}
 
