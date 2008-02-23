210705e55b5161e51c49fa3e09edd666df6faa81
Dan McCallum <dan.mccallum@gmail.com>
2008-02-23 01:04:57 +0000

    SAK-12848. Added license boilerplate and cleaned up imports.
    git-svn-id: https://source.sakaiproject.org/svn/providers/trunk@41552 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- providers/jldap/src/test/edu/amc/sakai/user/EmailAddressDerivingLdapAttributeMapperTest.java
+++ providers/jldap/src/test/edu/amc/sakai/user/EmailAddressDerivingLdapAttributeMapperTest.java
@@ -1,11 +1,29 @@
-package edu.amc.sakai.user;
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
 
-import java.util.HashSet;
+package edu.amc.sakai.user;
 
 import org.jmock.Mock;
 import org.jmock.cglib.MockObjectTestCase;
 
-import com.novell.ldap.LDAPAttribute;
 import com.novell.ldap.LDAPAttributeSet;
 import com.novell.ldap.LDAPEntry;
 
