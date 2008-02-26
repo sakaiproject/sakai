caca3979a86b8ada9e4919734e819eb1043d2ed5
Beth Kirschner <bkirschn@umich.edu>
2008-02-26 14:14:57 +0000

    SAK-13062 clean up digest messages
    git-svn-id: https://source.sakaiproject.org/svn/announcement/trunk@41634 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- announcement/announcement-impl/impl/src/java/org/sakaiproject/announcement/impl/SiteEmailNotificationAnnc.java
+++ announcement/announcement-impl/impl/src/java/org/sakaiproject/announcement/impl/SiteEmailNotificationAnnc.java
@@ -248,27 +248,19 @@ public class SiteEmailNotificationAnnc extends SiteEmailNotification
 	/**
 	 * @inheritDoc
 	 */
-	protected String getTag(String newline, String title, boolean shouldUseHtml)
+	protected String getTag(String title, boolean shouldUseHtml)
 	{
 		if (shouldUseHtml) {
-			return ("<hr/>" + newline + rb.getString("this") + " "
+			return ("<hr/><br/>" + rb.getString("this") + " "
 					+ ServerConfigurationService.getString("ui.service", "Sakai") + " (<a href=\""
 					+ ServerConfigurationService.getPortalUrl() + "\">" + ServerConfigurationService.getPortalUrl() + "</a>) "
-					+ rb.getString("forthe") + " " + title + " " + rb.getString("site") + newline + rb.getString("youcan") + newline);
+					+ rb.getString("forthe") + " " + title + " " + rb.getString("site") + "<br/>" + rb.getString("youcan") + "<br/>");
 		} else {
-			return (rb.getString("separator") + newline + rb.getString("separator") + newline + rb.getString("this") + " "
+			return (rb.getString("separator") + "\n" + rb.getString("this") + " "
 					+ ServerConfigurationService.getString("ui.service", "Sakai") + " (" + ServerConfigurationService.getPortalUrl()
-					+ ") " + rb.getString("forthe") + " " + title + " " + rb.getString("site") + newline + rb.getString("youcan")
-					+ newline);
+					+ ") " + rb.getString("forthe") + " " + title + " " + rb.getString("site") + "\n" + rb.getString("youcan")
+					+ "\n");
 		}
-	}
-
-	/**
-	 * @inheritDoc
-	 */
-	protected boolean isBodyHTML(Event e)
-	{
-		return true;
 	}
 
 	/**
@@ -402,7 +394,6 @@ public class SiteEmailNotificationAnnc extends SiteEmailNotification
 
 	@Override
 	protected String plainTextContent() {
-		// TODO replace this with a reasonable plain text equivalent
 		String content = htmlContent();
 		content = FormattedText.convertFormattedTextToPlaintext(content);
 		return content;
