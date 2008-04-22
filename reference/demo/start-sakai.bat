b3c20399eceedaf6a8cf8cff0c0ed5bd55a45e64
Lance Speelmon <lance@rsmart.com>
2008-04-22 15:54:11 +0000

    SAK-11553 http://jira.sakaiproject.org/jira/browse/SAK-11553  Demo failed to start correctly if CATALINA_HOME / CATALINA_BASE is set
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@45647 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/demo/start-sakai.bat
+++ reference/demo/start-sakai.bat
@@ -3,5 +3,8 @@
 set JAVA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true
 set CATALINA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true
 
+set CATALINA_HOME=
+set CATALINA_BASE=
+
 bin\startup.bat
 
--- reference/demo/start-sakai.sh
+++ reference/demo/start-sakai.sh
@@ -6,4 +6,7 @@ export JAVA_OPTS
 CATALINA_OPTS="-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true"
 export CATALINA_OPTS
 
+# Demo failed to start correctly if CATALINA_HOME / CATALINA_BASE is set SAK-11553
+unset CATALINA_HOME CATALINA_BASE 
+
 bin/startup.sh; tail -f logs/catalina.out
