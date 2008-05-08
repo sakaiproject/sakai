5337f2dfd2e7b20f301b2a58aee9e7d9b6ab04a6
Ian Boston <ieb@tfd.co.uk>
2008-05-08 22:42:46 +0000

    SAK-7670 Fixes due to API change
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@46314 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/test/org/sakaiproject/search/mock/MockEventTrackingService.java
+++ search/search-impl/impl/src/test/org/sakaiproject/search/mock/MockEventTrackingService.java
@@ -25,7 +25,9 @@ import java.util.Observer;
 
 import org.sakaiproject.event.api.Event;
 import org.sakaiproject.event.api.EventTrackingService;
+import org.sakaiproject.event.api.EventVoter;
 import org.sakaiproject.event.api.UsageSession;
+import org.sakaiproject.user.api.User;
 
 /**
  * @author ieb
@@ -104,6 +106,33 @@ public class MockEventTrackingService implements EventTrackingService
 	{
 		// TODO Auto-generated method stub
 
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sakaiproject.event.api.EventTrackingService#addVoter(org.sakaiproject.event.api.EventVoter)
+	 */
+	public void addVoter(EventVoter arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sakaiproject.event.api.EventTrackingService#deleteVoter(org.sakaiproject.event.api.EventVoter)
+	 */
+	public void deleteVoter(EventVoter arg0)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sakaiproject.event.api.EventTrackingService#post(org.sakaiproject.event.api.Event, org.sakaiproject.user.api.User)
+	 */
+	public void post(Event arg0, User arg1)
+	{
+		// TODO Auto-generated method stub
+		
 	}
 
 }
