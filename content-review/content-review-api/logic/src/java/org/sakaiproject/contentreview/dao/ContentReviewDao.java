8532f08a5abd38e547efe67b4c7ea59331dc5d20
David Horwitz <dhorwitz@gmail.com>
2008-05-08 11:02:58 +0000

    SAK-13489  	added methods to API for locking
    git-svn-id: https://source.sakaiproject.org/svn/content-review/trunk@46279 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content-review/content-review-api/logic/src/java/org/sakaiproject/contentreview/dao/ContentReviewDao.java
+++ content-review/content-review-api/logic/src/java/org/sakaiproject/contentreview/dao/ContentReviewDao.java
@@ -30,4 +30,36 @@ import org.sakaiproject.genericdao.api.CompleteGenericDao;
  */
 public interface ContentReviewDao extends CompleteGenericDao {
 
+	/**
+	    * Allows a lock to be obtained that is system wide,
+	    * this is primarily for ensuring something runs on a single server only in a cluster<br/>
+	    * <b>NOTE:</b> This intentionally returns a null on failure rather than an exception since exceptions will
+	    * cause a rollback which makes the current session effectively dead, this also makes it impossible to 
+	    * control the failure so instead we return null as a marker
+	    * 
+	    * @param lockId the name of the lock which we are seeking
+	    * @param holderId a unique id for the holder of this lock (normally a server id)
+	    * @param timePeriod the length of time (in milliseconds) that the lock should be valid for,
+	    * set this very low for non-repeating processes (the length of time the process should take to run)
+	    * and the length of the repeat period plus the time to run the process for repeating jobs
+	    * @return true if a lock was obtained, false if not, null if failure
+	    */
+	   @SuppressWarnings("unchecked")
+	   public Boolean obtainLock(String lockId, String executerId, long timePeriod);
+	   
+	   /**
+	    * Releases a lock that was being held,
+	    * this is useful if you know a server is shutting down and you want to release your locks early<br/>
+	    * <b>NOTE:</b> This intentionally returns a null on failure rather than an exception since exceptions will
+	    * cause a rollback which makes the current session effectively dead, this also makes it impossible to 
+	    * control the failure so instead we return null as a marker
+	    * 
+	    * @param lockId the name of the lock which we are seeking
+	    * @param holderId a unique id for the holder of this lock (normally a server id)
+	    * @return true if a lock was released, false if not, null if failure
+	    */
+	   @SuppressWarnings("unchecked")
+	   public Boolean releaseLock(String lockId, String executerId);
+	
+	
 }
