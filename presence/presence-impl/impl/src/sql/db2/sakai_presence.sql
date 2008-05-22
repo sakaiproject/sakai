a1bfb6f76668b1aec0b5fb11d682ce009d58a282
John Bush <johntylerbush@gmail.com>
2008-05-22 15:40:38 +0000

    SAK-12986 db2 support
    git-svn-id: https://source.sakaiproject.org/svn/presence/trunk@46653 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ presence/presence-impl/impl/src/sql/db2/sakai_presence.sql
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+-- SAKAI_PRESENCE
+-----------------------------------------------------------------------------
+
+CREATE TABLE SAKAI_PRESENCE
+(
+	SESSION_ID VARCHAR (36),
+	LOCATION_ID VARCHAR (255)
+);
+
+CREATE INDEX PRESENCE_SESS_IDX ON SAKAI_PRESENCE
+(
+	SESSION_ID
+) ALLOW REVERSE SCANS;
+
+CREATE INDEX PRESENCE_LOC_IDX ON SAKAI_PRESENCE
+(
+	LOCATION_ID
+) ALLOW REVERSE SCANS;
