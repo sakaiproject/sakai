9b7179d4d9521778435c1c5a2bfa27696c7897f8
John Bush <johntylerbush@gmail.com>
2008-05-22 15:40:38 +0000

    SAK-12986 db2 support
    git-svn-id: https://source.sakaiproject.org/svn/mailarchive/trunk@46653 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ mailarchive/mailarchive-impl/impl/src/sql/db2/sakai_mailarchive.sql
@@ -0,0 +1,57 @@
+-----------------------------------------------------------------------------
+-- MAILARCHIVE_CHANNEL
+-----------------------------------------------------------------------------
+
+CREATE TABLE MAILARCHIVE_CHANNEL
+(
+    CHANNEL_ID VARCHAR (99) NOT NULL,
+	NEXT_ID INT,
+    XML CLOB
+);
+
+CREATE UNIQUE INDEX MAILARC_CHAN ON MAILARCHIVE_CHANNEL
+(
+	CHANNEL_ID
+) ALLOW REVERSE SCANS;
+
+-----------------------------------------------------------------------------
+-- MAILARCHIVE_MESSAGE
+-----------------------------------------------------------------------------
+
+CREATE TABLE MAILARCHIVE_MESSAGE (
+       CHANNEL_ID           VARCHAR (99) NOT NULL,
+       MESSAGE_ID           VARCHAR (36) NOT NULL,
+       DRAFT                CHAR(1) ,
+                                   CHECK (DRAFT IN ('1', '0')),
+       PUBVIEW              CHAR(1) ,
+                                   CHECK (PUBVIEW IN ('1', '0')),
+       OWNER                VARCHAR (99),
+       MESSAGE_DATE         TIMESTAMP NOT NULL,
+       XML                  CLOB
+) ORGANIZE BY DIMENSIONS (DRAFT, PUBVIEW);
+
+ALTER TABLE MAILARCHIVE_MESSAGE
+       ADD  PRIMARY KEY (CHANNEL_ID, MESSAGE_ID)  ;
+
+CREATE INDEX MAILARC_MSG_OWN ON MAILARCHIVE_MESSAGE
+(
+       OWNER                          ASC
+) ALLOW REVERSE SCANS;
+
+CREATE INDEX MAILARC_MSG_DATE ON MAILARCHIVE_MESSAGE
+(
+       MESSAGE_DATE                   ASC
+) ALLOW REVERSE SCANS;
+
+
+CREATE INDEX MAILARC_MSG_CDD ON MAILARCHIVE_MESSAGE
+(
+	CHANNEL_ID,
+	MESSAGE_DATE
+) ALLOW REVERSE SCANS;
+
+INSERT INTO MAILARCHIVE_CHANNEL (CHANNEL_ID, NEXT_ID, XML) VALUES ('/mailarchive/channel/!site/postmaster', 1, '<?xml version="1.0" encoding="UTF-8"?>
+<channel context="!site" id="postmaster" next-message-id="1">
+	<properties/>
+</channel>
+');
