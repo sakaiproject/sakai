44e41406cdb562093951f8b8aba4b017453e2dc1
Stephen Marquard <stephen.marquard@uct.ac.za>
2008-06-14 17:21:43 +0000

    SAK-10801 Add additional method in test class for new event API
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@47466 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/test/org/sakaiproject/search/mock/MockEventTrackingService.java
+++ search/search-impl/impl/src/test/org/sakaiproject/search/mock/MockEventTrackingService.java
@@ -91,6 +91,15 @@ public class MockEventTrackingService implements EventTrackingService
 	}
 
 	/* (non-Javadoc)
+	 * @see org.sakaiproject.event.api.EventTrackingService#newEvent(java.lang.String, java.lang.String, java.lang.String, boolean, int)
+	 */
+	public Event newEvent(String event, String resource, String context, boolean modify, int priority)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
 	 * @see org.sakaiproject.event.api.EventTrackingService#post(org.sakaiproject.event.api.Event)
 	 */
 	public void post(Event event)
