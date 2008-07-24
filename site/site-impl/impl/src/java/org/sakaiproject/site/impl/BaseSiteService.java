93ee7e87c63519f278ad7ef76168dec780998513
Stephen Marquard <stephen.marquard@uct.ac.za>
2008-07-24 08:59:37 +0000

    SAK-14070 Merge site caching fixes from branch
    git-svn-id: https://source.sakaiproject.org/svn/site/trunk@49325 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site/site-impl/impl/src/java/org/sakaiproject/site/impl/BaseSiteService.java
+++ site/site-impl/impl/src/java/org/sakaiproject/site/impl/BaseSiteService.java
@@ -555,24 +555,23 @@ public abstract class BaseSiteService implements SiteService, StorageUser
 		if (id == null) throw new IdUnusedException("<null>");
 
 		Site rv = getCachedSite(id);
+		if ( rv != null ) return rv;
 
 		rv = m_storage.get(id);
 
 		// if not found
 		if (rv == null) throw new IdUnusedException(id);
 
-		// get all of the site loaded
-		rv.loadAll();
-
-		// track it - we don't track site access -ggolden
-		// EventTrackingService.post(EventTrackingService.newEvent(SECURE_ACCESS_SITE, site.getReference()));
-
-		// cache a copy
+		// Thanks to the SiteCacheImpl class lurking beneath the
+		// surface, caching the site will have the side-effect
+		// of doing a "loadAll()" plus more.
 		if (m_siteCache != null)
 		{
-			Site copy = new BaseSite(this,rv, true);
-			String ref = siteReference(id);
-			m_siteCache.put(ref, copy, m_cacheSeconds);
+			m_siteCache.put(siteReference(id), rv, m_cacheSeconds);
+			// Return a copy.
+			rv = new BaseSite(this, rv, true);
+		} else {
+			rv.loadAll();
 		}
 
 		return rv;
