2d685bf7f7a4dd569d921d9cbdc72d918b1386fc
David Horwitz <dhorwitz@gmail.com>
2008-07-28 08:15:48 +0000

    SAK-13310 ply changes to oracle script as sugested by seth and fix the correct column in hibernate query
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@49417 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- polls/docs/SAK-13310.sql
+++ polls/docs/SAK-13310.sql
@@ -3,4 +3,4 @@
 alter table POLL_POLL change POLL_DETAILS POLL_DETAILS text;
 
 #oracle
-alter table POLL_POLL modify POLL_DETAILS varchar2; 
+alter table POLL_POLL modify POLL_DETAILS VARCHAR2(4000); 
