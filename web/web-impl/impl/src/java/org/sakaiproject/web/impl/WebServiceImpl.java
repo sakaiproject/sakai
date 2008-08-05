66a117c316acd3096aba5308b85145d7fa7cf64a
Charles Severance <csev@umich.edu>
2008-08-05 18:41:48 +0000

    SAK-12938
    git-svn-id: https://source.sakaiproject.org/svn/web/trunk@50203 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- web/web-impl/impl/src/java/org/sakaiproject/web/impl/WebServiceImpl.java
+++ web/web-impl/impl/src/java/org/sakaiproject/web/impl/WebServiceImpl.java
@@ -3,7 +3,7 @@
  * $Id: WebServiceImpl.java 39315 2007-12-15 18:08:26Z cwen@iupui.edu $
  ***********************************************************************************
  *
- * Copyright 2006 Sakai Foundation
+ * Copyright 2003, 2004, 2005, 2006, 2007, 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. 
