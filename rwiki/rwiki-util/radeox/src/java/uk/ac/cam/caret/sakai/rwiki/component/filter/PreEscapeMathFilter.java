b0e00e9e65dadb515d7ab3ad69aef400878c2b6d
Charles Severance <csev@umich.edu>
2008-08-05 12:27:34 +0000

    SAK-12938
    git-svn-id: https://source.sakaiproject.org/svn/rwiki/trunk@50106 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- rwiki/rwiki-util/radeox/src/java/uk/ac/cam/caret/sakai/rwiki/component/filter/PreEscapeMathFilter.java
+++ rwiki/rwiki-util/radeox/src/java/uk/ac/cam/caret/sakai/rwiki/component/filter/PreEscapeMathFilter.java
@@ -1,24 +1,17 @@
 /*
- * This file is part of "Sakai Radeox Rendering Engine".
+ * Copyright 2006 Sakai Foundation
  *
- * Copyright (c) 2005 Andrew Thornton
- * All Rights Reserved.
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
  *
- * Please visit http://radeox.org/ for updates and contact.
+ *       http://www.osedu.org/licenses/ECL-2.0
  *
- * --LICENSE NOTICE--
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
  * limitations under the License.
- * --LICENSE NOTICE--
  */
 
 package uk.ac.cam.caret.sakai.rwiki.component.filter;
