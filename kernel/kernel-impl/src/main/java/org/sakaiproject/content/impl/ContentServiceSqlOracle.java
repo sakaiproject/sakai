fd0742bfb05551a6962c96f4660f82353f2f50a2
Charles Severance <csev@umich.edu>
2008-08-24 04:38:02 +0000

    SAK-12938
    Fix non printing characters.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@51317 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/ContentServiceSqlOracle.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/ContentServiceSqlOracle.java
@@ -3,18 +3,18 @@
  * $Id: ContentServiceSqlOracle.java 38956 2007-12-03 18:10:39Z jimeng@umich.edu $
  ***********************************************************************************
  *
- * Copyright 2007, 2008 Sakai Foundation
+ * Copyright (c) 2007, 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *       http://www.osedu.org/licenses/ECL-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  **********************************************************************************/
