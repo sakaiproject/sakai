0db7a539402a57f44467dcc428a1969712250c87
Charles Severance <csev@umich.edu>
2008-08-24 05:28:47 +0000

    SAK-12938
    Fix non-printing characters in copyright.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/trunk@51318 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- entitybroker/api/src/java/org/sakaiproject/entitybroker/entityprovider/capabilities/TagSearchable.java
+++ entitybroker/api/src/java/org/sakaiproject/entitybroker/entityprovider/capabilities/TagSearchable.java
@@ -3,18 +3,18 @@
  * $URL$
  * TagSearchable.java - entity-broker - Mar 21, 2008 9:39:17 PM - azeckoski
  **************************************************************************
- * Copyright 2008 Sakai Foundation
+ * Copyright (c) 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License. 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *       http://www.osedu.org/licenses/ECL-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
