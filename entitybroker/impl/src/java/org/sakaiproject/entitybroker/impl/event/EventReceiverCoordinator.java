6a5e6117c84cf8f518540a2003461dd10112f1b6
Aaron Zeckoski <azeckoski@vt.edu>
2008-08-24 21:02:02 +0000

    http://jira.sakaiproject.org/jira/browse/EVALSYS-607 Made adjustment to the EB observer from local to general
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/trunk@51329 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- entitybroker/impl/src/java/org/sakaiproject/entitybroker/impl/event/EventReceiverCoordinator.java
+++ entitybroker/impl/src/java/org/sakaiproject/entitybroker/impl/event/EventReceiverCoordinator.java
@@ -23,94 +23,101 @@ package org.sakaiproject.entitybroker.impl.event;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.sakaiproject.entitybroker.event.EventReceiver;
 import org.sakaiproject.event.api.Event;
 import org.sakaiproject.event.api.EventTrackingService;
+import org.sakaiproject.genericdao.util.refmap.ReferenceMap;
+import org.sakaiproject.genericdao.util.refmap.ReferenceType;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 /**
  * Collects beans implementing {@link EventReceiver} from around the context, and distributes
- * matching events to them.
+ * matching events to them.<br/>
+ * TODO - make a way to handle events by allowing manual registration of receivers
  * 
  * @author Antranig Basman (antranig@caret.cam.ac.uk)
+ * @author Aaron Zeckoski (azeckoski at gmail.com)
  */
 public class EventReceiverCoordinator implements ApplicationContextAware {
 
-   private Map<EventReceiver, Boolean> receivers = new ConcurrentHashMap<EventReceiver, Boolean>();
+    private Map<ClassLoader, EventReceiver> receivers = new ReferenceMap<ClassLoader, EventReceiver>(ReferenceType.SOFT, ReferenceType.STRONG);
 
-   EventTrackingService eventTrackingService;
+    EventTrackingService eventTrackingService;
+    public void setEventTrackingService(EventTrackingService eventTrackingService) {
+        this.eventTrackingService = eventTrackingService;
+    }
 
-   public void setEventTrackingService(EventTrackingService eventTrackingService) {
-      this.eventTrackingService = eventTrackingService;
-   }
-
-   public void init() {
-      eventTrackingService.addLocalObserver(new Observer() {
-
-         public void update(Observable o, Object arg) {
-            if (!(arg instanceof Event))
-               return;
-            Event event = (Event) arg;
-            handleEvent(event);
-         }
-      });
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
-    */
-   public void setApplicationContext(ApplicationContext context) throws BeansException {
-      String[] autobeans = context.getBeanNamesForType(EventReceiver.class, false, false);
-      for (String autobean : autobeans) {
-         EventReceiver register = (EventReceiver) context.getBean(autobean);
-         receivers.put(register, Boolean.TRUE);
-      }
-   }
-
-   /**
-    * @param receiver
-    * @param event
-    * @return
-    */
-   private boolean match(EventReceiver receiver, Event event) {
-      String name = event.getEvent();
-      String[] prefixes = receiver.getEventNamePrefixes();
-
-      boolean matchprefix = false;
-      if (prefixes == null) {
-         matchprefix = true;
-      } else {
-         for (String prefix : prefixes) {
-            if (name.startsWith(prefix)) {
-               matchprefix = true;
-               break;
+    public void init() {
+        // register a single observer for the EB system (switched from local observer)
+        eventTrackingService.addObserver(new Observer() {
+            public void update(Observable o, Object arg) {
+                if (arg instanceof Event) {
+                    Event event = (Event) arg;
+                    handleEvent(event);
+                }
             }
-         }
-      }
-      if (!matchprefix)
-         return false;
-      String resprefix = receiver.getResourcePrefix();
-      if (resprefix == null || event.getResource().startsWith(resprefix)) {
-         return true;
-      } else
-         return false;
-   }
+        });
+    }
 
-   /**
-    * @param event
-    */
-   protected void handleEvent(Event event) {
-      for (EventReceiver receiver : receivers.keySet()) {
-         if (match(receiver, event)) {
-            receiver.receiveEvent(event.getEvent(), event.getResource());
-         }
-      }
-   }
+    /*
+     * (non-Javadoc)
+     * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+     */
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        String[] autobeans = context.getBeanNamesForType(EventReceiver.class, false, false);
+        for (String autobean : autobeans) {
+            EventReceiver receiver = (EventReceiver) context.getBean(autobean);
+            if (receiver != null) {
+                receivers.put(receiver.getClass().getClassLoader(), receiver);
+            }
+        }
+    }
+
+    /**
+     * Called when events occur which come in from the event system
+     * @param event the event from the system
+     */
+    protected void handleEvent(Event event) {
+        for (EventReceiver receiver : receivers.values()) {
+            if (match(receiver, event)) {
+                receiver.receiveEvent(event.getEvent(), event.getResource());
+            }
+        }
+    }
+
+    /**
+     * @param receiver
+     * @param event
+     * @return
+     */
+    private boolean match(EventReceiver receiver, Event event) {
+        String name = event.getEvent();
+        String[] prefixes = receiver.getEventNamePrefixes();
+
+        boolean matchprefix = false;
+        if (prefixes == null) {
+            matchprefix = true;
+        } else {
+            for (String prefix : prefixes) {
+                if (name.startsWith(prefix)) {
+                    matchprefix = true;
+                    break;
+                }
+            }
+        }
+        if (!matchprefix) {
+            return false;
+        }
+        String resprefix = receiver.getResourcePrefix();
+        if (resprefix == null 
+                || event.getResource().startsWith(resprefix)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
