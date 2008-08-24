cb4fc2b5c6f7af6b7c2b08256ffb6f3d8069a853
Charles Severance <csev@umich.edu>
2008-08-24 07:20:58 +0000

    SAK-12938
    Copyrght cleanup.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@51321 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- polls/tool/src/java/org/sakaiproject/poll/tool/entityproviders/PollEntityProvider.java
+++ polls/tool/src/java/org/sakaiproject/poll/tool/entityproviders/PollEntityProvider.java
@@ -3,13 +3,19 @@
  * $URL$
  * PollEntityProvider.java - polls - Aug 21, 2008 7:34:47 PM - azeckoski
  **************************************************************************
- * Copyright (c) 2008 Aaron Zeckoski
- * Licensed under the Apache License, Version 2.0
- * 
- * A copy of the Apache License has been included in this 
- * distribution and is available at: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * Copyright (c) 2008 Sakai Foundation
  *
- * Aaron Zeckoski (azeckoski @ gmail.com) (aaronz @ vt.edu) (aaron @ caret.cam.ac.uk)
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.sakaiproject.poll.tool.entityproviders;
