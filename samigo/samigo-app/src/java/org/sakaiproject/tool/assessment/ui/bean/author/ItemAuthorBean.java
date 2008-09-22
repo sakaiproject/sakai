d4b1278c6a7f7375c63df086a453ffd8e1b9693a
Karen Tsao <ktsao@stanford.edu>
2008-09-22 17:18:00 +0000

    SAK-14151
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@52562 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/ItemAuthorBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/ItemAuthorBean.java
@@ -1188,6 +1188,9 @@ public class ItemAuthorBean
       session.removeAttribute(FilePickerHelper.FILE_PICKER_CANCEL);
       return newAttachmentList;
     }
+    else if (item == null) {
+    	return new ArrayList();
+    }
     else return item.getItemAttachmentList();
   }
 
