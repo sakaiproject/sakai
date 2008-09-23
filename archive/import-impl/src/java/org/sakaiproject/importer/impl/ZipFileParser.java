3343bb8275228e80f3f5b940b38206899522840b
Aaron Zeckoski <azeckoski@vt.edu>
2008-09-23 13:49:31 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-14386 NO 49,239: http://qa1-nl.sakaiproject.org/codereview/trunk/api/org/sakaiproject/importer/impl/ZipFileParser.java.html#205
    Need a finally or is.close may not be called.
    Like very much the comments
    
     protected byte[] getBytesFromFile(File file) throws IOException {
    205         InputStream is = new FileInputStream(file);
    206
    207         // Get the size of the file
    208         long length = file.length();
    209
    210         // You cannot create an array using a long type.
    211         // It needs to be an int type.
    212         // Before converting to an int type, check
    213         // to ensure that file is not larger than Integer.MAX_VALUE.
    214         if (length > Integer.MAX_VALUE) {
    215             // File is too large
    216         }
    217
    218         // Create the byte array to hold the data
    219         byte[] bytes = new byte[(int)length];
    220
    221         // Read in the bytes
    222         int offset = 0;
    223         int numRead = 0;
    224         while (offset < bytes.length
    225                && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
    226             offset += numRead;
    227         }
    228
    229         // Ensure all the bytes have been read in
    230         if (offset < bytes.length) {
    231             throw new IOException("Could not completely read file "+file.getName());
    232         }
    233
    234         // Close the input stream and return bytes
    235         is.close();
    236         return bytes;
    237     }
    
    
    git-svn-id: https://source.sakaiproject.org/svn/archive/trunk@52595 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- archive/import-impl/src/java/org/sakaiproject/importer/impl/ZipFileParser.java
+++ archive/import-impl/src/java/org/sakaiproject/importer/impl/ZipFileParser.java
@@ -228,41 +228,50 @@ public abstract class ZipFileParser implements ImportFileParser {
 		} catch (SAXException e) {
 			e.printStackTrace();
 			return null;
+		} finally {
+		    try {
+                zipStream.close();
+            } catch (IOException e) {
+                // we tried
+            }
 		}
 	}
 	
 	protected byte[] getBytesFromFile(File file) throws IOException {
         InputStream is = new FileInputStream(file);
     
-        // Get the size of the file
-        long length = file.length();
-    
-        // You cannot create an array using a long type.
-        // It needs to be an int type.
-        // Before converting to an int type, check
-        // to ensure that file is not larger than Integer.MAX_VALUE.
-        if (length > Integer.MAX_VALUE) {
-            // File is too large
-        }
-    
         // Create the byte array to hold the data
-        byte[] bytes = new byte[(int)length];
-    
-        // Read in the bytes
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length
-               && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
-            offset += numRead;
+        byte[] bytes;
+        try {
+            // Get the size of the file
+            long length = file.length();
+   
+            // You cannot create an array using a long type.
+            // It needs to be an int type.
+            // Before converting to an int type, check
+            // to ensure that file is not larger than Integer.MAX_VALUE.
+            if (length > Integer.MAX_VALUE) {
+                // File is too large
+            }
+   
+            bytes = new byte[(int)length];
+   
+            // Read in the bytes
+            int offset = 0;
+            int numRead = 0;
+            while (offset < bytes.length
+                   && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+                offset += numRead;
+            }
+   
+            // Ensure all the bytes have been read in
+            if (offset < bytes.length) {
+                throw new IOException("Could not completely read file "+file.getName());
+            }
+        } finally {
+            // Close the input stream and return bytes
+            is.close();
         }
-    
-        // Ensure all the bytes have been read in
-        if (offset < bytes.length) {
-            throw new IOException("Could not completely read file "+file.getName());
-        }
-    
-        // Close the input stream and return bytes
-        is.close();
         return bytes;
     }
 
