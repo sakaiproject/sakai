44d77ca6ed3013a3e1bd496a5a0061cc05d72a71
Steve Swinsburg <steve.swinsburg@gmail.com>
2008-09-24 13:20:44 +0000

    new package structure
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70148 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/EditProfile.java
@@ -0,0 +1,15 @@
+package uk.ac.lancs.e_science.profile2.tool.pages;
+
+import org.apache.log4j.Logger;
+
+
+public class EditProfile extends BasePage {
+
+	private transient Logger log = Logger.getLogger(EditProfile.class);
+
+	public EditProfile() {
+		
+		if(log.isDebugEnabled()) log.debug("EditProfile()");
+
+	}
+}
---
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/ViewProfile.java
@@ -0,0 +1,15 @@
+package uk.ac.lancs.e_science.profile2.tool.pages;
+
+import org.apache.log4j.Logger;
+
+
+public class ViewProfile extends BasePage {
+
+	private transient Logger log = Logger.getLogger(ViewProfile.class);
+
+	public ViewProfile() {
+		
+		if(log.isDebugEnabled()) log.debug("ViewProfile()");
+
+	}
+}
