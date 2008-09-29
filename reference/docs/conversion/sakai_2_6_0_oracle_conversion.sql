851cecb58a14738b71eb1f1434026b81705e1dd0
Ray Davis <raydavis@berkeley.edu>
2008-09-29 18:25:09 +0000

    SAK-14533 Add new IS_CURRENT column to academic sessions table of default Course Management implementation
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@52907 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
@@ -193,3 +193,12 @@ alter table SAM_ASSESSMENTGRADING_T add ISAUTOSUBMITTED number(1, 0) default '0'
 alter table GB_GRADABLE_OBJECT_T add (IS_EXTRA_CREDIT number(1,0), ASSIGNMENT_WEIGHTING double precision);
 alter table GB_CATEGORY_T add (IS_EXTRA_CREDIT number(1,0));
 alter table GB_GRADE_RECORD_T add (IS_EXCLUDED_FROM_GRADE number(1,0));
+
+-- SAK-12883, SAK-12582 - Allow control over which academic sessions are
+-- considered current; support more than one current academic session
+alter table CM_ACADEMIC_SESSION_T add IS_CURRENT number(1,0) default 0 not null;
+
+-- WARNING: This simply emulates the old runtime behavior. It is strongly
+-- recommended that you decide which terms should be treated as current
+-- and edit this script accordingly!
+update CM_ACADEMIC_SESSION_T set IS_CURRENT=1 where SYSDATE >= START_DATE and SYSDATE <= END_DATE;
