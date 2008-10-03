9296dd8195cc4d2d35380d00401cab750961e8d1
Charles Severance <csev@umich.edu>
2008-10-03 21:45:30 +0000

    SAK-13584
    Update the MailArchive 2.6 conversion script.
    
    As part of a 2.6 upgrade this script needs to be run on the database.
    It adds a BODY and SUBJECT column and extracts these 
    out of the XML to improve searching, sorting, and 
    paging performance.
    
    The conversion script is in the mialarchive directory and 
    it is called as follows:
    
    ./mailarchive-runconversion.sh upgradeschema-2.6-oracle.config 
    
    You must do this with CATALINA_HOME pointing to your Sakai
    deployment - it looks through sakai.properties for conneciton 
    details and finds the DB drivers in your Tomcat as well.
    
    The convrsion can be run more than once - it notices when a 
    message alreayd has been converted and skipps it.  You 
    can even drop the columns and then re-run the conversion.
    
    Please put this in 2.6 - it is a blocker.
    
    Note - this is a required conversion as part of 2.6 - it needs to go 
    into the release notes somewhere.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/mailarchive/trunk@53121 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- mailarchive/mailarchive-runconversion.sh
+++ mailarchive/mailarchive-runconversion.sh
@@ -1,41 +1,50 @@
 #!/bin/sh
 #
-# Usage:
-#   content-runconversion.sh -j JDBC_DRIVER_JAR -p SAKAI_PROPERTIES_FILE UPGRADESCHEMA_CONFIG
+# you could use the runconversion like this:
 #
-# Example:
-#   content-runconversion.sh -j "$CATALINA_HOME/shared/lib/ojdbc14.jar" \
-#      -p "$CATALINA_HOME/sakai/sakai.properties" \
-#      upgradeschema-2.6-oracle.config
+# ./mailarchive-runconversion.sh upgradeschema-2.6-mysql.config
+#
+# Or this
+#
+# ./mailarchive-runconversion.sh upgradeschema-2.6-oracle.config 
 
-# sh  mailarchive-runconversion.sh  \
-#       -j "/Users/csev/dev/sakai-trunk/apache-tomcat-5.5.23/common/lib/mysql-connector-java-5.1.6-bin.jar" \
-#       -p "/Users/csev/dev/sakai-trunk/apache-tomcat-5.5.23/sakai/sakai.properties" \
-#       upgradeschema-2.6-mysql.config
-
-
-# sh  mailarchive-runconversion.sh  \
-#       -j "/Users/csev/dev/sakai-trunk/apache-tomcat-5.5.23/shared/lib/ojdbc14.jar" \
-#       -p "/Users/csev/dev/sakai-trunk/apache-tomcat-5.5.23/sakai/sakai.properties" \
-#       upgradeschema-2.6-mysql.config
-
-# The "runconversion.sh" script does not have to be run
-# in place. It could be copied to the working directory.
-
-# For Cygwin, ensure paths are in the proper format.
-cygwin=false;
-case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-esac
-if $cygwin; then
-  [ -n "$CLASSPATH" ] && CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
-  m2repository=`cygpath --path --unix "$HOMEDRIVE""$HOMEPATH"`/.m2/repository
+if [ -d $CATALINA_HOME ] ;
+then
+  echo "Catalina_Home: $CATALINA_HOME"
 else
-  m2repository="$HOME"/.m2/repository
-fi
+  echo '$CATALINA_HOME not set properly ' $CATALINA_HOME
+fi 
 
-bash ./runconversion.sh \
-	-j "$m2repository"/commons-collections/commons-collections/3.2/commons-collections-3.2.jar \
-	-j "$m2repository"/commons-pool/commons-pool/1.3/commons-pool-1.3.jar \
-	-j "$m2repository"/org/sakaiproject/sakai-mailarchive-impl/SNAPSHOT/sakai-mailarchive-impl-SNAPSHOT.jar \
-	$@
+##### MAIL SPECIFIC STUFF #####
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/sakai-mailarchive-impl/2.6.0RC1-SNAPSHOT/sakai-mailarchive-impl-2.6.0RC1-SNAPSHOT.jar"
+
+##### COMMON KERNEL STUFF #####
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-logging/commons-logging/1.0.4/commons-logging-1.0.4.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-collections/commons-collections/3.2/commons-collections-3.2.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-dbcp/commons-dbcp/1.2.2/commons-dbcp-1.2.2.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/commons-pool/commons-pool/1.3/commons-pool-1.3.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-api/1.0RC2-SNAPSHOT/sakai-kernel-api-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-impl/1.0RC2-SNAPSHOT/sakai-kernel-impl-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-util/1.0RC2-SNAPSHOT/sakai-kernel-util-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-common/1.0RC2-SNAPSHOT/sakai-kernel-common-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-component/1.0RC2-SNAPSHOT/sakai-kernel-component-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/org/sakaiproject/kernel/sakai-kernel-private/1.0RC2-SNAPSHOT/sakai-kernel-private-1.0RC2-SNAPSHOT.jar"
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/log4j/log4j/1.2.9/log4j-1.2.9.jar"
+
+##### JDBC DRIVER #####
+##### SUPPLY PATH TO YOUR JDBC DRIVER #####
+## MYSQL ##
+CLASSPATH="$CLASSPATH:$HOME/.m2/repository/mysql/mysql-connector-java/3.1.14/mysql-connector-java-3.1.14-bin.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/mysql-connector-java-3.1.14-bin.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/mysql-connector-java-5.0.5-bin.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/mysql-connector-java-5.1.6-bin.jar"
+## ORACLE ##
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/ojdbc-14.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/ojdbc14.jar"
+
+# echo $CLASSPATH
+
+java $JAVA_OPTS  \
+      -classpath "$CLASSPATH" \
+      -Dsakai.properties=/Users/csev/dev/sakai-trunk/apache-tomcat-5.5.23/sakai/sakai.properties \
+	org.sakaiproject.util.conversion.UpgradeSchema "$@" 
