13dd45fc0c30140a0c374d0845f602a508461f6f
Charles Severance <csev@umich.edu>
2008-10-04 12:18:44 +0000

    SAK-12938
    Caught a straggler thanks to Alan.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/emailtemplateservice/trunk@53127 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- emailtemplateservice/tool/src/java/org/sakaiproject/emailtemplateservice/tool/producers/ModifyEmailProducer.java
+++ emailtemplateservice/tool/src/java/org/sakaiproject/emailtemplateservice/tool/producers/ModifyEmailProducer.java
@@ -3,13 +3,19 @@
  * $URL$
  * ModifyEmailProducer.java - evaluation - Feb 29, 2008 6:06:42 PM - azeckoski
  **************************************************************************
- * Copyright (c) 2008 Centre for Applied Research in Educational Technologies, University of Cambridge
- * Licensed under the Educational Community License version 1.0
- * 
- * A copy of the Educational Community License has been included in this 
- * distribution and is available at: http://www.opensource.org/licenses/ecl1.php
+ * Copyright (c) 2008 Sakai Foundation
  *
- * Aaron Zeckoski (azeckoski@gmail.com) (aaronz@vt.edu) (aaron@caret.cam.ac.uk)
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.sakaiproject.emailtemplateservice.tool.producers;
