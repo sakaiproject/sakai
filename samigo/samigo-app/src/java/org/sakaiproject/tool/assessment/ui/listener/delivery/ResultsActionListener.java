659dedbe015af1d6c1f233f1553f33c035d8f975
Charles Severance <csev@umich.edu>
2008-10-04 13:11:29 +0000

    SAK-12938
    A copyright straggler.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@53131 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/ResultsActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/ResultsActionListener.java
@@ -3,13 +3,13 @@
  * $Id: ResultsActionListener.java 34256 2007-08-22 18:00:32Z ktsao@stanford.edu $
  ***********************************************************************************
  *
- * Copyright (c) 2006 The Sakai Foundation.
+ * Copyright (c) 2006 Sakai Foundation
  *
- * Licensed under the Educational Community License, Version 1.0 (the"License");
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.opensource.org/licenses/ecl1.php
+ *       http://www.osedu.org/licenses/ECL-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
