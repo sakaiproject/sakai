becf2f5c2019f5a8c2268dbf64b77395d5c8d241
Steve Swinsburg <steve.swinsburg@gmail.com>
2008-11-11 15:44:05 +0000

    now a reusable component. still needs more constructors to make it more useful.
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70163 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/components/CloseButton.java
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/components/CloseButton.java
@@ -3,17 +3,29 @@ package uk.ac.lancs.e_science.profile2.tool.components;
 import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.image.ContextImage;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 
-public class CloseButton extends WebMarkupContainer{
+public class CloseButton extends Panel{
 
+	private static final String CLOSE_IMAGE = "/library/image/silk/cross.png";
+	
 	public CloseButton(String id) {
 		super(id);
-		setOutputMarkupId(true);
+	
+		WebMarkupContainer closeButton = new WebMarkupContainer("closeButton");
+		closeButton.setOutputMarkupId(true);
+		
+		ContextImage image = new ContextImage("img",new Model(CLOSE_IMAGE));
+		closeButton.add(image);
+		
+		add(closeButton);
+	
+		
+		//extend this to allow a behaviour to be set so that when its clicked, something happens
 	}
-
-	/*
-	 * this is meant to be a drop in close button component, will need to accept some args
-	 * 
-	 */
-
+	
+	
 }
