88b84d08c48be0d730a35c75cbc91899c77e5286
Steve Swinsburg <steve.swinsburg@gmail.com>
2008-11-19 20:24:49 +0000

    classes and markup for interests panel
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70169 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/panels/MyInterestsDisplay.java
@@ -0,0 +1,113 @@
+package uk.ac.lancs.e_science.profile2.tool.pages.panels;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.ResourceModel;
+
+import uk.ac.lancs.e_science.profile2.tool.models.UserProfile;
+
+public class MyInterestsDisplay extends Panel {
+	
+	private transient Logger log = Logger.getLogger(MyInterestsDisplay.class);
+	
+	private int visibleFieldCount = 0;
+
+	
+	public MyInterestsDisplay(final String id, final UserProfile userProfile) {
+		super(id);
+		
+		//this panel stuff
+		final Component thisPanel = this;
+		
+		//get userProfile from userProfileModel
+		//UserProfile userProfile = (UserProfile) this.getModelObject();
+		
+		//get info from userProfile since we need to validate it and turn things off if not set.
+		//otherwise we could just use a propertymodel
+		String favouriteBooks = userProfile.getFavouriteBooks();
+		String favouriteTvShows = userProfile.getFavouriteTvShows();
+		String favouriteMovies = userProfile.getFavouriteMovies();
+		String favouriteQuotes = userProfile.getFavouriteQuotes();
+		
+		//heading
+		add(new Label("heading", new ResourceModel("heading.interests")));
+		
+		//favourite books
+		WebMarkupContainer booksContainer = new WebMarkupContainer("booksContainer");
+		booksContainer.add(new Label("booksLabel", new ResourceModel("profile.favourite.books")));
+		booksContainer.add(new Label("favouriteBooks", favouriteBooks));
+		add(booksContainer);
+		if("".equals(favouriteBooks) || favouriteBooks == null) {
+			booksContainer.setVisible(false);
+		} else {
+			visibleFieldCount++;
+		}
+		
+		//favourite tv shows
+		WebMarkupContainer tvContainer = new WebMarkupContainer("tvContainer");
+		tvContainer.add(new Label("tvLabel", new ResourceModel("profile.favourite.tv")));
+		tvContainer.add(new Label("favouriteTvShows", favouriteTvShows));
+		add(tvContainer);
+		if("".equals(favouriteTvShows) || favouriteTvShows == null) {
+			tvContainer.setVisible(false);
+		} else {
+			visibleFieldCount++;
+		}
+		
+		//favourite movies
+		WebMarkupContainer moviesContainer = new WebMarkupContainer("moviesContainer");
+		moviesContainer.add(new Label("moviesLabel", new ResourceModel("profile.favourite.movies")));
+		moviesContainer.add(new Label("favouriteMovies", favouriteMovies));
+		add(moviesContainer);
+		if("".equals(favouriteMovies) || favouriteMovies == null) {
+			moviesContainer.setVisible(false);
+		} else {
+			visibleFieldCount++;
+		}
+		
+		//favourite quotes
+		WebMarkupContainer quotesContainer = new WebMarkupContainer("quotesContainer");
+		quotesContainer.add(new Label("quotesLabel", new ResourceModel("profile.favourite.quotes")));
+		quotesContainer.add(new Label("favouriteQuotes", favouriteQuotes));
+		add(quotesContainer);
+		if("".equals(favouriteQuotes) || favouriteQuotes == null) {
+			quotesContainer.setVisible(false);
+		} else {
+			visibleFieldCount++;
+		}
+		
+		
+				
+		//edit button
+		AjaxFallbackLink editButton = new AjaxFallbackLink("editButton", new ResourceModel("button.edit")) {
+			public void onClick(AjaxRequestTarget target) {
+				Component newPanel = new MyInterestsEdit(id, userProfile);
+				newPanel.setOutputMarkupId(true);
+				thisPanel.replaceWith(newPanel);
+				if(target != null) {
+					target.addComponent(newPanel);
+				}
+				
+			}
+						
+		};
+		editButton.setOutputMarkupId(true);
+		add(editButton);
+		
+		//no fields message
+		Label noFieldsMessage = new Label("noFieldsMessage", new ResourceModel("text.no.fields"));
+		add(noFieldsMessage);
+		if(visibleFieldCount > 0) {
+			noFieldsMessage.setVisible(false);
+		}
+		
+	}
+	
+}
---
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/panels/MyInterestsEdit.java
@@ -0,0 +1,155 @@
+package uk.ac.lancs.e_science.profile2.tool.pages.panels;
+
+
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.ajax.markup.html.form.AjaxFallbackButton;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
+import org.sakaiproject.api.common.edu.person.SakaiPerson;
+
+import uk.ac.lancs.e_science.profile2.api.SakaiProxy;
+import uk.ac.lancs.e_science.profile2.tool.ProfileApplication;
+import uk.ac.lancs.e_science.profile2.tool.models.UserProfile;
+
+public class MyInterestsEdit extends Panel {
+	
+	private transient Logger log = Logger.getLogger(MyInterestsEdit.class);
+	
+	public MyInterestsEdit(final String id, final UserProfile userProfile) {
+		super(id);
+		
+		//this panel stuff
+		final Component thisPanel = this;
+				
+		//create model
+		CompoundPropertyModel userProfileModel = new CompoundPropertyModel(userProfile);
+		
+		//heading
+		add(new Label("heading", new ResourceModel("heading.interests.edit")));
+		
+		//setup form		
+		Form form = new Form("form", userProfileModel);
+		form.setOutputMarkupId(true);
+		
+		//We don't need to get the info from userProfile, we load it into the form with a property model
+	    //just make sure that the form element id's match those in the model
+	   		
+		//favourite books
+		WebMarkupContainer booksContainer = new WebMarkupContainer("booksContainer");
+		booksContainer.add(new Label("booksLabel", new ResourceModel("profile.favourite.books")));
+		TextArea favouriteBooks = new TextArea("favouriteBooks", new PropertyModel(userProfile, "favouriteBooks"));
+		booksContainer.add(favouriteBooks);
+		form.add(booksContainer);
+		
+		//favourite tv shows
+		WebMarkupContainer tvContainer = new WebMarkupContainer("tvContainer");
+		tvContainer.add(new Label("tvLabel", new ResourceModel("profile.favourite.tv")));
+		TextArea favouriteTvShows = new TextArea("favouriteTvShows", new PropertyModel(userProfile, "favouriteTvShows"));
+		tvContainer.add(favouriteTvShows);
+		form.add(tvContainer);
+		
+		//favourite movies
+		WebMarkupContainer moviesContainer = new WebMarkupContainer("moviesContainer");
+		moviesContainer.add(new Label("moviesLabel", new ResourceModel("profile.favourite.movies")));
+		TextArea favouriteMovies = new TextArea("favouriteMovies", new PropertyModel(userProfile, "favouriteMovies"));
+		booksContainer.add(favouriteMovies);
+		form.add(moviesContainer);
+		
+		//favourite quotes
+		WebMarkupContainer quotesContainer = new WebMarkupContainer("quotesContainer");
+		quotesContainer.add(new Label("quotesLabel", new ResourceModel("profile.favourite.quotes")));
+		TextArea favouriteQuotes = new TextArea("favouriteQuotes", new PropertyModel(userProfile, "favouriteQuotes"));
+		quotesContainer.add(favouriteQuotes);
+		form.add(quotesContainer);
+		
+		//submit button
+		AjaxButton submitButton = new AjaxButton("submit") {
+			protected void onSubmit(AjaxRequestTarget target, Form form) {
+				//save() form, show message, then load display panel
+				if(save(form)) {
+					Component newPanel = new MyInterestsDisplay(id, userProfile);
+					newPanel.setOutputMarkupId(true);
+					thisPanel.replaceWith(newPanel);
+					if(target != null) {
+						target.addComponent(newPanel);
+					}
+				
+				} else {
+					String js = "alert('crap!');";
+					target.prependJavascript(js);
+				}
+            }
+		};
+		form.add(submitButton);
+		
+        
+		//cancel button
+		AjaxFallbackButton cancelButton = new AjaxFallbackButton("cancel", new ResourceModel("button.cancel"), form) {
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+            	//System.out.println("cancel clicked");
+            	Component newPanel = new MyInterestsDisplay(id, userProfile);
+				newPanel.setOutputMarkupId(true);
+				thisPanel.replaceWith(newPanel);
+				if(target != null) {
+					target.addComponent(newPanel);
+				}
+            	
+            }
+        };
+        cancelButton.setDefaultFormProcessing(false);
+        form.add(cancelButton);
+		
+        //feedback stuff - make this a class and insance it with diff params
+        //WebMarkupContainer formFeedback = new WebMarkupContainer("formFeedback");
+		//formFeedback.add(new Label("feedbackMsg", "some message"));
+		//formFeedback.add(new AjaxIndicator("feedbackImg"));
+		//form.add(formFeedback);
+        
+        
+		
+		//add form to page
+		add(form);
+		
+	}
+	
+	//called when the form is to be saved
+	private boolean save(Form form) {
+		
+       // System.out.println(getModelObject());
+
+		//get the backing model
+		UserProfile userProfile = (UserProfile) form.getModelObject();
+		
+		//get sakaiProxy, then get userId from sakaiProxy, then get sakaiperson for that userId
+		SakaiProxy sakaiProxy = ProfileApplication.get().getSakaiProxy();
+		
+		String userId = sakaiProxy.getCurrentUserId();
+		SakaiPerson sakaiPerson = sakaiProxy.getSakaiPerson(userId);
+	
+		//set the attributes from userProfile that this form dealt with, into sakaiPerson
+		//this WILL fail if there is no sakaiPerson for the user however this should have been caught already
+		//as a new Sakaiperson for a user is created in MyProfile if they don't have one.
+		
+		sakaiPerson.setFavouriteBooks(userProfile.getFavouriteBooks());
+
+		if(sakaiProxy.updateSakaiPerson(sakaiPerson)) {
+			log.info("Saved SakaiPerson for: " + userId );
+			return true;
+		} else {
+			log.info("Couldn't save SakaiPerson for: " + userId);
+			return false;
+		}
+	}
+
+	
+}
