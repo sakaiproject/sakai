8dfc84b20985364a7aa7cb4b382c50c60d0c8df0
Aaron Zeckoski <azeckoski@vt.edu>
2009-01-06 14:18:53 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-13898 Static code review fixes, constructor
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/trunk@55771 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- entitybroker/mocks/src/java/org/sakaiproject/entitybroker/mocks/data/MyEntity.java
+++ entitybroker/mocks/src/java/org/sakaiproject/entitybroker/mocks/data/MyEntity.java
@@ -13,102 +13,105 @@ package org.sakaiproject.entitybroker.mocks.data;
  */
 public class MyEntity {
 
-   private String id;
-   private String stuff;
-   private int number;
-   public String extra;
+    private String id;
+    private String stuff;
+    private int number;
+    public String extra;
 
-   public MyEntity() {
-   }
+    /**
+     * Basic empty constructor
+     */
+    public MyEntity() {
+    }
 
-   public MyEntity(String id, String stuff) {
-      this.id = id;
-      this.stuff = stuff;
-   }
-   
-   public MyEntity(String stuff, int number) {
-      this.stuff = stuff;
-      this.number = number;
-   }
+    public MyEntity(String id, String stuff) {
+        this.id = id;
+        this.stuff = stuff;
+    }
 
-   public MyEntity(String id, String stuff, int number) {
-      this.id = id;
-      this.stuff = stuff;
-      this.number = number;
-   }
+    public MyEntity(String stuff, int number) {
+        this.stuff = stuff;
+        this.number = number;
+    }
 
-   @Override
-   public boolean equals(Object obj) {
-      if (null == obj) {
-         return false;
-      }
-      if (!(obj instanceof MyEntity)) {
-         return false;
-      } else {
-         MyEntity castObj = (MyEntity) obj;
-         if (null == this.id || null == castObj.id) {
+    public MyEntity(String id, String stuff, int number) {
+        this.id = id;
+        this.stuff = stuff;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (null == obj) {
             return false;
-         } else {
-            return (this.id.equals(castObj.id));
-         }
-      }
-   }
+        }
+        if (!(obj instanceof MyEntity)) {
+            return false;
+        } else {
+            MyEntity castObj = (MyEntity) obj;
+            if (null == this.id || null == castObj.id) {
+                return false;
+            } else {
+                return (this.id.equals(castObj.id));
+            }
+        }
+    }
 
-   @Override
-   public int hashCode() {
-      if (null == this.id) {
-         return super.hashCode();
-      }
-      String hashStr = this.getClass().getName() + ":" + this.id.hashCode();
-      return hashStr.hashCode();
-   }
+    @Override
+    public int hashCode() {
+        if (null == this.id) {
+            return super.hashCode();
+        }
+        String hashStr = this.getClass().getName() + ":" + this.id.hashCode();
+        return hashStr.hashCode();
+    }
 
-   @Override
-   public String toString() {
-      return "id:" + this.id + ", stuff:" + this.stuff + ", number:" + number;
-   }
+    @Override
+    public String toString() {
+        return "id:" + this.id + ", stuff:" + this.stuff + ", number:" + number;
+    }
 
-   public String getId() {
-      return id;
-   }
+    public String getId() {
+        return id;
+    }
 
-   public void setId(String id) {
-      this.id = id;
-   }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-   public String getStuff() {
-      return stuff;
-   }
+    public String getStuff() {
+        return stuff;
+    }
 
-   public void setStuff(String stuff) {
-      this.stuff = stuff;
-   }
+    public void setStuff(String stuff) {
+        this.stuff = stuff;
+    }
 
-   public int getNumber() {
-      return number;
-   }
+    public int getNumber() {
+        return number;
+    }
 
-   public void setNumber(int number) {
-      this.number = number;
-   }
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
-   /**
-    * @return a copy of this object
-    */
-   public MyEntity copy() {
-      return copy(this);
-   }
+    /**
+     * @return a copy of this object
+     */
+    public MyEntity copy() {
+        return copy(this);
+    }
 
-   /**
-    * @return a copy of the supplied object
-    */
-   public static MyEntity copy(MyEntity me) {
-      if (me == null) {
-         throw new IllegalArgumentException("entity to copy must not be null");
-      }
-      MyEntity togo = new MyEntity(me.id, me.stuff, me.number);
-      togo.extra = me.extra;
-      return togo;
-   }
+    /**
+     * @return a copy of the supplied object
+     */
+    public static MyEntity copy(MyEntity me) {
+        if (me == null) {
+            throw new IllegalArgumentException("entity to copy must not be null");
+        }
+        MyEntity togo = new MyEntity(me.id, me.stuff, me.number);
+        togo.extra = me.extra;
+        return togo;
+    }
 
 }
