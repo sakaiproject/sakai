6784a0f276ba21e44b77ce0cbb200b8778d551ad
Lance Speelmon <lance@rsmart.com>
2009-01-09 20:18:25 +0000

    wkldfjkwjf
    git-svn-id: https://source.sakaiproject.org/svn/memory/trunk@55994 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- memory/memory-tool/tool/src/java/org/sakaiproject/memory/tool/MemoryAction.java
+++ memory/memory-tool/tool/src/java/org/sakaiproject/memory/tool/MemoryAction.java
@@ -21,11 +21,6 @@
 
 package org.sakaiproject.memory.tool;
 
-import org.sakaiproject.authz.cover.SecurityService;
-import org.sakaiproject.cheftool.Context;
-import org.sakaiproject.cheftool.JetspeedRunData;
-import org.sakaiproject.cheftool.RunData;
-import org.sakaiproject.cheftool.VelocityPortlet;
 import org.sakaiproject.cheftool.VelocityPortletPaneledAction;
 import org.sakaiproject.cheftool.api.Menu;
 import org.sakaiproject.event.api.SessionState;
