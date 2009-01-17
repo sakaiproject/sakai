18dc16f2e35d2a81aaba1309894adfe07d55d848
maarten <maarten@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-01-17 15:39:10 +0000

    SCO-32, SCO-48, SCO-52: Fixed issue with the content iframe of the player being too small by introducing some javascript code that resizes the iframe on window load and on window resize.

---
+++ scorm-tool/src/webapp/scripts/resize.js
@@ -0,0 +1,49 @@
+function initResizing() {
+	resizeWindow();
+}
+
+function resizeWindow() {
+	var windowHeight = getInnerHeight();
+	var headerHeight = document.getElementById("scormButtonPanel").offsetHeight;
+	var footerHeight = document.getElementById("scormFooter").offsetHeight;
+	var mainHeight = windowHeight - (headerHeight + footerHeight) -4;
+	
+	var windowWidth = getInnerWidth();
+	var navWidth = document.getElementById("scormNavPanel").offsetWidth;
+	var mainWidth = windowWidth - navWidth;
+
+	document.getElementById("scormNavPanel").style.height = mainHeight+"px";
+	document.getElementById("scormContentPanel").style.height = mainHeight+"px";
+	document.getElementById("scormContent").style.height = mainHeight+"px";
+	document.getElementById("scormContent").style.width = mainWidth + "px";
+}
+
+function getInnerHeight() {
+	var innerHeight = 0;
+	if( typeof( window.innerHeight ) == 'number' ) {
+		//Non-IE
+		innerHeight = window.innerHeight;
+	} else if( document.documentElement && document.documentElement.clientHeight ) {
+		//IE 6+ in 'standards compliant mode'
+		innerHeight = document.documentElement.clientHeight;
+	} else if( document.body && document.body.clientHeight ) {
+		//IE 4 compatible
+		innerHeight = document.body.clientHeight;
+	}
+	return innerHeight;
+}
+
+function getInnerWidth() {
+	var innerWidth = 0;
+	if( typeof( window.innerWidth ) == 'number' ) {
+		//Non-IE
+		innerWidth = window.innerWidth;
+	} else if( document.documentElement && document.documentElement.clientWidth ) {
+		//IE 6+ in 'standards compliant mode'
+		innerWidth = document.documentElement.clientWidth;
+	} else if( document.body && document.body.clientWidth ) {
+		//IE 4 compatible
+		innerWidth = document.body.clientWidth;
+	}
+	return innerWidth;
+}
