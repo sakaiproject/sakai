3995e54a8036ea6861fbf5c6989073ac5a6dd117
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-03-03 17:40:25 +0000

    PRFL-4 PRFL-24 changed db conversion scripts to take into account the privacy changes
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70294 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/docs/database/mysql/profile2-conversion-1.0.1-1.1-mysql.sql
+++ profile2/docs/database/mysql/profile2-conversion-1.0.1-1.1-mysql.sql
@@ -1,8 +1,27 @@
 --change Preferences to split out email into different fields that can be controlled individually.
---will default to true for all users.
 
+--add the new columns, default to false as we update them further down.
 alter table PROFILE_PREFERENCES_T 
-	drop COLUMN EMAIL,
-	add EMAIL_REQUEST bit not null DEFAULT true,
-    add EMAIL_CONFIRM bit not null DEFAULT true
+	add EMAIL_REQUEST bit not null DEFAULT false,
+    add EMAIL_CONFIRM bit not null DEFAULT false
 ;
+
+--update the new columns based on the old data
+--if all emails, then both true
+update PROFILE_PREFERENCES_T set EMAIL_REQUEST=true, EMAIL_CONFIRM=true where EMAIL=0;
+--if just requests, set requests to true, confirms to false
+update PROFILE_PREFERENCES_T set EMAIL_REQUEST=true, EMAIL_CONFIRM=false where EMAIL=1;
+--if just confirms, set confirms to true, requests to false
+update PROFILE_PREFERENCES_T set EMAIL_REQUEST=false, EMAIL_CONFIRM=true where EMAIL=2;
+--if all off, set both false
+update PROFILE_PREFERENCES_T set EMAIL_REQUEST=false, EMAIL_CONFIRM=false where EMAIL=3;
+
+--now drop the old column
+alter table PROFILE_PREFERENCES_T 
+	drop COLUMN EMAIL
+;
+
+
+
+--change name of profile field to be profile_image since it only controls that now (PRFL-24)
+alter table PROFILE_PRIVACY_T change PROFILE PROFILE_IMAGE int not null;
