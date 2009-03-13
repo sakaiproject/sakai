a9620ff5906b8d432331ea1425e8707abf65d8fd
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-03-13 16:58:53 +0000

    PRFL-30
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70318 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/models/SimpleText.java
@@ -0,0 +1,52 @@
+package uk.ac.lancs.e_science.profile2.tool.models;
+
+import java.io.Serializable;
+
+/**
+ * Simple model to back a simple single text field.
+ * To be used ONLY by the tool.
+ * 
+ * TODO make this model replace the Search model
+ * 
+ * @author Steve Swinsburg (s.swinsburg@lancaster.ac.uk)
+ */
+public class SimpleText implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String text;
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getText() {
+		return text;
+	}
+	
+	
+	
+	
+	/* for the form feedback, to get around a bug in Wicket where it needs a backing model */
+	private String textFeedback;
+	
+	public String getTextFeedback() {
+		return textFeedback;
+	}
+	public void setTextFeedback(String textFeedback) {
+		this.textFeedback = textFeedback;
+	}
+	
+
+	
+	/**
+	 * Default constructor
+	 */
+	public SimpleText() {
+	
+	}
+	
+	
+
+
+
+
+}
