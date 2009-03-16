68c641341db2704ef9f1c1c665b02c8aedc755be
Megan May <meganmay@gmail.com>
2009-03-16 20:43:11 +0000

    adding sak reference to conversion script for sql related to SAK-15040
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@58672 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_6_0_oracle_conversion.sql
@@ -361,7 +361,7 @@ drop table PERMISSIONS_SRC_TEMP;
 
     create sequence hibernate_sequence;
 
------ site.viewRoster is a newly added permission
+----- SAK-15040 site.viewRoster is a newly added permission
 
 INSERT INTO SAKAI_REALM_FUNCTION VALUES (SAKAI_REALM_FUNCTION_SEQ.NEXTVAL, 'site.viewRoster');
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.user'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'site.viewRoster'));
