4879df3359bcfc1c3ee275dac3770f2296abe937
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-03-18 17:32:04 +0000

    PRFL-3
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70343 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/panels/FriendsFeed.java
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/pages/panels/FriendsFeed.java
@@ -23,6 +23,7 @@ import uk.ac.lancs.e_science.profile2.api.SakaiProxy;
 import uk.ac.lancs.e_science.profile2.api.exception.ProfileBadConfigurationException;
 import uk.ac.lancs.e_science.profile2.tool.ProfileApplication;
 import uk.ac.lancs.e_science.profile2.tool.components.ExternalImage;
+import uk.ac.lancs.e_science.profile2.tool.components.ProfileImageRenderer;
 import uk.ac.lancs.e_science.profile2.tool.dataproviders.FriendsFeedDataProvider;
 import uk.ac.lancs.e_science.profile2.tool.pages.MyFriends;
 import uk.ac.lancs.e_science.profile2.tool.pages.MyProfile;
@@ -123,48 +124,12 @@ public class FriendsFeed extends Panel {
 				//is profile image allowed to be viewed by this user/friend?
 				final boolean isProfileImageAllowed = profile.isUserXProfileImageVisibleByUserY(friendId, viewingUserId, friend);
 				
-		    	if(isProfileImageAllowed) {
-		    		
-		    		//what type of picture should we use?
-		    		int profilePictureType = sakaiProxy.getProfilePictureType();
-		    		
-		    		if(profilePictureType == ProfileImageManager.PICTURE_SETTING_UPLOAD) {
-			    		//upload
-
-		    			imageBytes = profile.getCurrentProfileImageForUser(friendId, ProfileImageManager.PROFILE_IMAGE_THUMBNAIL);
-		    			
-		    			//add uplaoded image or default
-						if(imageBytes != null && imageBytes.length > 0){
-							BufferedDynamicImageResource photoResource = new BufferedDynamicImageResource(){
-								private static final long serialVersionUID = 1L;
-								protected byte[] getImageData() {
-									return imageBytes;
-								}
-							};
-							friendItem.add(new Image("friendPhoto",photoResource));
-						} else {
-							friendItem.add(new ContextImage("friendPhoto",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
-						}
-						
-		    		} else if (profilePictureType == ProfileImageManager.PICTURE_SETTING_URL) {
-		    			//url
-		    			
-						String externalUrl = profile.getExternalImageUrl(friendId, ProfileImageManager.PROFILE_IMAGE_MAIN, true);
-						
-						//add uploaded iamge or default
-						if(externalUrl != null) {
-							friendItem.add(new ExternalImage("friendPhoto",externalUrl));
-						} else {
-							friendItem.add(new ContextImage("friendPhoto",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
-						}
-		    		} else {
-		    			//invalid type, show default
-		    			friendItem.add(new ContextImage("friendPhoto",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
-					}
-		    	} else {
-		    		//not allowed, show default.
+				/* IMAGE */
+				if(isProfileImageAllowed) {
+					friendItem.add(new ProfileImageRenderer("friendPhoto", friendId, ProfileImageManager.PROFILE_IMAGE_THUMBNAIL));
+				} else {
 					friendItem.add(new ContextImage("friendPhoto",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
-				} 
+				}
 				
 				
 				//name (will be linked also)
