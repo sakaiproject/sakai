a5f2a5ad03a922ca568db074697bcde8f60b740f
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-03-23 15:23:12 +0000

    NOJIRA - improved component to abstract more logic and handle all image processing requirements.
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70355 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/components/ProfileImageRenderer.java
+++ profile2/tool/src/java/uk/ac/lancs/e_science/profile2/tool/components/ProfileImageRenderer.java
@@ -13,24 +13,30 @@ import uk.ac.lancs.e_science.profile2.api.SakaiProxy;
 import uk.ac.lancs.e_science.profile2.tool.ProfileApplication;
 
 /** 
- * This is a helper panel for displaying a user's profile image in a div.
- * Friend status and if the image is allowed to be viewed by a person should already be determined at this stage.
- * @author swinsbur
+ * This is a helper panel for displaying a user's profile image.
+ * @author Steve Swinsburg (s.swinsburg@lancaster.ac.uk)
  *
  */
 public class ProfileImageRenderer extends Panel {
 	
 	private static final long serialVersionUID = 1L;
-
+	
 	/**
 	 * Render a profile image for a user, based on the settings supplied
 	 * @param id		- wicket:id
 	 * @param userX		- user whose image we are showing
+	 * @param allowed	- if this image is allowed to be viewed by the person requesting it. If false, a default image will be used.
 	 * @param size		- ProfileImageManager.PROFILE_IMAGE_MAIN, ProfileImageManager.PROFILE_IMAGE_THUMBNAIL
 	 * @param cacheable	- if this image is allowed to be cached or not. If having issues with sticky images from AJAX updates, set this to false to ensure the image is updated every request
 	 */
-	public ProfileImageRenderer(String id, String userX, int size, boolean cacheable) {
+	public ProfileImageRenderer(String id, String userX, boolean allowed, int size, boolean cacheable) {
 		super(id);
+		
+		//if we aren't allowed to view it, no use processing, just show default
+		if(!allowed) {
+			add(new ContextImage("img",new Model(getDefaultImage())));
+			return;
+		}
 		
 		//get API's
         SakaiProxy sakaiProxy = ProfileApplication.get().getSakaiProxy();
@@ -58,26 +64,36 @@ public class ProfileImageRenderer extends Panel {
 					add(new NonCachingImage("img",photoResource));
 				}
 			} else {
-				add(new ContextImage("img",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
+				add(new ContextImage("img",new Model(getDefaultImage())));
 			}
 		
-		//EXTERNAL URL
+		//EXTERNAL IMAGE
 		} else if (type == ProfileImageManager.PICTURE_SETTING_URL) {
 			
 			String url = profile.getExternalImageUrl(userX, ProfileImageManager.PROFILE_IMAGE_MAIN, true);
 			
-			//add uploaded iamge or default
+			//add uploaded image or default
 			if(url != null) {
 				add(new ExternalImage("img",url));
 			} else {
-				add(new ContextImage("img",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
+				add(new ContextImage("img",new Model(getDefaultImage())));
 			}
 			
 		//INVALID TYPE, SHOW DEFAULT
 		} else {
-			add(new ContextImage("img",new Model(ProfileImageManager.UNAVAILABLE_IMAGE)));
+			add(new ContextImage("img",new Model(getDefaultImage())));
 		}
 		
 	}
-
+	
+	
+	/**
+	 * Get the default image to be used if not allowed or none available.
+	 * @return
+	 */
+	private String getDefaultImage() {
+		return ProfileImageManager.UNAVAILABLE_IMAGE;
+	}
+	
+	
 }
