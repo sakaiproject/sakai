99f02c3e798f796a464ce45ab1cd1624471d2b82
Anthony Whyte <arwhyte@umich.edu>
2009-04-01 00:02:04 +0000

    SAK-12938 update copyright statement to ECL 2.0
    git-svn-id: https://source.sakaiproject.org/svn/rwiki/trunk@59388 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- rwiki/rwiki-tool/tool/src/webapp/scripts/autosave.js
+++ rwiki/rwiki-tool/tool/src/webapp/scripts/autosave.js
@@ -1,4 +1,3 @@
-
 /**********************************************************************************
  * $URL$
  * $Id$
@@ -6,11 +5,12 @@
  *
  * Copyright (c) 2003, 2004, 2005, 2006 The Sakai Foundation.
  *
- * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.opensource.org/licenses/ecl1.php
+ *      http://www.osedu.org/licenses/ECL-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,7 @@
  * limitations under the License.
  *
  **********************************************************************************/
-
+ 
 var WikiAutoSave_autoSaveObjects = new Array();
 var WikiAutoSave__autoSaveObjectID = 0;
 
