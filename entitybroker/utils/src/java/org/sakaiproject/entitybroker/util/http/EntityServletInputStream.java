70a687dfc24b217a12d2aed4b41a404c935dc238
Anthony Whyte <arwhyte@umich.edu>
2009-04-01 20:14:10 +0000

    http://bugs.sakaiproject.org/jira/browse/SAK-12938 SAK-12938 update copyright statement to ECL 2.0
    git-svn-id: https://source.sakaiproject.org/svn/entitybroker/trunk@59496 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- entitybroker/utils/src/java/org/sakaiproject/entitybroker/util/http/EntityServletInputStream.java
+++ entitybroker/utils/src/java/org/sakaiproject/entitybroker/util/http/EntityServletInputStream.java
@@ -2,15 +2,21 @@
  * $Id$
  * $URL$
  * BufferedServletInputStream.java - entity-broker - Dec 24, 2008 11:14:58 AM - azeckoski
- **************************************************************************
- * Copyright (c) 2008 Aaron Zeckoski
- * Licensed under the Apache License, Version 2.0
- * 
- * A copy of the Apache License has been included in this 
- * distribution and is available at: http://www.apache.org/licenses/LICENSE-2.0.txt
+ ***********************************************************************************
+ * Copyright (c) 2008 Sakai Foundation
  *
- * Aaron Zeckoski (azeckoski @ gmail.com) (aaronz @ vt.edu) (aaron @ caret.cam.ac.uk)
- */
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************************/
 
 package org.sakaiproject.entitybroker.util.http;
 
