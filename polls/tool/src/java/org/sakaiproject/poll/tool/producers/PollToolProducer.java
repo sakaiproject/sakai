f27c0b9f9599406133c150fcae811dd079ce6c9b
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-04-03 10:03:38 +0000

    SAK-15802. Fixed date sorting by adding a parser and using date db string value for sorting key
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@59618 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- polls/tool/src/java/org/sakaiproject/poll/tool/producers/PollToolProducer.java
+++ polls/tool/src/java/org/sakaiproject/poll/tool/producers/PollToolProducer.java
@@ -68,6 +68,7 @@ import uk.org.ponder.stringutil.StringList;
 import uk.org.ponder.rsf.components.UIInternalLink;
 import uk.org.ponder.rsf.components.decorators.DecoratorList;
 import uk.org.ponder.rsf.components.decorators.UILabelTargetDecorator;
+import uk.org.ponder.rsf.components.decorators.UIStyleDecorator;
 import uk.org.ponder.rsf.components.decorators.UITooltipDecorator;
 //import uk.org.ponder.rsf.viewstate.EntityCentredViewParameters;
 import uk.org.ponder.beanutil.entity.EntityID;
@@ -263,12 +264,14 @@ DefaultView,NavigationCaseReporter {
 			}
 
 			if (poll.getVoteOpen()!=null)
-				UIOutput.make(pollrow,"poll-open-date",df.format(poll.getVoteOpen()));
+				UIOutput.make(pollrow,"poll-open-date",df.format(poll.getVoteOpen()))
+					.decorators = new DecoratorList(new UIStyleDecorator("realDate:"+poll.getVoteOpen().toString()));
 			else 
 				UIVerbatim.make(pollrow,"poll-open-date","  ");
 
 			if (poll.getVoteClose()!=null)
-				UIOutput.make(pollrow,"poll-close-date",df.format(poll.getVoteClose()));
+				UIOutput.make(pollrow,"poll-close-date",df.format(poll.getVoteClose()))
+					.decorators = new DecoratorList(new UIStyleDecorator("realDate:"+poll.getVoteClose().toString()));
 			else 
 				UIVerbatim.make(pollrow,"poll-close-date","  ");
 
---
+++ polls/tool/src/webapp/js/polls.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+// add a special date parser through the tablesorter addParser method
+    $.tablesorter.addParser({         
+        // set a unique id
+        id: 'realDate',
+        is: function(s) {
+             return false; //so this parser is not auto detected
+        },
+        format: function(s) {
+            // format the date data for normalization
+            return $.tablesorter.formatFloat(new Date(s.replace(/-/g,' ').replace(':00.0','')).getTime());
+        },
+        // set type, either numeric or text
+        type: 'numeric'
+    });
+
+    //A filter to extract the real date and pass that to the sorter
+    var dateExtractor = function(_that) {
+       var that = $(_that);
+        if(that.attr('class') && (that.attr('class').search(/realDate:/) != -1)){
+            return that.attr('class').replace(/realDate:/,'');
+        }
+       return that.text();
+    }
+    $("#sortableTable").tablesorter({
+        sortList: [[2,1]], // Initial order by the latest (NOT earliest) closing date
+        headers: { // disable it by setting the property sorter to false
+            1: {sorter: 'realDate'},
+            2: {sorter: 'realDate'},
+            3: {sorter: false},
+            4: {sorter: false}
+        },
+        textExtraction: dateExtractor
+    });
+});
