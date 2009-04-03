5efe9228c67a00bd345b6f6f2ec50921a3a9b5e5
Anthony Whyte <arwhyte@umich.edu>
2009-04-03 23:33:27 +0000

    SAK-15589 update copyright years
    git-svn-id: https://source.sakaiproject.org/svn/sakai-mock/trunk@59684 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sakai-mock/src/main/java/org/sakaiproject/mock/domain/Member.java
+++ sakai-mock/src/main/java/org/sakaiproject/mock/domain/Member.java
@@ -3,7 +3,7 @@
  * $Id$
  ***********************************************************************************
  *
- * Copyright (c) 2007 Sakai Foundation
+ * Copyright (c) 2007, 2008 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
