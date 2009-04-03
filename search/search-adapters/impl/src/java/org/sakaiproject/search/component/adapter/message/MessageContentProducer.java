3d2b3317bcf5af7a26f8e7542c2ca75c311dd13f
Anthony Whyte <arwhyte@umich.edu>
2009-04-03 23:36:24 +0000

    SAK-15589 update copyright years
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@59685 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-adapters/impl/src/java/org/sakaiproject/search/component/adapter/message/MessageContentProducer.java
+++ search/search-adapters/impl/src/java/org/sakaiproject/search/component/adapter/message/MessageContentProducer.java
@@ -3,7 +3,7 @@
  * $Id: SessionRequestHolder.java 14690 2006-09-15 11:43:18Z ian@caret.cam.ac.uk $
  ***********************************************************************************
  *
- * Copyright (c) 2006, 2007, 2008 Sakai Foundation
+ * Copyright (c) 2006, 2007, 2008 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
