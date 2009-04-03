6a4fadc55f48fc3d6fb81283aaa18c21e4b17f33
David Horwitz <dhorwitz@gmail.com>
2009-04-03 12:07:21 +0000

    SAK-15989 missing files
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@59626 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ polls/impl/src/test/org/sakaiproject/poll/logic/test/PollListManagerTest.java
@@ -0,0 +1,85 @@
+package org.sakaiproject.poll.logic.test;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.poll.dao.PollDao;
+import org.sakaiproject.poll.logic.test.TestDataPreload;
+import org.sakaiproject.poll.logic.test.stubs.EventTrackingServiceStub;
+import org.sakaiproject.poll.model.Poll;
+import org.sakaiproject.poll.service.impl.PollListManagerImpl;
+import org.springframework.test.AbstractTransactionalSpringContextTests;
+
+public class PollListManagerTest extends AbstractTransactionalSpringContextTests {
+
+	private static Log log = LogFactory.getLog(PollListManagerTest.class);	
+	
+	private TestDataPreload tdp = new TestDataPreload();
+
+	private PollListManagerImpl pollListManager;
+	
+	protected String[] getConfigLocations() {
+		// point to the needed spring config files, must be on the classpath
+		// (add component/src/webapp/WEB-INF to the build path in Eclipse),
+		// they also need to be referenced in the project.xml file
+		return new String[] { "hibernate-test.xml", "spring-hibernate.xml" };
+	}
+
+	// run this before each test starts
+	protected void onSetUpBeforeTransaction() throws Exception {
+	}
+	
+	// run this before each test starts and as part of the transaction
+	protected void onSetUpInTransaction() {
+		PollDao dao = (PollDao) applicationContext.getBean("org.sakaiproject.poll.dao.impl.PollDaoTarget");
+		if (dao == null) {
+			log.error("onSetUpInTransaction: DAO could not be retrieved from spring context");
+			
+		}
+		
+		pollListManager = new PollListManagerImpl();
+		pollListManager.setDao(dao);
+		pollListManager.setEventTrackingService(new EventTrackingServiceStub());
+		
+		
+		// preload testData
+		tdp.preloadTestData(dao);
+	}
+	
+    public void testGetPollById() {
+    	//we shouldNot find this poll
+    	Poll pollFail = pollListManager.getPollById(Long.valueOf(99));
+    	assertNull(pollFail);
+    	
+    	//this one should exist
+    	Poll poll1 = pollListManager.getPollById(Long.valueOf(1));
+    	assertNotNull(poll1);
+    }
+	
+    
+    public void testSavePoll() {
+		
+    	Poll poll1 = new Poll();
+		poll1.setCreationDate(new Date());
+		poll1.setVoteOpen(new Date());
+		poll1.setVoteClose(new Date());
+		poll1.setDescription("this is some text");
+		poll1.setText("something");
+		poll1.setOwner(TestDataPreload.USER_UPDATE);
+		poll1.setSiteId(TestDataPreload.LOCATION1_ID);
+		
+		//If this has a value something is wrong without POJO
+		assertNull(poll1.getPollId());
+		
+		pollListManager.savePoll(poll1);
+		
+		//if this is null we have a problem
+		assertNotNull(poll1.getPollId());
+		
+		Poll poll2 = pollListManager.getPollById(poll1.getPollId());
+		assertNotNull(poll2);
+		assertEquals(poll1.getPollText(), poll2.getPollText());
+    }
+	
+}
---
+++ polls/impl/src/test/org/sakaiproject/poll/logic/test/stubs/EventTrackingServiceStub.java
@@ -0,0 +1,85 @@
+package org.sakaiproject.poll.logic.test.stubs;
+
+import java.util.Observer;
+
+import org.sakaiproject.event.api.Event;
+import org.sakaiproject.event.api.EventDelayHandler;
+import org.sakaiproject.event.api.EventTrackingService;
+import org.sakaiproject.event.api.UsageSession;
+import org.sakaiproject.time.api.Time;
+import org.sakaiproject.user.api.User;
+
+public class EventTrackingServiceStub implements EventTrackingService {
+
+	public void addLocalObserver(Observer arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addObserver(Observer arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addPriorityObserver(Observer arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void cancelDelays(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void cancelDelays(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void delay(Event arg0, Time arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteObserver(Observer arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Event newEvent(String arg0, String arg1, boolean arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Event newEvent(String arg0, String arg1, boolean arg2, int arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Event newEvent(String arg0, String arg1, String arg2, boolean arg3,
+			int arg4) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void post(Event arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void post(Event arg0, UsageSession arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void post(Event arg0, User arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setEventDelayHandler(EventDelayHandler arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
