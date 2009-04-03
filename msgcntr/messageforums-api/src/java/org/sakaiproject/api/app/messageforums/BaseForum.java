314e17627f4d4728193e741f28ca64d9770fb6c7
Anthony Whyte <arwhyte@umich.edu>
2009-04-03 23:19:29 +0000

    SAK-15589 update copyright years
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@59677 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/BaseForum.java
+++ msgcntr/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/BaseForum.java
@@ -3,7 +3,7 @@
  * $Id: BaseForum.java 9227 2006-05-15 15:02:42Z cwen@iupui.edu $
  ***********************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006, 2007 Sakai Foundation
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/jsf/BarLinkComponent.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/jsf/BarLinkComponent.java
@@ -3,7 +3,7 @@
  * $Id: BarLinkComponent.java 9227 2006-05-15 15:02:42Z cwen@iupui.edu $
  ***********************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006 Sakai Foundation
+ * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
