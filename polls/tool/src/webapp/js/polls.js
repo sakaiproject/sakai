8939536c0a3b6750e62a4e8ed3250bb8f016ed0a
Lovemore Nalube <lovemore.nalube@uct.ac.za>
2009-04-09 12:07:23 +0000

    SAK-15802. Format real date into simple numbers and sort that using the 'digit' parser.
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@59897 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- polls/tool/src/webapp/js/polls.js
+++ polls/tool/src/webapp/js/polls.js
@@ -1,32 +1,17 @@
 $(document).ready(function() {
-// add a special date parser through the tablesorter addParser method
-    $.tablesorter.addParser({         
-        // set a unique id
-        id: 'realDate',
-        is: function(s) {
-             return false; //so this parser is not auto detected
-        },
-        format: function(s) {
-            // format the date data for normalization
-            return $.tablesorter.formatFloat(new Date(s.replace(/-/g,' ').replace(':00.0','')).getTime());
-        },
-        // set type, either numeric or text
-        type: 'numeric'
-    });
-
-    //A filter to extract the real date and pass that to the sorter
+    //A filter to extract the real date formatted as a simple number and pass that to the sorter
     var dateExtractor = function(_that) {
        var that = $(_that);
         if(that.attr('name') && (that.attr('name').search(/realDate:/) != -1)){
-            return that.attr('name').replace(/realDate:/,'');
+            return that.attr('name').replace(/realDate:/,'').replace(/-/g,' ').replace(':00.0','').replace(/:/g,'').replace(/ /g,'');
         }
        return that.text();
     }
     $("#sortableTable").tablesorter({
         sortList: [[2,1]], // Initial order by the latest (NOT earliest) closing date
         headers: { // disable it by setting the property sorter to false
-            1: {sorter: 'realDate'},
-            2: {sorter: 'realDate'},
+            1: {sorter: 'digit'},
+            2: {sorter: 'digit'},
             3: {sorter: false},
             4: {sorter: false}
         },
