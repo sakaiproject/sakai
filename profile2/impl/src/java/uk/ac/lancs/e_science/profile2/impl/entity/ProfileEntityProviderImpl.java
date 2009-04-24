94d939c8a0875d8cdbd3c33141e7361044c9e859
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-04-24 16:34:34 +0000

    ProfileEntityProvider returns profile images.
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70414 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/impl/src/java/uk/ac/lancs/e_science/profile2/impl/entity/ProfileEntityProviderImpl.java
+++ profile2/impl/src/java/uk/ac/lancs/e_science/profile2/impl/entity/ProfileEntityProviderImpl.java
@@ -1,12 +1,18 @@
 package uk.ac.lancs.e_science.profile2.impl.entity;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 
 import org.sakaiproject.entitybroker.DeveloperHelperService;
 import org.sakaiproject.entitybroker.EntityReference;
+import org.sakaiproject.entitybroker.EntityView;
 import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomAction;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.RESTful;
+import org.sakaiproject.entitybroker.entityprovider.extension.ActionReturn;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.entityprovider.search.Search;
 import org.sakaiproject.tool.api.SessionManager;
@@ -20,7 +26,7 @@ public class ProfileEntityProviderImpl implements ProfileEntityProvider, CoreEnt
 	public String getEntityPrefix() {
 		return ENTITY_PREFIX;
 	}
-
+	
 	public boolean entityExists(String eid) {
 		//check the user is valid. if it is then return true as everyone has a profile.
 		return profileService.checkUserProfileExists(eid);
@@ -36,12 +42,7 @@ public class ProfileEntityProviderImpl implements ProfileEntityProvider, CoreEnt
 		UserProfile userProfile = profileService.getPrototype();
 		return userProfile;
 	}
-
-	public void updateEntity(EntityReference ref, Object entity) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 	public Object getEntity(EntityReference ref) {
 	
 		if (sessionManager.getCurrentSessionUserId() == null) {
@@ -52,21 +53,38 @@ public class ProfileEntityProviderImpl implements ProfileEntityProvider, CoreEnt
 			return profileService.getPrototype();
 		}
 		
-		//no security yet. add another param on here for the person requesting the profile which will be used to check what they can see
-		//ie sessionManager.getCurrentSessionUserId()
+		//get the profile, with privacy checks against the requesting user
 		UserProfile entity = profileService.getUserProfile(ref.getId(), sessionManager.getCurrentSessionUserId());
 		return entity;
 	}
-
-	public void deleteEntity(EntityReference ref) {
-		// TODO Auto-generated method stub
+	
+	@EntityCustomAction(action="image",viewKey=EntityView.VIEW_SHOW)
+	public Object getImage(OutputStream out, EntityView view, EntityReference ref) {
 		
+		if (sessionManager.getCurrentSessionUserId() == null) {
+			throw new SecurityException();
+		}
+		
+		if (ref.getId() == null) {
+			return null;
+		}
+		
+		//get profile image. TODO add requesting userId and the type of image, or make this two functions one for main and one for thumbnail
+		byte[] b = profileService.getProfileImage(ref.getId());
+		
+		if(b == null) {
+			return null;
+		}
+		
+		try {
+			out.write(b);
+			return new ActionReturn(out);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-
-	public List<?> getEntities(EntityReference ref, Search search) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 	public String[] getHandledOutputFormats() {
 		return new String[] {Formats.XML, Formats.JSON};
@@ -75,8 +93,34 @@ public class ProfileEntityProviderImpl implements ProfileEntityProvider, CoreEnt
 	public String[] getHandledInputFormats() {
 		return new String[] {Formats.XML, Formats.JSON};
 	}
+		
 	
+	public String createEntity(EntityReference ref, Object entity, Map<String, Object> params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateEntity(EntityReference ref, Object entity) {
+		// TODO Auto-generated method stub
+	}
+
+	public void updateEntity(EntityReference ref, Object entity, Map<String, Object> params) {
+		// TODO Auto-generated method stub
+	}
+
+	public void deleteEntity(EntityReference ref, Map<String, Object> params) {
+		// TODO Auto-generated method stub
+	}
 	
+	public void deleteEntity(EntityReference ref) {
+		// TODO Auto-generated method stub
+	}
+
+	public List<?> getEntities(EntityReference ref, Search search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	
 	
 	
@@ -94,5 +138,8 @@ public class ProfileEntityProviderImpl implements ProfileEntityProvider, CoreEnt
 	public void setProfileService(ProfileService profileService) {
 		this.profileService = profileService;
 	}
+
+
+	
 
 }
