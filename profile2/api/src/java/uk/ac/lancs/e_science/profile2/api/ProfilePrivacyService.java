086c38fd9743135a583d001932ef5778922f9b41
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-05-21 17:12:34 +0000

    PRFL-53 profileprivacy service
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70508 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/api/src/java/uk/ac/lancs/e_science/profile2/api/ProfilePrivacyService.java
+++ profile2/api/src/java/uk/ac/lancs/e_science/profile2/api/ProfilePrivacyService.java
@@ -1,5 +1,7 @@
 package uk.ac.lancs.e_science.profile2.api;
 
+import uk.ac.lancs.e_science.profile2.api.model.ProfilePrivacy;
+
 
 /**
  * <p>This is the outward facing service that should be used by anyone or anything implementing Profile2 privacy methods.</p>
@@ -9,5 +11,25 @@ package uk.ac.lancs.e_science.profile2.api;
  */
 public interface ProfilePrivacyService {
 	
+	/**
+	 * Create a blank ProfilePrivacy object.
+	 * <p>DO NOT USE THIS METHOD.</p>
+	 * @return
+	 */
+	public ProfilePrivacy getPrototype();
+		
+	/**
+	 * Get the ProfilePrivacy object for the given user.
+	 * @param userId
+	 * @return ProfilePrivacy record for the user, default if they haven't got one, or null if invalid user
+	 */
+	public ProfilePrivacy getProfilePrivacyRecord(String userId);
 	
+	public boolean save(ProfilePrivacy obj);
+	
+	public boolean create(ProfilePrivacy obj);
+	
+	public boolean create(String userId);
+	
+	public boolean checkUserExists(String userId);
 }
--- profile2/impl/src/java/uk/ac/lancs/e_science/profile2/impl/ProfilePrivacyServiceImpl.java
+++ profile2/impl/src/java/uk/ac/lancs/e_science/profile2/impl/ProfilePrivacyServiceImpl.java
@@ -5,6 +5,8 @@ import org.apache.log4j.Logger;
 import uk.ac.lancs.e_science.profile2.api.Profile;
 import uk.ac.lancs.e_science.profile2.api.ProfilePrivacyService;
 import uk.ac.lancs.e_science.profile2.api.SakaiProxy;
+import uk.ac.lancs.e_science.profile2.api.entity.model.UserProfile;
+import uk.ac.lancs.e_science.profile2.api.model.ProfilePrivacy;
 
 /**
  * <p>This is the implementation of {@link ProfilePrivacyService}; see that interface for usage details.
@@ -16,6 +18,65 @@ public class ProfilePrivacyServiceImpl implements ProfilePrivacyService {
 
 	private static final Logger log = Logger.getLogger(ProfilePrivacyServiceImpl.class);
 	
+	/**
+	 * {@inheritDoc}
+	 */
+	public ProfilePrivacy getPrototype() {
+		return new ProfilePrivacy();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public ProfilePrivacy getProfilePrivacyRecord(String userId) {
+		
+		//convert userId into uuid
+		String userUuid = sakaiProxy.getUuidForUserId(userId);
+		if(userUuid == null) {
+			log.error("Invalid userId: " + userId);
+			return null;
+		}
+		
+		//get record or default if none.
+		ProfilePrivacy privacy = profile.getPrivacyRecordForUser(userUuid);
+		if(privacy == null) {
+			return getPrototype(userUuid);
+		}
+		
+		return privacy;
+	}
+	
+	public boolean save(ProfilePrivacy obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public boolean create(ProfilePrivacy obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public boolean create(String userId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean checkUserExists(String userId) {
+		return sakaiProxy.checkForUser(sakaiProxy.getUuidForUserId(userId));
+	}
+	
+	/**
+	 * Create a default ProfilePrivacy object for the given user.
+	 * 
+	 * @param userId - either internal user id (6ec73d2a-b4d9-41d2-b049-24ea5da03fca) or eid (jsmith26)
+	 * @return a ProfilePrivacy object filled with the default fields
+	 */
+	private ProfilePrivacy getPrototype(String userId) {
+		String userUuid = sakaiProxy.getUuidForUserId(userId);
+		return profile.getDefaultPrivacyRecord(userUuid);
+	}
+	
 	
 	private SakaiProxy sakaiProxy;
 	public void setSakaiProxy(SakaiProxy sakaiProxy) {
@@ -26,6 +87,7 @@ public class ProfilePrivacyServiceImpl implements ProfilePrivacyService {
 	public void setProfile(Profile profile) {
 		this.profile = profile;
 	}
+	
 
 	
 	
