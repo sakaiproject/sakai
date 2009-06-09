ac5416c2503b732fadc3016a24b3643d499b5b15
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-06-09 11:08:02 +0000

    PRFL-57 move model
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70570 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/api/src/java/org/sakaiproject/profile2/model/Friend.java
@@ -0,0 +1,65 @@
+package org.sakaiproject.profile2.api.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Friend.java
+ * 
+ * This is a model for storing information about a friend of a user. 
+ * It has a limited number of fields and is only ever populated when a List of friends for a given user is required
+ * with each friend having one of these objects for fast access to the info required.
+ */
+
+
+public class Friend implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String userUuid;
+	private String displayName;
+	private String statusMessage;
+	private Date statusDate;
+	private boolean confirmed;
+	private Date requestedDate;
+
+	
+	public String getUserUuid() {
+		return userUuid;
+	}
+	public void setUserUuid(String userUuid) {
+		this.userUuid = userUuid;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	public Date getStatusDate() {
+		return statusDate;
+	}
+	public void setStatusDate(Date statusDate) {
+		this.statusDate = statusDate;
+	}
+	public boolean isConfirmed() {
+		return confirmed;
+	}
+	public void setConfirmed(boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+	
+	public void setRequestedDate(Date requestedDate) {
+		this.requestedDate = requestedDate;
+	}
+	public Date getRequestedDate() {
+		return requestedDate;
+	}
+
+}
---
+++ profile2/api/src/java/org/sakaiproject/profile2/model/ProfileStatus.java
@@ -0,0 +1,72 @@
+package org.sakaiproject.profile2.api.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import org.sakaiproject.entitybroker.entityprovider.annotations.EntityId;
+
+
+/**
+ * Hibernate and EntityProvider model
+ * 
+ * @author Steve Swinsburg (s.swinsburg@lancaster.ac.uk)
+ *
+ */
+public class ProfileStatus implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@EntityId
+	private String userUuid;
+	private String message;
+	private Date dateAdded;
+	//private int cleared; //maybe to hold value if the status has been cleared
+	
+	/**
+	 * Empty constructor
+	 */
+	public ProfileStatus(){
+	}
+	
+	/** 
+	 * Constructor to create a status object in one go
+	 */
+	public ProfileStatus(String userUuid, String message, Date dateAdded){
+		this.userUuid = userUuid;
+		this.message = message;
+		this.dateAdded = dateAdded;
+	}
+
+
+	public String getUserUuid() {
+		return userUuid;
+	}
+
+
+	public void setUserUuid(String userUuid) {
+		this.userUuid = userUuid;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+	
+	
+	
+}
