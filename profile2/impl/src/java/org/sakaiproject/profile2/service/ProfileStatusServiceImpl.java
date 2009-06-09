4d643f3181e75291a2eb96e3779cb1fb57b1bfed
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-06-09 16:11:56 +0000

    PRFL-57 move service
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70585 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/impl/src/java/org/sakaiproject/profile2/service/ProfileStatusServiceImpl.java
@@ -0,0 +1,174 @@
+package org.sakaiproject.profile2.impl;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.sakaiproject.profile2.api.Profile;
+import org.sakaiproject.profile2.api.ProfileConstants;
+import org.sakaiproject.profile2.api.ProfileStatusService;
+import org.sakaiproject.profile2.api.SakaiProxy;
+import org.sakaiproject.profile2.api.model.ProfilePrivacy;
+import org.sakaiproject.profile2.api.model.ProfileStatus;
+
+/**
+ * <p>This is the implementation of {@link ProfileStatusService}; see that interface for usage details.
+ * 
+ * @author Steve Swinsburg (s.swinsburg@lancaster.ac.uk)
+ *
+ */
+public class ProfileStatusServiceImpl implements ProfileStatusService {
+
+	private static final Logger log = Logger.getLogger(ProfileStatusServiceImpl.class);
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public ProfileStatus getPrototype() {
+		return new ProfileStatus();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public ProfileStatus getProfileStatusRecord(String userId) {
+		
+		//check auth and get currentUserUuid
+		String currentUserUuid = sakaiProxy.getCurrentUserId();
+		if(currentUserUuid == null) {
+			throw new SecurityException("Must be logged in.");
+		}
+		
+		//convert userId into uuid
+		String userUuid = sakaiProxy.getUuidForUserId(userId);
+		if(userUuid == null) {
+			log.error("Invalid userId: " + userId);
+			return null;
+		}
+		
+		//get record for the user, will be null if none
+		ProfileStatus status = profile.getUserStatus(userUuid);
+		if(status == null) {
+			return null;
+		}
+		
+		//get privacy on the status (if no privacy, return based on default)
+		ProfilePrivacy privacy = profile.getPrivacyRecordForUser(userUuid);
+		if (privacy == null) {
+			return (ProfileConstants.DEFAULT_MYSTATUS_VISIBILITY ? status : null);
+		}
+		
+		//check if friends
+		boolean friend = profile.isUserXFriendOfUserY(userUuid, currentUserUuid);
+		
+		//now check if allowed, return if ok, null if not
+		return (profile.isUserXStatusVisibleByUserY(userUuid, privacy, currentUserUuid, friend) ? status : null);
+		
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean save(ProfileStatus obj) {
+		
+		//check auth and get currentUserUuid
+		String currentUserUuid = sakaiProxy.getCurrentUserId();
+		if(currentUserUuid == null) {
+			throw new SecurityException("Must be logged in.");
+		}
+		
+		//check currentUser and object uuid match
+		if(!currentUserUuid.equals(obj.getUserUuid())) {
+			throw new SecurityException("Not allowed to save.");
+		}
+		
+		//reset the date to be now
+		obj.setDateAdded(new Date());
+		
+		//save and return response
+		return persistProfileStatus(obj);
+	}
+	
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean create(String userId, String message) {
+		
+		//check auth and get currentUserUuid
+		String currentUserUuid = sakaiProxy.getCurrentUserId();
+		if(currentUserUuid == null) {
+			throw new SecurityException("Must be logged in.");
+		}
+		
+		//convert userId into uuid
+		String userUuid = sakaiProxy.getUuidForUserId(userId);
+		if(userUuid == null) {
+			log.error("Invalid userId: " + userId);
+			return false;
+		}
+		
+		//check currentUser and object uuid match
+		if(!currentUserUuid.equals(userUuid)) {
+			throw new SecurityException("Not allowed to save.");
+		}
+		
+		//create status object for user with message and date for now
+		ProfileStatus status = new ProfileStatus(userUuid,message,new Date());
+
+		//save and return response
+		return persistProfileStatus(status);
+	}
+	
+	
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean checkProfileStatusExists(String userId) {
+		
+		//convert userId into uuid
+		String userUuid = sakaiProxy.getUuidForUserId(userId);
+		if(userUuid == null) {
+			log.error("Invalid userId: " + userId);
+			return false;
+		}
+		
+		//check if we have a persisted object already
+		if(profile.getUserStatus(userUuid) == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * Helper method to take care of persisting a ProfileStatus object to the database.
+	 * 
+	 * @param ProfileStatus object
+	 * @return true/false for success
+	 */
+	private boolean persistProfileStatus(ProfileStatus obj) {
+
+		if(profile.setUserStatus(obj)) {
+			return true;
+		} 
+		return false;
+	}
+	
+		
+	
+	private SakaiProxy sakaiProxy;
+	public void setSakaiProxy(SakaiProxy sakaiProxy) {
+		this.sakaiProxy = sakaiProxy;
+	}
+	
+	private Profile profile;
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+	
+
+	
+	
+
+}
