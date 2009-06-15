3ef6e6e04aab101154c993860e605f4ae009ae93
Steve Swinsburg <steve.swinsburg@gmail.com>
2009-06-15 11:25:58 +0000

    PRFL-81 paging via AJAX rather than normal requests
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@70613 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/panels/ConfirmedFriends.java
+++ profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/panels/ConfirmedFriends.java
@@ -7,12 +7,12 @@ import org.apache.log4j.Logger;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.navigation.paging.AjaxPagingNavigator;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
@@ -232,7 +232,7 @@ public class ConfirmedFriends extends Panel {
 		add(confirmedFriendsContainer);
 		
 		//add pager
-		PagingNavigator pager = new PagingNavigator("navigator", confirmedFriendsDataView);
+		AjaxPagingNavigator pager = new AjaxPagingNavigator("navigator", confirmedFriendsDataView);
 		add(pager);
 
 		//initially, if no friends, hide container
