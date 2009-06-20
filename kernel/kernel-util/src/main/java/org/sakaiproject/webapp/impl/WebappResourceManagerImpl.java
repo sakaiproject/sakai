4a34764a3fc940732316c8f955615ee17134efed
David Horwitz <dhorwitz@gmail.com>
2009-06-20 10:00:53 +0000

    Add missing copyright headers
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@63901 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-util/src/main/java/org/sakaiproject/webapp/impl/WebappResourceManagerImpl.java
+++ kernel/kernel-util/src/main/java/org/sakaiproject/webapp/impl/WebappResourceManagerImpl.java
@@ -1,4 +1,27 @@
+/**********************************************************************************
+ * $URL: $
+ * $Id:  $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.webapp.impl;
+
+import java.io.InputStream;
 
 import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.webapp.api.WebappResourceManager;
@@ -7,10 +30,6 @@ import org.springframework.beans.factory.support.BeanDefinitionValidationExcepti
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Created by IntelliJ IDEA.
