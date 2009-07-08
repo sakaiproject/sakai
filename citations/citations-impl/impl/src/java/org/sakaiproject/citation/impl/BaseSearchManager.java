ea38571760145117f53410a9108b376b7644c405
Jim Eng <jimeng@umich.edu>
2009-07-08 03:14:16 +0000

    SAK-16669 Remove session id from URL
    git-svn-id: https://source.sakaiproject.org/svn/citations/trunk@64617 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseSearchManager.java
+++ citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseSearchManager.java
@@ -1848,7 +1848,7 @@ public class BaseSearchManager implements SearchManager, Observer
 
 
 	// google scholar constants
-	public static final String SAKAI_SESSION = "sakai.session";
+	public static final String SAKAI_SESSION = "sakai.session.key";
 	public static final String SAKAI_KEY = "sakai.key";
 	public static final String SAKAI_HOST = "sakai.host";
 
@@ -2589,8 +2589,8 @@ public class BaseSearchManager implements SearchManager, Observer
     public String getGoogleScholarUrl(String resourceId)
     {
     	String serverUrl = serverConfigurationService.getServerUrl();
- 		SessionManager sessionManager = (SessionManager) ComponentManager.get("org.sakaiproject.tool.api.SessionManager");
-		String sessionId = sessionManager.getCurrentSession().getId();
+ 		//SessionManager sessionManager = (SessionManager) ComponentManager.get("org.sakaiproject.tool.api.SessionManager");
+		//String sessionId = sessionManager.getCurrentSession().getId();
 
 		try
 		{
@@ -2603,8 +2603,7 @@ public class BaseSearchManager implements SearchManager, Observer
 							resourceId +
 							"?" +
 							SAKAI_SESSION +
-							"=" +
-							sessionId + "&", "UTF-8" )
+							"=nada&", "UTF-8" )
 					+ "&linkurl_id="
 					+ java.net.URLEncoder.encode( m_configService.getSiteConfigSakaiServerKey(), "UTF-8" ) );
 		}
