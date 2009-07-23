88e4e1cd4688538f702fe897f417e858bcc0e1df
Anthony Whyte <arwhyte@umich.edu>
2009-07-23 19:15:17 +0000

    SAK-16751 comment out site.add.course backfill script.
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@65121 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_6_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_6_0_mysql_conversion.sql
@@ -425,37 +425,43 @@ INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where RE
 -- backfill new site.add.course permissions into existing realms
 -- --------------------------------------------------------------------------------------------------------------------------------------
 
--- for each realm that has a role matching something in this table, we will add to that role the function from this table
-CREATE TABLE PERMISSIONS_SRC_TEMP (ROLE_NAME VARCHAR(99), FUNCTION_NAME VARCHAR(99));
+-- SAK-16751 Backfilling for site.add.course has been commented out as it updates all realms with .auth role unnecessarily (site realms only require backfilling)
+-- It is a rather promiscuous conversion that may not square with an institution's permission strategy.  If you
+-- uncomment it and run it you will add site.add.course to realms like /announcement/channel/!site/motd,
+-- /site/!urlError and !user.template.guest. None of these realms have include site.add so they cannot create sites.
+-- So consider your options carefully before uncommenting and running this backfill operation.
 
-INSERT INTO PERMISSIONS_SRC_TEMP values ('.auth','site.add.course');
+-- for each realm that has a role matching something in this table, we will add to that role the function from this table
+-- CREATE TABLE PERMISSIONS_SRC_TEMP (ROLE_NAME VARCHAR(99), FUNCTION_NAME VARCHAR(99));
+
+-- INSERT INTO PERMISSIONS_SRC_TEMP values ('.auth','site.add.course');
 
 -- lookup the role and function numbers
-CREATE TABLE PERMISSIONS_TEMP (ROLE_KEY INTEGER, FUNCTION_KEY INTEGER);
-INSERT INTO PERMISSIONS_TEMP (ROLE_KEY, FUNCTION_KEY)
-SELECT SRR.ROLE_KEY, SRF.FUNCTION_KEY
-from PERMISSIONS_SRC_TEMP TMPSRC
-JOIN SAKAI_REALM_ROLE SRR ON (TMPSRC.ROLE_NAME = SRR.ROLE_NAME)
-JOIN SAKAI_REALM_FUNCTION SRF ON (TMPSRC.FUNCTION_NAME = SRF.FUNCTION_NAME);
+-- CREATE TABLE PERMISSIONS_TEMP (ROLE_KEY INTEGER, FUNCTION_KEY INTEGER);
+-- INSERT INTO PERMISSIONS_TEMP (ROLE_KEY, FUNCTION_KEY)
+-- SELECT SRR.ROLE_KEY, SRF.FUNCTION_KEY
+-- from PERMISSIONS_SRC_TEMP TMPSRC
+-- JOIN SAKAI_REALM_ROLE SRR ON (TMPSRC.ROLE_NAME = SRR.ROLE_NAME)
+-- JOIN SAKAI_REALM_FUNCTION SRF ON (TMPSRC.FUNCTION_NAME = SRF.FUNCTION_NAME);
 
 -- insert the new functions into the roles of any existing realm that has the role (don't convert the "!site.helper")
-INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
-SELECT
-    SRRFD.REALM_KEY, SRRFD.ROLE_KEY, TMP.FUNCTION_KEY
-FROM
-    (SELECT DISTINCT SRRF.REALM_KEY, SRRF.ROLE_KEY FROM SAKAI_REALM_RL_FN SRRF) SRRFD
-    JOIN PERMISSIONS_TEMP TMP ON (SRRFD.ROLE_KEY = TMP.ROLE_KEY)
-    JOIN SAKAI_REALM SR ON (SRRFD.REALM_KEY = SR.REALM_KEY)
-    WHERE SR.REALM_ID != '!site.helper'
-    AND NOT EXISTS (
-        SELECT 1
-            FROM SAKAI_REALM_RL_FN SRRFI
-            WHERE SRRFI.REALM_KEY=SRRFD.REALM_KEY AND SRRFI.ROLE_KEY=SRRFD.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
-    );
+-- INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+-- SELECT
+--    SRRFD.REALM_KEY, SRRFD.ROLE_KEY, TMP.FUNCTION_KEY
+-- FROM
+--    (SELECT DISTINCT SRRF.REALM_KEY, SRRF.ROLE_KEY FROM SAKAI_REALM_RL_FN SRRF) SRRFD
+--    JOIN PERMISSIONS_TEMP TMP ON (SRRFD.ROLE_KEY = TMP.ROLE_KEY)
+--    JOIN SAKAI_REALM SR ON (SRRFD.REALM_KEY = SR.REALM_KEY)
+--    WHERE SR.REALM_ID != '!site.helper'
+--    AND NOT EXISTS (
+--        SELECT 1
+--            FROM SAKAI_REALM_RL_FN SRRFI
+--            WHERE SRRFI.REALM_KEY=SRRFD.REALM_KEY AND SRRFI.ROLE_KEY=SRRFD.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
+--    );
 
 -- clean up the temp tables
-DROP TABLE PERMISSIONS_TEMP;
-DROP TABLE PERMISSIONS_SRC_TEMP;
+-- DROP TABLE PERMISSIONS_TEMP;
+-- DROP TABLE PERMISSIONS_SRC_TEMP;
 
 --
 -- Citations SAK-14517 Support more RIS types for import/export
