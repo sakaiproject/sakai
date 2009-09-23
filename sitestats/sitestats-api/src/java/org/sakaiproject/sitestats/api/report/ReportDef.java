0808a9ad97a90425bc9e1b63f773e5c2f99eb483
Anthony Whyte <arwhyte@umich.edu>
2009-09-23 00:48:53 +0000

    STAT-183 add/update copyright header to Sakai ECL 2.0
    git-svn-id: https://source.sakaiproject.org/svn/sitestats/ufp/sitestats/trunk@72172 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sitestats/sitestats-api/src/java/org/sakaiproject/sitestats/api/report/ReportDef.java
+++ sitestats/sitestats-api/src/java/org/sakaiproject/sitestats/api/report/ReportDef.java
@@ -1,21 +1,21 @@
-/**********************************************************************************
+/**
+ * $URL:$
+ * $Id:$
  *
- * Copyright (c) 2006 Universidade Fernando Pessoa
+ * Copyright (c) 2006-2009 The Sakai Foundation
  *
- * Licensed under the Educational Community License Version 1.0 (the "License");
- * By obtaining, using and/or copying this Original Work, you agree that you have read,
- * understand, and will comply with the terms and conditions of the Educational Community License.
- * You may obtain a copy of the License at:
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://cvs.sakaiproject.org/licenses/license_1_0.html
+ *             http://www.osedu.org/licenses/ECL-2.0
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
- * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- **********************************************************************************/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sakaiproject.sitestats.api.report;
 
 import java.io.Serializable;
