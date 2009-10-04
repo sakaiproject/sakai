d6062adb9d6c0f5897d19dceaa8df9cd82cb7f69
David Horwitz <dhorwitz@gmail.com>
2009-10-04 10:06:33 +0000

    SAK-17094 update for references to new pom locations
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/trunk@67141 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/service/api/src/java/org/sakaiproject/service/gradebook/shared/ConflictingCategoryNameException.java
+++
@@ -1,7 +0,0 @@
-package org.sakaiproject.service.gradebook.shared;
-
-public class ConflictingCategoryNameException extends GradebookException {
-	public ConflictingCategoryNameException(String message) {
-        super(message);
-    }
-}
--- gradebook/service/api/src/java/org/sakaiproject/service/gradebook/shared/MultipleAssignmentSavingException.java
+++
@@ -1,9 +0,0 @@
-package org.sakaiproject.service.gradebook.shared;
-
-public class MultipleAssignmentSavingException extends GradebookException 
-{
-	public MultipleAssignmentSavingException(String message) 
-	{
-		super(message);
-  }
-}
