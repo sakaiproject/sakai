15e6ac07f3b97896c7c3badcf7a125e0cf381b74
David Horwitz <dhorwitz@gmail.com>
2009-10-08 13:45:37 +0000

    SAK-16034 use commons to get digest rather than use our own code
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@67366 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/java/org/sakaiproject/search/util/DigestStorageUtil.java
+++ search/search-impl/impl/src/java/org/sakaiproject/search/util/DigestStorageUtil.java
@@ -1,48 +1,20 @@
 package org.sakaiproject.search.util;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class DigestStorageUtil {
+
 	private static final Log log = LogFactory.getLog(DigestStorageUtil.class);
 	
+
 	private static String getHashOfFile(String ref) {
-		MessageDigest md;
-		String ret = null;
-		try {
-			md = MessageDigest.getInstance("MD5");
-			md.update(ref.getBytes());
-			// convert the binary md5 hash into hex
-			StringBuilder md5 = new StringBuilder();
-			byte[] b_arr = md.digest();
-
-			for (int i = 0; i < b_arr.length; i++) {
-				// convert the high nibble
-				byte b = b_arr[i];
-				b >>>= 4;
-				b &= 0x0f; // this clears the top half of the byte
-				md5.append(Integer.toHexString(b));
-
-				// convert the low nibble
-				b = b_arr[i];
-				b &= 0x0F;
-				md5.append(Integer.toHexString(b));
-			}
-			ret = md5.toString();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		} 
-	
-		log.debug("got hash of reference of: " + ret);
-		return ret;
+		return DigestUtils.md5Hex(ref);
 	}
 	
 	public static String getPath(String reference) {
+		log.debug("getPath(" + reference);
 		String ret = "";
 		
 		reference = getHashOfFile(reference);
