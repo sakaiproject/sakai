95cc9b55ac3d16b73afd5b8a8df94dfe379ddc37
David Horwitz <dhorwitz@gmail.com>
2009-10-12 14:01:02 +0000

    NOJIRA add a stack on possible swallowed error
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@67659 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/java/org/sakaiproject/search/journal/impl/JournaledFSIndexStorage.java
+++ search/search-impl/impl/src/java/org/sakaiproject/search/journal/impl/JournaledFSIndexStorage.java
@@ -21,11 +21,7 @@
 
 package org.sakaiproject.search.journal.impl;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -903,6 +899,9 @@ public class JournaledFSIndexStorage implements JournaledIndex, IndexStorageProv
 		}
 		catch (IOException ex)
 		{
+			if (log.isDebugEnabled()) {
+				ex.printStackTrace();
+			}
 			seginfo.add("Failed to get Segment Info list " + ex.getClass().getName()
 					+ " " + ex.getMessage());
 
