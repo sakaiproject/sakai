5c6751194308b3976697830983dd7458a76ce71e
David Horwitz <dhorwitz@gmail.com>
2009-10-27 09:48:05 +0000

    KNL-308
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@68289 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/CollectionAccessFormatter.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/CollectionAccessFormatter.java
@@ -146,8 +146,9 @@ public class CollectionAccessFormatter
 				out.println("<html><head>");
 				out.println("<title>" + "Index of " + pl.getProperty(ResourceProperties.PROP_DISPLAY_NAME) + "</title>");
 				String webappRoot = ServerConfigurationService.getServerUrl();
+				String skinRepo = ServerConfigurationService.getString("skin.repo", "/library/skins");
 				out.println("<link href=\"" + webappRoot
-						+ "/library/skin/default/access.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen\" />");
+						+ skinRepo+ "/default/access.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen\" />");
 				out.println("<script src=\"" + webappRoot
 						+ "/library/js/jquery.js\" type=\"text/javascript\">");
 				out.println("</script>");
