da34d9ee3a1b359c3a7c88d129edeb2eed9a27dc
Zhen Qian <zqian@umich.edu>
2009-11-17 16:51:13 +0000

    SAK-17407
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@69061 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -3047,7 +3047,10 @@ public class SiteAction extends PagedResourceActionII {
 					try
 					{
 						Section s = cms.getSection(providerSectionId);
-						providerSectionListTitles.put(s.getEid(), s.getTitle()); 
+						if (s != null)
+						{
+							providerSectionListTitles.put(s.getEid(), s.getTitle()); 
+						}
 					}
 					catch (Exception e)
 					{
@@ -3497,7 +3500,10 @@ public class SiteAction extends PagedResourceActionII {
 				try
 				{
 					Section s = cms.getSection(sectionId);
-					sectionTitles.put(sectionId, s.getTitle());
+					if (s != null)
+					{
+						sectionTitles.put(sectionId, s.getTitle());
+					}
 				}
 				catch (Exception e)
 				{
@@ -3536,9 +3542,11 @@ public class SiteAction extends PagedResourceActionII {
 					
 					try
 					{
-					Section s = cms.getSection(courseEid);
-					if (s!=null)
-						soList.add(new SectionObject(s));
+						Section s = cms.getSection(courseEid);
+						if (s!=null)
+						{
+							soList.add(new SectionObject(s));
+						}
 					}
 					catch (Exception e)
 					{
@@ -4567,7 +4575,8 @@ public class SiteAction extends PagedResourceActionII {
 			// default title
 			String title = sectionFieldProvider.getSectionTitle(t.getEid(), (List) multiCourseInputs.get(0));
 			try {
-				title = cms.getSection(sectionEid).getTitle();
+				Section s = cms.getSection(sectionEid);
+				title = s != null?s.getTitle():title;
 			} catch (IdNotFoundException e) {
 				// cannot find section, use the default title 
 				M_log.warn(this + ":readCourseSectionInfo: cannot find section with eid=" + sectionEid);
@@ -11115,9 +11124,12 @@ public class SiteAction extends PagedResourceActionII {
 			if (selections != null && selections.size() == lvlSz) {
 				Section sect = cms.getSection((String) selections.get(selections
 						.size() - 1));
-				SectionObject so = new SectionObject(sect);
-		
-				state.setAttribute(STATE_CM_SELECTED_SECTION, so);
+				if (sect != null)
+				{
+					SectionObject so = new SectionObject(sect);
+			
+					state.setAttribute(STATE_CM_SELECTED_SECTION, so);
+				}
 			} else
 				state.removeAttribute(STATE_CM_SELECTED_SECTION);
 		
@@ -11384,9 +11396,12 @@ public class SiteAction extends PagedResourceActionII {
 			for (int i = 0; i < sectionList.size(); i++) {
 				String sectionEid = (String) sectionList.get(i);
 				Section s = cms.getSection(sectionEid);
-				SectionObject so = new SectionObject(s);
-				so.setAuthorizer(userId);
-				list.add(so);
+				if (s != null)
+				{
+					SectionObject so = new SectionObject(s);
+					so.setAuthorizer(userId);
+					list.add(so);
+				}
 			}
 		}
 		return list;
