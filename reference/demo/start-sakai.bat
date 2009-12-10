b42b202498d9cd0d759bb7d6f7fdf1e64d891f6d
Anthony Whyte <arwhyte@umich.edu>
2009-12-10 23:01:12 +0000

    SAK-17425 add strict quote escaping = false setting.
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@69707 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/demo/start-sakai.bat
+++ reference/demo/start-sakai.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set JAVA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true
-set CATALINA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true
+set JAVA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false
+set CATALINA_OPTS=-server -Xmx1024m -XX:MaxNewSize=256m -XX:MaxPermSize=256m -Dsakai.demo=true -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false
 
 set CATALINA_HOME=
 set CATALINA_BASE=
