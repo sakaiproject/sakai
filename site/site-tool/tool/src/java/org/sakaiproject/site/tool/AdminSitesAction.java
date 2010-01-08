b0bd1b4d28e8e498678c1b39acc1527c2cf196cb
Savitha Prakash <savithap@umich.edu>
2010-01-08 15:52:15 +0000

    SAK-17652
    git-svn-id: https://source.sakaiproject.org/svn/site/trunk@71048 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site/site-tool/tool/src/java/org/sakaiproject/site/tool/AdminSitesAction.java
+++ site/site-tool/tool/src/java/org/sakaiproject/site/tool/AdminSitesAction.java
@@ -567,7 +567,7 @@ public class AdminSitesAction extends PagedResourceActionII
 		// build the menu
 		// we need the form fields for the remove...
 		Menu bar = new MenuImpl();
-		if (SiteService.allowRemoveSite(site.getId()))
+		if (site!= null && SiteService.allowRemoveSite(site.getId()))
 		{
 			bar.add(new MenuEntry(rb.getString("sitact.remsit"), null, true, MenuItem.CHECKED_NA, "doRemove", "site-form"));
 		}
