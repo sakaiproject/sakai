b1aaae4345500819c36b0ed1e0008fd998635ad2
Steve Swinsburg <steve.swinsburg@gmail.com>
2010-01-12 01:41:00 +0000

    PRFL-169
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@71382 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/MyProfile.java
+++ profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/MyProfile.java
@@ -6,9 +6,11 @@ import java.io.ObjectInputStream;
 
 import org.apache.log4j.Logger;
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxLazyLoadPanel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.EmptyPanel;
@@ -303,11 +305,16 @@ public class MyProfile extends BasePage {
 		//my quick links panel
 		
 		
-		//friends feed panel for self
-		Panel friendsFeed = new FriendsFeed("friendsFeed", userUuid, userUuid);
-		friendsFeed.setOutputMarkupId(true);
-		add(friendsFeed);
+		//friends feed panel for self - lazy loaded
+		add(new AjaxLazyLoadPanel("friendsFeed") {
 
+            @Override
+            public Component getLazyLoadComponent(String id) {
+            	return new FriendsFeed(id, userUuid, userUuid);
+            }
+
+        });
+		
 	}
 	
 	
