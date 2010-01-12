34866e16dfb3ef930ea42560639922c80a5c3fa0
Steve Swinsburg <steve.swinsburg@gmail.com>
2010-01-12 23:23:41 +0000

    PRFL-171
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@71397 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/docs/database/mysql/profile2-conversion-1.3-1.4-mysql.sql
+++ profile2/docs/database/mysql/profile2-conversion-1.3-1.4-mysql.sql
@@ -1,6 +1,11 @@
-/* add message table and indexes */
+/* add message tables and indexes */
+/* TODO */
 
+/* add gallery table and indexes */
+/* TODO */
 
 /* add the new email message preference column, default to 0, (PRFL-152) */
 alter table PROFILE_PREFERENCES_T add EMAIL_PRIVATE_MESSAGE bit not null DEFAULT false;
 
+/* add the new gallery column (PRFL-171)*/
+alter table PROFILE_PRIVACY_T add MY_PICTURES int not null DEFAULT 0;
--- profile2/docs/database/oracle/profile2-conversion-1.3-1.4-oracle.sql
+++ profile2/docs/database/oracle/profile2-conversion-1.3-1.4-oracle.sql
@@ -1,5 +1,12 @@
-/* add message table and indexes */
+/* add message tables and indexes */
+/* TODO */
+
+/* add gallery table and indexes */
+/* TODO */
 
 
 /* add the new email message preference column, default to 0, (PRFL-152) */
 alter table PROFILE_PREFERENCES_T add (EMAIL_PRIVATE_MESSAGE number(1,0) default 0;
+
+/* add the new gallery column (PRFL-171)*/
+alter table PROFILE_PRIVACY_T add MY_PICTURES number(1,0) default 0;
