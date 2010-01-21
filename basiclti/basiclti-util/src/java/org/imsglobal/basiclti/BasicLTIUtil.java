46ee7ddba36025f4af96e1780c2a72f46756e89f
Charles Severance <csev@umich.edu>
2010-01-21 17:20:49 +0000

    BLTI-17
    Move Sakai 2 specific code from util to impl so the artfact
    can be used in Sakai 3 as well as Sakai 2.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@71696 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-util/src/java/org/imsglobal/basiclti/BasicLTIUtil.java
+++ basiclti/basiclti-util/src/java/org/imsglobal/basiclti/BasicLTIUtil.java
@@ -2,7 +2,7 @@
  * $URL$
  * $Id$
  *
- * Copyright (c) 2008 IMS GLobal Learning Consortium
+ * Copyright (c) 2008 IMS Global Learning Consortium
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
