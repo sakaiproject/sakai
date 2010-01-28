4c627721124d88eac6d326ed8b98ee446dbbdc9e
Bryan Holladay <holladay@longsight.com>
2010-01-28 16:52:34 +0000

    SAK-17841 Put Msgcntr conversion script in 2.6.x to 2.7 conversion script
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@72948 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_7_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_7_0_oracle_conversion.sql
@@ -498,3 +498,148 @@ create index SAKAI_PERSON_META_USER_UUID_I on SAKAI_PERSON_META_T (USER_UUID);
 create index SAKAI_PERSON_META_PROPERTY_I on SAKAI_PERSON_META_T (PROPERTY);
 -- END Profile2 1.3 (SAK-17773)
 
+
+
+--///////////////////////////////////
+--///////////////////////////////////
+--///////////////////////////////////
+--// MSGCNTR-2.7
+--///////////////////////////////////
+--///////////////////////////////////
+--///////////////////////////////////
+
+
+
+--////////////////////////////////////////////////////
+--// SAK-11740
+--// Email notification of new posts to forum
+--////////////////////////////////////////////////////
+
+--You may need to run these drop commands if this table still exists in your db
+--DROP TABLE MFR_EMAIL_NOTIFICATION_TIF EXISTS ;
+--drop sequence MFR_EMAIL_NOTIFICATION_S;
+
+CREATE TABLE  "MFR_EMAIL_NOTIFICATION_T"
+   (    "ID" NUMBER(19,0) NOT NULL ENABLE,
+        "VERSION" NUMBER(10,0) NOT NULL ENABLE,
+        "USER_ID" VARCHAR2(255 BYTE) NOT NULL ENABLE,
+        "CONTEXT_ID" VARCHAR2(255 BYTE) NOT NULL ENABLE,
+        "NOTIFICATION_LEVEL" NUMBER(1,0) NOT NULL ENABLE,
+         PRIMARY KEY ("ID")
+  
+   )  ;
+CREATE INDEX "MFR_EMAIL_USER_ID_I" ON  "MFR_EMAIL_NOTIFICATION_T" ("USER_ID")  ;
+
+CREATE INDEX  "MFR_EMAIL_CONTEXT_ID_I" ON  "MFR_EMAIL_NOTIFICATION_T" ("CONTEXT_ID") ;
+
+
+
+create sequence MFR_EMAIL_NOTIFICATION_S;
+
+
+--////////////////////////////////////////////////////
+--// SAK-15052
+--// update cafe versions to 2.7.0-SNAPSHOT
+--////////////////////////////////////////////////////
+
+alter table MFR_MESSAGE_T add THREADID NUMBER(20);
+alter table MFR_MESSAGE_T add LASTTHREADATE TIMESTAMP;
+alter table MFR_MESSAGE_T add LASTTHREAPOST NUMBER(20);
+
+update MFR_MESSAGE_T set THREADID=IN_REPLY_TO,LASTTHREADATE=CREATED;
+
+--////////////////////////////////////////////////////
+--// SAK-10869
+--// Displaying all messages should mark them as read
+--////////////////////////////////////////////////////
+
+
+-- Add AutoMarkThreadsRead functionality to Message Center (SAK-10869)
+
+-- add column to allow AutoMarkThreadsRead as template setting
+alter table MFR_AREA_T add (AUTO_MARK_THREADS_READ NUMBER(1,0));
+update MFR_AREA_T set AUTO_MARK_THREADS_READ=0 where AUTO_MARK_THREADS_READ is NULL;
+alter table MFR_AREA_T modify (AUTO_MARK_THREADS_READ NUMBER(1,0) not null);
+
+-- add column to allow AutoMarkThreadsRead to be set at the forum level
+alter table MFR_OPEN_FORUM_T add (AUTO_MARK_THREADS_READ NUMBER(1,0));
+update MFR_OPEN_FORUM_T set AUTO_MARK_THREADS_READ=0 where AUTO_MARK_THREADS_READ is NULL;
+alter table MFR_OPEN_FORUM_T modify (AUTO_MARK_THREADS_READ NUMBER(1,0) not null);
+
+-- add column to allow AutoMarkThreadsRead to be set at the topic level
+alter table MFR_TOPIC_T add (AUTO_MARK_THREADS_READ NUMBER(1,0));
+update MFR_TOPIC_T set AUTO_MARK_THREADS_READ=0 where AUTO_MARK_THREADS_READ is NULL;
+alter table MFR_TOPIC_T modify (AUTO_MARK_THREADS_READ NUMBER(1,0) not null);
+
+
+
+--////////////////////////////////////////////////////
+--// SAK-10559
+--// View who has read a message
+--////////////////////////////////////////////////////
+
+--if MFR_MESSAGE_T is missing NUM_READERS, run alter and update commands
+--alter table MFR_MESSAGE_T add NUM_READERS int;
+--update MFR_MESSAGE_T set NUM_READERS = 0;
+
+
+--////////////////////////////////////////////////////
+--// SAK-15655
+--// Rework MyWorkspace Synoptic view of Messages & Forums
+--////////////////////////////////////////////////////
+
+create table MFR_SYNOPTIC_ITEM
+(SYNOPTIC_ITEM_ID number(19,0) not null,
+VERSION number(10,0) not null,
+USER_ID varchar2(99 char) not null,
+SITE_ID varchar2(99 char) not null,
+SITE_TITLE varchar2(255 char),
+NEW_MESSAGES_COUNT number(10,0),
+MESSAGES_LAST_VISIT_DT timestamp,
+NEW_FORUM_COUNT number(10,0),
+FORUM_LAST_VISIT_DT timestamp,
+HIDE_ITEM NUMBER(1,0),
+primary key (SYNOPTIC_ITEM_ID),
+unique (USER_ID, SITE_ID));
+
+create index MRF_SYN_USER on MFR_SYNOPTIC_ITEM (USER_ID);
+
+
+
+--////////////////////////////////////////////////////
+--// MSGCNTR-177
+--// MyWorkspace/Home does now show the Messages & Forums Notifications by default
+--////////////////////////////////////////////////////
+
+update SAKAI_SITE_TOOL
+Set TITLE = 'Unread Messages and Forums'
+Where REGISTRATION = 'sakai.synoptic.messagecenter'; 
+
+INSERT INTO SAKAI_SITE_TOOL VALUES('!user-145', '!user-100', '!user', 'sakai.synoptic.messagecenter', 2, 'Unread Messages and Forums', '1,1' );
+
+create table MSGCNTR_TMP(
+    PAGE_ID VARCHAR2(99),
+    SITE_ID VARCHAR2(99)
+);
+
+insert into MSGCNTR_TMP
+(   
+    Select PAGE_ID, SITE_ID 
+    from SAKAI_SITE_PAGE 
+    where SITE_ID like '~%' 
+    and TITLE = 'Home'
+    and PAGE_ID not in (Select PAGE_ID from SAKAI_SITE_TOOL where REGISTRATION = 'sakai.synoptic.messagecenter')
+);
+
+insert into SAKAI_SITE_TOOL
+(select SYS_GUID(), PAGE_ID, SITE_ID, 'sakai.synoptic.messagecenter', 2, 'Unread Messages and Forums', '1,1' from MSGCNTR_TMP);
+
+drop table MSGCNTR_TMP;
+
+--///////////////////////////////////
+--///////////////////////////////////
+--///////////////////////////////////
+--//  END MSGCNTR 2.7
+--///////////////////////////////////
+--///////////////////////////////////
+--///////////////////////////////////
