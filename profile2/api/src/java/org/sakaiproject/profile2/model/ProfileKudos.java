fee7326674ca1451d65de6285c46b576063e6a03
Steve Swinsburg <steve.swinsburg@gmail.com>
2010-02-02 12:33:15 +0000

    PRFL-178 hibernate mapping, model and wiring as well as notes for db upgrade
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@73068 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/api/src/java/org/sakaiproject/profile2/model/ProfileKudos.java
@@ -0,0 +1,50 @@
+package org.sakaiproject.profile2.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Model for a kudos score for a user - persistent
+ * 
+ * @author Steve Swinsburg (steve.swinsburg@gmail.com)
+ *
+ */
+public class ProfileKudos implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String userUuid;
+	private BigDecimal kudos;
+	private Date dateAdded;
+	
+	/** 
+	 * Empty constructor
+	 */
+	public ProfileKudos() {		
+	}
+
+	public String getUserUuid() {
+		return userUuid;
+	}
+
+	public void setUserUuid(String userUuid) {
+		this.userUuid = userUuid;
+	}
+
+	public BigDecimal getKudos() {
+		return kudos;
+	}
+
+	public void setKudos(BigDecimal kudos) {
+		this.kudos = kudos;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+	
+}
--- profile2/docs/database/mysql/profile2-conversion-1.3-1.4-mysql.sql
+++ profile2/docs/database/mysql/profile2-conversion-1.3-1.4-mysql.sql
@@ -7,6 +7,9 @@
 /* add official image table and indexes */
 /* TODO */
 
+/* add kudos table */
+/* TODO */
+
 /* add the new email message preference columns, default to 0, (PRFL-152, PRFL-186) */
 alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_NEW bit not null DEFAULT false;
 alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_REPLY bit not null DEFAULT false;
