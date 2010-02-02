da732b33d6678c7aa7ff1407b6a808867dba5f8b
David Horwitz <dhorwitz@gmail.com>
2010-02-02 09:10:26 +0000

    SAK-17799
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@73066 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
@@ -856,7 +856,7 @@ public class FilePickerAction extends PagedResourceHelperAction
     protected void disableSecurityAdvisors()
     {
     	// remove all security advisors
-    	SecurityService.clearAdvisors();
+    	SecurityService.popAdvisor();
     }
 
     /**
