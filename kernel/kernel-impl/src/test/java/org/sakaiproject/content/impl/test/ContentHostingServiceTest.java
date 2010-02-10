0cc8d78f538365c2baee58708aba1077a5a89929
David Horwitz <dhorwitz@gmail.com>
2010-02-10 14:05:44 +0000

    KNL-413 cover creating folders
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@73395 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/test/java/org/sakaiproject/content/impl/test/ContentHostingServiceTest.java
+++ kernel/kernel-impl/src/test/java/org/sakaiproject/content/impl/test/ContentHostingServiceTest.java
@@ -1,7 +1,6 @@
 package org.sakaiproject.content.impl.test;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import junit.extensions.TestSetup;
@@ -10,6 +9,8 @@ import junit.framework.TestSuite;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.content.api.ContentCollection;
+import org.sakaiproject.content.api.ContentCollectionEdit;
 import org.sakaiproject.content.api.ContentHostingService;
 import org.sakaiproject.content.api.ContentResource;
 import org.sakaiproject.content.api.ContentResourceEdit;
@@ -17,9 +18,7 @@ import org.sakaiproject.exception.IdInvalidException;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.IdUsedException;
 import org.sakaiproject.exception.InconsistentException;
-import org.sakaiproject.exception.OverQuotaException;
 import org.sakaiproject.exception.PermissionException;
-import org.sakaiproject.exception.ServerOverloadException;
 import org.sakaiproject.exception.TypeException;
 import org.sakaiproject.test.SakaiKernelTestBase;
 import org.sakaiproject.tool.api.Session;
@@ -27,6 +26,7 @@ import org.sakaiproject.tool.api.SessionManager;
 
 public class ContentHostingServiceTest extends SakaiKernelTestBase {
 
+	private static final String SIMPLE_FOLDER1 = "/admin/folder1/";
 	private static final Log log = LogFactory.getLog(ContentHostingServiceTest.class);
 	
 	
@@ -83,6 +83,87 @@ public class ContentHostingServiceTest extends SakaiKernelTestBase {
 		assertEquals(0, resource.getContentLength());
 		assertEquals(0, resource.getContent().length);
 		
+		
+	}
+	
+	
+	public void testSaveRetriveFolder() {
+		ContentHostingService ch = org.sakaiproject.content.cover.ContentHostingService.getInstance();
+		
+		try {
+			ContentCollectionEdit ce = ch.addCollection(SIMPLE_FOLDER1);
+			ch.commitCollection(ce);
+			log.info("commited folder:" + ce.getId());
+		} catch (IdUsedException e) {
+			e.printStackTrace();
+			fail("Got an id Used exception!");
+		} catch (IdInvalidException e) {
+			e.printStackTrace();
+			fail("That id is invalid!");
+		} catch (PermissionException e) {
+			e.printStackTrace();
+			fail();
+		} catch (InconsistentException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+		
+		//now try retrieve the folder
+		try {
+			ContentCollection cc = ch.getCollection(SIMPLE_FOLDER1);
+			assertNotNull(cc);
+		} catch (IdUnusedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		} catch (TypeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		} catch (PermissionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+		//lets test saving a utf8
+		String utf8Folder = String.valueOf("\u6c92\u6709\u5df2\u9078\u8981\u522a\u9664\u7684\u9644\u4ef6");
+		String utfId = "/admin/" + utf8Folder + "/";
+		try {
+			ContentCollectionEdit cce = ch.addCollection(utfId);
+			ch.commitCollection(cce);
+			log.info("commited folder:" + cce.getId());
+		} catch (IdUsedException e) {
+			e.printStackTrace();
+		} catch (IdInvalidException e) {
+			e.printStackTrace();
+			fail();
+		} catch (PermissionException e) {
+			e.printStackTrace();
+			fail();
+		} catch (InconsistentException e) {
+			e.printStackTrace();
+			fail();
+		}
+		
+		//now try retrieve the folder
+		try {
+			ContentCollection cc = ch.getCollection(utfId);
+			assertNotNull(cc);
+		} catch (IdUnusedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		} catch (TypeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		} catch (PermissionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
 		
 	}
 }
