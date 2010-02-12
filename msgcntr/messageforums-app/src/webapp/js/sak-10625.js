c6ff2b91dbc0a4fbac1ed92f3378dff0b2136256
Bryan Holladay <holladay@longsight.com>
2010-02-12 18:37:08 +0000

    MSGCNTR-107 Javascript error in the Forums tool
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@73482 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-app/src/webapp/js/sak-10625.js
+++ msgcntr/messageforums-app/src/webapp/js/sak-10625.js
@@ -2,7 +2,11 @@
 sak10625_disabler = function(){
    $("a, input[@type=button], input[@type=submit]").attr("onclick", "");
 }
+if (typeof window.jQuery != "undefined") {
+
+
 $(document).ready(function(){
 	$("a").filter(function(){regexp=/submit\(\)/; return regexp.test($(this).attr("onclick"));}).bind("click", sak10625_disabler);
    $("input[@type=button], input[@type=submit]").bind("click", sak10625_disabler);
 });
+    }
