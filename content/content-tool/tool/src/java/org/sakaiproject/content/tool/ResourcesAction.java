a7036e3387f41ac62b5bbf494a56e94d0e3e81d7
David Horwitz <dhorwitz@gmail.com>
2010-03-30 10:32:21 +0000

    NOJIRA unessasary use of cover instead of API to get fields
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@75420 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/ResourcesAction.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/ResourcesAction.java
@@ -89,7 +89,7 @@ import org.sakaiproject.entity.api.ResourcePropertiesEdit;
 import org.sakaiproject.entity.cover.EntityManager;
 import org.sakaiproject.event.api.SessionState;
 import org.sakaiproject.event.api.UsageSession;
-import org.sakaiproject.event.cover.NotificationService;
+import org.sakaiproject.event.api.NotificationService;
 import org.sakaiproject.event.cover.UsageSessionService;
 import org.sakaiproject.exception.IdInvalidException;
 import org.sakaiproject.exception.IdLengthException;
