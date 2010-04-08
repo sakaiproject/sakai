103ec7fb8f2dc840aaf2110b09a7b52ad6bf1908
Steve Swinsburg <steve.swinsburg@gmail.com>
2010-04-08 13:56:07 +0000

    PRFL-281 Add support for SiteStats reporting for certain events
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@75856 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/impl/src/java/org/sakaiproject/profile2/entity/ProfileEventsEntityProvider.java
@@ -0,0 +1,55 @@
+package org.sakaiproject.profile2.entity;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Statisticable;
+import org.sakaiproject.profile2.util.ProfileConstants;
+
+/**
+ * This is the entity provider to interact with SiteStats 
+ * and provided anonymous reporting of Profile2 usage.
+ * 
+ * @author Steve Swinsburg (steve.swinsburg@gmail.com)
+ *
+ */
+public class ProfileEventsEntityProvider implements AutoRegisterEntityProvider, Statisticable, Describeable {
+
+    public final static String PREFIX = "profile-events";
+    public final static String TOOL_ID = "sakai.profile2";
+    
+    public final static String[] EVENT_KEYS = new String[] {
+    	ProfileConstants.EVENT_PROFILE_VIEW_OWN,
+		ProfileConstants.EVENT_PROFILE_VIEW_OTHER,
+		ProfileConstants.EVENT_FRIEND_REQUEST,
+		ProfileConstants.EVENT_FRIEND_CONFIRM,
+		ProfileConstants.EVENT_FRIENDS_VIEW_OWN,
+		ProfileConstants.EVENT_FRIENDS_VIEW_OTHER,
+		ProfileConstants.EVENT_SEARCH_BY_NAME,
+		ProfileConstants.EVENT_SEARCH_BY_INTEREST,
+		ProfileConstants.EVENT_STATUS_UPDATE,
+		ProfileConstants.EVENT_TWITTER_UPDATE,
+		ProfileConstants.EVENT_MESSAGE_SENT,
+	};
+
+   
+    
+	public String getEntityPrefix() {
+		return PREFIX;
+	}
+
+	public String getAssociatedToolId() {
+		return ProfileConstants.TOOL_ID;
+	}
+
+	public String[] getEventKeys() {
+		return EVENT_KEYS;
+	}
+
+	public Map<String, String> getEventNames(Locale locale) {
+		return null;
+	}
+
+}
