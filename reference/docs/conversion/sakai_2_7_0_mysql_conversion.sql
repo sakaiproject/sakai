1496406f630fe632309da669f4675dfcf409aeab
Michelle Wagner <wagnermr@iu.edu>
2010-05-03 12:52:00 +0000

    SAK-18453 Remove addition of gradebook2 tables from 2.7 conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@76981 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_7_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_7_0_mysql_conversion.sql
@@ -669,29 +669,7 @@ DROP COLUMN GRADEBOOK_ASSIGNMENT;
 --/////////////////////////////////////////////////
 
 
--- SAK-17847
-create table GB_ACTION_RECORD_T
-(
-	ID bigint NOT NULL AUTO_INCREMENT,
-	KEY(ID),
-	VERSION int,
-	GRADEBOOK_UID varchar(756),
-	GRADEBOOK_ID bigint,
-	ENTITY_TYPE varchar(200),
-	ACTION_TYPE varchar(200),
-	ENTITY_NAME varchar(756),
-	ENTITY_ID varchar(200),
-	PARENT_ID varchar(200),
-	LEARNER_UID varchar(99),
-	FIELD_NAME varchar(756),
-	FIELD_VALUE varchar(756),
-	FIELD_START_VALUE varchar(756),
-	ACTION_STATUS varchar(100),
-	DATE_PERFORMED timestamp,
-	DATE_RECORDED timestamp,
-	GRADER_ID varchar(99)
-);
-
+-- SAK-17428
 
 alter table GB_CATEGORY_T
 add (
@@ -700,7 +678,6 @@ add (
 	CATEGORY_ORDER INT,
 	ENFORCE_POINT_WEIGHTING tinyint(1)
 ); 
-
 
 alter table GB_GRADEBOOK_T
 add (
@@ -713,93 +690,12 @@ add (
 	DO_SHOW_ITEM_STATS tinyint(1)
 );
 
-
-
 alter table GB_GRADABLE_OBJECT_T
 add (
 	IS_NULL_ZERO tinyint(1)
 );
 
-
-
-
-create index GB_ACTION_RECORD_ID_IDX on GB_ACTION_RECORD_T(ID);
-
-
-
-
-create index GB_ACTION_RECORD_GRADEBOOK_IDX on GB_ACTION_RECORD_T(GRADEBOOK_UID);
-
-
-
-create table GB_ACTION_RECORD_PROPERTY_T 
-(
-	ACTION_RECORD_ID bigint,
-	PROPERTY_NAME varchar(756),
-	PROPERTY_VALUE varchar(756)
-);
-
-
-
-
-create index GB_ACTION_RECORD_PROP_ID_IDX on GB_ACTION_RECORD_PROPERTY_T(ACTION_RECORD_ID);
-
-
-
-
-create table GB_USER_DEREFERENCE_T 
-(
-	ID bigint NOT NULL AUTO_INCREMENT,
-	KEY(ID),
-	USER_UID varchar(99),
-	EID varchar(99),
-	DISPLAY_ID varchar(99),
-	DISPLAY_NAME varchar(756),
-	LAST_NAME_FIRST varchar(756),
-	SORT_NAME varchar(756),
-	EMAIL varchar(756),
-	CREATED_ON timestamp
-);
-
-
-
-create unique index GB_USER_DEREF_USER_IDX on GB_USER_DEREFERENCE_T(USER_UID);
-
-
-
-
-
-create table GB_USER_DEREF_RM_UPDATE_T 
-(
-	ID bigint NOT NULL AUTO_INCREMENT,
-	KEY(ID),
-	REALM_ID varchar(99),
-	LAST_UPDATE timestamp,
-	REALM_COUNT bigint
-);
-
-
-
-
-
-create index GB_USER_DEREF_RM_UP_IDX on GB_USER_DEREF_RM_UPDATE_T(REALM_ID);
-
-
-alter table GB_GRADE_RECORD_T
-add (
-	EXCLUDED tinyint(1)
-);
-
-create table GB_USER_CONFIG_T 
-(
-	ID bigint NOT NULL AUTO_INCREMENT,
-	KEY(ID),
-	USER_UID varchar(99),
-	GRADEBOOK_ID bigint,
-	CONFIG_FIELD varchar(99),
-	CONFIG_VALUE varchar(756)
-);
--- END SAK-17847
+-- END SAK-17428
 
 
 -- SAK-15311
--- reference/docs/conversion/sakai_2_7_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_7_0_oracle_conversion.sql
@@ -684,7 +684,8 @@ DROP COLUMN GRADEBOOK_ASSIGNMENT;
 --///////////////////////////////////
 
 
--- SAK-17847
+-- SAK-17428
+
 alter table GB_CATEGORY_T
 add (
 	IS_EQUAL_WEIGHT_ASSNS number(1,0),
@@ -709,106 +710,7 @@ add (
 	IS_NULL_ZERO number(1,0)
 );
 
-create sequence GB_ACTION_RECORD_S
-start with 1000
-increment by 1
-nocache
-nocycle;
-
-create table GB_ACTION_RECORD_T
-(
-	ID number(19),
-	VERSION number(10),
-	GRADEBOOK_UID varchar2(756),
-	GRADEBOOK_ID number(19),
-	ENTITY_TYPE varchar2(200),
-	ACTION_TYPE varchar2(200),
-	ENTITY_NAME varchar2(756),
-	ENTITY_ID varchar2(200),
-	PARENT_ID varchar2(200),
-	LEARNER_UID varchar2(99),
-	FIELD_NAME varchar2(756),
-	FIELD_VALUE varchar2(756),
-	FIELD_START_VALUE varchar2(756),
-	ACTION_STATUS varchar2(100),
-	DATE_PERFORMED timestamp,
-	DATE_RECORDED timestamp,
-	GRADER_ID varchar2(99)
-);
-
-create index GB_ACTION_RECORD_ID_IDX on GB_ACTION_RECORD_T(ID);
-
-create index GB_ACTION_RECORD_GRADEBOOK_IDX on GB_ACTION_RECORD_T(GRADEBOOK_UID);
-
-create table GB_ACTION_RECORD_PROPERTY_T 
-(
-	ACTION_RECORD_ID number(19),
-	PROPERTY_NAME varchar(756),
-	PROPERTY_VALUE varchar(756)
-);
-
-create index GB_ACTION_RECORD_PROP_ID_IDX on GB_ACTION_RECORD_PROPERTY_T(ACTION_RECORD_ID);
-
-create table GB_USER_DEREFERENCE_T 
-(
-	ID number(19),
-	USER_UID varchar2(99),
-	EID varchar2(99),
-	DISPLAY_ID varchar2(99),
-	DISPLAY_NAME varchar2(756),
-	LAST_NAME_FIRST varchar2(756),
-	SORT_NAME varchar2(756),
-	EMAIL varchar2(756),
-	CREATED_ON timestamp
-);
-
-create unique index GB_USER_DEREF_USER_IDX on GB_USER_DEREFERENCE_T(USER_UID);
-
-create sequence GB_USER_DEREFERENCE_S
-start with 1000
-increment by 1
-nocache
-nocycle;
-
-create table GB_USER_DEREF_RM_UPDATE_T 
-(
-	ID number(19),
-	REALM_ID varchar2(99),
-	LAST_UPDATE timestamp,
-	REALM_COUNT number(19)
-);
-
-create sequence GB_USER_DEREF_RM_UPDATE_S
-start with 1000
-increment by 1
-nocache
-nocycle;
-
-create index GB_USER_DEREF_RM_UP_IDX on GB_USER_DEREF_RM_UPDATE_T(REALM_ID);
-
-
--- The following should be run when moving from QA-8 to QA-9
-
-create index GB_USER_DEREF_SORT_NM_IDX on GB_USER_DEREFERENCE_T(SORT_NAME);
-create index GB_USER_DEREF_EMAIL_IDX on GB_USER_DEREFERENCE_T(EMAIL);
-
-create table GB_USER_CONFIG_T 
-(
-	ID number(19),
-	USER_UID varchar2(99),
-	GRADEBOOK_ID number(19),
-	CONFIG_FIELD varchar2(99),
-	CONFIG_VALUE varchar2(756)
-);
-
-create sequence GB_USER_CONFIG_S
-start with 1000
-increment by 1
-nocache
-nocycle;
-
-create unique index GB_USER_CONFIG_UNIQ_IDX on GB_USER_CONFIG_T(USER_UID, GRADEBOOK_ID, CONFIG_FIELD);
--- END SAK-17847
+-- END SAK-17428
 
 
 -- SAK-15311
