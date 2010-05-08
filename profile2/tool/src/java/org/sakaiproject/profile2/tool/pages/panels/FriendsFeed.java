c5e5c77e3e323bb19ac21e8a4358c7664c669eab
Steve Swinsburg <steve.swinsburg@gmail.com>
2010-05-08 13:23:22 +0000

    NOJIRA - fix logger
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@77271 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/panels/FriendsFeed.java
+++ profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/panels/FriendsFeed.java
@@ -56,7 +56,7 @@ import org.sakaiproject.profile2.util.ProfileConstants;
 public class FriendsFeed extends Panel {
 	
 	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger(ChangeProfilePictureUrl.class);
+	private static final Logger log = Logger.getLogger(FriendsFeed.class);
     
 	@SpringBean(name="org.sakaiproject.profile2.logic.SakaiProxy")
 	private SakaiProxy sakaiProxy;
