sakaiproject/sakai

commit 2be80f82e7c4c9fe2d9b64d6a9496d947c8d6c77

SAK-18101 Apply AZ patch including new method getCourierPresenceString().
git-svn-id: https://source.sakaiproject.org/svn/chat/trunk@76625 66ffb92e-73f9-0310-93c1-f5514f145a0a

chat/chat-tool/tool/src/java/org/sakaiproject/chat2/tool/ChatTool.java
@@ -873,9 +873,29 @@ public class ChatTool implements RoomObserver, PresenceObserver {
     * @return String
     */
    public String getCourierString() {
-      return "/courier/" + getCurrentChatChannelId() + "/" + CHAT_CONTEXT_PRESENCE_PREFIX + getContext();
+	   StringBuilder courierString = new StringBuilder("/courier/");
+	   courierString.append(getCurrentChatChannelId());
+	   courierString.append("/");
+	   courierString.append(getToolString());
+	   courierString.append("?sessionId=");
+	   courierString.append(SessionManager.getCurrentSession().getId());
+	   
+	   return courierString.toString();
    }
-
+   
+   /**
+    * Get a string that includes the courier-presence sessionId
+    * @return String
+    */
+   public String getCourierPresenceString() {
+		StringBuilder courierString = new StringBuilder("/courier/");
+		courierString.append(getToolString());
+		courierString.append("-presence?sessionId=");
+		courierString.append(SessionManager.getCurrentSession().getId());
+		  
+		return courierString.toString();
+   }
+   
    /**
     * Check for add/edit/del perms on the channel
     * @return

commit 68cd562e9c8a23c31c9772b4c050495335835f98

SAK-14202 Fix regression introduced in r76625 for SAK-18101
git-svn-id: https://source.sakaiproject.org/svn/chat/trunk@77353 66ffb92e-73f9-0310-93c1-f5514f145a0a

chat/chat-tool/tool/src/java/org/sakaiproject/chat2/tool/ChatTool.java
@@ -876,24 +876,12 @@ public class ChatTool implements RoomObserver, PresenceObserver {
 	   StringBuilder courierString = new StringBuilder("/courier/");
 	   courierString.append(getCurrentChatChannelId());
 	   courierString.append("/");
-	   courierString.append(getToolString());
+	   courierString.append(CHAT_CONTEXT_PRESENCE_PREFIX);
+	   courierString.append(getContext());
 	   courierString.append("?userId=");
 	   courierString.append(SessionManager.getCurrentSessionUserId());
 	   
 	   return courierString.toString();
-   }
-   
-   /**
-    * Get a string that includes the courier-presence sessionId
-    * @return String
-    */
-   public String getCourierPresenceString() {
-		StringBuilder courierString = new StringBuilder("/courier/");
-		courierString.append(getToolString());
-		courierString.append("-presence?userId=");
-		courierString.append(SessionManager.getCurrentSessionUserId());
-		  
-		return courierString.toString();
    }
    
    /**