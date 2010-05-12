f1e502a765de6350189b2ceffedcfb82de093c68
Adrian Fish <a.fish@lancaster.ac.uk>
2010-05-12 14:59:19 +0000

    Added a first draft ProfileContentProducer.
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@77407 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/impl/src/java/org/sakaiproject/profile2/entity/ProfileContentProducer.java
@@ -0,0 +1,314 @@
+package org.sakaiproject.profile2.entity;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.Reader;
+
+import org.sakaiproject.api.common.edu.person.SakaiPerson;
+import org.sakaiproject.db.api.SqlService;
+import org.sakaiproject.entity.api.Entity;
+import org.sakaiproject.event.api.Event;
+import org.sakaiproject.profile2.logic.ProfileLogic;
+import org.sakaiproject.profile2.logic.SakaiProxy;
+import org.sakaiproject.profile2.model.UserProfile;
+import org.sakaiproject.profile2.util.ProfileConstants;
+import org.sakaiproject.search.api.EntityContentProducer;
+import org.sakaiproject.search.api.SearchIndexBuilder;
+import org.sakaiproject.search.api.SearchService;
+import org.sakaiproject.search.model.SearchBuilderItem;
+import org.sakaiproject.user.api.User;
+
+import org.apache.log4j.Logger;
+
+public class ProfileContentProducer implements EntityContentProducer {
+	
+	private ProfileLogic profileLogic = null;
+	public void setProfileLogic(ProfileLogic profileLogic) {
+		this.profileLogic = profileLogic;
+	}
+	
+	private SearchService searchService = null;
+	public void setSearchService(SearchService searchService) {
+		this.searchService = searchService;
+	}
+	
+	private SearchIndexBuilder searchIndexBuilder = null;
+	public void setSearchIndexBuilder(SearchIndexBuilder searchIndexBuilder) {
+		this.searchIndexBuilder = searchIndexBuilder;
+	}
+	
+	private SakaiProxy sakaiProxy = null;
+	public void setSakaiProxy(SakaiProxy sakaiProxy) {
+		this.sakaiProxy = sakaiProxy;
+	}
+	
+	private SqlService sqlService = null;
+	public void setSqlService(SqlService sqlService) {
+		this.sqlService = sqlService;
+	}
+
+	private Logger logger = Logger.getLogger(ProfileContentProducer.class);
+
+	public void init() {
+		searchService.registerFunction(ProfileConstants.EVENT_PROFILE_INFO_UPDATE);
+		searchIndexBuilder.registerEntityContentProducer(this);
+	}
+
+	public boolean canRead(String reference) {
+		if (logger.isDebugEnabled())
+			logger.debug("canRead()");
+
+		// TODO: sort this !
+		return true;
+	}
+
+	public Integer getAction(Event event) {
+		if (logger.isDebugEnabled())
+			logger.debug("getAction()");
+
+		String eventName = event.getEvent();
+
+		if (ProfileConstants.EVENT_PROFILE_INFO_UPDATE.equals(eventName)) {
+			return SearchBuilderItem.ACTION_ADD;
+		} else
+			return SearchBuilderItem.ACTION_UNKNOWN;
+	}
+
+	public String getContainer(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getContainer()");
+
+		return null;
+	}
+
+	public String getContent(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getContent(" + ref + ")");
+
+		String[] parts = ref.split(Entity.SEPARATOR);
+
+		String type = parts[1];
+		String id = parts[2];
+
+		if ("profile".equals(type)) {
+			Connection conn = null;
+			Statement st = null;
+			try {
+				conn = sqlService.borrowConnection();
+				st = conn.createStatement();
+				ResultSet rs = st.executeQuery("SELECT NOTES FROM SAKAI_PERSON_T WHERE AGENT_UUID = '" + id + "'");
+				String notes = "";
+				if(rs.next())
+					notes = /* a.k.a. personalSummary :) */ rs.getString("NOTES");
+				
+				rs.close();
+				
+				return notes;
+				/*
+				SakaiPerson sakaiPerson = sakaiProxy.getSakaiPerson(id);
+				if(sakaiPerson != null) {
+					String content = sakaiPerson.getNotes();
+					return content;
+				}
+				else {
+					return null;
+				}
+				*/
+			} catch (Exception e) {
+				logger.error(
+						"Caught exception whilst getting content for profile '"
+								+ id + "'", e);
+			}
+			finally {
+				if(st != null) {
+					try {
+						st.close();
+					}
+					catch(SQLException sqle) {}
+				}
+				
+				if(conn != null) {
+					sqlService.returnConnection(conn);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public Reader getContentReader(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getContentReader(" + ref + ")");
+
+		return null;
+	}
+
+	public Map getCustomProperties(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getCustomProperties(" + ref + ")");
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getCustomRDF(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getCustomRDF(" + ref + ")");
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getId(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getId(" + ref + ")");
+
+		String[] parts = ref.split(Entity.SEPARATOR);
+
+		if (parts.length == 3) {
+			return parts[2];
+		}
+
+		return "unknown";
+	}
+
+	public List getSiteContent(String siteId) {
+		if (logger.isDebugEnabled())
+			logger.debug("getSiteContent(" + siteId + ")");
+		
+		List refs = new ArrayList();
+		
+		if(siteId.startsWith("~") && siteId.length() == 37) {
+			// This is a user workspace
+			String uuid = siteId.substring(1);
+			
+			if(uuid != null) {
+				refs.add("/profile/" + uuid);
+			}
+		}
+
+		return refs;
+	}
+
+	public Iterator getSiteContentIterator(String siteId) {
+		if (logger.isDebugEnabled())
+			logger.debug("getSiteContentIterator(" + siteId + ")");
+
+		return getSiteContent(siteId).iterator();
+	}
+
+	public String getSiteId(String eventRef) {
+		if (logger.isDebugEnabled())
+			logger.debug("getSiteId(" + eventRef + ")");
+
+		String[] parts = eventRef.split(Entity.SEPARATOR);
+		
+		if (parts.length == 3) {
+			String id = "~" + parts[2];
+			return id;
+		}
+
+		return null;
+	}
+
+	public String getSubType(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getSubType(" + ref + ")");
+
+		return null;
+	}
+
+	public String getTitle(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getTitle(" + ref + ")");
+
+		String[] parts = ref.split(Entity.SEPARATOR);
+		String type = parts[1];
+		String id = parts[2];
+
+		if ("profile".equals(type)) {
+			try {
+				User user = sakaiProxy.getUserById(id);
+				return user.getDisplayName();
+			} catch (Exception e) {
+				logger.error("Caught exception whilst getting user display name for id '"
+						+ id + "'", e);
+			}
+		}
+
+		return "Unrecognised";
+	}
+
+	public String getTool() {
+		return "Profile";
+	}
+
+	public String getType(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getType(" + ref + ")");
+
+		return "profile";
+	}
+
+	public String getUrl(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("getUrl(" + ref + ")");
+
+		String[] parts = ref.split(Entity.SEPARATOR);
+		String type = parts[1];
+		String id = parts[2];
+
+		if ("profile".equals(type)) {
+			try {
+				UserProfile profile = profileLogic.getUserProfile(id);
+				return sakaiProxy.getDirectUrlToUserProfile(id, null);
+			} catch (Exception e) {
+				logger.error("Caught exception whilst getting url for profile '"
+						+ id + "'", e);
+			}
+		}
+
+		return null;
+	}
+
+	public boolean isContentFromReader(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("isContentFromReader(" + ref + ")");
+
+		return false;
+	}
+
+	public boolean isForIndex(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("isForIndex(" + ref + ")");
+
+		return true;
+	}
+
+	public boolean matches(String ref) {
+		if (logger.isDebugEnabled())
+			logger.debug("matches(" + ref + ")");
+
+		String[] parts = ref.split(Entity.SEPARATOR);
+
+		if ("profile".equals(parts[1]))
+			return true;
+
+		return false;
+	}
+
+	public boolean matches(Event event) {
+		String eventName = event.getEvent();
+
+		if (ProfileConstants.EVENT_PROFILE_INFO_UPDATE.equals(eventName)) {
+			return true;
+		}
+
+		return false;
+	}
+}
