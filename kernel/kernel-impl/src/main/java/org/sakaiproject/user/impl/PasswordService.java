418544dfb50546262af7da028b31dd299e83f20f
David Horwitz <dhorwitz@gmail.com>
2010-05-13 13:20:58 +0000

    KNL-328 add copyright headers
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@77425 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/user/impl/PasswordService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/user/impl/PasswordService.java
@@ -1,3 +1,24 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/svn/kernel/trunk/kernel-impl/src/main/java/org/sakaiproject/user/impl/OpenAuthnComponent.java $
+ * $Id: OpenAuthnComponent.java 51317 2008-08-24 04:38:02Z csev@umich.edu $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2005, 2006, 2008, 2009, 2010 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.user.impl;
 
 import java.io.ByteArrayOutputStream;
