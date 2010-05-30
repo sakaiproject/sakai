d9f00a64b19e5cfc24c561bd63acc0faf8c40343
Charles Severance <csev@umich.edu>
2010-05-30 15:56:11 +0000

    BLTI-49
    Align the license headers with the OAuth project per this bug
    report and fix:
    
    http://code.google.com/p/oauth/issues/detail?id=167
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@78159 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-util/src/java/net/oauth/ParameterStyle.java
+++ basiclti/basiclti-util/src/java/net/oauth/ParameterStyle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007, 2008 Netflix, Inc.
+ * Copyright 2009 John Kristian
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- basiclti/basiclti-util/src/java/net/oauth/client/ExcerptInputStream.java
+++ basiclti/basiclti-util/src/java/net/oauth/client/ExcerptInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 Netflix, Inc.
+ * Copyright 2008 Sam Pullara
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.oauth.client;
 
 import java.io.BufferedInputStream;
