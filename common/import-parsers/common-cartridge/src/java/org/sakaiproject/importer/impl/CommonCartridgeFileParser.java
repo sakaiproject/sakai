574cb957032cb81954eb533c6540c88de5c1abba
Stephen Marquard <stephen.marquard@uct.ac.za>
2010-06-30 11:44:28 +0000

    SAK-18634 Regression: IMS-CC Packages can no longer be imported (patch from Sam Ottenhof)
    git-svn-id: https://source.sakaiproject.org/svn/common/trunk@78914 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- common/import-parsers/common-cartridge/src/java/org/sakaiproject/importer/impl/CommonCartridgeFileParser.java
+++ common/import-parsers/common-cartridge/src/java/org/sakaiproject/importer/impl/CommonCartridgeFileParser.java
@@ -21,35 +21,23 @@
 
 package org.sakaiproject.importer.impl;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.sakaiproject.archive.api.ImportMetadata;
-import org.sakaiproject.importer.api.ImportDataSource;
 import org.sakaiproject.importer.api.ImportFileParser;
 import org.sakaiproject.importer.api.Importable;
-import org.sakaiproject.importer.impl.importables.FileResource;
 import org.sakaiproject.importer.impl.importables.Folder;
-import org.sakaiproject.importer.impl.importables.WebLink;
 import org.sakaiproject.importer.impl.translators.CCAssessmentTranslator;
 import org.sakaiproject.importer.impl.translators.CCDiscussionTopicTranslator;
 import org.sakaiproject.importer.impl.translators.CCLearningApplicationResourceTranslator;
@@ -60,7 +48,7 @@ import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
 public class CommonCartridgeFileParser extends IMSFileParser {
-	private static final String CC_SCHEMA_NAME = "IMS Common Cartridge";
+	private static final String CC_NAMESPACE_URI = "http://www.imsglobal.org/xsd/imscc/imscp_v1p1";
 	
 	public CommonCartridgeFileParser() {
 		// add resource translators here
@@ -78,10 +66,14 @@ public class CommonCartridgeFileParser extends IMSFileParser {
 	public boolean isValidArchive(byte[] fileData) {
 		if (super.isValidArchive(fileData)) {
 			Document manifest = extractFileAsDOM("/imsmanifest.xml", fileData);
-			return CC_SCHEMA_NAME.equals(XPathHelper.getNodeValue("/manifest/metadata/schema", manifest));
+			return (enclosingDocumentContainsNamespaceDeclaration(manifest, CC_NAMESPACE_URI));
 		} else return false;
 	}
 	
+	private boolean enclosingDocumentContainsNamespaceDeclaration(Node node, String nameSpaceURI) {
+		return node.isDefaultNamespace(CC_NAMESPACE_URI);
+	}
+
 	public ImportFileParser newParser() {
 		return new CommonCartridgeFileParser();
 	}
