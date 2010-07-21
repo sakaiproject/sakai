e7a64a8a0278ace87407db274708b02547672e04
Stephen Marquard <stephen.marquard@uct.ac.za>
2010-07-21 10:41:56 +0000

    SAK-18602
    git-svn-id: https://source.sakaiproject.org/svn/citations/trunk@79734 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/CitationListAccessServlet.java
+++ citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/CitationListAccessServlet.java
@@ -312,7 +312,7 @@ public class CitationListAccessServlet implements HttpAccess
     			String href = citation.hasPreferredUrl() ? citation.getCustomUrl(citation.getPreferredUrlId()) : citation.getOpenurl();
     			
     			out.println("\t\t<td headers=\"details\">");
-    			out.println("\t\t\t<a href=\"" + href + "\" target=\"_blank\">" + Validator.escapeHtml( (String)citation.getCitationProperty( Schema.TITLE ) ) + "</a><br />");
+    			out.println("\t\t\t<a href=\"" + Validator.escapeHtml(href) + "\" target=\"_blank\">" + Validator.escapeHtml( (String)citation.getCitationProperty( Schema.TITLE ) ) + "</a><br />");
     			out.println("\t\t\t\t" + Validator.escapeHtml( citation.getCreator() ) );
     			out.println("\t\t\t\t" + Validator.escapeHtml( citation.getSource() ) );
     			out.println("\t\t\t<div class=\"itemAction\">");
@@ -327,7 +327,7 @@ public class CitationListAccessServlet implements HttpAccess
       					String urlLabel = ( citation.getCustomUrlLabel( urlId ) == null ||
       							citation.getCustomUrlLabel( urlId ).trim().equals("") ) ? rb.getString( "nullUrlLabel.view" ) : Validator.escapeHtml( citation.getCustomUrlLabel( urlId ) );
               
-    					  out.println("\t\t\t\t<a href=\"" + citation.getCustomUrl( urlId ).toString() + "\" target=\"_blank\">" + urlLabel + "</a>");
+    					  out.println("\t\t\t\t<a href=\"" + Validator.escapeHtml(citation.getCustomUrl( urlId )) + "\" target=\"_blank\">" + urlLabel + "</a>");
     	    			out.println("\t\t\t\t |");
     	    		}
     				}
