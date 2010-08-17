1e07a891ec0974742d5ad9fb38ae12edeb955dbe
Carl Hall <carl.hall@gmail.com>
2010-08-17 13:25:39 +0000

    Added more test bits to fix the build.
    git-svn-id: https://source.sakaiproject.org/svn/mailsender/mailsender/trunk@98295 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- mailsender/impl/src/test/org/sakaiproject/mailsender/logic/impl/ConfigLogicImplTest.java
+++ mailsender/impl/src/test/org/sakaiproject/mailsender/logic/impl/ConfigLogicImplTest.java
@@ -16,20 +16,45 @@
  **********************************************************************************/
 package org.sakaiproject.mailsender.logic.impl;
 
+import static org.mockito.Mockito.when;
+
+import java.util.Properties;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.sakaiproject.mailsender.logic.ConfigLogic;
+import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.mailsender.logic.ExternalLogic;
+import org.sakaiproject.tool.api.ToolManager;
 
 @RunWith(value = MockitoJUnitRunner.class)
 public class ConfigLogicImplTest {
-	
+
+	@Mock
+	ExternalLogic externalLogic;
+
+	@Mock
+	ServerConfigurationService configService;
+
+	@Mock(answer = Answers.RETURNS_DEEP_STUBS)
+	ToolManager toolManager;
+
+	Properties props;
 	ConfigLogicImpl logic;
 
 	@Before
 	public void setUp() {
+		props = new Properties();
+
+		when(toolManager.getCurrentPlacement().getConfig()).thenReturn(props);
+
 		logic = new ConfigLogicImpl();
+		logic.setExternalLogic(externalLogic);
+		logic.setServerConfigurationService(configService);
+		logic.setToolManager(toolManager);
 	}
 
 	@Test
