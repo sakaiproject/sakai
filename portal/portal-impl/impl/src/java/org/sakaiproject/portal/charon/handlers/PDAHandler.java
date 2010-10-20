68a0bf762e26cbeff184a80a7a121dcce9cd9c53
Zhen Qian <zqian@umich.edu>
2010-10-20 19:56:11 +0000

    SAK-14808
    git-svn-id: https://source.sakaiproject.org/svn/portal/trunk@83592 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/PDAHandler.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/PDAHandler.java
@@ -71,8 +71,9 @@ public class PDAHandler extends PageHandler
 
 	private static final String URL_FRAGMENT = "pda";
 	private static final String SAKAI_COOKIE_DOMAIN = "sakai.cookieDomain"; //RequestFilter.SAKAI_COOKIE_DOMAIN
-
-
+	
+	private static final String TOOLCONFIG_SHOW_RESET_BUTTON = "reset.button";
+	
 	public PDAHandler()
 	{
 		setUrlFragment(PDAHandler.URL_FRAGMENT);
@@ -82,7 +83,6 @@ public class PDAHandler extends PageHandler
 	public int doGet(String[] parts, HttpServletRequest req, HttpServletResponse res,
 			Session session) throws PortalHandlerException
 			{
-
 		if ((parts.length >= 2) && (parts[1].equals("pda")))
 		{
 			// Indicate that we are the controlling portal
@@ -224,7 +224,7 @@ public class PDAHandler extends PageHandler
 		{
 			return NEXT;
 		}
-			}
+	}
 
 	/*
 	 * Optionally actually grab the tool's output and include it in the same
@@ -265,9 +265,10 @@ public class PDAHandler extends PageHandler
 
 
 		boolean retval;
+		String toolContextPath = req.getContextPath() + req.getServletPath() + Web.makePath(parts, 1, 5);
 		try {
 			retval = doToolBuffer(req, bufferedResponse, session, parts[4], 
-					req.getContextPath() + req.getServletPath() + Web.makePath(parts, 1, 5), 
+					toolContextPath, 
 					Web.makePath(parts, 5, parts.length));
 			if ( ! retval ) return;
 		} catch (ToolException e) {
@@ -299,6 +300,13 @@ public class PDAHandler extends PageHandler
 		if( tidAllow.indexOf(":debug:") >= 0 )
 			log.info("Frameless HS="+headStart+" HE="+headEnd+" BS="+bodyStart+" BE="+bodyEnd);
 
+		boolean showResetButton = !"false".equals(siteTool.getConfig().getProperty(
+				TOOLCONFIG_SHOW_RESET_BUTTON));
+		rcontext.put("showResetButton", Boolean.valueOf(showResetButton));
+		if (showResetButton)
+		{
+			rcontext.put("resetActionUrl", toolContextPath.replace("/tool/", "/tool-reset/"));
+		}
 		if (bodyEnd > bodyStart && bodyStart > headEnd && headEnd > headStart
 				&& headStart > 1)
 		{
