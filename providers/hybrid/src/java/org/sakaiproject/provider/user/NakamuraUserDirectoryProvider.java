7d92614c294e7e0527d5387ccf43376245e0c6fd
Lance Speelmon <lance@rsmart.com>
2010-11-10 19:40:57 +0000

    HYB-1 Extract common behavior from NakamuraUserDirectoryProvider and NakamuraAuthenticationFilter into a common util class
    git-svn-id: https://source.sakaiproject.org/svn/providers/trunk@84583 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- providers/hybrid/src/java/org/sakaiproject/provider/user/NakamuraUserDirectoryProvider.java
+++ providers/hybrid/src/java/org/sakaiproject/provider/user/NakamuraUserDirectoryProvider.java
@@ -18,70 +18,87 @@
 
 package org.sakaiproject.provider.user;
 
-import java.net.URI;
 import java.util.Collection;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-
-import net.sf.json.JSONObject;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.client.HttpResponseException;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.thread_local.api.ThreadLocalManager;
 import org.sakaiproject.user.api.UserDirectoryProvider;
 import org.sakaiproject.user.api.UserEdit;
+import org.sakaiproject.util.NakamuraAuthenticationHelper;
+import org.sakaiproject.util.NakamuraAuthenticationHelper.AuthInfo;
 import org.sakaiproject.util.XSakaiToken;
 
 /**
- *
+ * Authenticates users who have already been authenticated to a Nakamura
+ * instance.
  */
 public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 	private static final Log LOG = LogFactory
 			.getLog(NakamuraUserDirectoryProvider.class);
 	/**
-	 * Key in the ThreadLocalManager for access to the current http request
-	 * object.
+	 * Key in the ThreadLocalManager for access to the current
+	 * {@link HttpServletRequest} object.
 	 */
-	public final static String CURRENT_HTTP_REQUEST = "org.sakaiproject.util.RequestFilter.http_request";
-	private static final String COOKIE_NAME = "SAKAI-TRACKING";
-	private static final String ANONYMOUS = "anonymous";
-	private static final String THREAD_LOCAL_CACHE_KEY = NakamuraUserDirectoryProvider.class
-			.getName()
-			+ ".cache";
+	private static final String CURRENT_HTTP_REQUEST = "org.sakaiproject.util.RequestFilter.http_request";
+	/**
+	 * All sakai.properties settings will be prefixed with this string.
+	 */
 	public static final String CONFIG_PREFIX = "org.sakaiproject.provider.user.NakamuraUserDirectoryProvider";
+	/**
+	 * The principal that will be used when connecting to Nakamura REST
+	 * end-point. Must have permissions to read /var/cluster/user.cookie.json.
+	 * 
+	 * @see XSakaiToken#createToken(String, String)
+	 */
 	public static final String CONFIG_PRINCIPAL = CONFIG_PREFIX + ".principal";
+	/**
+	 * The hostname we will use to lookup the sharedSecret for access to
+	 * validateUrl.
+	 * 
+	 * @see XSakaiToken#createToken(String, String)
+	 */
 	public static final String CONFIG_HOST_NAME = CONFIG_PREFIX + ".hostname";
+	/**
+	 * The Nakamura REST end-point we will use to validate the cookie
+	 */
 	public static final String CONFIG_VALIDATE_URL = CONFIG_PREFIX
 			+ ".validateUrl";
 
 	/**
-	 * The Nakamura RESTful service to validate authenticated users
+	 * The Nakamura RESTful service to validate authenticated users. A good
+	 * default for common hybrid implementations is supplied.
+	 * 
+	 * @see #CONFIG_VALIDATE_URL
 	 */
 	protected String validateUrl = "http://localhost/var/cluster/user.cookie.json?c=";
 
 	/**
 	 * The nakamura user that has permissions to GET
-	 * /var/cluster/user.cookie.json.
+	 * /var/cluster/user.cookie.json. A good default for common hybrid
+	 * implementations is supplied.
+	 * 
+	 * @see XSakaiToken#createToken(String, String)
+	 * @see #CONFIG_PRINCIPAL
 	 */
 	protected String principal = "admin";
 
 	/**
 	 * The hostname we will use to lookup the sharedSecret for access to
-	 * validateUrl.
+	 * validateUrl. A good default for common hybrid implementations is
+	 * supplied.
+	 * 
+	 * @see XSakaiToken#createToken(String, String)
+	 * @see #CONFIG_HOST_NAME
 	 */
 	protected String hostname = "localhost";
 
-	/**
-	 * Injected ThreadLocalManager
-	 */
-	private ThreadLocalManager threadLocalManager;
+	// dependencies
+	private ThreadLocalManager threadLocalManager; // injected
+	private NakamuraAuthenticationHelper nakamuraAuthenticationHelper;
 
 	/**
 	 * @see org.sakaiproject.user.api.UserDirectoryProvider#authenticateUser(java.lang.String,
@@ -95,9 +112,11 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 		if (eid == null) {
 			// maybe should throw exception instead?
 			// since I assume I am in a chain, I will be quiet about it
+			LOG.debug("eid == null");
 			return false;
 		}
-		final AuthInfo authInfo = getPrincipalLoggedIntoK2(getHttpServletRequest());
+		final AuthInfo authInfo = nakamuraAuthenticationHelper
+				.getPrincipalLoggedIntoNakamura(getHttpServletRequest());
 		if (authInfo != null) {
 			if (eid.equalsIgnoreCase(authInfo.getPrincipal())) {
 				edit.setFirstName(authInfo.getFirstName());
@@ -129,9 +148,11 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 			LOG.debug("findUserByEmail(UserEdit edit, String " + email + ")");
 		}
 		if (email == null) {
+			LOG.debug("String email == null");
 			return false;
 		}
-		final AuthInfo authInfo = getPrincipalLoggedIntoK2(getHttpServletRequest());
+		final AuthInfo authInfo = nakamuraAuthenticationHelper
+				.getPrincipalLoggedIntoNakamura(getHttpServletRequest());
 		if (authInfo != null) {
 			if (email.equalsIgnoreCase(authInfo.getEmailAddress())) {
 				edit.setEid(authInfo.getPrincipal());
@@ -150,6 +171,7 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 	public boolean getUser(UserEdit edit) {
 		LOG.debug("getUser(UserEdit edit)");
 		if (edit == null) {
+			LOG.debug("UserEdit edit == null");
 			return false;
 		}
 		return this.authenticateUser(edit.getEid(), edit, null);
@@ -167,63 +189,11 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 		return;
 	}
 
-	private AuthInfo getPrincipalLoggedIntoK2(HttpServletRequest request) {
-		LOG.debug("getPrincipalLoggedIntoK2(HttpServletRequest request)");
-		final Object cache = threadLocalManager.get(THREAD_LOCAL_CACHE_KEY);
-		if (cache != null && cache instanceof AuthInfo) {
-			LOG.debug("cache hit!");
-			return (AuthInfo) cache;
-		}
-		AuthInfo authInfo = null;
-		final String secret = getSecret(request);
-		if (secret != null) {
-			final DefaultHttpClient http = new DefaultHttpClient();
-			try {
-				final URI uri = new URI(validateUrl + secret);
-				final HttpGet httpget = new HttpGet(uri);
-				// authenticate to Nakamura using x-sakai-token mechanism
-				final String token = XSakaiToken.createToken(hostname,
-						principal);
-				httpget.addHeader(XSakaiToken.X_SAKAI_TOKEN_HEADER, token);
-				//
-				final ResponseHandler<String> responseHandler = new BasicResponseHandler();
-				final String responseBody = http.execute(httpget,
-						responseHandler);
-				authInfo = new AuthInfo(responseBody);
-			} catch (HttpResponseException e) {
-				// usually a 404 error - could not find cookie / not valid
-				if (LOG.isDebugEnabled()) {
-					LOG.debug("HttpResponseException: " + e.getMessage() + ": "
-							+ e.getStatusCode() + ": " + validateUrl + secret);
-				}
-			} catch (Exception e) {
-				LOG.error(e.getMessage(), e);
-				throw new Error(e);
-			} finally {
-				http.getConnectionManager().shutdown();
-			}
-		}
-
-		// cache results in thread local
-		threadLocalManager.set(THREAD_LOCAL_CACHE_KEY, authInfo);
-
-		return authInfo;
-	}
-
-	private String getSecret(HttpServletRequest req) {
-		LOG.debug("getSecret(HttpServletRequest req)");
-		String secret = null;
-		final Cookie[] cookies = req.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if (COOKIE_NAME.equals(cookie.getName())) {
-					secret = cookie.getValue();
-				}
-			}
-		}
-		return secret;
-	}
-
+	/**
+	 * Gets a reference to {@link HttpServletRequest} via ThreadLocalManager.
+	 * 
+	 * @return
+	 */
 	private HttpServletRequest getHttpServletRequest() {
 		LOG.debug("getHttpServletRequest()");
 		final HttpServletRequest request = (HttpServletRequest) threadLocalManager
@@ -234,6 +204,9 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 		return request;
 	}
 
+	/**
+	 * Initialize class.
+	 */
 	public void init() {
 		LOG.debug("init()");
 		validateUrl = ServerConfigurationService.getString(CONFIG_VALIDATE_URL,
@@ -242,6 +215,8 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 				principal);
 		hostname = ServerConfigurationService.getString(CONFIG_HOST_NAME,
 				hostname);
+		nakamuraAuthenticationHelper = new NakamuraAuthenticationHelper(
+				threadLocalManager, validateUrl, principal, hostname);
 	}
 
 	/**
@@ -250,63 +225,5 @@ public class NakamuraUserDirectoryProvider implements UserDirectoryProvider {
 	 */
 	public void setThreadLocalManager(ThreadLocalManager threadLocalManager) {
 		this.threadLocalManager = threadLocalManager;
-	}
-
-	/**
-	 * Private class for storing cached results from Nakamura lookup. Use of a
-	 * private class will help prevent hijacking of the cache results.
-	 */
-	final static class AuthInfo {
-		private static final Log AILOG = LogFactory.getLog(AuthInfo.class);
-
-		private String principal;
-		private String firstName;
-		private String lastName;
-		private String emailAddress;
-
-		private AuthInfo(String json) {
-			if (AILOG.isDebugEnabled()) {
-				AILOG.debug("new AuthInfo(String " + json + ")");
-			}
-			final JSONObject user = JSONObject.fromObject(json).getJSONObject(
-					"user");
-			final String p = user.getString("principal");
-			if (p != null && !"".equals(p) && !ANONYMOUS.equals(p)) {
-				principal = p;
-			}
-
-			final JSONObject properties = user.getJSONObject("properties");
-			firstName = properties.getString("firstName");
-			lastName = properties.getString("lastName");
-			emailAddress = properties.getString("email");
-		}
-
-		/**
-		 * @return the givenName
-		 */
-		String getFirstName() {
-			return firstName;
-		}
-
-		/**
-		 * @return the familyName
-		 */
-		String getLastName() {
-			return lastName;
-		}
-
-		/**
-		 * @return the emailAddress
-		 */
-		String getEmailAddress() {
-			return emailAddress;
-		}
-
-		/**
-		 * @return the principal
-		 */
-		String getPrincipal() {
-			return principal;
-		}
 	}
 }
