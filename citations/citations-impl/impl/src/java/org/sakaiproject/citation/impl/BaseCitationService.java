156d810966aa9ccf8bc87bbc7e4f4a218954860b
Anthony Whyte <arwhyte@umich.edu>
2010-12-14 20:36:49 +0000

    SAK-19611
    git-svn-id: https://source.sakaiproject.org/svn/citations/trunk@86413 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseCitationService.java
+++ citations/citations-impl/impl/src/java/org/sakaiproject/citation/impl/BaseCitationService.java
@@ -2400,8 +2400,8 @@ public abstract class BaseCitationService implements CitationService
 	            		String title0 = cit0.getDisplayName().toLowerCase();
 	            		String title1 = cit1.getDisplayName().toLowerCase();
 	            		*/
-	            		String title0 = ((String)cit0.getCitationProperty( Schema.TITLE )).toLowerCase();;
-	            		String title1 = ((String)cit1.getCitationProperty( Schema.TITLE )).toLowerCase();;
+	            		String title0 = ((String)cit0.getCitationProperty( Schema.TITLE )).toLowerCase();
+	            		String title1 = ((String)cit1.getCitationProperty( Schema.TITLE )).toLowerCase();
 
 	            		if (title0 == null)
 	            		{
