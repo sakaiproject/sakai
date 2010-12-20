44594a71d5413fd3618dbe33a9e23e2e51f85921
Zhen Qian <zqian@umich.edu>
2010-12-20 21:05:32 +0000

    SAK-19725
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@86634 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -126,6 +126,7 @@ import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.IdUsedException;
 import org.sakaiproject.exception.InUseException;
 import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.ServerOverloadException;
 import org.sakaiproject.javax.PagingPosition;
 import org.sakaiproject.service.gradebook.shared.AssignmentHasIllegalPointsException;
 import org.sakaiproject.service.gradebook.shared.CategoryDefinition;
@@ -12278,7 +12279,9 @@ public class AssignmentAction extends PagedResourceActionII
 			}
 			catch(Exception e)
 			{
-	
+				// other exceptions should be caught earlier
+				M_log.debug(this + ".doAttachupload ***** Unknown Exception ***** " + e.getMessage());
+				addAlert(state, rb.getFormattedMessage("failed.upload", new Object[]{i}));
 			}
 			if(fileitem == null)
 			{
@@ -12346,11 +12349,17 @@ public class AssignmentAction extends PagedResourceActionII
 							addAlert(state, rb.getString("failed"));
 						}
 					}
-	
+					catch (ServerOverloadException e)
+					{
+						// disk full or no writing permission to disk
+						M_log.debug(this + ".doAttachupload ***** Disk IO Exception ***** " + e.getMessage());
+						addAlert(state, rb.getString("failed.diskio"));
+					}
 					catch(Exception ignore)
 					{
 						// other exceptions should be caught earlier
 						M_log.debug(this + ".doAttachupload ***** Unknown Exception ***** " + ignore.getMessage());
+						addAlert(state, rb.getString("failed"));
 					}
 				}
 				else
