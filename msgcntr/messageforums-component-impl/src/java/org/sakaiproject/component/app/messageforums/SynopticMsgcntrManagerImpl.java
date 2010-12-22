707530176735b8e1136cb6216728b22b92ff1fb3
David Horwitz <dhorwitz@gmail.com>
2010-12-22 13:08:30 +0000

    MSGCNTR-430 don't check read counts for the anon user
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@86646 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/SynopticMsgcntrManagerImpl.java
+++ msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/SynopticMsgcntrManagerImpl.java
@@ -524,6 +524,11 @@ public class SynopticMsgcntrManagerImpl extends HibernateDaoSupport implements S
 		dcms.setSiteName(site.getTitle());
 		dcms.setSiteId(siteId);
 		
+		//MSGCNTR-430 if this is the anon user we can't actualy do anything more -DH
+		if (userId == null) {
+			return dcms;
+		}
+		
 		if (isMessageForumsPageInSite || isMessagesPageInSite(site)) 
 		{
 			if(unreadMessagesHM != null){
