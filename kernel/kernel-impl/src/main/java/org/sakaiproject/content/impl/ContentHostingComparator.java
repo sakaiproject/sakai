5d39122b405beff466cef5512f08c912468cf491
David Roldan Martinez <darolmar@gmail.com>
2011-01-01 18:50:25 +0000

    SAK-19055
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@86746 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/ContentHostingComparator.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/ContentHostingComparator.java
@@ -54,6 +54,13 @@ public class ContentHostingComparator implements Comparator
 	 */
 	boolean m_smart_sort = true;
 	
+	/**
+	 * That object is used to allow locale-sensitive ordering. 
+	 */
+	private static final Collator collator = Collator.getInstance();
+	
+	
+	
 	public ContentHostingComparator(String property, boolean  ascending) {
 		this(property, ascending, true);
 	}
@@ -69,6 +76,10 @@ public class ContentHostingComparator implements Comparator
 		m_property = property;
 		m_ascending = ascending;
 		m_smart_sort = is_smart;
+		
+		//sets the collator properties
+		 collator.setDecomposition(Collator.CANONICAL_DECOMPOSITION);
+		 collator.setStrength(Collator.PRIMARY);
 
 	} // ContentHostingComparator
 
@@ -194,7 +205,7 @@ public class ContentHostingComparator implements Comparator
 	 */
 	public int compareLikeMacFinder(String s1, String s2) {
 		if (! (containsDigits(s1) || containsDigits(s2))) {
-			return s1.compareTo(s2);
+			return collator.compare(s1,s2);
 		}
 		
 		/*
@@ -216,12 +227,12 @@ public class ContentHostingComparator implements Comparator
 			}
 			int v = 0;
 			if (c1[i].getClass().getName().equals("java.lang.String") && c2[i].getClass().getName().equals("java.lang.String")) {
-				v = c1[i].toString().compareToIgnoreCase((c2[i].toString()));
+				v = collator.compare(c1[i].toString(),c2[i].toString());
 			} else if (c1[i].getClass().equals(c2[i].getClass())) {
 				v = c1[i].compareTo(c2[i]);
 			}
 			else {
-				v = c1[i].toString().compareToIgnoreCase((c2[i].toString()));
+				v = collator.compare(c1[i].toString(),c2[i].toString());
 			}
 			if (v != 0) {
 				return v;
