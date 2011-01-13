39f1912b7a4782b0171ff6ccab1fa2b73cad2893
Zhen Qian <zqian@umich.edu>
2011-01-13 18:45:11 +0000

    SAK-19817
    git-svn-id: https://source.sakaiproject.org/svn/calendar/trunk@87215 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
+++ calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
@@ -7611,6 +7611,10 @@ extends VelocityPortletStateAction
 
 		}
 		state.setAttribute("permissionDescriptions",  pRbValues);
+		
+		String groupAware = ToolManager.getCurrentTool().getRegisteredConfig().getProperty("groupAware");
+		state.setAttribute("groupAware", groupAware != null?Boolean.valueOf(groupAware):Boolean.FALSE);
+
 	}
 
 	/**
