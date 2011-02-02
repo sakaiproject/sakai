659bb5b8967f197861b69ecd1b0c14a66f1983d3
Anthony Whyte <arwhyte@umich.edu>
2011-02-02 04:26:47 +0000

    PRFL-539 remove redundant profile2 scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@87926 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
@@ -208,20 +208,7 @@ alter table PROFILE_PRIVACY_T add MY_KUDOS int not null DEFAULT 0;
 -- add gallery feed preference (PRFL-382)
 alter table PROFILE_PREFERENCES_T add SHOW_GALLERY_FEED bit not null DEFAULT true;
 
--- remove twitter from preferences (PRFL-94)
-alter table PROFILE_PREFERENCES_T drop TWITTER_ENABLED;
-alter table PROFILE_PREFERENCES_T drop TWITTER_USERNAME;
-alter table PROFILE_PREFERENCES_T drop TWITTER_PASSWORD;
-
--- add external integration table (PRFL-94)
-create table PROFILE_EXTERNAL_INTEGRATION_T (
-	USER_UUID varchar(99) not null,
-	TWITTER_TOKEN varchar(255),
-	TWITTER_SECRET varchar(255),
-	primary key (USER_UUID)
-);
 -- Profile2 1.3-1.4 upgrade end
-
 
 -- ShortenedUrlService 1.0.0 db creation start
 
