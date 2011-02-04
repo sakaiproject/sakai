03779bb8e350f7d0ca0c2cc24a014926ec72720a
Anthony Whyte <arwhyte@umich.edu>
2011-02-04 13:30:53 +0000

    SAK-19088 fix changed data types (profile2, emailtemplateservice, gradebook); lower case for all keywords.
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@88010 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
@@ -13,89 +13,63 @@
 -- SAK-XXXXX
 alter table ANNOUNCEMENT_MESSAGE add column MESSAGE_ORDER int(11) default null;
 
--- SAK-18864, SAK-19951 adds missing scheduler_trigger_events table for new persistent jobscheduler event feature
-create table scheduler_trigger_events (
-    uuid varchar(36) not null, 
-    type varchar(255) not null, 
-    jobName varchar(255) not null, 
-    triggerName varchar(255) default null, 
-    time datetime not null, 
-    message text,
-    primary key (uuid)
-);
+-- SAK-18532/SAK-19522 new column for Email Template service
+alter table EMAIL_TEMPLATE_ITEM add column EMAILFROM text;
 
--- SAK-17821 Add additional fields to SakaiPerson
-alter table SAKAI_PERSON_T add column STAFF_PROFILE text;
-alter table SAKAI_PERSON_T add column UNIVERSITY_PROFILE_URL text;
-alter table SAKAI_PERSON_T add column ACADEMIC_PROFILE_URL text;
-alter table SAKAI_PERSON_T add column PUBLICATIONS text;
-alter table SAKAI_PERSON_T add column BUSINESS_BIOGRAPHY text;
-
--- SAM-666
-alter table SAM_ASSESSFEEDBACK_T add column FEEDBACKCOMPONENTOPTION int(11) default null;
-update SAM_ASSESSFEEDBACK_T set FEEDBACKCOMPONENTOPTION = 2;
-alter table SAM_PUBLISHEDFEEDBACK_T add column FEEDBACKCOMPONENTOPTION int(11) default null;
-update SAM_PUBLISHEDFEEDBACK_T set FEEDBACKCOMPONENTOPTION = 2; 
-
--- SAM-971
-alter table SAM_ASSESSMENTGRADING_T add column LASTVISITEDPART integer default null;
-alter table SAM_ASSESSMENTGRADING_T add column LASTVISITEDQUESTION integer default null;
+-- SAK-XXXXX
+alter table EMAIL_TEMPLATE_ITEM modify HTMLMESSAGE LONGTEXT;
 
 -- SAK-19080 / GRBK-736 Gradebook2 support
-alter table GB_GRADE_RECORD_T add column USER_ENTERED_GRADE varchar(127);
+alter table GB_GRADE_RECORD_T add column USER_ENTERED_GRADE varchar(255) default null;
 
--- MSGCNTR-309 start and End dates on Forums and Topics
+-- MSGCNTR-309 start and end dates on Forums and Topics
 alter table MFR_AREA_T add column AVAILABILITY_RESTRICTED bit;
-update MFR_AREA_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_AREA_T modify column AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '';
+update MFR_AREA_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_AREA_T modify column AVAILABILITY_RESTRICTED bit not null default '';
 
 alter table MFR_AREA_T add column AVAILABILITY bit;
-update MFR_AREA_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_AREA_T modify column AVAILABILITY bit NOT NULL DEFAULT '';
+update MFR_AREA_T set AVAILABILITY=0 where AVAILABILITY is null;
+alter table MFR_AREA_T modify column AVAILABILITY bit not null default '';
 
 alter table MFR_AREA_T add column OPEN_DATE datetime;
 
 alter table MFR_AREA_T add column CLOSE_DATE datetime;
 
 alter table MFR_OPEN_FORUM_T add column AVAILABILITY_RESTRICTED bit;
-update MFR_OPEN_FORUM_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_OPEN_FORUM_T modify column AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '';
+update MFR_OPEN_FORUM_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_OPEN_FORUM_T modify column AVAILABILITY_RESTRICTED bit not null default '';
 
 alter table MFR_OPEN_FORUM_T add column AVAILABILITY bit;
-update MFR_OPEN_FORUM_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_OPEN_FORUM_T modify column AVAILABILITY bit NOT NULL DEFAULT '';
+update MFR_OPEN_FORUM_T set AVAILABILITY=0 where AVAILABILITY is null;
+alter table MFR_OPEN_FORUM_T modify column AVAILABILITY bit not null default '';
 
 alter table MFR_OPEN_FORUM_T add column OPEN_DATE datetime;
 
 alter table MFR_OPEN_FORUM_T add column CLOSE_DATE datetime;
 
 alter table MFR_TOPIC_T add column AVAILABILITY_RESTRICTED bit;
-update MFR_TOPIC_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_TOPIC_T modify column AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '';
+update MFR_TOPIC_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_TOPIC_T modify column AVAILABILITY_RESTRICTED bit not null default '';
 
 alter table MFR_TOPIC_T add column AVAILABILITY bit;
-update MFR_TOPIC_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_TOPIC_T modify column AVAILABILITY bit NOT NULL DEFAULT '';
+update MFR_TOPIC_T set AVAILABILITY=0 where AVAILABILITY is null;
+alter table MFR_TOPIC_T modify column AVAILABILITY bit not null default '';
 
 alter table MFR_TOPIC_T add column OPEN_DATE datetime null;
-
 alter table MFR_TOPIC_T add column CLOSE_DATE datetime null;
 
 -- MSGCNTR-355
 insert into MFR_TOPIC_T (UUID, MODERATED, AUTO_MARK_THREADS_READ, SORT_INDEX, MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID)
 
-    select UUID, MODERATED, AUTO_MARK_THREADS_READ, 3 as SORT_INDEX, 0 as MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID from (
-                    select count(*) as c1, uuid() as UUID, mtt.MODERATED, mtt.AUTO_MARK_THREADS_READ, mtt.TOPIC_DTYPE, 0 as VERSION, mtt.CREATED, mtt.CREATED_BY, mtt.MODIFIED, mtt.MODIFIED_BY, 'pvt_drafts' as TITLE, 'short-desc' as SHORT_DESCRIPTION, 'ext-desc' as EXTENDED_DESCRIPTION, mtt.TYPE_UUID, mtt.pf_surrogateKey, mtt.USER_ID
-                    from MFR_PRIVATE_FORUM_T mpft, MFR_TOPIC_T mtt
-                    where mpft.ID = mtt.pf_surrogateKey and mpft.TYPE_UUID = mtt.TYPE_UUID
-                    Group By mtt.USER_ID, mtt.pf_surrogateKey) s1
-    where s1.c1 = 3;
+select UUID, MODERATED, AUTO_MARK_THREADS_READ, 3 as SORT_INDEX, 0 as MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID from (
+    select count(*) as c1, uuid() as UUID, mtt.MODERATED, mtt.AUTO_MARK_THREADS_READ, mtt.TOPIC_DTYPE, 0 as VERSION, mtt.CREATED, mtt.CREATED_BY, mtt.MODIFIED, mtt.MODIFIED_BY, 'pvt_drafts' as TITLE, 'short-desc' as SHORT_DESCRIPTION, 'ext-desc' as EXTENDED_DESCRIPTION, mtt.TYPE_UUID, mtt.pf_surrogateKey, mtt.USER_ID
+    from MFR_PRIVATE_FORUM_T mpft, MFR_TOPIC_T mtt
+    where mpft.ID = mtt.pf_surrogateKey and mpft.TYPE_UUID = mtt.TYPE_UUID
+    Group By mtt.USER_ID, mtt.pf_surrogateKey) s1
+where s1.c1 = 3;
 
 -- MSGCNTR-360 Hibernate could have missed this index, if this fails, then the index may already be in the table
 create index user_type_context_idx on MFR_PVT_MSG_USR_T (USER_ID(36), TYPE_UUID(36), CONTEXT_ID(36), READ_STATUS);
-
--- SAK-18532/SAK-19522 new column for Email Template service
-alter table EMAIL_TEMPLATE_ITEM add column EMAILFROM text;
 
 -- SAK-18855
 alter table POLL_POLL add column POLL_IS_PUBLIC bit not null default false;
@@ -151,12 +125,16 @@ create index PROFILE_MESSAGE_PARTICIPANT_READ_I on PROFILE_MESSAGE_PARTICIPANTS_
 create table PROFILE_GALLERY_IMAGES_T (
 	ID bigint not null auto_increment,
 	USER_UUID varchar(99) not null,
-	RESOURCE_MAIN varchar(255) not null,
-	RESOURCE_THUMB varchar(255) not null,
+	RESOURCE_MAIN text not null,
+	RESOURCE_THUMB text not null,
 	DISPLAY_NAME varchar(255) not null,
 	primary key (ID)
 );
 create index PROFILE_GALLERY_IMAGES_USER_UUID_I on PROFILE_GALLERY_IMAGES_T (USER_UUID);
+
+-- Data type changes
+alter table PROFILE_IMAGES_T modify RESOURCE_MAIN text not null;
+alter table PROFILE_IMAGES_T modify RESOURCE_THUMB text not null;
 
 -- PRFL-252, PRFL-224 add social networking table
 create table PROFILE_SOCIAL_INFO_T (
@@ -186,45 +164,73 @@ create table PROFILE_KUDOS_T (
 );
 
 -- PRFL-152, PRFL-186 add the new email message preference columns, default to 0
-alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_NEW bit not null DEFAULT false;
-alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_REPLY bit not null DEFAULT false;
+alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_NEW bit not null default false;
+alter table PROFILE_PREFERENCES_T add EMAIL_MESSAGE_REPLY bit not null default false;
 
 -- PRFL-285 add social networking privacy column
-alter table PROFILE_PRIVACY_T add SOCIAL_NETWORKING_INFO int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add SOCIAL_NETWORKING_INFO int not null default 0;
 
 -- PRFL-171 add the new gallery column
-alter table PROFILE_PRIVACY_T add MY_PICTURES int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add MY_PICTURES int not null default 0;
 
 -- PRFL-194 add the new messages column
-alter table PROFILE_PRIVACY_T add MESSAGES int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add MESSAGES int not null default 0;
 
 -- PRFL-210 add the new businessInfo column
-alter table PROFILE_PRIVACY_T add BUSINESS_INFO int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add BUSINESS_INFO int not null default 0;
 
 -- PRFL-267 add the new staff and student info columns 
 -- and copy old ACADEMIC_INFO value into them to maintain privacy
-alter table PROFILE_PRIVACY_T add STAFF_INFO int not null DEFAULT 0;
-alter table PROFILE_PRIVACY_T add STUDENT_INFO int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add STAFF_INFO int not null default 0;
+alter table PROFILE_PRIVACY_T add STUDENT_INFO int not null default 0;
 update PROFILE_PRIVACY_T set STAFF_INFO = ACADEMIC_INFO;
 update PROFILE_PRIVACY_T set STUDENT_INFO = ACADEMIC_INFO;
 alter table PROFILE_PRIVACY_T drop ACADEMIC_INFO;
 
 -- PRFL-90 add the new useOfficialImage column
-alter table PROFILE_PREFERENCES_T add USE_OFFICIAL_IMAGE bit not null DEFAULT false;
+alter table PROFILE_PREFERENCES_T add USE_OFFICIAL_IMAGE bit not null default false;
 
 -- PRFL-293 remove search privacy setting
 alter table PROFILE_PRIVACY_T drop SEARCH;
 
 -- PRFL-336 add kudos preference
-alter table PROFILE_PREFERENCES_T add SHOW_KUDOS bit not null DEFAULT true;
+alter table PROFILE_PREFERENCES_T add SHOW_KUDOS bit not null default true;
 
 -- PRFL-336 add kudos privacy
-alter table PROFILE_PRIVACY_T add MY_KUDOS int not null DEFAULT 0;
+alter table PROFILE_PRIVACY_T add MY_KUDOS int not null default 0;
 
 -- PRFL-382 add gallery feed preference
-alter table PROFILE_PREFERENCES_T add SHOW_GALLERY_FEED bit not null DEFAULT true;
+alter table PROFILE_PREFERENCES_T add SHOW_GALLERY_FEED bit not null default true;
 
 -- Profile2 1.3-1.4 upgrade end
+
+-- SAK-18864, SAK-19951 adds missing scheduler_trigger_events table for new persistent jobscheduler event feature
+create table scheduler_trigger_events (
+    uuid varchar(36) not null, 
+    type varchar(255) not null, 
+    jobName varchar(255) not null, 
+    triggerName varchar(255) default null, 
+    time datetime not null, 
+    message text,
+    primary key (uuid)
+);
+
+-- SAK-17821 Add additional fields to SakaiPerson
+alter table SAKAI_PERSON_T add column STAFF_PROFILE text;
+alter table SAKAI_PERSON_T add column UNIVERSITY_PROFILE_URL text;
+alter table SAKAI_PERSON_T add column ACADEMIC_PROFILE_URL text;
+alter table SAKAI_PERSON_T add column PUBLICATIONS text;
+alter table SAKAI_PERSON_T add column BUSINESS_BIOGRAPHY text;
+
+-- SAM-666
+alter table SAM_ASSESSFEEDBACK_T add column FEEDBACKCOMPONENTOPTION int(11) default null;
+update SAM_ASSESSFEEDBACK_T set FEEDBACKCOMPONENTOPTION = 2;
+alter table SAM_PUBLISHEDFEEDBACK_T add column FEEDBACKCOMPONENTOPTION int(11) default null;
+update SAM_PUBLISHEDFEEDBACK_T set FEEDBACKCOMPONENTOPTION = 2; 
+
+-- SAM-971
+alter table SAM_ASSESSMENTGRADING_T add column LASTVISITEDPART integer default null;
+alter table SAM_ASSESSMENTGRADING_T add column LASTVISITEDQUESTION integer default null;
 
 -- ShortenedUrlService 1.0.0 db creation start
 
@@ -242,39 +248,39 @@ create index KEY_INDEX on URL_RANDOMISED_MAPPINGS_T (TINY);
 
 -- SAK-XXXX table structure for sakai_message_bundle
 create table SAKAI_MESSAGE_BUNDLE (
-  ID bigint(20) not null auto_increment,
-  MODULE_NAME varchar(255) not null,
-  BASENAME varchar(255) not null,
-  PROP_NAME varchar(255) not null,
-  PROP_VALUE text,
-  LOCALE varchar(255) not null,
-  DEFAULT_VALUE text not null,
-  primary key (ID)
+    ID bigint(20) not null auto_increment,
+    MODULE_NAME varchar(255) not null,
+    BASENAME varchar(255) not null,
+    PROP_NAME varchar(255) not null,
+    PROP_VALUE text,
+    LOCALE varchar(255) not null,
+    DEFAULT_VALUE text not null,
+    primary key (ID)
 );
 
 -- STAT-XXXX table structure for sst_presences
 create table SST_PRESENCES (
-  ID bigint(20) not null auto_increment,
-  SITE_ID varchar(99) not null,
-  USER_ID varchar(99) not null,
-  DATE date not null,
-  DURATION bigint(20) not null default '0',
-  LAST_VISIT_START_TIME datetime default null,
-  primary key (ID)
+    ID bigint(20) not null auto_increment,
+    SITE_ID varchar(99) not null,
+    USER_ID varchar(99) not null,
+    DATE date not null,
+    DURATION bigint(20) not null default '0',
+    LAST_VISIT_START_TIME datetime default null,
+    primary key (ID)
 );
 
 --  Table structure for validationaccount_item
 create table VALIDATIONACCOUNT_ITEM (
-  id bigint(20) not null auto_increment,
-  USER_ID varchar(255) not null,
-  VALIDATION_TOKEN varchar(255) not null,
-  VALIDATION_SENT datetime default null,
-  VALIDATION_RECEIVED datetime default null,
-  VALIDATIONS_SENT int(11) default null,
-  STATUS int(11) default null,
-  FIRST_NAME varchar(255) not null,
-  SURNAME varchar(255) not null,
-  ACCOUNT_STATUS int(11) default null,
-  PRIMARY KEY (id)
+    id bigint(20) not null auto_increment,
+    USER_ID varchar(255) not null,
+    VALIDATION_TOKEN varchar(255) not null,
+    VALIDATION_SENT datetime default null,
+    VALIDATION_RECEIVED datetime default null,
+    VALIDATIONS_SENT int(11) default null,
+    STATUS int(11) default null,
+    FIRST_NAME varchar(255) not null,
+    SURNAME varchar(255) not null,
+    ACCOUNT_STATUS int(11) default null,
+    PRIMARY KEY (id)
 );
 
