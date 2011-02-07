d50779bf6486de2a6487e922da2dd984465e9044
Anthony Whyte <arwhyte@umich.edu>
2011-02-07 18:29:18 +0000

    SAK-19894, SAK-8005 add comment -- SAK-8005
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@88075 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0_mysql_conversion.sql
@@ -10,14 +10,12 @@
 -- SQL statement
 -- --------------------------------------------------------------------------------------------------------------------------------------
 
--- SAK-XXXXX
+-- SAK-8005
 alter table ANNOUNCEMENT_MESSAGE add column MESSAGE_ORDER int(11) default null;
 
--- SAK-XXXXX
 drop index IE_ANNC_MSG_ATTRIB on ANNOUNCEMENT_MESSAGE;
 create index IE_ANNC_MSG_ATTRIB on ANNOUNCEMENT_MESSAGE (DRAFT, PUBVIEW, OWNER, MESSAGE_ORDER);
 
--- SAK-XXXXX
 drop index ANNOUNCEMENT_MESSAGE_CDD on ANNOUNCEMENT_MESSAGE;
 create index ANNOUNCEMENT_MESSAGE_CDD on ANNOUNCEMENT_MESSAGE (CHANNEL_ID, MESSAGE_DATE, MESSAGE_ORDER, DRAFT);
 
