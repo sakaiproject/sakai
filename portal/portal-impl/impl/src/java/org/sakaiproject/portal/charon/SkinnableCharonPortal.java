26b9e0b9383ae7d6d3e9dc4eb1095846572890e8
Matthew Jones <jonespm@gmail.com>
2011-03-14 15:58:03 +0000

    Committed SAK-17858 : Portal - Directtool url does not work when user is not logged in.
    git-svn-id: https://source.sakaiproject.org/svn/portal/trunk@89792 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -1306,8 +1306,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			// punt
 			if (session.getUserId() == null)
 			{
-				doLogin(req, res, session, req.getPathInfo() + "?sakai.site="
-						+ res.encodeURL(siteId), false);
+				doLogin(req, res, session, req.getPathInfo(), false);
 				return null;
 			}
 			return placementId; // cannot resolve placement
