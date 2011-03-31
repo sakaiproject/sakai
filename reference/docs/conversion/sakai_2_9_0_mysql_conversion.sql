268f58b5d61db7fe3b7a89688276810bda5a1f5a
David Horwitz <dhorwitz@gmail.com>
2011-03-31 10:36:47 +0000

    KNL-705 add new conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@90986 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_9_0_mysql_conversion.sql
@@ -1,2 +1,8 @@
 -- KNL-576 provider_id field is too small for large site with long list of provider id
 alter table SAKAI_REALM modify PROVIDER_ID varchar(4000);
+
+
+-- KNL-705 new soft deletion of sites
+alter table SAKAI_SITE add IS_SOFTLY_DELETED char(1) not null DEFAULT 0;
+alter table SAKAI_SITE add SOFTLY_DELETED_DATE datetime;
+
