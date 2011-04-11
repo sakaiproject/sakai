d69ead9722c50759fcb32d830742f91b01326471
Steve Swinsburg <steve.swinsburg@gmail.com>
2011-04-11 01:18:15 +0000

    SAK-20430 create 2.7.x upgrade scripts and add profile2 requirements
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@91508 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ reference/docs/conversion/sakai_2_7_1-2_7_2_oracle_conversion.sql
@@ -0,0 +1,18 @@
+-- This is the Oracle Sakai 2.7.1 -> 2.7.2 conversion script
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- 
+-- use this to convert a Sakai database from 2.7.1 to 2.7.2.  Run this before you run your first app server.
+-- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
+--
+-- Script insertion format
+-- -- [TICKET] [short comment]
+-- -- [comment continued] (repeat as necessary)
+-- SQL statement
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
+/* PRFL-392 change row size of image URI columns */
+alter table PROFILE_IMAGES_T modify RESOURCE_MAIN varchar2(4000);
+alter table PROFILE_IMAGES_T modify RESOURCE_THUMB varchar2(4000);
+
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_MAIN varchar2(4000);
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_THUMB varchar2(4000);
