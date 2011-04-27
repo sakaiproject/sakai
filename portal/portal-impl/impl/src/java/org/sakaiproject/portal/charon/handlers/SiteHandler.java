b634a63f1736e04e54de8ba5bbc2ec875666fec7
Gonzalo Silverio <gsilver@umich.edu>
2011-04-27 15:27:09 +0000

    SAK-20503 - make more sites search conditional on more sites number
    git-svn-id: https://source.sakaiproject.org/svn/portal/branches/SAK-19813@92350 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
@@ -83,6 +83,8 @@ public class SiteHandler extends WorksiteHandler
 	private int configuredTabsToDisplay = 5;
 
 	private boolean useDHTMLMore = false;
+	
+	private int showSearchWhen = 2; 
 
 	// When these strings appear in the URL they will be replaced by a calculated value based on the context.
 	// This can be replaced by the users myworkspace.
@@ -97,6 +99,8 @@ public class SiteHandler extends WorksiteHandler
 				Portal.CONFIG_DEFAULT_TABS, 5);
 		useDHTMLMore = Boolean.valueOf(ServerConfigurationService.getBoolean(
 				"portal.use.dhtml.more", false));
+		showSearchWhen = Integer.valueOf(ServerConfigurationService.getInt(
+				"portal.show.search.when", 2));
         mutableSitename =  ServerConfigurationService.getString("portal.mutable.sitename", "-");
         mutablePagename =  ServerConfigurationService.getString("portal.mutable.pagename", "-");
 	}
@@ -647,6 +651,7 @@ public class SiteHandler extends WorksiteHandler
 			}
 
 			rcontext.put("useDHTMLMore", useDHTMLMore);
+			rcontext.put("showSearchWhen", showSearchWhen);
 			if (useDHTMLMore)
 			{
 				SiteView siteView = portal.getSiteHelper().getSitesView(
