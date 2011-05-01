f47c51a3d90ba99f95ef9cb6cda9eda41640c36e
David Horwitz <dhorwitz@gmail.com>
2011-05-01 12:51:02 +0000

    SAK-20525 a button item should extend CommandButton not CommandLink
    git-svn-id: https://source.sakaiproject.org/svn/jsf/trunk@92466 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- jsf/jsf-widgets/src/java/org/sakaiproject/jsf/component/ButtonBarItemComponent.java
+++ jsf/jsf-widgets/src/java/org/sakaiproject/jsf/component/ButtonBarItemComponent.java
@@ -11,6 +11,10 @@
  *
  *       http://www.osedu.org/licenses/ECL-2.0
  *
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,10 +26,10 @@
 
 package org.sakaiproject.jsf.component;
 
-import javax.faces.component.html.HtmlCommandLink;
+import javax.faces.component.html.HtmlCommandButton;
 
 
-public class ButtonBarItemComponent extends HtmlCommandLink
+public class ButtonBarItemComponent extends HtmlCommandButton
 {
   
   public ButtonBarItemComponent()
