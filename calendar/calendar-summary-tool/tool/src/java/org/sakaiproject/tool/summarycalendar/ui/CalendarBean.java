cf16aca448a7ff8aff6ab6157315943476ab62d8
Aaron Zeckoski <azeckoski@vt.edu>
2011-05-17 13:08:27 +0000

    SAK-20352 Replace use of Sakai StringUtil with apache commons-lang StringUtils
    git-svn-id: https://source.sakaiproject.org/svn/calendar/trunk@92894 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- calendar/calendar-summary-tool/tool/src/java/org/sakaiproject/tool/summarycalendar/ui/CalendarBean.java
+++ calendar/calendar-summary-tool/tool/src/java/org/sakaiproject/tool/summarycalendar/ui/CalendarBean.java
@@ -39,6 +39,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
