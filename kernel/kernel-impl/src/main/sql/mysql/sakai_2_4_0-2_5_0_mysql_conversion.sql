34ef034684960220fb6b99c3ed59d588290bbb1b
David Horwitz <dhorwitz@gmail.com>
2011-05-23 11:41:17 +0000

    KNL-735
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@93148 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/sql/mysql/sakai_2_4_0-2_5_0_mysql_conversion.sql
+++
@@ -1,8 +0,0 @@
--- SAK-8780, SAK-7452 - Add SESSION_ACTIVE flag to explicitly indicate when
--- a session is active rather than relying on SESSION_START and SESSION_END
--- having the same value.
-
--- It is important that the new column have a null value by default,
--- since that allows for an extremely efficient Oracle index.
-alter table SAKAI_SESSION add column SESSION_ACTIVE tinyint(1);
-create index SESSION_ACTIVE_IE on SAKAI_SESSION (SESSION_ACTIVE);
