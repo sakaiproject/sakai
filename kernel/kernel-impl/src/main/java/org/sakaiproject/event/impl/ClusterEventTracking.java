32ef97210de9c4d2f0fa3d563398e163153ca8f8
Aaron Zeckoski <azeckoski@vt.edu>
2011-05-23 14:36:09 +0000

    KNL-587 fixed up the sessions check so there will be no failure message and it will run a lot faster in mysql (almost instantly)
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@93155 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/event/impl/ClusterEventTracking.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/event/impl/ClusterEventTracking.java
@@ -281,7 +281,7 @@ public abstract class ClusterEventTracking extends BaseEventTrackingService impl
                     try {
                         m_totalEventsCount = result.getLong(1);
                     } catch (SQLException ignore) {
-                        M_log.warn("Could not get count of events table using SQL (" + eventCountStmt + "): " + ignore);
+                        M_log.info("Could not get count of events table using SQL (" + eventCountStmt + ")");
                     }
                     return Long.valueOf(m_totalEventsCount);
                 }
@@ -308,7 +308,7 @@ public abstract class ClusterEventTracking extends BaseEventTrackingService impl
                     try {
                         m_totalSessionsCount = result.getLong(1);
                     } catch (SQLException ignore) {
-                        M_log.warn("Could not get count of sessions table using SQL (" + sessionCountStmt + "): " + ignore);
+                        M_log.info("Could not get count of sessions table using SQL (" + sessionCountStmt + ")");
                     }
                     return Long.valueOf(m_totalSessionsCount);
                 }
