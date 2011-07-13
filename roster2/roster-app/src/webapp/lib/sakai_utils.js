0f3cccb5a6b6787acf652a118fb128962eb3e41f
Daniel Robinson <d.b.robinson@lancaster.ac.uk>
2011-07-13 15:06:25 +0000

    RSTR-37: need to check for quotes; sometimes they are there
    git-svn-id: https://source.sakaiproject.org/svn/roster2/trunk@168520 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- roster2/roster-app/src/webapp/lib/sakai_utils.js
+++ roster2/roster-app/src/webapp/lib/sakai_utils.js
@@ -88,7 +88,9 @@ var SakaiUtils;
         	var c = ca[i];
         	while (c.charAt(0)==' ') c = c.substring(1,c.length);
         	if (c.indexOf(nameEQ) == 0) {
-        		return c.substring(nameEQ.length + 1,c.length - 1);
+        		var start = c.charAt(nameEQ.length) == "\"" ? nameEQ.length + 1 : nameEQ.length;
+        		var end = c.charAt(c.length - 1) == "\"" ? c.length - 1 : c.length;        		
+        		return c.substring(start,end);
         	}
     	}
     	return null;
