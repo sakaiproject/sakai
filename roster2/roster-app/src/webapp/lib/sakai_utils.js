9d6e2dcebbf1a4cc68989c4bf08bf92a27c142c3
Daniel Robinson <d.b.robinson@lancaster.ac.uk>
2011-07-13 12:00:57 +0000

    RSTR-37: cookie string has quotes around it so alter bounds
    git-svn-id: https://source.sakaiproject.org/svn/roster2/trunk@168518 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- roster2/roster-app/src/webapp/lib/sakai_utils.js
+++ roster2/roster-app/src/webapp/lib/sakai_utils.js
@@ -87,7 +87,9 @@ var SakaiUtils;
     	for(var i=0;i < ca.length;i++) {
         	var c = ca[i];
         	while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        	if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        	if (c.indexOf(nameEQ) == 0) {
+        		return c.substring(nameEQ.length + 1,c.length - 1);
+        	}
     	}
     	return null;
 	}
