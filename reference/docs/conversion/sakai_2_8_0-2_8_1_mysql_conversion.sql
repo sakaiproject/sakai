3db764db2b669f1161a46d31235d02bb8ac1ee0a
Steve Swinsburg <steve.swinsburg@gmail.com>
2011-07-27 05:18:49 +0000

    SAK-20926 fix nulls
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@95702 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
@@ -7,3 +7,9 @@ alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME TIMESTAMP;
 
 -- SAK-20717 mailarchive messages need updating with new field
 UPDATE mailarchive_message SET xml = REPLACE(XML, ' mail-from="', ' message_order="1" mail-from="') WHERE xml NOT LIKE '% message_order="1" %';
+
+-- SAK-20926 / PRFL-392 fix null status
+alter table PROFILE_IMAGES_T modify RESOURCE_MAIN text not null;
+alter table PROFILE_IMAGES_T modify RESOURCE_THUMB text not null;
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_MAIN text not null;
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_THUMB text;
--- reference/docs/conversion/sakai_2_8_0-2_8_1_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0-2_8_1_oracle_conversion.sql
@@ -15,3 +15,9 @@ ALTER TABLE SAKAI_CLUSTER MODIFY (UPDATE_TIME TIMESTAMP WITH TIME ZONE);
 
 -- SAK-20717 mailarchive messages need updating with new field
 UPDATE mailarchive_message SET xml = REPLACE(XML, ' mail-from="', ' message_order="1" mail-from="') WHERE xml NOT LIKE '% message_order="1" %';
+
+-- SAK-20926 / PRFL-392 fix null status
+alter table PROFILE_IMAGES_T modify RESOURCE_MAIN varchar2(4000) not null;
+alter table PROFILE_IMAGES_T modify RESOURCE_THUMB varchar2(4000) not null;
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_MAIN varchar2(4000) not null;
+alter table PROFILE_IMAGES_EXTERNAL_T modify URL_THUMB varchar2(4000);
