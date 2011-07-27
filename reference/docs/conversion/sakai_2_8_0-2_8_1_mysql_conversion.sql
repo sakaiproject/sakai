a9a7f2691a25c463a4ebe31f52ac10ab11eea7f5
Anthony Whyte <arwhyte@umich.edu>
2011-07-27 20:21:17 +0000

    SAK-20934
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@95795 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
@@ -1,12 +1,24 @@
+-- This is the MYSQL Sakai 2.8.0 -> 2.8.1 conversion script
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- 
+-- use this to convert a Sakai database from 2.8.0 to 2.8.1.  Run this before you run your first app server.
+-- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
+--
+-- Script insertion format
+-- -- [TICKET] [short comment]
+-- -- [comment continued] (repeat as necessary)
+-- SQL statement
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
 -- SAK-8005/SAK-20560
--- -- The conversion for SAK-8005 in 2.8.0 conversion do not handle the message_order data in the xml clob
-update ANNOUNCEMENT_MESSAGE set MESSAGE_ORDER='1', XML=REPLACE(XML, ' subject=', ' message_order="1" subject=') WHERE MESSAGE_ORDER IS NULL; 
+-- The conversion for SAK-8005 in the 2.8.0 conversion script did not handle the message_order data in the xml clob
+update ANNOUNCEMENT_MESSAGE set MESSAGE_ORDER='1', XML=replace(XML, ' subject=', ' message_order="1" subject=') where MESSAGE_ORDER is null; 
 
 -- KNL-725 use a column type that stores the timezone
-alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME TIMESTAMP;
+alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME timestamp;
 
 -- SAK-20717 mailarchive messages need updating with new field
-UPDATE mailarchive_message SET xml = REPLACE(XML, ' mail-from="', ' message_order="1" mail-from="') WHERE xml NOT LIKE '% message_order="1" %';
+update MAILARCHIVE_MESSAGE set XML=replace(XML, ' mail-from="', ' message_order="1" mail-from="') where XML not like '% message_order="1" %';
 
 -- SAK-20926 / PRFL-392 fix null status
 alter table PROFILE_IMAGES_T modify RESOURCE_MAIN text not null;
