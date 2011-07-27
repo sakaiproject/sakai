90000a01884388ada4559f514d2c0bcc5608932e
Steve Swinsburg <steve.swinsburg@gmail.com>
2011-07-27 02:28:04 +0000

    SAK-20924 add missing SQL for upgrade of profile2 that people may not have already run when upgrading profile2
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@95697 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
@@ -7,3 +7,18 @@ alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME TIMESTAMP;
 
 -- SAK-20717 mailarchive messages need updating with new field
 UPDATE mailarchive_message SET xml = REPLACE(XML, ' mail-from="', ' message_order="1" mail-from="') WHERE xml NOT LIKE '% message_order="1" %';
+
+-- PRFL-94 remove twitter from preferences 
+-- this was part of the Profile2  1.3.x-1.3.9 upgrade so you may have already run it.
+alter table PROFILE_PREFERENCES_T drop TWITTER_ENABLED;
+alter table PROFILE_PREFERENCES_T drop TWITTER_USERNAME;
+alter table PROFILE_PREFERENCES_T drop TWITTER_PASSWORD;
+
+-- PRFL-94 add external integration table
+-- this was part of the Profile2 1.3.x-1.3.9 upgrade so you may have already run it. There is a check in place.
+create table if not exists PROFILE_EXTERNAL_INTEGRATION_T (
+	USER_UUID varchar(99) not null,
+	TWITTER_TOKEN varchar(255),
+	TWITTER_SECRET varchar(255),
+	primary key (USER_UUID)
+);
