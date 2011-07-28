08c437eaab7f2010e917905211913f7c63cf3927
Anthony Whyte <arwhyte@umich.edu>
2011-07-28 15:42:25 +0000

    SAK-20939
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@95824 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_9_0_mysql_conversion.sql
@@ -1,37 +1,40 @@
+-- This is the MYSQL Sakai 2.9.0 conversion script
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- 
+-- use this to convert a Sakai database from 2.8.x to 2.9.0.  Run this before you run your first app server.
+-- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
+--
+-- Script insertion format
+-- -- [TICKET] [short comment]
+-- -- [comment continued] (repeat as necessary)
+-- SQL statement
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
 -- KNL-576 provider_id field is too small for large site with long list of provider id
 alter table SAKAI_REALM modify PROVIDER_ID varchar(4000);
 
-
 -- KNL-705 new soft deletion of sites
-alter table SAKAI_SITE add IS_SOFTLY_DELETED char(1) not null DEFAULT 0;
+alter table SAKAI_SITE add IS_SOFTLY_DELETED char(1) not null default 0;
 alter table SAKAI_SITE add SOFTLY_DELETED_DATE datetime;
 
 -- KNL-725 use a column type that stores the timezone
-alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME TIMESTAMP;
+alter table SAKAI_CLUSTER change UPDATE_TIME UPDATE_TIME timestamp;
 
 -- KNL-734 type of session and event date column
-alter table SAKAI_SESSION change SESSION_START SESSION_START TIMESTAMP;
-alter table SAKAI_SESSION change SESSION_END SESSION_END TIMESTAMP;
-alter table SAKAI_EVENT change EVENT_DATE EVENT_DATE TIMESTAMP;
-
+alter table SAKAI_SESSION change SESSION_START SESSION_START timestamp;
+alter table SAKAI_SESSION change SESSION_END SESSION_END timestamp;
+alter table SAKAI_EVENT change EVENT_DATE EVENT_DATE timestamp;
 
 --SAK-19964 Gradebook drop highest and/or lowest or keep highest score for a student
-ALTER TABLE GB_CATEGORY_T
-ADD COLUMN DROP_HIGHEST int(11) NULL;
+alter table GB_CATEGORY_T add column DROP_HIGHEST int(11) null;
+update GB_CATEGORY_T set DROP_HIGHEST = 0;
 
-Update GB_CATEGORY_T
-Set DROP_HIGHEST = 0;
-
-
-ALTER TABLE GB_CATEGORY_T
-ADD COLUMN KEEP_HIGHEST int(11) NULL;
-
-Update GB_CATEGORY_T
-Set KEEP_HIGHEST = 0;
+alter table GB_CATEGORY_T add column KEEP_HIGHEST int(11) null;
+update GB_CATEGORY_T set KEEP_HIGHEST = 0;
 
 --SAK-19731 - Add ability to hide columns in All Grades View for instructors
 alter table GB_GRADABLE_OBJECT_T add column (HIDE_IN_ALL_GRADES_TABLE bit default false);
-update GB_GRADABLE_OBJECT_T set HIDE_IN_ALL_GRADES_TABLE=0 where HIDE_IN_ALL_GRADES_TABLE is NULL;
+update GB_GRADABLE_OBJECT_T set HIDE_IN_ALL_GRADES_TABLE = 0 where HIDE_IN_ALL_GRADES_TABLE is null;
 
 -- SAK-20598 change column type to mediumtext
 alter table SAKAI_PERSON_T change NOTES NOTES mediumtext null;
