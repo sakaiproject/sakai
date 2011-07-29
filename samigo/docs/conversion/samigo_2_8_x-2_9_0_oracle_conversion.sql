c3cebf9775c8ed39fd80d6bf3bbd3e29445f0c46
Karen Tsao <ktsao@stanford.edu>
2011-07-29 22:33:04 +0000

    SAM-939: Implement survey question type to support rating items against the same set of answers
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@95936 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/docs/conversion/samigo_2_8_x-2_9_0_oracle_conversion.sql
+++ samigo/docs/conversion/samigo_2_8_x-2_9_0_oracle_conversion.sql
@@ -8,3 +8,10 @@ alter table SAM_PUBLISHEDANSWER_T add (TEMP_CLOB_TEXT clob);
 update SAM_PUBLISHEDANSWER_T SET TEMP_CLOB_TEXT = TEXT;
 alter table SAM_PUBLISHEDANSWER_T drop column TEXT;
 alter table SAM_PUBLISHEDANSWER_T rename column TEMP_CLOB_TEXT to TEXT;
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (13 , 'stanford.edu' ,'assessment.item' ,'Matrix Choices Survey' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);   
