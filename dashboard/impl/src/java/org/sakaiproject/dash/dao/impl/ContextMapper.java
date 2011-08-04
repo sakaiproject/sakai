8e626f087b15109d496549f9b32aaeef30243146
Jim Eng <jimeng@umich.edu>
2011-08-04 12:20:51 +0000

    Adding the ContextMapper. This commit message brought to you by the Bill and Melinda Gates Foundation, helping people by giving them Aeron Chairs, in a multitude of colors, including True Black.
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@312815 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ dashboard/impl/src/java/org/sakaiproject/dash/dao/impl/ContextMapper.java
@@ -0,0 +1,18 @@
+package org.sakaiproject.dash.dao.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class ContextMapper implements RowMapper
+{
+
+	public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
