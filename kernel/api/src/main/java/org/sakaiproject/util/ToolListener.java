4d13a6ffe22bb890266a222439d2e951c82d7f7b
Matthew Buckett <buckett@bumph.org>
2011-08-16 15:01:53 +0000

    KNL-787 Improve JavaDoc and logging on ToolListener.
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@97042 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/api/src/main/java/org/sakaiproject/util/ToolListener.java
+++ kernel/api/src/main/java/org/sakaiproject/util/ToolListener.java
@@ -42,6 +42,14 @@ import org.sakaiproject.tool.cover.ActiveToolManager;
  * have have a name of the form [toolId][_][localCode].properties.  This format allows
  * tool litles (etc) to be localized even when multiple tool registrations are included.
  * </p>
+ * This listener can be added to the web.xml file with a snippet like this:
+ * <p>
+ * <code>
+ * &lt;listener&gt;<br/>
+ * &nbsp;&lt;listener-class&gt;org.sakaiproject.util.ToolListener&lt;/listener-class&gt;<br/>
+ * &lt;/listener&gt;<br/>
+ * </code>
+ * </p>
  */
 public class ToolListener implements ServletContextListener
 {
@@ -61,6 +69,7 @@ public class ToolListener implements ServletContextListener
 		if (paths == null) {
 			return;
 		}
+		int registered = 0;
 		for (Iterator i = paths.iterator(); i.hasNext();)
 		{
 			final String path = (String) i.next();
@@ -79,7 +88,14 @@ public class ToolListener implements ServletContextListener
 					M_log.info("registering tools from resource: " + path);
 					ActiveToolManager.register(event.getServletContext().getResourceAsStream(path), event.getServletContext());
 				}
+				registered++;
 			}
+		}
+		
+		if (registered == 0)
+		{
+			// Probably misconfigured as we should have at least one registered.
+			M_log.warn("No tools found to be registered.");
 		}
 
 		//	Second pass, search for message bundles.  Two passes are necessary to make sure the tool is registered first.
