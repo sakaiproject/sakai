fa02316f85f2113185e29c53207d37f163ecd473
Matthew Buckett <buckett@bumph.org>
2011-08-16 16:05:09 +0000

    KNL-788 Add notes about how this differs from URLEncode.encode()
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@97044 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-util/src/main/java/org/sakaiproject/util/Validator.java
+++ kernel/kernel-util/src/main/java/org/sakaiproject/util/Validator.java
@@ -54,27 +54,27 @@ public class Validator
 	/** These characters are not allowed in a site skin */
 	protected static final String INVALID_CHARS_IN_SITE_SKIN = " $&':<>[]{}#%@/;=?\\^|~\"";
 	
-	protected static final String MAP_TO_A = "‰ŠˆŒ€‡‡";
+	protected static final String MAP_TO_A = "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_B = "§§";
+	protected static final String MAP_TO_B = "ï¿½ï¿½";
 
-	protected static final String MAP_TO_C = "‚¢¢";
+	protected static final String MAP_TO_C = "ï¿½ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_E = "‘ƒ¾®®";
+	protected static final String MAP_TO_E = "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_I = "•”“’’";
+	protected static final String MAP_TO_I = "ï¿½ï¿½ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_L = "££";
+	protected static final String MAP_TO_L = "ï¿½ï¿½";
 
-	protected static final String MAP_TO_N = "–„„";
+	protected static final String MAP_TO_N = "ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_O = "™š˜…——";
+	protected static final String MAP_TO_O = "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½";
 
-	protected static final String MAP_TO_U = "Ÿ†œœ";
+	protected static final String MAP_TO_U = "ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½";
 
 	protected static final String MAP_TO_Y = "Ø´??";
 
-	protected static final String MAP_TO_X = "???¤©»¨±?«µ¦À?";
+	protected static final String MAP_TO_X = "???ï¿½ï¿½ï¿½ï¿½ï¿½?ï¿½ï¿½ï¿½ï¿½?";
 
 	/**
 	 * These characters are allowed; but if escapeResourceName() is called, they are escaped (actually, removed) Certain characters cause problems with filenames in certain OSes - so get rid of these characters in filenames
@@ -173,7 +173,9 @@ public class Validator
 
 	/**
 	 * Return a string based on id that is fully escaped using URL rules, using a UTF-8 underlying encoding.
-	 *
+	 * One reason for this existing is that the standard URLEncoder in Java will encode slashes ('/') but this method doesn't.
+	 * Also watch out as it trims trailing spaces, and other character get lost here too.
+	 * 
 	 * Note: java.net.URLEncode.encode() provides a more standard option
 	 *       FormattedText.decodeNumericCharacterReferences() undoes this op
 	 * 
@@ -202,6 +204,9 @@ public class Validator
 					buf.append(toHex(b));
 					buf.append('^');
 				}
+				// 0x1F is the last control character
+				// 0x7F is DEL chatecter
+				// 0x80 is the start of the top of the 256bit set.
 				else if ((ESCAPE_URL.indexOf((char) b) != -1) || (b <= 0x1F) || (b == 0x7F) || (b >= 0x80))
 				{
 					buf.append("%");
