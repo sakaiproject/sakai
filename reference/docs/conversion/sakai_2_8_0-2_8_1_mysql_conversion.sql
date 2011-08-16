4dbe1018da4e41359044c279bd58f0e2a346ba2a
Anthony Whyte <arwhyte@umich.edu>
2011-08-16 03:07:31 +0000

    SAK-20598 merge trunk r95577
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@97023 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_0-2_8_1_mysql_conversion.sql
@@ -29,3 +29,18 @@ alter table PROFILE_IMAGES_T modify RESOURCE_MAIN text not null;
 alter table PROFILE_IMAGES_T modify RESOURCE_THUMB text not null;
 alter table PROFILE_IMAGES_EXTERNAL_T modify URL_MAIN text not null;
 alter table PROFILE_IMAGES_EXTERNAL_T modify URL_THUMB text;
+
+-- SAK-20598 change column type to mediumtext
+alter table SAKAI_PERSON_T change NOTES NOTES mediumtext null;
+alter table SAKAI_PERSON_T change FAVOURITE_BOOKS FAVOURITE_BOOKS mediumtext null;
+alter table SAKAI_PERSON_T change FAVOURITE_TV_SHOWS FAVOURITE_TV_SHOWS mediumtext null;
+alter table SAKAI_PERSON_T change FAVOURITE_MOVIES FAVOURITE_MOVIES mediumtext null;
+alter table SAKAI_PERSON_T change FAVOURITE_QUOTES FAVOURITE_QUOTES mediumtext null;
+alter table SAKAI_PERSON_T change EDUCATION_COURSE EDUCATION_COURSE mediumtext null;
+alter table SAKAI_PERSON_T change EDUCATION_SUBJECTS EDUCATION_SUBJECTS mediumtext null;
+alter table SAKAI_PERSON_T change STAFF_PROFILE STAFF_PROFILE mediumtext null;
+alter table SAKAI_PERSON_T change UNIVERSITY_PROFILE_URL UNIVERSITY_PROFILE_URL mediumtext null;
+alter table SAKAI_PERSON_T change ACADEMIC_PROFILE_URL ACADEMIC_PROFILE_URL mediumtext null;
+alter table SAKAI_PERSON_T change PUBLICATIONS PUBLICATIONS mediumtext null;
+alter table SAKAI_PERSON_T change BUSINESS_BIOGRAPHY BUSINESS_BIOGRAPHY mediumtext null;
+-- end SAK-20598
