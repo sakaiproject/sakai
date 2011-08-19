fcf0196cb9ce8951a293f4f4824064c7b0fb87e9
Michelle Wagner <wagnermr@iu.edu>
2011-08-19 18:18:29 +0000

    MSGCNTR-561 2.7.x - 2.8.x mysql conversion script issues
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@97226 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-hbm/src/sql/mysql/mfr_conversion_27x-28x.sql
+++ msgcntr/messageforums-hbm/src/sql/mysql/mfr_conversion_27x-28x.sql
@@ -1,42 +1,42 @@
 --MSGCNTR-309
 --Start and End dates on Forums and Topics
 
-alter table MFR_AREA_T add (AVAILABILITY_RESTRICTED bit);
-update MFR_AREA_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_AREA_T modify (AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '0');
+alter table MFR_AREA_T add column AVAILABILITY_RESTRICTED bit;
+update MFR_AREA_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_AREA_T modify column AVAILABILITY_RESTRICTED bit not null default false;
 
-alter table MFR_AREA_T add (AVAILABILITY bit);
-update MFR_AREA_T set AVAILABILITY=1 where AVAILABILITY is NULL;
-alter table MFR_AREA_T modify (AVAILABILITY bit NOT NULL DEFAULT '1'));
+alter table MFR_AREA_T add column AVAILABILITY bit;
+update MFR_AREA_T set AVAILABILITY=1 where AVAILABILITY is null;
+alter table MFR_AREA_T modify column AVAILABILITY bit not null default true;
 
 alter table MFR_AREA_T add (OPEN_DATE datetime);
 
-alter table MFR_AREA_T add (CLOSE_DATE datetime);
+alter table MFR_AREA_T add column CLOSE_DATE datetime;
 
 
-alter table MFR_OPEN_FORUM_T add (AVAILABILITY_RESTRICTED bit);
-update MFR_OPEN_FORUM_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_OPEN_FORUM_T modify (AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '0'));
+alter table MFR_OPEN_FORUM_T add column AVAILABILITY_RESTRICTED bit;
+update MFR_OPEN_FORUM_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_OPEN_FORUM_T modify column AVAILABILITY_RESTRICTED bit not null default false;
 
-alter table MFR_OPEN_FORUM_T add (AVAILABILITY bit);
-update MFR_OPEN_FORUM_T set AVAILABILITY=1 where AVAILABILITY is NULL;
-alter table MFR_OPEN_FORUM_T modify (AVAILABILITY bit NOT NULL DEFAULT '1'));
+alter table MFR_OPEN_FORUM_T add column AVAILABILITY bit;
+update MFR_OPEN_FORUM_T set AVAILABILITY=1 where AVAILABILITY is null;
+alter table MFR_OPEN_FORUM_T modify column AVAILABILITY bit not null default true;
 
-alter table MFR_OPEN_FORUM_T add (OPEN_DATE datetime);
+alter table MFR_OPEN_FORUM_T add column OPEN_DATE datetime;
 
-alter table MFR_OPEN_FORUM_T add (CLOSE_DATE datetime);
+alter table MFR_OPEN_FORUM_T add column CLOSE_DATE datetime;
 
-alter table MFR_TOPIC_T add (AVAILABILITY_RESTRICTED bit);
-update MFR_TOPIC_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_TOPIC_T modify (AVAILABILITY_RESTRICTED bit NOT NULL DEFAULT '0'));
+alter table MFR_TOPIC_T add column AVAILABILITY_RESTRICTED bit;
+update MFR_TOPIC_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is null;
+alter table MFR_TOPIC_T modify column AVAILABILITY_RESTRICTED bit not null default false;
 
-alter table MFR_TOPIC_T add (AVAILABILITY bit);
-update MFR_TOPIC_T set AVAILABILITY=1 where AVAILABILITY is NULL;
-alter table MFR_TOPIC_T modify (AVAILABILITY bit NOT NULL DEFAULT '1'));
+alter table MFR_TOPIC_T add column AVAILABILITY bit;
+update MFR_TOPIC_T set AVAILABILITY=1 where AVAILABILITY is null;
+alter table MFR_TOPIC_T modify column AVAILABILITY bit not null default true;
 
-alter table MFR_TOPIC_T add (OPEN_DATE datetime null);
+alter table MFR_TOPIC_T add column OPEN_DATE datetime null;
 
-alter table MFR_TOPIC_T add (CLOSE_DATE datetime null);
+alter table MFR_TOPIC_T add column CLOSE_DATE datetime null;
 
 
 --MSGCNTR-355
