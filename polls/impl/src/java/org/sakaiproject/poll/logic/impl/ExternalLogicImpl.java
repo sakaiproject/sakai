cd4292c6257a1e8226ee070d329360ffb5f50353
Matthew Buckett <buckett@bumph.org>
2011-08-19 16:06:05 +0000

    POLL-159 Load resource string for poll.deleteOwn correctly. Also switched to using constants in ExternalLogicImpl to avoid typos.
    git-svn-id: https://source.sakaiproject.org/svn/polls/trunk@97211 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- polls/impl/src/java/org/sakaiproject/poll/logic/impl/ExternalLogicImpl.java
+++ polls/impl/src/java/org/sakaiproject/poll/logic/impl/ExternalLogicImpl.java
@@ -650,12 +650,12 @@ public class ExternalLogicImpl implements ExternalLogic {
 	public List<String> getPermissionKeys() {
 		
 		String[] perms = new String[]{
-				"poll.vote",
-			    "poll.add",
-			    "poll.deleteown",
-			    "poll.deleteAny",
-			    "poll.editAny",
-			    "poll.editOwn",
+				PollListManager.PERMISSION_VOTE,
+			    PollListManager.PERMISSION_ADD,
+			    PollListManager.PERMISSION_EDIT_OWN,
+			    PollListManager.PERMISSION_EDIT_ANY,
+			    PollListManager.PERMISSION_DELETE_OWN,
+			    PollListManager.PERMISSION_DELETE_ANY
 		}; 
 		List<String> ret = Arrays.asList(perms);
 		return ret;
