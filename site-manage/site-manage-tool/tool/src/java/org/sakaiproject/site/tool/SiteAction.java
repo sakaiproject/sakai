fae39b6735ebdcfd5db49d62b740c2c85bda9c82
Zhen Qian <zqian@umich.edu>
2011-08-22 19:03:46 +0000

    SAK-21038
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@97295 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4323,12 +4323,9 @@ public class SiteAction extends PagedResourceActionII {
 				if (state.getAttribute(STATE_SITE_INFO) != null) {
 					siteInfo = (SiteInfo) state.getAttribute(STATE_SITE_INFO);
 				}
-				User currentUser = UserDirectoryService.getCurrentUser();
-				List<String> pList = new ArrayList<String>();
-				pList.add(currentUser.getId());
-				siteInfo.title = siteTypeProvider.getSiteTitle(type, pList);
-				siteInfo.description = siteTypeProvider.getSiteDescription(type, pList);
-				siteInfo.short_description = siteTypeProvider.getSiteShortDescription(type, pList);
+				siteInfo.title = siteTypeProvider.getSiteTitle(type, null);
+				siteInfo.description = siteTypeProvider.getSiteDescription(type, null);
+				siteInfo.short_description = siteTypeProvider.getSiteShortDescription(type, null);
 				siteInfo.include = false;
 				state.setAttribute(STATE_SITE_INFO, siteInfo);
 
@@ -10888,9 +10885,7 @@ public class SiteAction extends PagedResourceActionII {
 	
 					// now that the site and realm exist, we can set the email alias
 					// set the site alias as:
-					List<String> pList = new ArrayList<String>();
-					pList.add(SessionManager.getCurrentSessionUserId());
-					String alias = siteTypeProvider.getSiteAlias(type, pList);
+					String alias = siteTypeProvider.getSiteAlias(type, null);
 					String channelReference = mailArchiveChannelReference(id);
 					try {
 						AliasService.setAlias(alias, channelReference);
