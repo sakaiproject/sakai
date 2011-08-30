3be67f63690952f3a7ffab981fe00549e6e869cf
Gonzalo Silverio <gsilver@umich.edu>
2011-08-30 18:50:15 +0000

    DASH-38  - revealing details of one assignment reveals previously rev and hidden ones
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@312878 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- dashboard/tool/src/webapp/js/dashboard.js
+++ dashboard/tool/src/webapp/js/dashboard.js
@@ -81,19 +81,19 @@ var setupLinks = function(){
         var link = $(this).attr('href');
         var title = $(this).text();
         var parentRow = $(this).closest('tr');
-        var colCount = $(parentRow).find('td').length
-        var parentCell = $(this).closest('td')
+        var colCount = $(parentRow).find('td').length;
+        var parentCell = $(this).closest('td');
         //daft - need better way of identifying type
         var itemType = $(this).closest('tr').find('.itemType').text();
         if ($(parentRow).next('tr.newRow').length === 1) {
             $(parentRow).next('tr.newRow').find('.results').fadeToggle('slow', '', function(){
                 if ($(parentRow).next('tr.newRow').find('.results:visible').length === 0) {
-                    $(parentCell).attr('class', '')
+                    $(parentCell).attr('class', '');
                 }
                 else {
                     $(parentCell).addClass('activeCell');
                 }
-            })
+            });
         }
         else {
             if (itemType === "assignment" || $(this).attr('href').indexOf('assignment') !== -1) {
@@ -108,18 +108,18 @@ var setupLinks = function(){
                         results = results + '<div id=\"metaDataGradSub\">' + resolveTypeOfGrade(data.content.typeOfGrade) + ', ' + resolveTypeOfSubmission(data.content.typeOfSubmission) + resolveMaxGradePointDisplay(data.content.maxGradePointDisplay) + '<div id=\"link\">' + '<a target ="_top" href=' + link + '>' + langdata.seemore + '</a>' + '</div>' + '</div>';
                         results = results + '<div id=\"description\">' + resolveInstructions(data.content.instructions) + '</div></div>';
                         
-                        $('.activeCell').removeClass('.activeCell')
+                        $('.activeCell').removeClass('.activeCell');
                         $(parentCell).addClass('activeCell');
-                        $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\">' + results + '</td></tr>').insertAfter(parentRow)
-                        $('.newRow').find('.results').slideDown('slow', function(){
-                            resizeFrame('grow')
+                        $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\">' + results + '</td></tr>').insertAfter(parentRow);
+                        $(parentRow).next('tr.newRow').find('.results').slideDown('slow', function(){
+                            resizeFrame('grow');
                         });
                         
                     },
                     error: function(){
-                        $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\">Error retriving data - assignment may have had attachments</div></td></tr>').insertAfter(parentRow)
-                        $('.newRow').find('.results').slideDown('slow', function(){
-                            resizeFrame('grow')
+                        $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\">Error retriving data - assignment may have had attachments</div></td></tr>').insertAfter(parentRow);
+                        $(parentRow).next('tr.newRow').find('.results').slideDown('slow', function(){
+                            resizeFrame('grow');
                         });
                         
                     }
@@ -154,41 +154,41 @@ var setupLinks = function(){
                             var linkList = "";
                             var md = $('#dialogDum').find('table').eq(0);
                             $('#dialogDum').find('table').eq(0).remove();
-                            $('#dialogDum').find('h1').remove()
-                            $('#dialogDum').find('meta').remove()
-                            $('#dialogDum').find('style').remove()
-                            $('#dialogDum').find('title').remove()
+                            $('#dialogDum').find('h1').remove();
+                            $('#dialogDum').find('meta').remove();
+                            $('#dialogDum').find('style').remove();
+                            $('#dialogDum').find('title').remove();
                             
                             var content = $('#dialogDum').contents();
                             
-                            $('#results').append((md).find('td').eq(3).text() + ' (' + (md).find('td').eq(1).text() + ')')
-                            $('#results').append('<hr>')
-                            $('#results').append(content)
+                            $('#results').append((md).find('td').eq(3).text() + ' (' + (md).find('td').eq(1).text() + ')');
+                            $('#results').append('<hr>');
+                            $('#results').append(content);
                             if ($('#results').find('p').eq(-2).children('b').length === 1) {
-                                $('#results').append('<ul class=\"attachList\"></ul')
+                                $('#results').append('<ul class=\"attachList\"></ul');
                                 $('#results').find('p').eq(-1).children('a').each(function(i){
-                                    var urlArr = $(this).text().split('/')
-                                    var anchorText = urlArr[urlArr.length - 1]
+                                    var urlArr = $(this).text().split('/');
+                                    var anchorText = urlArr[urlArr.length - 1];
                                     linkList = linkList + '<li><a href=\"' + $(this).attr('href') + '\">' + anchorText + '</a></li>';
-                                })
+                                });
                                 $('#results').find('p').eq(-1).remove();
-                                $('#results').find('.attachList').append((linkList))
+                                $('#results').find('.attachList').append((linkList));
                             }
                             var results = $('#results').html();
-                            $('.activeCell').removeClass('.activeCell')
+                            $('.activeCell').removeClass('.activeCell');
                             $(parentCell).addClass('activeCell');
-                            $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\" style=\"display:none\">' + results + '</div></td></tr>').insertAfter(parentRow)
+                            $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\" style=\"display:none\">' + results + '</div></td></tr>').insertAfter(parentRow);
                             $(parentRow).next('tr').find('.results').slideDown('slow', function(){
-                                resizeFrame('grow')
+                                resizeFrame('grow');
                             });
                             
                             
                         },
                         error: function(){
                         
-                            $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\" style=\"display:none\">There was an error retrieving this data.</div></td></tr>').insertAfter(parentRow)
+                            $('<tr class=\"newRow\"><td colspan=\"' + colCount + '\"><div class=\"results\" style=\"display:none\">There was an error retrieving this data.</div></td></tr>').insertAfter(parentRow);
                             $(parentRow).next('tr').find('.results').slideDown('slow', function(){
-                                resizeFrame('grow')
+                                resizeFrame('grow');
                             });
                             
                         }
@@ -264,7 +264,7 @@ var resolveInstructions = function(instructions){
      but users can just enter whitespace - that will resolve to meaningless
      markup, roundabout way of doing this!
      */
-    $('#instructionHolder').html(instructions)
+    $('#instructionHolder').html(instructions);
     $('#instructionHolder').htmlClean();
     if ($("#instructionHolder").text().length !== 0) {
         return ('<hr>' + instructions);
