d61dc4f378ef6c580a58abfa8a59f40e18343bcb
maarten@edia.nl <maarten@edia.nl@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-09-08 15:57:15 +0000

    Changing the default error page and cleaning up code.

--- scorm-tool/src/java/org/sakaiproject/scorm/ui/player/SingleScormPackageApplication.java
+++ scorm-tool/src/java/org/sakaiproject/scorm/ui/player/SingleScormPackageApplication.java
@@ -2,38 +2,38 @@ package org.sakaiproject.scorm.ui.player;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.sakaiproject.scorm.service.api.ScormResourceService;
+import org.apache.wicket.settings.IExceptionSettings;
 import org.sakaiproject.scorm.ui.ContentPackageResourceMountStrategy;
 import org.sakaiproject.scorm.ui.console.pages.DisplayDesignatedPackage;
-import org.sakaiproject.scorm.ui.console.pages.PackageListPage;
 import org.sakaiproject.wicket.protocol.http.SakaiWebApplication;
 
 public class SingleScormPackageApplication extends SakaiWebApplication {
 
-    private static Log log = LogFactory.getLog(SingleScormPackageApplication.class);
-    
-    ScormResourceService resourceService;
-    
-    @Override
-    public void init() {
-        log.info(">>> Initializing singleScormPackageApplication");
-        super.init();
-        mount(new ContentPackageResourceMountStrategy("contentpackages"));
-    }
-    
-    @Override
-    public Class getHomePage() {
-        return DisplayDesignatedPackage.class;
-    }
-    
-    
-    public ScormResourceService getResourceService() {
-        return resourceService;
-    }
+	private static Log log = LogFactory.getLog(SingleScormPackageApplication.class);
 
-    public void setResourceService(ScormResourceService resourceService) {
-        this.resourceService = resourceService;
-    }
+	//ScormResourceService resourceService;
+
+	@Override
+	public void init() {
+		log.info(">>> Initializing singleScormPackageApplication");
+		super.init();
+		mount(new ContentPackageResourceMountStrategy("contentpackages"));
+
+		getExceptionSettings().setUnexpectedExceptionDisplay(IExceptionSettings.SHOW_INTERNAL_ERROR_PAGE);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Class getHomePage() {
+		return DisplayDesignatedPackage.class;
+	}
+
+//	public ScormResourceService getResourceService() {
+//		return resourceService;
+//	}
+//
+//	public void setResourceService(ScormResourceService resourceService) {
+//		this.resourceService = resourceService;
+//	}
 
 } // class
