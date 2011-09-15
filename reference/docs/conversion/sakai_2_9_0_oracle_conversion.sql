7481da936ac64fad807e8f5e5749b369103f53ae
Karen Tsao <ktsao@stanford.edu>
2011-09-15 05:43:17 +0000

    SAM-1008, SAM-1255, and SAM-1205
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@98173 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_9_0_oracle_conversion.sql
@@ -102,7 +102,45 @@ alter table SAKAI_PERSON_T rename column TMP_PUBLICATIONS to PUBLICATIONS;
 alter table SAKAI_PERSON_T rename column TMP_BUSINESS_BIOGRAPHY to BUSINESS_BIOGRAPHY;
 -- end SAK-20598
 
+-- SAM-1008:  oracle only
+alter table SAM_ANSWER_T add (TEMP_CLOB_TEXT clob);
+update SAM_ANSWER_T SET TEMP_CLOB_TEXT = TEXT;
+alter table SAM_ANSWER_T drop column TEXT;
+alter table SAM_ANSWER_T rename column TEMP_CLOB_TEXT to TEXT;
+
+alter table SAM_PUBLISHEDANSWER_T add (TEMP_CLOB_TEXT clob);
+update SAM_PUBLISHEDANSWER_T SET TEMP_CLOB_TEXT = TEXT;
+alter table SAM_PUBLISHEDANSWER_T drop column TEXT;
+alter table SAM_PUBLISHEDANSWER_T rename column TEMP_CLOB_TEXT to TEXT;
+
+INSERT INTO SAM_TYPE_T ("TYPEID" ,"AUTHORITY" ,"DOMAIN" ,"KEYWORD",
+    "DESCRIPTION" ,
+    "STATUS" ,"CREATEDBY" ,"CREATEDDATE" ,"LASTMODIFIEDBY" ,
+    "LASTMODIFIEDDATE" )
+    VALUES (13 , 'stanford.edu' ,'assessment.item' ,'Matrix Choices Survey' ,NULL ,1 ,1 ,
+    SYSDATE ,1 ,SYSDATE);   
+    
 -- SAM-1255    
 Update SAM_ASSESSEVALUATION_T
 Set ANONYMOUSGRADING = 2
 WHERE ASSESSMENTID = (Select ID from SAM_ASSESSMENTBASE_T where TITLE='Default Assessment Type' AND TYPEID='142' AND ISTEMPLATE=1)    
+
+-- SAM-1205
+INSERT INTO SAM_ASSESSMETADATA_T ("ASSESSMENTMETADATAID", "ASSESSMENTID","LABEL",
+    "ENTRY")
+    VALUES(sam_assessMetaData_id_s.nextVal, (SELECT ID FROM SAM_ASSESSMENTBASE_T WHERE TITLE='Default Assessment Type' AND TYPEID='142' AND ISTEMPLATE=1), 'lockedBrowser_isInstructorEditable', 'true');
+    
+INSERT INTO SAM_ASSESSMETADATA_T (ASSESSMENTMETADATAID, ASSESSMENTID, LABEL,
+    ENTRY)
+    VALUES(sam_assessMetaData_id_s.nextVal, (SELECT ID FROM SAM_ASSESSMENTBASE_T WHERE TITLE='Test'
+     AND TYPEID='142' AND ISTEMPLATE=1),
+      'lockedBrowser_isInstructorEditable', 'true')
+;
+
+INSERT INTO SAM_ASSESSMETADATA_T (ASSESSMENTMETADATAID, ASSESSMENTID, LABEL,
+    ENTRY)
+    VALUES(sam_assessMetaData_id_s.nextVal, (SELECT ID FROM SAM_ASSESSMENTBASE_T WHERE TITLE='Timed Test'
+     AND TYPEID='142' AND ISTEMPLATE=1),
+      'lockedBrowser_isInstructorEditable', 'true')
+;  
+
