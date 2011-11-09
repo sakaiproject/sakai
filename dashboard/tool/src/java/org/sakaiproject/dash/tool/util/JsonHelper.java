e9a9f381a05de38a191240eb856ca3e5e6ef2e0b
Jim Eng <jimeng@umich.edu>
2011-11-09 21:23:49 +0000

    DASH-67 DASH-95 Moved json methods to JsonHelper class
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@313000 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ dashboard/tool/src/java/org/sakaiproject/dash/tool/util/JsonHelper.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package org.sakaiproject.dash.tool.util;
+
+import java.util.List;
+import java.util.Map;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.sakaiproject.dash.logic.DashboardLogic;
+import org.sakaiproject.dash.model.CalendarItem;
+import org.sakaiproject.dash.model.NewsItem;
+
+/**
+ * 
+ *
+ */
+public class JsonHelper {
+	
+	protected DashboardLogic dashboardLogic;
+	
+	public JsonHelper(DashboardLogic dashboardLogic) {
+		this.dashboardLogic = dashboardLogic;
+	}
+	
+	public JSONObject getJsonObjectFromMap(Map<String, Object> map) {
+		JSONObject json = new JSONObject();
+		if(map != null) {
+			for(Map.Entry<String, Object> entry : map.entrySet()) {
+				String key = entry.getKey();
+				Object value = entry.getValue();
+				if(value instanceof String) {
+					json.element(key, value);
+				} else if(value instanceof Boolean) {
+					json.element(key, value);
+				} else if(value instanceof Number) {
+					json.element(key, value);
+				} else if(value instanceof Map) {
+					json.element(key, getJsonObjectFromMap((Map<String, Object>) value));
+				} else if(value instanceof List) {
+					json.element(key, getJsonArrayFromList((List) value));
+				}
+			}
+				
+		}
+		return json;
+	}
+
+	public JSONArray getJsonArrayFromList(List list) {
+		JSONArray json = new JSONArray();
+		if(list != null) {
+			for(Object value : list) {
+				if(value instanceof String) {
+					json.element(value);
+				} else if(value instanceof Boolean) {
+					json.element(value);
+				} else if(value instanceof Number) {
+					json.element(value);
+				} else if(value instanceof Map) {
+					json.element(getJsonObjectFromMap((Map<String, Object>) value));
+				} else if(value instanceof List) {
+					json.element(getJsonArrayFromList((List) value));
+				} else if(value instanceof NewsItem) {
+					json.element(getJsonObjectFromNewsItem((NewsItem) value));
+				}
+				
+			}
+		}
+		return json;
+	}
+
+	public JSONObject getJsonObjectFromNewsItem(NewsItem newsItem) {
+		JSONObject json = new JSONObject();
+		json.element("entityReference", newsItem.getEntityReference());
+		json.element("id", newsItem.getId());
+		json.element("newsTime", newsItem.getNewsTime());
+		json.element("label", newsItem.getNewsTimeLabelKey());
+		json.element("entityType", newsItem.getSourceType().getIdentifier());
+		json.element("subtype", newsItem.getSubtype());
+		json.element("title", newsItem.getTitle());
+		json.element("iconUrl", dashboardLogic.getEntityIconUrl(newsItem.getSourceType().getIdentifier(), newsItem.getSubtype()));
+		return json;
+	}
+
+	public JSONObject getJsonObjectFromCalendarItem(CalendarItem calendarItem) {
+		JSONObject json = new JSONObject();
+		json.element("entityReference", calendarItem.getEntityReference());
+		json.element("id", calendarItem.getId());
+		json.element("calendarTime", calendarItem.getCalendarTime());
+		json.element("label", calendarItem.getCalendarTimeLabelKey());
+		json.element("entityType", calendarItem.getSourceType().getIdentifier());
+		json.element("subtype", calendarItem.getSubtype());
+		json.element("title", calendarItem.getTitle());
+		json.element("iconUrl", dashboardLogic.getEntityIconUrl(calendarItem.getSourceType().getIdentifier(), calendarItem.getSubtype()));
+		return json;
+	}
+
+}
