8946c618c9c4553eff22ad8ec45e7483391ad494
Steve Swinsburg <steve.swinsburg@gmail.com>
2011-11-21 03:01:33 +0000

    SAK-21438 sql update for incorrect messages setting that may be present
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@100919 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ reference/docs/conversion/sakai_2_8_1-2_8_2_mysql_conversion.sql
@@ -0,0 +1,15 @@
+-- This is the MySQL Sakai 2.8.1 -> 2.8.2 conversion script
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- 
+-- use this to convert a Sakai database from 2.8.1 to 2.8.2.  Run this before you run your first app server.
+-- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
+--
+-- Script insertion format
+-- -- [TICKET] [short comment]
+-- -- [comment continued] (repeat as necessary)
+-- SQL statement
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
+
+-- PRFL-687 incorrect default value for messages setting
+update PROFILE_PRIVACY_T set MESSAGES=1 where MESSAGES=0;
