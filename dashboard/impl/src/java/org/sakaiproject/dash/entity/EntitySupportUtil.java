fe59bc7080b14e84a824538cec382e82b46c897d
Jim Eng <jimeng@umich.edu>
2011-11-23 21:54:07 +0000

    DASH-144 DASH-140 Descending order for all lists of news items. Also cleaned up a couple minor things related to DASH-144.
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@313070 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- dashboard/impl/src/java/org/sakaiproject/dash/entity/EntitySupportUtil.java
+++ dashboard/impl/src/java/org/sakaiproject/dash/entity/EntitySupportUtil.java
@@ -3,42 +3,13 @@
  */
 package org.sakaiproject.dash.entity;
 
-import java.lang.reflect.InvocationTargetException;
-import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.TimeZone;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentEntityType;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentNewEventProcessor;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentRemoveEventProcessor;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentUpdateAccessEventProcessor;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentUpdateEventProcessor;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentUpdateOpenDateEventProcessor;
-import org.sakaiproject.dash.entity.AssignmentSupport.AssignmentUpdateTitleEventProcessor;
 import org.sakaiproject.dash.logic.DashboardLogic;
-import org.sakaiproject.dash.logic.SakaiProxy;
-import org.sakaiproject.dash.model.CalendarItem;
-import org.sakaiproject.dash.model.Context;
 import org.sakaiproject.dash.model.NewsItem;
-import org.sakaiproject.dash.model.RepeatingCalendarItem;
-import org.sakaiproject.dash.model.SourceType;
-import org.sakaiproject.entity.api.Entity;
-import org.sakaiproject.entity.api.EntityPropertyNotDefinedException;
-import org.sakaiproject.entity.api.EntityPropertyTypeException;
-import org.sakaiproject.entity.api.Reference;
-import org.sakaiproject.entity.api.ResourceProperties;
 import org.sakaiproject.event.api.Event;
-import org.sakaiproject.thread_local.api.ThreadLocalManager;
-import org.sakaiproject.time.api.TimeRange;
-import org.sakaiproject.time.api.TimeService;
-import org.sakaiproject.user.api.User;
 import org.sakaiproject.util.ResourceLoader;
 
 /**
