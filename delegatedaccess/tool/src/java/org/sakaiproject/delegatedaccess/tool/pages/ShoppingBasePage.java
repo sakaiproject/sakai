6310463889fb62160e69e35da89d82646f688f9f
Bryan Holladay <holladay@longsight.com>
2011-11-23 21:53:23 +0000

    adding shopping tool
    git-svn-id: https://source.sakaiproject.org/svn/delegatedaccess/trunk@236489 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ delegatedaccess/tool/src/java/org/sakaiproject/delegatedaccess/tool/pages/ShoppingBasePage.java
@@ -0,0 +1,130 @@
+package org.sakaiproject.delegatedaccess.tool.pages;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.sakaiproject.delegatedaccess.logic.ProjectLogic;
+import org.sakaiproject.delegatedaccess.logic.SakaiProxy;
+
+public class ShoppingBasePage  extends WebPage implements IHeaderContributor {
+
+	private static final Logger log = Logger.getLogger(BasePage.class); 
+
+	@SpringBean(name="org.sakaiproject.delegatedaccess.logic.SakaiProxy")
+	protected SakaiProxy sakaiProxy;
+
+	@SpringBean(name="org.sakaiproject.delegatedaccess.logic.ProjectLogic")
+	protected ProjectLogic projectLogic;
+
+	Link<Void> firstLink;
+
+	FeedbackPanel feedbackPanel;
+
+	public ShoppingBasePage() {
+
+		log.debug("BasePage()");
+
+
+		//first link
+		firstLink = new Link<Void>("firstLink") {
+			private static final long serialVersionUID = 1L;
+			public void onClick() {
+				setResponsePage(new UserPage());
+			}
+		};
+		firstLink.add(new Label("firstLinkLabel",new ResourceModel("link.first")).setRenderBodyOnly(true));
+		firstLink.add(new AttributeModifier("title", true, new ResourceModel("link.first.tooltip")));
+		add(firstLink);
+
+
+
+		// Add a FeedbackPanel for displaying our messages
+		feedbackPanel = new FeedbackPanel("feedback"){
+
+			@Override
+			protected Component newMessageDisplayComponent(final String id, final FeedbackMessage message) {
+				final Component newMessageDisplayComponent = super.newMessageDisplayComponent(id, message);
+
+				if(message.getLevel() == FeedbackMessage.ERROR ||
+						message.getLevel() == FeedbackMessage.DEBUG ||
+						message.getLevel() == FeedbackMessage.FATAL ||
+						message.getLevel() == FeedbackMessage.WARNING){
+					add(new SimpleAttributeModifier("class", "alertMessage"));
+				} else if(message.getLevel() == FeedbackMessage.INFO){
+					add(new SimpleAttributeModifier("class", "success"));        			
+				} 
+
+				return newMessageDisplayComponent;
+			}
+		};
+		add(feedbackPanel); 
+
+	}
+
+	/**
+	 * Helper to clear the feedbackpanel display.
+	 * @param f	FeedBackPanel
+	 */
+	public void clearFeedback(FeedbackPanel f) {
+		if(!f.hasFeedbackMessage()) {
+			f.add(new SimpleAttributeModifier("class", ""));
+		}
+	}
+
+	/**
+	 * This block adds the required wrapper markup to style it like a Sakai tool. 
+	 * Add to this any additional CSS or JS references that you need.
+	 * 
+	 */
+	public void renderHead(IHeaderResponse response) {
+
+
+		//get Sakai skin
+		String skinRepo = sakaiProxy.getSkinRepoProperty();
+		String toolCSS = sakaiProxy.getToolSkinCSS(skinRepo);
+		String toolBaseCSS = skinRepo + "/tool_base.css";
+
+		//Sakai additions
+		response.renderJavascriptReference("/library/js/headscripts.js");
+		response.renderCSSReference(toolBaseCSS);
+		response.renderCSSReference(toolCSS);
+		response.renderOnLoadJavascript("setMainFrameHeight( window.name )");
+
+		//Tool additions (at end so we can override if required)
+		response.renderString("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />");
+		//response.renderCSSReference("css/my_tool_styles.css");
+		//response.renderJavascriptReference("js/my_tool_javascript.js");
+		
+		//for jQuery
+		response.renderJavascriptReference("/library/js/jquery-latest.min.js");
+			
+		//for datepicker
+		response.renderCSSReference("css/flora.datepicker.css");
+		response.renderJavascriptReference("javascript/jquery.ui.core-1.5.2.min.js");
+		response.renderJavascriptReference("javascript/jquery.datepicker-1.5.2.min.js");
+
+	}
+
+
+	/** 
+	 * Helper to disable a link. Add the Sakai class 'current'.
+	 */
+	protected void disableLink(Link<Void> l) {
+		l.add(new AttributeAppender("class", new Model<String>("current"), " "));
+		l.setRenderBodyOnly(true);
+		l.setEnabled(false);
+	}
+
+}
---
+++ delegatedaccess/tool/src/java/org/sakaiproject/delegatedaccess/tool/pages/ShoppingPageSiteSearch.java
@@ -0,0 +1,242 @@
+package org.sakaiproject.delegatedaccess.tool.pages;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.tree.TreeModel;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
+import org.apache.wicket.markup.repeater.DefaultItemReuseStrategy;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
+import org.sakaiproject.delegatedaccess.model.NodeModel;
+import org.sakaiproject.delegatedaccess.util.DelegatedAccessConstants;
+import org.sakaiproject.delegatedaccess.util.NodeModelComparator;
+import org.sakaiproject.site.api.Site;
+
+public class ShoppingPageSiteSearch  extends ShoppingBasePage {
+
+	private String searchModel = "";
+	private int orderBy = DelegatedAccessConstants.SEARCH_COMPARE_DEFAULT;
+	private boolean orderAsc = true;
+	private NodeModelDataProvider provider;
+	private String search = "";
+	private TreeModel treeModel;
+
+
+	public ShoppingPageSiteSearch(final String search, TreeModel treeModel){
+		this.search = search;
+		this.treeModel = treeModel;
+		//Create Search Form:
+		final PropertyModel<String> messageModel = new PropertyModel<String>(this, "search");
+		Form<?> form = new Form("form");
+		form.add(new TextField<String>("search", messageModel));
+		add(form);
+
+		//show user's search (if not null)
+		add(new Label("searchResultsTitle", new StringResourceModel("searchResultsTitle", null)){
+			@Override
+			public boolean isVisible() {
+				return messageModel.getObject() != null && !"".equals(messageModel.getObject());
+			}
+		});
+		add(new Label("searchString",messageModel){
+			@Override
+			public boolean isVisible() {
+				return messageModel.getObject() != null && !"".equals(messageModel.getObject());
+			}
+		});
+
+		//search result table:
+		//Headers
+		Link<Void> siteTitleSort = new Link<Void>("siteTitleSortLink"){
+			private static final long serialVersionUID = 1L;
+			public void onClick() {
+				changeOrder(DelegatedAccessConstants.SEARCH_COMPARE_SITE_TITLE);
+			}
+
+			@Override
+			public boolean isVisible() {
+				return provider.size() > 0;
+			}
+		};
+		add(siteTitleSort);
+		Link<Void> siteIdSort = new Link<Void>("siteIdSortLink"){
+			private static final long serialVersionUID = 1L;
+			public void onClick() {
+				changeOrder(DelegatedAccessConstants.SEARCH_COMPARE_SITE_ID);
+			}
+
+			@Override
+			public boolean isVisible() {
+				return provider.size() > 0;
+			}
+		};
+		add(siteIdSort);
+
+		//Data:
+		provider = new NodeModelDataProvider();
+		final DataView<NodeModel> dataView = new DataView<NodeModel>("searchResult", provider) {
+			@Override
+			public void populateItem(final Item item) {
+				final NodeModel nodeModel = (NodeModel) item.getModelObject();
+				AjaxLink<Void> siteTitleLink = new AjaxLink("siteTitleLink"){
+					private static final long serialVersionUID = 1L;
+					public void onClick(AjaxRequestTarget target) {
+						Site site = sakaiProxy.getSiteByRef(nodeModel.getNode().description);
+						if(site != null){
+							//redirect the user to the site
+							target.appendJavascript("top.location='" + site.getUrl() + "'");
+						}
+					}
+				};
+				siteTitleLink.add(new Label("siteTitle", nodeModel.getNode().title));
+				item.add(siteTitleLink);
+				String siteId = nodeModel.getNode().description;
+				if(siteId.startsWith("/site/")){
+					siteId = siteId.substring(6);
+				}
+
+				item.add(new Label("siteId", siteId));
+			}
+			@Override
+			public boolean isVisible() {
+				return provider.size() > 0;
+			}
+		};
+		dataView.setItemReuseStrategy(new DefaultItemReuseStrategy());
+		dataView.setItemsPerPage(DelegatedAccessConstants.SEARCH_RESULTS_PAGE_SIZE);
+		add(dataView);
+		//Navigation
+		//add a pager to our table, only visible if we have more than SEARCH_RESULTS_PAGE_SIZE items
+		add(new PagingNavigator("navigatorTop", dataView) {
+
+			@Override
+			public boolean isVisible() {
+				if(provider.size() > DelegatedAccessConstants.SEARCH_RESULTS_PAGE_SIZE) {
+					return true;
+				}
+				return false;
+			}
+
+			@Override
+			public void onBeforeRender() {
+				super.onBeforeRender();
+
+				//clear the feedback panel messages
+				clearFeedback(feedbackPanel);
+			}
+		});
+		add(new PagingNavigator("navigatorBottom", dataView) {
+
+			@Override
+			public boolean isVisible() {
+				if(provider.size() > DelegatedAccessConstants.SEARCH_RESULTS_PAGE_SIZE) {
+					return true;
+				}
+				return false;
+			}
+
+			@Override
+			public void onBeforeRender() {
+				super.onBeforeRender();
+
+				//clear the feedback panel messages
+				clearFeedback(feedbackPanel);
+			}
+		});
+
+
+
+	}
+
+	/**
+	 * changes order by desc or asc
+	 * 
+	 * @param sortByColumn
+	 */
+	private void changeOrder(int sortByColumn){
+		if(sortByColumn == orderBy){
+			orderAsc = !orderAsc;
+		}else{
+			orderBy = sortByColumn;
+		}
+	}
+
+	/**
+	 * A data provider for the search results table.  This calls the search functions in Sakai
+	 *
+	 */
+	private class NodeModelDataProvider implements IDataProvider<NodeModel>{
+
+		private static final long serialVersionUID = 1L;
+
+		private boolean lastOrderAsc = true;
+		private int lastOrderBy = DelegatedAccessConstants.SEARCH_COMPARE_DEFAULT;
+
+		private List<NodeModel> list;
+		public void detach() {
+			list = null;
+		}
+
+		public Iterator<? extends NodeModel> iterator(int first, int count) {
+			return getData().subList(first, first + count).iterator();
+		}
+
+		public IModel<NodeModel> model(final NodeModel object) {
+			return new AbstractReadOnlyModel<NodeModel>() {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public NodeModel getObject() {
+					return object;
+				}
+			};
+		}
+
+		public int size() {
+			return getData().size();
+		}
+
+		private List<NodeModel> getData(){
+			if(list == null){
+				list = projectLogic.searchUserSites(getSearch(), treeModel);
+				sortList();
+			}else if(lastOrderAsc != orderAsc || lastOrderBy != orderBy){
+				sortList();
+			}
+			return list;
+		}
+
+		private void sortList(){
+			Collections.sort(list, new NodeModelComparator(orderBy));
+			if(!orderAsc){
+				Collections.reverse(list);
+			}
+			this.lastOrderAsc = orderAsc;
+			this.lastOrderBy = orderBy;
+		}
+
+	}
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+
+}
