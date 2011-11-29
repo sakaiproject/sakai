63d2c8bfd8e2b383fb7d0102b6e10e1a5670fc32
Gonzalo Silverio <gsilver@umich.edu>
2011-11-29 17:44:12 +0000

    DASH-137 - reflect in the UI the hiding/starring/unstarring actions in collections (aggregated post)
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@313083 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- dashboard/tool/src/webapp/js/dashboard.js
+++ dashboard/tool/src/webapp/js/dashboard.js
@@ -259,7 +259,7 @@ var setupLinks = function(){
                             	hideAction = "hideThis";
                             }
                             var link = '';
-                            row = '<td class="one">\n<span class="itemType" style="display:none;">' + this.itemType + '</span>\n<span class="actionTargetId" style="display:none;">' + this.newsItemId + '</span>\n<span class="itemCount" style="display:none;">0</span>\n<span class="entityReference" style="display:none;">' + this.entityReference + '</span>\n</td>\n<td class="two date"></td>\n<td class="tab three">\n<a href="#" class="itemLink" target="_top">' + icon + ' ' + this.title + '</a><span class="itemLabel">' + this.label + '</span>\n</td>\n<td class="four"></td>\n<td class="action five">\n<a class="' + starAction + '" href="#"><img alt="[ Star/Unstar This ]" src="' + this.starringActionIcon + '" /></a>\n</td>\n<td class="action six">\n<a class="' + hideAction + '" href="#"><img alt="[ Hide/Show This ]" src="' + this.hidingActionIcon + '" /></a>\n</td>\n'
+                            row = '<td class="one">\n<span class="itemType" style="display:none;">' + this.itemType + '</span>\n<span class="actionTargetId" style="display:none;">' + this.newsItemId + '</span>\n<span class="itemCount" style="display:none;">0</span>\n<span class="entityReference" style="display:none;">' + this.entityReference + '</span>\n</td>\n<td class="two date"></td>\n<td class="tab three">\n<a href="#" class="itemLink" target="_top">' + icon + ' ' + this.title + '</a><span class="itemLabel">' + this.label + '</span>\n</td>\n<td class="four"></td>\n<td class="action five">\n<a class="' + starAction + '" href="#"><img alt="[ Star/Unstar This ]" src="' + this.starringActionIcon + '" /></a>\n</td>\n<td class="action six">\n<a class="' + hideAction + '" href="#"><img alt="[ Hide/Show This ]" src="' + this.hidingActionIcon + '" /></a>\n</td>\n';
 
                             //if (itemType === "resource") {
                             //    row = '<td style=\"width:50%\" class=\"toggleCell resourceLink\"><a href=\"#\" class =\"itemLink\">' + icon + this.title + '</a></td><td style=\"width:50%\"><em style=\"display:none\"><span class=\"itemType\">' + itemType + '</span><span class=\"itemCount\">1</span><span class=\"entityReference\">' + this.entityReference + '</span></em><a href=\"/access' + this.entityReference + '\" target =\"_blank\">Download</a></td>';
@@ -320,7 +320,19 @@ function updateItemStatus(element, dashAction, itemId) {
         contentType: 'application/json',
         dataType: 'json',
         success: function(json){
-        	alert('updateItemStatus() message == ' + json.message + ' success == ' + json.success);
+            if (dashAction ==='hide' || dashAction ==='unHide'){
+              $(element).closest('tr').fadeOut('slow');
+            }
+            else{
+               if (dashAction === 'unstar') {
+                   $(element).attr('src', '/dashboard-tool/css/img/star-inact.png');
+               }
+               else {
+                   $(element).attr('src', '/dashboard-tool/css/img/star-act.png');
+               }
+               
+            }
+            reportSuccess(json.message);
         }
     });    
 }
