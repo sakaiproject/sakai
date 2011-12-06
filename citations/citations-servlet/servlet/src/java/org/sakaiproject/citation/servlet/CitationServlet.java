871323ed7dc3a790ab83b714aa02a9249e5eaa8a
Noah Botimer <botimer@umich.edu>
2011-12-06 21:21:14 +0000

    SAK-21494 - Initial refactoring; pulling up base servlet class
    git-svn-id: https://source.sakaiproject.org/svn/citations/branches/SAK-21494@101486 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ citations/citations-servlet/servlet/src/java/org/sakaiproject/citation/servlet/CitationServlet.java
@@ -0,0 +1,174 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2006, 2007, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.citation.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.LogFactory;
+
+import org.sakaiproject.citation.api.Citation;
+import org.sakaiproject.citation.api.CitationCollection;
+import org.sakaiproject.citation.api.CitationService;
+import org.sakaiproject.citation.api.Schema;
+import org.sakaiproject.citation.cover.ConfigurationService;
+import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.content.api.ContentResource;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.entity.api.Reference;
+import org.sakaiproject.entity.cover.EntityManager;
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.ServerOverloadException;
+import org.sakaiproject.exception.TypeException;
+import org.sakaiproject.tool.api.ActiveTool;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.Tool;
+import org.sakaiproject.tool.api.ToolException;
+import org.sakaiproject.tool.cover.ActiveToolManager;
+import org.sakaiproject.tool.cover.SessionManager;
+import org.sakaiproject.user.cover.UserDirectoryService;
+import org.sakaiproject.util.BasicAuth;
+import org.sakaiproject.util.ParameterParser;
+import org.sakaiproject.util.ResourceLoader;
+import org.sakaiproject.util.Validator;
+import org.sakaiproject.util.Web;
+
+/**
+ * 
+ *
+ */
+public class CitationServlet extends BaseCitationServlet
+{
+
+	/**
+	 * respond to an HTTP GET request
+	 * 
+	 * @param req
+	 *        HttpServletRequest object with the client request
+	 * @param res
+	 *        HttpServletResponse object back to the client
+	 * @exception ServletException
+	 *            in case of difficulties
+	 * @exception IOException
+	 *            in case of difficulties
+	 */
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		// process any login that might be present
+		basicAuth.doLogin(req);
+		
+		// catch the login helper requests
+		String option = req.getPathInfo();
+		String[] parts = option.split("/");
+		
+		if ((parts.length == 2) && ((parts[1].equals("login"))))
+		{
+			doLogin( req, res, null );
+		}
+		else
+		{
+			// Setup velocity.
+			setupResponse(req, res);
+			ContentResource resource = null;
+			try
+			{
+				ParameterParser paramParser = (ParameterParser) req
+					.getAttribute(ATTR_PARAMS);
+				resource = findResource(paramParser, option);
+       
+				boolean fromGoogle = false;
+				Citation citation = findOpenURLVersion01(paramParser);
+				if (citation == null) {
+         
+					citation = findOpenUrlCitation(req);
+				}
+				// set the success flag
+				setVmReference("success", citation != null, req);
+       
+				if (citation != null) {
+					addCitation(resource, citation);
+					setVmReference( "citation", citation, req );
+					setVmReference("topRefresh", Boolean.TRUE, req ); // TODO
+				} else {
+					// return failure
+					setVmReference("error", rb.getString("error.notfound"), req);
+				}
+			} catch (IdUnusedException iue) {
+				setVmReference("error", rb.getString("error.noid"), req);
+			} catch (ServerOverloadException e) {
+				setVmReference("error", rb.getString("error.unavailable"), req);
+			} catch (PermissionException e) {
+				setVmReference("error", rb.getString("error.permission"), req);
+			}
+			// Set near end so we always have something
+			setVmReference( "titleArgs",  new String[]{ getCollectionTitle(resource) }, req );
+			// return the servlet template
+			includeVm( SERVLET_TEMPLATE, req, res );
+
+		}
+	}
+
+	/**
+	 * 
+	 * @param req
+	 *        HttpServletRequest object with the client request
+	 * @param res
+	 *        HttpServletResponse object back to the client
+	 * @exception ServletException
+	 *            in case of difficulties
+	 * @exception IOException
+	 *            in case of difficulties
+	 */
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		// process any login that might be present
+		basicAuth.doLogin(req);
+		
+		// catch the login helper posts
+		String option = req.getPathInfo();
+		String[] parts = option.split("/");
+		
+		if ((parts.length == 2) && ((parts[1].equals("login"))))
+		{
+			doLogin(req, res, null);
+		}
+
+		else
+		{
+			// don't handle POSTs
+			sendError(res, HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+	
+}
