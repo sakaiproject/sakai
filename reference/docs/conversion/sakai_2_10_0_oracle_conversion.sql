d37ca6568d47bebdefc4f6442a0b158b547e9f2b
Bryan Holladay <holladay@longsight.com>
2012-01-06 16:55:32 +0000

    SAK-21225 Extra Credit in Gradebook
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@102932 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_10_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_10_0_oracle_conversion.sql
@@ -1,4 +1,10 @@
 --SAK-21225 Extra Credit in Gradebook
+Update GB_CATEGORY_T
+set IS_EXTRA_CREDIT = false
+where IS_EXTRA_CREDIT is null
+
 update GB_GRADABLE_OBJECT_T
 Set IS_EXTRA_CREDIT = false
 Where IS_EXTRA_CREDIT is null
+
+--END SAK-21225
