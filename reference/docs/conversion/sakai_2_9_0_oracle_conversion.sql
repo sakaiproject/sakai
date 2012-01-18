7448cb12b5d6eb6f6256f7f0d1a8d627a0c51a7e
Beth Kirschner <bkirschn@umich.edu>
2012-01-18 17:21:58 +0000

    SAK-21679 lesson builder oracle conversion
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@103246 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_9_0_oracle_conversion.sql
@@ -565,3 +565,126 @@ alter table MFR_TOPIC_T modify CONTEXT_ID varchar(255)
 
 
 -- END MSGCNTR conversion
+
+-- Start Lesson Builder table creation (if no auto.ddl)
+
+    create table lesson_builder_comments (
+        id number(19,0) not null,
+        itemId number(19,0) not null,
+        pageId number(19,0) not null,
+        timePosted date not null,
+        author varchar2(36) not null,
+        commenttext clob,
+        UUID varchar2(36) not null,
+        html number(1,0) not null,
+        points double precision,
+        primary key (id)
+    );
+
+    create table lesson_builder_groups (
+        id number(19,0) not null,
+        itemId varchar2(255) not null,
+        groupId varchar2(255) not null,
+        groups varchar2(500),
+        primary key (id)
+    );
+
+    create table lesson_builder_items (
+        id number(19,0) not null,
+        pageId number(19,0) not null,
+        sequence number(10,0) not null,
+        type number(10,0) not null,
+        sakaiId varchar2(250),
+        name varchar2(100),
+        html clob,
+        description varchar2(500),
+        height varchar2(8),
+        width varchar2(8),
+        alt varchar2(500),
+        nextPage number(1,0),
+        format varchar2(255),
+        required number(1,0),
+        alternate number(1,0),
+        prerequisite number(1,0),
+        subrequirement number(1,0),
+        requirementText varchar2(20),
+        sameWindow number(1,0),
+        groups varchar2(500),
+        anonymous number(1,0),
+        showComments number(1,0),
+        forcedCommentsAnonymous number(1,0),
+        gradebookId varchar2(35),
+        gradebookPoints number(10,0),
+        gradebookTitle varchar2(200),
+        altGradebook varchar2(35),
+        altPoints number(10,0),
+        altGradebookTitle varchar2(200),
+        primary key (id)
+    );
+
+    create table lesson_builder_log (
+        id number(19,0) not null,
+        lastViewed date not null,
+        itemId number(19,0) not null,
+        userId varchar2(255) not null,
+        firstViewed date not null,
+        complete number(1,0) not null,
+        dummy number(1,0) not null,
+        path varchar2(255),
+        toolId varchar2(250),
+        studentPageId number(19,0),
+        primary key (id)
+    );
+
+    create table lesson_builder_pages (
+        pageId number(19,0) not null,
+        toolId varchar2(250) not null,
+        siteId varchar2(250) not null,
+        title varchar2(100) not null,
+        parent number(19,0),
+        topParent number(19,0),
+        hidden number(1,0),
+        releaseDate date,
+        gradebookPoints double precision,
+        owner varchar2(36),
+        groupOwned number(1,0),
+        cssSheet varchar2(250),
+        primary key (pageId)
+    );
+
+    create table lesson_builder_student_pages (
+        id number(19,0) not null,
+        lastUpdated date not null,
+        itemId number(19,0) not null,
+        pageId number(19,0) not null,
+        title varchar2(100) not null,
+        owner varchar2(36) not null,
+        groupOwned number(1,0) not null,
+        commentsSection number(19,0),
+        lastCommentChange date,
+        deleted number(1,0),
+        points double precision,
+        primary key (id)
+    );
+
+    create sequence hibernate_sequence;
+
+-- End Lesson Builder table creation
+
+-- Start Lesson Builder Index creation
+
+create index lb_comments_itemid_author on lesson_builder_comments(itemId, author);
+create index lb_student_pages_pageId on lesson_builder_student_pages(pageId);
+create index lb_student_pages_itemId on lesson_builder_student_pages(itemId);
+create index lb_log_index on lesson_builder_log(userId,itemId, studentPageId);
+create index lb_student_pages_index on lesson_builder_student_pages(itemId, owner, deleted);
+create index lb_comments_uuid on lesson_builder_comments(UUID);
+create index lb_comments_author on lesson_builder_comments(pageId, author);
+create index lb_log_index3 on lesson_builder_log(itemId);
+create index lb_log_index2 on lesson_builder_log(userId,toolId);
+create index lb_groups_itemid on lesson_builder_groups(itemId);
+create index lb_pages_toolid on lesson_builder_pages(toolId, parent);
+create index lb_items_pageid on lesson_builder_items(pageId);
+create index lb_items_sakaiid on lesson_builder_items(sakaiId);
+
+-- End Lesson Builder Index creation
