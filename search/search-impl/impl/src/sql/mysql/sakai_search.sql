e28856f872276ab76c0bd36c83c98586c897bce0
David Horwitz <dhorwitz@gmail.com>
2012-01-21 08:08:47 +0000

    SRCH-78 remove dupliacte index
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@103390 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/sql/mysql/sakai_search.sql
+++ search/search-impl/impl/src/sql/mysql/sakai_search.sql
@@ -3,4 +3,4 @@ alter table searchbuilderitem add index isearchbuilderitem_name (name);
 alter table searchbuilderitem add index isearchbuilderitem_ctx (context);
 alter table searchbuilderitem add index isearchbuilderitem_act (searchaction);
 alter table searchbuilderitem add index isearchbuilderitem_sta (searchstate);
-alter table searchwriterlock add index isearchwriterlock_lk (lockkey);
+
--- search/search-impl/impl/src/sql/oracle/sakai_search.sql
+++ search/search-impl/impl/src/sql/oracle/sakai_search.sql
@@ -1,4 +1,3 @@
 create index isearchbuilderitem_ctx on  searchbuilderitem (context);
 create index isearchbuilderitem_act on  searchbuilderitem (searchaction);
 create index isearchbuilderitem_sta on  searchbuilderitem (searchstate);
-create index isearchwriterlock_lk on  searchwriterlock (lockkey);
