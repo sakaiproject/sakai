de250dccd1080925a9980eb9ce1148c17f48af17
Earle Nietzel <earle.nietzel@gmail.com>
2012-02-21 04:05:34 +0000

    SAK-20908 Fix files not saved as UTF8
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@104895 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/ShaUtil.java
+++ basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/ShaUtil.java
@@ -72,9 +72,9 @@ public class ShaUtil {
 	}
 
 	public static void main(String[] args) {
-		final String sample1 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'联＂挨Ε患协窃夜况瑺ǐ蠈赌汽律局巢调脥沤";
-		final String sample2 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'联＂挨Ε患协窃夜况瑺ǐ蠈赌汽律局巢调脥沤";
-		final String sample3 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'联＂挨Ε患协窃夜况瑺ǐ蠈赌汽律局巢调脥排";
+		final String sample1 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'脕陋拢垄掳陇娄楼禄录脨颅脟脭脪鹿驴枚卢聽篓芦脧聦露脛漏煤脝没脗脡戮脰鲁虏碌梅脙聧脜陆";
+		final String sample2 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'脕陋拢垄掳陇娄楼禄录脨颅脟脭脪鹿驴枚卢聽篓芦脧聦露脛漏煤脝没脗脡戮脰鲁虏碌梅脙聧脜陆";
+		final String sample3 = "12345:/sites/foo/bar !@#$%^&*()_+|}{\":?><[]\';/.,'脕陋拢垄掳陇娄楼禄录脨颅脟脭脪鹿驴枚卢聽篓芦脧聦露脛漏煤脝没脗脡戮脰鲁虏碌梅脙聧脜脜";
 		final String sample1Hash = sha1Hash(sample1);
 		final String sample2Hash = sha1Hash(sample2);
 		final String sample3Hash = sha1Hash(sample3);
