e34426e4e806cf286175d3fe2cd7b9538a8cb580
Sam Ottenhoff <ottenhoff@longsight.com>
2012-02-24 22:03:01 +0000

    SAK-20908 fixing bad UTF8 characters in the license header
    git-svn-id: https://source.sakaiproject.org/svn/providers/trunk@105075 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- providers/cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/ConfigurationTestBase.java
+++ providers/cm-authz-integration-test/src/test/java/org/sakaiproject/coursemanagement/test/ConfigurationTestBase.java
@@ -8,7 +8,7 @@
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at
  *
  *       http://www.osedu.org/licenses/ECL-2.0
  *
