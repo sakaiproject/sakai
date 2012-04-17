548413edad39eae695ff231b46d5a468ad1ee864
Aaron Zeckoski <azeckoski@vt.edu>
2012-04-17 17:20:50 +0000

    SAK-21857 restoring the missing gradebook provider
    git-svn-id: https://source.sakaiproject.org/svn/gradebook/trunk@107329 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/ui/helpers/entity/GradebookEntityProvider.java
+++ gradebook/app/ui/src/java/org/sakaiproject/tool/gradebook/ui/helpers/entity/GradebookEntityProvider.java
@@ -3,10 +3,11 @@ package org.sakaiproject.tool.gradebook.ui.helpers.entity;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.sakaiproject.entitybroker.IdEntityReference;
+import org.sakaiproject.entitybroker.EntityReference;
 import org.sakaiproject.entitybroker.entityprovider.CoreEntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.EntityProvider;
 import org.sakaiproject.entitybroker.entityprovider.EntityProviderManager;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
 import org.sakaiproject.tool.gradebook.ui.helpers.producers.GradebookItemProducer;
 import org.sakaiproject.tool.gradebook.ui.helpers.producers.AuthorizationFailedProducer;
 import org.sakaiproject.tool.gradebook.ui.helpers.params.GradebookItemViewParams;
@@ -18,25 +19,25 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 /*
  * This is a provider for looking up and adding/editing Gradebook Items.
+ * It is actually passing along to a gradebook UI via RSF and does not provide any rest access to grades data
  */
-public class GradebookEntityProvider implements EntityProvider, CoreEntityProvider,
-EntityViewParamsInferrer {
+public class GradebookEntityProvider implements EntityProvider, CoreEntityProvider, EntityViewParamsInferrer, Describeable {
     private Log log = LogFactory.getLog(GradebookEntityProvider.class);
     public final static String ENTITY_PREFIX = "gradebook";
     private EntityProviderManager entityProviderManager;
-    
+
     private GradebookService gradebookService;
-    
+
     public void init() {
         log.info("init()");
         entityProviderManager.registerEntityProvider(this);
     }
-    
+
     public void destroy() {
         log.info("destroy()");
         entityProviderManager.unregisterEntityProvider(this);
     }
-    
+
     public String getEntityPrefix() {
         return ENTITY_PREFIX;
     }
@@ -50,25 +51,25 @@ EntityViewParamsInferrer {
     }
 
     public ViewParameters inferDefaultViewParameters(String reference) {
-    	
-        IdEntityReference ep = new IdEntityReference(reference);
-    	String contextId = ep.id;
-    	
-    	if(gradebookService.currentUserHasEditPerm(contextId)){
-    		Long gradebookEntryId = null;
-    		return new GradebookItemViewParams(GradebookItemProducer.VIEW_ID, contextId, gradebookEntryId);
-    	}else{
-    		return new SimpleViewParameters(AuthorizationFailedProducer.VIEW_ID);
-    	}
+        //IdEntityReference ep = new IdEntityReference(reference);
+        //String contextId = ep.id;
+        String contextId = new EntityReference(reference).getId();
+
+        if(gradebookService.currentUserHasEditPerm(contextId)){
+            Long gradebookEntryId = null;
+            return new GradebookItemViewParams(GradebookItemProducer.VIEW_ID, contextId, gradebookEntryId);
+        }else{
+            return new SimpleViewParameters(AuthorizationFailedProducer.VIEW_ID);
+        }
 
     }
 
     public void setEntityProviderManager(EntityProviderManager entityProviderManager) {
         this.entityProviderManager = entityProviderManager;
     }
-    
+
     public void setGradebookService(GradebookService gradebookService) {
-    	this.gradebookService = gradebookService;
+        this.gradebookService = gradebookService;
     }
 
 }
