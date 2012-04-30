3b46b77c43b3a75439b759526be6b7b350b30ec8
Steve Swinsburg <steve.swinsburg@gmail.com>
2012-04-30 07:12:53 +0000

    SAK-22097 Mongolian translation
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@107703 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ reference/library/src/webapp/htmlarea/lang/mn.js
@@ -0,0 +1,17 @@
+// I18N constants
+
+// LANG: "en", ENCODING: UTF-8 | ISO-8859-1
+// Author:
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+ResourceSearch.I18N = {
+	"RS-resource-search"								: "Архивын нөөцөд хайлт хийх"
+};
