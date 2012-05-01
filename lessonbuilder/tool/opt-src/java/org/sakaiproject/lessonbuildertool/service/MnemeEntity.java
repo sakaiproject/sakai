45f97272460b644618289c1cdd7bbd11cf577a40
Charles Hedrick <hedrick@rutgers.edu>
2012-05-01 18:25:28 +0000

    LSNBLDR-67; fix mneme support for deleted items
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/trunk@107747 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- lessonbuilder/tool/opt-src/java/org/sakaiproject/lessonbuildertool/service/MnemeEntity.java
+++ lessonbuilder/tool/opt-src/java/org/sakaiproject/lessonbuildertool/service/MnemeEntity.java
@@ -165,6 +165,10 @@ public class MnemeEntity implements LessonEntity, QuizEntity {
 	Assessment ret = (Assessment)assessmentCache.get(id);
 
 	if (ret != null) {
+	    // don't check for published. Faculty may temporarily unpublish
+	    // to edit. We don't want it to disappear from students
+            if (ret.getArchived() || ret.getMint())
+		return null;
 	    return ret;
 	}
 
@@ -172,8 +176,15 @@ public class MnemeEntity implements LessonEntity, QuizEntity {
 	    return null;
 	ret = assessmentService.getAssessment(id);
 
-	if (ret != null) 
+	if (ret != null) {
+	    // cache it if we find it, even if not acceptable
 	    assessmentCache.put(id, ret, DEFAULT_EXPIRATION);
+
+	    // don't check for published. Faculty may temporarily unpublish
+	    // to edit. We don't want it to disappear from students
+            if (ret.getArchived() || ret.getMint())
+		return null;
+	}
 
 	return ret;
     }
