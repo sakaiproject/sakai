25383b60b1fe138e280e1ea99547e778b9d6854a
Bryan Holladay <holladay@longsight.com>
2012-05-09 13:08:22 +0000

    adding missing files for admin access feature
    git-svn-id: https://source.sakaiproject.org/svn/delegatedaccess/trunk@236564 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ delegatedaccess/tool/src/java/org/sakaiproject/delegatedaccess/tool/pages/EditablePanelEmpty.java
@@ -0,0 +1,11 @@
+package org.sakaiproject.delegatedaccess.tool.pages;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class EditablePanelEmpty extends Panel{
+
+	public EditablePanelEmpty(String id) {
+		super(id);
+	}
+
+}
