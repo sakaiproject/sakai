e7b292ec591b2647f9542993e6162673d6f19e80
David Horwitz <dhorwitz@gmail.com>
2012-05-21 12:52:53 +0000

    RES-44 patch from Bryan
    git-svn-id: https://source.sakaiproject.org/svn/reset-pass/trunk@108326 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
@@ -2,17 +2,24 @@ package org.sakaiproject.tool.resetpass;
 
 
 import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.tool.api.Placement;
+import org.sakaiproject.tool.api.ToolManager;
 
 import uk.org.ponder.rsf.components.UIContainer;
 import uk.org.ponder.rsf.components.UILink;
 import uk.org.ponder.rsf.components.UIMessage;
+import uk.org.ponder.rsf.components.UIVerbatim;
 import uk.org.ponder.rsf.view.ComponentChecker;
 import uk.org.ponder.rsf.view.ViewComponentProducer;
 import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 public class ConfirmProducer implements ViewComponentProducer {
 	public static final String VIEW_ID = "confirm";
-
+	private ToolManager toolManager;
+	public void setToolManager(ToolManager toolManager) {
+		this.toolManager = toolManager;
+	}
+	
 	public String getViewID() {
 		return VIEW_ID;
 	}
@@ -38,7 +45,12 @@ public class ConfirmProducer implements ViewComponentProducer {
 		} else {
 			UIMessage.make(tofill,"message","confirm.validate",parms);
 		}
-		if (serverConfigurationService.getString("support.email", null) != null) {
+		// Get the instructions from the tool placement.
+		Placement placement = toolManager.getCurrentPlacement();
+		String supportInstructions = placement == null ? "" :  placement.getConfig().getProperty("supportInstructions");
+		if(supportInstructions != null && !"".equals(supportInstructions)){
+			UIVerbatim.make(tofill, "supportMessage", supportInstructions);
+		}else if (serverConfigurationService.getString("support.email", null) != null) {
 			UIMessage.make(tofill, "supportMessage", "supportMessage");
 			UILink.make(tofill, "supportEmail",serverConfigurationService.getString("support.email", ""),"mailto:" + serverConfigurationService.getString("support.email", ""));
 		}
