6261c9ae724b93bc5474e39923771e6a69de69fe
Sam Ottenhoff <ottenhoff@longsight.com>
2012-05-31 15:22:34 +0000

    SAK-20983 removing accidental commit of System.out.println
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@108804 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/pageorder/tool/src/java/org/sakaiproject/site/tool/helper/order/impl/SitePageEditHandler.java
+++ site-manage/pageorder/tool/src/java/org/sakaiproject/site/tool/helper/order/impl/SitePageEditHandler.java
@@ -256,7 +256,6 @@ public class SitePageEditHandler {
      *
      */
     public String savePages () {
-System.out.println("state:" + state);
         if (state != null) {
             String[] pages = state.split(" ");
             for (int i = 0; i < pages.length; i++) {
