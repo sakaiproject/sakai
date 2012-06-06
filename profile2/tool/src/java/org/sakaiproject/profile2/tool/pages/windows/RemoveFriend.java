a51fab63c66e57d064df4a5bba7e7a087d61cb62
Steve Swinsburg <steve.swinsburg@gmail.com>
2012-06-06 04:02:09 +0000

    PRFL-737 move license for html outside of doctype, updated licenses at the same time
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@109043 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/windows/RemoveFriend.java
+++ profile2/tool/src/java/org/sakaiproject/profile2/tool/pages/windows/RemoveFriend.java
@@ -1,11 +1,11 @@
 /**
- * Copyright (c) 2008-2010 The Sakai Foundation
+ * Copyright (c) 2008-2012 The Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *             http://www.opensource.org/licenses/ECL-2.0
+ *             http://www.osedu.org/licenses/ECL-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.sakaiproject.profile2.tool.pages.windows;
 
 import org.apache.wicket.AttributeModifier;
