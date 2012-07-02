72baefa24eb7d74281c5c741858feff95443dcd8
Adrian Fish <a.fish@lancaster.ac.uk>
2012-07-02 09:08:23 +0000

    RSTR-47
    Spanish translation update from Samoo. Cheers :)
    
    git-svn-id: https://source.sakaiproject.org/svn/roster2/trunk@168681 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- roster2/roster-app/src/webapp/roster-translations-es.js
+++ roster2/roster-app/src/webapp/roster-translations-es.js
@@ -12,53 +12,87 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
-profile_email="Correo electr\u00F3nico";
-facet_name="Nombre";
-facet_userId="ID de usuario";
-title_msg="Para a\u00F1adir o quitar usuarios del listado, vaya a la herramienta Informaci\u00F3n del sitio.";
-//A\u00F1adido por la UPV
-// #############UPV Revision ###################################";
-profile_picture_alt="Fotograf\u00EDa de";
-// #Revisi\u00F3n de la versi\u00F3n 2.1.2";
-// ######### Revision 2.3.X ###########";
-facet_role="Rol";
-no_participants_msg="No hay participantes en esta orla.";
-no_permission_msg="No tiene permiso para ver la orla.";
-// ###### Revisi\u00F3n 2.5.X (UPV) #############";
-currently_displaying_participant="Mostrando actualmente participante {0} ";
-currently_displaying_participants="Mostrando actualmente {0} participantes";
-enrollment_status_filter="Ver alumnos en";
-enrollment_status_filter_label="Estado de la matr\u00EDcula";
-enrollments_currently_displaying="Actualmente, mostrando {0} {1} alumnos";
-export_roster="Exportar";
-facet_credits="Cr\u00E9ditos";
-facet_email="Direcci\u00F3n de correo elect\u00BFonico";
-facet_status="Estado";
-navbar_overview="Introducci\u00F3n";
+ */
+// Navigation bar links and tooltips
+navbar_overview="Informaci\u00F3n General";
+navbar_overview_tooltip="Informaci\u00F3n General de los miembros del sitio";
 navbar_pics="Fotograf\u00EDas";
-navbar_status="Estado de la matr\u00EDcula";
-no_participants="No se han encontrado resultados";
-print_roster="Imprimir";
-role_breakdown_fragment="{0} en el rol {1}";
-roster_clear_button="Limpiar";
-roster_enrollment_status_all="Todos";
-roster_hide_names="Ocultar nombres";
-roster_search_button="Buscar";
-roster_search_text="Nombre o identificador";
-roster_section_sep_line="---------------------";
-roster_sections_all="Todos disponibles";
-roster_show_names="Mostrar nombres";
-section_filter_post="Participantes";
-section_filter_pre="Ver";
-show_profile="Ver el perfil de este participante";
-title_overview="Introducci\u00F3n";
+navbar_pics_tooltip="Ver los miembros del sitio con sus fotograf\u00EDas";
+navbar_group_membership="Grupos";
+navbar_group_membership_tooltip="Ver los miembros del sitio organizados por grupo";
+navbar_status="Estado del usuario en el sitio";
+navbar_status_tooltip="Ver los miembros del sitio organizados por estado en el sitio";
+navbar_permissions="Permisos";
+navbar_permissions_tooltip="Ver y editar los permisos de los usuarios para este sitio";
+// Page titles
+title_overview="Informaci\u00F3n General";
 title_pictures="Fotograf\u00EDas";
-title_status="Estado de la matr\u00EDcula";
+title_group_membership="Grupos";
+title_status="Estado del usuario en el sitio";
+title_permissions="Permisos por rol";
+
+// Search
+roster_search_text="Nombre o Identificador";
+roster_search_button="Buscar";
+roster_clear_button="Limpiar";
+// Participants drop-down
+roster_sections_all="Todos los grupos";
+roster_section_sep_line="---------------------";
+// Group membership drop-down
+roster_group_ungrouped="Sin Grupo";
+roster_group_bygroup="Por Grupo";
+roster_group_unassigned="Sin Asignar";
+// Enrollment drop-down
+roster_enrollment_status_all="Todos";
+// Participants and roles
+currently_displaying_participants="Mostrando {0} participantes";
+currently_displaying_participant="Mostrando {0} participante";
+role_breakdown_fragment="{0} con el rol {1}";
+enrollments_currently_displaying="Mostrando {0} {1} estudiantes";
+// Filters
+section_filter_pre="Ver";
+section_filter_post="Participantes";
+enrollment_status_filter="Ver estudiantes en";
+enrollment_status_filter_label="Estado en el sitio";
+// View table/single column button
+view_table="Ver en tabla";
+view_single="Ver en una columna";
+// Print and export buttons
+print_roster="Imprimir";
+export_roster="Exportar";
+// Show/hide names button
+roster_show_names="Mostrar Nombres";
+roster_hide_names="Ocultar Nombres";
+// Official/Profile radio
+roster_show_official_pictures="Fotograf\u00EDas Oficiales";
+roster_show_profile_pictures="Fotograf\u00EDas del Perfil";
+// Profile
+show_profile="Ver el perfil de este participante";
+profile_connection_accept="Aceptar contacto";
+profile_connection_add="A\u00F1adir contacto";
+profile_connection_remove="Eliminar contacto";
+profile_connection_requested="Contacto solicitado";
+profile_connection_self="Eres t\u00FA!"
+profile_email="Correo Electr\u00F3nico";
+profile_picture_alt="Foto de";
+// Facets
+facet_name="Nombre";
+facet_userId="Identificador de usuario";
+facet_email="Correo Electr\u00F3nico";
+facet_role="Rol";
+facet_status="Estado";
+facet_credits="Cr\u00E9ditos";
 facet_groups="Grupos";
-navbar_group_membership="Pertenencia a grupos";
-roster_group_bygroup="Por grupo";
-roster_group_unassigned="Sin asignar";
-roster_group_ungrouped="Sin grupo";
-title_group_membership="Pertenencia a grupos";
-title_msg_groups="Para a\u00F1adir o borrar participantes de Grupos, vaya la Informaci\u00F3n del Sitio.";
+// Page title messages
+title_msg="Para a\u00F1adir o borrar participantes del sitio, vaya a la herramienta Informaci\u00F3n del Sitio.";
+title_msg_groups="Para a\u00F1adir o borrar participantes de Grupos, vaya a la herramienta Informaci\u00F3n del Sitio.";
+title_msg_permissions="Definir los permisos para los roles en este sitio."
+no_participants="No se han encontrado resultados";
+// TODO this is currently unused but could display the search term entered
+no_participants_msg="No se han encontrado resultados para \"{0}\"";
+// TODO this is currently unused, but could be used to display a message
+no_permission_msg="No tienes permisos para ver la Orla.";
+// Permissions
+roster_permissions_role="Rol"
+roster_save_button="Guardar"
+roster_cancel_button="Cancelar"
