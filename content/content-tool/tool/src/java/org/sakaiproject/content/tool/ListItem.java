02edf93599f752dbe3aaf1ba659e1c33b6b2f93a
Savitha Prakash <savithap@umich.edu>
2012-08-02 18:38:26 +0000

    SAK-22403: Resources tool generates Concurrent Mode Failure when expanding large Admin Resources folders
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@111011 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
@@ -670,7 +670,7 @@ public class ListItem
 			//Prevent concurrent mode failures in admin Resource tool when clicking on resources that are too large.  Similar to 'isTooBig' but defined in properties
 			//To enable add the property sakai.content.resourceLimit=<int> to sakai.properties where int is the limit of an accessible resource folder
 			String siteId = ToolManager.getCurrentPlacement().getContext();
-			if( SiteService.getUserSiteId("admin").contains(siteId) && this.EXPANDABLE_FOLDER_NAV_SIZE_LIMIT != 0 && (collection_size > this.EXPANDABLE_FOLDER_NAV_SIZE_LIMIT))
+			if(this.EXPANDABLE_FOLDER_NAV_SIZE_LIMIT != 0 && (siteId.equals("!admin") || SiteService.getUserSiteId("admin").contains(siteId)) && (collection_size > this.EXPANDABLE_FOLDER_NAV_SIZE_LIMIT))
 			{
 				setIsTooBigNav(true);
 			}
