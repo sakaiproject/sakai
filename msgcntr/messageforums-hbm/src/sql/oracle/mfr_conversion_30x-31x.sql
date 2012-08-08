535eeb28dd7d9c1e00717a074bdda3ce1b9f1fc2
Michelle Wagner <wagnermr@iu.edu>
2012-08-08 20:29:46 +0000

    MSGCNTR-708 Conversion required for new settings for controlling sending email copies of messages
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@111156 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-hbm/src/sql/oracle/mfr_conversion_30x-31x.sql
+++ msgcntr/messageforums-hbm/src/sql/oracle/mfr_conversion_30x-31x.sql
@@ -1,7 +1,7 @@
 -- MSGCNTR-708 - change the boolean SENDEMAILOUT functionality to a multiple
 -- option setting for finer grained control of email copies of messages
 
-alter table MFR_AREA_T add column SEND_TO_EMAIL int(11) not null DEFAULT 1;
+alter table MFR_AREA_T add SEND_TO_EMAIL NUMBER(10,0) default 1 not null;
 
 -- set the SEND_TO_EMAIL column equal to the equivalent value for SENDEMAILOUT
 update MFR_AREA_T area set area.SEND_TO_EMAIL=0 
