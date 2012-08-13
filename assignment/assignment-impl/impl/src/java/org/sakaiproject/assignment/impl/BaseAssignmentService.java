sakaiproject/sakai

commit 0b318d066baa5f205f2a4b8e81a064b59c531016

SAK-22035 no need to check for the group membership again within the checkAssignmentAccessibleForUsers() function, since the group membership should be handled by the unlock() checks already
git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@106654 66ffb92e-73f9-0310-93c1-f5514f145a0a

assignment/assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
@@ -955,14 +955,6 @@ public abstract class BaseAssignmentService implements AssignmentService, Entity
 
 	private Assignment checkAssignmentAccessibleForUser(Assignment assignment, String currentUserId) throws PermissionException {
 		
-		if (assignment.getAccess() == Assignment.AssignmentAccess.GROUPED)
-        {
-        	Collection asgGroups = assignment.getGroups();
-        	Collection allowedGroups = getGroupsAllowGetAssignment(assignment.getContext(), currentUserId);
-        	// reject and throw PermissionException if there is no intersection
-			if (!isIntersectionGroupRefsToGroups(asgGroups, allowedGroups)) throw new PermissionException(currentUserId, SECURE_ACCESS_ASSIGNMENT, assignment.getReference());
-        }
-		
 		if (allowAddAssignment(assignment.getContext()))
 		{
 			// always return for users can add assignent in the context

commit 192aeabe5cf4db5ea174075e92423e12b88315a7

SAK-22035 fixed the bug (SAK-22360) introduced by r106654 checkin by adding the group level checkins back
git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@111279 66ffb92e-73f9-0310-93c1-f5514f145a0a

assignment/assignment-impl/impl/src/java/org/sakaiproject/assignment/impl/BaseAssignmentService.java
@@ -980,6 +980,15 @@ public abstract class BaseAssignmentService implements AssignmentService, Entity
 
 	private Assignment checkAssignmentAccessibleForUser(Assignment assignment, String currentUserId) throws PermissionException {
 		
+		if (assignment.getAccess() == Assignment.AssignmentAccess.GROUPED)
+        {
+			String context = assignment.getContext();
+        	Collection asgGroups = assignment.getGroups();
+        	Collection allowedGroups = getGroupsAllowGetAssignment(context, currentUserId);
+        	// reject and throw PermissionException if there is no intersection
+			if (!allowAllGroups(context) && !isIntersectionGroupRefsToGroups(asgGroups, allowedGroups)) throw new PermissionException(currentUserId, SECURE_ACCESS_ASSIGNMENT, assignment.getReference());
+        }
+		
 		if (allowAddAssignment(assignment.getContext()))
 		{
 			// always return for users can add assignent in the context