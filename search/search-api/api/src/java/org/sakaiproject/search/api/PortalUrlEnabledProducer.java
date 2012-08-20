403ed3c72c97927a7cac6425386f0d8620d1a4c4
David Horwitz <dhorwitz@gmail.com>
2012-08-20 11:38:44 +0000

    SAK-15580 add since anotation
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@111633 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-api/api/src/java/org/sakaiproject/search/api/PortalUrlEnabledProducer.java
+++ search/search-api/api/src/java/org/sakaiproject/search/api/PortalUrlEnabledProducer.java
@@ -4,6 +4,7 @@ package org.sakaiproject.search.api;
  * Optional interface to extend EntityContentProducer if implemented the ContentProducer should
  * return valid portal urls 
  * @author dhorwitz
+ * @since 1.2.0
  *
  */
 public interface PortalUrlEnabledProducer {
