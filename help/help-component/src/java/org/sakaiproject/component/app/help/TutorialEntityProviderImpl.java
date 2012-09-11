1288c11435dfb89d626c3772c264d3d92216c62e
Bryan Holladay <holladay@longsight.com>
2012-09-11 15:13:25 +0000

    SAK-22568 Seperate i18n and config properties for Tutorial Tool
    git-svn-id: https://source.sakaiproject.org/svn/help/trunk@112285 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- help/help-component/src/java/org/sakaiproject/component/app/help/TutorialEntityProviderImpl.java
+++ help/help-component/src/java/org/sakaiproject/component/app/help/TutorialEntityProviderImpl.java
@@ -1,9 +1,15 @@
 package org.sakaiproject.component.app.help;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.reloading.InvariantReloadingStrategy;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.api.app.help.TutorialEntityProvider;
 import org.sakaiproject.entitybroker.EntityReference;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
@@ -15,7 +21,25 @@ import org.sakaiproject.util.ResourceLoader;
 
 public class TutorialEntityProviderImpl implements TutorialEntityProvider, AutoRegisterEntityProvider, RESTful{
 
+	protected final Log log = LogFactory.getLog(getClass());
 	private ResourceLoader msgs = new ResourceLoader("TutorialMessages");
+	private static PropertiesConfiguration tutorialProps;
+	
+	private void initConfig() {
+		
+		URL url = getClass().getClassLoader().getResource("Tutorial.config"); 
+		
+		try {
+			tutorialProps = new PropertiesConfiguration(); //must use blank constructor so it doesn't parse just yet (as it will split)
+			tutorialProps.setReloadingStrategy(new InvariantReloadingStrategy());	//don't watch for reloads
+			tutorialProps.setThrowExceptionOnMissing(false);	//throw exception if no prop
+			tutorialProps.setDelimiterParsingDisabled(true); //don't split properties
+			tutorialProps.load(url); //now load our file
+		} catch (ConfigurationException e) {
+			log.error(e.getClass() + ": " + e.getMessage());
+			return;
+		}
+	}
 	
 	@Override
 	public String getEntityPrefix() {
@@ -40,15 +64,21 @@ public class TutorialEntityProviderImpl implements TutorialEntityProvider, AutoR
 
 	@Override
 	public Object getEntity(EntityReference ref) {
-		String previousUrl = msgs.getString(ref.getId() + ".previousUrl");
-		String nextUrl = msgs.getString(ref.getId() + ".nextUrl");
+		if(tutorialProps == null){
+			initConfig();
+			if(tutorialProps == null){
+				return null;
+			}
+		}
+		String previousUrl = tutorialProps.getString(ref.getId() + ".previousUrl");
+		String nextUrl = tutorialProps.getString(ref.getId() + ".nextUrl");
 		Map valuesMap = new HashMap<String, String>();
-		valuesMap.put("selection", msgs.get(ref.getId() + ".selection"));
+		valuesMap.put("selection", tutorialProps.getString(ref.getId() + ".selection"));
 		valuesMap.put("title", msgs.get(ref.getId() + ".title"));
-		valuesMap.put("dialog", msgs.get(ref.getId() + ".dialog"));
-		valuesMap.put("positionTooltip", msgs.get(ref.getId() + ".positionTooltip"));
-		valuesMap.put("positionTarget", msgs.get(ref.getId() + ".positionTarget"));
-		valuesMap.put("fadeout", msgs.get(ref.getId() + ".fadeout"));
+		valuesMap.put("dialog", tutorialProps.getString(ref.getId() + ".dialog"));
+		valuesMap.put("positionTooltip", tutorialProps.getString(ref.getId() + ".positionTooltip"));
+		valuesMap.put("positionTarget", tutorialProps.getString(ref.getId() + ".positionTarget"));
+		valuesMap.put("fadeout", tutorialProps.getString(ref.getId() + ".fadeout"));
 		valuesMap.put("previousUrl", previousUrl);
 		valuesMap.put("nextUrl", nextUrl);
 		
