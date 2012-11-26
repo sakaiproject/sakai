77ff797cbc20f88064d14c37bb731bd09dfa3f62
Bryan Holladay <holladay@longsight.com>
2012-11-26 15:32:28 +0000

    DAC-46 improve instructor's interface for shopping period fixing use case for DA user seeing "remove" links for "Logged In" tab
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@116779 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/webapp/js/site-manage-delegatedaccess.js
+++ site-manage/site-manage-tool/tool/src/webapp/js/site-manage-delegatedaccess.js
@@ -280,8 +280,6 @@ function setupTools(data, tools){
 					setShoppingToolOptionsEnabled(true, function(){setShoppingToolOptionsEnabled(false);});
 					return false;
 				}); 
-				
-				setShoppingToolOptionsEnabled(true, function(){setShoppingToolOptionsEnabled(false);});
 			}
 		);
 }
