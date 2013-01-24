3cbf49ae49e22ccde705e556f32a27e40ce35323
Aaron Zeckoski <azeckoski@vt.edu>
2013-01-24 22:28:39 +0000

    SAK-23164 updated the conversion script table name case Changes from Cynthia Walters (UVA)
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@118781 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_8_2-2_8_3_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_8_2-2_8_3_mysql_conversion.sql
@@ -10,17 +10,18 @@
 -- SQL statement
 -- --------------------------------------------------------------------------------------------------------------------------------------
 
+
 -- This file contains SQL to fix SAK-22700 ("The citations importer in Resources doesn't always correctly import RIS- formatted files")
-update citation_schema_field set PROPERTY_VALUE='A1,AU' where FIELD_ID='creator' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='BT,T1,TI' where SCHEMA_ID='book' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='CT,T1,TI' where SCHEMA_ID='chapter' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='ED,A2,A3' where FIELD_ID='editor' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='JF,JO,JA,J1,J2,BT' where SCHEMA_ID='article' and FIELD_ID='sourceTitle' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='N1,AB' where FIELD_ID='note' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='N2,AB' where FIELD_ID='abstract' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='SP' where FIELD_ID='pages' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='T1,TI,CT,BT' where SCHEMA_ID='unknown' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='T1,TI,CT' where SCHEMA_ID='article' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='T1,TI' where SCHEMA_ID='report' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='Y1,PY' where FIELD_ID='year' and PROPERTY_NAME='sakai:ris_identifier';
-update citation_schema_field set PROPERTY_VALUE='Y1,PY' where FIELD_ID='date' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='A1,AU' where FIELD_ID='creator' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='BT,T1,TI' where SCHEMA_ID='book' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='CT,T1,TI' where SCHEMA_ID='chapter' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='ED,A2,A3' where FIELD_ID='editor' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='JF,JO,JA,J1,J2,BT' where SCHEMA_ID='article' and FIELD_ID='sourceTitle' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='N1,AB' where FIELD_ID='note' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='N2,AB' where FIELD_ID='abstract' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='SP' where FIELD_ID='pages' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='T1,TI,CT,BT' where SCHEMA_ID='unknown' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='T1,TI,CT' where SCHEMA_ID='article' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='T1,TI' where SCHEMA_ID='report' and FIELD_ID='title' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='Y1,PY' where FIELD_ID='year' and PROPERTY_NAME='sakai:ris_identifier';
+update CITATION_SCHEMA_FIELD set PROPERTY_VALUE='Y1,PY' where FIELD_ID='date' and PROPERTY_NAME='sakai:ris_identifier';
