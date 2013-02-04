fb89fcc9b2218bc4e29b43874f1816c53ee87d27
Michelle Wagner <wagnermr@iu.edu>
2013-02-04 21:06:54 +0000

    MSGCNTR-780 Resolve case issues in local conversion script
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/trunk@119494 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- msgcntr/messageforums-hbm/src/sql/mysql/mfr_conversion_28x-30x.sql
+++ msgcntr/messageforums-hbm/src/sql/mysql/mfr_conversion_28x-30x.sql
@@ -145,74 +145,74 @@ alter table MFR_MESSAGE_T add column RECIPIENTS_AS_TEXT_BCC TEXT;
 
 -- MSGCNTR-503 - Internationalization of message priority
 -- Default locale
-update mfr_message_t set label='pvt_priority_high' where label='High';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='Low';
+update MFR_MESSAGE_T set label='pvt_priority_high' where label='High';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Low';
 
 -- Locale ar
-update  mfr_message_t set label='pvt_priority_high' where label='\u0645\u0631\u062A\u0641\u0639';
-update  mfr_message_t set label='pvt_priority_normal' where label='\u0639\u0627\u062F\u064A';
-update  mfr_message_t set label='pvt_priority_low' where label='\u0645\u0646\u062E\u0641\u0636';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='\u0645\u0631\u062A\u0641\u0639';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='\u0639\u0627\u062F\u064A';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='\u0645\u0646\u062E\u0641\u0636';
 
 -- Locale ca
-update  mfr_message_t set label='pvt_priority_high' where label='Alta';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='Baixa';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Alta';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Baixa';
 
 -- Locale es
-update  mfr_message_t set label='pvt_priority_high' where label='Alta';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='Baja';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Alta';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Baja';
 
 -- Locale eu
-update  mfr_message_t set label='pvt_priority_high' where label='Gutxikoa';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normala';
-update  mfr_message_t set label='pvt_priority_low' where label='Handikoa';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Gutxikoa';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normala';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Handikoa';
 
 -- Locale fr_CA
-update  mfr_message_t set label='pvt_priority_high' where label='\u00C9lev\u00E9e';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normale';
-update  mfr_message_t set label='pvt_priority_low' where label='Basse';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='\u00C9lev\u00E9e';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normale';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Basse';
 
 -- Locale fr_FR
-update  mfr_message_t set label='pvt_priority_high' where label='Elev\u00E9e';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normale';
-update  mfr_message_t set label='pvt_priority_low' where label='Basse';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Elev\u00E9e';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normale';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Basse';
 
 -- Locale ja
-update  mfr_message_t set label='pvt_priority_high' where label='\u9ad8\u3044';
-update  mfr_message_t set label='pvt_priority_normal' where label='\u666e\u901a';
-update  mfr_message_t set label='pvt_priority_low' where label='\u4f4e\u3044';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='\u9ad8\u3044';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='\u666e\u901a';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='\u4f4e\u3044';
 
 -- Locale nl
-update  mfr_message_t set label='pvt_priority_high' where label='Hoog';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normaal';
-update  mfr_message_t set label='pvt_priority_low' where label='Laag';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Hoog';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normaal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Laag';
 
 -- Locale pt_BR
-update  mfr_message_t set label='pvt_priority_high' where label='Alta';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='Baixa';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Alta';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Baixa';
 
 -- Locale pt_PT
-update  mfr_message_t set label='pvt_priority_high' where label='Alta';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='Baixa';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='Alta';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='Baixa';
 
 -- Locale ru
-update  mfr_message_t set label='pvt_priority_high' where label='\u0412\u044b\u0441\u043e\u043a\u0438\u0439';
-update  mfr_message_t set label='pvt_priority_normal' where label='\u041e\u0431\u044b\u0447\u043d\u044b\u0439';
-update  mfr_message_t set label='pvt_priority_low' where label='\u041d\u0438\u0437\u043a\u0438\u0439';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='\u0412\u044b\u0441\u043e\u043a\u0438\u0439';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='\u041e\u0431\u044b\u0447\u043d\u044b\u0439';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='\u041d\u0438\u0437\u043a\u0438\u0439';
 
 -- Locale sv
-update  mfr_message_t set label='pvt_priority_high' where label='H\u00F6g';
-update  mfr_message_t set label='pvt_priority_normal' where label='Normal';
-update  mfr_message_t set label='pvt_priority_low' where label='L\u00E5g';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='H\u00F6g';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='Normal';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='L\u00E5g';
 
 -- Locale zh_TW
-update  mfr_message_t set label='pvt_priority_high' where label='\u9ad8';
-update  mfr_message_t set label='pvt_priority_normal' where label='\u666e\u901a';
-update  mfr_message_t set label='pvt_priority_low' where label='\u4f4e';
+update  MFR_MESSAGE_T set label='pvt_priority_high' where label='\u9ad8';
+update  MFR_MESSAGE_T set label='pvt_priority_normal' where label='\u666e\u901a';
+update  MFR_MESSAGE_T set label='pvt_priority_low' where label='\u4f4e';
 
 -- END MSGCNTR-503 --
 
@@ -233,7 +233,7 @@ CREATE TABLE MFR_HIDDEN_GROUPS_T  (
 ALTER TABLE MFR_HIDDEN_GROUPS_T
     ADD CONSTRAINT FK1DDE4138A306F94D
     FOREIGN KEY(a_surrogateKey)
-    REFERENCES mfr_area_t(ID)
+    REFERENCES MFR_AREA_T(ID)
     ON DELETE RESTRICT 
     ON UPDATE RESTRICT;
 
