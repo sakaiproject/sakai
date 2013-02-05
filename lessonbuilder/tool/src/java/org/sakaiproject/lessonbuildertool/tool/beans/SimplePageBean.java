92795898358076ba0acfa46a2c4ac084605139e4
Charles Hedrick <hedrick@rutgers.edu>
2013-02-05 17:47:38 +0000

    LSNBLDR-202; can't complete subpage if it has a	required item not visible to you
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/trunk@119515 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
@@ -4117,7 +4117,7 @@ public class SimplePageBean {
 		List<SimplePageItem> items = getItemsOnPage(Long.valueOf(findItem(itemId).getSakaiId()));
 
 		for (SimplePageItem item : items) {
-			if (!isItemComplete(item)) {
+			if (!isItemComplete(item) && isItemVisible(item)) {
 			    if (item.getType() == SimplePageItem.PAGE) {
 				// If we get here, must be not completed or isItemComplete would be true
 				SimplePageLogEntry entry = getLogEntry(item.getId());
@@ -4187,7 +4187,7 @@ public class SimplePageBean {
 			if (i.getSakaiId().equals(currentPageId)) {
 				return needed;  // reached current page. we're done
 			}
-			if (i.isRequired() && !isItemComplete(i))
+			if (i.isRequired() && !isItemComplete(i) && isItemVisible(i))
 				needed.add(i.getName());
 		}
 
