0d4db27b09ba1ca47cffc2a944888d33e4e13fed
Matthew Buckett <buckett@bumph.org>
2013-02-15 16:46:10 +0000

    SAK-22519 Correctly detect a maintainer when uploading file to dropbox.
    When a maintainer uploaded a new file to a dropbox and checked notification the id was set without the path which meant that the dropboxId couldn't be found. This makes it fallback to the containingCollectionId if the lookup failed.
    
    git-svn-id: https://source.sakaiproject.org/svn/content/trunk@120126 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/ListItem.java
@@ -4065,11 +4065,14 @@ public class ListItem
 		if(this.isDropbox)
 		{
 			String dropboxId = null;
+			// When modifying an ListItem's properties the id contains the full path.
 			if(id != null && !id.trim().equals(""))
 			{
 				dropboxId = getIndividualDropboxId(id);
 			}
-			else if(containingCollectionId != null && ! containingCollectionId.trim().equals(""))
+			// When uploading a new item the ListItem's id just contains the filename and the containingCollectionId
+			// contains the dropbox into which it is going.
+			if(dropboxId == null && containingCollectionId != null && ! containingCollectionId.trim().equals(""))
 			{
 				dropboxId = getIndividualDropboxId(containingCollectionId);
 			}
