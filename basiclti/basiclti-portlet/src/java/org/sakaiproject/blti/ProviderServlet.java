49a7effeea410cd064fa43de80c8c858fbe955c4
Charles Severance <csev@umich.edu>
2013-02-21 21:47:33 +0000

    BLTI-212
    Remove the need for 
    
    webservices.allow=.+ 
    
    To use LTI services.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@120370 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-portlet/src/java/org/sakaiproject/blti/ProviderServlet.java
+++ basiclti/basiclti-portlet/src/java/org/sakaiproject/blti/ProviderServlet.java
@@ -78,10 +78,6 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  * so it must be carefully reviewed and any changes must be looked at carefully.
  * Here are some issues:
  * 
- * - This uses the RemoteHostFilter so by default it only accepts local IP
- * addresses. This configuration can be changed in web.xml or using the
- * webservices.allow, etc (see RemoteHostFilter)
- * 
  * - This will only function when it is enabled via sakai.properties
  * 
  * - This servlet makes use of security advisors - once an advisor has been
