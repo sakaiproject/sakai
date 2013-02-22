fade17f618462e09e5e527f4119a68e1265915a1
Gonzalo Silverio <gsilver@umich.edu>
2013-02-22 21:03:26 +0000

    SAK-23273 - correct _View All_ tool menu rendering
    git-svn-id: https://source.sakaiproject.org/svn/portal/trunk@120409 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- portal/portal-charon/charon/src/webapp/scripts/neoscripts.js
+++ portal/portal-charon/charon/src/webapp/scripts/neoscripts.js
@@ -23,7 +23,6 @@ var dhtml_view_sites = function(){
             });
             createDHTMLMask(dhtml_view_sites);
             jQuery('.selectedTab').bind('click', function(e){
-                console.log(e.pageX)
                 dhtml_view_sites();
                 return false;
             });
@@ -567,7 +566,7 @@ var setupSiteNav = function(){
             var siteId = jqObjDrop.attr('data').replace(/!/g, '\\!').replace(/~/g, '\\~');
             var maxToolsInt = parseInt($('#maxToolsInt').text());
             var maxToolsText = $('#maxToolsAnchor').text();
-            var goToSite = '<li class=\"icon-sakai-see-all-tools\"><span><a role=\"menuitem\" class=\"icon-sakai-see-all-tools\" href=\"' + portal.portalPath + '/site/' + siteId + '\" title=\"' + maxToolsText + '\">' + maxToolsText + '</a></span></li>';
+            var goToSite = '<li class=\"submenuitem\"><span><a role=\"menuitem\" class=\"icon-sakai-see-all-tools\" href=\"' + portal.portalPath + '/site/' + siteId + '\" title=\"' + maxToolsText + '\">' + maxToolsText + '</a></span></li>';
             var siteURL = '/direct/site/' + jqObjDrop.attr('data') + '/pages.json';
             jQuery.ajax({
                 url: siteURL,
@@ -581,7 +580,7 @@ var setupSiteNav = function(){
                         }
                     });
                     if ((data.length - 1) > maxToolsInt) {
-                        navsubsubmenu = navsubmenu + goToSite
+                        navsubmenu = navsubmenu + goToSite
                     }
                     navsubmenu = navsubmenu + "</ul>"
                     jqObjDrop.after(navsubmenu);
