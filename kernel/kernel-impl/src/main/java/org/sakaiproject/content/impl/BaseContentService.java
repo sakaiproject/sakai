83d6dceb08913084bc450bbbe0830279b4621778
Bryan Holladay <holladay@longsight.com>
2013-03-06 15:46:33 +0000

    SAK-22283 iSyllabus linking and link migration SAK-23041 Link Migration Improvements
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@120846 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -169,7 +169,7 @@ import org.xml.sax.SAXException;
 
 import com.ibm.icu.text.CharsetDetector;
 import com.ibm.icu.text.CharsetMatch;
-import org.sakaiproject.util.LinkMigrationHelper;
+import org.sakaiproject.util.cover.LinkMigrationHelper;
 
 /**
  * <p>
@@ -7467,31 +7467,12 @@ SiteContentAdvisorProvider, SiteContentAdvisorTypeRegistry, EntityTransferrerRef
 							newValue = (String) transversalMap.get(oldValue);
 							targetId = (String) transversalMap.get(oldValue);
 							if(newValue.length()>0){
-								try {
-									rContent = LinkMigrationHelper.editLinks(rContent, "sam_pub");
-									rContent = LinkMigrationHelper.editLinks(rContent, "/posts/");
-								} catch (Exception e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
+								rContent = LinkMigrationHelper.migrateOneLink(oldValue, newValue, rContent);
 								}
-/*									
-								oldValue = oldValue.replace(" ",ESCAPED_SPACE);
-								newValue = newValue.replace(" ",ESCAPED_SPACE);
-								rContent = rContent.replaceAll(oldValue, newValue);
-*/
-								rContent = LinkMigrationHelper.miagrateOneLink(oldValue, newValue, rContent);
-	
-/*
-								if(!saveOldEntity.toString().equals(rContent)){
-									contentChanged=true;
 								}
-*/
 							}
-						}
-					}
 					try {
-						rContent = LinkMigrationHelper.bracketLinks(rContent, "assignment");
-						rContent = LinkMigrationHelper.bracketLinks(rContent, "forum");
+						rContent = LinkMigrationHelper.bracketAndNullifySelectedLinks(rContent);
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						M_log.debug ("Forums LinkMigrationHelper.editLinks failed" + e);
