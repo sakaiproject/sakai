06ea84d7f06cc9e53d0cd6e30b07c31c5a2c5bd4
David Horwitz <dhorwitz@gmail.com>
2013-03-08 15:03:55 +0000

    SAK-23329 remove calls to getGradebook(String) in the 2 cases where the result is never read
    git-svn-id: https://source.sakaiproject.org/svn/edu-services/trunk@120933 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookExternalAssessmentServiceImpl.java
+++ edu-services/gradebook-service/impl/src/java/org/sakaiproject/component/gradebook/GradebookExternalAssessmentServiceImpl.java
@@ -487,7 +487,7 @@ public class GradebookExternalAssessmentServiceImpl extends BaseHibernateManager
 	public Map<String, String> getExternalAssignmentsForCurrentUser(String gradebookUid)
 		throws GradebookNotFoundException
 	{
-		final Gradebook gradebook = getGradebook(gradebookUid);
+		
 		Map<String, String> allAssignments = new HashMap<String, String>();
 		for (ExternalAssignmentProvider provider : getExternalAssignmentProviders().values()) {
 			String appKey = provider.getAppKey();
@@ -502,7 +502,7 @@ public class GradebookExternalAssessmentServiceImpl extends BaseHibernateManager
 	public Map<String, List<String>> getVisibleExternalAssignments(String gradebookUid, Collection<String> studentIds)
 		throws GradebookNotFoundException
 	{
-		final Gradebook gradebook = getGradebook(gradebookUid);
+		
 		Map<String, Set<String>> visible = new HashMap<String, Set<String>>();
 		for (String studentId : studentIds) {
 			visible.put(studentId, new HashSet<String>());
