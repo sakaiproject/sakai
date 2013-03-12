14513182f6b356442d837e8edb70d4f6f89c72cf
Adrian Fish <a.fish@lancaster.ac.uk>
2013-03-12 09:39:05 +0000

    PRFL-807
    Missed adding this file. Doh!
    
    
    git-svn-id: https://source.sakaiproject.org/svn/profile2/trunk@121049 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ profile2/api/src/java/org/sakaiproject/profile2/model/MyProfilePanelState.java
@@ -0,0 +1,10 @@
+package org.sakaiproject.profile2.model;
+
+public class MyProfilePanelState {
+	
+	public boolean showStudentDisplay = true;
+	public boolean showSocialNetworkingDisplay = true;
+	public boolean showBusinessDisplay = true;
+	public boolean showStaffDisplay = true;
+	public boolean showInterestsDisplay = true;
+}
