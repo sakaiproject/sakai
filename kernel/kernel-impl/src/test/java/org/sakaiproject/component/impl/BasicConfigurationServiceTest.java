6b6ce3bb51d5fc6c0324c747849173368f237a63
Aaron Zeckoski <azeckoski@vt.edu>
2013-03-18 20:59:36 +0000

    KNL-1038 added unit test to confirm the failure reported in trunk
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@121412 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/test/java/org/sakaiproject/component/impl/BasicConfigurationServiceTest.java
+++ kernel/kernel-impl/src/test/java/org/sakaiproject/component/impl/BasicConfigurationServiceTest.java
@@ -1,9 +1,9 @@
 /**********************************************************************************
-*
-* $Id: BasicConfigurationServiceTest.java 107118 2012-04-16 14:41:07Z azeckoski@unicon.net $
-*
-***********************************************************************************
-*
+ *
+ * $Id: BasicConfigurationServiceTest.java 107118 2012-04-16 14:41:07Z azeckoski@unicon.net $
+ *
+ ***********************************************************************************
+ *
  * Copyright (c) 2007, 2008 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
@@ -17,8 +17,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*
-**********************************************************************************/
+ *
+ **********************************************************************************/
 
 package org.sakaiproject.component.impl;
 
@@ -34,12 +34,12 @@ import org.sakaiproject.component.api.ServerConfigurationService.ConfigData;
 public class BasicConfigurationServiceTest extends TestCase {
 
     private static Log log = LogFactory.getLog(BasicConfigurationServiceTest.class);
-	
-	private BasicConfigurationService basicConfigurationService;
-	private String SOURCE = "TEST";
-	
-	public void setUp() throws Exception {
-	    basicConfigurationService = new BasicConfigurationService();
+
+    private BasicConfigurationService basicConfigurationService;
+    private String SOURCE = "TEST";
+
+    public void setUp() throws Exception {
+        basicConfigurationService = new BasicConfigurationService();
         basicConfigurationService.addConfigItem( new ConfigItemImpl("name", "Aaron"), SOURCE);
         basicConfigurationService.addConfigItem( new ConfigItemImpl("AZ", "Aaron Zeckoski"), SOURCE);
         basicConfigurationService.addConfigItem( new ConfigItemImpl("testKeyEmpty", ""), SOURCE);
@@ -54,14 +54,14 @@ public class BasicConfigurationServiceTest extends TestCase {
         basicConfigurationService.addConfigItem( new ConfigItemImpl("test6", "test6"), SOURCE);
         basicConfigurationService.addConfigItem( new ConfigItemImpl("test7", "${AZ}"), SOURCE);
         log.info(basicConfigurationService.getConfigData());
-	}
+    }
 
-	public void testDereferenceString() throws Exception {
-	    // https://jira.sakaiproject.org/browse/SAK-22148
-	    String value = null;
-	    // testing for - String dereferenceValue(String value)
-	    value = basicConfigurationService.dereferenceValue(null);
-	    assertNull(value);
+    public void testDereferenceString() throws Exception {
+        // https://jira.sakaiproject.org/browse/SAK-22148
+        String value = null;
+        // testing for - String dereferenceValue(String value)
+        value = basicConfigurationService.dereferenceValue(null);
+        assertNull(value);
         value = basicConfigurationService.dereferenceValue("");
         assertNotNull(value);
         assertEquals("", value);
@@ -86,18 +86,30 @@ public class BasicConfigurationServiceTest extends TestCase {
         value = basicConfigurationService.dereferenceValue("hello ${testKeyNestedMulti}");
         assertNotNull(value);
         assertEquals("hello testing az=Aaron Zeckoski nested=testing name=Aaron testing invalid=${invalid}", value);
-	}
+    }
 
-	public void testDereferenceAll() throws Exception {
-	    // https://jira.sakaiproject.org/browse/SAK-22148
-	    int changed = basicConfigurationService.dereferenceConfig();
-	    ConfigData cd = basicConfigurationService.getConfigData();
+    public void testDereferenceAll() throws Exception {
+        // https://jira.sakaiproject.org/browse/SAK-22148
+        int changed = basicConfigurationService.dereferenceConfig();
+        ConfigData cd = basicConfigurationService.getConfigData();
         assertEquals(14, cd.getTotalConfigItems());
-	    assertEquals(3, changed); // 4 of them have keys but 1 key is invalid so it will not be replaced
-	    assertEquals("Aaron", basicConfigurationService.getConfig("name", "default") );
+        assertEquals(3, changed); // 4 of them have keys but 1 key is invalid so it will not be replaced
+        assertEquals("Aaron", basicConfigurationService.getConfig("name", "default") );
         assertEquals("testing name=Aaron testing", basicConfigurationService.getConfig("testKeyNested", "default") );
         assertEquals("testing az=Aaron Zeckoski nested=testing name=Aaron testing invalid=${invalid}", basicConfigurationService.getConfig("testKeyNestedMulti", "default") );
         assertEquals("Aaron Zeckoski", basicConfigurationService.getConfig("test7", "default") );
-	}
+    }
+
+    public void disabled_testKNL1038() throws Exception {
+        // KNL-1038 - "${sakai.home}/samigo/answerUploadRepositoryPath"
+        String val = null;
+        val = basicConfigurationService.getString("name");
+        assertNotNull(val);
+        assertEquals("Aaron", val);
+
+        val = basicConfigurationService.getString("namePlusLast", "${name} Zeckoski");
+        assertNotNull(val);
+        assertEquals("Aaron Zeckoski", val);
+    }
 
 }
