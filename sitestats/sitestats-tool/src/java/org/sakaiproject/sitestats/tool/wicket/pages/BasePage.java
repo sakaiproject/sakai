f401a2d210b1448283a043b38486502aff9ea60f
Steve Swinsburg <steve.swinsburg@gmail.com>
2013-03-25 20:07:58 +0000

    STAT-346 include the Sakai skin the proper way, via the request attribute
    git-svn-id: https://source.sakaiproject.org/svn/sitestats/trunk@121744 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sitestats/sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/pages/BasePage.java
+++ sitestats/sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/pages/BasePage.java
@@ -18,6 +18,8 @@
  */
 package org.sakaiproject.sitestats.tool.wicket.pages;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.IHeaderContributor;
 import org.apache.wicket.markup.html.IHeaderResponse;
@@ -35,7 +37,6 @@ import org.sakaiproject.util.ResourceLoader;
 public class BasePage extends WebPage implements IHeaderContributor {
 	
 	private static final long		serialVersionUID	= 1L;
-	public static final String		HEADSCRIPTS			= "/library/js/headscripts.js";
 	public static final String		COMMONSCRIPT		= StatsManager.SITESTATS_WEBAPP+"/script/common.js";
 	public static final String		JQUERYSCRIPT		= StatsManager.SITESTATS_WEBAPP+"/script/jquery-1.3.2.min.js";
 	public static final String		BODY_ONLOAD_ADDTL	= "setMainFrameHeightNoScroll(window.name, 0, 400)";
@@ -48,13 +49,12 @@ public class BasePage extends WebPage implements IHeaderContributor {
 	}
 
 	public void renderHead(IHeaderResponse response) {
-		// compute sakai skin
-		String skinRepo = ServerConfigurationService.getString("skin.repo");
-		response.renderCSSReference(skinRepo + "/tool_base.css");
-		response.renderCSSReference(getToolSkinCSS(skinRepo));
+		
+		//get the Sakai skin header fragment from the request attribute
+		HttpServletRequest request = getWebRequestCycle().getWebRequest().getHttpServletRequest();
+		response.renderString((String)request.getAttribute("sakai.html.head"));
 
 		// include sakai headscripts and resize iframe on load
-		response.renderJavascriptReference(HEADSCRIPTS);
 		response.renderJavascriptReference(COMMONSCRIPT);		
 		response.renderOnLoadJavascript(BODY_ONLOAD_ADDTL);
 		//response.renderOnDomReadyJavascript(BODY_ONLOAD_ADDTL);
@@ -71,50 +71,6 @@ public class BasePage extends WebPage implements IHeaderContributor {
 		add(componentTree);
 		*/
 		super.onBeforeRender();
-	}
-
-	public String getPortalSkinCSS() {
-		return getPortalSkinCSS(null);
-	}
-	
-	private String getPortalSkinCSS(String skinRepo) {
-		String skin = null;
-		if(skinRepo == null) {
-			skinRepo = ServerConfigurationService.getString("skin.repo");
-		}
-		try{
-			skin = SiteService.findTool(SessionManager.getCurrentToolSession().getPlacementId()).getSkin();
-		}catch(Exception e){
-			skin = ServerConfigurationService.getString("skin.default");
-		}
-
-		if(skin == null){
-			skin = ServerConfigurationService.getString("skin.default");
-		}
-
-		return skinRepo + "/" + skin + "/portal.css";
-	}
-
-	public String getToolSkinCSS() {
-		return getToolSkinCSS(null);
-	}
-
-	protected String getToolSkinCSS(String skinRepo) {
-		String skin = null;
-		if(skinRepo == null) {
-			skinRepo = ServerConfigurationService.getString("skin.repo");
-		}
-		try{
-			skin = SiteService.findTool(SessionManager.getCurrentToolSession().getPlacementId()).getSkin();
-		}catch(Exception e){
-			skin = ServerConfigurationService.getString("skin.default");
-		}
-
-		if(skin == null){
-			skin = ServerConfigurationService.getString("skin.default");
-		}
-
-		return skinRepo + "/" + skin + "/tool.css";
 	}
 
 	protected Label newResourceLabel(String id, Component component) {
