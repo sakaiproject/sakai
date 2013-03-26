72f41425c05e4c0ebd1f584e1ad4ff5b787a0bb2
Steve Swinsburg <steve.swinsburg@gmail.com>
2013-03-26 11:41:48 +0000

    KNL-1040 add javadoc for the getSite method to indicate it does not perform security checks
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@121766 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/api/src/main/java/org/sakaiproject/site/api/SiteService.java
+++ kernel/api/src/main/java/org/sakaiproject/site/api/SiteService.java
@@ -373,7 +373,8 @@ public interface SiteService extends EntityProducer
 	boolean siteExists(String id);
 
 	/**
-	 * Access a site object.
+	 * Access a site object. This method does not perform any security/permission checks. 
+	 * If you need permission checks to occur, use {@link getSiteVisit(String id)} instead
 	 * 
 	 * @param id
 	 *        The site id string.
