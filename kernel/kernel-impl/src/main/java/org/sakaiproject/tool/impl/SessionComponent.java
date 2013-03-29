9a425b091f187052fa5708ce78ef1c625d4fb24d
Matthew Jones <jonespm@gmail.com>
2013-03-29 15:58:13 +0000

    KNL-1050 - Limit SessionComponent to returning values less than the maximum size of the session (set to UUID), also log for when exceptions occur
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@121962 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/kernel-impl/src/main/java/org/sakaiproject/tool/impl/SessionComponent.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/tool/impl/SessionComponent.java
@@ -62,6 +62,7 @@ public abstract class SessionComponent implements SessionManager, SessionStore
 {
 	/** Our log (commons). */
 	private static Log M_log = LogFactory.getLog(SessionComponent.class);
+    public static final int MAX_SESSION_LENGTH = 36;
 
 	/** The sessions - keyed by session id. */
 	protected Map<String, Session> m_sessions = new ConcurrentHashMap<String, Session>();
@@ -258,10 +259,17 @@ public abstract class SessionComponent implements SessionManager, SessionStore
 		} catch (NoSuchAlgorithmException e) {
 			// Fallback to new uuid rather than a non-hashed id
 			sessionId = idManager().createUuid();
+            //This may need to be changed to a debug
+			M_log.warn("makeSessionId fallback to Uuid!",e);
+
 		} catch (UnsupportedEncodingException e) {
 			sessionId = idManager().createUuid();
+            //This may need to be changed to a debug
+			M_log.warn("makeSessionId fallback to Uuid!",e);
 		}
 		
+        //Trim the sessionId to MAX_SESSION_LENGTH characters
+        sessionId = sessionId.substring(0, Math.min(MAX_SESSION_LENGTH, sessionId.length()));
 		return sessionId;
 	}
 	
