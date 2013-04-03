b321dadf8cf2392c7f00f464c95cd4150913d125
Aaron Zeckoski <azeckoski@vt.edu>
2013-04-03 18:23:06 +0000

    KNL-1051 Add in the class to hold the list of default locales so it is easy to edit
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@122138 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ kernel/component-manager/src/main/java/org/sakaiproject/component/locales/SakaiLocales.java
@@ -0,0 +1,37 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007, 2008 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.component.locales;
+
+/**
+ * This stores constants related to the Sakai default locales
+ * 
+ * @author Aaron Zeckoski (azeckoski @ unicon.net) (azeckoski @ vt.edu)
+ */
+public class SakaiLocales {
+
+    /**
+     * This should be complete list of supported locales and should match the commented list in default.sakai.properties
+     * It MUST be a comma separated list of locale keys (be careful with your formatting)
+     */
+    public static final String SAKAI_LOCALES_DEFAULT = "en_US, en_GB, en_AU, en_NZ, en_ZA, ja_JP, ko_KR, nl_NL, zh_CN, zh_TW, es_ES, fr_CA, fr_FR, ca_ES, sv_SE, ar, ru_RU, pt_PT, pt_BR, eu, vi_VN, tr_TR, es_MX, mn, pl_PL";
+
+}
