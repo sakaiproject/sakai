868612c2fc1cc3423a418460e2358d840719f326
Anthony Whyte <arwhyte@umich.edu>
2013-04-12 01:35:10 +0000

    SAK-23473 add BLTI conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@122644 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_1-2_9_2_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_9_1-2_9_2_mysql_conversion.sql
@@ -10,7 +10,33 @@
 -- SQL statement
 -- --------------------------------------------------------------------------------------------------------------------------------------
 
--- SAK-23452 Roster throws errors in 2.9 after upgrade mysql 
+-- BLTI-222
+ALTER TABLE lti_content ADD pagetitle VARCHAR(255);
+ALTER TABLE lti_content MODIFY launch TEXT(1024);
+ALTER TABLE lti_content ADD consumerkey VARCHAR(255);
+ALTER TABLE lti_content ADD secret VARCHAR(255);
+ALTER TABLE lti_content ADD settings TEXT(8096);
+ALTER TABLE lti_content ADD placementsecret TEXT(512);
+ALTER TABLE lti_content ADD oldplacementsecret TEXT(512);
+ALTER TABLE lti_tools ADD allowtitle TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD pagetitle VARCHAR(255);
+ALTER TABLE lti_tools ADD allowpagetitle TINYINT DEFAULT '0';
+ALTER TABLE lti_tools MODIFY launch TEXT(1024);
+ALTER TABLE lti_tools ADD allowlaunch TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD domain VARCHAR(255);
+ALTER TABLE lti_tools ADD allowconsumerkey TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD allowsecret TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD allowoutcomes TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD allowroster TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD allowsettings TINYINT DEFAULT '0';
+ALTER TABLE lti_tools ADD allowlori TINYINT DEFAULT '0';
+
+-- BLTI-208
+ALTER TABLE lti_tools MODIFY launch VARCHAR(255) NULL;
+ALTER TABLE lti_tools MODIFY consumerkey VARCHAR(255) NULL;
+ALTER TABLE lti_tools MODIFY secret VARCHAR(255) NULL;
+
+-- SAK-23452 Roster throws errors in 2.9 after MySQL upgrade 
 update PROFILE_PREFERENCES_T set USE_GRAVATAR = false where USE_GRAVATAR is null; 
 update PROFILE_PREFERENCES_T set EMAIL_WALL_ITEM_NEW = true where EMAIL_WALL_ITEM_NEW is null; 
 update PROFILE_PREFERENCES_T set EMAIL_WORKSITE_NEW = true where EMAIL_WORKSITE_NEW is null; 
