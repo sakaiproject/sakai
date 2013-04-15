56424c62157d5d8602f157bc8bce745ac64b4dd9
Aaron Zeckoski <azeckoski@vt.edu>
2013-04-15 11:03:07 +0000

    KNL-1057 Remove unused and outdated K1 conversion working files from Kernel Patch from virginia tech
    git-svn-id: https://source.sakaiproject.org/svn/kernel/trunk@122757 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- kernel/conversion/branchcoreprojects.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-targets="alias authz cluster component content db email entity event jcr memory site tool user util"
-for i in $targets
-do 
-    svn cp -m "Redoing Branch for pre-K1" https://source.sakaiproject.org/svn/$i/trunk https://source.sakaiproject.org/svn/$i/branches/pre-K1
-done
