e53f36bd6efb19bae15179236a2bf3ee3a17068b
Zhen Qian <zqian@umich.edu>
2013-04-15 21:05:51 +0000

    DASH-240 add entityprovider to get Dashboard feed
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/trunk@313450 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ dashboard/impl/src/java/org/sakaiproject/dash/entity/DashboardEntityProvider.java
@@ -0,0 +1,105 @@
+package org.sakaiproject.dash.entity;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Setter;
+
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.lang.StringUtils;
+import org.sakaiproject.dash.logic.DashboardLogic;
+import org.sakaiproject.dash.app.DashboardCommonLogic;
+import org.sakaiproject.dash.app.SakaiProxy;
+import org.sakaiproject.entitybroker.EntityReference;
+import org.sakaiproject.entitybroker.EntityView;
+import org.sakaiproject.entitybroker.entityprovider.EntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomAction;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.ActionsExecutable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEntityProvider;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
+import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
+import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
+
+public class DashboardEntityProvider extends AbstractEntityProvider implements EntityProvider, AutoRegisterEntityProvider, Outputable, Describeable, ActionsExecutable {
+    
+    	public String getEntityPrefix() {
+            		return "dash";
+            	}
+    
+    	public String[] getHandledOutputFormats() {
+            		return new String[] {Formats.JSON, Formats.XML};
+            	}
+    
+    	public boolean entityExists(String id) {
+            		return true;
+            	}
+    
+    	@EntityCustomAction(action="news",viewKey=EntityView.VIEW_LIST)
+    	public List<?> getNewsItems(EntityView view, EntityReference ref, Map<String, Object> params) {
+            		
+            		String userUuid = sakaiProxy.getCurrentUserId();
+            		
+            		if(StringUtils.isBlank(userUuid)) {
+                    			throw new SecurityException("You must be logged in to get a user's dashboard");
+                    		}
+            			
+            		//get optional params
+            		String siteId = (String)params.get("site");
+            		boolean hidden = BooleanUtils.toBoolean((String)params.get("hidden"));
+        		boolean starred = BooleanUtils.toBoolean((String)params.get("starred"));
+        		
+            		//only return hidden items
+            		if(hidden){
+                    			return dashboardCommonLogic.getHiddenNewsLinks(userUuid, siteId);
+                    		}
+            		
+            		//only return starred items
+            		if(starred){
+                    			return dashboardCommonLogic.getStarredNewsLinks(userUuid, siteId);
+                    		}
+            		
+            		//return everything
+            		return dashboardCommonLogic.getCurrentNewsLinks(userUuid, siteId);
+            	}
+    	
+    	@EntityCustomAction(action="calendar",viewKey=EntityView.VIEW_LIST)
+    	public List<?> getCalendarItems(EntityView view, EntityReference ref, Map<String, Object> params) {
+            		
+            		String userUuid = sakaiProxy.getCurrentUserId();
+        		
+            		if(StringUtils.isBlank(userUuid)) {
+                    			throw new SecurityException("You must be logged in to get a user's dashboard");
+                    		}
+            			
+        		//get optional params
+            		String siteId = (String)params.get("site");
+            		boolean hidden = BooleanUtils.toBoolean((String)params.get("hidden"));
+            		boolean starred = BooleanUtils.toBoolean((String)params.get("starred"));
+            		boolean past = BooleanUtils.toBoolean((String)params.get("past"));
+            				
+            		//only return starred items
+            		if(starred){
+                    			return dashboardCommonLogic.getStarredCalendarLinks(userUuid, siteId);
+                    		}
+            		
+            		//only return past items. Could be hidden depending on param
+            		if(past){
+                    			return dashboardCommonLogic.getPastCalendarLinks(userUuid, siteId, hidden);
+                    		}
+            		
+            		//return everything. Could be hidden depending on param
+            		return dashboardCommonLogic.getFutureCalendarLinks(userUuid, siteId, hidden);
+            	}
+    	
+    	
+    	@Setter
+    	private DashboardLogic dashboardLogic;
+
+	@Setter
+	private DashboardCommonLogic dashboardCommonLogic;
+    	
+    	@Setter
+    	private SakaiProxy sakaiProxy;
+
+    }
