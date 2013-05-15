e2ed79ac9381896c28e33e96110dbbfaa80af60e
John Bush <johntylerbush@gmail.com>
2013-05-15 18:54:03 +0000

    SRCH-117 Split the code in SearchEntityProvider and add documentation, patch from Colin Hebert
    git-svn-id: https://source.sakaiproject.org/svn/search/trunk@124128 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- search/search-impl/impl/src/java/org/sakaiproject/search/entitybroker/SearchEntityProvider.java
+++ search/search-impl/impl/src/java/org/sakaiproject/search/entitybroker/SearchEntityProvider.java
@@ -22,167 +22,203 @@
 
 package org.sakaiproject.search.entitybroker;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import org.sakaiproject.entitybroker.entityprovider.capabilities.CollectionResolvable;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
-import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
-import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.EntityReference;
-import org.sakaiproject.entitybroker.entityprovider.search.Search;
+import org.sakaiproject.entitybroker.EntityView;
+import org.sakaiproject.entitybroker.entityprovider.annotations.EntityCustomAction;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.ActionsExecutable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Describeable;
+import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
+import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
 import org.sakaiproject.entitybroker.entityprovider.search.Restriction;
+import org.sakaiproject.entitybroker.entityprovider.search.Search;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
-import org.sakaiproject.site.api.Site;
-import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.search.api.InvalidSearchQueryException;
-import org.sakaiproject.search.api.SearchService;
 import org.sakaiproject.search.api.SearchList;
 import org.sakaiproject.search.api.SearchResult;
+import org.sakaiproject.search.api.SearchService;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.user.api.UserDirectoryService;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * Provides basic search functionality via EB.
+ * Entity provider for Entity broker giving access to search services through a an HTTP method
  *
  * @author Adrian Fish (a.fish@lancaster.ac.uk)
+ * @author Colin Hebert
  */
-public class SearchEntityProvider extends AbstractEntityProvider implements CollectionResolvable,Outputable,Describeable {
+public class SearchEntityProvider extends AbstractEntityProvider implements ActionsExecutable, Outputable, Describeable {
+    private static final int DEFAULT_RESULT_COUNT = 10;
+    private UserDirectoryService userDirectoryService;
+    private SearchService searchService;
+    private SiteService siteService;
 
-    private static Log log = LogFactory.getLog(SearchEntityProvider.class);
-
-	private SearchService searchService = null;
-
-	public void setSearchService(SearchService searchService) {
-		this.searchService = searchService;
-	}
-
-	private SiteService siteService = null;
-
-	public void setSiteService(SiteService siteService) {
-		this.siteService = siteService;
-	}
-
-   	public UserDirectoryService userDirectoryService;
-
-   	public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
-      	this.userDirectoryService = userDirectoryService;
-   	}
-
-	public String getEntityPrefix() {
-		return "search";
-	}
-
-    public String[] getHandledOutputFormats() {
-        return new String[] { Formats.JSON, Formats.XML };
+    /**
+     * Name of the service, here "search"
+     *
+     * @return the constant name of this service
+     */
+    @Override
+    public String getEntityPrefix() {
+        return "search";
     }
 
-	public List<?> getEntities(EntityReference ref, Search search) {
-		Restriction searchTermsRestriction = search.getRestrictionByProperty("searchTerms");
+    /**
+     * Handled formats, such as JSon and XML
+     *
+     * @return formats supported
+     */
+    @Override
+    public String[] getHandledOutputFormats() {
+        return new String[]{Formats.JSON, Formats.XML};
+    }
 
-		if(searchTermsRestriction == null)
-			throw new IllegalArgumentException("No searchTerms supplied");
+    /**
+     * Simple search method
+     *
+     * @param ref
+     * @param search
+     * @return a list of SearchResults
+     */
+    @EntityCustomAction(action = "search", viewKey = EntityView.VIEW_LIST)
+    public List<SearchResultEntity> search(EntityReference ref, Search search) {
+        try {
+            //Get the query sent by the client
+            String query = extractQuery(search.getRestrictionByProperty("searchTerms"));
+            //Get the list of contexts (sites) used for this search, or every accessible site if the user hasn't provided a context list
+            List<String> contexts = extractContexts(search.getRestrictionByProperty("contexts"));
 
-		String searchTerms = searchTermsRestriction.getStringValue();
+            //Set the limit if it hasn't been set already
+            if (search.getLimit() < 0)
+                search.setLimit(DEFAULT_RESULT_COUNT);
 
-        // fix up the search limits
-        if (search.getLimit() > 50 || search.getLimit() == 0) {
-            search.setLimit(50);
-        }
-        if (search.getStart() > 49) {
-            search.setStart(0);
-        }
+            //Actual search
+            SearchList searchResults = searchService.search(query, contexts, (int) search.getStart(), (int) search.getLimit());
 
-		List<String> contexts = null;
-
-		Restriction contextsRestriction = search.getRestrictionByProperty("contexts");
-
-		if(contextsRestriction != null) {
-			String[] contextsArray = contextsRestriction.getStringValue().split(",");
-			contexts = Arrays.asList(contextsArray);
-		}
-		else {
-			// No contexts supplied. Get all the sites the current user is a member of
-			contexts = getAllUsersSites();
-		}
-
-		SearchList searchResults;
-		try {
-			searchResults = searchService.search(searchTerms,contexts,(int) search.getStart(),(int) search.getLimit(),"normal","normal");
-		} catch (InvalidSearchQueryException e) {
-			throw new IllegalArgumentException(searchTerms + " is not a valid query expression");
-		}
-
-		Restriction toolRestriction = search.getRestrictionByProperty("tool");
-
-		String tool = null;
-
-		if(toolRestriction != null) {
-			tool = toolRestriction.getStringValue();
-		}
-		
-		List<SearchResultEntity> results = new ArrayList<SearchResultEntity>();
-
-		for(SearchResult result : searchResults) {
-			if(tool == null || result.getTool().equalsIgnoreCase(tool))
-				results.add(new SearchResultEntity(result));
-		}
-
-		return results;
-	}
-
-	public Object getEntity(EntityReference ref) {
-		System.out.println("getEntity(" + ref.getReference() + ")");
-		return null;
-	}
-
-	private List<String> getAllUsersSites() {
-        List<Site> sites = siteService.getSites(
-			SiteService.SelectionType.ACCESS,null, null, null, null, null);
-        List<String> siteIds = new ArrayList<String>(sites.size());
-        for (Site site: sites) {
-            if (site != null && site.getId() != null) {
-                siteIds.add(site.getId());
+            //Transforms SearchResult in a SearchResultEntity to avoid conflicts with the getId() method (see SRCH-85)
+            List<SearchResultEntity> results = new ArrayList<SearchResultEntity>(searchResults.size());
+            for (SearchResult result : searchResults) {
+                results.add(new SearchResultEntity(result));
             }
+
+            return results;
+        } catch (InvalidSearchQueryException e) {
+            throw new IllegalArgumentException(e);
         }
-        siteIds.add(siteService.getUserSiteId(
-			userDirectoryService.getCurrentUser().getId()));
+    }
+
+    /**
+     * Extract the query from users parameters
+     *
+     * @param searchTermsRestriction parameter given to EntityBroker
+     * @return A search String
+     * @throws IllegalArgumentException If no query has been provided
+     */
+    private String extractQuery(Restriction searchTermsRestriction) {
+        if (searchTermsRestriction == null)
+            throw new IllegalArgumentException("No searchTerms supplied");
+
+        StringBuilder searchQuery = new StringBuilder();
+        for (String term : (String[]) searchTermsRestriction.getArrayValue()) {
+            //Concatenate with spaces, if the user wants a coma separated value he can easily enter comas in his query.
+            searchQuery.append(term).append(' ');
+        }
+        return searchQuery.toString();
+    }
+
+    /**
+     * Extract contexts from users parameters
+     *
+     * @param contextsRestriction parameter given to EntityBroker
+     * @return A list of contexts (sites) where the search will be done
+     */
+    private List<String> extractContexts(Restriction contextsRestriction) {
+        List<String> contexts;
+        if (contextsRestriction != null)
+            contexts = Arrays.asList((String[]) contextsRestriction.getArrayValue());
+        else
+            // No contexts supplied. Get all the sites the current user is a member of
+            contexts = getAllSites();
+        return contexts;
+    }
+
+    /**
+     * Get all sites available for the current user
+     *
+     * @return a list of contexts (sites IDs) available for the current user
+     */
+    private List<String> getAllSites() {
+        List<Site> sites = siteService.getSites(SiteService.SelectionType.ACCESS, null, null, null, null, null);
+        List<String> siteIds = new ArrayList<String>(sites.size());
+        for (Site site : sites) {
+            if (site != null && site.getId() != null)
+                siteIds.add(site.getId());
+        }
+
+        //Manually add the user's site
+        siteIds.add(siteService.getUserSiteId(userDirectoryService.getCurrentUser().getId()));
         return siteIds;
     }
 
-	public class SearchResultEntity {
+    //--------------------------
+    //Spring injected components
+    //--------------------------
+    public void setSearchService(SearchService searchService) {
+        this.searchService = searchService;
+    }
 
-		private SearchResult result = null;
+    public void setSiteService(SiteService siteService) {
+        this.siteService = siteService;
+    }
 
-		private SearchResultEntity(SearchResult result) {
-			this.result = result;
-		}
+    public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
+        this.userDirectoryService = userDirectoryService;
+    }
 
-		public String getId() {
-			return result.getId();
-		}
+    /**
+     * A wrapper to customise the result sent through EntityBroker
+     * <p>
+     * Wraps a {@link SearchResult} to avoid issues with the {@link org.sakaiproject.search.api.SearchResult#getId()}
+     * method and {@link EntityReference#checkPrefixId(String, String)}.<br />
+     * Can also filter which parts of the query are accessible to a remote user.
+     * </p>
+     */
+    public class SearchResultEntity {
+        private final SearchResult searchResult;
 
-		public float getScore() {
-			return result.getScore();
-		}
+        private SearchResultEntity(SearchResult searchResult) {
+            this.searchResult = searchResult;
+        }
 
-		public String getSearchResult() {
-			return result.getSearchResult();
-		}
+        public String getReference() {
+            return searchResult.getReference();
+        }
 
-		public String getTitle() {
-			return result.getTitle();
-		}
+        public String getContentId() {
+            return searchResult.getId();
+        }
 
-		public String getTool() {
-			return result.getTool();
-		}
+        public float getScore() {
+            return searchResult.getScore();
+        }
 
-		public String getUrl() {
-			return result.getUrl();
-		}
-	}
+        public String getSearchResult() {
+            return searchResult.getSearchResult();
+        }
+
+        public String getTitle() {
+            return searchResult.getTitle();
+        }
+
+        public String getTool() {
+            return searchResult.getTool();
+        }
+
+        public String getUrl() {
+            return searchResult.getUrl();
+        }
+    }
 }
