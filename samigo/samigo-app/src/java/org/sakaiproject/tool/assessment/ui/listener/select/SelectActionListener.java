b32a195efae3dcc0b6d75acbeedf852763b4f6f4
Sam Ottenhoff <ottenhoff@longsight.com>
2013-05-29 16:39:17 +0000

    SAM-2167 Javascript check for "null" string. Patch by Yuki
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@125088 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/select/SelectActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/select/SelectActionListener.java
@@ -531,7 +531,7 @@ public class SelectActionListener
     String reviewAscending = ContextUtil.lookupParam("reviewableAscending");
     String takeAscending = ContextUtil.lookupParam("takeAscending");
 
-    if (takeOrder != null && !takeOrder.trim().equals("")) {
+    if (takeOrder != null && !takeOrder.trim().equals("") && !takeOrder.equals("null")) {
       bean.setTakeableSortOrder(takeOrder);
     }
 
@@ -539,7 +539,7 @@ public class SelectActionListener
       bean.setReviewableSortOrder(reviewOrder);
     }
 
-    if (takeAscending != null && !takeAscending.trim().equals("")) {
+    if (takeAscending != null && !takeAscending.trim().equals("") && !takeAscending.equals("null")) {
       try {
         bean.setTakeableAscending((Boolean.valueOf(takeAscending)).booleanValue());
       }
