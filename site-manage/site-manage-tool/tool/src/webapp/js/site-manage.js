e4b9d2c7469c86be25057f341aa87fc06c7b8047
Gonzalo Silverio <gsilver@umich.edu>
2013-06-17 12:57:07 +0000

    SAK-23699 - add ability for IE to search arrays
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@125849 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/webapp/js/site-manage.js
+++ site-manage/site-manage-tool/tool/src/webapp/js/site-manage.js
@@ -868,3 +868,11 @@ var setupRecentSite = function(){
         
     })
 }
+if (!Array.prototype.indexOf) { 
+    Array.prototype.indexOf = function(obj, start) {
+         for (var i = (start || 0), j = this.length; i < j; i++) {
+             if (this[i] === obj) { return i; }
+         }
+         return -1;
+    }
+}
