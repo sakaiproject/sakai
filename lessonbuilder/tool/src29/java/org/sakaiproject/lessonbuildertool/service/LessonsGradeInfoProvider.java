58d79d58407ad18d561bf1f16c90b8a0b45ebbb8
Charles Hedrick <hedrick@rutgers.edu>
2013-06-30 17:24:18 +0000

    LSNBLDR-249; handle subpage heirarchiesproperly, ie. LSNBLDR-255
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/trunk@126556 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- lessonbuilder/tool/src29/java/org/sakaiproject/lessonbuildertool/service/LessonsGradeInfoProvider.java
+++ lessonbuilder/tool/src29/java/org/sakaiproject/lessonbuildertool/service/LessonsGradeInfoProvider.java
@@ -49,6 +49,10 @@ import org.sakaiproject.tool.api.SessionManager;
 import org.sakaiproject.user.cover.UserDirectoryService;
 import org.sakaiproject.user.api.User;
 
+import org.sakaiproject.memory.api.Cache;
+import org.sakaiproject.memory.api.CacheRefresher;
+import org.sakaiproject.memory.api.MemoryService;
+
 import org.sakaiproject.lessonbuildertool.SimplePageItem;
 import org.sakaiproject.lessonbuildertool.SimplePage;
 import org.sakaiproject.lessonbuildertool.model.SimplePageToolDao;
@@ -58,13 +62,20 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 
     private Log log = LogFactory.getLog(LessonsGradeInfoProvider.class);
 
+    // caching
+    private static Cache cache = null;
+    protected static final int DEFAULT_EXPIRATION = 10 * 60;
+
     // Sakai Service Beans
     private GradebookExternalAssessmentService geaService;
     private SimplePageToolDao dao;
     private AuthzGroupService authzGroupService;
     private SecurityService securityService;
+    private MemoryService memoryService;
 
     public void init() {
+	cache = memoryService
+	    .newCache("org.sakaiproject.lessonbuildertool.service.LessonsGradeInfoProvider.cache");
         log.info("INIT and register LessonsGradeInfoProvider");
         geaService.registerExternalAssignmentProvider(this);
     }
@@ -72,6 +83,8 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
     public void destroy() {
         log.info("DESTROY and unregister LessonsGradeInfoProvider");
         geaService.unregisterExternalAssignmentProvider(getAppKey());
+	cache.destroy();
+	cache = null;
     }
 
     public String getAppKey() {
@@ -108,6 +121,107 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 	return false;
     }
 
+    // my best estimate is about 100 bytes / call. The maximum likely chain is 
+    // 100. So that could put 10K on the stack. With 1 G stacks I think that's OK
+
+    /*
+      main entry to this is getItemGroups(item, [])
+      find all the groups allowed to use this item
+
+      null means no constraints
+      [], i.e. empty set, means impossible
+    */
+
+    Set<String> getItemGroups (SimplePageItem item) {
+	return getItemGroups(item, new HashSet<Long>());
+    }
+
+    // find all groups allowed to use this item
+    // intersect group directly associated with item
+    // and groups allowed to use the page it's on
+
+    Set<String> getItemGroups (SimplePageItem item, Set<Long>seen) {
+	/// System.out.println("item " + item.getId() + " groups");
+
+	// null is a possible value. Because get returns null if something isn't
+	// in the cache, use "null" for null. could also check whether it's in the cache
+	// but documentation says this is expensive
+	//	Object cached = cache.get(item.getId());
+	Object cached = null; // for testing
+	if (cached != null) {
+	    System.out.println("returning cached");
+	    if (cached instanceof String) // "null"
+		return null;
+	    return (Set<String>)cached;
+	}
+	Long itemId = (Long)item.getId();
+
+	if (seen.contains(itemId)) // loop, can't really get there
+	    return new HashSet<String>();
+	seen.add(itemId); // note that we're in progress, to stop infinte loop
+
+	// if either has no restriction use the other
+	// otherwise intersect them
+
+	String itemGroupString = item.getGroups();
+	Set<String>itemGroups = null;
+	if (itemGroupString != null && itemGroupString.length() > 0)
+	    itemGroups = new HashSet<String>(Arrays.asList(itemGroupString.split(",")));
+	/// System.out.println("item " + item.getId() + "local groups " + itemGroups);
+
+	Set<String>  pageGroups = getPageGroups(item.getPageId(), seen);
+	/// System.out.println("item " + item.getId() + "page groups " + pageGroups);
+	if (itemGroups == null)
+	    itemGroups = pageGroups;
+	else if (pageGroups == null)
+            ; //nothing
+	else
+	    itemGroups.retainAll(pageGroups);
+	if (itemGroups == null)
+	    cache.put(item.getId(), "null", DEFAULT_EXPIRATION);
+	else
+	    cache.put(item.getId(), itemGroups, DEFAULT_EXPIRATION);
+	/// System.out.println("item " + item.getId() + " returned " + itemGroups);
+
+        // no longer is progress
+	seen.remove(itemId);
+	return itemGroups;
+    }
+	    
+    // return all group allowed to access this page
+    // find all items that point to the page and
+    // take the union of groups allowed t use each
+
+    Set<String> getPageGroups(long pageId, Set<Long>seen) {
+	/// System.out.println("page " + pageId + " getgroups");
+	// if pageid is 0 this is a top level page. No further constraints
+	if (pageId == 0)
+	    return null;
+
+	Set<String> ret = new HashSet<String>();
+
+	// List of items with this page on it
+	List<SimplePageItem> items = dao.findPageItemsBySakaiId(Long.toString(pageId));
+	/// System.out.print("page " + pageId + " called from items ");
+	/// for (SimplePageItem item: items) {
+	///	    System.out.print(item.getId() + " ");
+	/// }
+	/// System.out.println();
+
+	for (SimplePageItem item: items) {
+	    // union all their groups
+	    Set<String> pageGroups = getItemGroups(item, seen);
+
+	    /// System.out.println("page " + pageId + "item " + item.getId() + " " + pageGroups);
+	    // except if we find one that's unconstrained, the final result is thta
+	    if (pageGroups == null)
+		return null;
+	    ret.addAll(pageGroups);
+	 }
+	/// System.out.println("page " + pageId + " returns " + ret);
+	return ret;
+    }
+
     public boolean isAssignmentVisible(String id, String userId) {
 	int i = id.lastIndexOf(":");
 	if (i < 0)
@@ -135,15 +249,13 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 	    return false;
 
 	// can access the site. If not grouped, we're done.
-	String groupString = item.getGroups();
-	if (groupString == null || groupString.equals("")) {
-	    return true;
-	}
 
-	// grouped. See if user is in the group
-	String[] groupsArray = groupString.split(",");
+	Set<String> groupIds = getItemGroups(item);
+	if (groupIds == null)
+	    return true;
+
 	ArrayList<String> groups = new ArrayList<String>();
-	for (String groupId: groupsArray)
+	for (String groupId: groupIds)
 	    groups.add("/site/" + siteId + "/group/" + groupId);
 	List<AuthzGroup> matched = authzGroupService.getAuthzUserGroupIds(groups, userId);
 	return (matched.size() > 0);
@@ -178,16 +290,16 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 		continue;
 	    }
 
-	    String groupString = item.getGroups();
-	    if (groupString == null || groupString.equals("")) {
+	    Set<String> groupIds = getItemGroups(item);
+	    /// System.out.println("item " + item.getId() + " " + groupIds);
+	    if (groupIds ==  null) {
 		// no group restriction. add this item
 		ret.add(externalId);
 		continue;
 	    }
 
-	    String[] groupsArray = groupString.split(",");
 	    ArrayList<String> groups = new ArrayList<String>();
-	    for (String groupId: groupsArray)
+	    for (String groupId: groupIds)
 		groups.add("/site/" + gradebookUid + "/group/" + groupId);
 	    List<AuthzGroup> matched = authzGroupService.getAuthzUserGroupIds(groups, userId);
 	    if (matched.size() > 0)
@@ -230,8 +342,8 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 	    } catch (Exception e){
 		continue;
 	    }
-	    String groupString = item.getGroups();
-	    if (groupString == null || groupString.equals("")) {
+	    Set<String> groupIds = getItemGroups(item);
+	    if (groupIds == null) {
 		// no restriction add to all users
 		for (String userId : studentIds)
 		    if (allExternals.containsKey(userId)) {
@@ -239,9 +351,8 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
 		    }
 	    } else {
 		// restricted to group
-		String[] groupsArray = groupString.split(",");
 		Set<String> groups = new HashSet<String>();
-		for (String groupId: groupsArray)
+		for (String groupId: groupIds)
 		    groups.add("/site/" + gradebookUid + "/group/" + groupId);
 
 		// see if anyone on our list is in one of the groups
@@ -285,6 +396,11 @@ public class LessonsGradeInfoProvider implements ExternalAssignmentProvider {
     public void setSimplePageToolDao(SimplePageToolDao s) {
         dao = s;
     }
+
+    public void setMemoryService(MemoryService m) {
+	memoryService = m;
+    }
+
 
 }
 
