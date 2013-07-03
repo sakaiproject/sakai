d1dcda1bc46812a795e7e481dcc45bbca0f14419
Steve Swinsburg <steve.swinsburg@gmail.com>
2013-07-03 16:15:24 +1000

    update license headers and notice

--- src/main/java/au/edu/anu/portal/portlets/rss/FeedParser.java
+++ src/main/java/au/edu/anu/portal/portlets/rss/FeedParser.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2011-2012 The Australian National University
+ * Copyright 2011-2013 The Australian National University
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
