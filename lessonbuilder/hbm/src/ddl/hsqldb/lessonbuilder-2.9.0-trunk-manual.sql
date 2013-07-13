0dbf4026efab0ea1623cd93c31ff669adf813eab
Charles Hedrick <hedrick@rutgers.edu>
2013-07-13 02:00:12 +0000

    LSNBLDR-267; supply manual update process that includes indices
    git-svn-id: https://source.sakaiproject.org/svn/lessonbuilder/trunk@127004 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ lessonbuilder/hbm/src/ddl/hsqldb/lessonbuilder-2.9.0-trunk-manual.sql
@@ -0,0 +1,7 @@
+create index lesson_builder_group_site on lesson_builder_groups(siteId);
+create index lesson_builder_item_gb on lesson_builder_items(gradebookid);
+create index lesson_builder_item_altgb on lesson_builder_items(altGradebook);
+create index lesson_builder_prop_idx on lesson_builder_properties(attribute);
+create index lesson_builder_qr_questionId_userId on lesson_builder_q_responses(questionId, userId);
+create index lesson_builder_qr_total_qi on lesson_builder_qr_totals(questionId);
+create index lesson_builder_qr_questionId on lesson_builder_q_responses(questionId);
--- lessonbuilder/hbm/src/ddl/oracle/lessonbuilder-2.9.0-trunk.sql
+++ lessonbuilder/hbm/src/ddl/oracle/lessonbuilder-2.9.0-trunk.sql
@@ -47,3 +47,29 @@ alter table lesson_builder_pages add groupid varchar2(36 char);
 
 alter table lesson_builder_student_pages add groupid varchar2(36 char);
 alter table lesson_builder_student_pages add create sequence hibernate_sequence;;
+--- alter table lesson_builder_items modify description clob;
+alter table lesson_builder_items add temp clob;
+update lesson_builder_items set temp=description;
+alter table lesson_builder_items drop column description;
+alter table lesson_builder_items rename column temp to description;
+
+--- alter table lesson_builder_items modify groups clob;
+alter table lesson_builder_items add temp clob;
+update lesson_builder_items set temp=groups;
+alter table lesson_builder_items drop column groups;
+alter table lesson_builder_items rename column temp to groups;
+
+
+--- alter table lesson_builder_items modify ownerGroups clob;
+alter table lesson_builder_items add temp clob;
+update lesson_builder_items set temp=ownerGroups;
+alter table lesson_builder_items drop column ownerGroups;
+alter table lesson_builder_items rename column temp to ownerGroups;
+
+create index lb_group_site on lesson_builder_groups(siteId);
+create index lb_item_gb on lesson_builder_items(gradebookid);
+create index lb_item_altgb on lesson_builder_items(altGradebook);
+create index lb_prop_idx on lesson_builder_properties(attribute);
+create index lb_qr_questionId_userId on lesson_builder_q_responses(questionId, userId);
+create index lb_qr_total_qi on lesson_builder_qr_totals(questionId);
+create index lb_qr_questionId on lesson_builder_q_responses(questionId);
