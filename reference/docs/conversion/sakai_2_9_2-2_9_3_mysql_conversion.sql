c3aafc5168f46cbbf547a246cbff8b3e6b55f889
Sam Ottenhoff <ottenhoff@longsight.com>
2013-07-25 16:02:07 +0000

    SAK-23749 re-syncing LSNBLDR conversion scripts for 2.9.3
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@127742 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_9_2-2_9_3_mysql_conversion.sql
+++ reference/docs/conversion/sakai_2_9_2-2_9_3_mysql_conversion.sql
@@ -1,6 +1,18 @@
 
 -- LSNBLDR-227
+alter table lesson_builder_groups add siteId varchar(250);
 alter table lesson_builder_items modify description text;
 alter table lesson_builder_items modify groups text;
-alter table lesson_builder_items modify ownerGroups text;
+
+create table if not exists lesson_builder_properties (
+     id bigint not null auto_increment,
+     attribute varchar(255) not null unique,
+     value longtext,
+     primary key (id)
+);
+
+create index lesson_builder_group_site on lesson_builder_groups(siteId);
+create index lesson_builder_item_gb on lesson_builder_items(gradebookid);
+create index lesson_builder_item_altgb on lesson_builder_items(altGradebook);
+create index lesson_builder_prop_idx on lesson_builder_properties(attribute);
 -- end LSNBLDR-227
--- reference/docs/conversion/sakai_2_9_2-2_9_3_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_9_2-2_9_3_oracle_conversion.sql
@@ -1,17 +1,28 @@
 
 -- LSNBLDR-227
+alter table lesson_builder_groups add siteId varchar2(250 char);
+
+-- alter table lesson_builder_items modify description clob;
 alter table lesson_builder_items add temp clob;
 update lesson_builder_items set temp=description;
 alter table lesson_builder_items drop column description;
 alter table lesson_builder_items rename column temp to description;
 
+-- alter table lesson_builder_items modify groups clob;
 alter table lesson_builder_items add temp clob;
 update lesson_builder_items set temp=groups;
 alter table lesson_builder_items drop column groups;
 alter table lesson_builder_items rename column temp to groups;
 
-alter table lesson_builder_items add temp clob;
-update lesson_builder_items set temp=ownerGroups;
-alter table lesson_builder_items drop column ownerGroups;
-alter table lesson_builder_items rename column temp to ownerGroups;
+create table lesson_builder_properties (
+      id number(19,0) not null,
+      attribute varchar2(255 char) not null unique,
+      value clob,
+      primary key (id)
+);
+
+create index lb_item_gb on lesson_builder_items(gradebookid);
+create index lb_item_altgb on lesson_builder_items(altGradebook);
+create index lb_prop_idx on lesson_builder_properties(attribute);
+create index lb_group_site on lesson_builder_groups(siteId);
 -- end LSNBLDR-227
