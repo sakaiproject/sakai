6091b49db45f1579ef898fee6fe40c8c504bd607
Karen Tsao <ktsao@stanford.edu>
2013-10-24 19:13:38 +0000

    SAM-2099: Fix Display of 'Assignment Released To' under Samigo Settings
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@130838 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
@@ -1307,16 +1307,20 @@ public void setFeedbackComponentOption(String feedbackComponentOption) {
 	  HashMap targets = ptHelper.getTargets();
 	  Set e = targets.keySet();
 	  Iterator iter = e.iterator();
-	  int numSelections = getNumberOfGroupsForSite() > 0 ? 2 : 1;
+	  int numSelections = getNumberOfGroupsForSite() > 0 ? 3 : 2;
 	  SelectItem[] target = new SelectItem[numSelections];
 	  ResourceLoader rb = new ResourceLoader("org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages");
 	  while (iter.hasNext()){
-	      String t = (String)iter.next();
-	      if (numSelections == 2 && t.equals(AssessmentAccessControl.RELEASE_TO_SELECTED_GROUPS)) {
-	          target[1] = new SelectItem(t, rb.getString("selected_groups"));
-	      } else if (t.equals(AgentFacade.getCurrentSiteName())) {
-	        target[0] = new SelectItem(t, rb.getString("entire_site"));
-	      }
+		  String t = (String)iter.next();
+		  if (t.equals("Anonymous Users")) {
+			  target[0] = new SelectItem(t, rb.getString("anonymous_users"));
+		  }
+		  else if (numSelections == 3 && t.equals(AssessmentAccessControl.RELEASE_TO_SELECTED_GROUPS)) {
+			  target[2] = new SelectItem(t, rb.getString("selected_groups"));
+		  }
+		  else if (t.equals(AgentFacade.getCurrentSiteName())) {
+			  target[1] = new SelectItem(t, rb.getString("entire_site"));
+		  }
 	  }
 	  return target;
   }
