a4b3614f36c10e7d0dccfec90f33fdcaff5100f2
Ingo Herbote <thewatcher@watchersnet.de>
2013-10-25 13:56:00 +0200

    updated german translation

--- autosave/lang/de.js
+++ autosave/lang/de.js
@@ -3,10 +3,10 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang('autosave', 'de', {
-    loadSavedContent: 'Is wurde ein autom. Gespeicherter Inhalt gefunden (Von "{0}"). Wollen Sie den Dialog öffnen um den Inhalt zu vergleichen, um dann entscheiden zu können ob der Inhalt geladen werden soll?',
-    title: 'Temp. Gespeichert Inhalt mit dem geladenen Inhalt der Website vergleichen',
+    loadSavedContent: 'Es wurde ein autom. Gespeicherter Inhalt gefunden (vom "{0}"). Wollen Sie den Inhalt vergleichen, um dann entscheiden zu können ob der Inhalt geladen werden soll?',
+    title: 'Temp. Gespeichert Inhalt vergleichen',
     loadedContent: 'Geladener Inhalt',
-    autoSavedContent: 'Auton. Gespeicherten Inhalt (Vom: \'',
+    autoSavedContent: 'Autom. Gespeicherten Inhalt (vom: \'',
     ok: 'Lade Autom. Gespeicherten Inhalt',
     diffType: 'Diff Anzeigetyp:',
     sideBySide: 'Nebeneinander',
