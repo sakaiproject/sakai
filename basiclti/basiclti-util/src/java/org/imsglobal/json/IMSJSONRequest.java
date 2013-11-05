4fada5b211893d201262538572adba6f0181eab4
Charles Severance <csev@umich.edu>
2013-11-05 04:41:34 +0000

    SAK-24180
    More rawness in the oauth body hashing.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/branches/BLTI-230@131219 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-util/src/java/org/imsglobal/json/IMSJSONRequest.java
+++ basiclti/basiclti-util/src/java/org/imsglobal/json/IMSJSONRequest.java
@@ -2,6 +2,7 @@ package org.imsglobal.json;
 
 import java.io.Reader;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 import java.net.URLDecoder;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.logging.Logger;
 
 import java.lang.IllegalArgumentException;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
 import net.oauth.OAuth;
@@ -113,35 +115,45 @@ public class IMSJSONRequest {
 		}
 
 		System.out.println("OBH="+oauth_body_hash);
-		final char[] buffer = new char[0x10000];
+        byte[] buf = new byte[1024];
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		int chars = 0;
 		try {
-			StringBuilder out = new StringBuilder();
-			Reader in = request.getReader();
-			int read;
+			ServletInputStream is = request.getInputStream();
+			int readNum;
 			do {
-				read = in.read(buffer, 0, buffer.length);
-				if (read>0) {
-					out.append(buffer, 0, read);
+				readNum = is.read(buf);
+				if (readNum>0) {
+					bos.write(buf, 0, readNum); 
+					chars = chars + readNum;
+					// We dont' want a DOS
+					if ( chars > 10000000 ) {
+						errorMessage = "Message body size exceeded";
+						return;
+					}
 				}
-			} while (read>=0);
-			postBody = out.toString();
+			} while (readNum>=0);
 		} catch(Exception e) {
 			errorMessage = "Could not read message body:"+e.getMessage();
 			return;
 		}
 
+        byte[] bytes = bos.toByteArray();
+
 		try {
+			postBody = new String(bytes, "UTF-8");
 			MessageDigest md = MessageDigest.getInstance("SHA1");
-			md.update(postBody.getBytes()); 
+			md.update(bytes); 
 			byte[] output = Base64.encode(md.digest());
 			String hash = new String(output);
-			System.out.println("HASH="+hash);
+			System.out.println("HASH="+hash+" byts="+bytes.length);
 			if ( ! hash.equals(oauth_body_hash) ) {
-				errorMessage = "Body hash does not match header";
+				errorMessage = "Body hash does not match. bytes="+bytes.length;
+				System.out.println(postBody);
 				return;
 			}
 		} catch (Exception e) {
-			errorMessage = "Could not compute body hash";
+			errorMessage = "Could not compute body hash.  bytes="+bytes.length;
 			return;
 		}
 		valid = true;  // So far we are valid
