7b536901c6edcf30509f34f173b63e09b5114746
Gregory J Thomas <gjthomas@iu.edu>
2013-11-26 22:46:05 +0000

    SAK-23634 - Add User Auditing Log that tracks when users were added/removed/updated from a site
    - Adding to the conversion scripts
    
    
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@131987 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_2_10_0_oracle_conversion.sql
+++ reference/docs/conversion/sakai_2_10_0_oracle_conversion.sql
@@ -919,3 +919,21 @@ alter table GB_GRADEBOOK_T add COURSE_AVERAGE_DISPLAYED number(1, 0) not null;
 update GB_GRADEBOOK_T set TOTAL_POINTS_DISPLAYED=1, COURSE_AVERAGE_DISPLAYED=1 where COURSE_GRADE_DISPLAYED=1;
 update GB_GRADEBOOK_T set TOTAL_POINTS_DISPLAYED=0, COURSE_AVERAGE_DISPLAYED=0 where COURSE_GRADE_DISPLAYED=0;
 -- END SAK-24337 Gradebook edu service features
+
+-- SAK-23634 - new table to track user add/drop/update actions done in sites
+CREATE TABLE user_audits_log (
+	id NUMBER(19,0) NOT NULL,
+	site_id VARCHAR2(255) NOT NULL,
+	user_id VARCHAR2(99) NOT NULL,
+	role_name VARCHAR2(255) NOT NULL,
+	action_taken VARCHAR2(1) NOT NULL,
+	audit_stamp TIMESTAMP NOT NULL,
+	source VARCHAR2(1),
+	action_user_id VARCHAR2(99),
+	PRIMARY KEY(id)
+);
+
+CREATE INDEX user_audits_log_index ON user_audits_log (id,site_id);
+
+CREATE SEQUENCE user_audits_log_seq;
+-- END SAK-23634
