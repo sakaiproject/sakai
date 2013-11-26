2ecd00794341f43a621ee5428159ca4228d308f3
Gonzalo Silverio <gsilver@umich.edu>
2013-11-26 17:16:39 +0000

    DASH-288 - stub in method for handling paging in mobile view
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/branches/DASH-269@313527 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- dashboard/tool/src/webapp/jqm/js/mob-dash.js
+++ dashboard/tool/src/webapp/jqm/js/mob-dash.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
-    log('ok fine');  
+$(document).ready(function(){
+    //function to examine initial length of $('#activ .pagerCell .pager a[title="Go to next page"]') to show or
+    //not the dummy button (1 and 0)
+
+    //Note - we will need to do this once for each view - Events, Activity, Starred
+
+    //function to forward clicks on dummy button to "next" in hidden pager
+    
+    $('#activ .pagerCell .pager a[title="Go to next page"]').live('click', function(e){
+        // loading items this way works, but starring still being handled via wicket
+        //options: 1) hide stars; 2) show stars for information purposes; 3) spend some time
+        //working around this limitation
+        e.preventDefault();
+        var href=$(this).attr('href');
+        $.ajax({
+            url: href,
+            cache:false,
+            dataType:'html'
+        }).done(function(data){
+            //extract from payload only the new items
+            var newItems = $(data).find('ul.itemList').find('li');
+            //and also extract the new pager
+            var newPager = $(data).find('.pagerCell').find('span.pager');
+            //insert logic here to see if there are any new items to retrieve
+            //by examining length of $('#activ .pagerCell .pager a[title="Go to next page"]');
+            // if no length, hide dummy button, if there is, do not hide
+            //replace old pager with the new one
+            $('.pagerCell span.pager').replaceWith(newPager);
+            //append new items to the list
+            $(newItems).appendTo('#newsPanel');
+            //need something to flag which are the new items inserted - maybe an animation (start off
+            //light yellow background and fade to white) 
+        });
+    });
 });
