27fdb28c38f5402369542fb05a73184d502bdd92
Gonzalo Silverio <gsilver@umich.edu>
2013-11-27 21:37:49 +0000

    DASH-288 - ore subs for paging
    git-svn-id: https://source.sakaiproject.org/svn/dashboard/branches/DASH-269@313528 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- dashboard/tool/src/webapp/jqm/js/mob-dash.js
+++ dashboard/tool/src/webapp/jqm/js/mob-dash.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+
+    if($('.pagerCell span.pager a[title="Go to next page"]').length){
+        $('#newsMoreDummyButton').show();
+    }
+    else {
+        $('#newsMoreDummyButton').hide();
+    }
+    $('#newsMoreDummyButton').click(function(e){
+        e.preventDefault();
+        $('.pagerCell span.pager a[title="Go to next page"]').trigger('click');
+    })
     //function to examine initial length of $('#activ .pagerCell .pager a[title="Go to next page"]') to show or
     //not the dummy button (1 and 0)
 
@@ -26,8 +37,16 @@ $(document).ready(function(){
             // if no length, hide dummy button, if there is, do not hide
             //replace old pager with the new one
             $('.pagerCell span.pager').replaceWith(newPager);
+            //show or not the dummy "More" button
+            if($('.pagerCell span.pager a[title="Go to next page"]').length){
+                console.log($('.pagerCell span.pager a[title="Go to next page"]').length)
+                $('#newsMoreDummyButton').show();
+            }
+            else {
+                $('#newsMoreDummyButton').hide();
+            }
             //append new items to the list
-            $(newItems).appendTo('#newsPanel');
+            $(newItems).appendTo('#newsPanel .itemList');
             //need something to flag which are the new items inserted - maybe an animation (start off
             //light yellow background and fade to white) 
         });
