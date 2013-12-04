ce98c3800bbb004fdedda1ef2329eb5f06b1b968
Bryan Holladay <holladay@longsight.com>
2013-12-04 13:49:49 +0000

    SAM-1604 Edit random draw questions in published assessments
    git-svn-id: https://source.sakaiproject.org/svn/sam/trunk@132192 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/SectionContentsBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/SectionContentsBean.java
@@ -24,8 +24,11 @@
 package org.sakaiproject.tool.assessment.ui.bean.delivery;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
@@ -431,9 +434,21 @@ public class SectionContentsBean
           if(randomDrawDate != null && !"".equals(randomDrawDate)){
 
         	  try{
-        		  //The Date Time is in ISO format
-        		  DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
-        		  DateTime drawDate = fmt.parseDateTime(randomDrawDate);
+
+        		// bjones86 - SAM-1604
+				DateTime drawDate;
+				DateTimeFormatter fmt = ISODateTimeFormat.dateTime();	//The Date Time is in ISO format
+				try {
+					drawDate = fmt.parseDateTime(randomDrawDate);
+				} catch(IllegalStateException ex) {
+					DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+					Date date = df.parse(randomDrawDate);
+					if(date == null) {
+						throw new IllegalArgumentException();
+					}
+					drawDate = new DateTime(date);
+				}
+        		  
         		  //We need the locale to localize the output string
         		  Locale loc = new ResourceLoader().getLocale();
         		  String drawDateString = DateTimeFormat.fullDate().withLocale(loc).print(drawDate);
