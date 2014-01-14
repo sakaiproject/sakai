54df272cf9ab6d20242ffd8595c97326309c069e
John Bush <johntylerbush@gmail.com>
2014-01-14 21:07:44 +0000

    SAK-23187 Allow login filters to be configured outside of the login tool. Useful for CAS/Shibboleth and other types of sign on extensions.
    git-svn-id: https://source.sakaiproject.org/svn/login/trunk@133281 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ login/login-tool/tool/src/java/org/sakaiproject/login/filter/SakaiCasAuthenticationFilter.java
@@ -0,0 +1,168 @@
+/**********************************************************************************
+ * $URL$
+ * $Id$
+ ***********************************************************************************
+ *
+ * Copyright (c) 2005, 2006, 2008 The Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.login.filter;
+
+import org.jasig.cas.client.authentication.DefaultGatewayResolverImpl;
+import org.jasig.cas.client.authentication.GatewayResolver;
+import org.jasig.cas.client.util.AbstractCasFilter;
+import org.jasig.cas.client.util.CommonUtils;
+import org.jasig.cas.client.validation.Assertion;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.cover.SessionManager;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jbush
+ * Date: 1/23/13
+ * Time: 4:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
+
+    /**
+     * The URL to the CAS Server login.
+     */
+    private String casServerLoginUrl;
+
+    /**
+     * Whether to send the renew request or not.
+     */
+    private boolean renew = false;
+
+    /**
+     * Whether to send the gateway request or not.
+     */
+    private boolean gateway = false;
+
+    private GatewayResolver gatewayStorage = new DefaultGatewayResolverImpl();
+
+    protected void initInternal(final FilterConfig filterConfig) throws ServletException {
+        if (!isIgnoreInitConfiguration()) {
+            super.initInternal(filterConfig);
+            setCasServerLoginUrl(getPropertyFromInitParams(filterConfig, "casServerLoginUrl", null));
+            log.trace("Loaded CasServerLoginUrl parameter: " + this.casServerLoginUrl);
+            setRenew(parseBoolean(getPropertyFromInitParams(filterConfig, "renew", "false")));
+            log.trace("Loaded renew parameter: " + this.renew);
+            setGateway(parseBoolean(getPropertyFromInitParams(filterConfig, "gateway", "false")));
+            log.trace("Loaded gateway parameter: " + this.gateway);
+
+            final String gatewayStorageClass = getPropertyFromInitParams(filterConfig, "gatewayStorageClass", null);
+
+            if (gatewayStorageClass != null) {
+                try {
+                    this.gatewayStorage = (GatewayResolver) Class.forName(gatewayStorageClass).newInstance();
+                } catch (final Exception e) {
+                    log.error(e,e);
+                    throw new ServletException(e);
+                }
+            }
+        }
+    }
+
+    public void init() {
+        super.init();
+        CommonUtils.assertNotNull(this.casServerLoginUrl, "casServerLoginUrl cannot be null.");
+    }
+
+    public final void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final HttpSession session = request.getSession(false);
+        final String serviceUrl = constructServiceUrl(request, response);
+        final Assertion assertion = session != null ? (Assertion) session.getAttribute(CONST_CAS_ASSERTION) : null;
+
+        if (assertion != null && loggedOutOfSakai()) {
+            log.debug("found a CAS assertion and we are logged out of Sakai. Invalidating the session so we don't get logged back on by an old assertion.");
+            session.invalidate();
+        }  else if (assertion != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String ticket = CommonUtils.safeGetParameter(request,getArtifactParameterName());
+        final boolean wasGatewayed = this.gatewayStorage.hasGatewayedAlready(request, serviceUrl);
+
+        if (CommonUtils.isNotBlank(ticket) || wasGatewayed) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String modifiedServiceUrl;
+
+        log.debug("no ticket and no assertion found");
+        if (this.gateway) {
+            log.debug("setting gateway attribute in session");
+            modifiedServiceUrl = this.gatewayStorage.storeGatewayInformation(request, serviceUrl);
+        } else {
+            modifiedServiceUrl = serviceUrl;
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Constructed service url: " + modifiedServiceUrl);
+        }
+
+        final String urlToRedirectTo = CommonUtils.constructRedirectUrl(this.casServerLoginUrl, getServiceParameterName(), modifiedServiceUrl, this.renew, this.gateway);
+
+        if (log.isDebugEnabled()) {
+            log.debug("redirecting to \"" + urlToRedirectTo + "\"");
+        }
+
+        response.sendRedirect(urlToRedirectTo);
+    }
+
+    private boolean loggedOutOfSakai() {
+        Session session = SessionManager.getCurrentSession();
+        if (session != null && session.getUserEid() != null) {
+            log.debug("currently logged into Sakai");
+            return false;
+        }
+        log.debug("currently logged into Sakai");
+        return true;
+    }
+
+    public final void setRenew(final boolean renew) {
+        this.renew = renew;
+    }
+
+    public final void setGateway(final boolean gateway) {
+        this.gateway = gateway;
+    }
+
+    public final void setCasServerLoginUrl(final String casServerLoginUrl) {
+        this.casServerLoginUrl = casServerLoginUrl;
+    }
+
+    public final void setGatewayStorage(final GatewayResolver gatewayStorage) {
+    	this.gatewayStorage = gatewayStorage;
+    }
+}
