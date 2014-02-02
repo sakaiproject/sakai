af138d463e0947f81e426031bb2781156a4253c0
Charles Severance <csev@umich.edu>
2014-02-02 20:01:07 +0000

    NOJIRA
    Fux nonfatal error in test.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@133994 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-docs/resources/docs/sakai-api-test/util/OAuth.php
+++ basiclti/basiclti-docs/resources/docs/sakai-api-test/util/OAuth.php
@@ -264,11 +264,10 @@ class OAuthRequest {
 
     // Parse the query-string to find and add GET parameters
     $parts = parse_url($http_url);
-    if ( $parts['query'] ) {
+    if ( isset($parts['query']) ) {
       $qparms = OAuthUtil::parse_parameters($parts['query']);
       $parameters = array_merge($qparms, $parameters);
     }
-     
 
     return new OAuthRequest($http_method, $http_url, $parameters);
   }
