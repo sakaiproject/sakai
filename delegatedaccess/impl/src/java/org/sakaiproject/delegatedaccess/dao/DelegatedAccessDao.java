bd43e5163be29a8fb8485b3313854858cd2de08d
Anthony Whyte <arwhyte@umich.edu>
2014-02-05 15:58:06 +0000

    DAC-68 add LICENSE, NOTICE and source file headers, ECL 2.0, copyright Columbia
    git-svn-id: https://source.sakaiproject.org/svn/delegatedaccess/trunk@236913 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- delegatedaccess/impl/src/java/org/sakaiproject/delegatedaccess/dao/DelegatedAccessDao.java
+++ delegatedaccess/impl/src/java/org/sakaiproject/delegatedaccess/dao/DelegatedAccessDao.java
@@ -1,3 +1,19 @@
+/*
+* The Trustees of Columbia University in the City of New York
+* licenses this file to you under the Educational Community License,
+* Version 2.0 (the "License"); you may not use this file
+* except in compliance with the License. You may obtain a copy of the
+* License at:
+*
+* http://opensource.org/licenses/ecl2.txt
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.sakaiproject.delegatedaccess.dao;
 
 import java.util.List;
