60d295403101023cb4fec4a3cef714134e4b35d9
John Bush <johntylerbush@gmail.com>
2014-02-11 19:11:12 +0000

    SAK-25568 move to doc/literal style scrap the wsdl approach go right to annotations
    git-svn-id: https://source.sakaiproject.org/svn/webservices/branches/SAK-25678@134375 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
@@ -0,0 +1,43 @@
+package org.sakaiproject.webservices;
+
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.SessionManager;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * Created by jbush on 2/11/14.
+ */
+@WebService
+public class AbstractWebService {
+    protected SessionManager sessionManager;
+
+    @WebMethod(exclude = true)
+    public void init() {
+    }
+
+    /**
+     * Get the Session related to the given sessionid
+     *
+     * @param sessionid the id of the session to retrieve
+     * @return the session, if it is active
+     * @	if session is inactive
+     */
+    protected Session establishSession(String sessionid) {
+        Session s = sessionManager.getSession(sessionid);
+
+        if (s == null) {
+            throw new RuntimeException("Session \"" + sessionid + "\" is not active");
+        }
+        s.setActive();
+        sessionManager.setCurrentSession(s);
+        return s;
+    }
+
+    @WebMethod(exclude = true)
+    public void setSessionManager(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+}
