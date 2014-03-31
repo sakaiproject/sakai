299b1b248af09b7a448aff91a5b492863f6e549b
Aaron Zeckoski <azeckoski@vt.edu>
2014-03-31 15:29:37 +0000

    Updating the license header to Apereo Foundation for easy copy pasting
    git-svn-id: https://source.sakaiproject.org/svn/master/trunk@307632 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- master/header.java
+++ master/header.java
@@ -1,15 +1,15 @@
-/**********************************************************************************
+/******************************************************************************
  * $URL$
  * $Id$
- ***********************************************************************************
+ ******************************************************************************
  *
- * Copyright (c) 2003, 2004, 2005, 2006, 2008 The Sakai Foundation
+ * Copyright (c) 2003-2014 The Apereo Foundation
  *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Educational Community License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance with 
+ * the License. You may obtain a copy of the License at
  *
- *       http://www.opensource.org/licenses/ECL-2.0
+ *       http://opensource.org/licenses/ecl2
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,5 +17,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- **********************************************************************************/
-
+ *****************************************************************************/
