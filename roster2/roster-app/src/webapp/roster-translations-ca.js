5a45ebf692410f5487cca9f66d342c7712662e98
Miguel Carro Pellicer <miguel@educlever.es>
2014-04-09 09:59:15 +0000

    SAK-25972 - Catalán Translation Update
    git-svn-id: https://source.sakaiproject.org/svn/roster2/trunk@307952 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- roster2/roster-app/src/webapp/roster-translations-ca.js
+++ roster2/roster-app/src/webapp/roster-translations-ca.js
@@ -13,50 +13,87 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-profile_email="Correu electrònic";
-title_missing_participants="Per què alguns estudiants matriculats no figuren a la meva llista de classe?";
-title_missing_participants_link="http://kb.iu.edu/data/ahob.html";
-facet_name="Nom";
-facet_userId="Identificació de l'usuari";
-title_msg="Per afegir o suprimir participants de la Llista, visiteu l'eina Info Lloc.";
-profile_picture_alt="Imatge de";
-currently_displaying_participant="Actualment es mostra {0} participant";
-currently_displaying_participants="Actualment es mostren {0} participants";
-enrollment_status_filter="Veure estudiants en";
-enrollment_status_filter_label="Estat de la matrícula";
-enrollments_currently_displaying="Actualment es mostren {0} {1} estudiants";
-export_roster="Exportar";
-facet_credits="Crèdits";
-facet_email="Adreça electrònica";
-facet_role="Rol";
-facet_status="Estat";
-navbar_overview="Vista general";
-navbar_pics="Fotos";
-navbar_status="Estat de la matrícula";
-no_participants="No s'han trobat resultats";
-no_participants_msg="No s'han trobat resultats per a \"{0}\"";
-no_permission_msg="No teniu permisos per visualitzar la llista.";
-print_roster="Imprimir";
-role_breakdown_fragment="{0} en {1} rol";
-roster_clear_button="Esborrar";
-roster_enrollment_status_all="Tot";
-roster_hide_names="Amagar noms";
-roster_search_button="Cercar";
-roster_search_text="Nom o identificador";
-roster_section_sep_line="---------------------";
-roster_sections_all="Tots els disponibles";
-roster_show_names="Mostrar noms";
-section_filter_post="Participants";
-section_filter_pre="Visualitzar";
-show_profile="Veure el perfil d'aquest participant";
-title_overview="Vista general";
-title_pictures="Fotos";
-title_status="Estat de la matrícula";
-// Sakai 2.7
-facet_groups="Grups";
-navbar_group_membership="Afiliació del Grup ";
-roster_group_bygroup="Per Grup";
-roster_group_unassigned="Sense assignar";
-roster_group_ungrouped="Sense grup";
-title_group_membership="Afiliació del Grup";
-title_msg_groups="Per a afegir o eliminar participants dels Grups, vagi a l'eina d'Administració";
+// Navigation bar links and tooltips
+navbar_overview = "Vista general";
+navbar_overview_tooltip="Resum general de membres de l'espai";
+navbar_pics="Fotografies";
+navbar_pics_tooltip = "Mostra els participants amb les fotografies";
+navbar_group_membership = "Membres del grup ";
+navbar_group_membership_tooltip="Mostra els membres de l'espai organitzats per grups";
+navbar_status = "Estat de la matr\u00EDcula";
+navbar_status_tooltip="Mostra els membres de l'espai organitzats per l'estat de les matr\u00EDcules";
+navbar_permissions = "Autoritzacions"
+navbar_permissions_tooltip="Mostra i edita els permisos dels participants d'aquest espai";
+// Page titles
+title_overview = "Vista general";
+title_pictures = "Fotografies";
+title_group_membership = "Membres del grup";
+title_status = "Estat de la matr\u00EDcula";
+title_permissions = "Autoritzacions";
+// Search
+roster_search_text = "Nom o identificador";
+roster_search_button = "Cerca";
+roster_clear_button = "Neteja";
+// Participants drop-down
+roster_sections_all = "Tots els disponibles";
+roster_section_sep_line = "---------------------";
+// Group membership drop-down
+roster_group_bygroup = "Per grup";
+roster_group_unassigned = "Sense assignar";
+roster_group_ungrouped = "Sense grup";
+// Enrollment drop-down
+roster_enrollment_status_all = "Tots";
+// Participants and roles
+currently_displaying_participant = "Actualment es mostra {0} participant";
+currently_displaying_participants = "Actualment es mostren {0} participants";
+role_breakdown_fragment = "{0} amb rol {1}";
+enrollments_currently_displaying = "Actualment es mostren {0} {1} estudiants";
+// Filters
+section_filter_pre = "Visualitza";
+section_filter_post = "";
+enrollment_status_filter = "Mostra els estudiants a";
+enrollment_status_filter_label = "Estat de la matr\u00EDcula";
+// View table/single column button
+view_single = "Mostra en una \u00FAnica columna";
+view_table = "Mostra en una taula";
+// Print and export buttons
+export_roster = "Exporta l'orla";
+print_roster = "Imprimeix l'orla";
+// Show/hide names button
+roster_hide_names = "Amaga els noms";
+roster_show_names = "Mostra els noms";
+// Official/Profile radio
+roster_show_official_pictures = "Fotografies oficials";
+roster_show_profile_pictures = "Fotografies del perfil";
+// Profile
+show_profile = "Mostra el perfil d'aquest participant";
+profile_connection_accept = "Accepta com a contacte";
+profile_connection_add = "Afegeix com a contacte";
+profile_connection_remove = "Elimina dels contactes";
+profile_connection_requested = "Demana ser afegit com a contacte";
+profile_connection_self = "Aquest \u00E9s vost\u00E8";
+profile_email = "Correu electr\u00F2nic";
+profile_picture_alt = "Fotografia de";
+// Facets
+facet_name = "Nom";
+facet_userId = "Identificaci\u00F3 de l'usuari";
+facet_email = "Adre\u00E7a electr\u00F2nica";
+facet_role = "Rol";
+facet_status = "Estat";
+facet_credits = "Cr\u00E8dits";
+facet_groups = "Grups";
+// Page title messages
+title_msg = "Per afegir o suprimir participants de la llista, visiteu l'eina \"Informaci\u00F3 de l'espai\".";
+title_msg_groups = "Per a afegir o eliminar participants dels grups, aneu a l'eina \"Informaci\u00F3 de l'espai\".";
+title_msg_permissions = "Estableix els permisos corresponents als rols d'aquest espai"
+no_participants = "No s'han trobat resultats";
+// TODO this is currently unused but could display the search term entered
+no_participants_msg = "No s''han trobat resultats per a \"{0}\"";
+// TODO this is currently unused, but could be used to display a message
+no_permission_msg = "No teniu autoritzaci\u00F3 per veure la llista.";
+// Permissions
+roster_permissions_role = "Rol"
+roster_save_button = "Desa"
+roster_cancel_button = "Cancel\u00B7la"
+//title_missing_participants = "Per qu\u00E8 alguns estudiants matriculats no figuren a la meva llista de classe?";
+//title_missing_participants_link = "http://kb.iu.edu/data/ahob.html";
