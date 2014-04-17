322b3b116f0a899865b252b2aaaa0f3b09cefeb0
Matthew Buckett <buckett@bumph.org>
2014-04-17 16:48:47 +0000

    KNL-1123 Update mocks for password validation
    git-svn-id: https://source.sakaiproject.org/svn/sitestats/trunk@308305 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- sitestats/sitestats-impl/src/test/org/sakaiproject/sitestats/test/mocks/FakeUserDirectoryService.java
+++ sitestats/sitestats-impl/src/test/org/sakaiproject/sitestats/test/mocks/FakeUserDirectoryService.java
@@ -261,4 +261,9 @@ public class FakeUserDirectoryService implements UserDirectoryService {
 	return UserDirectoryService.PasswordRating.PASSED_DEFAULT;
     }
 
+	@Override
+	public PasswordPolicyProvider getPasswordPolicy() {
+		return null;
+	}
+
 }
