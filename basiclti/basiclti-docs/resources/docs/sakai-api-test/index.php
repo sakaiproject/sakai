728d63dbd6d3cd753ff3b1eabfa18c93f39f0625
Charles Severance <csev@umich.edu>
2014-05-05 16:00:23 +0000

    NOJIRA
    Update the top level index.php to link to the LTI 2.0 links
    in the test harness.
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/basiclti/trunk@309174 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- basiclti/basiclti-docs/resources/docs/sakai-api-test/index.php
+++ basiclti/basiclti-docs/resources/docs/sakai-api-test/index.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 ini_set("display_errors", 1);
+require_once("util/lti_util.php");
 header('Content-Type: text/html; charset=utf-8');
 session_start();
 ?>
@@ -14,22 +15,21 @@ session_start();
 <img src="http://www.sakaiger.com/images/Sakaiger.png" align="right"></a>
 <h1>Sakai LTI Unit Tests</h1>
 <p>
-This code is sample code and unit test code for IMS LTI 1.1 
-that is maintained in the Sakai project.  It not only
-supports the IMS LTI 1.1 code - it also supports the Sakai
-LTI extensions.  
+This code is sample code and unit test code for IMS LTI 1.0, 1.1, and
+2.0 that is maintained in the Sakai project.  
+It also supports the Sakai LTI extensions.  
 </p>
 <p>
 The latest Sakai LTI documentation is here:
 <a href="https://confluence.sakaiproject.org/display/LTI/Home" target="_blank">
 https://confluence.sakaiproject.org/display/LTI/Home</a>.
 </p>
-<p>If you want to test your LMS with LTI 1.1 or Sakai's extensions, use
+<ul>
+<li>
+<p>If you want to test your LMS with LTI 1.0 or LTI 1.1 or Sakai's extensions, use
 the following test harness:
 <pre>
 <?php
-require_once("util/lti_util.php");
-
   $cur_url = curPageURL();
   $cur_url = str_replace("index.php","tool.php",$cur_url);
 echo("URL: ".$cur_url."\n");
@@ -38,17 +38,58 @@ Key: 12345
 Secret: secret
 </pre>
 </p>
+</li>
+<li>
 <p>
-If you want to test your tool, you can use this fake LMS test harness:
+If you want to test your LTI 1.0 or 1.1 tool, you can use this fake LMS test harness:
 <pre>
 <?php
-require_once("util/lti_util.php");
-
   $cur_url = curPageURL();
   $cur_url = str_replace("index.php","lms.php",$cur_url);
 echo('URL: <a href="'.$cur_url.'">'.$cur_url."</a>\n");
 ?>
 </pre>
+</p>
+</li>
+<li>
+If you want to test your LTI 2.0 LMS, you can use this LTI 2.0 registration URL:
+<pre>
+<?php
+
+  $cur_url = curPageURL();
+  $cur_url = str_replace("index.php","tp.php",$cur_url);
+echo('URL: <a href="'.$cur_url.'">'.$cur_url."</a>\n");
+?>
+</pre>
+</p>
+</li>
+<li>
+If you want to test your LTI 2.0 Tool, you can use this LTI 2.0 Tool Consumer:
+<pre>
+<?php
+  $cur_url = curPageURL();
+  $cur_url = str_replace("index.php","tc.php",$cur_url);
+echo('URL: <a href="'.$cur_url.'">'.$cur_url."</a>\n");
+?>
+</pre>
+</p>
+</li>
+<li>
+If you want to test your LTI 2.0 Tool with Sakai's tool Consumer, you can use our
+nightly server (rebuilt every 4 hours):
+<pre>
+<?php
+echo('URL: <a href="http://nightly2.sakaiproject.org:8082/portal">http://nightly2.sakaiproject.org:8082/portal</a>'."\n");
+?>
+
+How To: <a href="https://www.youtube.com/watch?v=-Dt2Sz5ilLQ">A video of using Sakai's LTI 2.0</a>
+</pre>
+</p>
+</li>
+</ul>
+<p>
+Sakai itself has passed the LTI certifications but this test suite itself 
+has not passed IMS certifications.
 <p>
 You can also compare Base Strings using my
 <a href="basecheck.php" target="_blank">Bsae String Comparison Tool</a>.  This tool also accepts 
