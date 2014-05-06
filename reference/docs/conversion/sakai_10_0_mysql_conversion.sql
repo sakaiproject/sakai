f9f0513f83a74bf7d383a1463e32e4cefd8c4e88
Sam Ottenhoff <ottenhoff@longsight.com>
2014-05-06 20:09:47 +0000

    LSNBLDR-329 fix LessonBuilder SQL scripts to use proper sizes for userId and groupId and move the scripts into official reference/docs/conversion
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@309238 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_10_0_mysql_conversion.sql
+++ reference/docs/conversion/sakai_10_0_mysql_conversion.sql
@@ -1124,3 +1124,56 @@ ALTER TABLE SAKAI_POSTEM_STUDENT MODIFY COLUMN username VARCHAR(255) NOT NULL;
 ALTER TABLE VALIDATIONACCOUNT_ITEM MODIFY COLUMN SURNAME VARCHAR(255) NULL DEFAULT NULL;
 ALTER TABLE VALIDATIONACCOUNT_ITEM MODIFY COLUMN FIRST_NAME VARCHAR(255) NULL DEFAULT NULL;
 -- END SAK-24427 / SAK-25532
+
+-- BEGIN LSNBLDR-329
+alter table lesson_builder_items add showPeerEval bit;
+alter table lesson_builder_items add groupOwned bit;
+alter table lesson_builder_items add ownerGroups longtext;
+alter table lesson_builder_items add attributeString longtext;
+
+create table IF NOT EXISTS lesson_builder_p_eval_results (
+        PEER_EVAL_RESULT_ID bigint not null auto_increment,
+        PAGE_ID bigint not null,
+        TIME_POSTED datetime,
+        GRADER varchar(99) not null,
+        GRADEE varchar(99) not null,
+        ROW_TEXT varchar(255) not null,
+        COLUMN_VALUE integer not null,
+        SELECTED bit,
+        primary key (PEER_EVAL_RESULT_ID)
+);
+
+alter table lesson_builder_pages add groupid varchar(99);
+
+create table IF NOT EXISTS lesson_builder_q_responses (
+        id bigint not null auto_increment,
+        timeAnswered datetime not null,
+        questionId bigint not null,
+        userId varchar(99) not null,
+        correct bit not null,
+        shortanswer longtext,
+        multipleChoiceId bigint,
+        originalText longtext,
+        overridden bit not null,
+        points double precision,
+        primary key (id)
+);
+
+create table IF NOT EXISTS lesson_builder_qr_totals (
+        id bigint not null auto_increment,
+        questionId bigint,
+        responseId bigint,
+        respcount bigint,
+        primary key (id)
+);
+
+alter table lesson_builder_student_pages add groupid varchar(99);
+alter table lesson_builder_items modify html mediumtext; 
+alter table lesson_builder_items modify ownerGroups text;
+alter table lesson_builder_items modify gradebookId varchar(100);
+alter table lesson_builder_items modify altGradebook varchar(100);
+
+create index lesson_builder_qr_questionId_userId on lesson_builder_q_responses(questionId, userId);
+create index lesson_builder_qr_total_qi on lesson_builder_qr_totals(questionId);
+create index lesson_builder_qr_questionId on lesson_builder_q_responses(questionId);
+-- END LSNBLDR-329
