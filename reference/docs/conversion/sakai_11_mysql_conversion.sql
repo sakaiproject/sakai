881b74b13620f99b09875ff650e7381579e89e3f
Sam Ottenhoff <ottenhoff@longsight.com>
2014-05-06 19:56:31 +0000

    SAK-25784 fixing up the conversion scripts
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@309229 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- reference/docs/conversion/sakai_11_mysql_conversion.sql
+++ reference/docs/conversion/sakai_11_mysql_conversion.sql
@@ -1,18 +1,12 @@
--- Convert News to RSS Portlet
+-- SAK-25784 Convert News to RSS Portlet
 -- ---------------------------
 -- Add the titles from all existing news tools
-INSERT INTO sakai_site_tool_property
-(site_id, tool_id, name, value)
-	SELECT site_id, tool_id, 'javax.portlet:portlet_title', title
-	FROM sakai_site_tool
-	WHERE registration = 'sakai.news';
+INSERT INTO SAKAI_SITE_TOOL_PROPERTY (site_id, tool_id, name, value)
+	SELECT site_id, tool_id, 'javax.portlet:portlet_title', title FROM SAKAI_SITE_TOOL WHERE registration = 'sakai.news';
 
 -- Setup all instances with the URL
-UPDATE sakai_site_tool_property
-SET name = 'javax.portlet:feed_url'
-WHERE name = 'channel-url';
+UPDATE SAKAI_SITE_TOOL_PROPERTY SET name = 'javax.portlet:feed_url' WHERE name = 'channel-url';
 
 -- Finally, convert all news tools to the new portlet (must run last)
-UPDATE sakai_site_tool
-SET registration = 'sakai.simple.rss'
-WHERE registration = 'sakai.news';
+UPDATE SAKAI_SITE_TOOL SET registration = 'sakai.simple.rss' WHERE registration = 'sakai.news';
+-- End SAK-25784
