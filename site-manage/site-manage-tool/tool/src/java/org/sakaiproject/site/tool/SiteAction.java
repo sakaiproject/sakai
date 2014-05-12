f825dd1d64c61a26e21eaa795abba167e565c646
Sam Ottenhoff <ottenhoff@longsight.com>
2014-05-12 22:23:56 +0000

    SAK-22918 apply new patch from Juan Jose Merono
    git-svn-id: https://source.sakaiproject.org/svn/site-manage/trunk@309451 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -9970,7 +9970,7 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 		if (email != null) {
 			if (!email.isEmpty() && !EmailValidator.getInstance().isValid(email)) {
 				// invalid email
-				addAlert(state, rb.getFormattedMessage("java.invalid.email", new Object[]{email}));
+				addAlert(state, rb.getFormattedMessage("java.invalid.email", new Object[]{FormattedText.escapeHtml(email,false)}));
 			}
 			siteInfo.site_contact_email = email;
 		}
