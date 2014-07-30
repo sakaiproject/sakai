ebc33f44441b78b26502630134da322a87a8d429
Mark Reilly <mark@alienresident.net>
2014-07-30 19:20:49 +0000

    Adding optional node/grunt build for active developers
    git-svn-id: https://source.sakaiproject.org/svn/reference/trunk@311308 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ reference/library/src/webapp/skin/morpheus-master/Gruntfile.js
@@ -0,0 +1,83 @@
+// Grunt: The JavaScript Task Runner
+module.exports = function(grunt) {
+
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		connect: {
+			server: {
+				options: {
+					port: 8080,
+					protocol: 'http',
+					hostname: 'localhost',
+					base: '.',
+					keepalive: false,
+					livereload: true,
+					open: true
+				}
+			}
+		},
+
+		watch: {
+			sass: {
+				files: ['sass/*.scss' ,'sass/**/*.scss','sass/**/**/*.scss'],
+				tasks: ['compass:dist']
+			},
+      
+      scripts: {
+        files: ['js/src/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        }
+      },
+			css: {
+				files: ['*.css']
+			},
+
+			livereload: {
+				files: [
+					'**/*.css'
+				],
+
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb',
+					sourcemap: true
+				}
+			}
+		},
+    concat: {   
+      dist: {
+        src: [
+          'js/src/*.js', // All JS in the libs folder
+        ],
+        dest: 'js/morpheus.scripts.js',
+      }
+    },
+    
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>' + ' This file is compiled */'
+      },
+      dist: {
+        src: 'js/morpheus.scripts.js',
+        dest: 'js/morpheus.scripts.min.js'
+      }
+    }
+    
+	});
+
+	grunt.registerTask('default', ['connect', 'watch']);
+};
