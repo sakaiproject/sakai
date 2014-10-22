378a34bb09e4e50704823dbb11e95ccb1ee4449d
Brian Jones <bjones86@uwo.ca>
2014-10-22 15:36:47 +0000

    SAK-28003 stop assignments from searching for 'Name, ID, or Email' literally; patch from Brian B.
    git-svn-id: https://source.sakaiproject.org/svn/assignment/trunk@314787 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/assignment-tool/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -4210,6 +4210,12 @@ public class AssignmentAction extends PagedResourceActionII
 		String search = (String) state.getAttribute(VIEW_SUBMISSION_SEARCH);
 		Boolean searchFilterOnly = (state.getAttribute(SUBMISSIONS_SEARCH_ONLY) != null && ((Boolean) state.getAttribute(SUBMISSIONS_SEARCH_ONLY)) ? Boolean.TRUE:Boolean.FALSE);
 
+		// Don't search for "Name, ID, or Email" literally
+		if (search != null && search.equals(rb.getString("search_student_instruction")))
+		{
+			search = null;
+		}
+
 		// get the realm and its member
 		List studentMembers = new ArrayList();
 		Iterator assignments = AssignmentService.getAssignmentsForContext(contextString);
@@ -13309,7 +13315,13 @@ public class AssignmentAction extends PagedResourceActionII
 			String allOrOneGroup = (String) state.getAttribute(VIEW_SUBMISSION_LIST_OPTION);
 			String search = (String) state.getAttribute(VIEW_SUBMISSION_SEARCH);
 			Boolean searchFilterOnly = (state.getAttribute(SUBMISSIONS_SEARCH_ONLY) != null && ((Boolean) state.getAttribute(SUBMISSIONS_SEARCH_ONLY)) ? Boolean.TRUE:Boolean.FALSE);
-            
+
+			// Don't search for "Name, ID, or Email" literally
+			if (search != null && search.equals(rb.getString("search_student_instruction")))
+			{
+				search = null;
+			}
+
 		    Boolean has_multiple_groups_for_user = false;
 		    List submissions = new ArrayList();
 
@@ -13406,6 +13418,12 @@ public class AssignmentAction extends PagedResourceActionII
 			String search = (String) state.getAttribute(VIEW_SUBMISSION_SEARCH);
 			String aRef = (String) state.getAttribute(EXPORT_ASSIGNMENT_REF);
 			Boolean searchFilterOnly = (state.getAttribute(SUBMISSIONS_SEARCH_ONLY) != null && ((Boolean) state.getAttribute(SUBMISSIONS_SEARCH_ONLY)) ? Boolean.TRUE:Boolean.FALSE);
+
+			// Don't search for "Name, ID, or Email" literally
+			if (search != null && search.equals(rb.getString("search_student_instruction")))
+			{
+				search = null;
+			}
 			
 			try {
 			    if (AssignmentService.getAssignment(aRef).isGroup()) {
