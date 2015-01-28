84dd538e77f14588bd459bb615e764953b6c7647
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-01-28 23:21:57 +1100

    Add grade item window stub

---
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanel.java
@@ -0,0 +1,21 @@
+package org.sakaiproject.gradebookng.tool.panels;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * The panel for the add grade item window
+ * @author Steve Swinsburg (steve.swinsburg@gmail.com)
+ *
+ */
+public class AddGradeItemPanel extends Panel {
+
+	private static final long serialVersionUID = 1L;
+
+	public AddGradeItemPanel(String id) {
+		super(id);
+		
+		
+
+	}
+
+}
