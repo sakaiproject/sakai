437d52f99bb36f2ce3a0b03d096af9946edd5523
Payten Giles <payten.giles@gmail.com>
2015-01-30 12:01:11 +1100

    Include reference/library bootstrap v3.3.2 in the base page. Removed custom menu javascript as bootstrap js now controls menus.  Override wicket grid css to force overflow:visible so bootstrap menus display.

--- tool/src/webapp/scripts/gradebook-grades.js
+++ tool/src/webapp/scripts/gradebook-grades.js
@@ -10,56 +10,7 @@ function GradebookSpreadsheet($spreadsheet) {
 };
 
 GradebookSpreadsheet.prototype.initContextSensitiveMenus = function() {
-  var self = this;
-
-  //The Wicket JS is stopping the propagation of click events back up to
-  //the container so cannot bind on the $spreadsheet.  This may become
-  //a performance issue.
-  //self.$spreadsheet.on("click", ".dropdown-toggle", function(event) {
-  $(".dropdown-toggle", self.$spreadsheet).on("click", function(event) {
-    event.preventDefault();
-    event.stopImmediatePropagation();
-
-    var $toggle = $(event.target).closest(".dropdown-toggle");
-
-    // Store the menu on the toggle
-    if (!$toggle.data("menu")) {
-      var tmp_menu = $toggle.closest(".btn-group").find(".dropdown-menu").clone().addClass("gradebook-menu");
-      $toggle.data("menu", tmp_menu);
-      $toggle.closest(".btn-group").find(".dropdown-menu").remove();
-    }
-
-    var $menu = $toggle.data("menu");
-
-    var scrollEvent;
-
-    if ($toggle.is(".on")) {
-      $menu.remove();
-      $(document).off("scroll", scrollEvent);
-    } else {
-      // Hide all other menus
-      self.$spreadsheet.find(".dropdown-toggle.on").trigger("click");
-      // Append the menu to the body
-      $menu.appendTo(document.body).show();
-
-      $menu.css({
-        left: $toggle.offset().left + $toggle.outerWidth() - $menu.outerWidth() + "px",
-        top: $toggle.offset().top + $toggle.outerHeight() - 1 + "px"
-      });
-
-      $(document.body).one("click", function(event) {
-        $(document.body).find(".dropdown-toggle.on").trigger("click");
-      });
-      scrollEvent = $(document).on("scroll", function() {
-        $menu.css({
-          left: $toggle.offset().left + $toggle.outerWidth() - $menu.outerWidth() + "px",
-          top: $toggle.offset().top + $toggle.outerHeight() - 1 + "px"
-        });
-      });
-    }
-
-    $toggle.toggleClass("on");
-  });
+  // let bootstrap handle btn-group dropdown-menus
 };
 
 
