c52c5715d6ba72fcf55a0d9188edab9f39b7c322
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-02-03 22:33:01 +1100

    Fix imports

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/StudentNameCellPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/StudentNameCellPanel.java
@@ -1,24 +1,8 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
-import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.model.ResourceModel;
-import org.sakaiproject.gradebookng.business.StudentSortOrder;
-import org.sakaiproject.gradebookng.tool.model.StringModel;
-import org.sakaiproject.gradebookng.tool.model.StudentName;
 
 /**
  * 
