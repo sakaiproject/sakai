be3819b27deca828c5f694ab17e36b7d9932eff9
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-02-10 23:13:33 +1100

    Add markup for for new grade item window

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanel.java
@@ -1,6 +1,14 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.form.DateTextField;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
 /**
  * The panel for the add grade item window
@@ -14,8 +22,27 @@ public class AddGradeItemPanel extends Panel {
 	public AddGradeItemPanel(String id) {
 		super(id);
 		
+		add(new TextField("title"));
+		add(new TextField("points"));
+		add(new DateTextField("duedate", "MM/DD/yy"));
+		//add(new DropDownChoice("category", new Model()));
+		add(new DropDownChoice("category", new Model(), Arrays.asList(new String[] { "A", "B", "C" })));
 		
 
+	}
+	
+	private class CategorySelectList extends DropDownChoice {
+
+		public CategorySelectList(String id, Model m) {
+			super(id);
+			
+			List categories = Arrays.asList(new String[] { "A", "B", "C" });
+			
+			this.setChoices(categories);
+			
+			
+		}
+		
 	}
 
 }
