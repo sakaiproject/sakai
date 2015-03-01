b278a1acaaad9116dbd50feefe5419ad91690b53
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-03-02 00:08:23 +1100

    Remove EID from  student name link

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/StudentNameCellPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/StudentNameCellPanel.java
@@ -18,40 +18,42 @@ public class StudentNameCellPanel extends Panel {
 
 	private static final long serialVersionUID = 1L;
 	
-	StudentGradesDetailsWindow studentGradesDetailsWindow;
+	DetailsWindow detailsWindow;
 
 	public StudentNameCellPanel(String id, String name, String eid) {
 		super(id);
 		
 		//change this to be a model passed in wrapping the details for the student
 		
+		// also need to have the config passed in
+		
 		//link
-		StudentGradesDetailsLink detailsLink = new StudentGradesDetailsLink("link");
-		add(detailsLink);
+		add(new DetailsLink("link"));
+		
+		//eid
+		add(new EidLabel("eid", eid));
 
-		//details
-		studentGradesDetailsWindow = new StudentGradesDetailsWindow("details");
-		add(studentGradesDetailsWindow);
+		//details window
+		detailsWindow = new DetailsWindow("details");
+		add(detailsWindow);
 		
 	}
 	
-	private class StudentGradesDetailsLink extends AjaxLink {
+	private class DetailsLink extends AjaxLink {
 
-		public StudentGradesDetailsLink(String id) {
+		private static final long serialVersionUID = 1L;
+
+		public DetailsLink(String id) {
 			super(id);
 			
 			//TODO need a model passed in wrapping the details for the user
-			
 			add(new Label("name", new Model<String>("123")));
-			
-			//TODO make this configurable
-			add(new Label("eid", new Model<String>("123123123")));
 			
 		}
 
 		@Override
 		public void onClick(AjaxRequestTarget target) {
-			studentGradesDetailsWindow.show(target);
+			detailsWindow.show(target);
 			
 			//when this appears we want to alter the css so it is
 			//opacity: 0.8; 
@@ -60,17 +62,34 @@ public class StudentNameCellPanel extends Panel {
 			
 		}
 		
+	}
+	
+	/**
+	 * Label for showing a user's eid. Configurable.
+	 *
+	 */
+	private class EidLabel extends Label {
+
+		public EidLabel(String id, String label) {
+			super(id, label);
+
+		}
+		
+		public boolean isVisible() {
+			return true; //TODO use config
+		}
 		
 	}
 	
 	/**
 	 * Window for viewing a student's grades
 	 */
-	private class StudentGradesDetailsWindow extends ModalWindow {
+	private class DetailsWindow extends ModalWindow {
 
-		public StudentGradesDetailsWindow(String componentId) {
+		public DetailsWindow(String componentId) {
 			super(componentId);
 			
+			//TODO point to correct data
 			this.setContent(new AddGradeItemPanel(this.getContentId()));
 			this.setUseInitialHeight(false);
 
