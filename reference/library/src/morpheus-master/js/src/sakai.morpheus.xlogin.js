ebc5e3b31f040c0fc06314f85d81d63ec10f4a18
Eduardo Rey <proyectos@seduerey.com>
2015-05-25 10:40:07 +0200

    SAK-29416 - Remove gravatar animation from login form

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.xlogin.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.xlogin.js
@@ -1,35 +1,15 @@
-/* XLogin JS snippets */
-
+/* XLogin over portal body */
 
 $PBJQ(document).ready(function(){
 
-  var xlogin = function(){
-
-      var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
-
-      $PBJQ('#Mrphs-xlogin #eid').focusout( function(){
-        if( testEmail.test( $PBJQ(this).val() ) ){
-          $PBJQ("#Mrphs-xlogin #gravatar").attr("src","http://www.gravatar.com/avatar/" + MD5( $PBJQ(this).val().toLowerCase() ) );
-          $PBJQ('#Mrphs-xlogin #gravatar').parent().addClass('gravatar');
-        }else{
-          $PBJQ("#Mrphs-xlogin #gravatar").removeAttr("src");
-          $PBJQ('#Mrphs-xlogin #gravatar').parent().removeClass('gravatar');
-        }
-      });
-  };
-
-  if( $PBJQ('#Mrphs-xlogin').length == 1 ){
-    xlogin();
-  }
-
   if( $PBJQ('#loginLink2').length == 1 ){
+
     $PBJQ('#loginLink2').click( function( e ){
 
       $PBJQ('body').append('<div id="Mrphs-xlogin-container" />');
       $PBJQ('#Mrphs-xlogin-container').load('/portal/xlogin #Mrphs-xlogin',function(){
         $PBJQ('#Mrphs-xlogin-container').addClass('loaded');
         $PBJQ('#Mrphs-xlogin').addClass('loadedByAjax');
-        xlogin();
       });
       $('.Mrphs-portalWrapper').addClass('blurry');
 
