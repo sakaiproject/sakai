d56c9f2feda36a4aead127e4378792aece761904
Stephen Marquard <stephen.marquard@uct.ac.za>
2015-06-10 09:51:05 -0400

    SAM-2570 500 error for anonymous quiz results

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/BeginDeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/BeginDeliveryActionListener.java
@@ -123,6 +123,10 @@ public class BeginDeliveryActionListener implements ActionListener
     
     int action = delivery.getActionMode();
     PublishedAssessmentFacade pub = getPublishedAssessmentBasedOnAction(action, delivery, assessmentId, publishedId);
+
+    AssessmentAccessControlIfc control = pub.getAssessmentAccessControl();
+    boolean releaseToAnonymous = control.getReleaseTo() != null && control.getReleaseTo().indexOf("Anonymous Users")> -1;
+
     if(pub == null){
     	delivery.setOutcome("poolUpdateError");
     	throw new AbortProcessingException("pub is null");
@@ -143,7 +147,7 @@ public class BeginDeliveryActionListener implements ActionListener
       }
     }
     else if (DeliveryBean.REVIEW_ASSESSMENT == action || DeliveryBean.TAKE_ASSESSMENT == action) {
-      if (!authzBean.isUserAllowedToTakeAssessment(pub.getPublishedAssessmentId().toString())) {
+      if (!releaseToAnonymous && !authzBean.isUserAllowedToTakeAssessment(pub.getPublishedAssessmentId().toString())) {
         throw new IllegalArgumentException("User does not have permission to view assessment id " + pub.getPublishedAssessmentId());
       }
     }
