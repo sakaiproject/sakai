5cd02242eff4a3d94cd718a1ec2ea4480b0a7481
pushyami <pushyami@umich.edu>
2015-07-06 14:24:29 -0400

    SAK-29372 Sakai as provider trusting the TC email id and not creatig new users

--- basiclti/basiclti-impl/src/java/org/sakaiproject/lti/impl/jobs/SiteMembershipsSyncJob.java
+++ basiclti/basiclti-impl/src/java/org/sakaiproject/lti/impl/jobs/SiteMembershipsSyncJob.java
@@ -2,10 +2,13 @@ package org.sakaiproject.lti.impl.jobs;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.imsglobal.basiclti.BasicLTIUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.lti.api.LTIException;
 import org.sakaiproject.lti.api.LTIService;
 import org.sakaiproject.lti.api.SiteMembershipsSynchroniser;
@@ -31,18 +34,30 @@ public class SiteMembershipsSyncJob implements StatefulJob {
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 
         M_log.info("SiteMembershipsSyncJob.execute");
+        List<String> emailTrustedConsumersList =null;
+        final String emailTrustedConsumersConfig = ServerConfigurationService
+                .getString("basiclti.provider.email.trusted.consumers", null);
+        if(BasicLTIUtil.isNotBlank(emailTrustedConsumersConfig)) {
+            String[] emailTrustedConsumers = emailTrustedConsumersConfig.split(":");
+            emailTrustedConsumersList = Arrays.asList(emailTrustedConsumers);
+
+        }
 
         // Get the current list of jobs
         List<Map<String, Object>> jobs = ltiService.getMembershipsJobs();
 
         for (Map<String, Object> job : jobs) {
+        	boolean isEmailTrustedConsumer=false;
             String siteId = (String) job.get("SITE_ID");
             String membershipsId = (String) job.get("memberships_id");
             String membershipsUrl = (String) job.get("memberships_url");
             String consumerKey = (String) job.get("consumerkey");
             String ltiVersion = (String) job.get("lti_version");
+            if (emailTrustedConsumersList!=null&&emailTrustedConsumersList.contains(consumerKey)) {
+            	isEmailTrustedConsumer = true;
+            }
 
-            siteMembershipsSynchroniser.synchroniseSiteMemberships(siteId, membershipsId, membershipsUrl, consumerKey, ltiVersion);
+            siteMembershipsSynchroniser.synchroniseSiteMemberships(siteId, membershipsId, membershipsUrl, consumerKey, isEmailTrustedConsumer,ltiVersion);
         }
 	}
 }
