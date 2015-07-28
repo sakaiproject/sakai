90367bb50e5056280f03d06d88e5920203d5c24f
udayg <ghattamaneni.uday@gmail.com>
2015-07-28 15:42:09 -0400

    SAK-29723:Fix gateway page UI
    Fix gateway page UI remove unused space on left and make login form
    clean in mobile view

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
@@ -8,5 +8,13 @@ function toggleUserNav(event){
   $PBJQ(this).toggleClass('is-clicked');
 }
 
+ // Logout Confirm
+  $PBJQ('#loginLink1').click(function(e){
+    if(!confirm("Are you sure you want to log out ?")){
+	e.preventDefault();
+    }
+  });
+
+
 $PBJQ(".js-toggle-user-nav a#loginUser > .Mrphs-userNav__drop", "#loginLinks").on("click", toggleUserNav);
 $PBJQ(".js-toggle-user-nav .Mrphs-userNav__drop", "#loginLinks").on("click", toggleUserNav);
