06df19383711bb2924a1635cae1be3177af3f5d5
Matthew Jones <jonespm@gmail.com>
2015-09-03 13:21:35 -0400

    KNL-1256 - Lower BaseAuthzGroup logging level

--- kernel/kernel-impl/src/main/java/org/sakaiproject/authz/impl/BaseAuthzGroupService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/authz/impl/BaseAuthzGroupService.java
@@ -645,7 +645,9 @@ public abstract class BaseAuthzGroupService implements AuthzGroupService
                         permissions.add(rf.function);
                         roles.add(rf.role);
                     }
-                    M_log.info("Changed permissions for roles (" + roles + ") in " + azGroup.getId() + ": " + permissions);
+                    if (M_log.isDebugEnabled()) {
+                        M_log.debug("Changed permissions for roles (" + roles + ") in " + azGroup.getId() + ": " + permissions);
+                    }
                 }
                 ((SakaiSecurity) securityService()).notifyRealmChanged(azGroup.getId(), roles, permissions);
             } catch (Exception e) {
