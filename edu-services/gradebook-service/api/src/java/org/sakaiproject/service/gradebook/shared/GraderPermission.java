c2ca503cd296374fd03a462b0a626f6a73119658
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-09-07 23:10:31 +1000

    SAK-29867 fix the permission checks to be more explicit in the permissions the methods care about

--- edu-services/gradebook-service/api/src/java/org/sakaiproject/service/gradebook/shared/GraderPermission.java
+++ edu-services/gradebook-service/api/src/java/org/sakaiproject/service/gradebook/shared/GraderPermission.java
@@ -1,5 +1,9 @@
 package org.sakaiproject.service.gradebook.shared;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The list of permissions that can be assigned to a grader
  */
@@ -15,6 +19,18 @@ public enum GraderPermission {
 	@Override
 	public String toString() {
 		return this.name().toLowerCase();
+	}
+	
+	/**
+	 * Helper to get the view and grade permissions as a list
+	 * Used in a few places
+	 * @return
+	 */
+	public static List<String> getStandardPermissions() {
+		List<String> rval = new ArrayList<>();
+		rval.add(GraderPermission.VIEW.toString());
+		rval.add(GraderPermission.GRADE.toString());
+		return rval;
 	}
 
 }
