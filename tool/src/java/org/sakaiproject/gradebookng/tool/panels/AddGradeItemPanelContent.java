1325eca3ccc37654d42c155367d8ae363ff20f67
Payten Giles <payten.giles@gmail.com>
2015-09-08 15:23:00 +1000

    Show the category weight in the category dropdown when weights have been set

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanelContent.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanelContent.java
@@ -11,11 +11,16 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
+import org.sakaiproject.gradebookng.business.util.FormatHelper;
 import org.sakaiproject.service.gradebook.shared.Assignment;
 import org.sakaiproject.service.gradebook.shared.CategoryDefinition;
+import org.sakaiproject.service.gradebook.shared.GradebookService;
+import org.sakaiproject.tool.gradebook.Gradebook;
 
+import java.text.MessageFormat;
 import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
@@ -37,22 +42,30 @@ public class AddGradeItemPanelContent extends Panel {
     public AddGradeItemPanelContent(String id, Model<Assignment> assignment) {
         super(id, assignment);
 
+        final Gradebook gradebook = businessService.getGradebook();
+
         add(new TextField<String>("title", new PropertyModel<String>(assignment, "name")));
         add(new TextField<Double>("points", new PropertyModel<Double>(assignment, "points")));
         add(new DateTextField("duedate", new PropertyModel<Date>(assignment, "dueDate"), "MM/dd/yyyy")); //TODO needs to come from i18n
 
         final List<CategoryDefinition> categories = businessService.getGradebookCategories();
 
-        final Map<Long, String> categoryMap = new HashMap<>();
+        final Map<Long, CategoryDefinition> categoryMap = new HashMap<>();
         for (CategoryDefinition category : categories) {
-            categoryMap.put(category.getId(), category.getName());
+            categoryMap.put(category.getId(), category);
         }
 
         DropDownChoice<Long> categoryDropDown = new DropDownChoice<Long>("category", new PropertyModel<Long>(assignment, "categoryId"), new ArrayList<Long>(categoryMap.keySet()), new IChoiceRenderer<Long>() {
 			private static final long serialVersionUID = 1L;
 
 			public Object getDisplayValue(Long value) {
-                return categoryMap.get(value);
+                CategoryDefinition category = categoryMap.get(value);
+                if (GradebookService.CATEGORY_TYPE_WEIGHTED_CATEGORY == gradebook.getCategory_type()) {
+                    String weight = FormatHelper.formatDoubleAsPercentage(category.getWeight() * 100);
+                    return MessageFormat.format(getString("label.addgradeitem.categorywithweight"), category.getName(), weight);
+                } else {
+                    return category.getName();
+                }
             }
 
             public String getIdValue(Long object, int index) {
