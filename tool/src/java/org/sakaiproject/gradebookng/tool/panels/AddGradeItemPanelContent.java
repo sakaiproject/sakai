93aa408d3b54b4a737b98214990d0926426e09ab
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-09-10 21:38:01 +1000

    Restrict ability to apply extra credit items to an extra credit category #198

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanelContent.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/AddGradeItemPanelContent.java
@@ -1,17 +1,23 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
 import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.extensions.markup.html.form.DateTextField;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
 import org.sakaiproject.gradebookng.business.util.FormatHelper;
@@ -19,13 +25,6 @@ import org.sakaiproject.service.gradebook.shared.Assignment;
 import org.sakaiproject.service.gradebook.shared.CategoryDefinition;
 import org.sakaiproject.service.gradebook.shared.GradebookService;
 import org.sakaiproject.tool.gradebook.Gradebook;
-
-import java.text.MessageFormat;
-import java.util.Date;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 
 /**
  * The panel for the add grade item window
@@ -55,7 +54,7 @@ public class AddGradeItemPanelContent extends Panel {
             categoryMap.put(category.getId(), category);
         }
 
-        DropDownChoice<Long> categoryDropDown = new DropDownChoice<Long>("category", new PropertyModel<Long>(assignment, "categoryId"), new ArrayList<Long>(categoryMap.keySet()), new IChoiceRenderer<Long>() {
+        final DropDownChoice<Long> categoryDropDown = new DropDownChoice<Long>("category", new PropertyModel<Long>(assignment, "categoryId"), new ArrayList<Long>(categoryMap.keySet()), new IChoiceRenderer<Long>() {
 			private static final long serialVersionUID = 1L;
 
 			public Object getDisplayValue(Long value) {
@@ -84,8 +83,18 @@ public class AddGradeItemPanelContent extends Panel {
             }
         });
 
-        add(new CheckBox("extraCredit", new PropertyModel<Boolean>(assignment, "extraCredit")));
-       
+        //if an extra credit category is selected, this will be unchecked and disabled
+        final AjaxCheckBox extraCredit = new AjaxCheckBox("extraCredit", new PropertyModel<Boolean>(assignment, "extraCredit")) {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				//nothing required
+			}
+        };
+        extraCredit.setOutputMarkupId(true);
+        add(extraCredit);
+        
         final AjaxCheckBox released = new AjaxCheckBox("released", new PropertyModel<Boolean>(assignment, "released")) {
 			private static final long serialVersionUID = 1L;
 
@@ -112,8 +121,30 @@ public class AddGradeItemPanelContent extends Panel {
 				target.add(released);
 			}
         };
-        
         add(counted);
+        
+        //behaviour for when a category is chosen. If the category is extra credit, deselect and disable extra credit checkbox
+        categoryDropDown.add(new AjaxFormComponentUpdatingBehavior("onchange") {
+
+			@Override
+			protected void onUpdate(AjaxRequestTarget target) {
+				
+				Long selected = (Long) categoryDropDown.getModelObject();
+				
+				//if extra credit, deselect and disable the 'extraCredit' checkbox
+				CategoryDefinition category = categoryMap.get(selected);
+				
+				if(category.isExtraCredit()) {
+					extraCredit.setModelObject(false);
+					extraCredit.setEnabled(false);
+				} else {
+					extraCredit.setEnabled(true);
+				}
+				target.add(extraCredit);
+			}
+			
+		});
+        
         
     }
 }
