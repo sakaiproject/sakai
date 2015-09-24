5f2d03fc548089a393ecce96b67acec6d843ad37
Payten Giles <payten.giles@gmail.com>
2015-09-24 15:12:41 +1000

    Delivers #240: include the crossed-out eye icon in the Course Grade column header when the Course Grade has not been made visible to students

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/CourseGradeColumnHeaderPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/CourseGradeColumnHeaderPanel.java
@@ -1,10 +1,12 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
+import org.sakaiproject.tool.gradebook.Gradebook;
 
 public class CourseGradeColumnHeaderPanel extends Panel {
 
@@ -17,5 +19,10 @@ public class CourseGradeColumnHeaderPanel extends Panel {
 		super(id);
 
 		add(new Label("title", new ResourceModel("column.header.coursegrade")));
+
+		Gradebook gradebook = businessService.getGradebook();
+
+		add(new WebMarkupContainer("isReleasedFlag").setVisible(gradebook.isCourseGradeDisplayed()));
+		add(new WebMarkupContainer("notReleasedFlag").setVisible(!gradebook.isCourseGradeDisplayed()));
 	}
 }
