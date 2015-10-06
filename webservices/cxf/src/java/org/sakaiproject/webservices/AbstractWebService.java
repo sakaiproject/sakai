ebc965bda2cf4335b1264e85695657f009450d1f
Diego del Blanco Orobitg <ddelblanco@anisakai.com>
2015-10-06 11:04:00 -0700

    SAK-29808 New Webservices to read the user Activity
    -getUserActivityByType returns all the events of a type from a user
    -getUserLogonActivity returns all the logins of a user
    -getUserActivity and getUserActivityRestVersion return the events of a
    user between 2 dates.

--- webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
+++ webservices/cxf/src/java/org/sakaiproject/webservices/AbstractWebService.java
@@ -15,7 +15,6 @@ import org.sakaiproject.authz.api.SecurityService;
 import org.sakaiproject.calendar.api.CalendarService;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.content.api.ContentHostingService;
-import org.sakaiproject.db.api.SqlService;
 import org.sakaiproject.entity.api.EntityManager;
 import org.sakaiproject.event.api.EventTrackingService;
 import org.sakaiproject.event.api.UsageSessionService;
@@ -68,7 +67,7 @@ public class AbstractWebService {
     protected SamLiteService samLiteService;
     protected IdManager idManager;
     protected GradebookExternalAssessmentService gradebookExternalAssessmentService;
-    protected SqlService sqlService;
+
     
     @WebMethod(exclude = true)
     public void init() {
@@ -226,10 +225,5 @@ public class AbstractWebService {
     @WebMethod(exclude = true)
     public void setGradebookExternalAssessmentService(GradebookExternalAssessmentService service) {
         this.gradebookExternalAssessmentService = service;
-    }
-
-    @WebMethod(exclude = true)
-    public void setSqlService(SqlService sqlService) {
-        this.sqlService = sqlService;
     }
 }
--- webservices/cxf/src/java/org/sakaiproject/webservices/Activity.java
+++ webservices/cxf/src/java/org/sakaiproject/webservices/Activity.java
@@ -2,10 +2,8 @@ package org.sakaiproject.webservices;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.event.api.EventQueryService;
 import org.sakaiproject.tool.api.Session;
-import org.sakaiproject.util.Xml;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,14 +13,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
 import java.util.Date;
+import org.sakaiproject.event.api.EventQueryService;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,9 +28,15 @@ import java.util.Date;
 @SOAPBinding(style = SOAPBinding.Style.RPC, use = SOAPBinding.Use.LITERAL)
 public class Activity extends AbstractWebService {
     private static final Log LOG = LogFactory.getLog(Activity.class);
-    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    
+    /**
+     * Returns a list of events of a user between 2 dates. As this one requires 2 Date objects,
+     * it is the one to be called as a normal webservice.
+     * @param eid	is the user that we want to query
+     * @param startDate limit the query ti these dates
+     * @param endDate limit the query ti these dates
+     * @return	String as the result of the Query in xml
+     */
     @WebMethod
     @Path("/getUserActivity")
     @Produces("text/plain")
@@ -47,46 +46,50 @@ public class Activity extends AbstractWebService {
             @WebParam(name = "eid", partName = "eid") @QueryParam("eid") String eid,
             @WebParam(name = "startDate", partName = "startDate") @QueryParam("startDate") Date startDate,
             @WebParam(name = "endDate", partName = "endDate") @QueryParam("endDate") Date endDate) {
-        String query = "select se.event_id, se.event_date, se.event, se.ref, se.context, se.event_code, um.user_id, um.eid from " +
-                "sakai_event se, sakai_session ss, sakai_user_id_map um " +
-                "where se.SESSION_ID = ss.SESSION_ID and um.eid = ? and ";
 
-        if (sqlService.getVendor().equals("oracle")) {
-            query += "EVENT_DATE BETWEEN to_date(?, 'YYYY-MM-DD HH24:MI') AND to_date(?, 'YYYY-MM-DD HH24:MI') ";
-        } else {
-            query += "EVENT_DATE BETWEEN ? AND ?";
+        Session session = establishSession(sessionid);
+
+        if (!securityService.isSuperUser()) {
+            LOG.warn("WS getUserActivity(): Permission denied. Restricted to super users.");
+            throw new RuntimeException("WS getUserActivity(): Permission denied. Restricted to super users.");
         }
-        query += " and um.user_id = ss.session_user order by se.event_date desc";
 
-        return queryEventTableWithEid(sessionid, query, new Object[]{eid, startDate, endDate});
+        return eventQueryService.getUserActivity(eid, startDate, endDate);
     }
-    
-    
+
+    /**
+     * Returns a list of events of a user between 2 dates. This one allows to pass strings, so
+     * this can be called as a Rest Service
+     * @param eid	is the user that we want to query
+     * @param startDateString limit the query ti these dates. In this case as String if we call it as a rest
+     * @param endDateString limit the query ti these dates. In this case as String if we call it as a rest
+     * @return	String as the result of the Query in xml
+     */
     @WebMethod
-    @Path("/getUserActivityStringDates")
+    @Path("/getUserActivityRestVersion")
     @Produces("text/plain")
     @GET
-    public String getUserActivityStringDates(
+    public String getUserActivityRestVersion(
             @WebParam(name = "sessionid", partName = "sessionid") @QueryParam("sessionid") String sessionid,
             @WebParam(name = "eid", partName = "eid") @QueryParam("eid") String eid,
             @WebParam(name = "startDate", partName = "startDate") @QueryParam("startDate") String startDateString,
             @WebParam(name = "endDate", partName = "endDate") @QueryParam("endDate") String endDateString) {
-        
-        Date startDate = getDateFromString(startDateString);
-        Date endDate = getDateFromString(endDateString);
-        String query = "select se.event_id, se.event_date, se.event, se.ref, se.context, se.event_code, um.user_id, um.eid from " +
-                "sakai_event se, sakai_session ss, sakai_user_id_map um " +
-                "where se.SESSION_ID = ss.SESSION_ID and um.eid = ? and ";
 
-        if (sqlService.getVendor().equals("oracle")) {
-            query += "EVENT_DATE BETWEEN to_date(?, 'YYYY-MM-DD HH24:MI') AND to_date(?, 'YYYY-MM-DD HH24:MI') ";
-        } else {
-            query += "EVENT_DATE BETWEEN ? AND ?";
+        Session session = establishSession(sessionid);
+
+        if (!securityService.isSuperUser()) {
+            LOG.warn("WS getUserActivityStringDates(): Permission denied. Restricted to super users.");
+            throw new RuntimeException("WS getUserActivityStringDates(): Permission denied. Restricted to super users.");
         }
-        query += " and um.user_id = ss.session_user order by se.event_date desc";
 
-        return queryEventTableWithEid(sessionid, query, new Object[]{eid, startDate, endDate});
+        return eventQueryService.getUserActivityRestVersion(eid, startDateString, endDateString);
     }
+
+    /**
+     * Returns the User's logon activity.
+     * @param eid	is the user that we want to query
+     * @return	String as the result of the Query in xml
+     */
 
     @WebMethod
     @Path("/getUserLogonActivity")
@@ -95,14 +98,25 @@ public class Activity extends AbstractWebService {
     public String getUserLogonActivity(
             @WebParam(name = "sessionid", partName = "sessionid") @QueryParam("sessionid") String sessionid,
             @WebParam(name = "eid", partName = "eid") @QueryParam("eid") String eid) {
-        String query = ("select se.event_id, se.event_date, se.event, se.ref, se.context, se.event_code, um.user_id, um.eid from " +
-                "sakai_event se, sakai_session ss, sakai_user_id_map um " +
-                "where se.SESSION_ID = ss.SESSION_ID and um.eid = ? and " +
-                "um.user_id = ss.session_user and se.event = 'user.login' order by se.event_date desc");
 
-        return queryEventTableWithEid(sessionid, query, new Object[]{eid});
+        Session session = establishSession(sessionid);
+
+        if (!securityService.isSuperUser()) {
+            LOG.warn("WS getUserLogonActivity(): Permission denied. Restricted to super users.");
+            throw new RuntimeException("WS getUserLogonActivity(): Permission denied. Restricted to super users.");
+        }
+
+
+        return eventQueryService.getUserLogonActivity(eid);
     }
 
+
+    /**
+     * Returns the User's activity filtered by one event type.
+     * @param eid	is the user that we want to query
+     * @param eventType the event type to filter
+     * @return	String as the result of the Query in xml
+     */
 
     @WebMethod
     @Path("/getUserActivityByType")
@@ -112,125 +126,26 @@ public class Activity extends AbstractWebService {
             @WebParam(name = "sessionid", partName = "sessionid") @QueryParam("sessionid") String sessionid,
             @WebParam(name = "eid", partName = "eid") @QueryParam("eid") String eid,
             @WebParam(name = "eventType", partName = "eventType") @QueryParam("eventType") String eventType) {
-        String query = ("select se.event_id, se.event_date, se.event, se.ref, se.context, se.event_code, um.user_id, um.eid from " +
-                "sakai_event se, sakai_session ss, sakai_user_id_map um " +
-                "where se.SESSION_ID = ss.SESSION_ID and um.eid = ? and " +
-                "um.user_id = ss.session_user and se.event = ? order by se.event_date desc");
 
-        return queryEventTableWithEid(sessionid, query, new Object[]{eid, eventType});
-    }
-
-    protected String queryEventTableWithEid(String sessionid, String query, Object[] args) {
         Session session = establishSession(sessionid);
+
         if (!securityService.isSuperUser()) {
-            LOG.warn("NonSuperUser trying to collect activity stats: " + session.getUserId());
-            throw new RuntimeException("NonSuperUser trying to collect activity stats: " + session.getUserId());
+            LOG.warn("WS getUserActivityByType(): Permission denied. Restricted to super users.");
+            throw new RuntimeException("WS getUserActivityByType(): Permission denied. Restricted to super users.");
         }
 
-
-        Document doc = Xml.createDocument();
-        Node results = doc.createElement("events");
-        doc.appendChild(results);
-
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try {
-            conn = sqlService.borrowConnection();
-
-            ps = conn.prepareStatement(query);
-            for (int i = 0; i < args.length; i++) {
-                if (args[i] instanceof String) {
-                    ps.setString(i + 1, (String) args[i]);
-                } else if (args[i] instanceof Date) {
-                    ps.setString(i + 1, df.format(args[i]));
-                    long date = ((Date) args[i]).getTime();
-                }
-            }
-            rs = ps.executeQuery();
-
-            buildXmlFromResultSet(results, rs);
-        } catch (Exception e) {
-            LOG.error(e.getMessage(), e);
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-                }
-            }
-
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-        return Xml.writeDocumentToString(doc);
-
+        return eventQueryService.getUserActivityByType(eid,eventType);
     }
 
 
-    private Document buildXmlFromResultSet(Node results, ResultSet rs) throws SQLException {
-        Document doc = results.getOwnerDocument();
 
-        while (rs.next()) {
-            Node eventNode = doc.createElement("event");
-            results.appendChild(eventNode);
+    /** Dependency: EventQueryService. */
+    /**
+     * @return the EventQueryService collaborator.
+     */
+    protected EventQueryService eventQueryService;
 
-            Node id = doc.createElement("event_id");
-            id.appendChild(doc.createTextNode(rs.getString("event_id")));
-            eventNode.appendChild(id);
-
-            Node event_date = doc.createElement("event_date");
-            event_date.appendChild(doc.createTextNode(df.format(rs.getTimestamp("event_date"))));
-            eventNode.appendChild(event_date);
-
-            Node event = doc.createElement("event_type");
-            event.appendChild(doc.createTextNode(rs.getString("event")));
-            eventNode.appendChild(event);
-
-            Node ref = doc.createElement("ref");
-            ref.appendChild(doc.createTextNode(rs.getString("ref")));
-            eventNode.appendChild(ref);
-
-            Node context = doc.createElement("context");
-            context.appendChild(doc.createTextNode(rs.getString("context")));
-            eventNode.appendChild(context);
-
-            Node event_code = doc.createElement("event_code");
-            event_code.appendChild(doc.createTextNode(rs.getString("event_code")));
-            eventNode.appendChild(event_code);
-
-            Node user_id = doc.createElement("user_id");
-            user_id.appendChild(doc.createTextNode(rs.getString("user_id")));
-            eventNode.appendChild(user_id);
-
-            Node eidNode = doc.createElement("eid");
-            eidNode.appendChild(doc.createTextNode(rs.getString("eid")));
-            eventNode.appendChild(eidNode);
-
-
-        }
-        return doc;
-    }
-
-
-    private Date getDateFromString(String dateString) {
-        try {
-            Date date = df.parse(dateString);
-            return date;
-        } catch (ParseException e) {
-            LOG.warn("Date format should be yyyy-MM-dd HH:mm:ss");
-            return null;
-        }
-
+    public void setEventQueryService(EventQueryService eventQueryService) {
+        this.eventQueryService = eventQueryService;
     }
 }
