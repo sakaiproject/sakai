522dce2c250ff54d02e2e54400f18a402e81fc5d
Steve Swinsburg <steve.swinsburg@gmail.com>
2015-10-26 22:28:18 +1100

    Add category, and wire up the rest

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsCategoryPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsCategoryPanel.java
@@ -1,18 +1,36 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.HeadersToolbar;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.NavigationToolbar;
 import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.Radio;
 import org.apache.wicket.markup.html.form.RadioGroup;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
+import org.sakaiproject.service.gradebook.shared.Assignment;
 import org.sakaiproject.service.gradebook.shared.CategoryDefinition;
 import org.sakaiproject.service.gradebook.shared.GradebookInformation;
 
@@ -24,6 +42,17 @@ public class SettingsCategoryPanel extends Panel {
 	protected GradebookNgBusinessService businessService;
 	
 	IModel<GradebookInformation> model;
+	
+	DataTable table;
+	
+	@Setter @Getter
+	boolean isDropHighest = false;
+	
+	@Setter @Getter
+	boolean isDropLowest = false;
+	
+	@Setter @Getter
+	boolean isKeepHighest = false;
 
 	public SettingsCategoryPanel(String id, IModel<GradebookInformation> model) {
 		super(id, model);
@@ -36,25 +65,20 @@ public class SettingsCategoryPanel extends Panel {
 		super.onInitialize();
 		
 		//get categories
-		List<CategoryDefinition> categories = this.businessService.getGradebookCategories();
+		final List<CategoryDefinition> categories = this.businessService.getGradebookCategories();
 		
-		//parse the categories and see if we have any drophighest/lowest/keep highest and set some flags for the checkboxes to use
-		boolean isDropHighest = false;
-		boolean isDropLowest = false;
-		boolean isKeepHighest = false;
+		//parse the categories and see if we have any drophighest/lowest/keep highest and set the flags for the checkboxes to use
 		for(CategoryDefinition category: categories) {
-			if(category.getDropHighest() != null) {
+			if(category.getDropHighest() != null && category.getDropHighest() > 0) {
 				isDropHighest = true;
 			}
-			if(category.getDrop_lowest() != null) {
+			if(category.getDrop_lowest() != null && category.getDrop_lowest() > 0) {
 				isDropLowest = true;
 			}
-			if(category.getKeepHighest() != null) {
+			if(category.getKeepHighest() != null && category.getKeepHighest() > 0) {
 				isKeepHighest = true;
 			}
-		}
-		
-		
+		}		
 		
 		//category types
 		RadioGroup<Integer> categoryType = new RadioGroup<>("categoryType", new PropertyModel<Integer>(model, "categoryType"));
@@ -67,42 +91,182 @@ public class SettingsCategoryPanel extends Panel {
 		WebMarkupContainer categorySettings = new WebMarkupContainer("categorySettings");
 		
 		//drop highest
-        final AjaxCheckBox dropHighest = new AjaxCheckBox("dropHighest", Model.of(isDropHighest)) {
+        final AjaxCheckBox dropHighest = new AjaxCheckBox("dropHighest", Model.of(isDropHighest())) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onUpdate(AjaxRequestTarget target) {
-				//nothing required
+				setDropHighest(this.getModelObject());
+				target.add(table);
 			}
         };
         dropHighest.setOutputMarkupId(true);
         categorySettings.add(dropHighest);
         
         //drop lowest
-        final AjaxCheckBox dropLowest = new AjaxCheckBox("dropLowest", Model.of(isDropLowest)) {
+        final AjaxCheckBox dropLowest = new AjaxCheckBox("dropLowest", Model.of(isDropLowest())) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onUpdate(AjaxRequestTarget target) {
-				//nothing required
+				setDropLowest(this.getModelObject());
+				target.add(table);
 			}
         };
         dropLowest.setOutputMarkupId(true);
         categorySettings.add(dropLowest);
         
      	//keep highest
-        final AjaxCheckBox keepHighest = new AjaxCheckBox("keepHighest", Model.of(isKeepHighest)) {
+        final AjaxCheckBox keepHighest = new AjaxCheckBox("keepHighest", Model.of(isKeepHighest())) {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			protected void onUpdate(AjaxRequestTarget target) {
-				//nothing required
+				setKeepHighest(this.getModelObject());
+				target.add(table);
 			}
         };
         keepHighest.setOutputMarkupId(true);
         categorySettings.add(keepHighest);
         
         add(categorySettings);
+        
+        //categories
+        final ListDataProvider<CategoryDefinition> categoryProvider = new ListDataProvider<CategoryDefinition>(categories);
+        List<IColumn> cols = new ArrayList<IColumn>();
+        
+        //category name column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        AbstractColumn categoryNameColumn = new AbstractColumn(new Model("Category")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getName()));
+			}
+        };
+        cols.add(categoryNameColumn);
+        
+        //category % column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryPercentColumn = new AbstractColumn(new Model("%")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getWeight()));
+			}
+        };
+        cols.add(categoryPercentColumn);
+        
+        //category extra credit column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryExtraCreditColumn = new AbstractColumn(new Model("Extra Credit")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.isExtraCredit()));
+			}
+        };
+        cols.add(categoryExtraCreditColumn);
+        
+        //category items column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryItemsColumn = new AbstractColumn(new Model("Gradebook Items")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getAssignmentList().size()));
+			}
+        };
+        cols.add(categoryItemsColumn);
+        
+        //category drop highest column
+    	@SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryDropHighestColumn = new AbstractColumn(new ResourceModel("settingspage.categories.drophighestcolhead")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getDropHighest()));
+			}
+        };
+        cols.add(categoryDropHighestColumn);
+        
+        //category drop lowest column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryDropLowestColumn = new AbstractColumn(new ResourceModel("settingspage.categories.droplowestcolhead")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getDrop_lowest()));
+			}
+        };
+        cols.add(categoryDropLowestColumn);
+        
+        
+        //category keep highest column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryKeepHighestColumn = new AbstractColumn(new ResourceModel("settingspage.categories.keephighestcolhead")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(Item cellItem, String componentId, IModel rowModel) {
+				CategoryDefinition category = (CategoryDefinition) rowModel.getObject();
+				cellItem.add(new Label(componentId, category.getKeepHighest()));
+			}
+        };
+        cols.add(categoryKeepHighestColumn);
+        
+        //category remove column
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+		AbstractColumn categoryRemoveColumn = new AbstractColumn(new Model("Remove")){
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void populateItem(final Item cellItem, String componentId, IModel rowModel) {
+				final CategoryDefinition category = (CategoryDefinition) rowModel.getObject();				
+				AjaxButton removeCategory = new AjaxButton(componentId) {
+					private static final long serialVersionUID = 1L;
+					protected void onSubmit(AjaxRequestTarget target, Form<?> f) {
+						categories.remove(category);
+						target.add(table);
+					}
+				};
+				removeCategory.setModel(new ResourceModel("button.remove"));
+				removeCategory.add(new AttributeModifier("class", "btn btn-link"));
+				removeCategory.setOutputMarkupId(true);
+				removeCategory.setDefaultFormProcessing(false);
+				cellItem.add(removeCategory);
+			}
+        };
+        cols.add(categoryRemoveColumn);
+        
+        //table
+        table = new DataTable("categories", cols, categoryProvider, 100);
+        table.addTopToolbar(new HeadersToolbar(table, null));
+        table.setOutputMarkupId(true);
+        add(table);
+        
+        //add category button
+        AjaxButton addCategory = new AjaxButton("addCategory") {
+			private static final long serialVersionUID = 1L;
+        	
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> f) {
+				
+				CategoryDefinition cd = new CategoryDefinition();
+				cd.setAssignmentList(Collections.<Assignment> emptyList());
+				categories.add(cd);
+				target.add(table);
+			}
+        };
+        addCategory.setDefaultFormProcessing(false);
+        add(addCategory);
 		
 	}
+	
+	
+	
 }
