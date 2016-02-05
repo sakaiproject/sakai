d11921feb38d4633d9f7ef54a1382ffd76454677
Bryan Holladay <holladay@longsight.com>
2016-02-04 21:35:59 -0500

    SAM-2121 - Expand/Collapse All for samigo settings

--- samigo/samigo-app/src/webapp/js/authoring.js
+++ samigo/samigo-app/src/webapp/js/authoring.js
@@ -642,3 +642,71 @@ function checkLastHandling(){
 		$(retractDate).next().show();
 	}
 }
+
+//if the containing frame is small, then offsetHeight is pretty good for all but ie/xp.
+//ie/xp reports clientHeight == offsetHeight, but has a good scrollHeight
+function mySetMainFrameHeight(id)
+{
+	mySetMainFrameHeight(id, null);
+}
+
+function mySetMainFrameHeight(id, minHeight)
+{
+	// run the script only if this window's name matches the id parameter
+	// this tells us that the iframe in parent by the name of 'id' is the one who spawned us
+	if (typeof window.name != "undefined" && id != window.name) return;
+
+	var frame = parent.document.getElementById(id);
+	if (frame)
+	{
+
+		var objToResize = (frame.style) ? frame.style : frame;
+
+		var height; 
+		
+		var scrollH = document.body.scrollHeight;
+		var offsetH = document.body.offsetHeight;
+		var clientH = document.body.clientHeight;
+		var innerDocScrollH = null;
+
+		if (typeof(frame.contentDocument) != 'undefined' || typeof(frame.contentWindow) != 'undefined')
+		{
+			// very special way to get the height from IE on Windows!
+			// note that the above special way of testing for undefined variables is necessary for older browsers
+			// (IE 5.5 Mac) to not choke on the undefined variables.
+			var innerDoc = (frame.contentDocument) ? frame.contentDocument : frame.contentWindow.document;
+			innerDocScrollH = (innerDoc != null) ? innerDoc.body.scrollHeight : null;
+		}
+
+		if (document.all && innerDocScrollH != null)
+		{
+			// IE on Windows only
+			height = innerDocScrollH;
+		}
+		else
+		{
+			// every other browser!
+			height = offsetH;
+			if(innerDocScrollH != null && innerDocScrollH > height){
+				height = innerDocScrollH;
+			}
+		}
+
+		// here we fudge to get a little bigger
+		//gsilver: changing this from 50 to 10, and adding extra bottom padding to the portletBody		
+		var newHeight = height + 150;
+		//contributed patch from hedrick@rutgers.edu (for very long documents)
+		if (newHeight > 32760)
+		newHeight = 32760;
+
+		// no need to be smaller than...
+		if(minHeight && minHeight > newHeight){
+			newHeight = minHeight;
+		}
+		objToResize.height=newHeight + "px";
+	
+		var s = " scrollH: " + scrollH + " offsetH: " + offsetH + " clientH: " + clientH + " innerDocScrollH: " + innerDocScrollH + " Read height: " + height + " Set height to: " + newHeight;
+
+	}
+}
+
