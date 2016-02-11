95fd99c702f93678e0c30f0525347c34e8c72bbd
Mark Triggs <mark@dishevelled.net>
2016-02-11 11:10:12 +1100

    Incorporates several changes to make the new "Favorites" drawer look better on mobile devices:
      * Show the "Organize Favorites" tab as just a "Favorites"
        display (without the drag/drop that was there before).  The
        standard drag/drop library doesn't seem to support touch events, and
        mobile users never see the list they were organizing anyway.
    
      * Move the close button to the top right
    
      * Resize the other buttons to fit in a single row
    
      * Fix an issue where the search box wouldn't take focus because it was
        underneath another div.
    
    Based on feedback from @jeffpasch and @kyleblythe, introduce the
    following changes:
    
      * Only show the "Sites" button to logged in users
    
      * If the user isn't logged in, show all sites in the top bar (to
        ensure that Gateway sites are still displayed)
    
      * Remove the "Close" button from the Sites drawer
    
      * Display the Sites drawer as a dropdown-style menu.  Align it with
        the "Sites" button, display a small arrow, and continue to show the
        "Sites" button above the modal overlay to allow the user to click
        again to close.
    
      * In mobile mode, hide the tool menu (if it's shown) prior to
        displaying the sites list.  Otherwise they're both open at once and
        things look odd.
    
      * Remove the dropdown arrow from the user menu display
    
    Suggested by @jeffpasch

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.responsive.menus.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.responsive.menus.js
@@ -3,27 +3,12 @@
  */
 
 function toggleToolsNav(event){
-
-  event.preventDefault();
+  if (event) {
+    event.preventDefault();
+  }
   $PBJQ('body').toggleClass('toolsNav--displayed');
 
 }
-
-function toggleSitesNav(event){
-
-  event.preventDefault();
-  $PBJQ('body').toggleClass('sitesNav--displayed');
-  // remove class if siteNav submenus are activated
-  $PBJQ('#linkNav .Mrphs-sitesNav__dropdown').removeClass('is-clicked');
-  $PBJQ('#linkNav .Mrphs-sitesNav__submenu').removeClass('is-visible');
-
-}
-
-$PBJQ(document).ready(function(){
-	if( $PBJQ('#linkNav').length == 0 ){
-		$PBJQ('.js-toggle-sites-nav').hide();
-	}
-});
 
 $PBJQ(document).ready(function(){
   $PBJQ('i.clickable', '#roleSwitch').click( function(){
@@ -31,6 +16,4 @@ $PBJQ(document).ready(function(){
   });
 });
 
-// Remove toogle sites nav on Skip nav to More sites view @TODO need to clean this toggleSitesNav code since it is not used anymore
-//$PBJQ(".js-toggle-sites-nav", "#skipNav").on("click", toggleSitesNav);
 $PBJQ(".js-toggle-tools-nav", "#skipNav").on("click", toggleToolsNav);
