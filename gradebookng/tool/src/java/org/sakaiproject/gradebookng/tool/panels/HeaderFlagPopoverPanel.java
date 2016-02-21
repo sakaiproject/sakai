a609afbe3b0238d5e682b667445f403464a03139
Steve Swinsburg <steve.swinsburg@gmail.com>
2016-02-21 23:03:53 +1100

    #1752 updated wording and corrected the properties string being set for the popover Also applied the code style to the Panel

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/HeaderFlagPopoverPanel.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/HeaderFlagPopoverPanel.java
@@ -1,27 +1,17 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.core.util.string.ComponentRenderer;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.sakaiproject.gradebookng.business.GbRole;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
 import org.sakaiproject.gradebookng.tool.pages.SettingsPage;
-import org.sakaiproject.user.api.User;
 
 public class HeaderFlagPopoverPanel extends Panel {
 
@@ -33,7 +23,7 @@ public class HeaderFlagPopoverPanel extends Panel {
 	public enum Flag {
 		GRADE_ITEM_EXTRA_CREDIT("label.gradeitem.extracredit"),
 		GRADE_ITEM_RELEASED("label.gradeitem.released"),
-		GRADE_ITEM_NOT_RELEASED("label.gradeitem.released"),
+		GRADE_ITEM_NOT_RELEASED("label.gradeitem.notreleased"),
 		GRADE_ITEM_COUNTED("label.gradeitem.counted"),
 		GRADE_ITEM_NOT_COUNTED("label.gradeitem.notcounted"),
 		COURSE_GRADE_RELEASED("label.coursegrade.released"),
@@ -50,10 +40,8 @@ public class HeaderFlagPopoverPanel extends Panel {
 		}
 	}
 
-
 	Flag flag;
 	Long assignmentId;
-
 
 	public HeaderFlagPopoverPanel(final String id, final Flag flag, final Long assignmentId) {
 		super(id);
@@ -61,7 +49,6 @@ public class HeaderFlagPopoverPanel extends Panel {
 		this.flag = flag;
 		this.assignmentId = assignmentId;
 	}
-
 
 	public HeaderFlagPopoverPanel(final String id, final Flag flag) {
 		super(id);
@@ -73,14 +60,14 @@ public class HeaderFlagPopoverPanel extends Panel {
 	public void onInitialize() {
 		super.onInitialize();
 
-		add(new Label("message", getString(flag.getMessageKey())));
+		add(new Label("message", getString(this.flag.getMessageKey())));
 
-		GbRole role = businessService.getUserRole();
-		boolean isInstructor = GbRole.INSTRUCTOR == role;
+		final GbRole role = this.businessService.getUserRole();
+		final boolean isInstructor = GbRole.INSTRUCTOR == role;
 
 		if (isInstructor) {
-			if (flag == Flag.COURSE_GRADE_NOT_RELEASED || flag == Flag.COURSE_GRADE_RELEASED) {
-				Link link = new Link("link") {
+			if (this.flag == Flag.COURSE_GRADE_NOT_RELEASED || this.flag == Flag.COURSE_GRADE_RELEASED) {
+				final Link link = new Link("link") {
 					@Override
 					public void onClick() {
 						setResponsePage(new SettingsPage());
@@ -88,15 +75,15 @@ public class HeaderFlagPopoverPanel extends Panel {
 				};
 				link.add(new Label("linkText", getString("label.coursegrade.editsettings")));
 				add(link);
-			} else if (assignmentId != null) {
-				AjaxLink link = new AjaxLink("link") {
+			} else if (this.assignmentId != null) {
+				final AjaxLink link = new AjaxLink("link") {
 					@Override
-					public void onClick(AjaxRequestTarget target) {
+					public void onClick(final AjaxRequestTarget target) {
 						// do nothing
 					}
 				};
-				String javascript = String.format("sakai.gradebookng.spreadsheet.editAssignmentFromFlag('%s');",
-						String.valueOf(assignmentId));
+				final String javascript = String.format("sakai.gradebookng.spreadsheet.editAssignmentFromFlag('%s');",
+						String.valueOf(this.assignmentId));
 				link.add(new AttributeAppender("onclick", javascript));
 				link.add(new Label("linkText", getString("assignment.option.edit")));
 				add(link);
