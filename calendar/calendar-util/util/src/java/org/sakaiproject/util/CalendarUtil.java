e01380cd6a04451e167f9d0d09783e614ec3578b
Brian Zhou <brian.zhou@duke.edu>
2016-02-26 15:42:56 -0500

    Personal Practice Test

--- calendar/calendar-util/util/src/java/org/sakaiproject/util/CalendarUtil.java
+++ calendar/calendar-util/util/src/java/org/sakaiproject/util/CalendarUtil.java
@@ -16,9 +16,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * 
  **********************************************************************************/
-
+//Could Use adapter to link cells of calendar
 package org.sakaiproject.util;
 
 import java.text.DateFormat;
