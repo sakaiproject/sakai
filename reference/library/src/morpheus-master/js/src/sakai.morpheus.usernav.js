88bba25f9ba032833a62be52f8b27343916f7f4f
Mark Triggs <mark@dishevelled.net>
2016-02-29 15:04:58 +1100

    SAK-30426 Further changes to favorites and the user dropdown
      * While the "More sites" drawer modal is up, don't allow the page behind it to scroll
    
      * Reposition the arrow on the user dropdown to point at the user's name
    
      * Allow the user to click anywhere on the page to dismiss the user dropdown

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.usernav.js
@@ -6,6 +6,18 @@ function toggleUserNav(event){
   event.preventDefault();
   $PBJQ('.Mrphs-userNav__subnav').toggleClass('is-hidden');
   $PBJQ('.Mrphs-userNav__drop').toggleClass('is-clicked');
+
+  if ($PBJQ('.Mrphs-userNav__drop').hasClass('is-clicked')) {
+    // Add an invisible overlay to allow clicks to close the dropdown
+
+    var overlay = $('<div class="user-dropdown-overlay" />');
+    overlay.css({width: '100%', height: '100%', position: 'fixed', zIndex: 50});
+    overlay.on('click', function () { $(event.target).trigger('click'); });
+
+    $('body').prepend(overlay);
+  } else {
+    $('.user-dropdown-overlay').remove();
+  }
 }
 
  // Logout Confirm
