a9f48f1c64c520e0ca8f1b19b40b1f9d5d61a4c5
Sam Ottenhoff <ottenhoff@longsight.com>
2016-03-04 20:18:07 +0000

    SAK-30455 make sure to record the content.read when we bypass loading the asset

--- kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/content/impl/BaseContentService.java
@@ -7080,6 +7080,9 @@ SiteContentAdvisorProvider, SiteContentAdvisorTypeRegistry, EntityTransferrerRef
 					} else {
 						res.addHeader("Content-Length", Long.toString(len));
 					}
+					
+					// SAK-30455: Track event now so the direct link still records a content.read
+					eventTrackingService.post(eventTrackingService.newEvent(EVENT_RESOURCE_READ, resource.getReference(null), false));
 
 					// Bypass loading the asset and just send the user a link to it.
 					if (directLinkUri != null) {
@@ -7154,10 +7157,6 @@ SiteContentAdvisorProvider, SiteContentAdvisorTypeRegistry, EntityTransferrerRef
 							}
 						}
 					}
-					
-					// Track event - only for full reads
-					eventTrackingService.post(eventTrackingService.newEvent(EVENT_RESOURCE_READ, resource.getReference(null), false));
-
 		        } 
 		        else 
 		        {
