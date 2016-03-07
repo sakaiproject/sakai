9ac186c2618174785343be2dc49a84f3335d7b35
Adrian Fish <adrian.r.fish@gmail.com>
2016-03-07 17:37:30 +0000

    SAK-30463 Added incomingConnectionRequests feed

--- profile2/tool/src/java/org/sakaiproject/profile2/tool/entityprovider/ProfileEntityProvider.java
+++ profile2/tool/src/java/org/sakaiproject/profile2/tool/entityprovider/ProfileEntityProvider.java
@@ -54,6 +54,7 @@ import org.sakaiproject.profile2.logic.ProfileLinkLogic;
 import org.sakaiproject.profile2.logic.ProfileLogic;
 import org.sakaiproject.profile2.logic.SakaiProxy;
 import org.sakaiproject.profile2.model.BasicConnection;
+import org.sakaiproject.profile2.model.Person;
 import org.sakaiproject.profile2.model.ProfileImage;
 import org.sakaiproject.profile2.model.UserProfile;
 import org.sakaiproject.profile2.util.Messages;
@@ -363,6 +364,28 @@ public class ProfileEntityProvider extends AbstractEntityProvider implements Cor
 		}
 		else
 			return Messages.getString("Label.friend.add");
+	}
+
+    @EntityCustomAction(action="incomingConnectionRequests", viewKey=EntityView.VIEW_SHOW)
+	public Object getIncomingConnectionRequests(EntityView view, EntityReference ref) {
+		
+		if(!sakaiProxy.isLoggedIn()) {
+			throw new SecurityException("You must be logged in to get the incoming connection list.");
+		}
+		
+		//convert input to uuid
+		String uuid = sakaiProxy.ensureUuid(ref.getId());
+		if(StringUtils.isBlank(uuid)) {
+			throw new EntityNotFoundException("Invalid user.", ref.getId());
+		}
+		
+		//get list of connection requests
+		List<Person> requests = connectionsLogic.getConnectionRequestsForUser(uuid);
+		if(requests == null) {
+			throw new EntityException("Error retrieving connection requests for " + ref.getId(), ref.getReference());
+		}
+		ActionReturn actionReturn = new ActionReturn(requests);
+		return actionReturn;
 	}
 	
 	@EntityURLRedirect("/{prefix}/{id}/account")
