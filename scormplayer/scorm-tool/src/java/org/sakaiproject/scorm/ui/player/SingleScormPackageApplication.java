b6c1a672f92e2b0f9d4a7a5defdc1a242fb15420
Brian Jones <bjones86@uwo.ca>
2016-03-17 15:15:27 -0400

    SCORM SCO-128 SCORM Player errors don't bubble to portal

--- scormplayer/scorm-tool/src/java/org/sakaiproject/scorm/ui/player/SingleScormPackageApplication.java
+++ scormplayer/scorm-tool/src/java/org/sakaiproject/scorm/ui/player/SingleScormPackageApplication.java
@@ -6,8 +6,13 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.wicket.Page;
+import org.apache.wicket.Request;
 import org.apache.wicket.RequestCycle;
+import org.apache.wicket.Response;
+import org.apache.wicket.protocol.http.WebRequest;
+import org.apache.wicket.protocol.http.WebRequestCycle;
 import org.apache.wicket.protocol.http.WebRequestCycleProcessor;
+import org.apache.wicket.protocol.http.WebResponse;
 import org.apache.wicket.request.IRequestCycleProcessor;
 import org.apache.wicket.settings.IExceptionSettings;
 import org.sakaiproject.component.cover.ComponentManager;
@@ -21,30 +26,43 @@ import org.sakaiproject.wicket.protocol.http.SakaiWebApplication;
 
 public class SingleScormPackageApplication extends SakaiWebApplication {
 
-	private static Log log = LogFactory.getLog(SingleScormPackageApplication.class);
+	private static final Log LOG = LogFactory.getLog(SingleScormPackageApplication.class);
 
 	@Override
 	public void init() {
-		log.info(">>> Initializing singleScormPackageApplication");
+		LOG.info(">>> Initializing singleScormPackageApplication");
 		super.init();
-		
+
 		mount(new ContentPackageResourceMountStrategy("contentpackages"));
 
 		getExceptionSettings().setUnexpectedExceptionDisplay(IExceptionSettings.SHOW_INTERNAL_ERROR_PAGE);
-		
+
 		getResourceSettings().setDisableGZipCompression(false);
 	}
-	
+
+	@Override
+	public RequestCycle newRequestCycle( Request request, Response response )
+	{
+		return new WebRequestCycle( this, (WebRequest) request, (WebResponse) response )
+		{
+			@Override
+			public Page onRuntimeException( Page page, RuntimeException e )
+			{
+				// Let Sakai ErrorReportHandler (BugReport) handle errors
+				throw e;
+			}
+		};
+	}
+
 	@Override
 	protected IRequestCycleProcessor newRequestCycleProcessor() {
-	    return new WebRequestCycleProcessor(){
-	    	@Override
-	    	public void respond(RuntimeException e, RequestCycle requestCycle) {
-	    		log.debug("Exception occured during normal web processing (may be a 'valid' exception)", e);
-	    	    super.respond(e, requestCycle);
-	    	}
-	    	
-	    };
+		return new WebRequestCycleProcessor(){
+			@Override
+			public void respond(RuntimeException e, RequestCycle requestCycle) {
+				LOG.debug("Exception occured during normal web processing (may be a 'valid' exception)", e);
+				super.respond(e, requestCycle);
+			}
+		};
 	}
 
 	@Override
@@ -61,5 +79,4 @@ public class SingleScormPackageApplication extends SakaiWebApplication {
 		}
 		return NotConfiguredPage.class;
 	}
-
 } // class
