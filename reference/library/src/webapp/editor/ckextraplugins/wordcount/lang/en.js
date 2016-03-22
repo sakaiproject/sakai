37267814e48107cf2723982383a64cd6814ddc3f
Matthew Jones <jonespm@gmail.com>
2016-03-21 20:19:02 -0400

    SAK-30538 - Add raw character count value to wordcount plugin

--- reference/library/src/webapp/editor/ckextraplugins/wordcount/lang/en.js
+++ reference/library/src/webapp/editor/ckextraplugins/wordcount/lang/en.js
@@ -12,6 +12,7 @@ CKEDITOR.addPluginLang = function( plugin, lang, obj )
 
 CKEDITOR.addPluginLang('wordcount','en',
 		{
-				'WordCountTxt':'Word Count'
+				'WordCountTxt':'Word Count',
+				'CharCountTxt':'Character Count (Includes HTML formatting characters)'
 		}
 );
