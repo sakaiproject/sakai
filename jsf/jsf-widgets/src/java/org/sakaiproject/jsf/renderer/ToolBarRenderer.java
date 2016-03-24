045ffad9f02e2605828ecc5f20610e60de016d0f
Brian Jones <bjones86@uwo.ca>
2016-03-24 16:55:18 -0400

    SAK-30585 fix HTML structure of navBar for all JSF tools

--- jsf/jsf-widgets/src/java/org/sakaiproject/jsf/renderer/ToolBarRenderer.java
+++ jsf/jsf-widgets/src/java/org/sakaiproject/jsf/renderer/ToolBarRenderer.java
@@ -20,11 +20,9 @@
 *
 **********************************************************************************/
 
-
 package org.sakaiproject.jsf.renderer;
 
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.component.UIComponent;
@@ -41,122 +39,116 @@ import org.sakaiproject.jsf.util.RendererUtil;
  */
 public class ToolBarRenderer extends Renderer
 {
-  /**
-   * This component renders its children
-   * @return true
-   */
-  public boolean getRendersChildren()
-  {
-	  return true;
-  }
-  
-  public boolean supportsComponentType(UIComponent component)
-  {
-    return (component instanceof org.sakaiproject.jsf.component.ToolBarComponent);
-  }
-
-  public void encodeBegin(FacesContext context, UIComponent component) throws IOException
-  {
-	if(!component.isRendered()){
-		//tool_bar tag is not to be rendered, return now
-		return;
-	}
-    ResponseWriter writer = context.getResponseWriter();
-    writer.write("<ul class=\"navIntraTool actionToolbar jsfToolbar\">");
-
-    return;
-  }
-  
-  /**
-   * We put all our processing in the encodeChildren method
-   * @param context
-   * @param component
-   * @throws IOException
-   */
-  public void encodeChildren(FacesContext context, UIComponent component)
-    throws IOException
-  {
-    if (!component.isRendered())
+    /**
+     * This component renders its children
+     * @return true
+     */
+    public boolean getRendersChildren()
     {
-      return;
+        return true;
     }
 
-    String clientId = null;
-
-    if (component.getId() != null &&
-      !component.getId().startsWith(UIViewRoot.UNIQUE_ID_PREFIX))
+    public boolean supportsComponentType(UIComponent component)
     {
-      clientId = component.getClientId(context);
+        return (component instanceof org.sakaiproject.jsf.component.ToolBarComponent);
     }
 
-    ResponseWriter writer = context.getResponseWriter();
-
-    if (clientId != null)
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException
     {
-      writer.startElement("ul", component);
-      //writer.append(" class=\"jsfToolbar\" "); // we don't seem to get here
+        if(!component.isRendered()){
+            //tool_bar tag is not to be rendered, return now
+            return;
+        }
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write("<ul class=\"navIntraTool actionToolbar\">");
     }
 
-    @SuppressWarnings("unchecked")
-    List<UIComponent> children = component.getChildren();
-
-    // this is a special separator attribute, not supported by UIData
-    String separator = (String) RendererUtil.getAttribute(context, component, "separator");
-    if (separator==null) separator="";
-
-    boolean first = true;
-    boolean foundCurrent = false;
-    for (Iterator<UIComponent> iter = children.iterator(); iter.hasNext();)
+    /**
+     * We put all our processing in the encodeChildren method
+     * @param context
+     * @param component
+     * @throws IOException
+     */
+    public void encodeChildren(FacesContext context, UIComponent component) throws IOException
     {
-      UIComponent child = (UIComponent)iter.next();
-      // should instead leave the span open, and the item should then add class and aria attributes
-      // depending on the item is (disabled or not) and then close
-      if (child.isRendered()) {
-         if (!first)   
-         {
-        	 writer.write("<li>");
-         }
-         else
-         {
-        	 writer.write("<li class=\"firstToolBarItem\">");
-         }
-         // SAK-23062 improve JSF options menu
-         boolean current = false;
-         if (!foundCurrent) {
-             // check for the "current" attribute on the custom tag, mark that item as current if it set to true
-             boolean hasCurrentIndicated = (child.getAttributes().get("current") != null); // NOTE: child.getAttributes().containsKey("current") will NOT work here
-             current = (hasCurrentIndicated && ((Boolean)child.getAttributes().get("current")).booleanValue());
-             /* this breaks too many other things
-             if (!hasCurrentIndicated && !"javax.faces.Link".equals(child.getRendererType())) {
-                 // basically - if it is not a link, it is probably the current item
-                 current = true;
-             }
-             */
-         }
-         if (current) {
-             foundCurrent = true;
-             writer.write("<span class=\"current\">");
-         } else {
-             writer.write("<span>");
-         }
-         RendererUtil.encodeRecursive(context, child);
-    	 writer.write("</span></li> ");
-         first = false;
-      }
-    } 
-      if (clientId != null)
+        if (!component.isRendered())
         {
-        writer.endElement("ul");
-      }
+            return;
+        }
 
-  }
+        String clientId = null;
 
-  public void encodeEnd(FacesContext context, UIComponent component) throws IOException
-  {
-    ResponseWriter writer = context.getResponseWriter();
-    writer.write("</ul>");
-  }
+        if (component.getId() != null && !component.getId().startsWith(UIViewRoot.UNIQUE_ID_PREFIX))
+        {
+            clientId = component.getClientId(context);
+        }
+
+        ResponseWriter writer = context.getResponseWriter();
+
+        if (clientId != null)
+        {
+            writer.startElement("ul", component);
+            //writer.append(" class=\"jsfToolbar\" "); // we don't seem to get here
+        }
+
+        @SuppressWarnings("unchecked")
+        List<UIComponent> children = component.getChildren();
+
+        // this is a special separator attribute, not supported by UIData
+        String separator = (String) RendererUtil.getAttribute(context, component, "separator");
+        if (separator == null)
+        {
+            separator = "";
+        }
+
+        boolean first = true;
+        boolean foundCurrent = false;
+        for( UIComponent child : children )
+        {
+            // should instead leave the span open, and the item should then add class and aria attributes
+            // depending on the item is (disabled or not) and then close
+            if (child.isRendered()) {
+                if (!first)
+                {
+                    writer.write("<li>");
+                }
+                else
+                {
+                    writer.write("<li class=\"firstToolBarItem\">");
+                }
+                // SAK-23062 improve JSF options menu
+                boolean current = false;
+                if (!foundCurrent) {
+                    // check for the "current" attribute on the custom tag, mark that item as current if it set to true
+                    boolean hasCurrentIndicated = (child.getAttributes().get("current") != null); // NOTE: child.getAttributes().containsKey("current") will NOT work here
+                    current = (hasCurrentIndicated && ((Boolean)child.getAttributes().get("current")));
+                    /* this breaks too many other things
+                     * if (!hasCurrentIndicated && !"javax.faces.Link".equals(child.getRendererType())) {
+                     * // basically - if it is not a link, it is probably the current item
+                     * current = true;
+                     * }
+                     */
+                }
+                if (current) {
+                    foundCurrent = true;
+                    writer.write("<span class=\"current\">");
+                } else {
+                    writer.write("<span>");
+                }
+                RendererUtil.encodeRecursive(context, child);
+                writer.write("</span></li> ");
+                first = false;
+            }
+        }
+        if (clientId != null)
+        {
+             writer.endElement("ul");
+        }
+    }
+
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException
+    {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write("</ul>");
+    }
 }
-
-
-
