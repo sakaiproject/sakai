6bd90135490d6105c67e6562b51270bb1294dff9
Charles Hedrick <hedrick@rutgers.edu>
2016-04-06 10:00:33 -0400

    LSNBLDR-600; need conversoin for oracle, lesson_builder_groups

--- lessonbuilder/components/src/ddl/oracle/simplepage.for-11.sql
+++ lessonbuilder/components/src/ddl/oracle/simplepage.for-11.sql
@@ -4,4 +4,8 @@ alter table lesson_builder_p_eval_results modify (gradee null);
 alter table lesson_builder_p_eval_results modify (row_text null);                                                    
 alter table lesson_builder_p_eval_results add gradee_group varchar2(99) null;
 alter table lesson_builder_p_eval_results add row_id number(20,0) default 0;
+ALTER TABLE lesson_builder_groups ADD (tmpgroups CLOB);
+UPDATE lesson_builder_groups SET tmpgroups=groups;
+ALTER TABLE lesson_builder_groups DROP COLUMN groups;
+ALTER TABLE lesson_builder_groups RENAME COLUMN tmpgroups TO groups;
       
