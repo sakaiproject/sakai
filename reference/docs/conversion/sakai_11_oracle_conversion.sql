2ab0cf9d2819759d8af005437bebe779f49ee927
Earle Nietzel <earle@longsight.com>
2016-04-07 17:00:24 -0400

    SAK-30709 quartz regression with QRTZ_FIRED_TRIGGERS.IS_UPDATE_DATA

--- reference/docs/conversion/sakai_11_oracle_conversion.sql
+++ reference/docs/conversion/sakai_11_oracle_conversion.sql
@@ -530,11 +530,8 @@ alter table qrtz_job_details modify is_nonconcurrent not null;
 alter table qrtz_job_details modify is_update_data not null;
 alter table qrtz_job_details drop column is_stateful;
 alter table qrtz_fired_triggers add is_nonconcurrent varchar2(1);
-alter table qrtz_fired_triggers add is_update_data varchar2(1);
 update qrtz_fired_triggers set is_nonconcurrent = is_stateful;
-update qrtz_fired_triggers set is_update_data = is_stateful;
 alter table qrtz_fired_triggers modify is_nonconcurrent not null;
-alter table qrtz_fired_triggers modify is_update_data not null;
 alter table qrtz_fired_triggers drop column is_stateful;
 --
 -- add new 'sched_name' column to all tables
