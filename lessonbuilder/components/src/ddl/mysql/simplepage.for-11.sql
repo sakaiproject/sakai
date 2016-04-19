7799765c4187fc9a04c8243f19564f0f5b36d04b
Charles Hedrick <hedrick@rutgers.edu>
2016-04-19 13:47:29 -0400

    LSNBLDR-609; increase titles to 255 char

--- lessonbuilder/components/src/ddl/mysql/simplepage.for-11.sql
+++ lessonbuilder/components/src/ddl/mysql/simplepage.for-11.sql
@@ -1,3 +1,5 @@
+alter table lesson_builder_items modify column name varchar(255);
+alter table lesson_builder_pages modify column title varchar(255);
 alter table lesson_builder_p_eval_results modify column gradee varchar(99) null;                                     
 alter table lesson_builder_p_eval_results modify column row_text varchar(255) null;                                  
 alter table lesson_builder_p_eval_results add column gradee_group varchar(99) null;
--- lessonbuilder/components/src/ddl/oracle/simplepage.for-11.sql
+++ lessonbuilder/components/src/ddl/oracle/simplepage.for-11.sql
@@ -1,3 +1,5 @@
+alter table lesson_builder_items modify (name varchar2(255 char));
+alter table lesson_builder_pages modify (title varchar2(255 char));
 alter table lesson_builder_p_eval_results modify (gradee null);                                                      
 alter table lesson_builder_p_eval_results modify (row_text null);                                                    
 alter table lesson_builder_p_eval_results add gradee_group varchar2(99) null;
