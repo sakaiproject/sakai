fdfc2ab80edd34d6984030a86a79360182eca27e
Charles Hedrick <hedrick@rutgers.edu>
2016-04-28 12:44:31 -0400

    LSNBLDR-622; add lessons changes to conversion scripts

--- reference/docs/conversion/sakai_11_oracle_conversion.sql
+++ reference/docs/conversion/sakai_11_oracle_conversion.sql
@@ -757,6 +757,28 @@ CREATE TABLE lti_memberships_jobs (
 
 -- LSNBLDR-500
 alter table lesson_builder_pages add folder varchar2(250);
+-- LSNBLDR-622
+alter table lesson_builder_items modify (name varchar2(255 char));
+alter table lesson_builder_pages modify (title varchar2(255 char));
+alter table lesson_builder_p_eval_results modify (gradee null);                                                      
+alter table lesson_builder_p_eval_results modify (row_text null);                                                    
+alter table lesson_builder_p_eval_results add gradee_group varchar2(99) null;
+alter table lesson_builder_p_eval_results add row_id number(20,0) default 0;
+-- sites new with 10 will already have this but it was missing in 10 conversion scripts
+ALTER TABLE lesson_builder_groups ADD (tmpgroups CLOB);
+UPDATE lesson_builder_groups SET tmpgroups=groups;
+ALTER TABLE lesson_builder_groups DROP COLUMN groups;
+ALTER TABLE lesson_builder_groups RENAME COLUMN tmpgroups TO groups;
+
+create table lesson_builder_ch_status (
+        checklistId number(19,0) not null,
+        checklistItemId number(19,0) not null,
+        owner varchar2(99 char) not null,
+        done number(1,0),
+        primary key (checklistId,checklistItemId,owner)
+ );
+create index lb_p_eval_res_row on lesson_builder_p_eval_results(page_id);
+create index lb_page_folder on lesson_builder_pages(siteId, folder);
 
 -- ------------------------------
 -- DASHBOARD                -----
