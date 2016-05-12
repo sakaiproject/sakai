c608d249c60cfa1c9105fb326c15f9db76025e76
Juan José Meroño Sánchez <jjmerono@um.es>
2016-05-12 22:49:27 +0200

    SAK-31198 Upgrade CAS Client to allow new CAS 4 servers. (#2528)

--- login/login-tool/tool/src/java/org/sakaiproject/login/filter/SakaiCasAuthenticationFilter.java
+++ login/login-tool/tool/src/java/org/sakaiproject/login/filter/SakaiCasAuthenticationFilter.java
@@ -21,10 +21,19 @@
 
 package org.sakaiproject.login.filter;
 
+import org.jasig.cas.client.Protocol;
+import org.jasig.cas.client.authentication.AuthenticationRedirectStrategy;
+import org.jasig.cas.client.authentication.ContainsPatternUrlPatternMatcherStrategy;
+import org.jasig.cas.client.authentication.DefaultAuthenticationRedirectStrategy;
 import org.jasig.cas.client.authentication.DefaultGatewayResolverImpl;
+import org.jasig.cas.client.authentication.ExactUrlPatternMatcherStrategy;
 import org.jasig.cas.client.authentication.GatewayResolver;
+import org.jasig.cas.client.authentication.RegexUrlPatternMatcherStrategy;
+import org.jasig.cas.client.authentication.UrlPatternMatcherStrategy;
+import org.jasig.cas.client.configuration.ConfigurationKeys;
 import org.jasig.cas.client.util.AbstractCasFilter;
 import org.jasig.cas.client.util.CommonUtils;
+import org.jasig.cas.client.util.ReflectUtils;
 import org.jasig.cas.client.validation.Assertion;
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.cover.SessionManager;
@@ -38,6 +47,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -66,25 +77,65 @@ public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
 
     private GatewayResolver gatewayStorage = new DefaultGatewayResolverImpl();
 
+    private AuthenticationRedirectStrategy authenticationRedirectStrategy = new DefaultAuthenticationRedirectStrategy();
+    
+    private UrlPatternMatcherStrategy ignoreUrlPatternMatcherStrategyClass = null;
+    
+    private static final Map<String, Class<? extends UrlPatternMatcherStrategy>> PATTERN_MATCHER_TYPES =
+            new HashMap<String, Class<? extends UrlPatternMatcherStrategy>>();
+    
+    static {
+        PATTERN_MATCHER_TYPES.put("CONTAINS", ContainsPatternUrlPatternMatcherStrategy.class);
+        PATTERN_MATCHER_TYPES.put("REGEX", RegexUrlPatternMatcherStrategy.class);
+        PATTERN_MATCHER_TYPES.put("EXACT", ExactUrlPatternMatcherStrategy.class);
+    }
+
+    public SakaiCasAuthenticationFilter() {
+    	// By default use CAS2 for compatibility
+        this(Protocol.CAS2);
+    }
+
+    public SakaiCasAuthenticationFilter(final Protocol protocol) {
+        super(protocol);
+    }
+    
     protected void initInternal(final FilterConfig filterConfig) throws ServletException {
         if (!isIgnoreInitConfiguration()) {
             super.initInternal(filterConfig);
-            setCasServerLoginUrl(getPropertyFromInitParams(filterConfig, "casServerLoginUrl", null));
-            logger.trace("Loaded CasServerLoginUrl parameter: " + this.casServerLoginUrl);
-            setRenew(parseBoolean(getPropertyFromInitParams(filterConfig, "renew", "false")));
-            logger.trace("Loaded renew parameter: " + this.renew);
-            setGateway(parseBoolean(getPropertyFromInitParams(filterConfig, "gateway", "false")));
-            logger.trace("Loaded gateway parameter: " + this.gateway);
-
-            final String gatewayStorageClass = getPropertyFromInitParams(filterConfig, "gatewayStorageClass", null);
+            setCasServerLoginUrl(getString(ConfigurationKeys.CAS_SERVER_LOGIN_URL));
+            setRenew(getBoolean(ConfigurationKeys.RENEW));
+            setGateway(getBoolean(ConfigurationKeys.GATEWAY));
+                       
+            final String ignorePattern = getString(ConfigurationKeys.IGNORE_PATTERN);
+            final String ignoreUrlPatternType = getString(ConfigurationKeys.IGNORE_URL_PATTERN_TYPE);
+            
+            if (ignorePattern != null) {
+                final Class<? extends UrlPatternMatcherStrategy> ignoreUrlMatcherClass = PATTERN_MATCHER_TYPES.get(ignoreUrlPatternType);
+                if (ignoreUrlMatcherClass != null) {
+                    this.ignoreUrlPatternMatcherStrategyClass = ReflectUtils.newInstance(ignoreUrlMatcherClass.getName());
+                } else {
+                    try {
+                        logger.trace("Assuming {} is a qualified class name...", ignoreUrlPatternType);
+                        this.ignoreUrlPatternMatcherStrategyClass = ReflectUtils.newInstance(ignoreUrlPatternType);
+                    } catch (final IllegalArgumentException e) {
+                        logger.error("Could not instantiate class [{}]", ignoreUrlPatternType, e);
+                    }
+                }
+                if (this.ignoreUrlPatternMatcherStrategyClass != null) {
+                    this.ignoreUrlPatternMatcherStrategyClass.setPattern(ignorePattern);
+                }
+            }
+            
+            final Class<? extends GatewayResolver> gatewayStorageClass = getClass(ConfigurationKeys.GATEWAY_STORAGE_CLASS);
 
             if (gatewayStorageClass != null) {
-                try {
-                    this.gatewayStorage = (GatewayResolver) Class.forName(gatewayStorageClass).newInstance();
-                } catch (final Exception e) {
-                    logger.error("Could not retrieve gatewayStorageClass", e);
-                    throw new ServletException(e);
-                }
+                setGatewayStorage(ReflectUtils.newInstance(gatewayStorageClass));
+            }
+            
+            final Class<? extends AuthenticationRedirectStrategy> authenticationRedirectStrategyClass = getClass(ConfigurationKeys.AUTHENTICATION_REDIRECT_STRATEGY_CLASS);
+
+            if (authenticationRedirectStrategyClass != null) {
+                this.authenticationRedirectStrategy = ReflectUtils.newInstance(authenticationRedirectStrategyClass);
             }
         }
     }
@@ -97,8 +148,14 @@ public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
     public final void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
         final HttpServletRequest request = (HttpServletRequest) servletRequest;
         final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        
+        if (isRequestUrlExcluded(request)) {
+            logger.debug("Request is ignored.");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        
         final HttpSession session = request.getSession(false);
-        final String serviceUrl = constructServiceUrl(request, response);
         final Assertion assertion = session != null ? (Assertion) session.getAttribute(CONST_CAS_ASSERTION) : null;
 
         if (assertion != null && loggedOutOfSakai()) {
@@ -109,8 +166,9 @@ public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
             return;
         }
 
-        final String ticket = CommonUtils.safeGetParameter(request,getArtifactParameterName());
-        final boolean wasGatewayed = this.gatewayStorage.hasGatewayedAlready(request, serviceUrl);
+        final String serviceUrl = constructServiceUrl(request, response);
+        final String ticket = retrieveTicketFromRequest(request);
+        final boolean wasGatewayed = this.gateway && this.gatewayStorage.hasGatewayedAlready(request, serviceUrl);
 
         if (CommonUtils.isNotBlank(ticket) || wasGatewayed) {
             filterChain.doFilter(request, response);
@@ -128,16 +186,15 @@ public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
         }
 
         if (logger.isDebugEnabled()) {
-            logger.debug("Constructed service url: " + modifiedServiceUrl);
+        	logger.debug("Constructed service url: {}", modifiedServiceUrl);
         }
 
-        final String urlToRedirectTo = CommonUtils.constructRedirectUrl(this.casServerLoginUrl, getServiceParameterName(), modifiedServiceUrl, this.renew, this.gateway);
+        final String urlToRedirectTo = CommonUtils.constructRedirectUrl(this.casServerLoginUrl, getProtocol().getServiceParameterName(), modifiedServiceUrl, this.renew, this.gateway);
 
         if (logger.isDebugEnabled()) {
-            logger.debug("redirecting to \"" + urlToRedirectTo + "\"");
+        	logger.debug("redirecting to \"{}\"", urlToRedirectTo);
         }
-
-        response.sendRedirect(urlToRedirectTo);
+        this.authenticationRedirectStrategy.redirect(request, response, urlToRedirectTo);
     }
 
     private boolean loggedOutOfSakai() {
@@ -165,4 +222,18 @@ public class SakaiCasAuthenticationFilter extends AbstractCasFilter {
     public final void setGatewayStorage(final GatewayResolver gatewayStorage) {
     	this.gatewayStorage = gatewayStorage;
     }
+        
+    private boolean isRequestUrlExcluded(final HttpServletRequest request) {
+        if (this.ignoreUrlPatternMatcherStrategyClass == null) {
+            return false;
+        }
+        
+        final StringBuffer urlBuffer = request.getRequestURL();
+        if (request.getQueryString() != null) {
+            urlBuffer.append("?").append(request.getQueryString());
+        }
+        final String requestUri = urlBuffer.toString();
+        return this.ignoreUrlPatternMatcherStrategyClass.matches(requestUri);
+    }
+
 }
