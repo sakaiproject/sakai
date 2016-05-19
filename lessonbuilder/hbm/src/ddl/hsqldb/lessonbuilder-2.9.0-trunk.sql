e551f383bea1195f6202f6384f678159ae7f41b9
Sam Ottenhoff <ottenhoff@longsight.com>
2016-05-19 15:28:50 +0000

    Remove unnecessary index

--- lessonbuilder/hbm/src/ddl/hsqldb/lessonbuilder-2.9.0-trunk.sql
+++ lessonbuilder/hbm/src/ddl/hsqldb/lessonbuilder-2.9.0-trunk.sql
@@ -53,4 +53,3 @@ create index lesson_builder_item_altgb on lesson_builder_items(altGradebook);
 create index lesson_builder_prop_idx on lesson_builder_properties(attribute);
 create index lesson_builder_qr_questionId_userId on lesson_builder_q_responses(questionId, userId);
 create index lesson_builder_qr_total_qi on lesson_builder_qr_totals(questionId);
-create index lesson_builder_qr_questionId on lesson_builder_q_responses(questionId);
