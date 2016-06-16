82bb897c347525a0722484a821484da79489317e
Charles Severance <csev@umich.edu>
2016-06-16 11:06:21 -0400

    SAK-31364 - Restore legacy semicolon processing

---
+++ basiclti/basiclti-common/src/test/org/sakaiproject/basiclti/util/SakaiBLTIUtilTest.java
@@ -0,0 +1,55 @@
+package org.sakaiproject.basiclti.util;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sakaiproject.basiclti.util.SakaiBLTIUtil;
+
+public class SakaiBLTIUtilTest {
+
+	public static String [] shouldBeTheSame = {
+		null,
+		"",
+		"     ",
+		" \n \n",
+		"42",
+		"x=1",
+		"x=1;",
+		"x=1;\nx=2;",
+		"x=1; ",
+		"x=1;y=2;99;z=3", // Can have only 1 semicolon between = signs
+		"x=1;42",
+		"x=1;y=2z=3;",
+		"x;19=1;42",
+		"x=1\ny=2\nz=3",
+		"x=1;\ny=2\nz=3"
+	};
+
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+         * If it is null, blank, or has no equal signs return unchanged
+         * If there is one equal sign return unchanged
+         * If there is a new line anywhere in the string after trim, return unchanged
+         * If we see ..=..;..=..;..=..[;] - we replace ; with \n
+	 */
+
+	@Test
+	public void testStrings() {
+		String adj = null;
+		for(String s: shouldBeTheSame) {
+			adj = SakaiBLTIUtil.adjustCustom(s);
+			assertEquals(s, adj);
+		}
+			
+		adj = SakaiBLTIUtil.adjustCustom("x=1;y=2;z=3");
+		assertEquals(adj,"x=1;y=2;z=3".replace(';','\n'));
+		adj = SakaiBLTIUtil.adjustCustom("x=1;y=2;z=3;");
+		assertEquals(adj,"x=1;y=2;z=3;".replace(';','\n'));
+	}
+
+}
+
