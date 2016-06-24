f6874f92b7101b197eb8cdc7ffb90c8bc90a97a6
Matt Clare <matt@mattclare.ca>
2016-06-24 06:35:18 -0400

    SAK-31193 (#2904)
    Add header row information for accessibility purposes to two table examples.

--- reference/library/src/webapp/editor/ckextraplugins/templates/default.js
+++ reference/library/src/webapp/editor/ckextraplugins/templates/default.js
@@ -13,14 +13,12 @@ CKEDITOR.addTemplates('customtemplates', {imagesPath: CKEDITOR.getUrl(CKEDITOR.b
         title:"Strange Template",
         image:"template2.gif"
         ,description:"A template that defines two colums, each one with a title, and some text.",
-        html:'<table cellspacing="0" cellpadding="0" style="width:100%" border="0"><tr><td style="width:50%"><h3>Title 1</h3></td><td></td><td style="width:50%"><h3>Title 2</h3></td></tr><tr><td>Text 1</td><td></td><td>Text 2</td></tr></table><p>More text goes here.</p>'
-    },
+        html:'<table cellspacing="0" cellpadding="0" style="width:100%" border="0"><thead><tr><th style="width:50%" scope="col"><h3>Title 1</h3></th><th></th><th style="width:50%" scope="col"><h3>Title 2</h3></th></tr></thead><tr><td>Text 1</td><td></td><td>Text 2</td></tr></table><p>More text goes here.</p>'    },
     {
         title:"Text and Table",
         image:"template3.gif",
         description:"A title with some text and a table.",
-        html:'<div style="width: 80%"><h3>Title goes here</h3><table style="width:150px;float: right" cellspacing="0" cellpadding="0" border="1"><caption style="border:solid 1px black"><strong>Table title</strong></caption><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr></table><p>Type the text here</p></div>'
-    },
+        html:'<div style="width: 80%"><h3>Title goes here</h3><table style="width:150px;float: right" cellspacing="0" cellpadding="0" border="1"><caption style="border:solid 1px black"><strong>Table title</strong></caption><thead><tr><th scope="col">&nbsp;</th><th scope="col">&nbsp;</th><th>&nbsp;</th></tr></thead><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr></table><p>Type the text here</p></div>'    },
     {
         title: 'Two-column',
         image: 'template4.gif',
