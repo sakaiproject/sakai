4b879fe4aca8d12a72880905166414e09cbb2d9b
Payten Giles <payten.giles@gmail.com>
2016-06-24 10:27:16 +1000

    2791: ensure ajaxbutton stays disabled when it invokes a redirect and that redirect is SLOW

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbAjaxButton.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbAjaxButton.java
@@ -20,11 +20,20 @@ public class GbAjaxButton extends AjaxButton {
 		super.updateAjaxAttributes(attributes);
 		attributes.getAjaxCallListeners().add(new AjaxCallListener()
 			// disable the button right away after clicking it
-			.onBefore(String.format("$('#%s').prop('disabled',true);", getMarkupId()))
-			// add a slight delay in re-enabling it, just in case it succeeded and there's
-			// a delay in closing a parent modal
+			// and mark on it if the window is unloading
+			.onBefore(
+				String.format(
+					"$('#%s').prop('disabled', true);" +
+						"$(window).on('beforeunload', function() {" +
+						"$('#%s').data('unloading', true).prop('disabled', true)});",
+					getMarkupId(), getMarkupId()))
+			// if the page is unloading, keep it disabled, otherwise
+			// add a slight delay in re-enabling it, just in case it succeeded
+			// and there's a delay in closing a parent modal
 			.onComplete(
-				String.format("setTimeout(function() { $('#%s').prop('disabled',false); }, 1000);",
-					getMarkupId())));
+				String.format("setTimeout(function() {" +
+					"if (!$('#%s').data('unloading')) $('#%s').prop('disabled',false);" +
+					"}, 1000)",
+					getMarkupId(), getMarkupId())));
 	}
 }
