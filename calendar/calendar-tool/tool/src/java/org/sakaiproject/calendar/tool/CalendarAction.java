0d99a5fde046f9a639fbf4537e59d4d31be9fe62
frasese <francisco.saez.segura@gmail.com>
2016-06-27 14:19:16 +0200

    SAK-31413 Calendar - update default configuration for private url
    SAK-31413 : Change the default configuration in sakai 11.0 so that the
    option for users to generate a private URL for a calendar is enabled by
    default, create an additional property: ical.public.secureurl.subscribe

--- calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
+++ calendar/calendar-tool/tool/src/java/org/sakaiproject/calendar/tool/CalendarAction.java
@@ -7668,7 +7668,7 @@ extends VelocityPortletStateAction
 		
 		
 		// A link for subscribing to the implicit calendar
-		if ( ServerConfigurationService.getBoolean("ical.opaqueurl.subscribe", true) )
+		if ( ServerConfigurationService.getBoolean("ical.public.secureurl.subscribe", ServerConfigurationService.getBoolean("ical.opaqueurl.subscribe", true)) )
 		{
 			bar.add( new MenuEntry(rb.getString("java.opaque_subscribe"), rb.getString("java.opaque_subscribe.title"), null, allow_subscribe_this, MenuItem.CHECKED_NA, "doOpaqueUrl") );
 		}
