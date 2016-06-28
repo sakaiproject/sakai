679b43b953305235dcab1f784630dbead24a1211
Matthew Buckett <buckett@bumph.org>
2016-06-28 14:50:02 +0100

    SAK-31460 Changes for optional Quick Links functionality.

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -20,12 +20,19 @@
  **********************************************************************************/
 package org.sakaiproject.portal.charon;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -230,6 +237,8 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 	private boolean sakaiTutorialEnabled = true;
 	
 	private String handlerPrefix;
+
+	private List<Map>relatedLinks;
 
 	private PageFilter pageFilter = new PageFilter() {
 
@@ -1802,6 +1811,9 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			String image2 = null;
 			String logoutWarningMessage = "";
 
+			// The related links
+			List relatedLinks = null;
+
 			// for showing user display name and id next to logout (SAK-10492)
 			String loginUserDispName = null;
 			String loginUserDispId = null;
@@ -1892,9 +1904,11 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 
 				// since we are doing logout, cancel top.login
 				topLogin = false;
-				
+
 				logoutWarningMessage = ServerConfigurationService.getBoolean("portal.logout.confirmation",false)?rloader.getString("sit_logout_warn"):"";
+				relatedLinks = this.relatedLinks;
 			}
+
 			rcontext.put("userIsLoggedIn", session.getUserId() != null);
 			rcontext.put("loginTopLogin", Boolean.valueOf(topLogin));
 			rcontext.put("logoutWarningMessage", logoutWarningMessage);
@@ -1957,6 +1971,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 				rcontext.put("loginUserId", loginUserId);
 			}
 			rcontext.put("displayUserloginInfo", displayUserloginInfo && loginUserDispId != null);
+			rcontext.put("relatedLinks", relatedLinks);
 		}
 	}
 
@@ -2014,6 +2029,61 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 		gatewaySiteUrl = ServerConfigurationService.getString("gatewaySiteUrl", null);
 		
 		sakaiTutorialEnabled = ServerConfigurationService.getBoolean("portal.use.tutorial", true);
+
+		// Load the related links if they are in the properties file.
+		List<String> linkUrls = Arrays.asList(ArrayUtils.nullToEmpty(ServerConfigurationService.getStrings("related.link.url")));
+		List<String> linkTitles = Arrays.asList(ArrayUtils.nullToEmpty(ServerConfigurationService.getStrings("related.link.title")));
+		List<String> linkNames = Arrays.asList(ArrayUtils.nullToEmpty(ServerConfigurationService.getStrings("related.link.name")));
+		List<String> linkIcons = Arrays.asList(ArrayUtils.nullToEmpty(ServerConfigurationService.getStrings("related.link.icon")));
+
+		if (!linkUrls.isEmpty()) {
+			List<Map> relatedLinks = new ArrayList<Map>(linkUrls.size());
+			for (int i = 0; i < linkUrls.size(); i++) {
+				String url = linkUrls.get(i);
+				String title = linkTitles.get(i);
+				String name = linkNames.get(i);
+				String icon = linkIcons.get(i);
+
+				if (url != null) {
+					Map<String, String> linkDetails = new HashMap<String, String>();
+					linkDetails.put("url", url);
+					if (name != null) {
+						linkDetails.put("name", name);
+						if (title != null) {
+							linkDetails.put("title", title);
+						} else {
+							linkDetails.put("title", name);
+						}
+					} else {
+						if (title != null) {
+							linkDetails.put("name", title);
+							linkDetails.put("title", title);
+						} else {
+							linkDetails.put("name", url);
+							linkDetails.put("title", url);
+						}
+					}
+					if (icon != null) {
+						// if the 'related.link.icon' value has a type and at least one character for the icon name then try to parse it.
+						if (icon.length()>3){
+							String iconType = icon.substring(0,2);
+
+							if (iconType.equalsIgnoreCase("im")) {
+								linkDetails.put("iconType", "image");
+								linkDetails.put("imageURI", icon.substring(3));
+							}
+							else if (iconType.equalsIgnoreCase("cl")){
+								linkDetails.put("iconType", "icon");
+								linkDetails.put("iconClass", icon.substring(3));
+							}
+						}
+					}
+
+					relatedLinks.add(Collections.unmodifiableMap(linkDetails));
+				}
+			}
+			this.relatedLinks = Collections.unmodifiableList(relatedLinks);
+		}
 
 		basicAuth = new BasicAuth();
 		basicAuth.init();
@@ -2182,7 +2252,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 		M_log.info("Log marker " + se.getMethodName() + ":" + se.getFileName() + ":"
 				+ se.getLineNumber());
 	}
-
+	
 	/**
 	 * Check for any just expired sessions and redirect
 	 * 
