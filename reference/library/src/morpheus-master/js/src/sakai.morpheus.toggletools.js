9a907262a8b5c199347b9e16bcee35fd17b5bfbe
Fernando <fer.geraci@gmail.com>
2016-07-01 19:31:47 -0400

    SAK-31456 - Collapsible side-tools menu won't stayed minimized after (#2960)
    page refreshes

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.toggletools.js
@@ -10,22 +10,41 @@ function toggleMinimizeNav(){
   var label = $PBJQ('.accessibility-btn-label' , el);
 
   el.toggleClass('min max');
-
-  if (label.text() == el.data("title-expand")) {
-    label.text(el.data("text-original"));
+  
+  if (label.text() == el.data("title-expand") || collapsed) {
+	document.cookie = "sakai_nav_minimized=false; path=/";
+	collapsed = false;
+	label.text(el.data("text-original"));
     el.attr('title', (el.data("text-original")));
     el.attr('aria-pressed', true);
-    document.cookie = "sakai_nav_minimized=false; path=/";
-
   } else {
-
-    el.data("text-original", label.text());
+	document.cookie = "sakai_nav_minimized=true; path=/";
+	collapsed = true;
+	el.data("text-original", label.text());
     label.text(el.data("title-expand"));
     el.attr('title', (el.data("title-expand")));
     el.attr('aria-pressed', false);
-    document.cookie = "sakai_nav_minimized=true; path=/";
-
   }
 }
 
 $PBJQ(".js-toggle-nav").on("click", toggleMinimizeNav);
+
+var collapsed = false;
+
+$PBJQ(document).ready(function(){
+	if(getCookieVal('sakai_nav_minimized') === 'true') {
+		$PBJQ(".js-toggle-nav").click();
+		collapsed = true;
+	}
+});
+
+function getCookieVal(cookieName) {
+	var cks = document.cookie.split(';');
+	for (var i = 0; i < cks.length; ++i) {
+		var curCookie = (cks[i].substring(0,cks[i].indexOf('='))).trim();
+		if(curCookie === cookieName) {
+			return ((cks[i].split('='))[1]).trim();;
+		}
+	}
+	return undefined;
+}
