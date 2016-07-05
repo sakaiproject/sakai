7138088d7aaa423f73489518778db8d4e636324d
Charles Hedrick <hedrick@rutgers.edu>
2016-07-05 16:05:26 -0400

    LSNBLDR-673; don't show "choose all" in page chooser (#2993)

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/PagePickerProducer.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/PagePickerProducer.java
@@ -409,7 +409,8 @@ public class PagePickerProducer implements ViewComponentProducer, NavigationCase
 		    
 		    if (entry.level < 0) {
 		    	UIOutput.make(row, "heading", messageLocator.getMessage("simplepage.chooser.unused"));
-			UIOutput.make(row, "chooseall");
+			if (summaryPage)
+			    UIOutput.make(row, "chooseall");
 		    }
 		    // if no itemid, it's unused. Only canedit people will see it
 		    else if (summaryPage && entry.itemId != null) {
