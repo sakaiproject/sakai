8eae99292612c5a8f96fe9c592dfc12132092665
Steve Swinsburg <steve.swinsburg@gmail.com>
2016-07-17 19:32:04 +1000

    #3036 Add a new method that processes the assignment list if given a studentId to cater for external assignments being group scoped.Wire up the various panels to use it. (#3039)

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/InstructorGradeSummaryGradesPanel.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/InstructorGradeSummaryGradesPanel.java
@@ -1,5 +1,12 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
@@ -16,16 +23,8 @@ import org.sakaiproject.gradebookng.business.model.GbStudentGradeInfo;
 import org.sakaiproject.gradebookng.business.util.CourseGradeFormatter;
 import org.sakaiproject.gradebookng.tool.pages.GradebookPage;
 import org.sakaiproject.service.gradebook.shared.Assignment;
-import org.sakaiproject.service.gradebook.shared.CategoryDefinition;
 import org.sakaiproject.service.gradebook.shared.CourseGrade;
 import org.sakaiproject.tool.gradebook.Gradebook;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class InstructorGradeSummaryGradesPanel extends Panel {
 
@@ -49,13 +48,13 @@ public class InstructorGradeSummaryGradesPanel extends Panel {
 
 		final Gradebook gradebook = this.businessService.getGradebook();
 
-		this.setOutputMarkupId(true);
+		setOutputMarkupId(true);
 
 		final Map<String, Object> modelData = (Map<String, Object>) getDefaultModelObject();
 		final boolean groupedByCategoryByDefault = (Boolean) modelData.get("groupedByCategoryByDefault");
 
 		this.configuredCategoryType = GbCategoryType.valueOf(gradebook.getCategory_type());
-		this.isGroupedByCategory =  groupedByCategoryByDefault && this.configuredCategoryType != GbCategoryType.NO_CATEGORY;
+		this.isGroupedByCategory = groupedByCategoryByDefault && this.configuredCategoryType != GbCategoryType.NO_CATEGORY;
 		this.categoriesEnabled = this.configuredCategoryType != GbCategoryType.NO_CATEGORY;
 	}
 
@@ -67,28 +66,27 @@ public class InstructorGradeSummaryGradesPanel extends Panel {
 		final Map<String, Object> modelData = (Map<String, Object>) getDefaultModelObject();
 		final String userId = (String) modelData.get("userId");
 
-		final GradebookPage gradebookPage = (GradebookPage)getPage();
+		final GradebookPage gradebookPage = (GradebookPage) getPage();
 
 		// build the grade matrix for the user
 		final Gradebook gradebook = this.businessService.getGradebook();
-		final List<Assignment> assignments = this.businessService.getGradebookAssignments();
+		final List<Assignment> assignments = this.businessService.getGradebookAssignmentsForStudent(userId);
 
 		final CourseGradeFormatter courseGradeFormatter = new CourseGradeFormatter(
-			gradebook,
-			GbRole.INSTRUCTOR,
-			true,
-			gradebook.isCoursePointsDisplayed(),
-			true);
+				gradebook,
+				GbRole.INSTRUCTOR,
+				true,
+				gradebook.isCoursePointsDisplayed(),
+				true);
 
 		// TODO catch if this is null, the get(0) will throw an exception
 		// TODO also catch the GbException
 		final GbStudentGradeInfo studentGradeInfo = this.businessService
-			.buildGradeMatrix(
-				assignments,
-				Arrays.asList(userId),
-				gradebookPage.getUiSettings())
-			.get(0);
-		final List<CategoryDefinition> categories = this.businessService.getGradebookCategories();
+				.buildGradeMatrix(
+						assignments,
+						Arrays.asList(userId),
+						gradebookPage.getUiSettings())
+				.get(0);
 		final Map<Long, Double> categoryAverages = studentGradeInfo.getCategoryAverages();
 		final Map<Long, GbGradeInfo> grades = studentGradeInfo.getGrades();
 
@@ -110,21 +108,19 @@ public class InstructorGradeSummaryGradesPanel extends Panel {
 		}
 		Collections.sort(categoryNames);
 
-		
-
 		// build the model for table
-		Map<String, Object> tableModel = new HashMap<>();
+		final Map<String, Object> tableModel = new HashMap<>();
 		tableModel.put("grades", grades);
 		tableModel.put("categoryNamesToAssignments", categoryNamesToAssignments);
 		tableModel.put("categoryNames", categoryNames);
 		tableModel.put("categoryAverages", categoryAverages);
-		tableModel.put("categoriesEnabled", categoriesEnabled);
+		tableModel.put("categoriesEnabled", this.categoriesEnabled);
 		tableModel.put("isCategoryWeightEnabled", isCategoryWeightEnabled());
-		tableModel.put("isGroupedByCategory", isGroupedByCategory);
+		tableModel.put("isGroupedByCategory", this.isGroupedByCategory);
 		tableModel.put("showingStudentView", false);
 		tableModel.put("gradingType", GbGradingType.valueOf(gradebook.getGrade_type()));
 
-		addOrReplace(new GradeSummaryTablePanel("gradeSummaryTable", new LoadableDetachableModel<Map<String,Object>>() {
+		addOrReplace(new GradeSummaryTablePanel("gradeSummaryTable", new LoadableDetachableModel<Map<String, Object>>() {
 			@Override
 			public Map<String, Object> load() {
 				return tableModel;
