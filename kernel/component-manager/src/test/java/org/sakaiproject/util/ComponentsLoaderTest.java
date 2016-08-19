1c3da31122053735beafe4c1b2bb391ceb47ee5b
Code Hugger (Matthew Jones) <jonespm@gmail.com>
2016-08-19 13:41:43 -0700

    KNL-1452 - MBeanExporter JMX beans not being initialized (#3208)

--- kernel/component-manager/src/test/java/org/sakaiproject/util/ComponentsLoaderTest.java
+++ kernel/component-manager/src/test/java/org/sakaiproject/util/ComponentsLoaderTest.java
@@ -42,9 +42,7 @@ public class ComponentsLoaderTest {
 	@After
 	public void tearDown() throws Exception {
 		builder.tearDown();
-		NoisierDefaultListableBeanFactory.noisyClose = false;
 		componentMgr.close();
-		NoisierDefaultListableBeanFactory.noisyClose = true;
 	}
 	
 	/**
