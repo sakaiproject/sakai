0dd1d1352c4389f362ee00a9a639bfc032c0d490
Matthew Buckett <buckett@bumph.org>
2016-09-14 09:01:44 +0200

    Add support to LDAP multi-valued properties

--- providers/jldap/src/java/edu/amc/sakai/user/SimpleLdapAttributeMapper.java
+++ providers/jldap/src/java/edu/amc/sakai/user/SimpleLdapAttributeMapper.java
@@ -29,6 +29,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
@@ -366,7 +367,14 @@ public class SimpleLdapAttributeMapper implements LdapAttributeMapper {
         				"][physical attr name = " + attribute.getName() + 
         				"][value = " + attrValue + "]");
         	}
-            userData.setProperty(logicalAttrName, attrValue);
+        	// Support multivalue attributes.
+        	String[] attrValues = attribute.getStringValueArray();
+        	if (attrValues.length > 1) {
+        		List<String> newList = Arrays.asList(attrValues);
+        		userData.getProperties().put(logicalAttrName, newList);
+        	} else {
+        		userData.setProperty(logicalAttrName, attrValue);
+        	}
         }
         
     }
@@ -405,8 +413,14 @@ public class SimpleLdapAttributeMapper implements LdapAttributeMapper {
 		Properties srcProps = userData.getProperties();
 		ResourceProperties tgtProps = userEdit.getProperties();
 		for ( Entry srcProp : srcProps.entrySet() ) {
-			tgtProps.addProperty((String)srcProp.getKey(), 
-					(String)srcProp.getValue());
+			if (srcProp.getValue() instanceof String) {
+				tgtProps.addProperty((String)srcProp.getKey(), 
+						(String)srcProp.getValue());
+			} else if (srcProp.getValue() instanceof List) {
+				for(String value: (List<String>)srcProp.getValue()) {
+					tgtProps.addPropertyToList((String) srcProp.getKey(), value);
+				}
+			}
 		}
 		
 	}
