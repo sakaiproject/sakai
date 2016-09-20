b7e6ca7ba8de000cc466cc4e76e33f4f776e37ab
Miguel Pellicer Carro <mpellicer@entornosdeformacion.com>
2016-09-20 17:21:40 +0200

    SAK-31742 - NPE when creating an empty site (With no tools) - Regression from 10x (#3340)
    NPE when creating an empty site (With no tools) - Regression from 10x
     Conflicts:
    	portal/portal-util/util/src/java/org/sakaiproject/portal/util/ToolUtils.java

--- portal/portal-util/util/src/java/org/sakaiproject/portal/util/ToolUtils.java
+++ portal/portal-util/util/src/java/org/sakaiproject/portal/util/ToolUtils.java
@@ -179,10 +179,10 @@ public class ToolUtils
 	public static String getPageUrl(HttpServletRequest req, Site site, SitePage page, 
 		String portalPrefix, boolean reset, String effectiveSiteId, String pageAlias)
 	{
+		if ( page == null ) return "";
 		if ( req == null ) req = getRequestFromThreadLocal();
 		if ( effectiveSiteId == null ) effectiveSiteId = site.getId();
 		if ( pageAlias == null ) pageAlias = page.getId();
-
 		// The normal URL
 		String pageUrl = Web.returnUrl(req, "/" + portalPrefix + "/"
 				+ Web.escapeUrl(effectiveSiteId) + "/page/");
