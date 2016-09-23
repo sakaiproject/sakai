6ac6a3a02097bd21710e77b668e97ce22de861c5
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2016-09-23 13:11:26 +0200

    SAK-31404, SAK-31405 and SAK-31406 : Add the logger back in so it compiles.

--- providers/userdetail/src/java/org/sakaiproject/user/detail/ChainedDetailProvider.java
+++ providers/userdetail/src/java/org/sakaiproject/user/detail/ChainedDetailProvider.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.user.api.CandidateDetailProvider;
 import org.sakaiproject.user.api.User;
@@ -12,13 +14,15 @@ import org.sakaiproject.user.api.User;
 public class ChainedDetailProvider implements CandidateDetailProvider {
 
 	private List<CandidateDetailProvider> providers;
+	private Log M_log = LogFactory.getLog(ChainedDetailProvider.class);
+
 	public void setProviders(List<CandidateDetailProvider> providers) {
 		this.providers = providers;
 	}
 
 	public List<CandidateDetailProvider> getProviders() {
 		if(providers == null)
-			providers = new ArrayList<CandidateDetailProvider>();
+			providers = new ArrayList<>();
 		return providers;
 	}
 	public Optional<String> getCandidateID(User user, Site site) {
