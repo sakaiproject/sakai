821fd7701b6706994337370ffc87cf3659f8e42c
plukasew <plukasew@uwo.ca>
2016-09-29 11:39:08 -0400

    SAK-31736: Become User - Remind admins that 'logging out' returns them to the admin session they were in (#3294)
    * SAK-31736: Become User - Remind admins that 'logging out' returns them to the admin session they were in
    
    * SAK-31736: Become User - Remind admins that 'logging out' returns them to the admin session they were in
    
    * SAK-31736: update to use displayId
    
    * SAK-31736: switch to getFormattedMessage for better i18n
    
    * SAK-31736: fixed duplicate null check

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -50,6 +50,8 @@ import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.component.cover.ServerConfigurationService;
 import org.sakaiproject.entity.api.ResourceProperties;
 import org.sakaiproject.entity.api.ResourcePropertiesEdit;
+import org.sakaiproject.event.api.UsageSession;
+import org.sakaiproject.event.cover.UsageSessionService;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.IdUsedException;
 import org.sakaiproject.exception.PermissionException;
@@ -1870,11 +1872,19 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 					loginUserDispName = Validator.escapeHtml(thisUser.getDisplayName());
 					loginUserFirstName = Validator.escapeHtml(thisUser.getFirstName());
 				}
+				
+				// check if current user is being impersonated (by become user/sutool)
+				String impersonatorDisplayId = getImpersonatorDisplayId();
+				if (!impersonatorDisplayId.isEmpty())
+				{
+					message = rloader.getFormattedMessage("sit_return", impersonatorDisplayId);
+				}
 
 				// check for a logout text override
-				message = StringUtils.trimToNull(ServerConfigurationService
-						.getString("logout.text"));
-				if (message == null) message = rloader.getString("sit_log");
+				if (message == null)
+				{
+					message = ServerConfigurationService.getString("logout.text", rloader.getString("sit_log"));
+				}
 
 				// check for an image for the logout
 				image1 = StringUtils.trimToNull(ServerConfigurationService
@@ -2340,6 +2350,36 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 		}
 		
 		return rval;
+	}
+	
+	/**
+	 * Checks if current user is being impersonated (via become user/sutool) and returns displayId of
+	 * the impersonator. Adapted from SkinnableLogin's isImpersonating()
+	 * @return displayId of impersonator, or empty string if not being impersonated
+	 */
+	private String getImpersonatorDisplayId()
+	{
+		Session currentSession = SessionManager.getCurrentSession();
+		UsageSession originalSession = (UsageSession) currentSession.getAttribute(UsageSessionService.USAGE_SESSION_KEY);
+		
+		if (originalSession != null)
+		{
+			String originalUserId = originalSession.getUserId();
+			if (!StringUtils.equals(currentSession.getUserId(), originalUserId))
+			{
+				try
+				{
+					User originalUser = UserDirectoryService.getUser(originalUserId);
+					return originalUser.getDisplayId();
+				}
+				catch (UserNotDefinedException e)
+				{
+					M_log.debug("Unable to retrieve user for id: " + originalUserId);
+				}
+			}
+		}
+		
+		return "";
 	}
 
 }
