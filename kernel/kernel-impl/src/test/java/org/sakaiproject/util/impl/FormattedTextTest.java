ccf0c745a86611fd5a993e7b8af18b7546ee74e2
Matthew Jones <jonespm@gmail.com>
2016-10-03 20:56:14 -0400

    KNL-1464 - Clean up redundant form tags

--- kernel/kernel-impl/src/test/java/org/sakaiproject/util/impl/FormattedTextTest.java
+++ kernel/kernel-impl/src/test/java/org/sakaiproject/util/impl/FormattedTextTest.java
@@ -996,6 +996,37 @@ public class FormattedTextTest {
     }
 
     @Test
+    public void testKNL_1464() {
+        // https://jira.sakaiproject.org/browse/KNL-1464
+        String text = null;
+        String result = null;
+        StringBuilder errorMessages = new StringBuilder();
+
+        //These are all expected to be an empty string as these tags are removed
+        text = "<form>First name:<br><input type='text' name='firstname'><br>Last name:<br> <input type='text' name='lastname'></form>";
+        result = formattedText.processFormattedText(text,errorMessages);
+        Assert.assertTrue( errorMessages.length() > 1 );
+        Assert.assertEquals(result, "");
+
+        text = "<input type='text' name='firstname'>";
+        result = formattedText.processFormattedText(text,errorMessages);
+        Assert.assertTrue( errorMessages.length() > 1 );
+        Assert.assertEquals(result, "");
+
+        text = "<textarea rows='4' cols='50'>textarea</textarea>";
+        result = formattedText.processFormattedText(text,errorMessages);
+        Assert.assertTrue( errorMessages.length() > 1 );
+        Assert.assertEquals(result, "");
+
+        text = "<select><option value='sakai'>Sakai</option></select>";
+        result = formattedText.processFormattedText(text,errorMessages);
+        Assert.assertTrue( errorMessages.length() > 1 );
+        Assert.assertEquals(result, "");
+
+    }
+
+
+    @Test
     public void testGetShortenedTitles() {
         for (String siteTitle:SITE_TITLES) {
             for (int k=0; k<CUT_METHODS.length; k++) {
