029f7c9d4db588f190372870f4400f73febcc9ca
Earle Nietzel <earle@longsight.com>
2016-10-10 09:52:06 -0400

    SAK-31840 GradableObject to use Objects in POJO vs primitives (#3452)

--- reference/docs/conversion/sakai_12_mysql_conversion.sql
+++ reference/docs/conversion/sakai_12_mysql_conversion.sql
@@ -203,3 +203,8 @@ ALTER TABLE pasystem_popup_assign DROP COLUMN user_eid;
 ALTER TABLE pasystem_popup_dismissed DROP COLUMN user_eid;
 ALTER TABLE pasystem_banner_dismissed DROP COLUMN user_eid;
 
+--
+-- SAK-31840 drop defaults as its now managed in the POJO
+--
+alter table GB_GRADABLE_OBJECT_T alter column IS_EXTRA_CREDIT drop default;
+alter table GB_GRADABLE_OBJECT_T alter column HIDE_IN_ALL_GRADES_TABLE drop default;
