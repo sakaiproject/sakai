591b3aa8de3673f20746cdf360adaab8a84388d6
Leonardo Canessa <lcanessa1@udayton.edu>
2016-11-10 16:31:59 -0500

    SAK-31944 Fix Calendar tool Month display name
    Previously in the Calendar tool, the month name would be displayed
    incorrectly when it was the 31 day of a month. This was resolved
    by using the new Java 8 YearMonth object.
    
    Minor cleanup in CalendarUtil.java

--- calendar/calendar-util/util/src/java/org/sakaiproject/util/CalendarUtil.java
+++ calendar/calendar-util/util/src/java/org/sakaiproject/util/CalendarUtil.java
@@ -23,6 +23,10 @@ package org.sakaiproject.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -115,46 +119,40 @@ public class CalendarUtil
 	  dateFriday = calendarFriday.getTime();
 	  dateSaturday = calendarSaturday.getTime();
 
-	  Calendar calendarJanuary = Calendar.getInstance();
-	  Calendar calendarFebruary = Calendar.getInstance();
-	  Calendar calendarMarch = Calendar.getInstance();
-	  Calendar calendarApril = Calendar.getInstance();
-	  Calendar calendarMay = Calendar.getInstance();
-	  Calendar calendarJune = Calendar.getInstance();
-	  Calendar calendarJuly = Calendar.getInstance();
-	  Calendar calendarAugust = Calendar.getInstance();
-	  Calendar calendarSeptember = Calendar.getInstance();
-	  Calendar calendarOctober = Calendar.getInstance();
-	  Calendar calendarNovember = Calendar.getInstance();
-	  Calendar calendarDecember = Calendar.getInstance();
+	  // Previously Calendar was used, but it had problems getting the month right
+	  // when the current day of the month was 31.
+	  YearMonth currentYearMonth = YearMonth.now();
+	  YearMonth jan = currentYearMonth.with(Month.JANUARY);
+	  YearMonth feb = currentYearMonth.with(Month.FEBRUARY);
+	  YearMonth mar = currentYearMonth.with(Month.MARCH);
+	  YearMonth apr = currentYearMonth.with(Month.APRIL);
+	  YearMonth may = currentYearMonth.with(Month.MAY);
+	  YearMonth jun = currentYearMonth.with(Month.JUNE);
+	  YearMonth jul = currentYearMonth.with(Month.JULY);
+	  YearMonth aug = currentYearMonth.with(Month.AUGUST);
+	  YearMonth sep = currentYearMonth.with(Month.SEPTEMBER);
+	  YearMonth oct = currentYearMonth.with(Month.OCTOBER);
+	  YearMonth nov = currentYearMonth.with(Month.NOVEMBER);
+	  YearMonth dec = currentYearMonth.with(Month.DECEMBER);
 
-	  calendarJanuary.set(Calendar.MONTH, Calendar.JANUARY); 
-	  calendarFebruary.set(Calendar.MONTH, Calendar.FEBRUARY); 
-	  calendarMarch.set(Calendar.MONTH, Calendar.MARCH); 
-	  calendarApril.set(Calendar.MONTH, Calendar.APRIL); 
-	  calendarMay.set(Calendar.MONTH, Calendar.MAY); 
-	  calendarJune.set(Calendar.MONTH, Calendar.JUNE); 
-	  calendarJuly.set(Calendar.MONTH, Calendar.JULY); 
-	  calendarAugust.set(Calendar.MONTH, Calendar.AUGUST); 
-	  calendarSeptember.set(Calendar.MONTH, Calendar.SEPTEMBER); 
-	  calendarOctober.set(Calendar.MONTH, Calendar.OCTOBER); 
-	  calendarNovember.set(Calendar.MONTH, Calendar.NOVEMBER); 
-	  calendarDecember.set(Calendar.MONTH, Calendar.DECEMBER); 
-
-	  dateJanuary = calendarJanuary.getTime();
-	  dateFebruary = calendarFebruary.getTime();
-	  dateMarch = calendarMarch.getTime();
-	  dateApril = calendarApril.getTime();
-	  dateMay = calendarMay.getTime();
-	  dateJune = calendarJune.getTime();
-	  dateJuly = calendarJuly.getTime();
-	  dateAugust = calendarAugust.getTime();
-	  dateSeptember = calendarSeptember.getTime();
-	  dateOctober = calendarOctober.getTime();
-	  dateNovember = calendarNovember.getTime();
-	  dateDecember = calendarDecember.getTime();
+	  dateJanuary = getDateFromYearMonth(jan);
+	  dateFebruary = getDateFromYearMonth(feb);
+	  dateMarch = getDateFromYearMonth(mar);
+	  dateApril = getDateFromYearMonth(apr);
+	  dateMay = getDateFromYearMonth(may);
+	  dateJune = getDateFromYearMonth(jun);
+	  dateJuly = getDateFromYearMonth(jul);
+	  dateAugust = getDateFromYearMonth(aug);
+	  dateSeptember = getDateFromYearMonth(sep);
+	  dateOctober = getDateFromYearMonth(oct);
+	  dateNovember = getDateFromYearMonth(nov);
+	  dateDecember = getDateFromYearMonth(dec);
 
 	}
+
+	private Date getDateFromYearMonth(YearMonth ym) {
+        return Date.from(ym.atDay(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
 
 	/**
 	* Access the current user.
@@ -219,8 +217,7 @@ public class CalendarUtil
 	}	// getNextMonth
 
 	/**
-	* Set the calendar to the next year, and return this.
-	* @return the next year.
+	* Set the calendar to the next year
 	*/
 	public void setNextYear()
 	{
@@ -242,8 +239,7 @@ public class CalendarUtil
 	}	// getPrevMonth	
 
 	/**
-	* Set the calendar to the prev year, and return this.
-	* @return the prev year.
+	* Set the calendar to the prev year.
 	*/
 	public void setPrevYear()
 	{
@@ -274,8 +270,7 @@ public class CalendarUtil
 	}	// getDay_Of_Week
 
 	/**
-	* Set the calendar to the next week, and return this.
-	* @return the next week.
+	* Set the calendar to the next week
 	*/
 	public void setNextWeek()
 	{
@@ -284,8 +279,7 @@ public class CalendarUtil
 	}	// setNextWeek
 
 	/**
-	* Set the calendar to the prev week, and return this.
-	* @return the prev week.
+	* Set the calendar to the prev week
 	*/
 	public void setPrevWeek()
 	{
@@ -332,8 +326,7 @@ public class CalendarUtil
 	*/
 	public int getDayOfMonth() 
 	{
-		int dayofmonth = m_calendar.get(Calendar.DAY_OF_MONTH);
-		return dayofmonth;
+		return m_calendar.get(Calendar.DAY_OF_MONTH);
 
 	}	// getDayOfMonth
 	
@@ -410,7 +403,7 @@ public class CalendarUtil
 	public String[] getCalendarMonthNames(boolean longNames) {
 
 		Locale currentLocale = rb.getLocale();
-		String[] months = null; 
+		String[] months;
 		
 		if (longNames) {
 
@@ -492,7 +485,7 @@ public class CalendarUtil
 		SimpleDateFormat longDay = new SimpleDateFormat("EEEE", currentLocale);
 		SimpleDateFormat shortDay = new SimpleDateFormat("EEE", currentLocale);
 		
-		String[] weekDays = null; 
+		String[] weekDays;
 		String[] longWeekDays = new String[] 
 		{
 			longDay.format(dateSunday),
