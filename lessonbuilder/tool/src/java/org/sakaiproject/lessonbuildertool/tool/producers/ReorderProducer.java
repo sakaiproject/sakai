bbd846cb9c56f1203abaa3312a2d1fcf22438d46
Charles Hedrick <hedrick@rutgers.edu>
2016-11-28 14:24:44 -0500

    LSNBLDR-749; On reorder page, the forums widget doesnt have a name

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ReorderProducer.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ReorderProducer.java
@@ -187,6 +187,10 @@ public class ReorderProducer implements ViewComponentProducer, NavigationCaseRep
 				    if (text.length() > 100)
 					text = text.substring(0,100);
 				    UIOutput.make(row, "text-snippet", text);
+				} else if (SimplePageItem.ANNOUNCEMENTS == i.getType()) {
+					UIOutput.make(row, "text-snippet", messageLocator.getMessage("simplepage.announcements-snippet"));
+				} else if (SimplePageItem.FORUM_SUMMARY == i.getType()) {
+					UIOutput.make(row, "text-snippet", messageLocator.getMessage("simplepage.forums-snippet"));
 				} else if ("1".equals(subtype)) {
 				    // embed code, nothing useful to show
 				    UIOutput.make(row, "text-snippet", messageLocator.getMessage("simplepage.embedded-video"));
