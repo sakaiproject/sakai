fb54ddb27d0a3251aa0980a915eaf9494e283466
John Ansorge <ansorgej@ohsu.edu>
2016-12-01 13:17:13 -0800

    Fixing clumsy coding
    Also simplifying logic.  It makes no sense to set haswronganswer back
    to false under any circumstance if it had been set to true.

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -1352,12 +1352,9 @@ public class DeliveryActionListener
     		   
     	}
     	if ( (item.getTypeId().equals(TypeIfc.EXTENDED_MATCHING_ITEMS)) || (item.getTypeId().equals(TypeIfc.MULTIPLE_CORRECT) )|| (item.getTypeId().equals(TypeIfc.MATCHING) )){
-    		if (haswronganswer = false && mcmc_match_counter==correctAnswers){
-    			haswronganswer=false;
-    		}
-    		else {
-    			haswronganswer=true;
-    		}
+    		if (mcmc_match_counter != correctAnswers){
+				haswronganswer=true;
+			}
     	}
     	
     	if (haswronganswer) {
