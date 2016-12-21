ad784f85908e9bc056472162f6a12a4810291e85
Matthew Jones <jonespm@gmail.com>
2016-12-21 18:50:04 -0500

    Revert "SAK-25289 - Breadcrumbs trail incorrect when accessing forum topics (#3594)"
    This reverts commit d4dccbb6438a460391ba76f61a822912a62fa7ed.

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -7218,6 +7218,10 @@ public class DiscussionForumTool
    */
   private void setSelectedForumForCurrentTopic(DiscussionTopic topic)
   {
+    if (selectedForum != null)
+    {
+      return;
+    }
     DiscussionForum forum = (DiscussionForum) topic.getBaseForum();
     if (forum == null)
     {
