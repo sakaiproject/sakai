7dfece5e7a52d9fd9b032305eff71c6fba6b0d81
Sam Ottenhoff <ottenhoff@longsight.com>
2017-01-25 18:35:45 +0000

    Modify tests to use hyphens and colons

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/ImportGradesHelper.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/ImportGradesHelper.java
@@ -52,9 +52,9 @@ public class ImportGradesHelper {
 	public final static int USER_NAME_POS = 1;
 
 	// patterns for detecting column headers and their types
-	final static Pattern ASSIGNMENT_WITH_POINTS_PATTERN = Pattern.compile("(.* \\[[0-9]+(\\.[0-9][0-9]?)?\\])");
+	final static Pattern ASSIGNMENT_WITH_POINTS_PATTERN = Pattern.compile("([\\w\\s-:]+ \\[[0-9]+(\\.[0-9][0-9]?)?\\])");
 	final static Pattern ASSIGNMENT_COMMENT_PATTERN = Pattern.compile("(\\* .*)");
-	final static Pattern STANDARD_HEADER_PATTERN = Pattern.compile("(.* +)");
+	final static Pattern STANDARD_HEADER_PATTERN = Pattern.compile("([\\w\\s-:]+)");
 	final static Pattern POINTS_PATTERN = Pattern.compile("(\\d+)(?=]$)");
 	final static Pattern IGNORE_PATTERN = Pattern.compile("(\\#.+)");
 	final static Pattern COURSE_GRADE_OVERRIDE_PATTERN = Pattern.compile("(\\$.+)");
