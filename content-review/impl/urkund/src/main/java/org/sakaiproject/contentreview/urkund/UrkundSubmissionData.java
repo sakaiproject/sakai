f7bd83c4e7653be7e951d4f3e5581e962d8e52dd
frasese <francisco.saez.segura@gmail.com>
2017-02-03 08:21:13 +0100

    Commentary about Capitalized variables in UrkundSubmissionData

--- content-review/impl/urkund/src/main/java/org/sakaiproject/contentreview/urkund/UrkundSubmissionData.java
+++ content-review/impl/urkund/src/main/java/org/sakaiproject/contentreview/urkund/UrkundSubmissionData.java
@@ -10,6 +10,44 @@ import lombok.Setter;
 @Setter
 public class UrkundSubmissionData {
 
+	/* Capitalized variables because the returned JSON response by Urkund and Jackson deserialization.
+	 * 
+	 * EXAMPLE JSON returned by Urkund :
+	 * 
+	 [
+	  {
+	    "Id": 00000000,
+	    "ExternalId": "EXT-001",
+	    "Filename": "test.pdf",
+	    "MimeType": "application/pdf",
+	    "Timestamp": "2017-01-01T00:00:00",
+	    "Status": {
+	      "Message": "The document has been analyzed.",
+	      "State": "Analyzed"
+	    },
+	    "Document": {
+	      "Date": "2017-01-01T00:00:00",
+	      "DownloadUrl": "https://secure.urkund.com/archive/download/?c1=0000",
+	      "OptOutInfo": {
+	        "Message": "BIG MESSAGE",
+	        "Url": "https://secure.urkund.com/account/document/exemptionstatus/0000"
+	      },
+	      "Id": 00000000
+	    },
+	    "Report": {
+	      "MatchCount": 128,
+	      "ReportUrl": "https://secure.urkund.com/view/0000",
+	      "Significance": 99.08,
+	      "SourceCount": 74,
+	      "Warnings": [],
+	      "Id": 00000000
+	    },
+	    "Subject": null,
+	    "Message": null,
+	    "Anonymous": false
+	  }
+	]
+	 * */
 	public Integer Id;
 	public Date Timestamp;
 	public String ExternalId;
