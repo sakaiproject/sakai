fa6dbea9c52df2459089894eeac5804c171ca1e1
Brad Szabo <bszabo@unicon.net>
2017-02-07 21:20:44 -0700

    SAK-32151 Sakai Rubrics initial support. This includes a Rubrics Service API and Web Components, as well as a Sakai Rubrics Tool for managing Rubrics.
    This commit includes the basis for a very minor 3rd party Polymer library
    modification. The file is contained in:
    rubrics/services/src/main/webapp/lib/polymer-sortablejs/polymer-sortablejs.html
    This is the community version that is pulled down as part of the build.
    A subsequent commit will apply changes to fix an issue with that
    library.

---
+++ rubrics/services/src/main/java/org/sakaiproject/rubrics/security/exception/JwtTokenMalformedException.java
@@ -0,0 +1,37 @@
+/**********************************************************************************
+ *
+ * Copyright (c) 2017 The Sakai Foundation
+ *
+ * Original developers:
+ *
+ *   Unicon based on code created by pascal alma
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.rubrics.security.exception;
+
+import org.springframework.security.core.AuthenticationException;
+/**
+ * Thrown when token cannot be parsed
+ */
+public class JwtTokenMalformedException extends AuthenticationException {
+    public JwtTokenMalformedException(String msg) {
+        super(msg);
+    }
+
+    public JwtTokenMalformedException(String msg, Throwable throwable) {
+        super(msg, throwable);
+    }
+}
