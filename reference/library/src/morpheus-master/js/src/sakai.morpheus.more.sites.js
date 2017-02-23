da48b7ecce4bf12d595e92527c905c21aa05e501
Payten Giles <payten.giles@gmail.com>
2017-02-23 21:16:38 +1100

    SAK-31799 make favorites clickable on mobile (#3943)
    * SAK-31799 Allow favorite site links to be clickable on mobile devices
    - Now that links are clickable, make the drag handle icon the actual drag handle for dragging
    - Also reinstate the drag icon on mobile devices
    
    * SAK-31799 Fix favorites dragging not working on some pages by ensuring that touch-punch is included for touch devices

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.more.sites.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.more.sites.js
@@ -522,17 +522,13 @@ $PBJQ(document).ready(function($){
         var favoriteItem = itemsBySiteId[siteid].clone(false);
 
         favoriteItem.addClass('organize-favorite-item').data('site-id', siteid);
-        var dragHandle = $PBJQ('<i class="fa fa-bars fav-drag-handle"></i>');
+        var dragHandle = $PBJQ('<a href="javascript:void(0);" class="fav-drag-handle"><i class="fa fa-bars"></i></a>');
 
         // Hide the tool dropdown
         $PBJQ('.toolMenus', favoriteItem).remove();
 
         // Show a drag handle
         favoriteItem.append(dragHandle);
-
-        // And disable the link to site so we don't accidentally hit it while
-        // dragging
-        $PBJQ(favoriteItem).find('.fav-title a').attr('href', null);
 
         list.append(favoriteItem);
 
@@ -541,6 +537,7 @@ $PBJQ(document).ready(function($){
       });
 
       list.sortable({
+        handle: ".fav-drag-handle",
         stop: function () {
           // Update our ordering based on the new selection
           favoritesList = list.find('.organize-favorite-item').map(function () {
