35d04060f799ffc62651b28872d22aebf8d4fa61
Adrian Fish <adrian.r.fish@gmail.com>
2017-03-03 21:24:35 +0000

    SAK-32246 Added lessons comments alerts to bullhorns

--- library/src/morpheus-master/js/src/sakai.morpheus.bullhorns.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.bullhorns.js
@@ -147,6 +147,8 @@
                                     if (alert.event === 'asn.new.assignment'
                                                                 || alert.event === 'asn.grade.submission') {
                                         faClass = 'fa-file-text';
+                                    } else if (alert.event === 'lessonbuilder.comment.create') {
+                                        faClass = 'fa-leanpub';
                                     }
 
                                     markup += '<a href="' + alert.url + '" style="text-decoration: none;"><div id="portal-bullhorn-alert-' + alert.id + '" class="portal-bullhorn-alert">'
@@ -166,6 +168,8 @@
                                             = data.i18n.assignmentSubmissionGraded.replace('{0}', title).replace('{1}', siteTitle);
                                     } else if (alert.event === 'commons.comment.created') {
                                         markup += data.i18n.academicCommentCreated.replace('{0}', siteTitle);
+                                    } else if (alert.event === 'lessonbuilder.comment.create') {
+                                        markup += data.i18n.academicLessonBuilderCommentCreate.replace('{0}', siteTitle);
                                     } else {
                                         markup += data.i18n.unrecognisedAlert;
                                     }
