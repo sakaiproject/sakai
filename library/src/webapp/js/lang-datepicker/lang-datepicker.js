a4bb573fe5b8f74aa301e0b23622052b76c1b84e
Miguel Pellicer Carro <mpellicer@entornosdeformacion.com>
2017-03-29 12:13:10 +0200

    SAK-32416 - Replace jQuery datepicker icon to use font-awesome

--- library/src/webapp/js/lang-datepicker/lang-datepicker.js
+++ library/src/webapp/js/lang-datepicker/lang-datepicker.js
@@ -2226,8 +2226,11 @@ Za.fn=Ia.prototype;var Xd=bb(1,"add"),Yd=bb(-1,"subtract");a.defaultFormat="YYYY
 		var localDTPicker;
 
 		cfg.showOn = (options.icon === 0) ? "focus" : "both";
-		cfg.buttonImage = (options.icon === 0) ? null : "/library/image/silk/calendar.png";
-		cfg.buttonImageOnly = true;
+		//Use an image instead of font-awesome
+		//cfg.buttonImage = (options.icon === 0) ? null : "/library/image/silk/calendar.png";
+		//cfg.buttonImageOnly = true;
+		//Use font-awesome instead of an image
+		cfg.buttonText = (options.icon === 0) ? null : "<i class='fa fa-calendar'></i>";
 
 		cfg.stepMinute = options.stepMinute;
 		cfg.altField = options.altField;
