71552626c89a3ae83cb4d6964332504e1cfb17bf
David Hutchins <dnh8f@virginia.edu>
2017-04-10 22:36:09 +0000

    SAM-3170 Changed constant name

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/queue/delivery/TimedAssessmentRunnable.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/queue/delivery/TimedAssessmentRunnable.java
@@ -150,7 +150,7 @@ public class TimedAssessmentRunnable implements Runnable {
 
             notiValues.put( "confirmationNumber", confirmationNumber );
 
-            EventTrackingService.post(EventTrackingService.newEvent(SamigoConstants.EVENT_ASSESSMENT_TIMED_SUBMITTED,
+            EventTrackingService.post(EventTrackingService.newEvent(SamigoConstants.EVENT_ASSESSMENT_SUBMITTED_TIMED,
                notiValues.toString(),
                siteId,
                true,
