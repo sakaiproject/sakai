459977b7841b62baebbea881eacc5e330edb6945
Nick Wilson <nick.wilson@it.ox.ac.uk>
2017-04-11 16:14:21 +0100

    SAK-32279: Common stack trace from signup tool (#4047)
    SAK-32279: Renaming method

--- signup/tool/src/java/org/sakaiproject/signup/tool/jsf/SignupUIBaseBean.java
+++ signup/tool/src/java/org/sakaiproject/signup/tool/jsf/SignupUIBaseBean.java
@@ -703,6 +703,14 @@ abstract public class SignupUIBaseBean implements SignupBeanConstants, SignupMes
 		return uuids;
 	}
 
+	/**
+	 * Determines if user has a valid session
+	 * @return if user has a valid session
+	 */
+	public boolean isSessionValid() {
+		return getMeetingWrapper() == null;
+	}
+
 	
 	/**
 	 * Helper to get a formatted string of all attendee email addresses for all tineslots
@@ -714,11 +722,13 @@ abstract public class SignupUIBaseBean implements SignupBeanConstants, SignupMes
 		Set<String> emails = new HashSet<String>();
 		
 		StringBuilder sb = new StringBuilder();
-		for (TimeslotWrapper tsWrapper : timeslotWrappers) {
-			for(AttendeeWrapper atWrapper : tsWrapper.getAttendeeWrappers()) {
-				String email = atWrapper.getAttendeeEmail();
-				if(StringUtils.isNotBlank(email)){
-					emails.add(email);
+		if (timeslotWrappers!=null){
+			for (TimeslotWrapper tsWrapper : timeslotWrappers) {
+				for(AttendeeWrapper atWrapper : tsWrapper.getAttendeeWrappers()) {
+					String email = atWrapper.getAttendeeEmail();
+					if(StringUtils.isNotBlank(email)){
+						emails.add(email);
+					}
 				}
 			}
 		}
