08356c810e61f559a70c7d8e0c64d9bc9698673a
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2017-04-18 15:59:31 +0100

    SAK-32453 Switch to a loop to build the OR.
    This just drops out the older class.

--- tags/tags-impl/impl/src/test/org/sakaiproject/tags/impl/storage/BaseStorageTest.java
+++ tags/tags-impl/impl/src/test/org/sakaiproject/tags/impl/storage/BaseStorageTest.java
@@ -30,6 +30,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.internal.hamcrest.HamcrestArgumentMatcher;
 
+import org.mockito.internal.matchers.Any;
+import org.mockito.internal.matchers.Not;
+import org.mockito.internal.matchers.Or;
 import org.sakaiproject.event.api.EventTrackingService;
 import org.sakaiproject.tags.api.MissingUuidException;
 import org.sakaiproject.tags.api.Tag;
@@ -41,19 +44,15 @@ import org.sakaiproject.tool.api.SessionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
-import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.hasProperty;
 import static org.mockito.Mockito.argThat;
 import static org.mockito.Mockito.when;
-import static org.mockito.internal.matchers.Null.NULL;
 
 public abstract class BaseStorageTest {
 
@@ -262,7 +261,10 @@ public abstract class BaseStorageTest {
     }
 
     protected <T> T argThatMatchesAnyOf(ArgumentMatcher<?>... matchers) {
-        final Or or = new Or(Lists.newArrayList(matchers));
+        ArgumentMatcher or = new Not(Any.ANY);
+        for (ArgumentMatcher<?> matcher : matchers) {
+            or = new Or(matcher, or);
+        }
         return (T) argThat(or);
     }
 
@@ -270,35 +272,4 @@ public abstract class BaseStorageTest {
         return new Date().getTime();
     }
     
-    //This was changed in https://github.com/mockito/mockito/pull/833, using old implementation
-    @SuppressWarnings("unchecked")
-    public class Or implements ArgumentMatcher, Serializable {
-
-        private final List<ArgumentMatcher> matchers;
-
-        public Or(List<ArgumentMatcher> matchers) {
-            this.matchers = matchers;
-        }
-
-        public boolean matches(Object actual) {
-            for (ArgumentMatcher matcher : matchers) {
-                if (matcher.matches(actual)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public String toString() {
-            //TODO SF here and in other places we should reuse ValuePrinter
-            StringBuilder sb = new StringBuilder("or(");
-            for (Iterator<ArgumentMatcher> it = matchers.iterator(); it.hasNext();) {
-                sb.append(it.next().toString());
-                if (it.hasNext()) {
-                    sb.append(", ");
-                }
-            }
-            return sb.append(")").toString();
-        }
-    }
 }
