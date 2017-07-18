74bbfe9eb0af8d740c64528639224b5f7422bc74
Sam Ottenhoff <ottenhoff@longsight.com>
2017-07-18 18:32:40 -0400

    SAK-32708 HTML lang should be en-US not en_US (#4619)

--- chat/chat-impl/impl/src/java/org/sakaiproject/chat2/model/impl/ChatEntityProducer.java
+++ chat/chat-impl/impl/src/java/org/sakaiproject/chat2/model/impl/ChatEntityProducer.java
@@ -407,8 +407,8 @@ public class ChatEntityProducer implements EntityProducer, EntityTransferrer {
                String body = Web.escapeHtml(message.getBody());
 
                sw
-                     .write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
-                           + "<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">\n"
+                     .write("<!DOCTYPE html>\n"
+                           + "<html>\n"
                            + "<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n"
                            + "<link href=\"");
                sw.write(skinRepo);
