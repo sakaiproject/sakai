8d7f6a9dec8d1e31369056fe84fb6d5f5ec7f677
Charles Severance <csev@umich.edu>
2017-07-27 10:12:52 -0400

    SAK-32725 - Listen for LTI tool page refresh requests (#4641)

--- basiclti/basiclti-portlet/src/java/org/sakaiproject/portlets/IMSBLTIPortlet.java
+++ basiclti/basiclti-portlet/src/java/org/sakaiproject/portlets/IMSBLTIPortlet.java
@@ -282,11 +282,14 @@ public class IMSBLTIPortlet extends GenericPortlet {
 					text.append("    if ( message.subject == 'lti.frameResize' ) {\n");
 					text.append("      var height = message.height;\n");
 					text.append("      document.getElementById(idval).height = height;\n");
-					text.append("      console.log('Reveived lti.frameResize height='+height);\n");
+					text.append("      window.console && console.log('Received lti.frameResize height='+height);\n");
+					text.append("    }\n");
+					text.append("    else if ( message.subject == 'lti.pageRefresh' ) {\n");
+					text.append("      location.reload(true);\n");
 					text.append("    }\n");
 					text.append("  } catch (error) {\n");
-					text.append("   console.log('lti.frameResize of '+idval+' failed height='+height);\n");
-					text.append("   console.log(e.data);\n");
+					text.append("   window.console && console.log('lti.frameResize of '+idval+' failed height='+height);\n");
+					text.append("   window.console && console.log(e.data);\n");
 					text.append("  }\n");
 					text.append("});\n");
 					text.append("</script>\n");
