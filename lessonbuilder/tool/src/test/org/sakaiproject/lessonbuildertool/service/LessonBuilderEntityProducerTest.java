877894a7bd6750a0635f8d2615f98b431e2895d8
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2017-08-07 11:55:46 +0100

    LSNBLDR-843 Fix for stack trace parsing bad URL.
    When accessing a URL without an id rather than generating a stack trace
    lessonbuilder would throw a 500 error.

---
+++ lessonbuilder/tool/src/test/org/sakaiproject/lessonbuildertool/service/LessonBuilderEntityProducerTest.java
@@ -0,0 +1,96 @@
+package org.sakaiproject.lessonbuildertool.service;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.sakaiproject.entity.api.Reference;
+import org.sakaiproject.lessonbuildertool.SimplePage;
+import org.sakaiproject.lessonbuildertool.SimplePageItem;
+import org.sakaiproject.lessonbuildertool.model.SimplePageToolDao;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LessonBuilderEntityProducerTest {
+
+    private LessonBuilderEntityProducer producer;
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock
+    private SimplePageToolDao dao;
+
+    @Mock
+    private Reference ref;
+
+    @Before
+    public void setUp() {
+        producer = new LessonBuilderEntityProducer();
+        producer.setSimplePageToolDao(dao);
+    }
+
+    @Test
+    public void testParseEntityReferenceNotOurs() {
+        assertFalse(producer.parseEntityReference("/else", ref));
+        assertFalse(producer.parseEntityReference("/", ref));
+        assertFalse(producer.parseEntityReference("", ref));
+    }
+
+    @Test
+    public void testParseEntityReferenceNoItem() {
+        assertFalse(producer.parseEntityReference("/lessonbuilder/item", ref));
+    }
+
+    @Test
+    public void testParseEntityReferenceNoSite() {
+        assertFalse(producer.parseEntityReference("/lessonbuilder/site", ref));
+    }
+
+    @Test
+    public void testParseEntityReferenceNoPage() {
+        assertFalse(producer.parseEntityReference("/lessonbuilder/page", ref));
+    }
+
+    @Test
+    public void testParseEntityReferencePageNotInt() {
+        assertFalse(producer.parseEntityReference("/lessonbuilder/page/notNumber", ref));
+    }
+
+    @Test
+    public void testParseEntityReferencePageMissingId() {
+        assertFalse(producer.parseEntityReference("/lessonbuilder/page/10", ref));
+    }
+
+    @Test
+    public void testParseEntityReferencePageWithId() {
+        SimplePage page = Mockito.mock(SimplePage.class);
+        Mockito.when(page.getSiteId()).thenReturn("siteId");
+        Mockito.when(dao.getPage(10)).thenReturn(page);
+        assertTrue(producer.parseEntityReference("/lessonbuilder/page/10", ref));
+        Mockito.verify(ref).set("sakai:lessonbuilder", "page", "/page/10", null, "siteId");
+    }
+
+    @Test
+    public void testParseEntityReferenceItemWithId() {
+        SimplePageItem item = Mockito.mock(SimplePageItem.class);
+        Mockito.when(dao.findItem(10)).thenReturn(item);
+        Mockito.when(item.getPageId()).thenReturn(11L);
+        SimplePage page = Mockito.mock(SimplePage.class);
+        Mockito.when(page.getSiteId()).thenReturn("siteId");
+        Mockito.when(dao.getPage(11)).thenReturn(page);
+        assertTrue(producer.parseEntityReference("/lessonbuilder/item/10", ref));
+        Mockito.verify(ref).set("sakai:lessonbuilder", "item", "/item/10", null, "siteId");
+    }
+
+    @Test
+    public void testParseEntityReferenceSite() {
+        assertTrue(producer.parseEntityReference("/lessonbuilder/site/siteId", ref));
+        Mockito.verify(ref).set("sakai:lessonbuilder", "site", "/site/siteId", null, "/site/siteId");
+    }
+
+}
