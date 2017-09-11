5b243f7de34742af86f5370499199f85578157a9
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2017-09-11 15:24:32 +0200

    SAK-33330 - Signup new meeting validation error resets the dates (#4785)
    Signup new meeting validation error resets the dates

--- signup/tool/src/java/org/sakaiproject/signup/tool/jsf/organizer/CopyMeetingSignupMBean.java
+++ signup/tool/src/java/org/sakaiproject/signup/tool/jsf/organizer/CopyMeetingSignupMBean.java
@@ -79,6 +79,9 @@ public class CopyMeetingSignupMBean extends SignupUIBaseBean {
 	/* singup deadline before this minutes/hours/days */
 	private int deadlineTime;
 	
+	//Meeting title
+	private String title;
+	
 	//Location selected from the dropdown
 	private String selectedLocation;
 	
@@ -363,6 +366,16 @@ public class CopyMeetingSignupMBean extends SignupUIBaseBean {
 		Date eventEndTime = signupMeeting.getEndTime();
 		Date eventStartTime = signupMeeting.getStartTime();
 		
+		//Set Title		
+		if (StringUtils.isNotBlank(title)){
+			logger.debug("title set: " + title);
+			this.signupMeeting.setTitle(title);
+		}else{
+			validationError = true;
+			Utilities.addErrorMessage(Utilities.rb.getString("event.title_cannot_be_blank"));
+			return;
+		}
+
 		/*user defined own TS case*/
 		if(isUserDefinedTS()){
 			eventEndTime= getUserDefineTimeslotBean().getEventEndTime();
@@ -795,7 +808,15 @@ public class CopyMeetingSignupMBean extends SignupUIBaseBean {
 	public void setDeadlineTimeType(String deadlineTimeType) {
 		this.deadlineTimeType = deadlineTimeType;
 	}
-	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	/**
 	 * This is a getter method to provide selected location.
 	 * 
