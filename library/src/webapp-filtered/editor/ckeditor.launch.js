89bc29da5832f27adada844612d03d11e568976f
Code Hugger (Matthew Jones) <matthew@longsight.com>
2017-09-14 12:26:28 -0400

    SAK-33362 - Disable CKEditor autosave plugin until selector bug is fixed (#4798)

--- library/src/webapp-filtered/editor/ckeditor.launch.js
+++ library/src/webapp-filtered/editor/ckeditor.launch.js
@@ -273,7 +273,7 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config, w, h) {
         //ckconfig.extraPlugins+="atd-ckeditor,";
         //ckconfig.contentsCss = [basePath+'atd-ckeditor/atd.css'];
 
-        ckconfig.extraPlugins+="sakaipreview,image2,audiorecorder,contentitem,movieplayer,wordcount,fmath_formula,autosave,notification${ckeditor-a11y-extra-plugins}";
+        ckconfig.extraPlugins+="${ckeditor-extra-plugins}${ckeditor-a11y-extra-plugins}";
 
         // Load FontAwesome CSS in case a user wants to manually add FA markup
         ckconfig.contentsCss = [webJars+'fontawesome/4.7.0/css/font-awesome.min.css'];
