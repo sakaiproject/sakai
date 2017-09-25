96990a05e871ec986485cea8d6e671174b8d01fa
David Horwitz <dhorwitz@gmail.com>
2017-09-25 19:01:41 +0200

    SAK-33386 .setFillBackgroundColor(HSSFColor.BLUE_GREY.index); =>setFillBackgroundColor(IndexedColors.BLUE_GREY.getIndex()); (#4814)

--- jsf/jsf-spreadsheet/src/java/org/sakaiproject/jsf/spreadsheet/SpreadsheetDataFileWriterXls.java
+++ jsf/jsf-spreadsheet/src/java/org/sakaiproject/jsf/spreadsheet/SpreadsheetDataFileWriterXls.java
@@ -30,11 +30,11 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -77,7 +77,7 @@ public class SpreadsheetDataFileWriterXls implements SpreadsheetDataFileWriter {
 		
 		// Set the header style
 		headerCs.setBorderBottom(BorderStyle.THICK);
-		headerCs.setFillBackgroundColor(HSSFColor.BLUE_GREY.index);
+		headerCs.setFillBackgroundColor(IndexedColors.BLUE_GREY.getIndex());
 		// Set the font
 		CellStyle cellStyle = null;
 		String fontName = ServerConfigurationService.getString("spreadsheet.font");
