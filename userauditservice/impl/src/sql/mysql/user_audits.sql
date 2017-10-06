2a86106b4d6691b2251c7579c2bdb73c2665cfb8
Sam Ottenhoff <ottenhoff@longsight.com>
2017-10-06 10:50:04 -0400

    SAK-33430 use a proper index on site_id because queries are against site_id

--- userauditservice/impl/src/sql/mysql/user_audits.sql
+++ userauditservice/impl/src/sql/mysql/user_audits.sql
@@ -1,9 +1,9 @@
 -- SAK-23634 - new table to track user add/drop/update actions done in sites
 CREATE TABLE user_audits_log (
 	id BIGINT AUTO_INCREMENT NOT NULL,
-	site_id VARCHAR(255) NOT NULL,
+	site_id VARCHAR(99) NOT NULL,
 	user_id VARCHAR(99) NOT NULL,
-	role_name VARCHAR(255) NOT NULL,
+	role_name VARCHAR(99) NOT NULL,
 	action_taken VARCHAR(1) NOT NULL,
 	audit_stamp TIMESTAMP NOT NULL,
 	source VARCHAR(1),
@@ -11,4 +11,4 @@ CREATE TABLE user_audits_log (
 	PRIMARY KEY(id)
 );
 
-CREATE INDEX user_audits_log_index ON user_audits_log (id,site_id);
+CREATE INDEX user_audits_log_index ON user_audits_log (site_id);
