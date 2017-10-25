4d25b0d6bd7ca4909956380bb0118c76222ce4b9
Earle Nietzel <earle@longsight.com>
2017-10-25 12:09:42 -0400

    SAK-33505 Assignments ACCESS is a Oracle reserved word (#4915)

--- assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
+++ assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
@@ -351,7 +351,7 @@ public class AssignmentServiceImpl implements AssignmentService {
                         if (reference.getId() != null) {
                             Assignment a = getAssignment(reference);
                             if (a != null) {
-                                grouped = Assignment.Access.GROUPED == a.getAccess();
+                                grouped = Assignment.Access.GROUPED == a.getTypeOfAccess();
                                 groups = a.getGroups();
                             }
                         }
@@ -507,7 +507,7 @@ public class AssignmentServiceImpl implements AssignmentService {
 String assignmentId = AssignmentReferenceReckoner.reckoner().reference(assignmentReference).reckon().getId();
         try {
             Assignment a = getAssignment(assignmentId);
-            if (a.getAccess() == Assignment.Access.GROUPED) {
+            if (a.getTypeOfAccess() == Assignment.Access.GROUPED) {
                 // for grouped assignment, need to include those users that with "all.groups" and "grade assignment" permissions on the site level
                 try {
                     AuthzGroup group = authzGroupService.getAuthzGroup(siteService.siteReference(a.getContext()));
@@ -1075,7 +1075,7 @@ String assignmentId = AssignmentReferenceReckoner.reckoner().reference(assignmen
             Collection<Assignment> assignments = getAssignmentsForContext(context);
             for (Assignment assignment : assignments) {
                 Set<String> userIds = new HashSet<>();
-                if (assignment.getAccess() == Assignment.Access.GROUPED) {
+                if (assignment.getTypeOfAccess() == Assignment.Access.GROUPED) {
                     for (String groupRef : assignment.getGroups()) {
                         if (groupIdUserIds.containsKey(groupRef)) {
                             userIds.addAll(groupIdUserIds.get(groupRef));
@@ -1683,7 +1683,7 @@ String assignmentId = AssignmentReferenceReckoner.reckoner().reference(assignmen
             if (a != null) {
                 Site st = siteService.getSite(contextString);
                 if (StringUtils.equals(allOrOneGroup, AssignmentConstants.ALL) || StringUtils.isEmpty(allOrOneGroup)) {
-                    if (a.getAccess().equals(Assignment.Access.SITE)) {
+                    if (a.getTypeOfAccess().equals(Assignment.Access.SITE)) {
                         for (Group group : st.getGroups()) {
                             //if (_gg.getProperties().get(GROUP_SECTION_PROPERTY) == null) {		// NO SECTIONS (this might not be valid test for manually created sections)
                             rv.add(group);
@@ -1847,7 +1847,7 @@ String assignmentId = AssignmentReferenceReckoner.reckoner().reference(assignmen
                         // add those users who haven't made any submissions and with submission rights
                         //only initiate the group list once
                         if (groupRefs.isEmpty()) {
-                            if (a.getAccess() == Assignment.Access.SITE) {
+                            if (a.getTypeOfAccess() == Assignment.Access.SITE) {
                                 // for site range assignment, add the site reference first
                                 groupRefs.add(siteService.siteReference(contextString));
                             }
@@ -2381,7 +2381,7 @@ String assignmentId = AssignmentReferenceReckoner.reckoner().reference(assignmen
 
     private Assignment checkAssignmentAccessibleForUser(Assignment assignment, String currentUserId) throws PermissionException {
 
-        if (assignment.getAccess() == Assignment.Access.GROUPED) {
+        if (assignment.getTypeOfAccess() == Assignment.Access.GROUPED) {
             String context = assignment.getContext();
             Collection<String> asgGroups = assignment.getGroups();
             Collection<Group> allowedGroups = getGroupsAllowFunction(SECURE_ACCESS_ASSIGNMENT, context, currentUserId);
