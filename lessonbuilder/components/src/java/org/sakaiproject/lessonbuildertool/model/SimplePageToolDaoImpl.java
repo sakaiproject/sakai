938cd33596d3c6ef6aa796ea23d47f8655a4c360
Matthew Jones <jonespm@gmail.com>
2017-11-22 16:48:16 -0500

    From 315b35fa62e5aab4ad9bccd7908f2ee6f2debdde Mon Sep 17 00:00:00 2001 From: Sam Ottenhoff <ottenhoff@longsight.com> Date: Wed, 4 Oct 2017 10:16:45 -0400 Subject: [PATCH] LSNBLDR-877 use Hibernate and siteService to retrieve instead  of bad custom SQL (#4858)
    (cherry picked from commit 3b39981f57f453dd6155f2f5e185ff0d0f79ac49)

--- lessonbuilder/components/src/java/org/sakaiproject/lessonbuildertool/model/SimplePageToolDaoImpl.java
+++ lessonbuilder/components/src/java/org/sakaiproject/lessonbuildertool/model/SimplePageToolDaoImpl.java
@@ -48,6 +48,7 @@ import org.sakaiproject.authz.api.SecurityService;
 import org.sakaiproject.db.api.SqlReader;
 import org.sakaiproject.db.api.SqlService;
 import org.sakaiproject.event.cover.EventTrackingService;
+import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.lessonbuildertool.SimplePage;
 import org.sakaiproject.lessonbuildertool.SimplePageComment;
 import org.sakaiproject.lessonbuildertool.SimplePageCommentImpl;
@@ -76,7 +77,9 @@ import org.sakaiproject.lessonbuildertool.SimplePagePropertyImpl;
 import org.sakaiproject.lessonbuildertool.SimpleChecklistItem;
 import org.sakaiproject.lessonbuildertool.SimpleChecklistItemImpl;
 import org.sakaiproject.lessonbuildertool.ChecklistItemStatus;
-
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SitePage;
+import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.tool.api.ToolManager;
 import org.sakaiproject.user.cover.UserDirectoryService;
 import org.springframework.dao.DataAccessException;
@@ -92,9 +95,9 @@ public class SimplePageToolDaoImpl extends HibernateDaoSupport implements Simple
 
 	private ToolManager toolManager;
 	private SecurityService securityService;
+	private SiteService siteService;
 	private SqlService sqlService;
 	private AuthzGroupService authzGroupService;
-	private static String SITE_UPD = "site.upd";
 
         // part of HibernateDaoSupport; this is the only context in which it is OK
         // to modify the template configuration
@@ -183,6 +186,10 @@ public class SimplePageToolDaoImpl extends HibernateDaoSupport implements Simple
 		return canEdit;
 	}
 
+	public void setSiteService(SiteService service) {
+		siteService = service;
+	}
+
 	public void setSecurityService(SecurityService service) {
 		securityService = service;
 	}
@@ -221,19 +228,39 @@ public class SimplePageToolDaoImpl extends HibernateDaoSupport implements Simple
 	}
 
 	public List<SimplePageItem> findItemsInSite(String siteId) {
-	    Object [] fields = new Object[1];
-	    fields[0] = siteId;
-	    List<String> ids = sqlService.dbRead("select b.id from lesson_builder_pages a,lesson_builder_items b,SAKAI_SITE_PAGE c where a.siteId = ? and a.parent is null and a.pageId = b.sakaiId and b.type = 2 and b.pageId = 0 and a.toolId = c.PAGE_ID order by c.SITE_ORDER", fields, null);
+		// This is a three-step process.
+		// 1) Find the pages in the site
+		// 2) Find the Lessons pages 
+		// 3) Find the Lessons items
+		try {
+			Site site = siteService.getSite(siteId);
+			List<SitePage> sitePages = site.getOrderedPages();
 
-	    List<SimplePageItem> result = new ArrayList<SimplePageItem>();
-	    
-	    if (result != null) {
-		for (String id: ids) {
-		    SimplePageItem i = findItem(new Long(id));
-		    result.add(i);
+			List<String> sitePageIds = new ArrayList<>();
+			for (SitePage page : sitePages) {
+				sitePageIds.add(page.getId());
+			}
+
+			DetachedCriteria d = DetachedCriteria.forClass(SimplePage.class);
+			d.add(Restrictions.in("toolId", sitePageIds));
+			d.add(Restrictions.isNull("parent"));
+
+			List<SimplePage> lessonsPages = (List<SimplePage>) getHibernateTemplate().findByCriteria(d);
+			List<String> lessonsPageIds = new ArrayList<>();
+			if (!lessonsPages.isEmpty()) {
+				for (SimplePage lessonsPage : lessonsPages) {
+					String pageId = String.valueOf(lessonsPage.getPageId());
+					lessonsPageIds.add(pageId);
+				}
+
+				List<SimplePageItem> pageItems = findTopLevelPageItemsBySakaiIds(lessonsPageIds);
+				return pageItems;
+			}
+		} catch (IdUnusedException e) {
+			log.warn("Could not find site: " + siteId, e);
 		}
-	    }
-	    return result;
+
+		return null;
 	}
 
 	public List<SimplePageItem> findDummyItemsInSite(String siteId) {
@@ -509,6 +536,21 @@ public class SimplePageToolDaoImpl extends HibernateDaoSupport implements Simple
 		    return null;
 			
 		return list.get(0);
+	}
+
+	public List<SimplePageItem> findTopLevelPageItemsBySakaiIds(List<String> ids) {
+		DetachedCriteria d = DetachedCriteria.forClass(SimplePageItem.class)
+			.add(Restrictions.in("sakaiId", ids))
+			.add(Restrictions.eq("pageId", 0L))
+			.add(Restrictions.eq("type",SimplePageItem.PAGE));
+
+		List<SimplePageItem> list = (List<SimplePageItem>) getHibernateTemplate().findByCriteria(d);
+
+		if (list == null || list.size() < 1) {
+			return null;
+		}
+
+		return list;
 	}
 
 	public List<SimplePageItem> findPageItemsBySakaiId(String id) {
