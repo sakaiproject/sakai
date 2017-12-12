1d1b5c58d593ce2f5a1d5d1b9efe8665bed9b90a
Code Hugger (Matthew Jones) <matthew@longsight.com>
2017-12-12 15:35:29 -0500

    SAK-33685 - Syllabus icons are not able to be clicked (#5115)
    (cherry picked from commit 221a90344ff1ec204b839d00bf3a6e1123d08b19)

--- syllabus/syllabus-app/src/webapp/js/syllabus.js
+++ syllabus/syllabus-app/src/webapp/js/syllabus.js
@@ -65,7 +65,7 @@ function setupAccordion(iframId, isInstructor, msgs, openDataId){
 	if(isInstructor){
 		$( "#accordion span" ).sortable({
 			axis: "y",
-			handle: "h3 span.actionIcon",
+			handle: "h3 span.handleIcon",
 			start: function(event, ui){
 			dragStartIndex = ui.item.index();
 		},
