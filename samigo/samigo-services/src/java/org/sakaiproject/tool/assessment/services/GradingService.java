b8c662a36762a6b8299e065198503408e083face
Sam Ottenhoff <ottenhoff@longsight.com>
2018-01-02 13:48:30 -0500

    SAM-3330 use mxParser supported math expression library (#5142)
    * SAM-3330 use mxParser supported math expression library
    
    * SAM-3330 a few replacements now that we use mxParser
    
    * SAM-3330 POM for central deployment
    
    (cherry picked from commit 199b6804b6da0d974b91e83f722169eb9a803f97)

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/GradingService.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/GradingService.java
@@ -3195,6 +3195,7 @@ Here are the definition and 12 cases I came up with (lydia, 01/2006):
       } else {
           formula = StringUtils.trimToEmpty(formula).replaceAll("\\s+", " ");
       }
+
       return formula;
   }
 
