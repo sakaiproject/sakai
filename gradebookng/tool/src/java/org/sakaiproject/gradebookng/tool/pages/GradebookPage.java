27f396c6fd0697d87c346dc0937b3d3fff8dc08f
Brian Jones <bjones86@uwo.ca>
2018-01-12 11:17:54 -0500

    SAK-33830: minor code cleanup

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/pages/GradebookPage.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/pages/GradebookPage.java
@@ -15,6 +15,11 @@
  */
 package org.sakaiproject.gradebookng.tool.pages;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
@@ -71,8 +76,6 @@ import org.sakaiproject.service.gradebook.shared.GradingType;
 import org.sakaiproject.service.gradebook.shared.PermissionDefinition;
 import org.sakaiproject.service.gradebook.shared.SortType;
 import org.sakaiproject.tool.gradebook.Gradebook;
-
-import java.util.*;
 
 /**
  * Grades page. Instructors and TAs see this one. Students see the {@link StudentPage}.
