fb9475caa092c1b99eb6853bcbb4ceec630915db
joaquinmarques <jmarques@pentec.es>
2018-01-31 17:53:16 +0100

    SAK-33884 Update UsersAction.java, chef_users_edit.vm

--- admin-tools/src/java/org/sakaiproject/user/tool/UsersAction.java
+++ admin-tools/src/java/org/sakaiproject/user/tool/UsersAction.java
@@ -577,6 +577,13 @@ public class UsersAction extends PagedResourceActionII
 		// include the password fields?
 		context.put("incPw", state.getAttribute("include-password"));
 
+		boolean providedUserType = false;
+		
+		if (isProvidedType(user.getType())) {
+			providedUserType = true;
+		}
+		context.put("providedUserType", providedUserType);
+		
 		// include type fields (not if single user)
 		boolean singleUser = ((Boolean) state.getAttribute("single-user")).booleanValue();
 		context.put("incType", Boolean.valueOf(!singleUser));
