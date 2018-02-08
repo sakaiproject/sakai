1e062672a5be04a6f537bf19e3fe13f8f8f8a1b2
Jose Cebellán <josecebe@gmail.com>
2018-02-08 15:31:42 +0100

    SAM-3264 Copy to Pool for imported essay question not working (#5291)

--- samigo/samigo-hibernate/src/java/org/sakaiproject/tool/assessment/data/dao/assessment/ItemText.java
+++ samigo/samigo-hibernate/src/java/org/sakaiproject/tool/assessment/data/dao/assessment/ItemText.java
@@ -111,7 +111,9 @@ public class ItemText
 
   public List<AnswerIfc> getAnswerArray() {
     List<AnswerIfc> list = new ArrayList<AnswerIfc>();
-    list.addAll(answerSet);
+    if (answerSet != null) {
+      list.addAll(answerSet);
+    }
     return list;
   }
 
