ab756006c48f45d627fc4e10e3fd7bdac20a75fe
Code Hugger (Matthew Jones) <matthew@longsight.com>
2018-02-08 17:50:07 -0500

    Adding to cleanState

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -1202,6 +1202,8 @@ public class SiteAction extends PagedResourceActionII {
 
 		// SAK-24423 - remove joinable site settings from the state
 		JoinableSiteSettings.removeJoinableSiteSettingsFromState( state );
+		
+		state.removeAttribute(STATE_STATE_FROM_ARCHIVE);
 
 	} // cleanState
 
