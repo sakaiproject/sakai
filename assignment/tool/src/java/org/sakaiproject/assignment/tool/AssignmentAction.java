f88f667fd9fa38aea46e6003c23e04d44c25ef9b
Ben Peterson <ben.micah.peterson@gmail.com>
2018-03-16 15:06:40 -0400

    moved repo catch to avoid null assignments

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -6853,6 +6853,25 @@ public class AssignmentAction extends PagedResourceActionII {
         } else {
             state.setAttribute(NEW_ASSIGNMENT_REVIEW_SERVICE_EXCLUDE_VALUE, "1");
         }
+        
+        
+        //Check if all required content review fields are set
+        boolean useReviewService = "true".equalsIgnoreCase((String) state.getAttribute(NEW_ASSIGNMENT_USE_REVIEW_SERVICE));
+        boolean checkInternet = "true".equalsIgnoreCase((String) state.getAttribute(NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INTERNET));
+        boolean checkInstitution = "true".equalsIgnoreCase((String) state.getAttribute(NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INSTITUTION));
+        boolean showInternetCheck = "true".equals(params.getString("show_NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INTERNET"));
+        boolean showInstituionCheck = "true".equals(params.getString("show_NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INSTITUTION"));
+        if (useReviewService            		            		            	
+        		&& contentReviewService != null
+				&& Arrays.asList("TurnitinOC").contains(contentReviewService.getServiceName())
+				//At least one "check" option must be selected for TurnitinOC 
+				&& !checkInternet 
+				&& !checkInstitution
+				// If both are unchecked make sure options were visible to user
+				&& (showInstituionCheck || showInternetCheck)) {	            	
+            	String contentReviewNote = rb.getFormattedMessage("review.required", new String[] {contentReviewService.getServiceName()});
+            	addAlert(state, contentReviewNote);
+        }
 
         // treat the new assignment description as formatted text
         boolean checkForFormattingErrors = true; // instructor is creating a new assignment - so check for errors
@@ -7688,24 +7707,6 @@ public class AssignmentAction extends PagedResourceActionII {
                     //Numberformatexception
                 }
             }
-
-            // Set if required "check" options are visible to user                        
-            boolean showInternetCheck = "true".equals(params.getString("show_NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INTERNET"));
-            boolean showInstituionCheck = "true".equals(params.getString("show_NEW_ASSIGNMENT_REVIEW_SERVICE_CHECK_INSTITUTION"));
-                        
-            //Check if all required content review fields are set
-            if (useReviewService            		            		            	
-            		&& contentReviewService != null
-    				&& "TurnitinOC".equals(contentReviewService.getServiceName())
-    				//At least one "check" option must be selected for TurnitinOC 
-    				&& !checkInternet 
-    				&& !checkInstitution
-    				// If both are unchecked make sure options were visible to user
-    				&& (showInstituionCheck || showInternetCheck)) {	            	
-	            	String contentReviewNote = rb.getFormattedMessage("review.required", new String[] {contentReviewService.getServiceName()});
-	            	addAlert(state, contentReviewNote);
-	            	return;
-            }  
 
             // the attachments
             List attachments = (List) state.getAttribute(NEW_ASSIGNMENT_ATTACHMENT);
