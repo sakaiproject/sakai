fc21d4118416ee7b7f559ca763d65d17d960df2a
Amie Davis <amiedav@gmail.com>
2018-04-22 12:52:53 +1000

    Overrode the drawing method for Chartjs so that we can have rounded corners
    As documented, this code is based on code by Jed Trow. It has been modified so that only the top (or right, in the case of a horizontal bar) corners are rounded. I have also removed some code that was seemingly redundant.
    The github project is linked to in the doco. His code to deal with borderWidth has been left in, in case we want a border on the bars.
    The corner radius is defined as half the width or height, whichever is smaller.
    Hopefully this feature will be added to a future version of Chartjs and we can remove this method.
    
    Issue: SAK-34043

--- gradebookng/tool/src/webapp/scripts/gradebook-coursegrade-chart.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-coursegrade-chart.js
@@ -45,6 +45,108 @@ function refreshChart(siteId, schemaJson) {
 }
 
 /**
+ * Custom chart drawing, to enable us to have rounded corners
+ * 
+ * Based on code by Jed Trow
+ * https://github.com/jedtrow/Chart.js-Rounded-Bar-Charts/blob/master/Chart.roundedBarCharts.js
+ * 
+ * Updated to remove unnecessary code and only round the top corners
+ * 
+ * NOTE: This code should be removed as soon as rounded corners are added as a feature to Chartjs. 
+ * It is only a stopgap solution.
+ */
+
+Chart.elements.Rectangle.prototype.draw = function() {
+	
+    var ctx = this._chart.ctx;
+    var vm = this._view;
+    var left, right, top, bottom, signX, signY, borderSkipped, cornerRadius;
+    var borderWidth = vm.borderWidth;
+
+    if (!vm.horizontal) {
+        // bar
+        left = vm.x - vm.width / 2;
+        right = vm.x + vm.width / 2;
+        top = vm.y;
+        bottom = vm.base;
+        signX = 1;
+        signY = bottom > top? 1: -1;
+        borderSkipped = vm.borderSkipped || 'bottom';
+    } else {
+        // horizontal bar
+        left = vm.base;
+        right = vm.x;
+        top = vm.y - vm.height / 2;
+        bottom = vm.y + vm.height / 2;
+        signX = right > left? 1: -1;
+        signY = 1;
+        borderSkipped = vm.borderSkipped || 'left';
+    }
+
+    // Canvas doesn't allow us to stroke inside the width so we can
+    // adjust the sizes to fit if we're setting a stroke on the line
+    if (borderWidth) {
+        // borderWidth should be less than bar width and bar height.
+        var barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+        borderWidth = borderWidth > barSize? barSize: borderWidth;
+        var halfStroke = borderWidth / 2;
+        // Adjust borderWidth when bar top position is near vm.base (zero)
+        var borderLeft = left + (borderSkipped !== 'left'? halfStroke * signX: 0);
+        var borderRight = right + (borderSkipped !== 'right'? -halfStroke * signX: 0);
+        var borderTop = top + (borderSkipped !== 'top'? halfStroke * signY: 0);
+        var borderBottom = bottom + (borderSkipped !== 'bottom'? -halfStroke * signY: 0);
+        // not become a vertical line?
+        if (borderLeft !== borderRight) {
+            top = borderTop;
+            bottom = borderBottom;
+        }
+        // not become a horizontal line?
+        if (borderTop !== borderBottom) {
+            left = borderLeft;
+            right = borderRight;
+        }
+    }
+
+    width = right - left;
+    height = bottom - top;
+    
+    // Set the corner radius to be half the width or height (whichever is smaller)
+    if (this._chart.config.options.roundedCorners) {
+    	cornerRadius = (Math.abs(height) < Math.abs(width)) ? Math.floor(Math.abs(height)/2) : Math.floor(Math.abs(width)/2);
+    } else {
+    	cornerRadius = 0;
+    }
+
+    // Draw the rectangle
+    ctx.beginPath();
+    ctx.fillStyle = vm.backgroundColor;
+    ctx.strokeStyle = vm.borderColor;
+    ctx.lineWidth = borderWidth;
+    var x = left, y = top;
+    if (!vm.horizontal) {
+    	ctx.moveTo(x, y + height);
+    	ctx.lineTo(x, y + cornerRadius);
+        ctx.quadraticCurveTo(x, y, x + cornerRadius, y);
+        ctx.lineTo(x + width - cornerRadius, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + cornerRadius);
+        ctx.lineTo(x + width, y + height);
+        ctx.lineTo(x, y + height);
+    } else {
+    	ctx.moveTo(x, y);
+    	ctx.lineTo(x + width - cornerRadius, y);
+    	ctx.quadraticCurveTo(x + width, y, x + width, y + cornerRadius);
+    	ctx.lineTo(x + width, y + height - cornerRadius);
+    	ctx.quadraticCurveTo(x + width, y + height, x + width - cornerRadius, y + height);
+    	ctx.lineTo(x, y + height);
+    	ctx.lineTo(x, y);
+    }
+    ctx.fill();
+    if (borderWidth) {
+        ctx.stroke();
+    }
+};
+
+/**
  * Render the course grade summary chart from the given dataset.
  * @param dataset
  * @returns
@@ -69,10 +171,11 @@ function _renderChart(dataset) {
 			datasets: [{
 				data: data,
 				backgroundColor: 'rgb(64, 120, 209)',
-				borderWidth: 0,
+				borderWidth: 0
 			}]
 		},
 		options: {
+			roundedCorners: true,
 			title: {
 				display: true,
 				text: 'Course Grade Distribution'
