4e972e90162915c6b384fdd20af69bcaee0f14b2
Brian Baillargeon <bbailla2@uwo.ca>
2018-05-15 16:03:01 -0400

    SAK-39994: assignments > assign grade to participants without a grade, decimals multiplied by 100 (display issue)
    (cherry picked from commit a89b2f7e918df829892d10ff113eafba5b0ccd13)

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -12534,9 +12534,9 @@ public class AssignmentAction extends PagedResourceActionII {
 
                 // Only record the default grade setting for no-submission if there were no errors produced
                 if (state.getAttribute(STATE_MESSAGE) == null) {
-                    grade = scalePointGrade(state, grade, a.getScaleFactor());
 
                     try {
+                        // Save value as input by user, not scaled
                         a.getProperties().put(GRADE_NO_SUBMISSION_DEFAULT_GRADE, grade);
                         assignmentService.updateAssignment(a);
                     } catch (PermissionException e) {
@@ -12547,6 +12547,8 @@ public class AssignmentAction extends PagedResourceActionII {
 
 
             if (grade != null && state.getAttribute(STATE_MESSAGE) == null) {
+                grade = scalePointGrade(state, grade, a.getScaleFactor());
+
                 // get the user list
                 String aRef = AssignmentReferenceReckoner.reckoner().assignment(a).reckon().getReference();
                 List<AssignmentSubmission> submissions = getFilteredSubmitters(state, aRef);
@@ -12613,9 +12615,8 @@ public class AssignmentAction extends PagedResourceActionII {
 
                 // Only record the default grade setting for no-submission if there were no errors produced
                 if (state.getAttribute(STATE_MESSAGE) == null) {
-                    grade = scalePointGrade(state, grade, a.getScaleFactor());
-
                     try {
+                        // Save value as input by user, not scaled
                         a.getProperties().put(GRADE_NO_SUBMISSION_DEFAULT_GRADE, grade);
                         assignmentService.updateAssignment(a);
                     } catch (PermissionException e) {
@@ -12626,6 +12627,8 @@ public class AssignmentAction extends PagedResourceActionII {
 
 
             if (grade != null && state.getAttribute(STATE_MESSAGE) == null) {
+                grade = scalePointGrade(state, grade, a.getScaleFactor());
+
                 // get the submission list
                 String aRef = AssignmentReferenceReckoner.reckoner().assignment(a).reckon().getReference();
                 List<AssignmentSubmission> submissions = getFilteredSubmitters(state, aRef);
