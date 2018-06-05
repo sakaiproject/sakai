66b8e44ad9be70808182d05b2ec0e8ed7d517fab
Matthew Jones <jonespm@gmail.com>
2018-06-05 15:05:18 -0400

    SAK-40088 - Error from roster when adding member to site via webservice
    (cherry picked from commit 261e421dfc8357a5c1af806dc0b4332e07e58abd)

--- roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
+++ roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
@@ -1191,6 +1191,11 @@ public class SakaiProxyImpl implements SakaiProxy, Observer {
                 log.debug("Site membership or groups updated. Clearing caches ...");
                 String siteId = event.getContext();
 
+                if (siteId == null) {
+                    log.debug("siteId was null, skipping");
+                    return;
+                }
+
                 Cache enrollmentsCache = getCache(ENROLLMENTS_CACHE);
                 enrollmentsCache.remove(siteId);
 
