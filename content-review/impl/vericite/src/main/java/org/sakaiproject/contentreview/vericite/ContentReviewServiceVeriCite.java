7c7301a8d395e5a99110ab119a3946e43f2856e3
Bryan Holladay <baholladay@gmail.com>
2018-07-13 17:08:15 -0400

    SAK-40317 Content Review Webhooks should use the provider ID instead of Name

--- content-review/impl/vericite/src/main/java/org/sakaiproject/contentreview/vericite/ContentReviewServiceVeriCite.java
+++ content-review/impl/vericite/src/main/java/org/sakaiproject/contentreview/vericite/ContentReviewServiceVeriCite.java
@@ -1004,7 +1004,7 @@ public class ContentReviewServiceVeriCite extends BaseContentReviewService {
 	}
 
 	@Override
-	public void webhookEvent(HttpServletRequest request, String providerName, Optional<String> customParam) {
+	public void webhookEvent(HttpServletRequest request, int providerId, Optional<String> customParam) {
 		// TODO Auto-generated method stub
 		
 	}
