335e406261bd25b670784e2ed3de4b3c21f68a95
Code Hugger (Matthew Jones) <matthew@longsight.com>
2018-07-23 08:52:49 -0400

    SAK-40085 - Loss of information when a student add content in student page (#5820)
    (cherry picked from commit 3a0fbea3add28886e4768d5aff2ae60446170cd8)

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
@@ -1734,7 +1734,7 @@ public class SimplePageBean {
 				continue;
 			    }
 			}
-			if (after) {
+			if (after && item.getPageId() >= 0) {
 			    item.setSequence(item.getSequence() + 1);
 			    simplePageToolDao.quickUpdate(item);
 			}
