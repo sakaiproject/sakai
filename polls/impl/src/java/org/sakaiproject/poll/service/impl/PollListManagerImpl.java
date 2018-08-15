f1d8fade93321b49b2e25b721f04856158cdd791
Brian Jones <bjones86@uwo.ca>
2018-08-15 13:55:13 +0000

    Update PollListManagerImpl.java
    braces

--- polls/impl/src/java/org/sakaiproject/poll/service/impl/PollListManagerImpl.java
+++ polls/impl/src/java/org/sakaiproject/poll/service/impl/PollListManagerImpl.java
@@ -177,8 +177,9 @@ public class PollListManagerImpl implements PollListManager,EntityTransferrer {
         try {
             if(!newPoll) {
                 Poll poll = dao.findById(Poll.class, Long.parseLong(t.getId()));
-                if(poll != null && !t.getOwner().equals(poll.getOwner()))
+                if(poll != null && !t.getOwner().equals(poll.getOwner())) {
                    t.setOwner(poll.getOwner());
+		}
             }
             dao.save(t);
 
