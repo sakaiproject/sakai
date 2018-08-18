d8ca1bf050637b3fb7023cd729d03a65fc7a6f9a
Brian Jones <bjones86@uwo.ca>
2018-08-18 13:22:36 -0400

    SAK-40468: Roster > fix NPE on 'Enrollment Status' UI

--- roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
+++ roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
@@ -539,10 +539,14 @@ public class SakaiProxyImpl implements SakaiProxy, Observer {
 		}
 
 		Map<String, User> userMap = getUserMap(membership);
+
+		// Audio URL for how to pronunce each name
+		Map<String, String> pronunceMap = getPronunciationMap(userMap);
+
 		Collection<Group> groups = site.getGroups();
 		for (Member member : membership) {
 			try {
-				RosterMember rosterMember = getRosterMember(userMap, groups, member, site, null);
+				RosterMember rosterMember = getRosterMember(userMap, groups, member, site, pronunceMap);
 				rosterMembers.put(rosterMember.getEid(), rosterMember);
 			} catch (UserNotDefinedException e) {
 				log.warn("user not found: " + e.getId());
