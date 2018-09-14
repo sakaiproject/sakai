c17d118567ed88d71e5d30146a6a464ece2c7490
Mark Triggs <mark@dishevelled.net>
2018-09-14 15:15:55 -0400

    SAK-40603 limit cells that get redrawn after each score item save. Big performance boost in course with many categories and items.
    (cherry picked from commit b1c75e1bacf06aaadb8c30c3002781c8cace03ef)

--- gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
@@ -1431,11 +1431,21 @@ GbGradeTable.updateComment = function(assignmentId, studentId, comment) {
 };
 
 GbGradeTable.redrawCell = function(row, col) {
-  var $cell = $(GbGradeTable.instance.getCell(row, col));
-  $cell.removeData('cell-initialised');
-
-  GbGradeTable.instance.render();
+    GbGradeTable.redrawCells([[row, col]])
 };
+
+GbGradeTable.redrawCells = function(cells) {
+    cells.forEach(function(cell) {
+        var row = cell[0];
+        var col = cell[1];
+        var $cell = $(GbGradeTable.instance.getCell(row, col));
+        $cell.removeData('cell-initialised');
+    });
+
+    GbGradeTable.instance.render();
+};
+
+
 
 GbGradeTable.formatCategoryAverage = function(value) {
   if (value != null && (value+"").length > 0 && value != "-") {
@@ -2897,13 +2907,16 @@ GbGradeTable.syncCategoryAverage = function(studentId, categoryId, categoryScore
 
     // update dropped status of all items in this category
     var categoryItems = GbGradeTable.itemsInCategory(categoryId);
+    var cellsToRedraw = [];
     categoryItems.forEach(function(col) {
         var dropped = droppedItems.indexOf(col.assignmentId) > -1;
         var columnIndex = GbGradeTable.colForAssignment(col.assignmentId);
         var student = GbGradeTable.modelForStudent(studentId);
         GbGradeTable.updateHasDroppedScores(student, columnIndex - GbGradeTable.FIXED_COLUMN_OFFSET, dropped);
-        GbGradeTable.redrawCell(tableRow, columnIndex);
+        cellsToRedraw.push([tableRow, columnIndex]);
     });
+
+    GbGradeTable.redrawCells(cellsToRedraw);
 };
 
 
@@ -2949,12 +2962,16 @@ GbGradeTable.setScore = function(studentId, assignmentId, oldScore, newScore) {
 
         // update the course grade cell
         if (data.courseGrade) {
-          GbGradeTable.syncCourseGrade(studentId, data.courseGrade);
+            setTimeout(function () {
+                GbGradeTable.syncCourseGrade(studentId, data.courseGrade);
+            }, 0);
         }
 
         // update the category average cell
         if (assignment.categoryId) {
-          GbGradeTable.syncCategoryAverage(studentId, assignment.categoryId, data.categoryScore, data.categoryDroppedItems);
+            setTimeout(function () {
+                GbGradeTable.syncCategoryAverage(studentId, assignment.categoryId, data.categoryScore, data.categoryDroppedItems);
+            }, 0);
         }
 
         GbGradeTable.syncScore(studentId, assignmentId, newScore);
