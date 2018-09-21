bd335aa79a081536c1e2a91b09077f8dfce7b5f4
Earle Nietzel <earle@longsight.com>
2018-09-21 15:41:11 -0400

    SAK-40663 BaseSiteService wrong site user cache invalidation on prefs.upd (#6026)

--- kernel/kernel-impl/src/main/java/org/sakaiproject/site/impl/BaseSiteService.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/site/impl/BaseSiteService.java
@@ -3702,30 +3702,32 @@ public abstract class BaseSiteService implements SiteService, Observer
 
 		String eventType = event.getEvent();
 
-		if (EVENT_SITE_USER_INVALIDATE.equals(eventType) || PreferencesService.SECURE_EDIT_PREFS.equals(eventType)) {
-			// KNL-1171: always clear the cache for the user as the Site below may have been deleted
-			clearUserCacheForUser(event.getUserId());
+        if (eventType != null) {
+            switch (eventType) {
+                case SiteService.SECURE_UPDATE_SITE_MEMBERSHIP:
+                case SiteService.SECURE_UPDATE_GROUP_MEMBERSHIP:
+                case SiteService.SECURE_UPDATE_SITE:
+                    notifySiteParticipant("/gradebook/" + event.getContext() + "/");
+                    break;
+                case EVENT_SITE_USER_INVALIDATE:
+                    try {
+                        Site site = getSite(event.getResource());
+                        clearUserCacheForSite(site);
+                    } catch (IdUnusedException iue) {
+                        log.warn("Site not found when handling event ({})", event);
+                    }
+                case AuthzGroupService.SECURE_UNJOIN_AUTHZ_GROUP:
+                case EVENT_SITE_VISIT_DENIED:
+                case PreferencesService.SECURE_EDIT_PREFS:
+                    // always clear the cache for the user as the Site below may have been deleted
+                    clearUserCacheForUser(event.getUserId());
+                    break;
+                default:
+                    // do nothing for all other events
+            }
+        }
+    }
 
-			try {
-				Site site = getSite(event.getResource());
-				clearUserCacheForSite(site);
-			} catch (IdUnusedException e) {
-				if (log.isDebugEnabled())
-				{
-					log.debug("Site not found when handling an event (" + eventType + "), ID/REF: " + event.getResource());
-				}
-			}
-		}
-		else if (EVENT_SITE_VISIT_DENIED.equals(eventType) || AuthzGroupService.SECURE_UNJOIN_AUTHZ_GROUP.equals(eventType))
-		{
-			clearUserCacheForUser(event.getUserId());
-		}
-		else if(SiteService.SECURE_UPDATE_SITE_MEMBERSHIP.equals(eventType) || SiteService.SECURE_UPDATE_GROUP_MEMBERSHIP.equals(eventType)
-				|| SiteService.SECURE_UPDATE_SITE.equals(eventType))
-		{
-			notifySiteParticipant("/gradebook/" + event.getContext() + "/");
-		}
-	}
 	protected Storage storage() {
 		return m_storage;
 	}
