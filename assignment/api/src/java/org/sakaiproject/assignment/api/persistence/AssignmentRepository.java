89d1ab3d31d17384020fbff80de3a07e8836c0fe
RyanAFinney <12520113+RyanAFinney@users.noreply.github.com>
2018-09-25 11:34:27 -0400

    SAK-33934 reduce assignment count when person is removed from course (#5802)
    * SAK-33934 reduce assignment count when person is removed from course
    
    added check in the count submissions list to get just the count of the submission for the students active in the course
    
    * SAK-33934 update submission count as people drop course
    
    code cleanup per review.
    
    * SAK-33934
    
    removed unused import
    
    * SAK-33934 Null userids should not be added to count query
    
    (cherry picked from commit 5d7ff8c0e0b2f99c7e1f2eed706040c04a6ea9cf)

--- assignment/api/src/java/org/sakaiproject/assignment/api/persistence/AssignmentRepository.java
+++ assignment/api/src/java/org/sakaiproject/assignment/api/persistence/AssignmentRepository.java
@@ -77,7 +77,7 @@ public interface AssignmentRepository extends SerializableRepository<Assignment,
      * @param userSubmission if not null adds the requirement that the submission's userSubmission field matches this value
      * @return
      */
-    long countAssignmentSubmissions(String assignmentId, Boolean graded, Boolean hasSubmissionDate, Boolean userSubmission);
+    long countAssignmentSubmissions(String assignmentId, Boolean graded, Boolean hasSubmissionDate, Boolean userSubmission, List<String> userIds);
 
     void resetAssignment(Assignment assignment);
 }
