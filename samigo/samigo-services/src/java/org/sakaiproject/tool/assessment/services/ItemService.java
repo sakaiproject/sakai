8a501f11cb79f13f4910c96d1f7758b81c0208a2
Bernardo <bgarcia@entornosdeformacion.com>
2018-10-29 09:12:15 +0100

    SAK-40848 allow removing attachments from items

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/ItemService.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/services/ItemService.java
@@ -464,6 +464,15 @@ public class ItemService
     }
   }
   
+  public void removeItemAttachment(Long attachmentId) {
+    try {
+      PersistenceService.getInstance().getItemFacadeQueries().removeItemAttachment(attachmentId);
+    } catch(Exception e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException(e);
+    }
+  }
+  
   /**
    * Save favorite column choices for matrix survey question.
    */
