5b8486dc7114b69c93c80d4e5cbe589c0cd97a1b
Sam Ottenhoff <ottenhoff@longsight.com>
2018-11-13 11:26:56 -0500

    SAK-40784 fix for NoClassDefFound (#6234)
    (cherry picked from commit a027194500932cdefccc2f4f85195b54a349a26e)

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/integration/helper/integrated/GradebookServiceHelperImpl.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/integration/helper/integrated/GradebookServiceHelperImpl.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.math3.util.Precision;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.service.gradebook.shared.GradebookExternalAssessmentService;
