8e803918ea31afcae2da7e8658e841bbf6cdb1dc
Bernardo <bgarcia@entornosdeformacion.com>
2018-11-29 09:32:21 +0100

    SAK-41008 Instructors weren't able to remove Rubric associations
    (cherry picked from commit e4f639b272da5973a6de97cfac48f07ba898cb96)

--- rubrics/impl/src/main/java/org/sakaiproject/rubrics/logic/RubricsServiceImpl.java
+++ rubrics/impl/src/main/java/org/sakaiproject/rubrics/logic/RubricsServiceImpl.java
@@ -779,7 +779,11 @@ public class RubricsServiceImpl implements RubricsService, EntityProducer, Entit
             conn.setRequestMethod("DELETE");
             conn.setRequestProperty("Accept", "application/json");
             conn.setRequestProperty("Cookie", cookie );
-            conn.setRequestProperty("Authorization", "Bearer " + generateJsonWebToken(toolId, siteId));
+            if(siteId != null) {
+                conn.setRequestProperty("Authorization", String.format("Bearer %s", generateJsonWebToken(toolId, siteId)));
+            } else {
+                conn.setRequestProperty("Authorization", String.format("Bearer %s", generateJsonWebToken(toolId)));
+            }
 
             if (conn.getResponseCode() != 204) {
                 throw new RuntimeException("Failed : HTTP error code : "
