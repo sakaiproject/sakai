82c2b77ff925c75df5c3ebb9bdba06a42b4c9638
Adrian Fish <adrian.r.fish@gmail.com>
2018-12-14 20:42:33 +0000

    SAK-41038 Let Spring decide on the @Temporal column type

--- portal/portal-service-impl/impl/src/java/org/sakaiproject/portal/service/BullhornServiceImpl.java
+++ portal/portal-service-impl/impl/src/java/org/sakaiproject/portal/service/BullhornServiceImpl.java
@@ -413,7 +413,7 @@ public class BullhornServiceImpl implements BullhornService, Observer {
                 ba.setRef(ref);
                 ba.setTitle(title);
                 ba.setSiteId(siteId);
-                ba.setEventDate(eventDate);
+                ba.setEventDate(eventDate.toInstant());
                 ba.setUrl(url);
 
                 boolean deferred = false;
@@ -443,7 +443,7 @@ public class BullhornServiceImpl implements BullhornService, Observer {
                 ba.setRef(ref);
                 ba.setTitle("");
                 ba.setSiteId("");
-                ba.setEventDate(eventDate);
+                ba.setEventDate(eventDate.toInstant());
                 ba.setUrl(url);
                 ba.setDeferred(false);
 
