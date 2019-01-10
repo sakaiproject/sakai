796d6466c0b01a1d09c945a5cbfa6f577341df21
Earle Nietzel <earle.nietzel@gmail.com>
2019-01-09 23:14:42 -0500

    SAK-41112 SiteManage update references after all tool content is copied
    (cherry picked from commit 155ae3ced286da62d1b824687240bea6c1de114b)
    
     Conflicts:
    	site-manage/site-manage-api/api/src/java/org/sakaiproject/sitemanage/api/SiteManageConstants.java

--- site-manage/site-manage-api/api/src/java/org/sakaiproject/sitemanage/api/SiteManageConstants.java
+++ site-manage/site-manage-api/api/src/java/org/sakaiproject/sitemanage/api/SiteManageConstants.java
@@ -4,6 +4,7 @@ public class SiteManageConstants {
 
     public static final String SAK_PROP_IMPORT_NOTIFICATION = "site.setup.import.notification";
     public static final String SITE_INFO_TOOL_ID = "sakai.iframe.site";
+    public static final String RESOURCES_TOOL_ID = "sakai.resources";
 
     private SiteManageConstants() {
         throw new RuntimeException(this.getClass().getCanonicalName() + " is not to be instantiated");
