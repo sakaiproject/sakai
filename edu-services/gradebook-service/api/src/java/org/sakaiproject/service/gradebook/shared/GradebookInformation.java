c9d90f3b934de26ec36a0679f9a01b90779cbbca
Steve Swinsburg <steve.swinsburg@gmail.com>
2019-02-07 10:24:24 +1100

    SAK-33855 migrated to @Data

--- edu-services/gradebook-service/api/src/java/org/sakaiproject/service/gradebook/shared/GradebookInformation.java
+++ edu-services/gradebook-service/api/src/java/org/sakaiproject/service/gradebook/shared/GradebookInformation.java
@@ -20,112 +20,74 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 /**
  * Represents the settings for the gradebook
  *
  */
+@Data
 public class GradebookInformation implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Getter
-	@Setter
 	private String selectedGradingScaleUid;
 
 	/**
 	 * The ID of the GradeMapping that should be used for this gradebook
 	 */
-	@Getter
-	@Setter
 	private String selectedGradeMappingId;
 
 	/**
 	 * The list of GradeMappings defined for this gradebook but as a DTO representation
 	 */
-	@Getter
-	@Setter
 	private List<GradeMappingDefinition> gradeMappings;
 
 	/**
 	 * The grading schema map currently in use for the this gradebook. For example A+ = 100 etc.
 	 */
-	@Getter
-	@Setter
 	private Map<String, Double> selectedGradingScaleBottomPercents;
 
-	@Getter
-	@Setter
 	private boolean displayReleasedGradeItemsToStudents;
 
-	@Getter
-	@Setter
 	private int gradeType;
 
-	@Getter
-	@Setter
 	private int categoryType;
 
-	@Getter
-	@Setter
 	private List<CategoryDefinition> categories;
 
 	/**
 	 * The name of the grading scale, e.g. Pass / Not Pass
 	 */
-	@Getter
-	@Setter
 	private String gradeScale;
 
 	/**
 	 * Is the course grade to be shown at all?
 	 */
-	@Getter
-	@Setter
 	private boolean courseGradeDisplayed;
 
 	/**
 	 * If the course grade is displayed, should the letter grade be displayed?
 	 */
-	@Getter
-	@Setter
 	private boolean courseLetterGradeDisplayed;
 
 	/**
 	 * If the course grade is displayed, should the total points be displayed?
 	 */
-	@Getter
-	@Setter
 	private boolean coursePointsDisplayed;
 
 	/**
 	 * If the course grade is displayed, should the percentage be displayed?
 	 */
-	@Getter
-	@Setter
 	private boolean courseAverageDisplayed;
 
 	/**
 	 * Are assignment stats to be shown to students?
 	 */
-	@Getter
-	@Setter
 	private boolean assignmentStatsDisplayed;
 
 	/**
 	 * Are course grade stats to be shown to students?
 	 */
-	@Getter
-	@Setter
 	private boolean courseGradeStatsDisplayed;
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-	}
 
 }
