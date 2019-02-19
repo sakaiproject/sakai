c342c3b2f9a738c885c73827dfdcb4a70bc941d1
Sam Ottenhoff <ottenhoff@longsight.com>
2019-02-19 13:53:55 -0500

    SAK-40806 use Bootstrap Table plugin instead of regular table (#6551)

---
+++ library/src/webapp/editor/ckextraplugins/bt_table/lang/en.js
@@ -0,0 +1,7 @@
+"use strict"
+CKEDITOR.plugins.setLang( 'bt_table', 'en', {
+  "compactStyle": "Compact style",
+  "addBorders": "Add borders",
+  "addStripes": "Add stripes",
+  "addHover": "Add hover effect",
+});
