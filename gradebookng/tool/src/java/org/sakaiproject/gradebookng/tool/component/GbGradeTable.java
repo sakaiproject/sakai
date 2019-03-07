339550df2b3191be9cd2dd99d071466cd7b207d9
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2019-03-07 10:32:37 +0100

    SAK-41210 Instructor feedback: Suggest previous inputs (#6584)
    SAK-41210 Instructor feedback: Suggest previous inputs
    
    (cherry picked from commit d624ab0b0147f64e61eae80132424c682bc6d38f)

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbGradeTable.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbGradeTable.java
@@ -109,6 +109,8 @@ public class GbGradeTable extends Panel implements IHeaderContributor {
 
 		final String version = serverConfigService.getString("portal.cdn.version", "");
 
+		response.render(JavaScriptHeaderItem.forUrl("/library/js/sakai-reminder.js"));
+
 		response.render(
 				JavaScriptHeaderItem.forUrl(String.format("/gradebookng-tool/scripts/gradebook-gbgrade-table.js?version=%s", version)));
 
