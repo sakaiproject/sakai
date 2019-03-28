eff2ff9a309bbbc37b64649c0b244b464a941d13
josecebe <josecebe@gmail.com>
2019-03-28 14:45:14 +0100

    SAK-41572 WebJars: Upgrade Momentjs from 2.22.2 to 2.24.0 (april-2019)

--- library/src/webapp/js/headscripts.js
+++ library/src/webapp/js/headscripts.js
@@ -778,7 +778,7 @@ function includeWebjarLibrary(library) {
 		document.write('\x3Cscript type="text/javascript" src="' + webjars + 'featherlight/src/featherlight.js' + ver + '">' + '\x3C/script>');
 		document.write('\x3Clink rel="stylesheet" href="' + webjars + 'featherlight/src/featherlight.css' + ver + '"/>');
 	} else if (library == 'momentjs') {
-		libraryVersion = "2.22.2";
+		libraryVersion = "2.24.0";
 		document.write('\x3Cscript type="text/javascript" src="' + webjars + 'momentjs/' + libraryVersion + '/min/moment-with-locales.min.js' + ver + '">' + '\x3C/script>');
 	} else if (library == 'dropzone') {
 		libraryVersion = "5.5.0";
