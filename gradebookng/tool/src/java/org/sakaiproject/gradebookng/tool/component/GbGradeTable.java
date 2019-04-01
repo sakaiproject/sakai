c57293904e5e59ce70fa94d8c47456a1cea38ea1
Adrian Fish <adrian.r.fish@gmail.com>
2019-04-01 10:18:03 +0100

    SAK-41590 Add Sakai property to enable column resizing in Gradebook
    https://jira.sakaiproject.org/browse/SAK-41590

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbGradeTable.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/component/GbGradeTable.java
@@ -129,8 +129,10 @@ public class GbGradeTable extends Panel implements IHeaderContributor {
 
 		int sectionsColumnWidth = serverConfigService.getInt("gradebookng.sectionsColumnWidth", 140);
 		int studentNumberColumnWidth = serverConfigService.getInt("gradebookng.studentNumberColumnWidth", 140);
+		boolean allowColumnResizing = serverConfigService.getBoolean("gradebookng.allowColumnResizing", false);
 		StringBuilder sb = new StringBuilder();
 		sb.append("var sectionsColumnWidth = ").append(sectionsColumnWidth);
+		sb.append(", allowColumnResizing = ").append(allowColumnResizing);
 		sb.append(", studentNumberColumnWidth = ").append(studentNumberColumnWidth).append(";");
 		response.render(JavaScriptHeaderItem.forScript(sb.toString(), null));
 	}
