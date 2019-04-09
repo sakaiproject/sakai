4464f4be8812f38e85e104ea377aeb6ff888eb55
josecebe <josecebe@gmail.com>
2019-04-09 18:15:26 +0200

    SAK-41635 GB: Adding or removing mapping displays browser console errors
    (cherry picked from commit 8bc1562ec7436c9192d2aba62d283527e80f5c8a)

--- gradebookng/tool/src/webapp/scripts/gradebook-chart.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-chart.js
@@ -11,52 +11,21 @@
  * @param gbChartData the data for the chart. Must be a GbChartData object serialised to JSON.
  * @returns
  */
+
+var myChart;
+
 function renderChart(gbChartData) {
-	
 	var chartData = JSON.parse(gbChartData);
-		
-	var dataset = chartData.dataset;
+
+	var chartId = chartData.chartId;
+	var chartType = chartData.chartType;
 	var chartTitle = chartData.chartTitle;
 	var xAxisLabel = chartData.xAxisLabel;
 	var yAxisLabel = chartData.yAxisLabel;
-	var chartType = chartData.chartType;
-	var chartId = chartData.chartId;
-	var studentGradeRange = chartData.studentGradeRange;
-	var backgroundColour = [];
-
-	var data = $.map(dataset, function(value, index) {
-		return value;
-	});
-	var labels = $.map(dataset, function(value, index) {
-		return index;
-	});
-	
-	//If this chart is being viewed by a student, display the bar that 
-	//includes their mark in a different colour
-	if (studentGradeRange != null) {
-		var index = labels.indexOf(studentGradeRange);
-		for (i = 0; i < labels.length; i++) {
-			backgroundColour.push('#15597e');
-		}
-		backgroundColour[index] = '#5bc0de'; // this is the highlight colour for the student's grade
-	} else {
-		backgroundColour = '#15597e';
-	}
-	
-	//clear data. If ChartJS implements a better way, change this.
-	$('#'+chartId).replaceWith('<canvas id="'+chartId+'"></canvas>');
 
 	var ctx = $('#'+chartId);
-	var myChart = new Chart(ctx, {
+	myChart = new Chart(ctx, {
 		type: chartType,
-		data: {
-			labels: labels,
-			datasets: [{
-				data: data,
-				backgroundColor: backgroundColour,
-				borderWidth: 0
-			}]
-		},
 		options: {
 			title: {
 				display: true,
@@ -111,12 +80,12 @@ function renderChart(gbChartData) {
 					label: function() {},
 					afterTitle: function(tooltipItem) {
 						switch(chartType) {
-						case 'bar': 
+						case 'bar':
 							if (studentGradeRange != null && studentGradeRange == tooltipItem[0].xLabel) {
 								return 'Your grade';
 							}
 							break;
-						case 'horizontalBar': 
+						case 'horizontalBar':
 							if (studentGradeRange != null && studentGradeRange == tooltipItem[0].yLabel) {
 								return 'Your grade';
 							}
@@ -126,4 +95,43 @@ function renderChart(gbChartData) {
 			}
 		}
 	});
+	renderChartData(gbChartData);
+}
+
+function renderChartData(gbChartData) {
+	var chartData = JSON.parse(gbChartData);
+
+	var dataset = chartData.dataset;
+	var chartId = chartData.chartId;
+	var studentGradeRange = chartData.studentGradeRange;
+	var backgroundColour = [];
+
+	var data = $.map(dataset, function(value, index) {
+		return value;
+	});
+	var labels = $.map(dataset, function(value, index) {
+		return index;
+	});
+
+	//If this chart is being viewed by a student, display the bar that
+	//includes their mark in a different colour
+	if (studentGradeRange != null) {
+		var index = labels.indexOf(studentGradeRange);
+		for (i = 0; i < labels.length; i++) {
+			backgroundColour.push('#15597e');
+		}
+		backgroundColour[index] = '#5bc0de'; // this is the highlight colour for the student's grade
+	} else {
+		backgroundColour = '#15597e';
+	}
+
+	myChart.data = {
+		labels: labels,
+		datasets: [{
+			data: data,
+			backgroundColor: backgroundColour,
+			borderWidth: 0
+		}]
+	};
+	myChart.update();
 }
