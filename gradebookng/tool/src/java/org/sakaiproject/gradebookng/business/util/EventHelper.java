d4a2850aaae23fcca6bb3aaf4a1a9d21b4aca007
Sam Ottenhoff <ottenhoff@longsight.com>
2019-04-09 17:38:08 -0400

    lang3

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/EventHelper.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/util/EventHelper.java
@@ -1,6 +1,6 @@
 package org.sakaiproject.gradebookng.business.util;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.sakaiproject.gradebookng.business.GbEvent;
 import org.sakaiproject.gradebookng.business.GbRole;
