dcc6da857f3916146b1fb8f9f92991a9cf080ff9
David Hutchins <dnh8f@virginia.edu>
2019-06-11 16:22:56 -0400

    SAK-41868 Tweaks from codacy
    Codacy> Expressions don't expand in single quotes, use double quotes for that.
    Me> That's because I do not want them to do that.. fine,, I'll escape it... better?

--- docker/dev/lib/tomcat_entry_nogl_nosearch.sh
+++ docker/dev/lib/tomcat_entry_nogl_nosearch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i '/^common.loader\=/ s/$/,"${catalina.base}\/sakai-lib\/*.jar"/' /usr/local/tomcat/conf/catalina.properties
+sed -i '/^common.loader\=/ s/$/,"\${catalina.base}\/sakai-lib\/*.jar"/' /usr/local/tomcat/conf/catalina.properties
 mkdir -p /usr/local/sakai/properties
 ls -al /usr/local/sakai/properties
 cp /usr/local/sakai/es.properties /usr/local/sakai/properties/sakai.properties
