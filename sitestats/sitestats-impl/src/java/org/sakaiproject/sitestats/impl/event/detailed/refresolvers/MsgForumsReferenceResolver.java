d6eaa20919059d4c99ef45ebf55ef0467cac1418
plukasew <plukasew@uwo.ca>
2019-06-18 11:21:07 -0400

    SAK-41957: Forums ClassCastException when calling services from SiteStats (#7030)
    * SAK-41957: Forums ClassCastException when calling services from SiteStats
    
    * logging fixup

--- sitestats/sitestats-impl/src/java/org/sakaiproject/sitestats/impl/event/detailed/refresolvers/MsgForumsReferenceResolver.java
+++ sitestats/sitestats-impl/src/java/org/sakaiproject/sitestats/impl/event/detailed/refresolvers/MsgForumsReferenceResolver.java
@@ -82,48 +82,53 @@ public class MsgForumsReferenceResolver
 				Message msg = dfMan.getMessageById(parsedRef.itemId);
 				if (msg == null)
 				{
+					log.error("Unable to retrieve message for id: {}", parsedRef.itemId);
 					return ResolvedEventData.ERROR;
 				}
 
 				// we need the topic (and later the thread or "conversation") in order to provide necessary context around this message,
 				// and also to properly support the anonymous forums feature and check forums permissions
 				Topic tpc = msg.getTopic(); // lightweight topic, has only the id
-				if (tpc == null)
+				if (tpc == null || tpc.getId() == null)
 				{
+					log.error("Message {} has no topic.", msg.getId());
 					return ResolvedEventData.ERROR;
 				}
 
 				Optional<DiscussionTopic> dTopic = findTopic(tpc.getId(), dfMan);
-				if (dTopic.isPresent())
+				if (!dTopic.isPresent())
 				{
-					TopicData td = buildTopicData(dTopic.get(), dfMan, broker, uiPermMan);
-					if (td.deleted || !td.userPermittedToReadMsgs)
-					{
-						// we can't check permissions on the topic because the forum is required,
-						// or the current user does not have permissions to read this topic (or the topic/forum is in draft, according to the implementation)
-						return new MessageData.MessageDataTopicError(td);
-					}
-
-					// get the thread (conversation), if different from message
-					MessageData thread = null;
-					Long threadId = msg.getThreadId();
-					if (threadId != null && !msg.getId().equals(threadId))
-					{
-						Message threadMsg = dfMan.getMessageById(threadId);
-						if (threadMsg != null)
-						{
-							thread = new MessageData(td, null, threadMsg.getTitle(), threadMsg.getAuthor(), threadMsg.getCreated(), false);
-						}
-					}
-
-					if ("forums.response".equals(eventType))
-					{
-						return new MessageData(td, thread, msg.getTitle(), msg.getAuthor(), msg.getCreated(), true);
-					}
-					return new MessageData(td, thread, msg.getTitle(), msg.getAuthor(), msg.getCreated(), false);
+					log.error("Unable to find DiscussionTopic for topic id: {}", tpc.getId());
+					return ResolvedEventData.ERROR;
 				}
+
+				TopicData td = buildTopicData(dTopic.get(), dfMan, broker, uiPermMan);
+				if (td.deleted || !td.userPermittedToReadMsgs)
+				{
+					// we can't check permissions on the topic because the forum is required,
+					// or the current user does not have permissions to read this topic (or the topic/forum is in draft, according to the implementation)
+					return new MessageData.MessageDataTopicError(td);
+				}
+
+				// get the thread (conversation), if different from message
+				MessageData thread = null;
+				Long threadId = msg.getThreadId();
+				if (threadId != null && !msg.getId().equals(threadId))
+				{
+					Message threadMsg = dfMan.getMessageById(threadId);
+					if (threadMsg != null)
+					{
+						thread = new MessageData(td, null, threadMsg.getTitle(), threadMsg.getAuthor(), threadMsg.getCreated(), false);
+					}
+				}
+
+				if ("forums.response".equals(eventType))
+				{
+					return new MessageData(td, thread, msg.getTitle(), msg.getAuthor(), msg.getCreated(), true);
+				}
+				return new MessageData(td, thread, msg.getTitle(), msg.getAuthor(), msg.getCreated(), false);
 			default:
-				log.error("Invalid HierarchyLevel: " + parsedRef.level + " for ref " + ref);
+				log.error("Invalid HierarchyLevel: {} for ref {}", parsedRef.level, ref);
 				return ResolvedEventData.ERROR;
 		}
 	}
@@ -145,7 +150,7 @@ public class MsgForumsReferenceResolver
 		catch (IllegalArgumentException e)
 		{
 			// this is thrown if any of the valueOf() calls above fail, in which case the ref is malformed and cannot be resolved
-			log.warn("Unable to parse, ref is malformed: " + eventRef, e) ;
+			log.warn("Unable to parse, ref is malformed: {}", eventRef, e) ;
 			return Optional.empty();
 		}
 	}
