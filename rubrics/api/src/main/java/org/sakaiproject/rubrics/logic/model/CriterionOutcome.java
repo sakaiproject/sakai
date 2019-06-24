87f988a74c8aeb2c682222b76abb32886896165e
bgarciaentornos <bgarcia@entornosdeformacion.com>
2019-06-24 11:12:16 +0200

    SAK-41841 Allow more characters for Rubrics comments (#7041)

--- rubrics/api/src/main/java/org/sakaiproject/rubrics/logic/model/CriterionOutcome.java
+++ rubrics/api/src/main/java/org/sakaiproject/rubrics/logic/model/CriterionOutcome.java
@@ -29,6 +29,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -68,6 +69,7 @@ public class CriterionOutcome implements Serializable {
         @NonNull
         private Integer points;
 
+        @Lob
         private String comments;
 
 }
