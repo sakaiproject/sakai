3f8ed6662d379746612467cb7ec6376ca06c045b
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2019-07-24 09:23:48 +0200

    SAK-41909 - Jun19 Upgrade libraries and dependencies (#6995)

--- emailtemplateservice/impl/logic/src/java/org/sakaiproject/emailtemplateservice/service/impl/EmailTemplateServiceImpl.java
+++ emailtemplateservice/impl/logic/src/java/org/sakaiproject/emailtemplateservice/service/impl/EmailTemplateServiceImpl.java
@@ -46,10 +46,10 @@ import org.apache.commons.lang3.LocaleUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import org.simpleframework.xml.core.Persister;
 import org.springframework.dao.DataIntegrityViolationException;
--- sections/sections-app/src/test/org/sakaiproject/test/section/SectionsTestBase.java
+++ sections/sections-app/src/test/org/sakaiproject/test/section/SectionsTestBase.java
@@ -35,7 +35,3 @@ public class SectionsTestBase extends AbstractTransactionalSpringContextTests {
     }
 
 }
-
-
-
-
