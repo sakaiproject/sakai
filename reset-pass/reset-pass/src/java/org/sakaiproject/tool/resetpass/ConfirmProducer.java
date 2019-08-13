51ec23ff0afceaf90def08d856a8c18d0fb1cc02
Brian Jones <bjones86@uwo.ca>
2019-08-13 14:39:30 -0400

    SAK-42333
    (cherry picked from commit 272a96e7d26c7baadbcd34c432a8e79ea59fec60)
    
     Conflicts:
    	reset-pass/reset-pass/src/bundle/org/sakaiproject/tool/resetpass/bundle/Messages.properties
    	reset-pass/reset-pass/src/bundle/org/sakaiproject/tool/resetpass/bundle/Messages_eu.properties
    	reset-pass/reset-pass/src/bundle/org/sakaiproject/tool/resetpass/bundle/Messages_tr_TR.properties
    	reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
    	reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java

--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/ConfirmProducer.java
@@ -15,7 +15,7 @@
  */
 package org.sakaiproject.tool.resetpass;
 
-
+import org.apache.commons.lang3.StringUtils;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.tool.api.Placement;
 import org.sakaiproject.tool.api.ToolManager;
@@ -30,11 +30,12 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 
 public class ConfirmProducer implements ViewComponentProducer {
 	public static final String VIEW_ID = "confirm";
+
 	private ToolManager toolManager;
 	public void setToolManager(ToolManager toolManager) {
 		this.toolManager = toolManager;
 	}
-	
+
 	public String getViewID() {
 		return VIEW_ID;
 	}
@@ -43,32 +44,31 @@ public class ConfirmProducer implements ViewComponentProducer {
 	public void setServerConfigurationService(ServerConfigurationService s) {
 		this.serverConfigurationService = s;
 	}
-	
+
 	private RetUser userBean;
 	public void setUserBean(RetUser u){
 		this.userBean = u;
 	}
-	
-	public void fillComponents(UIContainer tofill, ViewParameters arg1,
-			ComponentChecker arg2) {
-		
-		String[] parms = new String[] {userBean.getEmail()};
+
+	public void fillComponents(UIContainer tofill, ViewParameters arg1, ComponentChecker arg2) {
+
 		boolean validatingAccounts = serverConfigurationService.getBoolean("siteManage.validateNewUsers", true);
 		
 		if (!validatingAccounts) {
-			UIMessage.make(tofill,"message","confirm",parms);
+			UIMessage.make(tofill,"message", "confirm", new String[] {userBean.getEmail()});
 		} else {
-			UIMessage.make(tofill,"message","confirm.validate",parms);
+			UIMessage.make(tofill,"message", "confirm.validate", new String[] {serverConfigurationService.getString("ui.service", "Sakai"), userBean.getEmail()});
 		}
+
 		// Get the instructions from the tool placement.
 		Placement placement = toolManager.getCurrentPlacement();
 		String supportInstructions = placement == null ? "" :  placement.getConfig().getProperty("supportInstructions");
-		if(supportInstructions != null && !"".equals(supportInstructions)){
+		if(StringUtils.isNotBlank(supportInstructions)){
 			UIVerbatim.make(tofill, "supportMessage", supportInstructions);
 		}else if (serverConfigurationService.getString("mail.support", null) != null) {
+			String supportEmail = serverConfigurationService.getString("mail.support", "");
 			UIMessage.make(tofill, "supportMessage", "supportMessage");
-			UILink.make(tofill, "supportEmail",serverConfigurationService.getString("mail.support", ""),"mailto:" + serverConfigurationService.getString("mail.support", ""));
+			UILink.make(tofill, "supportEmail", supportEmail, "mailto:" + supportEmail);
 		}
 	}
-
 }
--- reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
+++ reset-pass/reset-pass/src/java/org/sakaiproject/tool/resetpass/UserValidator.java
@@ -21,7 +21,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -29,42 +28,31 @@ import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.user.api.User;
 import org.sakaiproject.user.api.UserDirectoryService;
 import org.sakaiproject.authz.api.SecurityService;
-import org.sakaiproject.tool.api.Placement;
-import org.sakaiproject.tool.api.ToolManager;
 
 @Slf4j
 public class UserValidator implements Validator {
 
-	// prefix for error messages - indicates they are to be pulled from tool configuration rather than a resource bundle
-	private final String TOOL_CONFIG_PREFIX = "toolconfig_";
-	
+	public String userEmail;
+
 	public boolean supports(Class clazz) {
 		return clazz.equals(User.class);
 	}
 
-	public String userEmail;
-	
-	
 	private ServerConfigurationService serverConfigurationService;
 	public void setServerConfigurationService(ServerConfigurationService s) {
 		this.serverConfigurationService = s;
 	}
-	
+
 	private UserDirectoryService userDirectoryService;
 	public void setUserDirectoryService(UserDirectoryService ds){
 		this.userDirectoryService = ds;
 	}
-	
+
 	private SecurityService securityService;
 	public void setSecurityService(SecurityService ss){
 		this.securityService = ss;
 	}
 
-	private ToolManager toolManager;
-	public void setToolManager(ToolManager tm) {
-		this.toolManager = tm;
-	}
-	
 	public void validate(Object obj, Errors errors) {
 		RetUser retUser = (RetUser)obj;
 		log.debug("validating user " + retUser.getEmail());
@@ -78,22 +66,27 @@ public class UserValidator implements Validator {
 		
 		Collection<User> c = this.userDirectoryService.findUsersByEmail(retUser.getEmail().trim());
 		if (c.size()>1) {
-			log.debug("more than one email!");
-			errors.reject("morethanone","more than one email");
+			// Email is tied to more than one user, null out the user and transfer to next page
+			log.warn("more than one account with email: {}", retUser.getEmail());
+			retUser.setUser(null);
 			return;
 		} else if (c.size()==0) {
-			log.debug("no such email");
-			errors.reject("nosuchuser","no such user");
+			// User doesn't exist, null out the user and transfer to next page
+			log.debug("no such email: {}", retUser.getEmail());
+			retUser.setUser(null);
 			return;
 		}
+
 		Iterator<User> i = c.iterator();
 		User user = (User)i.next();
 		log.debug("got user " + user.getId() + " of type " + user.getType());
 		if (securityService.isSuperUser(user.getId())) {
+			// Email belongs to super user, null out the user and transfer to next page
 			log.warn("tryng to change superuser password");
-			rejectWrongType(errors);
+			retUser.setUser(null);
 			return;
 		}
+
 		boolean allroles = serverConfigurationService.getBoolean("resetPass.resetAllRoles",false);
 		if (!allroles){
 			// SAK-24379 - deprecate the resetRoles property
@@ -107,36 +100,18 @@ public class UserValidator implements Validator {
 					roles = rolesOld;
 				}
 			}
-		    if (roles == null ){
-		        roles = new String[]{"guest"};
-		    }
-		    List<String> rolesL = Arrays.asList(roles);
-		    if (!rolesL.contains(user.getType())) {
-		        log.warn("this is a type don't change");
-		        rejectWrongType(errors);
-		        return;
-		    }
+			if (roles == null ){
+				roles = new String[]{"guest"};
+			}
+			List<String> rolesL = Arrays.asList(roles);
+			if (!rolesL.contains(user.getType())) {
+				// Email belongs to a user who's type is not allowed to use this tool, null out the user and transfer to the next page
+				log.warn("this is a user type which isn't allowed to reset password via tool");
+				retUser.setUser(null);
+				return;
+			}
 		}
+
 		retUser.setUser(user);
 	}
-
-	/**
-	 * Explains that the user's type is incorrect.
-	 * Looks for a custom message in the tool properties first,
-	 * if there is no custom message, it goes to the message bundle
-	 */
-	private void rejectWrongType(Errors errors)
-	{
-		Placement placement = toolManager.getCurrentPlacement();
-		String toolPropWrongType = placement.getConfig().getProperty("wrongtype");
-		if (StringUtils.isBlank(toolPropWrongType))
-		{
-			errors.reject("wrongtype", "wrong type");
-		}
-		else
-		{
-			errors.reject(TOOL_CONFIG_PREFIX + "wrongtype", toolPropWrongType);
-		}
-	}
-
 }
