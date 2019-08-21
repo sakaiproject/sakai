0309ab88096453495f7a0cd8b4c96b6b1d3869a0
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2019-08-21 03:47:14 -0400

    Add translation for rubric criterion titles (SAK-41965) (#7260)
    * Add translation for rubric criterion titles
    
    * Add point_value string to messages

--- rubrics/tool/src/main/frontend/webcomponents/sakai-rubric-criterion-readonly.js
+++ rubrics/tool/src/main/frontend/webcomponents/sakai-rubric-criterion-readonly.js
@@ -1,5 +1,6 @@
 import {SakaiElement} from "/webcomponents/sakai-element.js";
 import {html} from "/webcomponents/assets/lit-element/lit-element.js";
+import {tr} from "./sakai-rubrics-language.js";
 
 export class SakaiRubricCriterionReadonly extends SakaiElement {
 
@@ -37,7 +38,7 @@ export class SakaiRubricCriterionReadonly extends SakaiElement {
                 <div class="cr-table">
                   <div class="cr-table-row">
                     ${c.ratings.map(r => html`
-                      <div tabindex="0" title$="Rating Title: ${r.title}. Rating Description: ${r.description}. Point Value: ${r.points}" class="rating-item" id="rating_item_${r.id}" on-save-ratings="saveRatings" @on-delete-rating="${this.deleteCriterionRating}">
+                      <div tabindex="0" title$="${tr("rating_title")}: ${r.title}. ${tr("rating_description")}: ${r.description}. ${tr("point_value")}: ${r.points}" class="rating-item" id="rating_item_${r.id}" on-save-ratings="saveRatings" @on-delete-rating="${this.deleteCriterionRating}">
                         <h5 class="criterion-item-title">
                           ${r.title}
                         </h5>
