8cea9ee131a623c7008b23f6b618e4e21ef5e53d
Earle Nietzel <earle.nietzel@gmail.com>
2019-08-21 03:47:39 -0400

    SAK-40083 Site Manage refresh site after importing content (#7262)
    (cherry picked from commit 4484a05a53ae8e8816e1d24e1ccc68f9e4dc3f44)
    
     Conflicts:
    	site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -11338,21 +11338,17 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 			state.removeAttribute(STATE_TOOL_EMAIL_ADDRESS);
 		}
 
-		// commit
 		commitSite(site);
 		
+		siteManageService.importToolsIntoSite(site, chosenList, importTools, false);
+
+		// after importing content we need to refresh the site
 		site = refreshSiteObject(site);
 
-		// import
-		siteManageService.importToolsIntoSite(site, chosenList, importTools, false);
-		
-		// SAK-22384 add LaTeX (MathJax) support
-		if (MathJaxEnabler.prepareMathJaxToolSettingsForSave(site, state))
-		{
-			commitSite(site);
-		}
-
-		if (LessonsSubnavEnabler.prepareSiteForSave(site, state)) {
+		boolean updateSite;
+		updateSite = MathJaxEnabler.prepareMathJaxToolSettingsForSave(site, state);
+		updateSite = LessonsSubnavEnabler.prepareSiteForSave(site, state) || updateSite;
+		if (updateSite) {
 			commitSite(site);
 		}
 	} // saveFeatures
