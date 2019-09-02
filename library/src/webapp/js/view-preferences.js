e0d0f9699aef5a46205f85377d488745d67dbcfe
Adrian Fish <adrian.r.fish@gmail.com>
2019-09-02 10:54:33 +0100

    SAK-33439 Persist gradebook item column choices
    https://jira.sakaiproject.org/browse/SAK-33439
    
    Persist hidden gradebook item columns in session storage and on the
    server as a user preference. That way you can move between machines
    and keep your item choices.

---
+++ library/src/webapp/js/view-preferences.js
@@ -0,0 +1,22 @@
+function getViewPreferences(tool) {
+
+  let url = `/direct/userPrefs/key/${portal.user.id}/viewpreferences.json`;
+
+  return new Promise((resolve, reject) => {
+
+    fetch(url, { cache: "no-cache", headers: { "Content-Type": "application/json" },})
+    .then(d => d.json())
+    .then(json => resolve(decodeURIComponent(json.data[tool])))
+    .catch(error => { console.error(error); reject(); });
+  });
+}
+
+function updateViewPreferences(tool, value) {
+
+  let url = `/direct/userPrefs/updateKey/${portal.user.id}/viewpreferences?${tool}=${encodeURIComponent(value)}`;
+  fetch(url, { method: "PUT", cache: "no-cache" })
+  .catch(error => console.error(
+                    `Failed to update view preferences for tool '${tool}'. Take a look at the server logs?`));
+}
+
+export {getViewPreferences, updateViewPreferences};
