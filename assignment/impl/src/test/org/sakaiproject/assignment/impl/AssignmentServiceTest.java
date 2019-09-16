812f3beaa9b18a44b79970a4f658630437aa765d
Earle Nietzel <earle@longsight.com>
2019-09-16 10:03:26 -0400

    SAK-41275 Assignment remove NonUniqueResultException from submission (#7345)
    retriveal
    
    (cherry picked from commit 66cece7ed231d604232e10aaf9ecb358041515b2)

--- assignment/impl/src/test/org/sakaiproject/assignment/impl/AssignmentServiceTest.java
+++ assignment/impl/src/test/org/sakaiproject/assignment/impl/AssignmentServiceTest.java
@@ -518,6 +518,7 @@ public class AssignmentServiceTest extends AbstractTransactionalJUnit4SpringCont
 
         try {
             AssignmentSubmission fetchSubmission = assignmentService.getSubmission(assignment.getId(), submitterId);
+            Assert.assertNotNull(fetchSubmission);
             Assert.assertEquals(submission.getId(), fetchSubmission.getId());
             Assert.assertTrue(fetchSubmission.getUserSubmission());
         } catch (Exception e) {
@@ -534,10 +535,11 @@ public class AssignmentServiceTest extends AbstractTransactionalJUnit4SpringCont
         }
 
         try {
-            assignmentService.getSubmission(assignment.getId(), submitterId);
-            Assert.fail("NonUniqueResultException expected");
+            AssignmentSubmission finalSubmission = assignmentService.getSubmission(assignment.getId(), submitterId);
+            Assert.assertNotNull(finalSubmission);
+            Assert.assertEquals(submission.getId(), finalSubmission.getId());
         } catch (Exception e) {
-            Assert.assertEquals(NonUniqueResultException.class, e.getClass());
+            Assert.fail("No exception should be thrown\n" + e.toString());
         }
     }
 
