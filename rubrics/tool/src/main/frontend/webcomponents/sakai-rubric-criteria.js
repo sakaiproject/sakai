7cf08f5a43097e0feb18f7f4cfeefeeb6868d20d
ajordanedf <52909897+ajordanedf@users.noreply.github.com>
2019-09-26 18:02:14 +0200

    SAK-40190: Rubric rows - even when the same - are not lined up (#7298)

--- rubrics/tool/src/main/frontend/webcomponents/sakai-rubric-criteria.js
+++ rubrics/tool/src/main/frontend/webcomponents/sakai-rubric-criteria.js
@@ -70,9 +70,11 @@ export class SakaiRubricCriteria extends RubricsElement {
                   ${r.title}
                   <sakai-rubric-criterion-rating-edit criterion-id="${c.id}" @save-rating="${this.saveRating}" @delete-rating="${this.deleteRating}" minpoints="${c.pointrange ? c.pointrange.low : 0}" maxpoints="${c.pointrange ? c.pointrange.high : 0}" rating="${JSON.stringify(r)}"></sakai-rubric-criterion-rating-edit>
                 </h5>
-                <p>
+                <div class="div-description">
+                  <p>
                   ${r.description}
-                </p>
+                  </p>
+                </div>
                 <span class="points">
                   ${r.points} <sr-lang key="points">Points</sr-lang>
                 </span>
