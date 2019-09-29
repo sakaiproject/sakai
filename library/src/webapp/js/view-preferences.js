8c7359c18acab048f97b8e359d380c6d8285e58b
Adrian Fish <adrian.r.fish@gmail.com>
2019-09-29 12:09:43 +0100

    SAK-42552 Batched preference updates
    https://jira.sakaiproject.org/browse/SAK-42552
    
    Also, removed mutex from backend service. Properties are now checked
    for currency, before obtaining a edit lock.

--- library/src/webapp/js/view-preferences.js
+++ library/src/webapp/js/view-preferences.js
@@ -1,17 +1,29 @@
-function getViewPreferences(tool) {
+function getViewPreferences(tool, options) {
+
+  if (!options) options = {debug: false};
 
   let url = `/direct/userPrefs/key/${portal.user.id}/viewpreferences.json`;
 
+  if (options.debug) {
+    console.debug(`getViewPreferences: url=${url}`);
+  }
+
   return new Promise((resolve, reject) => {
 
-    fetch(url, { cache: "no-cache", headers: { "Content-Type": "application/json" },})
+    fetch(url, { cache: "no-store", headers: { "Content-Type": "application/json" },})
     .then(d => d.json())
     .then(json => {
 
       var preferencesString = decodeURIComponent(json.data[tool]);
       if (preferencesString && preferencesString !== "undefined") {
+        if (options.debug) {
+          console.debug(`getViewPreferences: resolving with "${preferencesString}"`);
+        }
         resolve(preferencesString);
       } else {
+        if (options.debug) {
+          console.debug("getViewPreferences: resolving with null");
+        }
         resolve(null);
       }
     })
@@ -19,10 +31,18 @@ function getViewPreferences(tool) {
   });
 }
 
-function updateViewPreferences(tool, value) {
+function updateViewPreferences(tool, value, options) {
+
+  if (!options) options = {debug: false};
 
   let url = `/direct/userPrefs/updateKey/${portal.user.id}/viewpreferences?${tool}=${encodeURIComponent(value)}`;
-  fetch(url, { method: "PUT", cache: "no-cache" })
+
+  if (options.debug) {
+    console.debug(`updateViewPreferences: url=${url}`);
+    console.debug(`updateViewPreference: value=${value}`);
+  }
+
+  fetch(url, { method: "PUT", cache: "no-store" })
   .catch(error => console.error(
                     `Failed to update view preferences for tool '${tool}'. Take a look at the server logs?`));
 }
