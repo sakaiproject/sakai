716f242f599387793e0a418d1dec424deca12be5
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2019-10-17 20:53:04 +0200

    SAK-42634 - Assignments download bulk: Escape question mark symbols (#7440)
    (cherry picked from commit bd31db861b226f8258605c97bd71859093c04a35)

--- assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
+++ assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
@@ -2269,7 +2269,9 @@ public class AssignmentServiceImpl implements AssignmentService, EntityTransferr
     @Override
     public String escapeInvalidCharsEntry(String accentedString) {
         String decomposed = Normalizer.normalize(accentedString, Normalizer.Form.NFD);
-        return decomposed.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+        decomposed = decomposed.replaceAll("\\p{InCombiningDiacriticalMarks}+", StringUtils.EMPTY);
+        decomposed = decomposed.replaceAll("\\?", StringUtils.EMPTY);
+        return decomposed;
     }
 
     @Override
