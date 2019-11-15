661b7119815c868f5222ff85c7a0f6e95a060f02
bgarciaentornos <bgarcia@entornosdeformacion.com>
2019-11-15 15:44:06 +0100

    SAK-42761 NPE when grading a Forum (#7544)
    (cherry picked from commit 4412a3d82fda6d96cf28a412fd910fcdf71702f8)

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -6146,20 +6146,24 @@ public class DiscussionForumTool
         
     String eventRef = "";
     String evaluatedItemId = "";
+    String evaluatedItemTitle = "";
     if(selectedMessage != null){
-    	eventRef = getEventReference(selectedMessage.getMessage());
-    	evaluatedItemId = studentUid+"."+selectedMessage.getMessage().getUuid();
+        eventRef = getEventReference(selectedMessage.getMessage());
+        evaluatedItemId = studentUid+"."+selectedMessage.getMessage().getUuid();
+        evaluatedItemTitle = selectedMessage.getMessage().getTitle();
     }else if(selectedTopic != null){
-    	eventRef = getEventReference(selectedTopic.getTopic());
-    	evaluatedItemId = studentUid+"."+selectedTopic.getTopic().getUuid();
+        eventRef = getEventReference(selectedTopic.getTopic());
+        evaluatedItemId = studentUid+"."+selectedTopic.getTopic().getUuid();
+        evaluatedItemTitle = selectedTopic.getTopic().getTitle();
     }else if(selectedForum != null){
-    	eventRef = getEventReference(selectedForum.getForum());
-    	evaluatedItemId = studentUid+"."+selectedForum.getForum().getUuid();
+        eventRef = getEventReference(selectedForum.getForum());
+        evaluatedItemId = studentUid+"."+selectedForum.getForum().getUuid();
+        evaluatedItemTitle = selectedForum.getForum().getTitle();
     }
     if(rubricsService.hasAssociatedRubric(RubricsConstants.RBCS_TOOL_FORUMS, getRubricAssociationId())){
     	rubricsService.saveRubricEvaluation(RubricsConstants.RBCS_TOOL_FORUMS, getRubricAssociationId(), evaluatedItemId, studentUid, getUserId(), getRubricConfigurationParameters());
     }
-    LRS_Statement statement = forumManager.getStatementForGrade(studentUid, selectedTopic.getTopic().getTitle(), gradeAsDouble).orElse(null);
+    LRS_Statement statement = forumManager.getStatementForGrade(studentUid, evaluatedItemTitle, gradeAsDouble).orElse(null);
     Event event = eventTrackingService.newEvent(DiscussionForumService.EVENT_FORUMS_GRADE, eventRef, null, true, NotificationService.NOTI_OPTIONAL, statement);
     eventTrackingService.post(event);
 
