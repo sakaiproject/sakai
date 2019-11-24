26370bcd1e1a6e6734977681657c3c5d27605ead
Matthew Jones <jonespm@gmail.com>
2019-11-23 19:01:46 -0500

    Changing restore to update the modified date

--- samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentFacadeQueries.java
+++ samigo/samigo-services/src/java/org/sakaiproject/tool/assessment/facade/AssessmentFacadeQueries.java
@@ -2403,13 +2403,21 @@ public class AssessmentFacadeQueries extends HibernateDaoSupport implements Asse
     }
 
     public void restoreAssessment(Long assessmentId) {
-        final String recoverQuery = "update AssessmentData set status = :status WHERE assessmentBaseId = :id";
-        getHibernateTemplate().execute(session -> {
-            Query q = session.createQuery(recoverQuery);
-            q.setInteger("status", AssessmentIfc.ACTIVE_STATUS);
-            q.setLong("id", assessmentId);
-            return q.executeUpdate();
-        });
+    	AssessmentData assessment = (AssessmentData) getHibernateTemplate().load(AssessmentData.class, assessmentId);
+    	assessment.setLastModifiedBy(AgentFacade.getAgentString());
+    	assessment.setLastModifiedDate(new Date());
+    	assessment.setStatus(AssessmentIfc.ACTIVE_STATUS);
+    	int retryCount = PersistenceService.getInstance().getPersistenceHelper().getRetryCount();
+    	while (retryCount > 0) {
+    		try {
+    			getHibernateTemplate().update(assessment);
+    			retryCount = 0;
+    		} catch (Exception e) {
+    			log.warn("problem updating asssessment: " + e.getMessage());
+    			retryCount = PersistenceService.getInstance().getPersistenceHelper()
+    					.retryDeadlock(e, retryCount);
+    		}
+    	}
     }
 
 }
