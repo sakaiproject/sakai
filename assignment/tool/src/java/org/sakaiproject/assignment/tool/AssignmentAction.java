fc39880a3cf69d93caa1b6626e7e2545763644c4
austin48 <austin48@hawaii.edu>
2019-11-25 13:04:50 -1000

    SAK-42808 Exception adding assignment with existing grades to gradebook

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -5028,7 +5028,7 @@ public class AssignmentAction extends PagedResourceActionII {
                                     }
                                 } else if (isExternalAssignmentDefined) {
                                     gradebookExternalAssessmentService.updateExternalAssessmentScoresString(gradebookUid, assignmentRef, sm);
-                                    gradebookExternalAssessmentService.updateExternalAssessmentComments(gradebookUid, associateGradebookAssignment, cm);
+                                    gradebookExternalAssessmentService.updateExternalAssessmentComments(gradebookUid, assignmentRef, cm);
                                 }
                             }
                         } else {
