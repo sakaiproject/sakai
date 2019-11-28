a1c69972305eacc92c2cd03e1e10ef63f7c41d68
Curtis van Osch <curtis.van-osch@hec.ca>
2019-11-28 10:25:19 -0500

    SAK-42721: check that assignment url is not empty string

--- portal/portal-api/api/src/java/org/sakaiproject/portal/beans/bullhornhandlers/GradeAssignmentBullhornHandler.java
+++ portal/portal-api/api/src/java/org/sakaiproject/portal/beans/bullhornhandlers/GradeAssignmentBullhornHandler.java
@@ -70,7 +70,10 @@ public class GradeAssignmentBullhornHandler extends AbstractBullhornHandler {
                 List<BullhornData> bhEvents = new ArrayList<>();
                 submission.getSubmitters().forEach(to -> {
                     try {
-                        bhEvents.add(new BullhornData(from, to.getSubmitter(), siteId, title, assignmentService.getDeepLink(siteId, assignment.getId(), to.getSubmitter())));
+                        String url = assignmentService.getDeepLink(siteId, assignment.getId(), to.getSubmitter());
+                        if (!url.isEmpty()) {
+                            bhEvents.add(new BullhornData(from, to.getSubmitter(), siteId, title, url));
+                        }
                         countCache.remove(to.getSubmitter());
                     } catch(Exception exc) {
                         log.error("Error retrieving deep link for assignment {} and user {} on site {}", assignment.getId(), to.getSubmitter(), siteId, exc);
