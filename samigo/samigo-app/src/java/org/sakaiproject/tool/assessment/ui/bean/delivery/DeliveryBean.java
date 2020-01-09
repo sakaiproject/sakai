9d08c20666994c4ee1a056a33f14c7e5edbe2285
bgarciaentornos <bgarcia@entornosdeformacion.com>
2020-01-09 12:38:05 +0100

    SAK-42966 T&Q : preview draft not working (#7747)
    (cherry picked from commit 63e0ee5c5846010492779f2fe523b33b972f4039)
    
     Conflicts:
    	samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/DeliveryBean.java

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/DeliveryBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/DeliveryBean.java
@@ -4076,9 +4076,12 @@ public class DeliveryBean
 		  return firstTimeTaking;
 	  }
 	  //SAM-2517
-	  public boolean getIsMathJaxEnabled(){ 
-		  PublishedAssessmentService publishedAssessmentService = new PublishedAssessmentService();
-		  String siteId = publishedAssessmentService.getPublishedAssessmentOwner(Long.parseLong(getAssessmentId()));
+	  public boolean getIsMathJaxEnabled(){
+		  String siteId = AgentFacade.getCurrentSiteId();
+		  if(siteId == null) {
+		    PublishedAssessmentService publishedAssessmentService = new PublishedAssessmentService();
+		    siteId = publishedAssessmentService.getPublishedAssessmentOwner(Long.parseLong(getAssessmentId()));
+		  }
 		  return Boolean.parseBoolean(getCurrentSite(siteId).getProperties().getProperty(Site.PROP_SITE_MATHJAX_ALLOWED));
 	  }
 	  public String getMathJaxHeader(){
