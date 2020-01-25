d070e4b347c01d79e74328c9cae51cad1a2ab82b
Earle Nietzel <earle@longsight.com>
2020-01-25 15:44:05 -0500

    SAK-41390 Add Spring jCache

--- kernel/kernel-impl/src/main/java/org/sakaiproject/ignite/IgniteConfigurationAdapter.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/ignite/IgniteConfigurationAdapter.java
@@ -1,33 +1,41 @@
 package org.sakaiproject.ignite;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.logger.slf4j.Slf4jLogger;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.util.BasicConfigItem;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfiguration> {
+
+    private static final IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
+    private static Boolean configured = Boolean.FALSE;
 
     @Setter private ServerConfigurationService serverConfigurationService;
     @Setter private CacheConfiguration[] cacheConfiguration;
 
-    @Getter @Setter private String node;
-    @Getter @Setter private String name;
+    @Getter @Setter private String address;
     @Getter @Setter private String home;
+    @Getter @Setter private String[] remoteAddresses;
+    @Getter @Setter private int port;
+    @Getter @Setter private int range;
     @Getter @Setter private String mode;
-
-    public void init() {
-        node = serverConfigurationService.getString("ignite.node", serverConfigurationService.getServerId());
-        name = serverConfigurationService.getString("ignite.name", name);
-        home = serverConfigurationService.getString("ignite.home", home);
-        mode = serverConfigurationService.getString("ignite.mode", mode);
-    }
+    @Getter @Setter private String name;
+    @Getter @Setter private String node;
 
     @Override
     public Class<?> getObjectType() {
@@ -35,32 +43,79 @@ public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfig
     }
 
     @Override
-    protected IgniteConfiguration createInstance() throws Exception {
-        configureHome();
+    protected IgniteConfiguration createInstance() {
+        if (!configured) {
+            address = serverConfigurationService.getString("ignite.address");
+            home = serverConfigurationService.getString("ignite.home", serverConfigurationService.getSakaiHomePath());
+            remoteAddresses = serverConfigurationService.getStrings("ignite.addresses");
+            port = serverConfigurationService.getInt("ignite.port", 49000);
+            range = serverConfigurationService.getInt("ignite.range", 10);
+            mode = serverConfigurationService.getString("ignite.mode", "worker");
+            name = serverConfigurationService.getServerName();
+            node = serverConfigurationService.getServerId();
 
-        IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
-        igniteConfiguration.setIgniteHome(home);
-        igniteConfiguration.setConsistentId(node);
-        igniteConfiguration.setIgniteInstanceName(name);
+            // disable banner
+            System.setProperty("IGNITE_NO_ASCII", "true");
+            System.setProperty("IGNITE_QUIET", "true");
+            System.setProperty("IGNITE_PERFORMANCE_SUGGESTIONS_DISABLED", "true");
 
-        if (StringUtils.equalsIgnoreCase("client", mode)) {
-            igniteConfiguration.setClientMode(true);
-        } else {
-            igniteConfiguration.setClientMode(false);
+            igniteConfiguration.setIgniteHome(configureHome(home, node));
+            igniteConfiguration.setConsistentId(node);
+            igniteConfiguration.setIgniteInstanceName(name);
+
+            if (StringUtils.equalsIgnoreCase("client", mode)) {
+                igniteConfiguration.setClientMode(true);
+            } else {
+                igniteConfiguration.setClientMode(false);
+            }
+
+            igniteConfiguration.setGridLogger(new Slf4jLogger());
+
+            igniteConfiguration.setCacheConfiguration(cacheConfiguration);
+
+            // local node network configuration
+            TcpCommunicationSpi tcpCommunication = new TcpCommunicationSpi();
+            if (StringUtils.isNotBlank(address)) tcpCommunication.setLocalAddress(address);
+            tcpCommunication.setLocalPort(port);
+            tcpCommunication.setLocalPortRange(range);
+            igniteConfiguration.setCommunicationSpi(tcpCommunication);
+
+            // remote node network configuration, 1.2.3.5:49000..49009
+            if (remoteAddresses != null) {
+                TcpDiscoverySpi discovery = new TcpDiscoverySpi();
+                TcpDiscoveryVmIpFinder finder = new TcpDiscoveryVmIpFinder();
+                finder.setAddresses(Arrays.asList(remoteAddresses));
+                discovery.setIpFinder(finder);
+                igniteConfiguration.setDiscoverySpi(discovery);
+            }
+
+            log.info("Ignite configured with home=[{}], node=[{}], name=[{}], client mode=[{}]",
+                    igniteConfiguration.getIgniteHome(),
+                    igniteConfiguration.getConsistentId(),
+                    igniteConfiguration.getIgniteInstanceName(),
+                    igniteConfiguration.isClientMode());
+
+            configured = Boolean.TRUE;
         }
-
-        igniteConfiguration.setGridLogger(new Slf4jLogger());
-
-        igniteConfiguration.setCacheConfiguration(cacheConfiguration);
-
         return igniteConfiguration;
     }
 
-    private void configureHome() {
-        File igniteHome = new File(home);
-        if (!igniteHome.exists()) igniteHome.mkdir();
+    private String configureHome(String path, String node) {
+        if (StringUtils.isBlank(path)) {
+            path = serverConfigurationService.getSakaiHomePath();
+        }
 
-        // update home with the absolute path
-        setHome(igniteHome.getAbsolutePath());
+        if (StringUtils.isNotBlank(node)) {
+            if (!StringUtils.endsWith(path, File.separator)) {
+                path = path + File.separator;
+            }
+            path = path + node;
+        }
+
+        File igniteHome = new File(path);
+        if (!igniteHome.exists()) igniteHome.mkdirs();
+
+        // return the absolute path
+        return igniteHome.getAbsolutePath();
     }
 }
