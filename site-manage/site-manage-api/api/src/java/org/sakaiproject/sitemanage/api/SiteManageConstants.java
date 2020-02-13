928d76b78a29e383295b4e3d867cf53ddbf633cc
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2020-02-13 10:43:27 +0100

    SAK-46072 RSF Deprecation: Remove deprecated RSF group helpers and cleanup properties

--- site-manage/site-manage-api/api/src/java/org/sakaiproject/sitemanage/api/SiteManageConstants.java
+++ site-manage/site-manage-api/api/src/java/org/sakaiproject/sitemanage/api/SiteManageConstants.java
@@ -5,8 +5,6 @@ public class SiteManageConstants {
     public static final String SAK_PROP_IMPORT_NOTIFICATION = "site.setup.import.notification";
     public static final String SITE_INFO_TOOL_ID = "sakai.iframe.site";
     public static final String RESOURCES_TOOL_ID = "sakai.resources";
-    // SAK-34034 enable/disable the group filter
-    public static final String PROP_SITEINFO_GROUP_FILTER_ENABLED = "siteinfo.group.filter.enabled";
 
     private SiteManageConstants() {
         throw new RuntimeException(this.getClass().getCanonicalName() + " is not to be instantiated");
