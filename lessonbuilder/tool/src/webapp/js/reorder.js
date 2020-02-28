e56faab952b83ef1baad58996baebfc25b2daf0b
David Bauer <dbauer1@udayton.edu>
2020-02-28 16:29:47 -0500

    SAKAI-4955 Fix lessons reorder Adds back the old reorder we used in sakai12. This should be fixed.

--- lessonbuilder/tool/src/webapp/js/reorder.js
+++ lessonbuilder/tool/src/webapp/js/reorder.js
@@ -1,40 +1,52 @@
+(function($, fluid){
+    //fluid.setLogging(true);
+    initlayoutReorderer = function(){
+        fluid.reorderLayout("#layoutReorderer", {
+            listeners: {
+                afterMove: function(args){
+                    recalculate();
+                }
+            },
+            styles: {
+                defaultStyle: "layoutReorderer-movable-default",
+                selected: "layoutReorderer-movable-selected",
+                dragging: "layoutReorderer-movable-dragging",
+                mouseDrag: "layoutReorderer-movable-mousedrag",
+                dropMarker: "layoutReorderer-dropMarker",
+                avatar: "layoutReorderer-avatar"
+            },
+            disableWrap: true,
+            containerRole: fluid.reorderer.roles.LIST
+        });
+    };
+})(jQuery, fluid);
+
 var recalculate = function(){
     var keepList = '';
     var removeList = '';
-    $('.col1 .layoutReorderer-module').each(function(i){
+    jQuery('.col1 .layoutReorderer-module').each(function(i){
         i > 0 ? keepList = keepList + ' ' + $(this).find('.reorderSeq').text() : keepList = $(this).find('.reorderSeq').text();
     });
-    $('.col2 .layoutReorderer-module').each(function(i){
+    jQuery('.col2 .layoutReorderer-module').each(function(i){
         i > 0 ? removeList = removeList + ' ' + $(this).find('.reorderSeq').text() : removeList = $(this).find('.reorderSeq').text();
     });
 
     keepList=keepList + ' --- ';
     keepList=keepList.replace('  ',' ');
     removeList=removeList.replace('  ',' ');
-    $('input[id=order]').val(keepList + removeList);
+    jQuery('input[id=order]').val(keepList + removeList);
 
-    if ($('.col2 .layoutReorderer-module').length===0){
-        $('#deleteListHead').attr('class','deleteListMessageEmpty panel-heading');
+    if (jQuery('.col2 .layoutReorderer-module').length===0){
+        jQuery('#deleteListHead').attr('class','deleteListMessageEmpty panel-heading');
     }
     else {
-        $('#deleteListHead').attr('class','deleteListMessage panel-heading');
+        jQuery('#deleteListHead').attr('class','deleteListMessage panel-heading');
     }
     $('.layoutReorderer-module').find('.marker').closest('.layoutReorderer-module').remove();
 };
 
 $(document).ready(function(){
-    $(".reorderItemsContainer").sortable({
-        start: function(event, ui) {
-            recalculate();
-        },
-        stop: function(event, ui) {
-            recalculate();
-        },
-        connectWith: ".reorderItemsContainer",
-        placeholder: "ui-state-highlight",
-        forcePlaceholderSize: true
-    }).disableSelection();
-
+    $('.layoutReorderer-module').find('.marker').closest('.layoutReorderer-module').remove();
     recalculate();
 
     $('#save').click(function(e){
@@ -44,11 +56,9 @@ $(document).ready(function(){
 
     $('.deleteAnswerTrashLink').click(function(e){
         e.preventDefault();
-        $(this).closest('.layoutReorderer-module').addClass('highlightEl').hide().appendTo('#deleteListHead').fadeIn(200, function(){
+        $(this).closest('.layoutReorderer-module').addClass('highlightEl').hide().appendTo('#reorderCol2').fadeIn(200, function(){
             $(this).removeClass('highlightEl');
         });
-
-        $(".reorderItemsContainer").sortable('refresh');
 
         recalculate();
     });
