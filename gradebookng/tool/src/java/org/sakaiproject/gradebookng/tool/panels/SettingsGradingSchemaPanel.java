3c23dd1d59d100ff1318825246589d4259e6efa9
Jose Cebellán <josecebe@gmail.com>
2020-03-18 08:44:48 +0100

    SAK-42766 Changing grade schema deletes categories in Gradebook (#8039)

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsGradingSchemaPanel.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/SettingsGradingSchemaPanel.java
@@ -301,6 +301,10 @@ public class SettingsGradingSchemaPanel extends BasePanel implements IFormModelU
 				// repaint table
 				target.add(SettingsGradingSchemaPanel.this.schemaWrap);
 
+				// reinitialize any custom behaviour
+				target.appendJavaScript("sakai.gradebookng.settings.gradingschemas = new GradebookGradingSchemaSettings($('#settingsGradingSchema'));");
+				// focus the new grading schema input
+				target.appendJavaScript("sakai.gradebookng.settings.gradingschemas.focusLastRow();");
 				// Note that we don't need to worry about showing warnings about modifications here as the change notifications will handle
 				// that once a value has been added to the schema
 			}
