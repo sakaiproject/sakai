5d590842f1037ea37aa0189b372a0a256f20b031
Sam Ottenhoff <ottenhoff@longsight.com>
2020-05-01 09:54:45 -0400

    Revert "SAK-43621 handle bad timezone like EST/PST/HST and remove confusing ETC timezones"
    This reverts commit 994ec4250e3ea8349a19711714de3d9a55a83a13.

--- kernel/kernel-impl/src/main/java/org/sakaiproject/time/impl/UserTimeServiceImpl.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/time/impl/UserTimeServiceImpl.java
@@ -79,18 +79,8 @@ public class UserTimeServiceImpl implements UserTimeService {
         ResourceProperties tzProps = prefs.getProperties(TimeService.APPLICATION_ID);
         String timeZone = tzProps.getProperty(TimeService.TIMEZONE_KEY);
 
-        if (StringUtils.isBlank(timeZone)) {
+        if (timeZone == null || timeZone.equals(""))
             timeZone = TimeZone.getDefault().getID();
-        }
-        else {
-            try {
-                ZoneId.of(timeZone);
-            }
-            catch (Exception e) {
-                log.warn("getUserTimezone bad tz: {}, {}", userId, timeZone);
-                timeZone = TimeZone.getDefault().getID();
-            }
-        }
 
         timeZoneLocale = timeZone;
 
