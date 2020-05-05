a308de19b2ba89e0707ea14acf489e7f24e97a39
Sam Ottenhoff <ottenhoff@longsight.com>
2020-05-05 10:46:07 -0400

    SAK-43615 handle bad timezone like EST/PST/HST and remove confusing ETC timezones (#8202)

--- user/user-tool-prefs/tool/src/java/org/sakaiproject/user/tool/UserPrefsTool.java
+++ user/user-tool-prefs/tool/src/java/org/sakaiproject/user/tool/UserPrefsTool.java
@@ -271,7 +271,7 @@ public class UserPrefsTool
 			Arrays.sort(timeZoneArray);
 			for (int i = 0; i < timeZoneArray.length; i++) {
 				String tzt = timeZoneArray[i];
-				if (StringUtils.contains(tzt, '/') && StringUtils.indexOf(tzt, "SystemV/") != 0) {
+				if (StringUtils.contains(tzt, '/') && !StringUtils.startsWith(tzt, "SystemV") && !StringUtils.startsWith(tzt, "Etc/GMT")) {
 					String id = tzt;
 					String name = tzt;
 					if (StringUtils.contains(tzt, '_')) {
