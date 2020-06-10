801c823209603c0e137ba58f75ed6988e360aa23
Earle Nietzel <earle@longsight.com>
2020-06-10 18:17:42 -0400

    SAK-43776 Improved Ignite Network Configuration
    https://jira.sakaiproject.org/browse/SAK-43776

--- kernel/kernel-impl/src/main/java/org/sakaiproject/ignite/IgniteConfigurationAdapter.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/ignite/IgniteConfigurationAdapter.java
@@ -1,6 +1,7 @@
 package org.sakaiproject.ignite;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,7 +13,6 @@ import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.sakaiproject.component.api.ServerConfigurationService;
-import org.sakaiproject.util.BasicConfigItem;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 
 import lombok.Getter;
@@ -46,12 +46,12 @@ public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfig
     protected IgniteConfiguration createInstance() {
         if (!configured) {
             address = serverConfigurationService.getString("ignite.address");
-            home = serverConfigurationService.getString("ignite.home", serverConfigurationService.getSakaiHomePath());
+            home = serverConfigurationService.getString("ignite.home");
             remoteAddresses = serverConfigurationService.getStrings("ignite.addresses");
-            port = serverConfigurationService.getInt("ignite.port", 49000);
+            port = serverConfigurationService.getInt("ignite.port", 0);
             range = serverConfigurationService.getInt("ignite.range", 10);
             mode = serverConfigurationService.getString("ignite.mode", "worker");
-            name = StringUtils.defaultIfBlank(serverConfigurationService.getServerName(), "localhost");
+            name = serverConfigurationService.getServerName();
             node = serverConfigurationService.getServerId();
 
             // disable banner
@@ -59,7 +59,11 @@ public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfig
             System.setProperty("IGNITE_QUIET", "true");
             System.setProperty("IGNITE_PERFORMANCE_SUGGESTIONS_DISABLED", "true");
 
-            igniteConfiguration.setIgniteHome(configureHome(home, node));
+            configureName();
+            configureHome();
+            configurePort();
+
+            igniteConfiguration.setIgniteHome(home);
             igniteConfiguration.setConsistentId(node);
             igniteConfiguration.setIgniteInstanceName(name);
 
@@ -75,34 +79,76 @@ public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfig
 
             // local node network configuration
             TcpCommunicationSpi tcpCommunication = new TcpCommunicationSpi();
-            if (StringUtils.isNotBlank(address)) tcpCommunication.setLocalAddress(address);
-            tcpCommunication.setLocalPort(port);
-            tcpCommunication.setLocalPortRange(range);
-            igniteConfiguration.setCommunicationSpi(tcpCommunication);
+            TcpDiscoverySpi tcpDiscovery = new TcpDiscoverySpi();
+            TcpDiscoveryVmIpFinder finder = new TcpDiscoveryVmIpFinder();
 
-            // remote node network configuration, 1.2.3.5:49000..49009
-            if (remoteAddresses != null) {
-                TcpDiscoverySpi discovery = new TcpDiscoverySpi();
-                TcpDiscoveryVmIpFinder finder = new TcpDiscoveryVmIpFinder();
-                finder.setAddresses(Arrays.asList(remoteAddresses));
-                discovery.setIpFinder(finder);
-                igniteConfiguration.setDiscoverySpi(discovery);
+            List<String> discoveryAddresses = new ArrayList<>();
+            if (StringUtils.isNotBlank(address)) {
+                tcpCommunication.setLocalAddress(address);
+                tcpDiscovery.setLocalAddress(address);
+                discoveryAddresses.add(address + ":" + (port + range) + ".." + (port + range + range - 1));
+            } else {
+                discoveryAddresses.add("127.0.0.1:" + (port + range) + ".." + (port + range + range - 1));
             }
 
-            log.info("Ignite configured with home=[{}], node=[{}], name=[{}], client mode=[{}]",
+            tcpCommunication.setLocalPort(port);
+            tcpCommunication.setLocalPortRange(range - 1);
+
+            tcpDiscovery.setLocalPort(port + range);
+            tcpDiscovery.setLocalPortRange(range - 1);
+
+            // remote node network configuration, 1.2.3.5:49000..49009
+            if (remoteAddresses != null && remoteAddresses.length > 0) {
+                discoveryAddresses.addAll(Arrays.asList(remoteAddresses));
+            }
+
+            finder.setAddresses(discoveryAddresses);
+            tcpDiscovery.setIpFinder(finder);
+
+            igniteConfiguration.setDiscoverySpi(tcpDiscovery);
+            igniteConfiguration.setCommunicationSpi(tcpCommunication);
+
+            log.info("Ignite configured with home=[{}], node=[{}], name=[{}], client mode=[{}], tcp ports=[{}..{}], discovery ports=[{}..{}]",
                     igniteConfiguration.getIgniteHome(),
                     igniteConfiguration.getConsistentId(),
                     igniteConfiguration.getIgniteInstanceName(),
-                    igniteConfiguration.isClientMode());
+                    igniteConfiguration.isClientMode(),
+                    tcpCommunication.getLocalPort(),
+                    tcpCommunication.getLocalPort() + tcpCommunication.getLocalPortRange(),
+                    tcpDiscovery.getLocalPort(),
+                    tcpDiscovery.getLocalPort() + tcpDiscovery.getLocalPortRange());
 
             configured = Boolean.TRUE;
         }
         return igniteConfiguration;
     }
 
-    private String configureHome(String path, String node) {
+    private void configureName() {
+        name = StringUtils.replaceChars(name, '.', '-');
+    }
+
+    private void configurePort() {
+        if (port < 1024) {
+            port = dynamicPort(name.hashCode());
+        }
+    }
+
+    private int dynamicPort(int number) {
+        // 65535 - range
+        if (number > (65535 - (range * 2))) {
+            return dynamicPort(number >> 1);
+        } else if (number < 49152) {
+            return dynamicPort((number / 3) + number);
+        }
+        return number;
+    }
+
+    private void configureHome() {
+        String path = home;
         if (StringUtils.isBlank(path)) {
+            // if the root path is not specified use sakai.home/ignite/
             path = serverConfigurationService.getSakaiHomePath();
+            path = path + File.separator + "ignite";
         }
 
         if (StringUtils.isNotBlank(node)) {
@@ -116,6 +162,6 @@ public class IgniteConfigurationAdapter extends AbstractFactoryBean<IgniteConfig
         if (!igniteHome.exists()) igniteHome.mkdirs();
 
         // return the absolute path
-        return igniteHome.getAbsolutePath();
+        home = igniteHome.getAbsolutePath();
     }
 }
