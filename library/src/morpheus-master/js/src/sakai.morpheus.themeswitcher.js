b23c2be34f194627c469e5173b2c2549f7634b06
Michael Greene <profmikegreene@gmail.com>
2020-07-28 10:12:14 -0400

    SAK-43981 WIP

--- library/src/morpheus-master/js/src/sakai.morpheus.themeswitcher.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.themeswitcher.js
@@ -1,29 +1,43 @@
-THEME_SELECTION_KEY = portal.user.id + "#theme";
+//There is a bright flash of light theme before this is loaded and dark theme
+//is set. Can this be moved higher in the DOM somehow or made more performant
+//to avoid this?
 
-// Update theme in session storage
-let setTheme = function (theme) {
-    console.log(theme);
-    localStorage.setItem('sak-theme', theme);
+let themeSwitcher = document.getElementById("themeSwitcher");
+
+SAK_THEME_SELECTION_KEY = portal.user.id + "#theme";
+
+if(themeSwitcher != null) {
+    
+    initTheme(); // on page load, if user has already selected a specific theme -> apply it
+  
+    themeSwitcher.addEventListener('click', setTheme, false);
+
 }
 
-// Detect theme change
-document.querySelectorAll(".switch").forEach(function(theSwitch) {
-    theSwitch.addEventListener("click", handleClickEvent, false);
-  });
-  
-  function handleClickEvent(evt) {
-    let el = evt.target;
+function setTheme() {
+
     // Set to dark mode
-    if (el.getAttribute("aria-checked") == "true") {
-        el.setAttribute("aria-checked", "false");
+    if (themeSwitcher.getAttribute("aria-checked") == "true") {
+        themeSwitcher.setAttribute("aria-checked", "false");
         document.firstElementChild.classList.add('sak-dark-theme');
-        setTheme('dark');
+        localStorage.setItem('sak-theme', 'dark');
     }
     // Set to light mode
     else {
-        el.setAttribute("aria-checked", "true");
+        themeSwitcher.setAttribute("aria-checked", "true");
         document.firstElementChild.classList.remove('sak-dark-theme');
-
-        setTheme('light');
+        localStorage.setItem('sak-theme', 'light')
     }
-  }
+ }
+
+// Use theme from localStorage
+function initTheme() {
+    if (localStorage.getItem('sak-theme') === 'dark') {
+        setTheme('dark');   
+    } 
+    // placeholder in case this is needed when pulling from sakai preferences
+    // else {
+    //     setTheme('light');
+    //     themeSwitcher.setAttribute("aria-checked", "true");
+    // }
+};
