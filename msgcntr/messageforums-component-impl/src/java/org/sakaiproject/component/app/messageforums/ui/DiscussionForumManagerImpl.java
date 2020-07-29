e6e4f8fe1778bc938a9a6e6c4b59fae45c13ddc3
Jesús María Méndez <jesusmmp@gmail.com>
2020-07-29 08:43:43 +0200

    SAK-44010: FORUMS - Error when saving a topic that was created automatically

--- msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/ui/DiscussionForumManagerImpl.java
+++ msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/ui/DiscussionForumManagerImpl.java
@@ -1202,7 +1202,7 @@ public class DiscussionForumManagerImpl extends HibernateDaoSupport implements
     if (saveForum)
     {
       forum.addTopic(topic);
-      forumManager.saveDiscussionForum(forum, forum.getDraft(), false, currentUser); // event already logged by saveDiscussionForumTopic()
+      forum = forumManager.saveDiscussionForum(forum, forum.getDraft(), false, currentUser); // event already logged by saveDiscussionForumTopic()
       //sak-5146 forumManager.saveDiscussionForum(forum);
     }
 
