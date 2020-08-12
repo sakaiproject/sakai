787f86e84f24cc008f3084ccac6551697812cde9
TurRil <lightscape2001@gmail.com>
2020-08-12 15:07:55 +0200

    SAK-44071: Updated code after review - removed variable declarations that where not used.

--- library/src/morpheus-master/js/src/sakai.morpheus.update.presence.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.update.presence.js
@@ -1,19 +1,6 @@
 var activeTabListenerAttached = false,// initial load
     handleVisibilityChange = function(){}; 
 
-// Set the name of the hidden property and the change event for visibility
-var hidden, visibilityChange;
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-    hidden = "hidden";
-    visibilityChange = "visibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-    hidden = "msHidden";
-    visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-    hidden = "webkitHidden";
-    visibilityChange = "webkitvisibilitychange";
-}
-
 function updatePresenceTimeout(_ms, _go) {
     if (window.sakaiLastPresenceTimeOut !== null) {
         clearTimeout(window.sakaiLastPresenceTimeOut);
