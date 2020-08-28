6eea259090cbe22b51d19b96c9b5ef121feb5194
austin48 <austin48@hawaii.edu>
2020-08-28 06:35:04 -1000

    SAK-34273 Random-order Answers Display Order While Grading (#8409)
    (cherry picked from commit a16fd4e8f1b9dc1597cf0bf299fe992d8866ae61)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -1399,8 +1399,17 @@ public class DeliveryActionListener
           // Randomize matching the same way for each
         }
 
+        // Show the answers in the same order that student did.
+        String agentString = "";
+        if (delivery.getActionMode() == DeliveryBean.GRADE_ASSESSMENT) {
+            StudentScoresBean studentscorebean = (StudentScoresBean) ContextUtil.lookupBean("studentScores");
+            agentString = studentscorebean.getStudentId();
+        } else {
+            agentString = getAgentString();
+        }
+
         String itemText = (item.getText() == null) ? "" : item.getText();
-        Collections.shuffle(shuffled, new Random( (long) itemText.hashCode() + (getAgentString() + "_" + item.getItemId().toString()).hashCode()));
+        Collections.shuffle(shuffled, new Random( (long) itemText.hashCode() + (agentString + "_" + item.getItemId().toString()).hashCode()));
         key2 = shuffled.iterator();
       }
       else
@@ -1673,7 +1682,17 @@ public class DeliveryActionListener
 
     if (item.getTypeId().equals(TypeIfc.MATCHING)) // matching
     {
-      populateMatching(item, itemBean, publishedAnswerHash);
+        // Show the answers in the same order that student did.
+        String agentString = "";
+
+        if (delivery.getActionMode() == DeliveryBean.GRADE_ASSESSMENT) {
+            StudentScoresBean studentscorebean = (StudentScoresBean) ContextUtil.lookupBean("studentScores");
+            agentString = studentscorebean.getStudentId();
+        } else {
+            agentString = getAgentString();
+        }
+
+        populateMatching(item, itemBean, publishedAnswerHash, agentString);
     }
     else if (item.getTypeId().equals(TypeIfc.EXTENDED_MATCHING_ITEMS))
     {
@@ -1775,7 +1794,7 @@ public class DeliveryActionListener
     bean.setIsMultipleItems(beans.size() > 1);
   }
 
-  public void populateMatching(ItemDataIfc item, ItemContentsBean bean, Map publishedAnswerHash)
+  public void populateMatching(ItemDataIfc item, ItemContentsBean bean, Map publishedAnswerHash, String agentString)
   {
 	  // used only for questions with distractors where the user has selected None of the Above
 	  final Long NONE_OF_THE_ABOVE = -1l;
@@ -1803,7 +1822,7 @@ public class DeliveryActionListener
       }
       Collections.shuffle(shuffled,
                           new Random( (long) item.getText().hashCode() +
-                          (getAgentString() + "_" + item.getItemId().toString()).hashCode()));
+                          (agentString + "_" + item.getItemId().toString()).hashCode()));
 
 /*
       Collections.shuffle
