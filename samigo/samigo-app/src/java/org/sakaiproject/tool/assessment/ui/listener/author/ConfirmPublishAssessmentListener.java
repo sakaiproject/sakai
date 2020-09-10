272c4603f73e20ec7bdbf8709d5bd11fec7265e5
Brian Jones <bjones86@uwo.ca>
2020-09-10 12:23:30 +0000

    SAK-44250: samigo > settings > add time limit validation (#8542)
    (cherry picked from commit fa57d451e450ae7daab1dee313330b1383fef856)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/ConfirmPublishAssessmentListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/author/ConfirmPublishAssessmentListener.java
@@ -57,6 +57,7 @@ import org.sakaiproject.tool.assessment.ui.bean.authz.AuthorizationBean;
 import org.sakaiproject.tool.assessment.ui.listener.util.ContextUtil;
 import org.sakaiproject.tool.assessment.util.ExtendedTimeValidator;
 import org.sakaiproject.tool.assessment.util.TextFormat;
+import org.sakaiproject.tool.assessment.util.TimeLimitValidator;
 import org.sakaiproject.util.api.FormattedText;
 
 /**
@@ -173,6 +174,17 @@ public class ConfirmPublishAssessmentListener
 		String dateError4 = ContextUtil.getLocalizedString("org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages", "due_same_as_available");
 		context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, dateError4, null));
 		error=true;
+	}
+
+	// if using a time limit, ensure open window is greater than or equal to time limit
+	boolean hasTimer = TimeLimitValidator.hasTimer(assessmentSettings.getTimedHours(), assessmentSettings.getTimedMinutes());
+	if(hasTimer) {
+		Date due = assessmentSettings.getRetractDate() != null ? assessmentSettings.getRetractDate() : assessmentSettings.getDueDate();
+		boolean availableLongerThanTimer = TimeLimitValidator.availableLongerThanTimer(startDate, due, assessmentSettings.getTimedHours(), assessmentSettings.getTimedMinutes(),
+																						"org.sakaiproject.tool.assessment.bundle.AssessmentSettingsMessages", "open_window_less_than_time_limit", context);
+		if(!availableLongerThanTimer) {
+			error = true;
+		}
 	}
 
 	boolean extendedTimesValid = ExtendedTimeValidator.validateEntries( assessmentSettings.getExtendedTimes(), context, assessmentSettings );
