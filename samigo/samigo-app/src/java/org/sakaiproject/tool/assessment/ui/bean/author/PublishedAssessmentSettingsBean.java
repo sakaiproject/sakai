2b9fdc68538a129889cadbe8b8eaf8a7dd2eab6a
Brian Jones <bjones86@uwo.ca>
2020-09-17 12:47:35 +0000

    SAK-44297 samigo > exception with no start date causes NPE (#8575)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/PublishedAssessmentSettingsBean.java
@@ -1822,7 +1822,7 @@ public void setFeedbackComponentOption(String feedbackComponentOption) {
   public void addExtendedTime() {
       ExtendedTime entry = this.extendedTime;
       FacesContext context = FacesContext.getCurrentInstance();
-      if (ExtendedTimeValidator.validateEntry(entry, context, this)) {
+      if (new ExtendedTimeValidator().validateEntry(entry, context, this)) {
           AssessmentAccessControlIfc accessControl = new AssessmentAccessControl();
           accessControl.setStartDate(this.startDate);
           accessControl.setDueDate(this.dueDate);
