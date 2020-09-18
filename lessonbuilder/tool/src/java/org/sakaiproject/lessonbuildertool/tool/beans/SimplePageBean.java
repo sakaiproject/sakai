e0c14dc0838e8f626209221ec4d110454271f553
plukasew <plukasew@uwo.ca>
2020-09-18 09:56:20 -0400

    SAK-42472: Lessons: Short Answer Question displays red X after student submission (#8541)

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/beans/SimplePageBean.java
@@ -7693,6 +7693,9 @@ public class SimplePageBean {
 					break;
 				}
 			}
+			if(totalTokens == 0 && !theirResponse.isEmpty()) {
+				foundAnswer = true;
+			}
 			if(foundAnswer) {
 				correct = true;
 			}else {
