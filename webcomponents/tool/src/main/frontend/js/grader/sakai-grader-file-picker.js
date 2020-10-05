5ccce56d8ea721983a941b07d04aeb2de3bef3e8
Adrian Fish <adrian.r.fish@gmail.com>
2020-10-05 17:31:41 +0100

    SAK-44074 Allow removal of files from picked list in grader (#8574)
    https://jira.sakaiproject.org/browse/SAK-44074
    (cherry picked from commit fa0856268e1122eb299dc57e972d9429c8bfd1c0)

--- webcomponents/tool/src/main/frontend/js/grader/sakai-grader-file-picker.js
+++ webcomponents/tool/src/main/frontend/js/grader/sakai-grader-file-picker.js
@@ -1,6 +1,5 @@
 import {SakaiElement} from "../sakai-element.js";
 import {html} from "../assets/lit-element/lit-element.js";
-import "../fa-icon.js";
 
 class SakaiGraderFilePicker extends SakaiElement {
 
@@ -9,14 +8,25 @@ class SakaiGraderFilePicker extends SakaiElement {
     super();
 
     this.files = [];
+    this.loadTranslations("file-picker").then(t => this.i18n = t);
   }
 
   static get properties() {
 
     return {
       buttonText: { attribute: "button-text", type: String },
-      files: Array,
+      files: { type: Array }
     };
+  }
+
+  removeFile(e) {
+
+    this.files.splice(this.files.findIndex(f => f.name === e.target.dataset.name), 1);
+    this.requestUpdate();
+  }
+
+  shouldUpdate(changed) {
+    return this.i18n;
   }
 
   render() {
@@ -24,7 +34,17 @@ class SakaiGraderFilePicker extends SakaiElement {
     return html`
       <button title="${this.title}" @click=${this.pickFile}>${this.buttonText}</button>
       <div class="sakai-file-picker-list">
-      ${this.files.map(f => html`<div style="overflow-wrap: break-word;">${f.name}</div>`)}
+      ${this.files.length > 0 ? html`
+        <div class="sakai-file-picker-list-title">${this.i18n["to_be_added"]}</div>
+        ${this.files.map(f => html`
+          <div class="file-row">
+            <div class="file">${f.name}</div>
+            <div class="file-remove">
+              <a @click=${this.removeFile} data-name="${f.name}" href="javascript:;">${this.i18n["remove"]}</a>
+            </div>
+          </div>
+        `)}
+      ` : ""}
       </div>
     `;
   }
