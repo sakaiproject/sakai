0977118989bb5e3a842f59de0e4814e5b522466f
Brian Jones <bjones86@uwo.ca>
2020-10-13 19:56:58 +0000

    SAK-29425 forums > add percent read and num authored to individual statistics UI (#8676)
    (cherry picked from commit e632966f6899d2c766a753233f9975c75ab76617)

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/ui/MessageForumStatisticsBean.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/ui/MessageForumStatisticsBean.java
@@ -3113,4 +3113,17 @@ public class MessageForumStatisticsBean {
 		// Condenses to:
 		return topic.getPostAnonymous() && (!topic.getRevealIDsToRoles() || !uiPermissionsManager.isIdentifyAnonAuthors((DiscussionTopic) topic));
 	}
+
+    public int getPercentRead()
+    {
+        int totalPosts = 0;
+        List<Object[]> studentAuthoredStats = messageManager.findAuthoredMessageCountForAllStudents();
+        for (Object[] pair : studentAuthoredStats)
+        {
+            totalPosts += ((Long) pair[1]).intValue();
+        }
+
+        int numRead = getUserReadStatistics().size();
+        return (numRead / totalPosts) * 100;
+    }
 }
