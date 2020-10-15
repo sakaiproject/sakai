99e323e89f75c423828b1a0130617d1880ddfc96
Adrian Fish <adrian.r.fish@gmail.com>
2020-10-15 17:08:25 +0100

    SAK-44466 Ensure we get a non null locale from preferences (#8694)
    https://jira.sakaiproject.org/browse/SAK-44466
    (cherry picked from commit 33cd5d201ea60a07a725e7e2d797a30f3ba49ce2)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/LinearAccessDeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/LinearAccessDeliveryActionListener.java
@@ -21,9 +21,7 @@
 
 package org.sakaiproject.tool.assessment.ui.listener.delivery;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
