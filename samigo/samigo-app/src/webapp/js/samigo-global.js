b0f288f509d52794033bf8ebfc6fec3bf89672a3
Jesús María Méndez Pérez <jesusmmp@gmail.com>
2020-12-08 17:13:31 +0100

    SAK-44743: SAMIGO - No need recalculate height when editor HTML is shown (#8867)
    (cherry picked from commit 2891d2936d0dea0f3d898bdc6c6ee90916a4f61c)

--- samigo/samigo-app/src/webapp/js/samigo-global.js
+++ samigo/samigo-app/src/webapp/js/samigo-global.js
@@ -23,8 +23,6 @@ function encodeHTML(text) {
 }
 
 function chef_setupformattedtextarea(client_id, shouldToggle, frame_id, max_chars) {
-	$("body").height($("body").outerHeight() + 600);
-
 	var textarea_id = client_id + "_textinput";
 
 	if (shouldToggle == true) {
