98fd26bfc2bfd024cc8437d28ededdf4e2391668
Earle Nietzel <earle@longsight.com>
2020-12-16 12:41:05 -0500

    SAK-44769 SiteManager DateManagerUpdate contructor allow nulls (#8910)

--- site-manage/datemanager/api/src/java/org/sakaiproject/datemanager/api/model/DateManagerUpdate.java
+++ site-manage/datemanager/api/src/java/org/sakaiproject/datemanager/api/model/DateManagerUpdate.java
@@ -16,16 +16,20 @@ package org.sakaiproject.datemanager.api.model;
 import java.time.Instant;
 
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class DateManagerUpdate {
-	@NonNull public Object object;
-	@NonNull public Instant openDate;
-	@NonNull public Instant dueDate;
-	@NonNull public Instant acceptUntilDate;
+	public Object object;
+	public Instant openDate;
+	public Instant dueDate;
+	public Instant acceptUntilDate;
 	public Instant feedbackStartDate;
 	public Instant feedbackEndDate;
+
+	public DateManagerUpdate(Object object, Instant openDate, Instant dueDate, Instant acceptUntilDate) {
+		this.object = object;
+		this.openDate = openDate;
+		this.dueDate = dueDate;
+		this.acceptUntilDate = acceptUntilDate;
+	}
 }
