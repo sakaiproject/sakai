df33890dad0cefcbb69d184b71a65d8752a57f0c
Earle Nietzel <earle@longsight.com>
2021-01-11 10:23:09 -0500

    SAK-33701 Assignments add submission log (#8936)

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -87,6 +87,7 @@ import java.util.Date;
 import java.util.Enumeration;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -1747,6 +1748,7 @@ public class AssignmentAction extends PagedResourceActionII {
                 // the attachments from the previous submission
                 Set<String> submittedAttachments = s.getAttachments();
                 newAttachments = areAttachmentsModified(submittedAttachments, currentAttachments);
+                putSubmissionLogMessagesInContext(context, s);
             } else {
                 // There is no previous submission, attachments are modified if anything has been uploaded
                 newAttachments = CollectionUtils.isNotEmpty(currentAttachments);
@@ -2128,6 +2130,7 @@ public class AssignmentAction extends PagedResourceActionII {
 									.collect(Collectors.toMap(User::getId, Function.identity()));
 					context.put("submitterNames", getSubmitterFormattedNames(s, "build_student_view_submission_confirmation_context"));
                 }
+                putSubmissionLogMessagesInContext(context, s);
             }
         }
 
@@ -2543,6 +2546,7 @@ public class AssignmentAction extends PagedResourceActionII {
                 }
             }
             context.put("NamePropContentReviewOptoutUrl", ContentReviewConstants.URKUND_OPTOUT_URL);
+            putSubmissionLogMessagesInContext(context, submission);
         }
 
         if (taggingManager.isTaggable() && submission != null) {
@@ -3654,6 +3658,7 @@ public class AssignmentAction extends PagedResourceActionII {
             s.getAttachments().forEach(r -> attachmentReferences.put(r, entityManager.newReference(r)));
             context.put("submissionAttachmentReferences", attachmentReferences);
 
+            putSubmissionLogMessagesInContext(context, s);
             rangeAndGroups.buildInstructorGradeSubmissionContextGroupCheck(assignment, s.getGroupId(), state);
         }
 
@@ -4266,6 +4271,31 @@ public class AssignmentAction extends PagedResourceActionII {
         context.put("value_" + timeName + "Year", (Integer) state.getAttribute(year));
         context.put("value_" + timeName + "Hour", (Integer) state.getAttribute(hour));
         context.put("value_" + timeName + "Min", (Integer) state.getAttribute(min));
+    }
+
+    private void putSubmissionLogMessagesInContext(Context context, AssignmentSubmission submission) {
+        List<String> messages = new LinkedList<>();
+        Map<String, String> properties = submission.getProperties();
+        Map<Integer, String> orderedLogKeys = properties.keySet().stream()
+                .filter(k -> k.startsWith("log"))
+                .map(k -> new String[] {k, StringUtils.split(k, "log")[0]})
+                .collect(Collectors.toMap(a -> new Integer(a[1]), a -> a[0], (e1, e2) -> e1, TreeMap::new));
+
+        orderedLogKeys.values().stream().map(properties::get).map(formattedText::escapeHtml).forEach(messages::add);
+
+        context.put("submissionLog", messages);
+    }
+
+    private String getNextSubmissionLogKey(AssignmentSubmission submission) {
+        String keyPrefix = "log";
+        Map<String, String> properties = submission.getProperties();
+        List<Integer> keys = properties.keySet().stream()
+                .filter(k -> k.startsWith("log"))
+                .map(k -> new Integer(StringUtils.split(k, "log")[0]))
+                .sorted()
+                .collect(Collectors.toList());
+        int next = keys.isEmpty() ? 0 : keys.get(keys.size() - 1) + 1;
+        return keyPrefix + next;
     }
 
     private List getPrevFeedbackAttachments(Map<String, String> p) {
@@ -6437,6 +6467,28 @@ public class AssignmentAction extends PagedResourceActionII {
                     properties.remove(AssignmentConstants.SUBMITTER_USER_ID);
                 }
 
+                // submission log
+                StringBuilder logEntry = new StringBuilder();
+                DateTimeFormatter dtf = DateTimeFormatter.RFC_1123_DATE_TIME
+                        .withZone(userTimeService.getLocalTimeZone(u.getId()).toZoneId())
+                        .withLocale(preferencesService.getLocale(u.getId()));
+                logEntry.append(dtf.format(Instant.now()));
+                boolean anonymousGrading = Boolean.parseBoolean(a.getProperties().get(NEW_ASSIGNMENT_CHECK_ANONYMOUS_GRADING));
+                String subOrDraft = post ? rb.getString("listsub.submitted") : rb.getString("listsub.submitted.draft");
+                if (!anonymousGrading) {
+                    if (submitter != null && !submitter.getEid().equals(u.getEid())) {
+                        logEntry.append(" ").append(submitter.getDisplayName())
+                                .append(" (").append(submitter.getEid()).append(") ").append(subOrDraft)
+                                .append(" ").append(rb.getString("listsub.submitted.on.behalf"))
+                                .append(" ").append(u.getDisplayName()).append(" (").append(u.getEid()).append(")");
+                    } else {
+                        logEntry.append(" ").append(u.getDisplayName())
+                                .append(" (").append(u.getEid()).append(") ")
+                                .append(subOrDraft);
+                    }
+                }
+                submission.getProperties().put(getNextSubmissionLogKey(submission), logEntry.toString());
+
                 try {
                     assignmentService.updateSubmission(submission);
                 } catch (PermissionException e) {
@@ -8093,7 +8145,7 @@ public class AssignmentAction extends PagedResourceActionII {
      * @param state
      * @param params
      * @param siteId
-     * @param a
+     * @param assignment
      */
     private void saveAssignmentSupplementItem(SessionState state,
                                               ParameterParser params, String siteId, Assignment assignment) {
@@ -8311,7 +8363,9 @@ public class AssignmentAction extends PagedResourceActionII {
      * Add submission objects if necessary for non-electronic type of assignment
      *
      * @param state
-     * @param a
+     * @param submissions
+     * @param addSubmissionForUsers
+     * @param assignment
      */
     private void addRemoveSubmissionsForNonElectronicAssignment(SessionState state, List submissions, HashSet<String> addSubmissionForUsers, HashSet<String> removeSubmissionForUsers, Assignment assignment) {
         // create submission object for those user who doesn't have one yet
@@ -8630,7 +8684,7 @@ public class AssignmentAction extends PagedResourceActionII {
      * Add event to calendar and then persist the event id to the assignment properties
      *
      * @param state
-     * @param a               AssignmentEdit
+     * @param assignment      Assignment
      * @param title           Event title
      * @param dueTime         Assignment due date/time
      * @param c               Calendar
