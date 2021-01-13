95353e5cc0d97a97c779fc162af10d8ae37b4cd5
Adrian Fish <adrian.r.fish@gmail.com>
2021-01-13 18:36:44 +0000

    SAK-44850 Set sortName to displayName when anon grading (#8954)
    https://jira.sakaiproject.org/browse/SAK-44850
    (cherry picked from commit 4147df455849172bf7ac5ab3948361136171a34b)

--- assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
@@ -1243,6 +1243,7 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
                     this.sortName = user.getSortName();
                 } else {
                     this.displayName = ass.getSubmission().getId() + " " + rb.getString("grading.anonymous.title");
+                    this.sortName = this.displayName;
                 }
             } catch (UserNotDefinedException e) {
                 this.displayName = this.id;
