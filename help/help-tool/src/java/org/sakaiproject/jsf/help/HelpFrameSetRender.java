7fe8aed88ef7877a908f050c72934f6f086d11b1
Sam Ottenhoff <ottenhoff@longsight.com>
2021-03-11 14:01:50 -0500

    SAK-44697 Sakai 21 help files (#9090)
    * SAK-44697 Sakai 21 help files

--- help/help-tool/src/java/org/sakaiproject/jsf/help/HelpFrameSetRender.java
+++ help/help-tool/src/java/org/sakaiproject/jsf/help/HelpFrameSetRender.java
@@ -101,7 +101,7 @@ public class HelpFrameSetRender extends Renderer
     
     writer.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Frameset//EN\" \"http://www.w3.org/TR/html4/frameset.dtd\">\n");
     writer.write("<html><head><title>" + helpWindowTitle + "</title></head>\n");
-    writer.write("<FRAMESET cols=\"20%, 80%\"><FRAMESET rows=\"150, 450\">");
+    writer.write("<FRAMESET cols=\"20%, 80%\"><FRAMESET rows=\"200, 400\">");
     writer.write("<FRAME src=\"" + searchToolUrl + "\" name=\"search\" title=\"Search\">");
     writer.write("<FRAME src=\"" + tocToolUrl + "\" name=\"toc\" title=\"Table of Contents\">");
     writer.write("</FRAMESET>\n");
