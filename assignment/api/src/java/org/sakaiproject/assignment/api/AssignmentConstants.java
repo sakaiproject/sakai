e898d136fba638e1d20a1df3f052f954e14342da
Jose Cebellán Sánchez <josecebe@gmail.com>
2021-04-08 17:58:46 +0200

    SAK-46707 Submission state on assignments should indicate if there is a resubmission pending

--- assignment/api/src/java/org/sakaiproject/assignment/api/AssignmentConstants.java
+++ assignment/api/src/java/org/sakaiproject/assignment/api/AssignmentConstants.java
@@ -291,7 +291,8 @@ public final class AssignmentConstants {
         UNGRADED,
         RETURNED,
         COMMENTED,
-        GRADED
+        GRADED,
+        RETURNED_PENDING_RESUBMIT
     }
 
 	// IMS Score Publishing Service - states
