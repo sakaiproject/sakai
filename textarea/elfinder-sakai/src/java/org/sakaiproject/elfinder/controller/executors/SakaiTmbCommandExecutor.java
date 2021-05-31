a5a09cbc255d273dfc97dafb1dcc1fd79ccf4a3a
Sam Ottenhoff <ottenhoff@longsight.com>
2021-05-31 09:33:46 -0400

    SAK-45523
    (cherry picked from commit c457d3c091cd0ac6c195d1253f72aee3a6eaa7d5)
    (cherry picked from commit 1d924949777507bca33d0c8d1107b16d1c67a6fe)

--- textarea/elfinder-sakai/src/java/org/sakaiproject/elfinder/controller/executors/SakaiTmbCommandExecutor.java
+++ textarea/elfinder-sakai/src/java/org/sakaiproject/elfinder/controller/executors/SakaiTmbCommandExecutor.java
@@ -5,17 +5,22 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Iterator;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 
 import cn.bluejoe.elfinder.controller.executor.AbstractCommandExecutor;
@@ -23,59 +28,76 @@ import cn.bluejoe.elfinder.controller.executor.CommandExecutor;
 import cn.bluejoe.elfinder.controller.executor.FsItemEx;
 import cn.bluejoe.elfinder.service.FsService;
 
+@Slf4j
 public class SakaiTmbCommandExecutor extends AbstractCommandExecutor implements CommandExecutor {
 
     public static final long MAX_TMB_SIZE_BYTES = 20971520;
+    public static final long MAX_IMG_BYTES_TO_LOAD = 104857600;
 
     @Override
     public void execute(FsService fsService, HttpServletRequest request, HttpServletResponse response, ServletContext servletContext) throws Exception {
         String target = request.getParameter("target");
         FsItemEx fsi = super.findItem(fsService, target);
         if (fsi.getSize() < MAX_TMB_SIZE_BYTES) { // image must be less than MAX_TMB_SIZE_BYTES
-            int width = fsService.getServiceConfig().getTmbWidth();
-            try (InputStream is = fsi.openInputStream()) {
-                ByteArrayOutputStream baos = resize(is, width);
-                response.setHeader(HttpHeaders.LAST_MODIFIED, DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT"))));
-                response.setHeader(HttpHeaders.EXPIRES, DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT")).plus(2, ChronoUnit.YEARS)));
-                response.getOutputStream().write(baos.toByteArray());
+            int tmbWidth = fsService.getServiceConfig().getTmbWidth();
+            try (InputStream inputStream = fsi.openInputStream()) {
+                ByteArrayOutputStream baos = resize(inputStream, tmbWidth);
+                if (baos != null) {
+                    response.setHeader(HttpHeaders.LAST_MODIFIED, DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT"))));
+                    response.setHeader(HttpHeaders.EXPIRES, DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now().withZoneSameInstant(ZoneId.of("GMT")).plus(2, ChronoUnit.YEARS)));
+                    response.getOutputStream().write(baos.toByteArray());
+                }
             }
         }
     }
 
-    public ByteArrayOutputStream resize(InputStream inputStream , int size) throws Exception {
-        BufferedImage originalImage = ImageIO.read(inputStream);
-        int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-        int width = originalImage.getWidth();
-        int height = originalImage.getHeight();
-        int scaledWidth;
-        int scaledHeight;
+    public ByteArrayOutputStream resize(InputStream inputStream, int size) throws Exception {
+        try (ImageInputStream imageInputStream = ImageIO.createImageInputStream(inputStream)) {
+            Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(imageInputStream);
+            ImageReader reader = imageReaders.next();
+            reader.setInput(imageInputStream);
 
-        if (width > height) {
-            scaledWidth = (width / height) * size;
-            scaledHeight = size;
-        } else if (width < height) {
-            scaledWidth = size;
-            scaledHeight = (height / width) * size;
-        } else {
-            scaledWidth = size;
-            scaledHeight = size;
+            int width = reader.getWidth(0);
+            int height = reader.getHeight(0);
+            if ((width * height * 4) > MAX_IMG_BYTES_TO_LOAD) {
+                return null;
+            }
+
+            // Now can load the whole image
+            BufferedImage originalImage = reader.read(0);
+            int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+            int scaledWidth;
+            int scaledHeight;
+
+            if (width > height) {
+                scaledWidth = (width / height) * size;
+                scaledHeight = size;
+            } else if (width < height) {
+                scaledWidth = size;
+                scaledHeight = (height / width) * size;
+            } else {
+                scaledWidth = size;
+                scaledHeight = size;
+            }
+
+            BufferedImage scaledImage = new BufferedImage(scaledWidth, scaledHeight, type);
+            Graphics2D g = scaledImage.createGraphics();
+            g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
+            g.dispose();
+            g.setComposite(AlphaComposite.Src);
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ImageIO.write(scaledImage, "png", out);
+
+            originalImage.flush();
+            originalImage = null;
+            scaledImage.flush();
+            scaledImage = null;
+
+            return out;
         }
-
-        BufferedImage scaledImage = new BufferedImage(scaledWidth, scaledHeight, type);
-        Graphics2D g = scaledImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
-        g.dispose();
-        g.setComposite(AlphaComposite.Src);
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ImageIO.write(scaledImage, "png", out);
-
-        originalImage.flush();
-        scaledImage.flush();
-
-        return out;
     }
 }
