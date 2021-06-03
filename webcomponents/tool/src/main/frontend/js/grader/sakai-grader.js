3b501138b16ed3ed27c50118df6d548e719872d5
Vicent Alfonso <81161239+V100X@users.noreply.github.com>
2021-06-03 09:31:50 +0200

    SAK-45346 Incorrect text on button to add feedback in an assignment (#9332)
    (cherry picked from commit cdf7b174c32fd680568c499da69ebdda181a27c4)

--- webcomponents/tool/src/main/frontend/js/grader/sakai-grader.js
+++ webcomponents/tool/src/main/frontend/js/grader/sakai-grader.js
@@ -185,7 +185,7 @@ export class SakaiGrader extends gradableDataMixin(SakaiElement) {
             <div class="sak-banner-info">${unsafeHTML(this.i18n["inline_feedback_instruction"])}</div>
             <textarea id="grader-feedback-text-editor" style="display: none">${this.submission.feedbackText}</textarea>
             <div id="grader-feedback-text">${unsafeHTML(this.submission.feedbackText)}</div>
-            <button id="edit-inline-feedback-button" class="inline-feedback-button" @click=${this.toggleInlineFeedback} aria-haspopup="true">${this.assignmentsI18n["feedbacktext"]}</button>
+            <button id="edit-inline-feedback-button" class="inline-feedback-button" @click=${this.toggleInlineFeedback} aria-haspopup="true">${this.assignmentsI18n["addfeedback"]}</button>
             <button id="show-inline-feedback-button" class="inline-feedback-button" @click=${this.toggleInlineFeedback} aria-haspopup="true" style="display: none;">${this.assignmentsI18n["gen.don"]}</button>
           ` : html`
             ${this.selectedAttachment ? html`
