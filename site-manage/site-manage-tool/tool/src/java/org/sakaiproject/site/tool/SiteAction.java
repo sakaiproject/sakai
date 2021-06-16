fca8c02f509eb6b714b6d1d19754760085522468
Sam Ottenhoff <ottenhoff@longsight.com>
2021-06-16 10:05:55 -0400

    SAK-45628 NPE protection (#9374)
    (cherry picked from commit cc335364bc092238a5a757313383da4df173e7a4)
    
    Co-authored-by: Brian Jones <bjones86@uwo.ca>
    (cherry picked from commit 64b110c4d270246d4f8f8f9ff4c5c4672ad8a821)
    
     Conflicts:
    	site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -182,6 +182,7 @@ import org.sakaiproject.util.comparator.GroupTitleComparator;
 import org.sakaiproject.util.comparator.ToolTitleComparator;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang.math.NumberUtils;
 
 /**
@@ -11943,7 +11944,7 @@ private Map<String, List<MyTool>> getTools(SessionState state, String type, Site
 			state.setAttribute(STATE_TOOL_HOME_SELECTED, Boolean.valueOf(
 					homeSelected));
 
-			if (!ltiSelectedTools.isEmpty() || !existingLtiIds.isEmpty())
+			if (!ltiSelectedTools.isEmpty() || MapUtils.isNotEmpty(existingLtiIds))
 			{
 				// add in existing lti tools where visibility is stealth
 				existingLtiIds.keySet().stream()
