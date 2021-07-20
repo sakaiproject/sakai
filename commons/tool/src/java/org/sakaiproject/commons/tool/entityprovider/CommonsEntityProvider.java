749474ab46252b0e47737eb3a52794fbaf9910ad
David Bauer <dbauer1@udayton.edu>
2021-07-20 16:32:35 -0400

    SAK-46022 Add missing import to fix build SAKAI-5670 Add missing import to fix build

--- commons/tool/src/java/org/sakaiproject/commons/tool/entityprovider/CommonsEntityProvider.java
+++ commons/tool/src/java/org/sakaiproject/commons/tool/entityprovider/CommonsEntityProvider.java
@@ -30,6 +30,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
