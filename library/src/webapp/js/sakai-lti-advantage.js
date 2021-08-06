81c28e7f0164cbe8d6bce4dce5ef8e3c44538bff
Charles Severance <csev@umich.edu>
2021-08-06 12:02:22 -0400

    Moar progress.

--- library/src/webapp/js/sakai-lti-advantage.js
+++ library/src/webapp/js/sakai-lti-advantage.js
@@ -1,8 +1,14 @@
 
-var _Sakai_LTI_Iframes = [];
-window.addEventListener('message', function (e) {
+!function(w) {
+
+let _Sakai_LTI_Iframes = [];
+let stored_data = {}
+
+//  https://github.com/MartinLenord/simple-lti-1p3/blob/cookie-shim/src/web/platform/csstorage.php
+
+w.addEventListener('message', function (event) {
     var outer_href = window.location.href;
-    var origin = e.origin;
+    var origin = event.origin;
     var same_origin = outer_href.startsWith(origin);
     var approved = false;
     var frame_id = false;
@@ -11,7 +17,7 @@ window.addEventListener('message', function (e) {
 
     // https://stackoverflow.com/questions/15329710/postmessage-source-iframe
     Array.prototype.forEach.call(document.getElementsByTagName('iframe'), function (element) {
-      if (element.contentWindow === e.source) {
+      if (element.contentWindow === event.source) {
         // console.log('source element', element);
         frame_id = element.getAttributeNode("id");
         if ( frame_id ) frame_id = frame_id.value;
@@ -19,12 +25,12 @@ window.addEventListener('message', function (e) {
     });
 
     if ( ! frame_id ) {
-        console.log('Message from frame without id', e.source, origin);
-        console.log(e);
+        console.log('Message from frame without id', event.source, origin);
+        console.log(event);
         return;
     }
 
-    var message = e.data;
+    var message = event.data;
     if ( typeof message == 'string' ) message = JSON.parse(message)
     console.log(message);
 
@@ -33,7 +39,7 @@ window.addEventListener('message', function (e) {
         console.log('id', frame_id, (approved ? 'approved' : 'not approved'));
         if ( ! approved ) {
             console.log(_Sakai_LTI_Iframes);
-            console.log(e);
+            console.log(event);
         }
     }
  
@@ -44,6 +50,56 @@ window.addEventListener('message', function (e) {
                 console.log('org.imsglobal.lti.prelaunch from same origin', origin, 'frame approved', frame_id);
             } else {
                 console.log('org.imsglobal.lti.prelaunch must come from same origin, not', origin);
+            }
+            break;
+        case 'org.imsglobal.lti.put_data':
+            if ( approved || same_origin ) {
+                if (!stored_data[frame_id]) {
+                    stored_data[frame_id] = {};
+                }
+                stored_data[frame_id][message.key] = message.value;
+                let send_data = {
+                    subject: 'org.imsglobal.lti.put_data.response',
+                    message_id: message.message_id,
+                    key: message.key,
+                    value: message.value,
+                };
+                console.log(w.location.origin + " Sending post message to " + event.origin);
+                console.log(JSON.stringify(send_data, null, '    '));
+                event.source.postMessage(send_data, event.origin);
+            } else {
+                console.log('org.imsglobal.lti.put_data must come from approved frame, not', origin);
+            }
+            break;
+        case 'org.imsglobal.lti.get_data':
+            if ( approved || same_origin ) {
+                console.log('get_data ',frame_id, ' ', message.key,' ', message.value);
+                let retval = false;
+                if (stored_data[frame_id] && stored_data[frame_id][message.key]) {
+                    let retval = stored_data[frame_id][message.key];
+                     let send_data = {
+                        subject: 'org.imsglobal.lti.get_data.response',
+                        message_id: message.message_id,
+                        key: message.key,
+                        value: stored_data[frame_id][message.key]
+                    };
+                    console.log(w.location.origin + " Sending post message to " + event.origin);
+                    console.log(JSON.stringify(send_data, null, '    '));
+                    event.source.postMessage(send_data, event.origin);
+                } else {
+                    let send_data = {
+                        subject: 'org.imsglobal.lti.get_data.response',
+                        message_id: message.message_id,
+                        key: message.key,
+                        error: 'Could not find key',
+                    };
+                    console.log(w.location.origin + " Sending post message to " + event.origin);
+                    console.log(JSON.stringify(send_data, null, '    '));
+                    event.source.postMessage(send_data, event.origin);
+
+                }
+            } else {
+                console.log('org.imsglobal.lti.get_data must come from approved frame, not', origin);
             }
             break;
         case 'org.sakailms.lti.postverify':
@@ -61,79 +117,10 @@ window.addEventListener('message', function (e) {
                     document.getElementById(frame_id).contentWindow.postMessage({subject:'org.sakailms.lti.close'}, '*');
                 });
             } else {
-                console.log('org.sakailms.lti.prelaunch must come from approved frame, not', origin);
-            }
-            break;
-        case 'org.imsglobal.lti.put_data':
-            if ( approved || same_origin ) {
-                var key = frame_id + message.key;
-                window.sessionStorage.setItem(key, message.value);
-                console.log('put_data ',key,' ', message.value);
-                var response = new Object();
-                response.key = message.key;
-                response.message_id = message.message_id;
-                response.subject = 'org.imsglobal.lti.put_data.response';
-                response.value = message.value;
-                document.getElementById(frame_id).contentWindow.postMessage(response, '*');
-            } else {
-                console.log('org.imsglobal.lti.put_data must come from approved frame, not', origin);
-            }
-            break;
-        case 'org.imsglobal.lti.get_data':
-            if ( approved || same_origin ) {
-                console.log('get_data from approved frame', frame_id);
-                var key = frame_id + message.key;
-                var value = window.sessionStorage.getItem(key);
-                var response = new Object();
-                response.key = message.key;
-                response.message_id = message.message_id;
-                response.subject = 'org.imsglobal.lti.get_data.response';
-                response.value = value;
-                document.getElementById(frame_id).contentWindow.postMessage(response, '*');
-            } else {
-                console.log('org.imsglobal.lti.get_data must come from approved frame, not', origin);
-            }
-            break;
-        case 'org.imsglobal.lti.remove_data':
-            if ( approved || same_origin ) {
-                console.log('remove_data from approved frame', frame_id);
-                var key = frame_id + message.key;
-                window.sessionStorage.removeItem(key, message.value);
-                var response = new Object();
-                response.key = message.key;
-                response.message_id = message.message_id;
-                response.subject = 'org.imsglobal.lti.remove_data.response';
-                document.getElementById(frame_id).contentWindow.postMessage(response, '*');
-            } else {
-                console.log('org.imsglobal.lti.remove_data must come from approved frame, not', origin);
-            }
-            break;
-        case 'org.imsglobal.lti.clear_data':
-            if ( approved || same_origin ) {
-                console.log('sessionStorage.clear from approved frame', frame_id);
-                var key = frame_id + message.key;
-                for (var i = 0; i < localStorage.length; i++) {
-                    var key = localStorage.key(i);
-                    console.log('key', key);
-                    if ( ! key.startsWith(frame_id) ) continue;
-                    console.log('removing', key);
-                    window.sessionStorage.removeItem(key);
-                }
-                var response = new Object();
-                response.message_id = message.message_id;
-                response.subject = 'org.imsglobal.lti.clear_data.response';
-                document.getElementById(frame_id).contentWindow.postMessage(response, '*');
-            } else {
-                console.log('org.imsglobal.lti.clear_data must come from approved frame, not', origin);
-            }
-            break;
-        // This is here - but processed elsewhere
-        case 'lti.frameResize':
-            if ( approved || same_origin ) {
-                console.log('frameResize from approved frame', frame_id);
-            } else {
-                console.log('frameResize must come from approved frame, not', origin);
+                console.log('org.sakailms.lti.postverify must come from approved frame, not', origin);
             }
             break;
     }
-});
+}, false);
+}(window);
+
