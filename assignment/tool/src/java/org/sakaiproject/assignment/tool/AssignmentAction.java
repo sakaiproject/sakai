2d7eaef1d4758f834f8ec1117e7e97c1fb84924f
plukasew <plukasew@uwo.ca>
2021-08-18 09:26:43 -0400

    SAK-45809 Resubmission count always -1 from expected (#9561)
    (cherry picked from commit 607843afee0e03102a27d3bf6779ac0e9b7e8682)

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -5984,7 +5984,8 @@ public class AssignmentAction extends PagedResourceActionII {
 
                     if (NumberUtils.isParsable(properties.get(AssignmentConstants.ALLOW_RESUBMIT_NUMBER))) {
                         // if this submission has been already been submitted previously.
-                        if (submission.getSubmitted() && submission.getDateSubmitted() != null) {
+                        boolean isResub = properties.entrySet().stream().anyMatch(e -> e.getKey().startsWith("log") && e.getValue().contains("submitted"));
+                        if (submission.getSubmitted() && isResub) {
                             // decrease the allow_resubmit_number,
                             int number = Integer.parseInt(properties.get(AssignmentConstants.ALLOW_RESUBMIT_NUMBER));
                             // minus 1 from the submit number, if the number is not -1 (not unlimited)
