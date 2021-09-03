ed48e51fdf80fdf77102a04d3c16b86170ab0b95
Michael Greene <profmikegreene@gmail.com>
2021-09-03 13:27:01 +0000

    SAK-45844 fix codacy errors

--- library/src/webapp/editor/ckextraplugins/sakaidropdowntoolbar/plugin.js
+++ library/src/webapp/editor/ckextraplugins/sakaidropdowntoolbar/plugin.js
@@ -9,32 +9,42 @@
  *               toolbarCanCollapse: true
  *               toolbarStartupExpanded: false
  */
- ( function() {
-
+( function() {
     CKEDITOR.plugins.add( 'sakaidropdowntoolbar', {
         requires: ['button', 'toolbar'],
         init: function( editor ){
-            if (!editor.config.sakaiDropdownToolbar)
-                return;
+            //Exit if config not set
+            if (!editor.config.sakaiDropdownToolbar) { return; }
 
             editor.on('loaded', function(event){
                 const top = document.getElementById(`${editor.id}_top`);
                 const toolbox = document.getElementById(`${editor.id}_toolbox`);
                 const toolboxMain = document.querySelector(`#${editor.id}_toolbox .cke_toolbox_main`);
                 const toolboxCollapser = document.querySelector(`#${editor.id}_toolbox .cke_toolbox_collapser`);
+
+                //Behavior for Full toolbar
                 if (editor.config.toolbar === "Full") {
-                
+
+                    //Show the first row on startup
                     if (!editor.config.toolbarStartupExpanded && editor.config.toolbarCanCollapse){
                         top.classList.add('min');
                         toolboxMain.style.display = 'block';
                     }
+
                     if (editor.config.toolbarCanCollapse){
+                        //Styles to allow collapsing
                         toolbox.style.display = 'flex';
                         toolboxCollapser.style.display = 'block';
 
-                        const config = {attributes: true, childList: false, subtree: false}
+                        //Watch for toggling of the collapser 
+                        const config = {
+                            attributes: true,
+                            childList: false,
+                            subtree: false
+                        };
                         const observer = new MutationObserver(function(mutations) {
                             mutations.forEach(function(mutation) {
+
                                 if (mutation.type === "attributes" && mutation.attributeName === "style") {
                                     if (toolboxMain.style.display === "none") {
                                         toolboxMain.style.display = "block";
@@ -44,13 +54,18 @@
                                 }
                             });
                         })
+
                         observer.observe(toolboxMain, config);
                     }
-                } else if (editor.config.toolbar === "Basic") {
+                }
+
+                //Behavior for Basic Toolbar
+                else if (editor.config.toolbar === "Basic") {
                     toolboxMain.style.display = 'block';
                     toolboxCollapser.style.display = 'none';
                 }
+                //If other toolbars are defined, add additional else if statements here
             });
         }
-    })
+    });
 })();
