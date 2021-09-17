e2f909ade9625187c02c047b1f524a89563948d8
Sam Ottenhoff <ottenhoff@longsight.com>
2021-09-17 09:49:31 -0400

    SAK-45832 fix issue where user not being redirected back to home afte… (#9820)
    * SAK-45832 fix issue where user not being redirected back to home after new forum creation
    
    (cherry picked from commit 4d5b30b974b1cba62a1727f7108c1b8efccdeee6)

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -7066,7 +7066,9 @@ public class DiscussionForumTool {
 	}
 	
    private String gotoMain() {
-     return (isForumsTool() ? FORUMS_MAIN : MAIN) + "?faces-redirect=true";
+     // ern believes the faces-redirect=true is crucial to carry over the error message to new page
+     // return (isForumsTool() ? FORUMS_MAIN : MAIN) + "?faces-redirect=true";
+     return (isForumsTool() ? FORUMS_MAIN : MAIN);
    }
    
 	/**
