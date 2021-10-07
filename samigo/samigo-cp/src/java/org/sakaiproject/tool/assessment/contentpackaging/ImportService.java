668f9222c5428626040068ec3fbeebe4ed4829b1
Fernando Alvarez-Castellanos Rojo <fernando.alvarez@ticarum.es>
2021-10-07 15:10:30 +0200

    SAK-46393 adding java.util.List

--- samigo/samigo-cp/src/java/org/sakaiproject/tool/assessment/contentpackaging/ImportService.java
+++ samigo/samigo-cp/src/java/org/sakaiproject/tool/assessment/contentpackaging/ImportService.java
@@ -28,6 +28,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 import java.util.zip.ZipEntry;
