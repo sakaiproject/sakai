1470ff845eecaa1e2b55b67e154784dd0b225d22
David Horwitz <dhorwitz@gmail.com>
2021-10-17 14:34:23 +0200

    SAK-46443 add missing licenses (content,dashboard; emailtemplateservice, entitybroker)

--- entitybroker/core-providers/src/java/org/sakaiproject/entitybroker/providers/model/EntityServerConfig.java
+++ entitybroker/core-providers/src/java/org/sakaiproject/entitybroker/providers/model/EntityServerConfig.java
@@ -1,15 +1,11 @@
 /**
- * $Id$
- * $URL$
- * EntityServerConfig.java - entity-broker - Jul 17, 2008 12:14:38 PM - azeckoski
- **************************************************************************
- * Copyright (c) 2008 The Sakai Foundation
+ * Copyright (c) 2003-2021 The Apereo Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.opensource.org/licenses/ECL-2.0
+ *             http://opensource.org/licenses/ecl2
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.sakaiproject.entitybroker.providers.model;
 
 import org.sakaiproject.entitybroker.entityprovider.annotations.EntityId;
