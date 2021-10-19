d1f6ad3a85c95d0b5447566cdc470f60d0c87394
David Horwitz <dhorwitz@gmail.com>
2021-10-19 15:23:26 +0200

    SAK-46443 add missing licenses  (#9949)
    (cherry picked from commit a20ee55d3f45f6eaaeb295e289c0332e6a1fbdb3)

--- entitybroker/core-providers/src/test/org/sakaiproject/entitybroker/providers/MembershipEntityProviderTest.java
+++ entitybroker/core-providers/src/test/org/sakaiproject/entitybroker/providers/MembershipEntityProviderTest.java
@@ -1,14 +1,11 @@
 /**
- * $Id$
- * $URL$
- **************************************************************************
- * Copyright (c) 2012 The Sakai Foundation
+ * Copyright (c) 2003-2021 The Apereo Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.osedu.org/licenses/ECL-2.0
+ *             http://opensource.org/licenses/ecl2
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
