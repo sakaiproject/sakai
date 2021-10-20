8b038c3473a0eb4899bf4abe3042e68941d56d19
Bernardo <bgarcia@entornosdeformacion.com>
2021-10-20 12:06:03 +0200

    SAK-46456 Reorder library not working for numerical inputs

--- library/src/morpheus-master/js/src/sakai.morpheus.reorderer.js
+++ library/src/morpheus-master/js/src/sakai.morpheus.reorderer.js
@@ -100,18 +100,18 @@ $PBJQ(document).ready(function(){
         //insert the row in new location - if new value is 1, insert before, if it is the last possible
         // insert after, otherwise insert before or after depending on if it is going up or down
         if (newVal === '1') {
-            $PBJQ($PBJQ(this).parents('.reorder-element')).insertBefore($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
+            $PBJQ($PBJQ(this).parents('.reorder-element')).insertBefore($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
         }
         else 
             if (newVal == inputs.length) {
-                $PBJQ($PBJQ(this).parents('.reorder-element')).insertAfter($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
+                $PBJQ($PBJQ(this).parents('.reorder-element')).insertAfter($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
             }
             else {
                 if (newVal > oldVal) {
-                    $PBJQ($PBJQ(this).parents('.reorder-element')).insertAfter($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
+                    $PBJQ($PBJQ(this).parents('.reorder-element')).insertAfter($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
                 }
                 else {
-                    $PBJQ($PBJQ(this).parents('.reorder-element')).insertBefore($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
+                    $PBJQ($PBJQ(this).parents('.reorder-element')).insertBefore($PBJQ(this).parents('.reorder-element').siblings('.reorder-element').children('span').children('span').children('input[value=' + newVal + ']').parents('.reorder-element'));
                 }
             }
         registerChange('notfluid', $PBJQ(this).parents('.reorder-element'));
