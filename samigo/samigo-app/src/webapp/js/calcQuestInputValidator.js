33956f50915500c973b3d32ca2146fcb7b902825
Curtis van Osch <curtis.vo@gmail.com>
2021-11-01 10:13:27 -0400

    SAK-46489: keep throttle() method in validator js files (#9979)
    Co-authored-by: Curtis van Osch <curtis.van-osch@hec.ca>
    (cherry picked from commit 972bcb6bef2b422d5dc2a1a002d4b76d0ec00330)

--- samigo/samigo-app/src/webapp/js/calcQuestInputValidator.js
+++ samigo/samigo-app/src/webapp/js/calcQuestInputValidator.js
@@ -69,3 +69,14 @@ var validateCalculatedQuestionInput = function(input) {
 
   return true;
 }
+
+function throttle(f, delay) {
+  var timer = null;
+  return function() {
+   var context = this, args = arguments;
+   clearTimeout(timer);
+   timer = window.setTimeout(function() {
+     f.apply(context, args);
+   }, delay || 200);
+  };
+}
