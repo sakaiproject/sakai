da8997e41cf0ebe0aa9c9499b9b92c593ac6d671
Michael Greene <profmikegreene@gmail.com>
2021-11-09 09:59:35 -0500

    TRINITY-7 TRINITY-8 TRINITY-9 TRINITY-10 Library Bootstrap 5 implementation (#9973)
    Co-authored-by: Shawn <shawn.foster@uwo.ca>

---
+++ library/src/trinity/config/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
