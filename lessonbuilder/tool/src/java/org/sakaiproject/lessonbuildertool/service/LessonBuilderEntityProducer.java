426ef91c04fdb4361e2a0ba07648f2335b5ece02
Jesús María Méndez Pérez <jesus.mendez@ticarum.es>
2022-01-10 17:14:34 +0100

    SAK-46422: ERROR: org.sakaiproject.lessonbuildertool.service.LessonBuilderEntityProducer.transferAttachmentFiles (#10100)
    (cherry picked from commit d3dd349d9513ec3cb434ccafb47ee7b1890519c7)

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/service/LessonBuilderEntityProducer.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/service/LessonBuilderEntityProducer.java
@@ -97,6 +97,7 @@ import org.sakaiproject.entitybroker.entityprovider.capabilities.Createable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.InputTranslatable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Statisticable;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
+import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.lessonbuildertool.LessonBuilderAccessAPI;
 import org.sakaiproject.lessonbuildertool.SimpleChecklistItem;
 import org.sakaiproject.lessonbuildertool.SimplePage;
@@ -2283,6 +2284,8 @@ public class LessonBuilderEntityProducer extends AbstractEntityProvider
                         String newReferenceId = StringUtils.replace(oldReferenceId, oldSiteId, newSiteId);
                         contentHostingService.copy(oldReferenceId, newReferenceId);
                         replacedBody = StringUtils.replace(replacedBody, oldSiteId, newSiteId);
+                        } catch(IdUnusedException ide) {
+                            log.warn("Warn transfering file from site {} to site {}.", oldSiteId, newSiteId, ide);
                         } catch(Exception e) {
                             log.error("Error transfering file from site {} to site {}.", oldSiteId, newSiteId, e);
                         }
