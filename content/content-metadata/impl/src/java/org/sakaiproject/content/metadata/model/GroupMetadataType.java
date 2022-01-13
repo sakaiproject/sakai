cc2967a3f2c9733f3d0a4154765455f3748a32d1
Earle Nietzel <earle@longsight.com>
2022-01-13 10:11:26 -0500

    SAK-46790 Resources upgrade content-metadata jackson lib to v2 (#10148)
    https://sakaiproject.atlassian.net/browse/SAK-46790

--- content/content-metadata/impl/src/java/org/sakaiproject/content/metadata/model/GroupMetadataType.java
+++ content/content-metadata/impl/src/java/org/sakaiproject/content/metadata/model/GroupMetadataType.java
@@ -26,8 +26,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * This allows you to group metadata items into a set.
