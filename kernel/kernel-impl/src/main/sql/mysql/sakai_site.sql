bf0ccca93d771b0ecf17099ba9c3cf72d4a6d9e4
ropemar <ropemar@gmail.com>
2022-02-04 10:07:22 +0100

    SAK-46891 Make Dashboard visible by default for users

--- kernel/kernel-impl/src/main/sql/mysql/sakai_site.sql
+++ kernel/kernel-impl/src/main/sql/mysql/sakai_site.sql
@@ -370,6 +370,8 @@ INSERT INTO SAKAI_SITE_TOOL VALUES('!user-610', '!user-600', '!user', 'sakai.pre
 INSERT INTO SAKAI_SITE_PAGE VALUES('!user-700', '!user', 'Account', '0', 9, '0' );
 INSERT INTO SAKAI_SITE_TOOL VALUES('!user-710', '!user-700', '!user', 'sakai.singleuser', 1, 'Account', NULL );
 INSERT INTO SAKAI_SITE_TOOL_PROPERTY VALUES('!user', '!user-710', 'include-password', 'true' );
+INSERT INTO SAKAI_SITE_PAGE VALUES('!user-800', '!user', 'Dashboard', '0', 10, '0' );
+INSERT INTO SAKAI_SITE_TOOL VALUES('!user-810', '!user-800', '!user', 'sakai.dashboard', 1, 'Dashboard', NULL );
 -- sakai.rwiki currently stealthed
 -- INSERT INTO SAKAI_SITE_PAGE VALUES('!user-800', '!user', 'Wiki', '0', 10, '0' );
 -- INSERT INTO SAKAI_SITE_TOOL VALUES('!user-810', '!user-800', '!user', 'sakai.rwiki', 1, 'Wiki', NULL );
