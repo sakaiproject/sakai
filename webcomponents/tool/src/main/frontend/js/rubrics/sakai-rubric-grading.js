fef820ef44c475292bfb13438d0433082ca91b54
Adrian Fish <adrian.r.fish@gmail.com>
2022-02-08 14:59:50 +0000

    SAK-45987 rubrics: cancel evaluation and don't set total points (#10197)
    https://sakaiproject.atlassian.net/browse/SAK-45987
    
    Also, added console.debug to all methods in sakai-rubric-grading.js
    
    (cherry picked from commit b847bd67ba369c063036f1ee769a7ec15600a2e3)

--- webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-grading.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-grading.js
@@ -187,6 +187,8 @@ export class SakaiRubricGrading extends RubricsElement {
 
   updateComment(e) {
 
+    console.debug("updateComment");
+
     this.criteria.forEach(c => {
 
       if (c.id === e.detail.criterionId) {
@@ -194,29 +196,17 @@ export class SakaiRubricGrading extends RubricsElement {
       }
     });
 
-    this._dispatchRatingChanged(this.criteria, 1);
-  }
-
-  calculateTotalPointsFromCriteria() {
-
-    this.totalPoints = this.criteria.reduce((a, c) => {
-
-      if (c.pointoverride) {
-        return a + parseFloat(c.pointoverride);
-      } else if (c.selectedvalue) {
-        return a + parseFloat(c.selectedvalue);
-      }
-      return a;
-
-    }, 0);
+    this.dispatchRatingChanged(this.criteria, 1);
   }
 
   release() {
 
+    console.debug("release");
+
     // If there are no criteria, this evaluation has been cancelled.
     if (this.criteria.length == 0) return;
 
-    this._dispatchRatingChanged(this.criteria, 2).then(evaluation => {
+    this.dispatchRatingChanged(this.criteria, 2).then(evaluation => {
 
       // We've saved the new returned evaluation. We now need to save the returned, backup copy.
 
@@ -250,10 +240,15 @@ export class SakaiRubricGrading extends RubricsElement {
   }
 
   save() {
-    this._dispatchRatingChanged(this.criteria, 1);
+
+    console.debug("save");
+
+    this.dispatchRatingChanged(this.criteria, 1);
   }
 
   decorateCriteria() {
+
+    console.debug("decorateCriteria");
 
     this.evaluation.criterionOutcomes.forEach(ed => {
 
@@ -288,6 +283,8 @@ export class SakaiRubricGrading extends RubricsElement {
 
   fineTuneRating(e) {
 
+    console.debug("fineTuneRating");
+
     const value = e.target.value;
 
     const parsed = value.replace(/,/g, ".");
@@ -315,10 +312,12 @@ export class SakaiRubricGrading extends RubricsElement {
     this.dispatchEvent(new CustomEvent("rubric-rating-tuned", { detail, bubbles: true, composed: true }));
 
     this.updateTotalPoints();
-    this._dispatchRatingChanged(this.criteria, 1);
+    this.dispatchRatingChanged(this.criteria, 1);
   }
 
-  _dispatchRatingChanged(criteria, status) {
+  dispatchRatingChanged(criteria, status) {
+
+    console.debug("dispatchRatingChanged");
 
     const crit = criteria.map(c => {
 
@@ -351,6 +350,8 @@ export class SakaiRubricGrading extends RubricsElement {
 
   saveEvaluation(evaluation) {
 
+    console.debug("saveEvaluation");
+
     let url = "/rubrics-service/rest/evaluations";
     if (this.evaluation && this.evaluation.id) url += `/${this.evaluation.id}`;
     return fetch(url, {
@@ -381,6 +382,8 @@ export class SakaiRubricGrading extends RubricsElement {
 
   deleteEvaluation() {
 
+    console.debug("deleteEvaluation");
+
     if (!this?.evaluation?.id) return;
 
     const url = `/rubrics-service/rest/evaluations/${this.evaluation.id}`;
@@ -394,7 +397,7 @@ export class SakaiRubricGrading extends RubricsElement {
       if (r.ok) {
         this.updateTotalPoints({ notify: true, totalPoints: 0 });
         this.evaluation = { criterionOutcomes: [] };
-        this.criteria = [];
+        this.criteria.forEach(c => this.emptyCriterion(c));
         this.requestUpdate();
       } else {
         throw new Error("Server error while deleting evaluation");
@@ -404,6 +407,8 @@ export class SakaiRubricGrading extends RubricsElement {
   }
 
   getOverriddenClass(ovrdvl, selected) {
+
+    console.debug("getOverriddenClass");
 
     if (!this.association.parameters.fineTunePoints) {
       return '';
@@ -416,7 +421,19 @@ export class SakaiRubricGrading extends RubricsElement {
 
   }
 
+  emptyCriterion(criterion) {
+
+    console.debug("emptyCriterion");
+
+    criterion.selectedvalue = 0.0;
+    criterion.selectedRatingId = 0;
+    criterion.pointoverride = 0.0;
+    criterion.ratings.forEach(r => r.selected = false);
+  }
+
   toggleRating(e) {
+
+    console.debug("toggleRating");
 
     e.stopPropagation();
 
@@ -430,9 +447,7 @@ export class SakaiRubricGrading extends RubricsElement {
     criterion.ratings.forEach(r => r.selected = false);
 
     if (rating.selected) {
-      criterion.selectedvalue = 0.0;
-      criterion.selectedRatingId = 0;
-      criterion.pointoverride = 0.0;
+      this.emptyCriterion(criterion);
       rating.selected = false;
     } else {
       const auxPoints = this.rubric.weighted ?
@@ -451,19 +466,33 @@ export class SakaiRubricGrading extends RubricsElement {
     this.requestUpdate();
     this.updateTotalPoints();
 
-    this._dispatchRatingChanged(this.criteria, 1);
+    this.dispatchRatingChanged(this.criteria, 1);
   }
 
   commentShown(e) {
+
+    console.debug("commentShown");
+
     this.querySelectorAll(`sakai-rubric-grading-comment:not(#${e.target.id})`).forEach(c => c.hide());
   }
 
   updateTotalPoints(options = { notify: true }) {
 
-    if (options.totalPoints) {
+    console.debug("updateTotalPoints");
+
+    if (typeof options.totalPoints !== "undefined") {
       this.totalPoints = options.totalPoints;
     } else {
-      this.calculateTotalPointsFromCriteria();
+      this.totalPoints = this.criteria.reduce((a, c) => {
+
+        if (c.pointoverride) {
+          return a + parseFloat(c.pointoverride);
+        } else if (c.selectedvalue) {
+          return a + parseFloat(c.selectedvalue);
+        }
+        return a;
+
+      }, 0);
     }
 
     // Make sure total points is not negative
@@ -475,11 +504,14 @@ export class SakaiRubricGrading extends RubricsElement {
         entityId: this.entityId,
         value: this.totalPoints.toLocaleString(this.locale, { maximumFractionDigits: 2 }),
       };
+
       this.dispatchEvent(new CustomEvent('total-points-updated', { detail, bubbles: true, composed: true }));
     }
   }
 
   cancel() {
+
+    console.debug("cancel");
 
     if (this.evaluation.status !== "DRAFT") return;
 
@@ -510,6 +542,8 @@ export class SakaiRubricGrading extends RubricsElement {
 
   getAssociation() {
 
+    console.debug("getAssociation");
+
     if (!this.toolId || !this.entityId || !this.token || !this.evaluatedItemId) {
       return;
     }
@@ -537,6 +571,8 @@ export class SakaiRubricGrading extends RubricsElement {
   }
 
   getRubric(rubricId) {
+
+    console.debug("getRubric");
 
     const url = `/rubrics-service/rest/rubrics/${rubricId}?projection=inlineRubric`;
     fetch(url, {
@@ -592,6 +628,8 @@ export class SakaiRubricGrading extends RubricsElement {
   }
 
   getReturnedEvaluation(originalEvaluationId) {
+
+    console.debug("getReturnedEvaluation");
 
     const returnedUrl = `/rubrics-service/rest/returned-evaluations/search/by-original-evaluation-id?id=${originalEvaluationId}`;
     return fetch(returnedUrl, {
