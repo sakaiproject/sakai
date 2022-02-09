3013971be0dd92c17bc45627f038d3d24fab0e35
Fernando Alvarez-Castellanos Rojo <fernando.alvarez@ticarum.es>
2022-02-09 11:27:08 +0100

    SAK-46911 Assignments Disable worklog to a site still shows elements of worklog (#10231)
    (cherry picked from commit 726e2d75e03dbdc1165e3db7cd9fa596d7ff250a)

--- assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
@@ -1476,7 +1476,7 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
             if (assignmentAllPurposeItem != null) {
                 this.allPurposeItemText = assignmentAllPurposeItem.getText();
             }
-            this.estimateRequired = a.getEstimateRequired();
+            this.estimateRequired = assignmentService.isTimeSheetEnabled(a.getContext()) && a.getEstimateRequired();
             this.estimate = a.getEstimate();
 
             this.allowPeerAssessment = a.getAllowPeerAssessment();
