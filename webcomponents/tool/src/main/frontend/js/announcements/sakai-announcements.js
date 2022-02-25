e556c2faa6013c0a4914a8d7c305d47d51ade9f1
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2022-02-25 13:53:59 +0100

    TRINITY-52 Update userId to current in endpoint path

--- webcomponents/tool/src/main/frontend/js/announcements/sakai-announcements.js
+++ webcomponents/tool/src/main/frontend/js/announcements/sakai-announcements.js
@@ -31,7 +31,7 @@ export class SakaiAnnouncements extends SakaiPageableElement {
   async loadAllData() {
 
     const url = this.siteId ? `/api/sites/${this.siteId}/announcements`
-      : `/api/users/${this.userId}/announcements`;
+      : `/api/users/current/announcements`;
 
     return fetch(url)
       .then(r => {
