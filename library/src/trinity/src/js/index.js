50983e1e595404aa5dbeb01f5e2cefb2598cb61b
Shawn Foster <shawn.foster@uwo.ca>
2022-04-05 01:20:32 -0400

    TRINITY-17 Portal implementation of Bootstrap

--- library/src/trinity/src/js/index.js
+++ library/src/trinity/src/js/index.js
@@ -1,8 +1,13 @@
 // Dependencies
 
-// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Trinity JavaScript
+var collapsibleSidebar = document.getElementById('sidebar');
+
+collapsibleSidebar.addEventListener('hidden.bs.collapse', function () {
+  console.log("Sidebar collapsed");
+});
 
 // Custom JavaScript
 import "./_customization-lib.js";
