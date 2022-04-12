c98f77c6695bc76b0ac9080e2b67905512ae7d9d
Adrian Fish <adrian.r.fish@gmail.com>
2022-04-12 17:22:02 +0100

    SAK-46986 Tasks: remove tasks for removed site users (#10302)
    https://sakaiproject.atlassian.net/browse/SAK-46986

--- assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
+++ assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
@@ -1479,6 +1479,7 @@ public class AssignmentServiceImpl implements AssignmentService, EntityTransferr
         task.setReference(reference);
         task.setSystem(true);
         task.setDescription(assignment.getTitle());
+        task.setGroups(assignment.getGroups());
 
         if (!assignment.getHideDueDate()) {
             task.setDue(assignment.getDueDate());
