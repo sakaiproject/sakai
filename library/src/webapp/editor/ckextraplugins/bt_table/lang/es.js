e3002cd964edc571a798ae7ac8918203785e6c6c
PabloArrz <60738066+PabloArrz@users.noreply.github.com>
2022-04-20 10:22:47 +0200

    SAK-47174 Upload spanish translations for CKEditor bt_table extra plugin

---
+++ library/src/webapp/editor/ckextraplugins/bt_table/lang/es.js
@@ -0,0 +1,7 @@
+"use strict"
+CKEDITOR.plugins.setLang( 'bt_table', 'es', {
+  "compactStyle": "Tabla condensada",
+  "addBorders": "Tabla con bordes",
+  "addStripes": "Tabla estilo cebra",
+  "addHover": "Tabla dinámica",
+});
