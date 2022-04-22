5c426d768ed234b81a7f50945a78e0b9d731e897
Adrian Fish <adrian.r.fish@gmail.com>
2022-04-22 18:11:21 +0100

    SAK-47171 Add the profile popup to the sakai-user-photo (#10432)
    * SAK-47171 Add the profile popup to the sakai-user-photo
    
    https://sakaiproject.atlassian.net/browse/SAK-47171
    
    * removed unnecessary aria labels

--- webcomponents/tool/src/main/frontend/js/sakai-user-photo.js
+++ webcomponents/tool/src/main/frontend/js/sakai-user-photo.js
@@ -1,6 +1,20 @@
 import { SakaiElement } from "./sakai-element.js";
 import { html } from "./assets/lit-html/lit-html.js";
 
+/**
+ * A simple wrapper for Sakai's profile picture.
+ *
+ * Usage:
+ *
+ * Renders adrian's profile picture and pops up the profile panel when clicked.
+ * <sakai-user-photo user-id="adrian">
+ *
+ * Renders adrian's profile picture without a popup
+ * <sakai-user-photo user-id="adrian" no-popup>
+ *
+ * Renders adrian's profile picture with a popup and some custom styles from the supplied class.
+ * <sakai-user-photo user-id="adrian" style-class="custom">
+ */
 class SakaiUserPhoto extends SakaiElement {
 
   constructor() {
@@ -8,7 +22,6 @@ class SakaiUserPhoto extends SakaiElement {
     super();
 
     this.sizeClass = "large-thumbnail";
-
   }
 
   static get properties() {
@@ -16,22 +29,41 @@ class SakaiUserPhoto extends SakaiElement {
     return {
       userId: { attribute: "user-id", type: String },
       sizeClass: { attribute: "size-class", type: String },
+      noPopup: { attribute: "no-popup", type: Boolean },
+      official: { type: Boolean },
     };
   }
+
+  set userId(value) {
+
+    const old = this._userId;
+    this._userId = value;
+    this.generatedId = `sakai-user-photo-${this.userId}-${Math.floor(Math.random() * 100)}`;
+    this.requestUpdate("userId", old);
+
+    if (!this.noPopup) {
+      this.updateComplete.then(() => {
+        profile.attachPopups($(`#${this.generatedId}`));
+      });
+    }
+  }
+
+  get userId() { return this._userId; }
 
   shouldUpdate() {
     return this.userId && this.sizeClass;
   }
 
   render() {
+
     return html`
-      <div class="sakai-user-photo ${this.sizeClass}"
-            style="background-image:url(/direct/profile/${this.userId}/image/thumb)">
-      </div>
+      <div id="${this.generatedId}"
+          data-user-id="${this.userId}"
+          class="sakai-user-photo ${this.sizeClass}"
+          style="background-image:url(/direct/profile/${this.userId}/image/${this.official ? "official" : "thumb"})">
     `;
   }
 }
 
-if (!customElements.get("sakai-user-photo")) {
-  customElements.define("sakai-user-photo", SakaiUserPhoto);
-}
+const tagName = "sakai-user-photo";
+!customElements.get(tagName) && customElements.define(tagName, SakaiUserPhoto);
