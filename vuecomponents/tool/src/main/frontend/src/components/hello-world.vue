a9c056a709c984b9beaa9b326218b7f81a7c9734
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2022-04-28 09:54:35 +0200

    SAK-47228 Enable building webcomponents based on vue

---
+++ vuecomponents/tool/src/main/frontend/src/components/hello-world.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="hi">{{ i18n.helloworld }}</span>
+</template>
+
+<style scoped>
+.hi {
+  color: var(--sakai-text-color-3);
+  background-color: var(--sakai-background-color-3);
+  border: 2px solid var(--sakai-border-color-3);
+  padding: 5px;
+}
+</style>
+
+<script>
+import i18nMixin from "../mixins/i18n-mixin.js";
+
+export default {
+  name: "hello-world",
+  mixins: [i18nMixin],
+};
+</script>
