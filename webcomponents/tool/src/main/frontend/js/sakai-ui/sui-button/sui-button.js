b2c64ac0c464caa20e8796be151d06a487d1ea8a
Adrian Fish <adrian.r.fish@gmail.com>
2022-05-01 17:45:56 +0100

    removed the debug from sui-button.

--- webcomponents/tool/src/main/frontend/js/sakai-ui/sui-button/sui-button.js
+++ webcomponents/tool/src/main/frontend/js/sakai-ui/sui-button/sui-button.js
@@ -1,8 +1,5 @@
-// TODO Review commented lines
-// TODO Review need for shadown DOM
 import { html } from "@assets/lit-element/lit-element.js";
 import { SakaiElement } from "../../sakai-element.js";
-// import styles from "./sui-button.scss";
 import "../sui-icon/sui-icon";
 
 export class SakaiUIButton extends SakaiElement {
@@ -16,9 +13,8 @@ export class SakaiUIButton extends SakaiElement {
       buttonClass: { attribute: "button-class", type: String },
       target: { type: String },
       icon: { type: String },
-      title: { type: String },
+      buttonTitle: { attribute: "button-title", type: String },
       label: { type: String },
-      debug: { type: Boolean },
     };
   }
 
@@ -29,35 +25,12 @@ export class SakaiUIButton extends SakaiElement {
     this.buttonClass = this.classList.value;
     // This prevents duplicate styles from being added to the component
     this.classList = "";
-    this.debug = false;
 
     this.loadTranslations("sui-button").then(r => this.i18n = r);
   }
 
-  connectedCallback() {
-
-    super.connectedCallback();
-    this.debug
-      ? console.debug(`sui-button ${this.title} connectedCallback`)
-      : null;
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-
-    this.debug
-      ? console.debug(
-          `sui-button ${this.title} attribute change: `,
-          name,
-          typeof newVal,
-          newVal
-        )
-      : null;
-    super.attributeChangedCallback(name, oldVal, newVal);
-  }
-
   clicked(e) {
 
-    this.debug ? console.debug(`sui-button ${this.title} clicked`, e) : null;
     if (this.href) {
       if (this.target === "_blank") {
         window.open(this.href);
@@ -69,18 +42,8 @@ export class SakaiUIButton extends SakaiElement {
 
     // Non-href buttons
     if (this.onclick) {
-      this.debug
-        ? console.debug(`sui-button ${this.title} onclick`, this.onclick)
-        : null;
       this.onclick;
     }
-  }
-
-  updated(changedProperties) {
-
-    this.debug
-      ? console.debug(`sui-button ${this.title} start updated`, changedProperties)
-      : null;
   }
 
   render() {
