9ad9385acb1510fe9cf9931d699c70883f9095a4
stetsche <53173679+stetsche@users.noreply.github.com>
2022-05-09 10:02:38 +0200

    SAK-47228 Enable building webcomponents based on vue (#10484)

---
+++ vuecomponents/tool/src/main/frontend/src/components/placeholder.vue
@@ -0,0 +1,28 @@
+/* This component serves as a placeholder, because we need two components for a
+bundle it will be replaced by the avatar component */
+<template>
+  <span class="hi">{{ i18n.helloworld }}</span>
+</template>
+
+<style scoped>
+.hi {
+  color: var(--sakai-text-color-3);
+  background-color: var(--sakai-background-color-3);
+  border: 2px solid var(--sakai-border-color-3);
+  padding: 5px;
+}
+</style>
+
+<script>
+import i18nMixin from "../mixins/i18n-mixin.js";
+
+export default {
+  name: "placeholder",
+  mixins: [i18nMixin],
+  data() {
+    return {
+      i18nProps: "hello-world",
+    };
+  },
+};
+</script>
