ac74212179fc072ebc921cc9a9dd92ada44e2c14
Adrian Fish <adrian.r.fish@gmail.com>
2022-05-11 15:45:03 +0100

    SAK-47293 Conversations: Don't show seconds in date picker (#10545)
    https://sakaiproject.atlassian.net/browse/SAK-47293
    (cherry picked from commit d18436cc915a747114dc395bd4182699436c3764)

--- webcomponents/tool/src/main/frontend/js/sakai-date-picker.js
+++ webcomponents/tool/src/main/frontend/js/sakai-date-picker.js
@@ -30,15 +30,15 @@ class SakaiDatePicker extends LitElement {
 
     if (value) {
       this._epochMillis = value;
-      this.isoDate = (new Date(value + parseInt(getOffsetFromServerMillis()))).toISOString().substring(0, 19);
+      this.isoDate = (new Date(value + parseInt(getOffsetFromServerMillis()))).toISOString().substring(0, 16);
+
     } else {
       this._epochMillis = null;
       this.isoDate = null;
     }
+
     const inputDate = this.shadowRoot.getElementById("date-picker-input");
-    if (inputDate) {
-      inputDate.value = this.isoDate;
-    }
+    inputDate && (inputDate.value = this.isoDate);
   }
 
   get epochMillis() { return this._epochMillis; }
@@ -55,7 +55,7 @@ class SakaiDatePicker extends LitElement {
 
   dateSelected(e) {
 
-    const epochMillis = ((new Date(e.target.value)).getTime());
+    const epochMillis = (new Date(e.target.value)).getTime();
     this.dispatchEvent(new CustomEvent("datetime-selected", { detail: { epochMillis }, bubbles: true }));
   }
 
