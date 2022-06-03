f46f3a59255bf2853810c4d3b9fe88750116b482
Fernando Alvarez-Castellanos Rojo <fernando.alvarez@ticarum.es>
2022-06-03 10:35:40 +0200

    SAK-47306 Forums - Word count is missing on the 'Statistics & Grading' tab (#10608)
    * SAK-47306 Forums - Word count is missing on the 'Statistics & Grading' tab
    
    * Adding alt text to images
    
    (cherry picked from commit faeffaeb49a48ab9900f0146ceb85953eacebcc4)

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/ui/MessageForumStatisticsBean.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/ui/MessageForumStatisticsBean.java
@@ -33,6 +33,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 import javax.faces.application.FacesMessage;
@@ -964,6 +965,7 @@ public class MessageForumStatisticsBean {
 			userAuthoredInfo.setForumDate(msg.getCreated());
 			userAuthoredInfo.setForumSubject(msg.getTitle());
 			userAuthoredInfo.setMessage(msg.getBody());
+			userAuthoredInfo.setWordCount(new StringTokenizer(msg.getBody()).countTokens());
 			userAuthoredInfo.setMsgId(Long.toString(msg.getId()));
 			userAuthoredInfo.setTopicId(Long.toString(msg.getTopic().getId()));
 			userAuthoredInfo.setForumId(Long.toString(msg.getTopic().getOpenForum().getId()));
@@ -1043,6 +1045,7 @@ public class MessageForumStatisticsBean {
 				userAuthoredInfo.setForumDate(mesWithAttach.getCreated());
 				userAuthoredInfo.setForumSubject(mesWithAttach.getTitle());
 				userAuthoredInfo.setMessage(mesWithAttach.getBody());
+				userAuthoredInfo.setWordCount(new StringTokenizer(mesWithAttach.getBody()).countTokens());
 				userAuthoredInfo.setMsgId(selectedMsgId);
 				userAuthoredInfo.setTopicId(Long.toString(t.getId()));
 				userAuthoredInfo.setForumId(Long.toString(d.getId()));
