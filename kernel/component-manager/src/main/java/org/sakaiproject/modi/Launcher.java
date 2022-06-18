ea8b3acb3dbdfd5d87a65f65587b4a5783430c94
Noah Botimer <noah@botimer.net>
2022-06-17 23:56:55 -0400

    Switch from SharedApplicationContext to GenericApplicationContext
    Getting the property overrides and placeholders loading in the normal
    startup phase was the last piece to using a standard Spring context. At
    this point, the SharedApplicationContext is unused, and probably not
    necessary. I'm leaving it in place for the moment, because it extends
    AbstractRefreshableApplicationContext, which could theoretically open
    the chance for reloadability.
    
    However, if we can fix up SakaiProperties and use regular beans and
    environment inheritance/resolution, we could load the kernel in a
    generic context and let each component build its own child context. That
    would require an entrypoint that delivers the parent context and
    initiates component load, but that seems like the logical progression.
    
    There doesn't seem to be much of a use case for kernel reloading if
    components could be redeployed, especially if there is parallel or lazy
    loading of tool webapps. The BDD/TDD cycle for tools can now be
    dramatically faster and easier. Reloadable components (and probably
    some proxy/reload trigger for the tools) would shorten the cycle time
    for interactive testing and changes.

--- kernel/component-manager/src/main/java/org/sakaiproject/modi/Launcher.java
+++ kernel/component-manager/src/main/java/org/sakaiproject/modi/Launcher.java
@@ -16,6 +16,7 @@
 package org.sakaiproject.modi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.GenericApplicationContext;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -57,7 +58,7 @@ public class Launcher {
 
     protected final Environment env;
     protected ComponentsDirectory components;
-    protected SharedApplicationContext context;
+    protected GenericApplicationContext context;
 
     /**
      * Start the Sakai instance.
@@ -76,8 +77,8 @@ public class Launcher {
         log.info("Booting Sakai in Modern Dependency Injection Mode");
         System.setProperty("sakai.modi.enabled", "true");
 
-        context = new SharedApplicationContext();
-        context.registerBeanSource(getConfiguration());
+        context = new GenericApplicationContext();
+        getConfiguration().registerBeans(context);
 
         components = new ComponentsDirectory(env.getComponentsRoot(), env.getOverridesFolder());
         components.starting(context);
