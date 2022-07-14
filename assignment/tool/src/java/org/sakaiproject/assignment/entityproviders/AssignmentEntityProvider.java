4d3d1bb123f9d081c97664dd7ec424f1a992f547
Fernando Álvarez-Castellanos Rojo <fernando.alvarez@ticarum.es>
2022-07-14 18:46:33 +0200

    SAK-47458 Assignments - TA can see and grade student submissions from groups to which TA does not belong

--- assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
@@ -678,7 +678,7 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
 
         Assignment assignment = null;
         try {
-            assignment = assignmentService.getAssignment(gradableId);
+            assignment = assignmentService.getAssignmentWithGradeableSubmissions(gradableId);
         } catch (IdUnusedException e) {
             throw new EntityException("No assignment for id", gradableId, HttpServletResponse.SC_BAD_REQUEST);
         } catch (PermissionException e) {
