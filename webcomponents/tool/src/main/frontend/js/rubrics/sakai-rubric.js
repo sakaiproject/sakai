60b995b4ff6ee7e480ecc11a5b56e8f3979ba3b5
Adrian Fish <adrian.r.fish@gmail.com>
2022-07-14 08:54:57 +0100

    SAK-47574 rubrics: fixed search by title, site and creator (#10741)
    https://sakaiproject.atlassian.net/browse/SAK-47574

--- webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric.js
@@ -49,6 +49,17 @@ export class SakaiRubric extends RubricsElement {
 
   get rubric() { return this._rubric; }
 
+  matches(search) {
+
+    const rubricTitle = this.querySelector('.rubric-name').textContent;
+    const rubricAuthor = this.querySelector('.rubric-creator-name').textContent;
+    const rubricSite = this.querySelector('.rubric-site-title').textContent;
+
+    return rubricAuthor.toLowerCase().includes(search) ||
+            rubricTitle.toLowerCase().includes(search) ||
+            rubricSite.toLowerCase().includes(search);
+  }
+
   shouldUpdate() {
     return this.rubric;
   }
@@ -70,8 +81,8 @@ export class SakaiRubric extends RubricsElement {
           }
         </div>
 
-        <div class="hidden-xs">${this.rubric.siteTitle}</div>
-        <div class="hidden-xs">${this.rubric.creatorDisplayName}</div>
+        <div class="hidden-xs rubric-site-title">${this.rubric.siteTitle}</div>
+        <div class="hidden-xs rubric-creator-name">${this.rubric.creatorDisplayName}</div>
         <div class="hidden-xs">${this.rubric.formattedModifiedDate}</div>
 
         <div class="actions">
