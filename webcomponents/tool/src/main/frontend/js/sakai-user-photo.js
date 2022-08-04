6e5ff7d2061f57af00ea81a7788c0815a4d78dd3
Adrian Fish <adrian.r.fish@gmail.com>
2022-08-04 16:42:22 +0100

    SAK-47614 roster: pass title to user photo
    https://sakaiproject.atlassian.net/browse/SAK-47614

--- webcomponents/tool/src/main/frontend/js/sakai-user-photo.js
+++ webcomponents/tool/src/main/frontend/js/sakai-user-photo.js
@@ -1,5 +1,6 @@
 import { SakaiElement } from "./sakai-element.js";
 import { html } from "./assets/lit-html/lit-html.js";
+import { ifDefined } from "./assets/lit-html/directives/if-defined.js";
 
 /**
  * A simple wrapper for Sakai's user profile picture.
@@ -13,6 +14,7 @@ import { html } from "./assets/lit-html/lit-html.js";
  * @property {string} [popup] By default, profile popups are off. Set this to "on" if you want them
  * @property {boolean} [official] Set this if you want the official Sakai photo
  * @property {string} [site-id] Set this to trigger permissions checks on the photo
+ * @property {string} [label] This will be used as the title and aria-label for the div
  * @property {boolean} [print] Set this to trigger the render of a print friendly img tag
  */
 class SakaiUserPhoto extends SakaiElement {
@@ -33,6 +35,7 @@ class SakaiUserPhoto extends SakaiElement {
       popup: { type: String },
       official: { type: Boolean },
       siteId: { attribute: "site-id", type: String },
+      label: { type: String },
       print: { type: Boolean },
     };
   }
@@ -63,7 +66,7 @@ class SakaiUserPhoto extends SakaiElement {
 
     if (this.print) {
       return html`
-        <img src="${this.url}" alt="Print view only" />
+        <img src="${this.url}" alt="${ifDefined(this.label ? this.label : undefined)}" />
       `;
     }
 
@@ -71,7 +74,9 @@ class SakaiUserPhoto extends SakaiElement {
       <div id="${this.generatedId}"
           data-user-id="${this.userId}"
           class="sakai-user-photo ${this.classes}"
-          style="background-image:url(${this.url}) ${this.noPopup ? "" : ";cursor: pointer;"}">
+          aria-label="${ifDefined(this.label ? this.label : undefined)}"
+          title="${ifDefined(this.label ? this.label : undefined)}"
+          style="background-image:url(${this.url}) ${this.popup === SakaiUserPhoto.OFF ? "" : ";cursor: pointer;"}">
     `;
   }
 }
