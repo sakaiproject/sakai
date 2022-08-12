a040d6ed5a6fd0e3fb617b95f92004c2398a7cf6
Vicent Alfonso <81161239+V100X@users.noreply.github.com>
2022-08-12 18:01:59 +0200

    SAK-47640 Rubrics Fix: Total Grade not updating correctly (#10796)

--- webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-criteria.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-criteria.js
@@ -9,7 +9,6 @@ import "./sakai-rubric-criterion-rating-edit.js";
 import { SharingChangeEvent } from "./sharing-change-event.js";
 import { Sortable } from "/webcomponents/assets/sortablejs/modular/sortable.esm.js";
 import { tr } from "./sakai-rubrics-language.js";
-import {calculateCriteriaPoints} from "./sakai-rubrics-utils.js";
 
 export class SakaiRubricCriteria extends RubricsElement {
 
@@ -351,6 +350,7 @@ export class SakaiRubricCriteria extends RubricsElement {
         rating.new = false;
         this.requestUpdate();
         this.letShareKnow();
+        this.dispatchEvent(new CustomEvent('refresh-total-weight', { detail: { criteria: this.criteria } }));
       } else {
         throw new Error("Network error while saving rating");
       }
@@ -449,8 +449,13 @@ export class SakaiRubricCriteria extends RubricsElement {
     })
     .then(c => {
 
+      // The current weight may be lost because comes from DB and the UI can have a different value.
+      c.weight = criterion.weight;
+
       Object.assign(criterion, c);
       this.requestUpdate();
+      this.letShareKnow();
+      this.dispatchEvent(new CustomEvent('refresh-total-weight', { detail: { criteria: this.criteria } }));
     })
     .catch (error => console.error(error));
   }
@@ -470,7 +475,10 @@ export class SakaiRubricCriteria extends RubricsElement {
     let totalPoints = 0;
     const criterion = this.criteria.find(c => c.id == criterionId);
 
-    totalPoints += calculateCriteriaPoints(criterion, minOrMax);
+    totalPoints += minOrMax(...criterion.ratings.map( (rating) => {
+      return rating.points * (criterion.weight / 100);
+    }));
+
     return parseFloat(totalPoints).toLocaleString(this.locale);
   }
 
