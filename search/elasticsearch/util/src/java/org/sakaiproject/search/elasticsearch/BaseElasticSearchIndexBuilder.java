06dd37d343a6df7044bbb27e6e6f20f2742e34a8
Sam Ottenhoff <ottenhoff@longsight.com>
2022-09-12 10:14:28 -0400

    SAK-47814 Search avoid matching an ECP with a null ref #10864
    (cherry picked from commit 1e37f2b400c22e4be9f13edc2d44c24895fd6531)

--- search/elasticsearch/util/src/java/org/sakaiproject/search/elasticsearch/BaseElasticSearchIndexBuilder.java
+++ search/elasticsearch/util/src/java/org/sakaiproject/search/elasticsearch/BaseElasticSearchIndexBuilder.java
@@ -1351,6 +1351,8 @@ public abstract class BaseElasticSearchIndexBuilder implements ElasticSearchInde
      */
     @Override
     public EntityContentProducer newEntityContentProducer(String ref) {
+        if (ref == null) return null;
+
         final Optional<EntityContentProducer> producer = matchEntityContentProducer(p -> p.matches(ref));
         if ( producer.isPresent() ) {
             getLog().debug("Matched content producer " + producer.get() + " for reference " + ref + " in index builder "
