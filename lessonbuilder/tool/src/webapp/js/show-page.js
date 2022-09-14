a7b0bb5c06285a150ff4b9fc23998c7e9ceeb5e5
Charles Severance <csev@umich.edu>
2022-09-14 10:10:05 -0400

    Update lessonbuilder/tool/src/webapp/js/show-page.js
    Co-authored-by: Brian Jones <bjones86@uwo.ca>

--- lessonbuilder/tool/src/webapp/js/show-page.js
+++ lessonbuilder/tool/src/webapp/js/show-page.js
@@ -2979,7 +2979,7 @@ $(document).ready(function() {
 		var href = this.href;
 		if ( this.href === null ) return;
 		if ( this.href.indexOf('/access/basiclti') < 0 ) return
-		if ( '_blank' != this.target ) return
+		if ( '_blank' !== this.target ) return
 		if ( this.onclick != null ) return;
 		console.log('Patching an LTI Launch in new window!', this.href);
 		this.onclick = function () { window.open(this.href,'_blank');return false; };
