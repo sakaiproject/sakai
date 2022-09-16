24f3e128d01367240f0bd86b04c00d57d90d6f69
Earle Nietzel <earle@longsight.com>
2022-09-16 13:40:03 -0400

    Revert "Revert "SAK-47458 GradebookNG: No groups are shown to instructor in the send message to students dialog (#10738)""
    This reverts commit c8ba4fb3cd5d72fd2e41eadeb14c2c4e0026b946.

--- entitybroker/core-providers/src/java/org/sakaiproject/entitybroker/providers/SiteEntityProvider.java
+++ entitybroker/core-providers/src/java/org/sakaiproject/entitybroker/providers/SiteEntityProvider.java
@@ -30,6 +30,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.commons.lang3.StringUtils;
 import org.azeckoski.reflectutils.ReflectUtils;
 
 import org.sakaiproject.authz.api.AuthzGroup;
@@ -125,6 +126,8 @@ public class SiteEntityProvider extends AbstractEntityProvider implements CoreEn
     public static final String PROP_SITE_PROVIDER_PAGESIZE_MAXIMUM = "site.entity.pagesize.maximum";
 
     private static final String PERMISSION_QUERY_PROPERTY_NAME = "permission";
+
+    private static final String INSTRUCTOR_ROLE_ID = "Instructor";
 
     /**
      * The default page size for lists of entities. May be overridden with
@@ -312,7 +315,8 @@ public class SiteEntityProvider extends AbstractEntityProvider implements CoreEn
         // check if the user can access site
         isAllowedAccessSite(site);
 
-        List<EntityGroup> groups = securityService.isSuperUser() ?
+        List<EntityGroup> groups =
+                securityService.isSuperUser() || StringUtils.equalsIgnoreCase(site.getUserRole(sessionManager.getCurrentSessionUserId()).getId(), INSTRUCTOR_ROLE_ID)  ?
                 site.getGroups().stream().map(EntityGroup::new).collect(Collectors.toList())
                 :
                 site.getGroupsWithMember(sessionManager.getCurrentSessionUserId()).stream()
