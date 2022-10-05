be8eba0bb45212c611c729c252881da737cd1e2c
Adrian Fish <adrian.r.fish@gmail.com>
2022-10-05 10:18:58 +0100

    SAK-47926 bullhorns: forward sse events to all clients (#10927)
    https://sakaiproject.atlassian.net/browse/SAK-47926

--- webapi/src/main/resources/static/sakai-sse-service-worker.js
+++ webapi/src/main/resources/static/sakai-sse-service-worker.js
@@ -11,10 +11,9 @@ self.addEventListener("message", clientEvent => {
 
       self.eventSource.addEventListener(clientEvent.data, async message => {
 
+        const stripped = { type: message.type, data: JSON.parse(message.data) };
         const clients = await self.clients.matchAll({ includeUncontrolled: true });
-        const client = clients?.length && clients[0];
-        const strippedMessage = { type: message.type, data: JSON.parse(message.data) };
-        client && client.postMessage(strippedMessage);
+        clients && clients.filter(c => c.visibilityState === "visible").forEach(c => c.postMessage(stripped));
       });
     };
   }
