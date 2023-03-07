8215454890913dd2463b472212a4a68a30df7cf8
Earle Nietzel <earle@longsight.com>
2023-03-07 11:14:49 -0500

    SAK-48533 Content allow spring to handle dependency injection for content types (#11318)
    * SAK-48533 Content allow spring to handle dependency injection for content types
    
    https://sakaiproject.atlassian.net/browse/SAK-48533
    (cherry picked from commit 5b4a22455fb48198a93de6792a6bb3fcc522a6d7)

--- content/content-types/src/java/org/sakaiproject/content/types/UrlResourceType.java
+++ content/content-types/src/java/org/sakaiproject/content/types/UrlResourceType.java
@@ -24,188 +24,79 @@ package org.sakaiproject.content.types;
 import static org.sakaiproject.content.api.ResourceToolAction.*;
 
 import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.content.api.ContentEntity;
 import org.sakaiproject.content.api.ResourceToolAction;
 import org.sakaiproject.content.api.ResourceType;
 import org.sakaiproject.content.api.ResourceToolAction.ActionType;
+import org.sakaiproject.content.api.ResourceTypeRegistry;
 import org.sakaiproject.content.util.BaseInteractionAction;
 import org.sakaiproject.content.util.BaseResourceType;
 import org.sakaiproject.content.util.BaseServiceLevelAction;
-import org.sakaiproject.content.util.BaseResourceAction.Localizer;
-import org.sakaiproject.entity.api.Reference;
-import org.sakaiproject.user.api.User;
 import org.sakaiproject.util.Resource;
-import org.sakaiproject.util.ResourceLoader;
 
-public class UrlResourceType extends BaseResourceType 
-{
-	protected String typeId = ResourceType.TYPE_URL;
-	protected String helperId = "sakai.resource.type.helper";
-	
-	/** localized tool properties **/
-	private static final String DEFAULT_RESOURCECLASS = "org.sakaiproject.localization.util.TypeProperties";
-	private static final String DEFAULT_RESOURCEBUNDLE = "org.sakaiproject.localization.bundle.type.types";
-	private static final String RESOURCECLASS = "resource.class.type";
-	private static final String RESOURCEBUNDLE = "resource.bundle.type";
-	private ServerConfigurationService serverConfigurationService =  (ServerConfigurationService) ComponentManager.get("org.sakaiproject.component.api.ServerConfigurationService");;
-	private String resourceClass = serverConfigurationService.getString(RESOURCECLASS, DEFAULT_RESOURCECLASS);
-	private String resourceBundle = serverConfigurationService.getString(RESOURCEBUNDLE, DEFAULT_RESOURCEBUNDLE);
-	private ResourceLoader rb = Resource.getResourceLoader(resourceClass, resourceBundle);
+import lombok.Setter;
 
-	// private static ResourceLoader rb = new ResourceLoader("types");
-	
-	protected EnumMap<ActionType, List<ResourceToolAction>> actionMap = new EnumMap<ActionType, List<ResourceToolAction>>(ActionType.class);
+public class UrlResourceType extends BaseResourceType {
 
-	protected Map<String, ResourceToolAction> actions = new HashMap<String, ResourceToolAction>();	
+    @Setter private ResourceTypeRegistry resourceTypeRegistry;
+    @Setter private ServerConfigurationService serverConfigurationService;
 
+    protected String typeId = ResourceType.TYPE_URL;
 
+    public void init() {
+        String resourceClassName = serverConfigurationService.getString(RESOURCECLASS, DEFAULT_RESOURCECLASS);
+        String resourceBundleName = serverConfigurationService.getString(RESOURCEBUNDLE, DEFAULT_RESOURCEBUNDLE);
+        setMessageSource(Resource.getResourceLoader(resourceClassName, resourceBundleName));
 
-	
-	private Localizer localizer(final String string) {
-		return new Localizer() {
+        String helperId = "sakai.resource.type.helper";
 
-			public String getLabel() {
-				return rb.getString(string);
-			}
-			
-		};
-	}
-	
-	public UrlResourceType()
-	{		
-		actions.put(CREATE, new BaseInteractionAction(CREATE, ActionType.NEW_URLS, typeId, helperId, localizer("create.urls")));
-		actions.put(REVISE_CONTENT, new BaseInteractionAction(REVISE_CONTENT, ActionType.REVISE_CONTENT, typeId, helperId, localizer("action.revise")));
-		actions.put(ACCESS_PROPERTIES, new BaseServiceLevelAction(ACCESS_PROPERTIES, ActionType.VIEW_METADATA, typeId, false, localizer("action.access")));
-		actions.put(REVISE_METADATA, new BaseServiceLevelAction(REVISE_METADATA, ActionType.REVISE_METADATA, typeId, false, localizer("action.props")));
-		actions.put(DUPLICATE, new BaseServiceLevelAction(DUPLICATE, ActionType.DUPLICATE, typeId, false, localizer("action.duplicate")));
-		actions.put(COPY, new BaseServiceLevelAction(COPY, ActionType.COPY, typeId, true, localizer("action.copy")));
-		actions.put(MOVE, new BaseServiceLevelAction(MOVE, ActionType.MOVE, typeId, true, localizer("action.move")));
-		actions.put(DELETE, new BaseServiceLevelAction(DELETE, ActionType.DELETE, typeId, true, localizer("action.delete")));
-		actions.put(MAKE_SITE_PAGE, new MakeSitePageAction(MAKE_SITE_PAGE, ActionType.MAKE_SITE_PAGE, typeId));
-		
-		// initialize actionMap with an empty List for each ActionType
-		for(ActionType type : ActionType.values())
-		{
-			actionMap.put(type, new ArrayList<ResourceToolAction>());
-		}
-		
-		// for each action in actions, add a link in actionMap
-		Iterator<String> it = actions.keySet().iterator();
-		while(it.hasNext())
-		{
-			String id = it.next();
-			ResourceToolAction action = actions.get(id);
-			List<ResourceToolAction> list = actionMap.get(action.getActionType());
-			if(list == null)
-			{
-				list = new ArrayList<ResourceToolAction>();
-				actionMap.put(action.getActionType(), list);
-			}
-			list.add(action);
-		}
-		
-	}
+        actions.put(CREATE, new BaseInteractionAction(CREATE, ActionType.NEW_URLS, typeId, helperId, localizer("create.urls")));
+        actions.put(REVISE_CONTENT, new BaseInteractionAction(REVISE_CONTENT, ActionType.REVISE_CONTENT, typeId, helperId, localizer("action.revise")));
+        actions.put(ACCESS_PROPERTIES, new BaseServiceLevelAction(ACCESS_PROPERTIES, ActionType.VIEW_METADATA, typeId, false, localizer("action.access")));
+        actions.put(REVISE_METADATA, new BaseServiceLevelAction(REVISE_METADATA, ActionType.REVISE_METADATA, typeId, false, localizer("action.props")));
+        actions.put(DUPLICATE, new BaseServiceLevelAction(DUPLICATE, ActionType.DUPLICATE, typeId, false, localizer("action.duplicate")));
+        actions.put(COPY, new BaseServiceLevelAction(COPY, ActionType.COPY, typeId, true, localizer("action.copy")));
+        actions.put(MOVE, new BaseServiceLevelAction(MOVE, ActionType.MOVE, typeId, true, localizer("action.move")));
+        actions.put(DELETE, new BaseServiceLevelAction(DELETE, ActionType.DELETE, typeId, true, localizer("action.delete")));
+        actions.put(MAKE_SITE_PAGE, new MakeSitePageAction(MAKE_SITE_PAGE, ActionType.MAKE_SITE_PAGE, typeId));
 
-	public ResourceToolAction getAction(String actionId) 
-	{
-		return actions.get(actionId);
-	}
+        // initialize actionMap with an empty List for each ActionType
+        for (ActionType type : ActionType.values()) {
+            actionMap.put(type, new ArrayList<>());
+        }
 
-	public List<ResourceToolAction> getActions(Reference entityRef, Set permissions) 
-	{
-		// TODO: use entityRef to filter actions
-		List<ResourceToolAction> rv = new ArrayList<ResourceToolAction>();
-		rv.addAll(actions.values());
-		return rv;
-	}
+        // for each action in actions, add a link in actionMap
+        for (String id : actions.keySet()) {
+            ResourceToolAction action = actions.get(id);
+            List<ResourceToolAction> list = actionMap.computeIfAbsent(action.getActionType(), k -> new ArrayList<>());
+            list.add(action);
+        }
 
-	public List<ResourceToolAction> getActions(Reference entityRef, User user, Set permissions) 
-	{
-		// TODO: use entityRef and user to filter actions
-		List<ResourceToolAction> rv = new ArrayList<ResourceToolAction>();
-		rv.addAll(actions.values());
-		return rv;
-	}
+        resourceTypeRegistry.register(this);
+    }
 
-	public String getIconLocation(ContentEntity entity) 
-	{
-		return null;
-	}
-	
-	public String getIconClass(ContentEntity entity) 
-	{
-		return null;
-	}
-	
-	public String getId() 
-	{
-		return typeId;
-	}
+    @Override
+    public String getId() {
+        return typeId;
+    }
 
-	public String getLabel() 
-	{
-		return rb.getString("type.url");
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.sakaiproject.content.api.ResourceType#getLocalizedHoverText(org.sakaiproject.entity.api.Reference)
-	 */
-	public String getLocalizedHoverText(ContentEntity member)
-	{
-		return rb.getString("type.url");
-	}
+    @Override
+    public String getLabel() {
+        return localizer("type.url").getI18nString();
+    }
 
-	/* (non-Javadoc)
-	 * @see org.sakaiproject.content.api.ResourceType#getActions(org.sakaiproject.content.api.ResourceType.ActionType)
-	 */
-	public List<ResourceToolAction> getActions(ActionType type)
-	{
-		List<ResourceToolAction> list = actionMap.get(type);
-		if(list == null)
-		{
-			list = new ArrayList<ResourceToolAction>();
-			actionMap.put(type, list);
-		}
-		return new ArrayList<ResourceToolAction>(list);
-	}
+    @Override
+    public String getLocalizedHoverText(ContentEntity member) {
+        return localizer("type.url").getI18nString();
+    }
 
-	/* (non-Javadoc)
-	 * @see org.sakaiproject.content.api.ResourceType#getActions(java.util.List)
-	 */
-	public List<ResourceToolAction> getActions(List<ActionType> types)
-	{
-		List<ResourceToolAction> list = new ArrayList<ResourceToolAction>();
-		if(types != null)
-		{
-			Iterator<ActionType> it = types.iterator();
-			while(it.hasNext())
-			{
-				ActionType type = it.next();
-				List<ResourceToolAction> sublist = actionMap.get(type);
-				if(sublist == null)
-				{
-					sublist = new ArrayList<ResourceToolAction>();
-					actionMap.put(type, sublist);
-				}
-				list.addAll(sublist);
-			}
-		}
-		return list;
-	}
-
-	@Override
-    public boolean hasRightsDialog()
-    {
-		return serverConfigurationService.getBoolean("content.url.rightsdialog", true);
+    @Override
+    public boolean hasRightsDialog() {
+        return serverConfigurationService.getBoolean("content.url.rightsdialog", true);
     }
 
 }
--- kernel/kernel-storage-util/src/main/java/org/sakaiproject/content/util/BaseResourceType.java
+++ kernel/kernel-storage-util/src/main/java/org/sakaiproject/content/util/BaseResourceType.java
@@ -21,52 +21,99 @@
 
 package org.sakaiproject.content.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.sakaiproject.content.api.ContentEntity;
 import org.sakaiproject.content.api.ExpandableResourceType;
+import org.sakaiproject.content.api.ResourceToolAction;
 import org.sakaiproject.content.api.ResourceType;
+import org.sakaiproject.entity.api.Reference;
+import org.sakaiproject.i18n.InternationalizedMessages;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.util.ResourceLoader;
 
-/**
- * 
- *
- */
-public abstract class BaseResourceType implements ResourceType 
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public abstract class BaseResourceType implements ResourceType
 {
-	
+	protected static final String DEFAULT_RESOURCECLASS = "org.sakaiproject.localization.util.TypeProperties";
+	protected static final String DEFAULT_RESOURCEBUNDLE = "org.sakaiproject.localization.bundle.type.types";
+	protected static final String RESOURCECLASS = "resource.class.type";
+	protected static final String RESOURCEBUNDLE = "resource.bundle.type";
+
+	@Setter private InternationalizedMessages messageSource;
+
+	final protected EnumMap<ResourceToolAction.ActionType, List<ResourceToolAction>> actionMap;
+	final protected Map<String, ResourceToolAction> actions;
+
+	public BaseResourceType() {
+		actionMap = new EnumMap<>(ResourceToolAction.ActionType.class);
+		actions = new HashMap<>();
+	}
+
+	@Override
 	public boolean hasAvailabilityDialog() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasDescription() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasGroupsDialog() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasNotificationDialog() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasOptionalPropertiesDialog() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasPublicDialog() 
 	{
 		return true;
 	}
 
+	@Override
 	public boolean hasRightsDialog() 
 	{
 		return true;
 	}
-	
+
+	@Override
+	public String getIconLocation(ContentEntity entity) {
+		return null;
+	}
+
+	@Override
+	public String getIconClass(ContentEntity entity) {
+		return null;
+	}
+
+	@Override
 	public boolean isExpandable()
     {
 	    return (this instanceof ExpandableResourceType);
@@ -79,7 +126,8 @@ public abstract class BaseResourceType implements ResourceType
 	 * the entity is needed, overrid this method to return a short string (no more than 
 	 * 25 characters) describing the "size" of the entity as appropriate.
 	 */
-	public String getSizeLabel(ContentEntity entity) 
+	@Override
+	public String getSizeLabel(ContentEntity entity)
 	{
 		return null;
 	}
@@ -91,9 +139,57 @@ public abstract class BaseResourceType implements ResourceType
 	 * the entity is needed, overrid this method to return a short string (no more than 
 	 * 80 characters) describing the "size" of the entity as appropriate.
 	 */
-	public String getLongSizeLabel(ContentEntity entity) 
+	@Override
+	public String getLongSizeLabel(ContentEntity entity)
 	{
 		return null;
 	}
 
+	@Override
+	public ResourceToolAction getAction(String actionId) {
+		return actions.get(actionId);
+	}
+
+	@Override
+	public List<ResourceToolAction> getActions(ResourceToolAction.ActionType type) {
+		List<ResourceToolAction> list = actionMap.computeIfAbsent(type, k -> new ArrayList<>());
+		return Collections.unmodifiableList(list);
+	}
+
+	@Override
+	public List<ResourceToolAction> getActions(List<ResourceToolAction.ActionType> types) {
+		List<ResourceToolAction> list = new ArrayList<>();
+		if (types != null) {
+			for (ResourceToolAction.ActionType type : types) {
+				list.addAll(actionMap.computeIfAbsent(type, k -> new ArrayList<>()));
+			}
+		}
+		return Collections.unmodifiableList(list);
+	}
+
+	public Collection<ResourceToolAction> getActions(Reference entityRef, Set permissions) {
+		// TODO: use entityRef to filter actions
+		return Collections.unmodifiableCollection(actions.values());
+	}
+
+	public Collection<ResourceToolAction> getActions(Reference entityRef, User user, Set permissions) {
+		// TODO: use entityRef and user to filter actions
+		return Collections.unmodifiableCollection(actions.values());
+	}
+
+	protected BaseResourceAction.Localizer localizer(final String key) {
+		if (messageSource != null) {
+			return () -> messageSource.getString(key);
+		}
+		log.warn("MessageSource has not been initialized for this ResourceType, ");
+		return null;
+	}
+
+	protected BaseResourceAction.FormattedLocalizer formattedLocalizer(final String key, Object[] args) {
+		if (messageSource != null) {
+			return () -> messageSource.getFormattedMessage(key, args);
+		}
+		log.warn("MessageSource has not been initialized for this ResourceType, ");
+		return null;
+	}
 }
