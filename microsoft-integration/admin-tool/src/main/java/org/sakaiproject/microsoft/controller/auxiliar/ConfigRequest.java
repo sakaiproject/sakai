5cda2eaa8736c2f759bd0f8ecf60830b220ba891
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-03-15 17:36:23 +0100

    S2U-46 5.3.1 Microsoft: User and Group synchronization v0

---
+++ microsoft-integration/admin-tool/src/main/java/org/sakaiproject/microsoft/controller/auxiliar/ConfigRequest.java
@@ -0,0 +1,20 @@
+package org.sakaiproject.microsoft.controller.auxiliar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sakaiproject.microsoft.api.data.MicrosoftUserIdentifier;
+import org.sakaiproject.microsoft.api.data.SakaiSiteFilter;
+import org.sakaiproject.microsoft.api.data.SakaiUserIdentifier;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ConfigRequest {
+	List<String> synch_config_items = new ArrayList<>();
+	SakaiUserIdentifier mapped_sakai_user_id;
+	MicrosoftUserIdentifier mapped_microsoft_user_id;
+	SakaiSiteFilter siteFilter;
+}
