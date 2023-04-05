c6d3d0f52710a89d472a9fe68fcd9ce59e6991c1
bgarciaentornos <bgarcia@entornosdeformacion.com>
2023-04-05 10:28:22 +0200

    S2U-34 5.1.4.5 Assignments + Rubrics: peer review, self review and group peer (#11418)

--- webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-student.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-student.js
@@ -27,10 +27,12 @@ class SakaiRubricStudent extends RubricsElement {
       preview: Boolean,
       instructor: Boolean,
       evaluatedItemId: { attribute: "evaluated-item-id", type: String },
+      evaluatedItemOwnerId: { attribute: "evaluated-item-owner-id", type: String },
       rubric: { type: Object },
       rubricId: { attribute: "rubric-id", type: String },
       forcePreview: { attribute: "force-preview", type: Boolean },
       enablePdfExport: { attribute: "enable-pdf-export", type: Object },
+      isPeerOrSelf: { attribute: "is-peer-or-self", type: Boolean }
     };
   }
 
@@ -178,7 +180,11 @@ class SakaiRubricStudent extends RubricsElement {
         .then(rubric => {
 
           // Now, get the evaluation
-          const evalUrl = `/api/sites/${association.siteId}/rubric-evaluations/tools/${this.toolId}/items/${this.entityId}/evaluations/${this.evaluatedItemId}`;
+          let evalUrl = `/api/sites/${association.siteId}/rubric-evaluations/tools/${this.toolId}/items/${this.entityId}/evaluations/${this.evaluatedItemId}/owners/${this.evaluatedItemOwnerId}`;
+          if (this.isPeerOrSelf) {
+            //for permission filters
+            evalUrl += "?isPeer=true";
+          }
           fetch(evalUrl, {
             credentials: "include",
             headers: { "Content-Type": "application/json" },
