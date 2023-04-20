b0f807eaf66e4497d040789ea0af60dd6fcf6118
Eduardo Rey Jara <proyectos@seduerey.com>
2023-04-20 10:59:12 +0200

    SAK-48748 HTML Editor: Embedded Videos should have controls enabled by default. (#11484)
    Co-authored-by: Eduardo Rey <eduardo.rey@ticarum.es>
    (cherry picked from commit 3cac83ed632d16b67add382ba19733c1a0f671f5)

--- library/src/webapp-filtered/editor/ckeditor.launch.js
+++ library/src/webapp-filtered/editor/ckeditor.launch.js
@@ -461,7 +461,7 @@ sakai.editor.editors.ckeditor.launch = function(targetId, config, w, h) {
 
         //These could be applicable to the basic toolbar
         CKEDITOR.plugins.addExternal('lineutils',basePath+'lineutils/', 'plugin.js');
-        CKEDITOR.plugins.addExternal('html5video',webJars+'github-com-bahriddin-ckeditor-html5-video/${ckeditor.html5video.version}/html5video/', 'plugin.js');
+        CKEDITOR.plugins.addExternal('html5video',webJars+'ckeditor-html5-video/${ckeditor.html5video.version}/', 'plugin.js');
         CKEDITOR.plugins.addExternal('audiorecorder',basePath+'audiorecorder/', 'plugin.js');
         CKEDITOR.plugins.addExternal('contentitem',basePath+'contentitem/', 'plugin.js');
         CKEDITOR.plugins.addExternal('sakaipreview',basePath+'sakaipreview/', 'plugin.js');
