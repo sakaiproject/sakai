ea522fa16ddc597fd63c5a624a45ffc046f0325a
Juan David Massanet Puentes <94039846+JuanDavid102@users.noreply.github.com>
2023-05-03 16:13:33 +0200

    S2U-43 5.2.8.1 Admin: Ability to enrol, unenrol and disable multiple users in multiple sites - Master

--- kernel/kernel-impl/src/main/sql/mysql/sakai_site.sql
+++ kernel/kernel-impl/src/main/sql/mysql/sakai_site.sql
@@ -302,6 +302,9 @@ INSERT INTO SAKAI_SITE_PAGE_PROPERTY VALUES('!admin', '!admin-1590', 'sitePage.c
 
 INSERT INTO SAKAI_SITE_USER VALUES('!admin', 'admin', -1);
 
+INSERT INTO SAKAI_SITE_PAGE VALUES('!admin-2050', '!admin', 'Bulk User Membership', '0', 24, '0' );
+INSERT INTO SAKAI_SITE_TOOL VALUES('!admin-2050', '!admin-2050', '!admin', 'bulk.user.membership', 1, 'Bulk User Membership', NULL );
+
 -- Create the !error site to be displayed when there is a problem accessing a site.
 
 INSERT INTO SAKAI_SITE VALUES('!error', 'Site Unavailable', null, null, 'The site you requested is not available.', null, null, null, 1, 0, 0, '', 'admin', 'admin', NOW(), NOW(), 1, 0, 0, 0, null);
