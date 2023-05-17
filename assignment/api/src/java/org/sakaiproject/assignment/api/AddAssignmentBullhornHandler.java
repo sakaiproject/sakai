23de57287dfb1e55d42368c8ac112186f899eab3
JulianJanetschek <54806745+jkjanetschek@users.noreply.github.com>
2023-05-17 14:58:48 +0200

    SAK-44623: Notifications: assignments's open date is in future (#11563)
    * SAK-44623: UserNotification if an assignment's Opening Date is in the future
    
    * Update assignment/api/src/java/org/sakaiproject/assignment/api/AddAssignmentUserNotificationHandler.java
    
    * Update assignment/api/src/java/org/sakaiproject/assignment/api/AddAssignmentUserNotificationHandler.java
    
    ---------
    
    Co-authored-by: Adrian Fish <adrian.r.fish@gmail.com>
    (cherry picked from commit 8071ba2384565345eb58367e5c5290f76850af5e)
    
     Conflicts:
    	assignment/api/src/java/org/sakaiproject/assignment/api/AddAssignmentBullhornHandler.java

--- assignment/api/src/java/org/sakaiproject/assignment/api/AddAssignmentBullhornHandler.java
+++ assignment/api/src/java/org/sakaiproject/assignment/api/AddAssignmentBullhornHandler.java
@@ -69,7 +69,7 @@ public class AddAssignmentBullhornHandler extends AbstractBullhornHandler {
 
     @Override
     public List<String> getHandledEvents() {
-        return Arrays.asList(EVENT_ADD_ASSIGNMENT, EVENT_UPDATE_ASSIGNMENT_ACCESS);
+        return Arrays.asList(EVENT_ADD_ASSIGNMENT, EVENT_UPDATE_ASSIGNMENT_ACCESS, EVENT_AVAILABLE_ASSIGNMENT);
     }
 
     @Override
@@ -138,7 +138,7 @@ public class AddAssignmentBullhornHandler extends AbstractBullhornHandler {
         transactionTemplate.execute(status -> {
 
             sessionFactory.getCurrentSession().createQuery("delete BullhornAlert where EVENT in :events and REF = :ref and TO_USER in :toUsers")
-                .setParameterList("events", new String[] {EVENT_ADD_ASSIGNMENT, EVENT_UPDATE_ASSIGNMENT_ACCESS})
+                .setParameterList("events", new String[] {EVENT_ADD_ASSIGNMENT, EVENT_UPDATE_ASSIGNMENT_ACCESS, EVENT_AVAILABLE_ASSIGNMENT})
                 .setParameter("ref", ref, StringType.INSTANCE)
                 .setParameterList("toUsers", users).executeUpdate();
             return null;
