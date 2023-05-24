728c6a7adf5cadd19b265a5a70cf030d7b2fc8d9
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-05-24 12:27:32 +0200

    SAK-48931 Rubrics criterion description can load lots of instances of CKEditor on page load (#11595)

--- webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-criterion-edit.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sakai-rubric-criterion-edit.js
@@ -21,6 +21,7 @@ export class SakaiRubricCriterionEdit extends RubricsElement {
       rubricId: { attribute: "rubric-id", type: String },
       criterion: { type: Object, notify: true },
       isCriterionGroup: {attribute: "is-criterion-group", type: Boolean},
+      enableCkEditor: { type: Boolean },
     };
   }
 
@@ -91,12 +92,18 @@ export class SakaiRubricCriterionEdit extends RubricsElement {
                 <sr-lang key="criterion_description">Criterion Description</sr-lang>
               `}
             </label>
-            <sakai-editor
-              toolbar="BasicText"
-              content="${this.criterionClone.description ? `${this.criterionClone.description}` : ``}"
-              @changed="${this.updateCriterionDescription}"
-              id="criterion-description-field-${this.criterion.id}">
-            </sakai-editor>
+            <br/><a tabindex="0" role="button" href="#" title="${tr("toggle_editor")}" aria-label="${tr("toggle_editor")}" @click="${this.toggleCkEditor}"><sr-lang key="toggle_editor">Toggle CK Editor</sr-lang></a>
+            ${!this.enableCkEditor ?
+              html`<textarea id="criterion-description-field-${this.criterion.id}" class="form-control">${this.criterionClone.description}</textarea>` :
+                html`
+                  <sakai-editor
+                    toolbar="BasicText"
+                    content="${this.criterionClone.description ? `${this.criterionClone.description}` : ``}"
+                    @changed="${this.updateCriterionDescription}"
+                    id="criterion-description-field-${this.criterion.id}">
+                  </sakai-editor>
+                `
+            }
           </div>
 
           <div class="mt-2">
@@ -127,7 +134,10 @@ export class SakaiRubricCriterionEdit extends RubricsElement {
     e.stopPropagation();
 
     const title = e.target.closest(".popover-body").querySelector("input").value;
-    const description = e.target.closest("div.form").querySelector("sakai-editor").getContent();
+    // Get the description from the CkEditor or the textarea.
+    const description = !this.enableCkEditor ?
+      document.getElementById(`criterion-description-field-${this.criterion.id}`).value
+      : this.criterionClone.description;
 
     const body = JSON.stringify([
       { "op": "replace", "path": "/title", "value": title },
@@ -166,6 +176,12 @@ export class SakaiRubricCriterionEdit extends RubricsElement {
   updateCriterionDescription(e) {
     this.criterionClone.description = e.detail.content;
   }
+
+  toggleCkEditor(e) {
+    e.preventDefault();
+    this.enableCkEditor = this.enableCkEditor === undefined ? true : !this.enableCkEditor;
+  }
+
 }
 
 const tagName = "sakai-rubric-criterion-edit";
