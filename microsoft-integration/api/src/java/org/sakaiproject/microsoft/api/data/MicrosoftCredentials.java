f29bd66b078c7cf08d1293e9dfe891a51d18b286
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-05-29 10:19:15 +0200

    S2U-49 5.3.4 Microsoft: OneDrive integration (#11606) (#11621)

--- microsoft-integration/api/src/java/org/sakaiproject/microsoft/api/data/MicrosoftCredentials.java
+++ microsoft-integration/api/src/java/org/sakaiproject/microsoft/api/data/MicrosoftCredentials.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import org.apache.commons.lang3.StringUtils;
+import org.sakaiproject.microsoft.api.MicrosoftAuthorizationService;
 
 @AllArgsConstructor
 @Data
@@ -17,6 +18,7 @@ public class MicrosoftCredentials {
 	public static final String KEY_AUTHORITY = KEY_PREFIX + "AUTHORITY";
 	public static final String KEY_SECRET = KEY_PREFIX + "SECRET";
 	public static final String KEY_SCOPE = KEY_PREFIX + "SCOPE";
+	public static final String KEY_DELEGATED_SCOPE = KEY_PREFIX + "DELEGATED_SCOPE";
 	public static final String KEY_EMAIL = KEY_PREFIX + "EMAIL";
 
 	private String clientId;
@@ -26,21 +28,38 @@ public class MicrosoftCredentials {
 	private String secret;
 
 	private String scope;
+	
+	private String delegatedScope;
 
 	private String email;
 	
+	public String getAuthority() {
+		if(StringUtils.isNotBlank(authority) && !authority.endsWith("/")) {
+			return authority + "/";
+		}
+		return authority;
+	}
+	
 	public String getScope() {
 		if(scope == null) {
-			return "https://graph.microsoft.com/.default";
+			return MicrosoftAuthorizationService.SCOPE_DEFAULT;
 		}
 		return scope;
+	}
+	
+	public String getDelegatedScope() {
+		if(delegatedScope == null) {
+			return MicrosoftAuthorizationService.DELEGATED_SCOPE_DEFAULT;
+		}
+		return delegatedScope;
 	}
 
 	public boolean hasValue() {
 		return StringUtils.isNotBlank(clientId) && 
 				StringUtils.isNotBlank(authority) && 
 				StringUtils.isNotBlank(secret) && 
-				StringUtils.isNotBlank(scope) &&
+				StringUtils.isNotBlank(getScope()) &&
+				StringUtils.isNotBlank(getDelegatedScope()) &&
 				StringUtils.isNotBlank(email);
 	}
 }
