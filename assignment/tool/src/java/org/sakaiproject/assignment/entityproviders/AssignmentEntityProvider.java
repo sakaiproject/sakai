66c2e793644010c7a103cc15fef544b0c8989e86
Adrian Fish <adrian.r.fish@gmail.com>
2023-06-13 10:32:20 +0100

    SAK-49010 Grader. Filter out submissions with no submitters
    https://sakaiproject.atlassian.net/browse/SAK-49010

--- assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/entityproviders/AssignmentEntityProvider.java
@@ -785,6 +785,8 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
 
         if (!submitters.isEmpty()) {
             submission.put("submitters", submitters);
+        } else {
+            return null;
         }
 
         if (StringUtils.isNotBlank(as.getGroupId())) {
@@ -878,7 +880,7 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
                     return null;
                 }
             })
-            .collect(Collectors.toList()));
+            .filter(Objects::nonNull).collect(Collectors.toList()));
 
         return new ActionReturn(data);
     }
@@ -950,6 +952,7 @@ public class AssignmentEntityProvider extends AbstractEntityProvider implements 
                 submissionMaps.add(submissionToMap(activeSubmitters, assignment, simpleAssignment, as, false));
             }
         }
+        submissionMaps.removeAll(Collections.singleton(null));
 
         Integer contentKey = assignment.getContentId();
         if (contentKey != null) {
