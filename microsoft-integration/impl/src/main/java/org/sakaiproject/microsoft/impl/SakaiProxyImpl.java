5d5943a8be85ba94c0297108abf896f45b4c08ed
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-06-14 11:06:56 +0200

    S2U-48: Microsoft Stream integration (#11673) (#11714)

--- microsoft-integration/impl/src/main/java/org/sakaiproject/microsoft/impl/SakaiProxyImpl.java
+++ microsoft-integration/impl/src/main/java/org/sakaiproject/microsoft/impl/SakaiProxyImpl.java
@@ -47,6 +47,7 @@ import org.sakaiproject.time.api.TimeRange;
 import org.sakaiproject.time.api.TimeService;
 import org.sakaiproject.tool.api.Session;
 import org.sakaiproject.tool.api.SessionManager;
+import org.sakaiproject.tool.api.ToolManager;
 import org.sakaiproject.user.api.PreferencesService;
 import org.sakaiproject.user.api.User;
 import org.sakaiproject.user.api.UserDirectoryService;
@@ -68,6 +69,9 @@ public class SakaiProxyImpl implements SakaiProxy {
 	
 	@Autowired
 	private SiteService siteService;
+	
+	@Autowired
+	private ToolManager toolManager;
 	
 	@Autowired
 	private UserDirectoryService userDirectoryService;
@@ -189,6 +193,16 @@ public class SakaiProxyImpl implements SakaiProxy {
 	
 	
 	// ------------------------------------------ SITES ----------------------------------------------------
+	@Override
+	public String getCurrentSiteId() {
+		return toolManager.getCurrentPlacement().getContext();
+	}
+	
+	@Override
+	public boolean isMyWorkspace() {
+		return siteService.isUserSite(getCurrentSiteId());
+	}
+	
 	@Override
 	public List<Site> getSakaiSites(){
 		return getSakaiSites(new SakaiSiteFilter());
