d266b2aa89a2dc6438da23673c7a1f435c0819b1
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-06-14 11:06:56 +0200

    S2U-48: Microsoft Stream integration (#11673) (#11714)

---
+++ microsoft-integration/media-gallery-tool/src/main/webapp/WEB-INF/templates/js/common.js
@@ -0,0 +1,100 @@
+<script th:inline="javascript">
+	function showError(text){
+		let errorElem = document.getElementById('exception_error');
+		errorElem.textContent = text;
+		errorElem.classList.remove('hidden');
+		setTimeout(() => { errorElem.classList.add('hidden'); }, 5000);
+	}
+
+	async function getThumbnail(elem){
+		let baseURL = [[@{/thumbnail/}]];
+		let url = baseURL + elem.dataset.elemid;
+
+		let response = await fetch(url);
+		if(!response.ok){
+			showError('[(#{error.thumbnail})]');
+			return;
+		}
+		let thumbnail = await response.text();
+		if(thumbnail !== ''){
+			const img = document.createElement('img');
+			img.src = thumbnail;
+			elem.replaceChildren(img);
+		}
+	}
+
+	async function openLink(elem){
+		if(!elem.classList.contains("fa-spinner")) {
+			let baseURL = [[@{/link/}]];
+			let url = baseURL + elem.dataset.elemid;
+			
+			elem.classList.remove('fa-play-circle');
+			elem.classList.add('fa-spinner', 'fa-spin');
+			
+			let response = await fetch(url);
+			if(!response.ok){
+				showError('[(#{error.link})]');
+				elem.classList.remove('fa-spinner', 'fa-spin');
+				elem.classList.add('fa-play-circle');
+				return;
+			}
+			let link = await response.text();
+			elem.classList.remove('fa-spinner', 'fa-spin');
+			elem.classList.add('fa-play-circle');
+			window.open(link, "_blank");
+		}
+	}
+
+	async function loadInfo(elemid, title){
+		let baseURL = [[@{/info/}]];
+		let url = baseURL + elemid;
+
+		let response = await fetch(url);
+		if(!response.ok){
+			showError('[(#{error.info})]');
+			return;
+		}
+		let contentHtml = await response.text();
+
+		let elem = document.getElementById('info-modal');
+		elem.querySelector('#body-container').innerHTML = contentHtml;
+		elem.querySelector('#info-label').textContent = title;
+
+		$('#info-modal').modal('show');
+	}
+
+	function sortChanged(elem){
+		elem.form.submit();
+	}
+
+	function doSearch(elem){
+		let searchValue = elem.value.toLowerCase();
+		document.getElementById('accordion_all').querySelectorAll('.video-item').forEach((item) => {
+			item.classList.remove('hidden');
+			if(searchValue){
+				let text = item.querySelector('.video-title').textContent.toLowerCase();
+				if (!text.includes(searchValue)) {
+					item.classList.add('hidden');
+				}
+			}
+		});
+	}
+
+	function toggleView(){
+		document.querySelectorAll('.container-view').forEach((item) => {
+			if(item.classList.contains("in")){
+				item.classList.remove("in");
+			} else {
+				item.classList.add("in");
+			}
+		});
+	}
+
+	function toggleFolder(typeId, elemId){
+		event.preventDefault();
+		document.getElementById("section_tree_"+typeId).querySelectorAll('.folder-row').forEach((row) => {
+			row.classList.remove('in');
+		});
+		document.getElementById(elemId).classList.add('in');
+	}
+</script>
