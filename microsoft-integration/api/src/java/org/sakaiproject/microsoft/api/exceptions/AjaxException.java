e373b752719d0893d64c6ac98a84696c2559880c
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-07-21 13:51:41 +0200

    S2U-50: Microsoft Collaborative Documents (#11760)

---
+++ microsoft-integration/api/src/java/org/sakaiproject/microsoft/api/exceptions/AjaxException.java
@@ -0,0 +1,9 @@
+package org.sakaiproject.microsoft.api.exceptions;
+
+public class AjaxException extends MicrosoftGenericException {
+	private static final long serialVersionUID = 1L;
+	
+	public AjaxException(String msg) {
+		super(msg);
+	}
+}
