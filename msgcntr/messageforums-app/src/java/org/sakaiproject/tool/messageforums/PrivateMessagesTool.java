9c59235ea3387f5ab4b9fe71b041955e36705420
susowrc <108749792+susowrc@users.noreply.github.com>
2023-07-24 13:35:33 +0200

    SAK-49042 Private messages: Scheduled message saved in draft and later moved to scheduled is not sent. (#11708)
    (cherry picked from commit 07a196ed3f0ff995bebb9ab3dd99db300891b52b)

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/PrivateMessagesTool.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/PrivateMessagesTool.java
@@ -3757,10 +3757,16 @@ public void processChangeSelectView(ValueChangeEvent eve)
     	else
     	{
     		for (PrivateMessageDecoratedBean decoMessage : (List<PrivateMessageDecoratedBean>) selectedMoveToFolderItems ) {
-		        final PrivateMessage initPrivateMessage = prtMsgManager.initMessageWithAttachmentsAndRecipients(decoMessage.getMsg());
-    			decoMessage = new PrivateMessageDecoratedBean(initPrivateMessage);
-    			
-    			prtMsgManager.movePvtMsgTopic(decoMessage.getMsg(), oldTopic, newTopic);
+				PrivateMessage message = decoMessage.getMsg();
+				final PrivateMessage initPrivateMessage = prtMsgManager.initMessageWithAttachmentsAndRecipients(message);
+				decoMessage = new PrivateMessageDecoratedBean(initPrivateMessage);
+
+				prtMsgManager.movePvtMsgTopic(message, oldTopic, newTopic);
+
+				if(Boolean.TRUE.equals((message.getScheduler())) && (message.getScheduledDate()!=null) && (Boolean.TRUE.equals((message.getDraft()))))
+				{
+					PrivateMessageSchedulerService.scheduleDueDateReminder(message.getId());
+				}
     		}
     	}
 	    
