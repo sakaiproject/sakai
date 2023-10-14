eaadfab5801b36a7d784fef632d9008139f2e3b6
Charles Severance <csev@umich.edu>
2023-10-14 10:01:41 -0400

    SAK-49345 - Address review comments

--- basiclti/tsugi-util/src/java/org/tsugi/util/Base64DoubleUrlEncodeSafe.java
+++ basiclti/tsugi-util/src/java/org/tsugi/util/Base64DoubleUrlEncodeSafe.java
@@ -4,43 +4,52 @@
  * problem and some firewalls remove double URL Encoding.
  *
  * And Base64 has a URL safe encoding - but it uses '=' for padding which does not survive double URL
- * Encoding. 
+ * Encoding.
  *
- * So our solution is to manually map '=' to '_' which produces strings which do not change when URL
- * Encoded - so double URL Encoding can be avoided.
+ * So our solution is to manually map '=' to '.' which produces strings which do not change when URL
+ * Encoded - so double URL Encoding can be avoided.  The use of '.' as one of the "encoded" characters
+ * is common in JWT encoders.  Sadly, we cannot use JWT encoder and decoder because it is specialized
+ * for JSON so we cannot use it here.  Also, since the only compatibility that JWT encoders need to work
+ * with are JWT decoders, so JWT dispenses with padding completely - hence you will never see an '='
+ * in an encoded JWT (https://jwt.io/)
  */
 
 package org.tsugi.util;
 
 public class Base64DoubleUrlEncodeSafe {
 
+	// https://datatracker.ietf.org/doc/html/rfc4648#section-3.2
+	public static final char CHARACTER_TO_AVOID = '=';
+	public static final char REPLACEMENT_CHARACTER = '.';
+
     /**
-     * Encodes a given string into a Base64 Double UrlEncode Safe string.
+     * Encodes a given string into a Base64-Double-UrlEncode-Safe string.
      *
      * @param data The input string to be encoded.
-     * @return The Base64 Double UrlEncode Safe encoded string.
+     * @return The Base64-Double-UrlEncode-Safe encoded string.
      */
     public static String encode(String data) {
 		if ( data == null ) return null;
 		try {
 			String encoded = java.util.Base64.getUrlEncoder().encodeToString(data.getBytes("UTF-8"));
-			return encoded.replace('=', '_');
+			return encoded.replace(CHARACTER_TO_AVOID, REPLACEMENT_CHARACTER);
 		} catch (java.io.UnsupportedEncodingException e ) {
-			// unlikely 
+			// Unlikely
+			e.printStackTrace();
 			return null;
 		}
     }
 
     /**
-     * Decodes a Base64 Double UrlEncode Safe string into its original string representation.
+     * Decodes a Base64-Double-UrlEncode-Safe string into its original string representation.
      *
-     * @param base62 The Base64 Double UrlEncode Safe string to be decoded.
+     * @param encoded The Base64-Double-UrlEncode-Safe string to be decoded.
      * @return The decoded original string.
-     * @throws IllegalArgumentException If the input string contains invalid characters.
+     * @throws java.io.UnsupportedEncodingException If the input string contains invalid characters.
      */
-    public static String decode(String decoded) {
-		if ( decoded == null ) return null;
-		return new String(java.util.Base64.getUrlDecoder().decode(decoded.replace('_', '=')));
+    public static String decode(String encoded) {
+		if ( encoded == null ) return null;
+		return new String(java.util.Base64.getUrlDecoder().decode(encoded.replace(REPLACEMENT_CHARACTER, CHARACTER_TO_AVOID)));
     }
 
 }
