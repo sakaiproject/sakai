8aee904fb9c400d8b5d4fff467a6ff33ef437b4a
Kunal Jaykam <kunaljaykam@gmail.com>
2023-10-29 13:50:20 +0530

    SAK-49381 T&Q: Numeric response question > Uncaught TypeError: .popover is not a function

--- samigo/samigo-app/src/webapp/js/finInputValidator.js
+++ samigo/samigo-app/src/webapp/js/finInputValidator.js
@@ -22,80 +22,85 @@ window.onload = function() {
 
 includeWebjarLibrary('mathjs');
 
-$( document ).ready(function() {
-
-  $('.fillInNumericInput').each( function() {
-    $(this).attr('data-bs-toggle', 'popover'); 
-    $(this).attr('data-content', finFormatError);
-    $(this).attr('data-trigger', 'focus');
+document.addEventListener('DOMContentLoaded', function() {
+  const fillInNumericInputs = document.querySelectorAll('.fillInNumericInput');
+  const takeAssessmentForm = document.getElementById('takeAssessmentForm');
+  
+  fillInNumericInputs.forEach(input => {
+    input.dataset.bsToggle = 'popover';
+    input.dataset.bsContent = finFormatError;
+    input.dataset.bsTrigger = 'focus';
   });
 
-  $('#takeAssessmentForm').submit(function() {
-    $('.fillInNumericInput').each(function() {
-      //If a part or an exam is submitted, validate all the FIN inputs and alert about the invalid ones to prevent a response loss.
-      validateFinInput(this);
+  takeAssessmentForm.addEventListener('submit', function(event) {
+    fillInNumericInputs.forEach(input => {
+      if (!validateFinInput(input)) {
+        event.preventDefault();
+      }
     });
   });
 
-  $('.fillInNumericInput').on('shown.bs.popover', function () {
-    $(this).delay(3000).popover('hide');
-  });
+  fillInNumericInputs.forEach(input => {
+    input.addEventListener('shown.bs.popover', function () {
+      setTimeout(() => {
+        const popover = new bootstrap.Popover(input);
+        popover.hide();
+      }, 3000);
+    });
 
-  $('.fillInNumericInput').change( function() {
-    if (validateFinInput(this)) {
-      $(this).popover('destroy');
-    }
-  });
+    input.addEventListener('change', function() {
+      if (validateFinInput(input)) {
+        const popover = new bootstrap.Popover(input);
+        popover.dispose();
+      }
+    });
 
-  $('.fillInNumericInput').keyup( throttle(function(){
-    // Do not validate on key up when the user is inserting a complex number or scientific notation or a real with sign.
-    if (this.value !== '' && 
-        (this.value.includes('+') ||
-        this.value.includes('-') ||
-        this.value.includes('{') ||
-        this.value.includes('}') ||
-        this.value.includes('e') ||
-        this.value.includes('E'))
-    ) {
+    input.addEventListener('input', function() {
+      if (input.value !== '' &&
+        (input.value.includes('+') ||
+        input.value.includes('-') ||
+        input.value.includes('{') ||
+        input.value.includes('}') ||
+        input.value.includes('e') ||
+        input.value.includes('E')
+        )
+      ) {
         return;
-    }
-    if (validateFinInput(this)) {
-      $(this).popover('destroy');
-    }
-  }));
-
+      }
+      if (validateFinInput(input)) {
+        const popover = new bootstrap.Popover(input);
+        popover.dispose();
+      }
+    });
+  });
 });
 
-var validateFinInput = function(input) {
+function validateFinInput(input) {
   if (!input.value) {
-    //Empty inputs are accepted.
     return true;
   }
-  //Replace the comma decimal separator as point, the JSF validator does the same and all the JS libraries work with point as decimal separator.
-  var rawInput = input.value.replace(/,/g, '.');
-  //Replace all the whitespaces.
-  rawInput = rawInput.replace(/\s/g,'');
+
+  const rawInput = input.value.replace(/,/g, '.');
   input.value = rawInput;
-  var isValidFinInput = true;
-  var numericInputValue = rawInput;
-  var complexInputValue = [];
-  rawInput.replace(/\{(.+?)\}/g, function(_, m) {complexInputValue.push(m)} );
-  // Check if the number is complex first
-  if (complexInputValue != '') {
-    try{
-      //Parsing relies on MathJS https://mathjs.org
+  let isValidFinInput = true;
+  const numericInputValue = rawInput;
+  const complexInputValue = [];
+  rawInput.replace(/\{(.+?)\}/g, function(_, m) {
+    complexInputValue.push(m);
+  });
+
+  if (complexInputValue.length !== 0) {
+    try {
       const complexNumber = math.complex(complexInputValue);
-    } catch(error) {
-      console.debug('The inserted complex number is not valid, please review the syntax. eg: {8.5 + 9.4i}');
+    } catch (error) {
+      console.debug('The inserted complex number is not valid, please review the syntax. e.g., {8.5 + 9.4i}');
       isValidFinInput = false;
     }
   } else {
-    // If not complex, lets check if is numeric.
     try {
-      //Simple as that, try to add 0.0 to a numeric value.
-      const numericNumber = math.add(numericInputValue, 0.0); 
-    } catch(error) {
-      console.debug('The inserted value is not numeric, please review the syntax. eg: 1.5 , 9, -4, -3.1415');
+      const numericNumber = math.add(numericInputValue, 0.0);
+    } catch (error) {
+      console.debug('The inserted value is not numeric, please review the syntax. e.g., 1.5, 9, -4, -3.1415');
       isValidFinInput = false;
     }
   }
@@ -107,16 +112,4 @@ var validateFinInput = function(input) {
   }
 
   return true;
-
-}
-
-function throttle(f, delay) {
-  var timer = null;
-  return function() {
-   var context = this, args = arguments;
-   clearTimeout(timer);
-   timer = window.setTimeout(function() {
-     f.apply(context, args);
-   }, delay || 200);
-  };
 }
