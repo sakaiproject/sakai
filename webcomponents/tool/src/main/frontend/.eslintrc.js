2e06e5b8dc2b424359e0e613bd004a849d6ef810
Adrian Fish <adrian.r.fish@gmail.com>
2023-10-31 15:57:11 +0000

    NOJIRA Add bootstrap to the 22.x eslint config

--- webcomponents/tool/src/main/frontend/.eslintrc.js
+++ webcomponents/tool/src/main/frontend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "env": { "browser": true, "es2021": true },
   "globals": {
     "$": "readonly",
+    "bootstrap": "readonly",
     "sakai": "readonly",
     "sakaiSessionId": "readonly",
     "portal": "readonly",
