b6794998af8f59f3edcef0e8934ad8413d4ea5dc
Earle Nietzel <earle@longsight.com>
2023-11-08 10:04:28 -0500

    SAK-49409 Assignment update date time inputs with sakai-date-picker (#12050)
    (cherry picked from commit 579917e97878b1d21104f00ba35fd3fcbdce7f66)

--- assignment/api/src/java/org/sakaiproject/assignment/api/AssignmentConstants.java
+++ assignment/api/src/java/org/sakaiproject/assignment/api/AssignmentConstants.java
@@ -341,17 +341,17 @@ public final class AssignmentConstants {
     public static final String GRADE_SUBMISSION_FEEDBACK_ATTACHMENT = "grade_submission_feedback_attachment";
 
     // submission level of resubmit due time
-    public static final String ALLOW_RESUBMIT_CLOSEYEAR = "allow_resubmit_closeyear";
-    public static final String ALLOW_RESUBMIT_CLOSEMONTH = "allow_resubmit_closemonth";
-    public static final String ALLOW_RESUBMIT_CLOSEDAY = "allow_resubmit_closeday";
-    public static final String ALLOW_RESUBMIT_CLOSEHOUR = "allow_resubmit_closehour";
-    public static final String ALLOW_RESUBMIT_CLOSEMIN = "allow_resubmit_closemin";
+    public static final String ALLOW_RESUBMIT_CLOSE_YEAR = "allow_resubmit_close_year";
+    public static final String ALLOW_RESUBMIT_CLOSE_MONTH = "allow_resubmit_close_month";
+    public static final String ALLOW_RESUBMIT_CLOSE_DAY = "allow_resubmit_close_day";
+    public static final String ALLOW_RESUBMIT_CLOSE_HOUR = "allow_resubmit_close_hour";
+    public static final String ALLOW_RESUBMIT_CLOSE_MIN = "allow_resubmit_close_min";
     public static final String ALLOW_RESUBMIT_CLOSE_EPOCH_MILLIS = "allow_resubmit_close_epoch_millis";
-    public static final String ALLOW_EXTENSION_CLOSEMONTH = "allow_extension_closeMonth";
-    public static final String ALLOW_EXTENSION_CLOSEDAY = "allow_extension_closeDay";
-    public static final String ALLOW_EXTENSION_CLOSEYEAR = "allow_extension_closeYear";
-    public static final String ALLOW_EXTENSION_CLOSEHOUR = "allow_extension_closeHour";
-    public static final String ALLOW_EXTENSION_CLOSEMIN = "allow_extension_closeMin";
+    public static final String ALLOW_EXTENSION_CLOSE_MONTH = "allow_extension_close_month";
+    public static final String ALLOW_EXTENSION_CLOSE_DAY = "allow_extension_close_day";
+    public static final String ALLOW_EXTENSION_CLOSE_YEAR = "allow_extension_close_year";
+    public static final String ALLOW_EXTENSION_CLOSE_HOUR = "allow_extension_close_hour";
+    public static final String ALLOW_EXTENSION_CLOSE_MIN = "allow_extension_close_min";
     public static final String ALLOW_EXTENSION_CLOSE_EPOCH_MILLIS = "allow_extension_close_epoch_millis";
     public static final String GRADE_SUBMISSION_DONT_CLEAR_CURRENT_ATTACHMENTS = "grade_submission_dont_clear_current_attachments";
 
