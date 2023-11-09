8ea6543b51a36f3054870dc5f73b7fe2aa755556
Adrian Fish <adrian.r.fish@gmail.com>
2023-11-09 11:58:24 +0000

    SAK-49429 rubrics. Fix visit count and last visit time
    https://sakaiproject.atlassian.net/browse/SAK-49429

--- roster2/tool/src/java/org/sakaiproject/roster/api/RosterMember.java
+++ roster2/tool/src/java/org/sakaiproject/roster/api/RosterMember.java
@@ -75,7 +75,7 @@ public class RosterMember {
 	private Map<String, String> userProperties = new HashMap<>();
 	private int connectionStatus; // connection status to the current user
 	private int totalSiteVisits;
-	private long lastVisitTime;
+	private String lastVisitTime;
 	private boolean instructor;
 
 	public void addGroup(String groupId, String groupTitle) {
