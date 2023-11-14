c4f8c0792cbd349458d93d663ee749a164172e79
Charles Severance <csev@umich.edu>
2023-11-14 14:06:20 -0500

    SAK-49451 - Fix height and resize in External Tool left nav (#12087)
    (cherry picked from commit e4bc487f13b0aa9ef803bde561fb44352959806e)

--- webcomponents/tool/src/main/frontend/js/sakai-lti-iframe.js
+++ webcomponents/tool/src/main/frontend/js/sakai-lti-iframe.js
@@ -1,30 +1,31 @@
 import { SakaiElement } from "./sakai-element.js";
 import { html } from "./assets/lit-html/lit-html.js";
-
 class SakaiLTIIframe extends SakaiElement {
   constructor() {
     super();
     const randomId = Math.floor(Math.random() * 1000000);
     this.randomId = randomId;
     this.newWindowText = null;
+
+    // Defaults
+    this.allow = "camera; microphone";
+    this.allowResize = "yes";
+    this.height = "1200px";
+
     this.loadTranslations("lti").then(t => {
       this.i18n = t;
-      if ( this.newWindowText == null ) this.newWindowText = this.i18n.new_window_text;
-    } );
-
-    setTimeout( (elem) => {
+      if (this.newWindowText == null) this.newWindowText = this.i18n.new_window_text;
+    });
+    setTimeout(elem => {
       console.debug('elem', elem);
-      if ( typeof elem == 'undefined' ) return;
+      if (typeof elem == 'undefined') return;
       console.debug("timeout check", elem.randomId, elem.launchUrl);
       // Only check off-server launches
-      if ( elem.launchUrl.indexOf('http://') != 0 && elem.launchUrl.indexOf('https://') != 0 ) return;
-
+      if (elem.launchUrl.indexOf('http://') != 0 && elem.launchUrl.indexOf('https://') != 0) return;
       console.debug('Actually checking...');
-
       const myframe = document.getElementById(`sakai-lti-iframe-${elem.randomId}`);
       const mybutton = document.getElementById(`sakai-lti-button-${elem.randomId}`);
       let loaded = false;
-
       try {
         const iframeLoc = String(myframe.contentWindow.location);
         console.debug('iframeLoc', iframeLoc);
@@ -32,30 +33,30 @@ class SakaiLTIIframe extends SakaiElement {
         window.console && console.debug("iframe.location=", windowLoc);
         let ipos = -1;
         let slashcount = 0;
-        for (let i = 0; i < windowLoc.length; i++ ) {
-          if ( windowLoc[i] == '/' ) slashcount++;
-          if ( slashcount == 3 ) {
+        for (let i = 0; i < windowLoc.length; i++) {
+          if (windowLoc[i] == '/') slashcount++;
+          if (slashcount == 3) {
             ipos = i;
             break;
           }
         }
-        if ( ipos == -1 ) {
+        if (ipos == -1) {
           loaded = false;
         } else {
           const prefix = windowLoc.substring(0, ipos);
           window.console && console.debug("iframe.prefix=", prefix);
           loaded = iframeLoc.indexOf(prefix) == 0;
         }
-      } catch (ex) {  // This is an expected/normal/hoped for occurance
+      } catch (ex) {
+        // This is an expected/normal/hoped for occurance
         loaded = true;
       }
-      if ( ! loaded ) {
+      if (!loaded) {
         mybutton.show();
       } else {
         window.console && console.debug('load success', elem.launchUrl);
       }
     }, 3000, this);
-
     window.addEventListener('message', e => {
       try {
         const idval = `sakai-lti-iframe-${randomId}`; // https://stackoverflow.com/questions/15329710/postmessage-source-iframe
@@ -68,14 +69,15 @@ class SakaiLTIIframe extends SakaiElement {
             allow_resize = element.getAttributeNode("data-allow-resize").nodeValue;
           }
         });
-        if ( frame_id != idval ) return; // The message is from our frame
-        if ( allow_resize != "yes" ) return;
-
+        if (frame_id != idval) return; // The message is from our frame
+        if (allow_resize != "yes") return;
         let message = e.data;
-        if ( typeof message == "string" ) message = JSON.parse(e.data);
-        if ( message.subject == 'lti.frameResize' ) {
-          const height = message.height;
-          document.getElementById(idval).height = height;
+        if (typeof message == "string") message = JSON.parse(e.data);
+        if (message.subject == 'lti.frameResize') {
+          let height = message.height;
+          const isnum = /^\d+$/.test(height);
+          if ( isnum ) height = height + 'px';
+          this.height = height;
           console.debug(`Received lti.frameResize height=${height} frame=${idval}`);
         }
       } catch (error) {
@@ -83,7 +85,6 @@ class SakaiLTIIframe extends SakaiElement {
       }
     });
   }
-
   static get properties() {
     return {
       allowResize: {
@@ -97,19 +98,24 @@ class SakaiLTIIframe extends SakaiElement {
       launchUrl: {
         attribute: "launch-url",
         type: String
+      },
+      allow: {
+        attribute: "allow",
+        type: String
+      },
+      height: {
+        attribute: "height",
+        type: String
       }
     };
   }
-
   shouldUpdate() {
     return this.newWindowText && this.launchUrl;
   }
-
   launchPopup() {
     window.open(this.launchUrl, '_blank');
     return false;
   }
-
   render() {
     return html`
           <div class="sakai-iframe-launch-button" id="sakai-lti-button-${this.randomId}" style="display:none;">
@@ -119,19 +125,18 @@ class SakaiLTIIframe extends SakaiElement {
           </div>
           <div class="sakai-iframe-launch">
                 <iframe src="${this.launchUrl}" id="sakai-lti-iframe-${this.randomId}"
-                  style="width: 100%; height: 100%; min-height: 80vh;"
-                    width="100%" frameborder="0" marginwidth="0"
+                  style="width: 100%; height: ${this.height}; min-height: 80vh;"
+                    frameborder="0" marginwidth="0"
                     data-allow-resize="${this.allowResize}"
+                    allow="${this.allow}"
                     marginheight="0" scrolling="auto"
                      allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"
-                     allow="camera; microphone">
+                  >
                 </iframe>
           </div>
     `;
   }
-
 }
-
 if (!customElements.get("sakai-lti-iframe")) {
   customElements.define("sakai-lti-iframe", SakaiLTIIframe);
 }
