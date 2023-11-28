cee1f067a661a2adfdaa8475342a4ffd46571bdc
Adrian Fish <adrian.r.fish@gmail.com>
2023-11-28 15:04:39 +0000

    SAK-49427 Rubrics fix ckeditor warning on reordering (#12125)

--- webcomponents/tool/src/main/frontend/js/rubrics/sharing-change-event.js
+++ webcomponents/tool/src/main/frontend/js/rubrics/sharing-change-event.js
@@ -1,6 +1,6 @@
 export class SharingChangeEvent extends CustomEvent {
 
   constructor() {
-    super("sharing-change", {bubbles: true, composed: true});
+    super("sharing-change", { bubbles: true, composed: true });
   }
 }
