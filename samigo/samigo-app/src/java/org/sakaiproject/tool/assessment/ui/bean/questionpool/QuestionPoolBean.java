b26ea255312c7b2cf348b3c4e7cfc08bb39b05e8
stetsche <53173679+stetsche@users.noreply.github.com>
2024-01-09 17:12:16 +0100

    S2U-18 SAK-49622 T&Q Fix question pool item sorting - MASTER (#12258)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/questionpool/QuestionPoolBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/questionpool/QuestionPoolBean.java
@@ -3344,6 +3344,12 @@ String poolId = ContextUtil.lookupParam("qpid");
 							.orderable(false)
 							.searchable(false)
 							.build());
+					add(DataTableColumn.builder()
+							.targets("columnQuestionText")
+							.orderable(true)
+							.searchable(true)
+							.type(DataTableColumn.TYPE_ANY_NUM)
+							.build());
 					// CHECKBOX (import)
 					add(DataTableColumn.builder()
 							.targets("columnCheckImport")
