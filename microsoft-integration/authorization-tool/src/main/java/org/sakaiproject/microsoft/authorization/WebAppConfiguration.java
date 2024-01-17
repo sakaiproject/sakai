c407b8ba75b27e0a6ee477d43a8dd2bd446d183d
Markus Stetschnig <mstetschnig@entornosdeformacion.com>
2024-01-17 14:13:34 +0100

    S2U-46 S2U-47 S2U-48 S2U-49 S2U-50 Add and fix license headers

--- microsoft-integration/authorization-tool/src/main/java/org/sakaiproject/microsoft/authorization/WebAppConfiguration.java
+++ microsoft-integration/authorization-tool/src/main/java/org/sakaiproject/microsoft/authorization/WebAppConfiguration.java
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2023 Apereo Foundation
+ * Copyright (c) 2024 The Apereo Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.sakaiproject.microsoft.authorization;
 
 import java.util.EnumSet;
