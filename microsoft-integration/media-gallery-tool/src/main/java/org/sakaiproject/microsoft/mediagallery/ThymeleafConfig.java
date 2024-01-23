b97bf3a49236a4b760b958bb838e8568bbfd1a5e
stetsche <53173679+stetsche@users.noreply.github.com>
2024-01-23 12:41:48 +0100

    S2U-46 S2U-47 S2U-48 S2U-49 S2U-50 Add and fix license headers (#12284)

--- microsoft-integration/media-gallery-tool/src/main/java/org/sakaiproject/microsoft/mediagallery/ThymeleafConfig.java
+++ microsoft-integration/media-gallery-tool/src/main/java/org/sakaiproject/microsoft/mediagallery/ThymeleafConfig.java
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2023 Apereo Foundation
+ * Copyright (c) 2024 The Apereo Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
