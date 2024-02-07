cae5a1db289eee789a397c24d4ca40fe40b43ff6
Horner, Sean A <hornersa@plu.edu>
2024-02-06 16:32:19 -0800

    SAK-49716 Scheduled (un)publishing not pinning/removing sites correctly

--- site-manage/site-manage-impl/impl/src/java/org/sakaiproject/sitemanage/impl/PublishingSiteScheduleServiceImpl.java
+++ site-manage/site-manage-impl/impl/src/java/org/sakaiproject/sitemanage/impl/PublishingSiteScheduleServiceImpl.java
@@ -19,20 +19,22 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.Setter;
 import java.time.Instant;
 import org.sakaiproject.api.app.scheduler.ScheduledInvocationManager;
-import org.sakaiproject.authz.api.SecurityAdvisor;
-import org.sakaiproject.authz.api.SecurityService;
 import org.sakaiproject.entity.api.ResourcePropertiesEdit;
+import org.sakaiproject.event.api.EventTrackingService;
 import org.sakaiproject.exception.IdUnusedException;
 import org.sakaiproject.exception.PermissionException;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.sitemanage.api.UnpublishingSiteScheduleService;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.api.SessionManager;
 
 @Slf4j
 public class PublishingSiteScheduleServiceImpl implements org.sakaiproject.sitemanage.api.PublishingSiteScheduleService {
     @Setter private ScheduledInvocationManager scheduledInvocationManager;
     @Setter private SiteService siteService;
-    @Setter private SecurityService securityService;
+    @Setter private EventTrackingService eventTrackingService;
+    @Setter private SessionManager sessionManager;
     private final String GROUPMEMEBERSHIP = "site.upd.grp.mbrshp";
     private final String SITEUPDATE = "site.upd";
     private final String READANNOUNCEMENTS = "annc.read";
@@ -47,22 +49,23 @@ public class PublishingSiteScheduleServiceImpl implements org.sakaiproject.sitem
     }
 
     public void execute(String siteId){
-        SecurityAdvisor advisor = (userId, function, reference) -> {
-            if(function.equals(GROUPMEMEBERSHIP) || function.equals(SITEUPDATE) || function.equals(READANNOUNCEMENTS)){
-                return SecurityAdvisor.SecurityAdvice.ALLOWED;  //allow only three special permissions for publishing
-            } else {
-                return SecurityAdvisor.SecurityAdvice.PASS;
-            }
-        };
-        securityService.pushAdvisor(advisor);   //add special authorization for the operation
+        Session session = null;
         try{
+	    session = sessionManager.getCurrentSession();
+	    session.setUserEid("admin");
+	    session.setUserId("admin");
             Site gettingPublished = siteService.getSite(siteId);
+            if (!gettingPublished.isPublished()) {
+		    eventTrackingService.post(eventTrackingService.newEvent(siteService.EVENT_SITE_PUBLISH, gettingPublished.getReference(), true));
+            }
             gettingPublished.setPublished(true);
             siteService.save(gettingPublished);
         } catch (IdUnusedException | PermissionException i){
             log.error("Unable to schedule publishing for site: " + siteId);
         } finally {
-            securityService.popAdvisor(advisor);   //remove special authorization
+            if (session != null) {
+		    session.clear();
+            }
         }
     }
 }
