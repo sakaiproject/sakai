d5ab60ffd2efebe91fe7126b54b05ab871e821dc
Sam Ottenhoff <ottenhoff@longsight.com>
2024-02-08 18:07:33 -0500

    SAK-49489 Samigo cleanup NPEs related to lack of startDate when doing a quick publish (#12347)
    (cherry picked from commit 91a919ef165aeafdd85fb53dc0e7037eefe2c68e)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/AssessmentSettingsBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/author/AssessmentSettingsBean.java
@@ -1902,7 +1902,7 @@ public class AssessmentSettingsBean extends SpringBeanAutowiringSupport implemen
     }
 
     public Date getExtendedTimeStart() {
-        return this.extendedTime.getStartDate();
+        return this.extendedTime != null ? this.extendedTime.getStartDate() : null;
     }
 
     public void setExtendedTimeStartString(String exTimeStartString) {
@@ -1917,7 +1917,7 @@ public class AssessmentSettingsBean extends SpringBeanAutowiringSupport implemen
     }
 
     public Date getExtendedTimeDue() {
-        return this.extendedTime.getDueDate();
+        return this.extendedTime != null ? this.extendedTime.getDueDate() : null;
     }
 
     public void setExtendedTimeDueString(String exTimeDueString) {
