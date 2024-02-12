9df0c69cc5b194dfc6c0e22261aecfca2923549c
Victor Gomollon Martos <victor.gomollon@ticarum.es>
2024-02-12 14:07:00 +0100

    SAK-49722: Remove Locale import

--- sections/sections-app/src/java/org/sakaiproject/tool/section/jsf/backingbean/EditManagersBean.java
+++ sections/sections-app/src/java/org/sakaiproject/tool/section/jsf/backingbean/EditManagersBean.java
@@ -28,7 +28,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 import javax.faces.context.FacesContext;
