ab7390eeb152bc5f6a2b2a8d8c7784655c6816af
Sam Ottenhoff <ottenhoff@longsight.com>
2024-02-28 11:57:26 -0500

    Update PollListManagerImpl.java with tabs

--- polls/impl/src/java/org/sakaiproject/poll/service/impl/PollListManagerImpl.java
+++ polls/impl/src/java/org/sakaiproject/poll/service/impl/PollListManagerImpl.java
@@ -545,19 +545,18 @@ public class PollListManagerImpl implements PollListManager,EntityTransferrer {
 			return toolIds;
 		}
 
-
 		public Map<String, String> transferCopyEntities(String fromContext, String toContext, List<String> resourceIds, List<String> transferOptions, boolean cleanup) {
 			if (cleanup) {
-                try {
-                    // Find and delete any polls from the destination site
-                    List<Poll> toPolls = findAllPolls(toContext);
-                    for (Poll poll : toPolls) {
-                        deletePoll(poll);
-                    }
-                } catch(Exception e) {
-                    log.error(e.getMessage(), e);
-                }
-            }
+				try {
+					// Find and delete any polls from the destination site
+					List<Poll> toPolls = findAllPolls(toContext);
+					for (Poll poll : toPolls) {
+						deletePoll(poll);
+					}
+				} catch(Exception e) {
+					log.error(e.getMessage(), e);
+				}
+			}
 			return transferCopyEntities(fromContext, toContext, resourceIds, transferOptions);
 		}
 
