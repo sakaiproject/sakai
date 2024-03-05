126c244107e776df3f9868b4075046c51ba02dd5
Charles Severance <csev@umich.edu>
2024-03-05 23:02:13 +0530

    SAK-49039 LTI improved message when score scope is missing (#11927)
    (cherry picked from commit 72c86a6e9dc0549103d8c706186e93451b9aa6b3)

--- basiclti/basiclti-blis/src/java/org/sakaiproject/lti13/LTI13Servlet.java
+++ basiclti/basiclti-blis/src/java/org/sakaiproject/lti13/LTI13Servlet.java
@@ -907,7 +907,7 @@ public class LTI13Servlet extends HttpServlet {
 			}
 			returnScopeSet.add(Endpoint.SCOPE_SCORE);
 
-			sat.addScope(SakaiAccessToken.SCOPE_BASICOUTCOME);
+			sat.addScope(SakaiAccessToken.SCOPE_SCORE);
 		}
 
 		if (scope.contains(Endpoint.SCOPE_RESULT_READONLY)) {
@@ -918,7 +918,7 @@ public class LTI13Servlet extends HttpServlet {
 			}
 			returnScopeSet.add(Endpoint.SCOPE_RESULT_READONLY);
 
-			sat.addScope(SakaiAccessToken.SCOPE_BASICOUTCOME);
+			sat.addScope(SakaiAccessToken.SCOPE_RESULT_READONLY);
 		}
 
 		if (scope.contains(LaunchLIS.SCOPE_NAMES_AND_ROLES)) {
@@ -973,9 +973,9 @@ public class LTI13Servlet extends HttpServlet {
 		if (sat == null) {
 			return;  // Error already set
 		}
-		if (!sat.hasScope(SakaiAccessToken.SCOPE_BASICOUTCOME)) {
-			LTI13Util.return400(response, "Scope basic outcome not in access token");
-			log.error("Scope basic outcome not in access token");
+		if (!sat.hasScope(SakaiAccessToken.SCOPE_SCORE)) {
+			LTI13Util.return400(response, "Scope score not in access token");
+			log.error("Scope score not in access token");
 			return;
 		}
 
@@ -1388,7 +1388,7 @@ public class LTI13Servlet extends HttpServlet {
 				JSONObject sakai_ext = new JSONObject();
 
 				/* SAK-47261 - Scope NRPS to Context, not Resource Link
-				if ( sat.hasScope(SakaiAccessToken.SCOPE_BASICOUTCOME)  && assignment_name != null ) {
+				if ( sat.hasScope(SakaiAccessToken.SCOPE_SCORE)  && assignment_name != null ) {
 					String placement_secret  = (String) content.get(LTIService.LTI_PLACEMENTSECRET);
 					String placement_id = getPlacementId(signed_placement);
 					String result_sourcedid = SakaiBLTIUtil.getSourceDID(user, placement_id, placement_secret);
@@ -2050,13 +2050,12 @@ public class LTI13Servlet extends HttpServlet {
 		if (sat == null) {
 			return;
 		}
-/*
-		if (! (sat.hasScope(SakaiAccessToken.SCOPE_LINEITEMS_READONLY) || sat.hasScope(SakaiAccessToken.SCOPE_LINEITEMS) )) {
-			LTI13Util.return400(response, "Scope lineitems.readonly not in access token");
-			log.error("Scope lineitems.readonly not in access token");
+
+		if (results && ! (sat.hasScope(SakaiAccessToken.SCOPE_RESULT_READONLY)) ) {
+			LTI13Util.return400(response, "Scope result.readonly not in access token");
+			log.error("Scope result.readonly not in access token");
 			return;
 		}
-*/
 
 		Site site = null;
 		Map<String, Object> tool = null;
