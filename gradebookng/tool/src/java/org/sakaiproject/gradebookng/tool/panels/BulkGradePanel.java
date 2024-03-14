9916d14fe9fe134107c060dabca11fb0910d6298
hornersa <sean.a.horner@gmail.com>
2024-03-14 08:54:48 -0700

    SAK-49837 Gradebook quick fix view does not adapt to percentages entry (#12431)
    Co-authored-by: Horner, Sean A <hornersa@plu.edu>
    (cherry picked from commit 39b7bed419c212ac681a415a5740136c9f244f3d)
    
     Conflicts:
    	gradebookng/bundle/src/main/bundle/gradebookng_ro_RO.properties
    	gradebookng/bundle/src/main/bundle/gradebookng_sr.properties

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/BulkGradePanel.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/tool/panels/BulkGradePanel.java
@@ -1,5 +1,6 @@
 package org.sakaiproject.gradebookng.tool.panels;
 
+import java.util.Objects;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
@@ -10,12 +11,13 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
 import org.sakaiproject.gradebookng.tool.pages.QuickEntryPage;
+import org.sakaiproject.grading.api.GradingConstants;
 import org.sakaiproject.portal.util.PortalUtils;
 
 public class BulkGradePanel extends Panel {
     private static final long serialVersionUID = 1L;
 
-    public BulkGradePanel(final String id, boolean comment, double points) {
+    public BulkGradePanel(final String id, boolean comment, double points, Integer gradeType) {
         super(id);
         AjaxLink<Void> replace = new AjaxLink<Void>("replace"){
             @Override
@@ -27,7 +29,7 @@ public class BulkGradePanel extends Panel {
         score.add(new AttributeAppender("id","replacementScore"));
         WebMarkupContainer commentBox = new WebMarkupContainer("replacementComment");
         commentBox.add(new AttributeAppender("id","replacementComment"));
-        Label pointsLabel = new Label("pointsLabel", " / " + points);
+        Label pointsLabel = new Label("pointsLabel", Objects.equals(GradingConstants.GRADE_TYPE_PERCENTAGE, gradeType) ? " %" : " / " + points);
         if(comment){
             replace.add(new AttributeModifier("onclick","fillComments();"));
             add(new Label("replacelabel",getString("quickentry.replacecomment")));
