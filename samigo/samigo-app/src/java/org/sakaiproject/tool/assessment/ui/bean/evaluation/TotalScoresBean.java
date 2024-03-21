2dce7fad37eac8a166913297bba305c88498a1e3
Sam Ottenhoff <ottenhoff@longsight.com>
2024-03-21 08:40:14 -0400

    SAK-49757 Samigo: fix attachment to a question response and cleanup (#12429)
    Co-authored-by: bgarciaentornos <bgarcia@entornosdeformacion.com>
    (cherry picked from commit eec3ca0ce649e10170383d7819788451308c5d7b)
    
     Conflicts:
    	samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/evaluation/QuestionScoresBean.java
    	samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/evaluation/TotalScoresBean.java

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/evaluation/TotalScoresBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/evaluation/TotalScoresBean.java
@@ -44,7 +44,7 @@ import org.sakaiproject.component.api.ServerConfigurationService;
 import org.sakaiproject.component.cover.ComponentManager;
 import org.sakaiproject.entity.api.ResourceProperties;
 import org.sakaiproject.jsf2.model.PhaseAware;
-import org.sakaiproject.jsf2.renderer.PagerRenderer;
+import org.sakaiproject.portal.util.PortalUtils;
 import org.sakaiproject.section.api.coursemanagement.CourseSection;
 import org.sakaiproject.section.api.coursemanagement.EnrollmentRecord;
 import org.sakaiproject.site.api.Site;
@@ -140,7 +140,7 @@ public class TotalScoresBean implements Serializable, PhaseAware {
   
   // Paging.
   private int firstScoreRow;
-  private int maxDisplayedScoreRows = PagerRenderer.MAX_PAGE_SIZE;
+  private int maxDisplayedScoreRows;
   private int scoreDataRows;
   
   // Searching
