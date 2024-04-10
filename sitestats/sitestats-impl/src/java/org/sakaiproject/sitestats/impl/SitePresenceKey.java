89a0af65ef9afc5e4c19812ab0ecc8f278ef63f9
stetsche <53173679+stetsche@users.noreply.github.com>
2024-04-10 17:16:17 +0200

    SAK-47101 Sitestats overlapping presences (#12485)

---
+++ sitestats/sitestats-impl/src/java/org/sakaiproject/sitestats/impl/SitePresenceKey.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2024 The Apereo Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://opensource.org/licenses/ecl2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sakaiproject.sitestats.impl;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@NonNull
+@Builder
+public class SitePresenceKey {
+
+
+    private String siteId;
+    private String userId;
+    private String sessionId;
+
+
+    public static SitePresenceKey from(SitePresenceRecord consolidation) {
+        return SitePresenceKey.builder()
+            .siteId(consolidation.getSiteId())
+            .userId(consolidation.getUserId())
+            .build();
+    }
+}
