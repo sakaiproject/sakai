ecaf59d7bec6e704096daeb703263b8188f81bf7
Horner, Sean A <hornersa@plu.edu>
2024-06-11 11:20:50 -0700

    SAK-50017 Assignments: Ungraded assignments with rubrics should have blank Grades

--- webcomponents/tool/src/main/frontend/packages/sakai-rubrics/src/SakaiRubricGrading.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-rubrics/src/SakaiRubricGrading.js
@@ -293,7 +293,7 @@ export class SakaiRubricGrading extends rubricsApiMixin(RubricsElement) {
       });
     });
 
-    this.updateTotalPoints({ notify: false });
+    this.updateTotalPoints(false);
   }
 
   fineTuneRating(e) {
