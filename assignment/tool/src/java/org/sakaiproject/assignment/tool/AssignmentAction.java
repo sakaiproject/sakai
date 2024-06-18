59dac23ccdc28528e1c4be5b40610ebc9e695a73
manu •• <mfuster99@outlook.es>
2024-06-18 17:07:33 +0200

    SAK-49998 Assignments updates to group filtering, grades viewing, and navigation (#12644)
    (cherry picked from commit 9cba23810068ab445316bf87c140c2056ade9fff)

--- assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
+++ assignment/tool/src/java/org/sakaiproject/assignment/tool/AssignmentAction.java
@@ -5807,6 +5807,8 @@ public class AssignmentAction extends PagedResourceActionII {
 
         ParameterParser params = data.getParameters();
         String assignmentReference = params.getString("assignmentReference");
+        String id = params.getString("submitterId");
+        state.setAttribute(EXPANDED_USER_ID, id);
 
         if (assignmentReference == null) {
             assignmentReference = params.getString("assignmentId");
@@ -10557,8 +10559,8 @@ public class AssignmentAction extends PagedResourceActionII {
         String assignmentId = params.getString("assignmentId");
         state.setAttribute(EXPORT_ASSIGNMENT_REF, assignmentId);
         String submissionId = params.getString("submissionId");
-        String userId = params.getString("user_id");
-        state.setAttribute(EXPANDED_USER_ID, userId);
+        String id = params.getString("submitterId");
+        state.setAttribute(EXPANDED_USER_ID, id);
         
         // SAK-29314 - put submission information into state
         boolean viewSubsOnlySelected = stringToBool((String) data.getParameters().getString(PARAMS_VIEW_SUBS_ONLY_CHECKBOX));
@@ -10783,7 +10785,7 @@ public class AssignmentAction extends PagedResourceActionII {
         ParameterParser params = data.getParameters();
 
         String id = params.getString("studentId");
-        state.removeAttribute(EXPANDED_USER_ID);
+        state.setAttribute(EXPANDED_USER_ID, id);
         // remove the student id from the table
         t.remove(id);
 
