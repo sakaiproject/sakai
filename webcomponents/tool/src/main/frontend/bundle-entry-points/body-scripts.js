6bf09cb1a9d4d8a0ce41db427ef86d68d54c44fe
Earle Nietzel <earle@longsight.com>
2024-06-18 22:13:55 -0400

    Husky removal

--- webcomponents/tool/src/main/frontend/bundle-entry-points/body-scripts.js
+++ webcomponents/tool/src/main/frontend/bundle-entry-points/body-scripts.js
@@ -1,4 +1,4 @@
-import "@sakai-ui/sakai-permissions/sakai-permissions.js"
+import "@sakai-ui/sakai-permissions/sakai-permissions.js";
 import "@sakai-ui/sakai-search/sakai-search.js";
 import "@sakai-ui/sakai-calendar/sakai-calendar.js";
 import "@sakai-ui/sakai-grades/sakai-grades.js";
