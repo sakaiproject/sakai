1d482e9e73f279480007274c9c34af1e2307d4f8
Adrian Fish <adrian.r.fish@gmail.com>
2024-06-20 07:56:21 +0100

    SAK-50223 sakai-forums only show sites with activity (#12662)
    (cherry picked from commit 52be9b3e0b809ce676a3c567a55fffa1d384ba20)

--- webcomponents/tool/src/main/frontend/packages/sakai-forums/test/data.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-forums/test/data.js
@@ -15,7 +15,7 @@ syn_hide_tooltip=Hide this site from this forums/messages view
 export const userId = "adrian";
 export const siteUrl = "/sites/1";
 
-export const userForumsUrl= `/api/users/${userId}/forums`;
+export const userForumsUrl= `/api/users/${userId}/forums/summary`;
 
 export const userForums = [
   { messageUrl: "/forums/1/2", forumUrl: "/forums/1", forumCount: 2, messageCount: 3, siteUrl, siteTitle: "A" },
@@ -24,7 +24,7 @@ export const userForums = [
 
 export const siteId = "xyz";
 
-export const siteForumsUrl= `/api/sites/${siteId}/forums`;
+export const siteForumsUrl= `/api/sites/${siteId}/forums/summary`;
 
 export const siteForums = [
   { messageUrl: "/forums/1/2", forumUrl: "/forums/1", forumCount: 2, messageCount: 3, siteUrl, siteTitle: "A" },
