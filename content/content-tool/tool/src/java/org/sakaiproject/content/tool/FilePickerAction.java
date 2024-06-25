ca6e00933569722f13bc2ab64436b1f21c0fdade
mylescarey2019 <50058682+mylescarey2019@users.noreply.github.com>
2024-06-25 08:37:58 -0500

    SAK-47869 Content file size limit check issue when upload max is greater than upload ceiling (#12666)
    Co-authored-by: Myles Carey <careymr@plu.edu>
    (cherry picked from commit 3c23ca7186eb6086424bee941a976bc1da56f766)

--- content/content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
+++ content/content-tool/tool/src/java/org/sakaiproject/content/tool/FilePickerAction.java
@@ -566,7 +566,48 @@ public class FilePickerAction extends PagedResourceHelperAction
 		context.put("stlang",srb);
 
 		ToolSession toolSession = sessionManager.getCurrentToolSession();
-		context.put("maxUploadFileSize", toolSession.getAttribute(STATE_FILE_UPLOAD_MAX_SIZE));
+
+
+		String uploadCeiling = ServerConfigurationService.getString("content.upload.ceiling");
+		String uploadMax = (String) toolSession.getAttribute(STATE_FILE_UPLOAD_MAX_SIZE);
+
+		if (!uploadCeiling.isBlank()) 
+		{
+			int uploadCeilingNum = Integer.MAX_VALUE;
+			int uploadMaxNum = Integer.MAX_VALUE;
+			try
+			{
+				uploadCeilingNum = Integer.parseInt(uploadCeiling);
+			}
+			catch(Exception e)
+			{
+				uploadCeilingNum = 20;
+				log.debug("Can't parse upload ceiling property. Using default of 20 ");
+			}
+			try
+			{
+				uploadMaxNum = Integer.parseInt(uploadMax);
+			}
+			catch(Exception e)
+			{
+				uploadMaxNum = 20;
+				log.debug("Can't parse upload max property. Using default of 20 ");
+			}	
+
+			if (uploadCeilingNum < uploadMaxNum) 
+			{
+				context.put("maxUploadFileSize", uploadCeiling);
+			}
+			else 
+			{
+				context.put("maxUploadFileSize", toolSession.getAttribute(STATE_FILE_UPLOAD_MAX_SIZE));
+			}
+		}
+		else 
+		{
+			context.put("maxUploadFileSize", toolSession.getAttribute(STATE_FILE_UPLOAD_MAX_SIZE));
+		}
+
 		context.put("alertMessage", "");
 		context.put("googledriveJson", toolSession.getAttribute(STATE_GOOGLEDRIVE_JSON));
 		toolSession.removeAttribute(STATE_GOOGLEDRIVE_JSON);
