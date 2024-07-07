b3bb486e76fdc33b57cde8e70b77d291d51a85f9
Matthew Jones <jonespm@gmail.com>
2024-07-07 16:53:35 -0400

    SAK-23981 - Basic LTI entity providers are missing documentation

--- basiclti/basiclti-tool/src/java/org/sakaiproject/blti/entityprovider/BLTIEventsEntityProvider.java
+++ basiclti/basiclti-tool/src/java/org/sakaiproject/blti/entityprovider/BLTIEventsEntityProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.sakaiproject.blti.entityprovider;
+package org.sakaiproject.blti.tool.entityprovider;
 
 import java.util.Locale;
 import java.util.Map;
