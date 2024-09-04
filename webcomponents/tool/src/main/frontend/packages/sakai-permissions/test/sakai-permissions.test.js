a4ff31b3726d8622eabff59d08115f5d7ea34ee1
Adrian Fish <adrian.r.fish@gmail.com>
2024-09-04 21:37:18 +0100

    SAK-50053 permissionswc Make permissions component responsive
    https://sakaiproject.atlassian.net/browse/SAK-50053

--- webcomponents/tool/src/main/frontend/packages/sakai-permissions/test/sakai-permissions.test.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-permissions/test/sakai-permissions.test.js
@@ -17,29 +17,6 @@ describe("sakai-permissions tests", () => {
     .get(data.permsUrl, data.perms, { overwriteRoutes: true })
     .get("*", 500, { overwriteRoutes: true });
 
-  /*
-  beforeEach(() =>  {
-
-
-    window.fetch = url => {
-
-      if (url === data.groupPickerI18nUrl) {
-        return Promise.resolve({ text: () => Promise.resolve(data.groupPickerI18n)});
-      } else if (url === data.permissionsI18nUrl) {
-        return Promise.resolve({ text: () => Promise.resolve(data.permissionsI18n)});
-      } else if (url === data.toolI18nUrl) {
-        return Promise.resolve({ text: () => Promise.resolve(data.toolI18n) });
-      } else if (url === data.groupsUrl) {
-        return Promise.resolve({ json: () => Promise.resolve(data.groups) });
-      } else if (url.startsWith(data.permsUrl)) {
-        return Promise.resolve({ json: () => Promise.resolve(data.perms) });
-      } else {
-        console.error(`Miss on ${url}`);
-      }
-    };
-  });
-  */
-
   it ("renders correctly", async () => {
  
     const el = await fixture(html`<sakai-permissions tool="tool" site-id="${data.siteId}"></sakai-permissions>`);
@@ -48,22 +25,22 @@ describe("sakai-permissions tests", () => {
 
     expect(el.querySelector("button:first-child").innerHTML).to.contain(el.i18n["per.lis.restoredef"]);
 
-    expect(el.querySelectorAll("table tr").length).to.equal(4);
-    expect(el.querySelectorAll("table tr:first-child th").length).to.equal(3);
-    expect(el.querySelectorAll("table tr:nth-child(2) input:checked").length).to.equal(2);
-    el.querySelector("table tr:nth-child(2) button").click();
+    expect(el.querySelectorAll("#permissions-container .row").length).to.equal(4);
+    expect(el.querySelectorAll("#permissions-container .row:first-child div").length).to.equal(3);
+    expect(el.querySelectorAll("#permissions-container .row:nth-child(2) input:checked").length).to.equal(2);
+    el.querySelector("#permissions-container .row:nth-child(2) button").click();
     expect(el.querySelectorAll("table tr:nth-child(2) input:checked").length).to.equal(0);
-    el.querySelector("table tr:nth-child(2) button").click();
-    expect(el.querySelectorAll("table tr:nth-child(2) input:checked").length).to.equal(2);
-    el.querySelector("table button:first-child").click();
-    expect(el.querySelectorAll("table input:checked").length).to.equal(0);
-    el.querySelector("table button:first-child").click();
-    expect(el.querySelectorAll("table input:checked").length).to.equal(6);
+    el.querySelector("#permissions-container .row:nth-child(2) button").click();
+    expect(el.querySelectorAll("#permissions-container .row:nth-child(2) input:checked").length).to.equal(2);
+    el.querySelector("#permissions-container button:first-child").click();
+    expect(el.querySelectorAll("#permissions-container input:checked").length).to.equal(0);
+    el.querySelector("#permissions-container button:first-child").click();
+    expect(el.querySelectorAll("#permissions-container input:checked").length).to.equal(6);
 
     // Reset the permissions
     el.querySelector("button:first-child").click();
     await el.updateComplete;
-    expect(el.querySelectorAll("table input:checked").length).to.equal(4);
+    expect(el.querySelectorAll("#permissions-container input:checked").length).to.equal(4);
 
     expect(el.querySelectorAll(".access-checkbox-cell input:checked").length).to.equal(1);
     el.querySelector("button[data-role='access']").click();
