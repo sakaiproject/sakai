a4468848c0490e8cd4e59837b6d7219c9a966407
Adrian Fish <adrian.r.fish@gmail.com>
2024-09-11 16:38:30 +0100

    SAK-49636 announcementswc Add sorting options (#12846)

---
+++ webcomponents/tool/src/main/frontend/packages/sakai-announcements/src/sakai-announcements-constants.js
@@ -0,0 +1,7 @@
+export const TITLE_A_TO_Z = "title_a_to_z";
+export const TITLE_Z_TO_A = "title_z_to_a";
+export const SITE_A_TO_Z = "site_a_to_z";
+export const SITE_Z_TO_A = "site_z_to_a";
+export const EARLIEST_FIRST = "earliest_first";
+export const LATEST_FIRST = "latest_first";
+
