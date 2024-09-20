f085811ef98cc035c3e2c4e36a3ddd937413ec9d
Earle Nietzel <earle@longsight.com>
2024-09-20 09:25:44 -0400

    Revert "SAK-49399 msgcntr acessing topic settings while not logged in redirects to default page (#12885)"
    This reverts commit eb939ea5969bea2f69517f1ead307b4bedaf5a81.

--- msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/MessageForumsFilePickerServlet.java
+++ msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/MessageForumsFilePickerServlet.java
@@ -167,18 +167,6 @@ public class MessageForumsFilePickerServlet extends JsfTool  implements HttpServ
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(target);
         dispatcher.forward(req, res);
 
-        try {
-            dispatcher.forward(req, res);
-        } catch (Exception e) {
-            log.warn("Error processing request: {}", e.toString());
-            String defaultPath = getInitParameter("default");
-            if (!defaultPath.startsWith("/")) {
-                defaultPath = "/" + defaultPath;
-            }
-            res.sendRedirect(Web.returnUrl(req, defaultPath));
-            return;
-        }
-
         // restore the request object
         req.removeAttribute(Tool.NATIVE_URL);
         req.removeAttribute(URL_PATH);
