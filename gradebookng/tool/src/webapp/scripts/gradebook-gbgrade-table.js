3c392db0a0d10444e394d399ab43d0f0bc18a40a
Adrian Fish <adrian.r.fish@gmail.com>
2024-12-09 21:40:12 +0000

    SAK-50610 conversations Integrate with gradebook (#13064)

--- gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
+++ gradebookng/tool/src/webapp/scripts/gradebook-gbgrade-table.js
@@ -2570,6 +2570,7 @@ GbGradeTable.setupCellMetaDataSummary = function() {
           function(comment) {
             // success
             $metadata.find("blockquote").html(comment).show();
+            $metadata.find("blockquote")[0].innerHTML = comment;
           },
           function() {
             // error
