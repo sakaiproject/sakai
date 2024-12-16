e2f2aeecf1442015b676751ba8de9661b33ea7c7
Matthew Jones <jonespm@gmail.com>
2024-12-15 19:58:17 -0500

    INFRSTR-368 - Updates to the docker directory
    Removed old config that waasn't likely used
    Put the Dockerfile at the top level with an updated readme
    Tested that this mimially works with the current version of Sakai
    
    TODO ideas in a future fix:
    
    * Use docker-compose.yml on the top level to make starting this up easier
    This would also allow for a persistent volume for MySQL
    
    * Split tomcat from the Dockerfile and have it run separately.
    
    * Verify if we still need the docker-maven-chrome or if we can use the regular maven to build

--- docker/dev/lib/build_image_entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Append any secret properties from /run/secrets/security.properties
-cat /run/secrets/security.properties >> /usr/local/tomcat/sakai/security.properties
-
-# Start tomcat
-catalina.sh run
