6130bd2e7fca7b8886f82cbf98f0c9148477d93c
manu •• <mfuster99@outlook.es>
2025-01-03 10:17:31 +0100

    SAK-50631 Samigo SEB autosave modal message is sometimes permanently displayed (#13058)
    Co-authored-by: bgarciaentornos <bgarcia@entornosdeformacion.com>

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/DeliveryBean.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/delivery/DeliveryBean.java
@@ -178,6 +178,8 @@ public class DeliveryBean implements Serializable {
   @Getter
   private String timeElapse;
   @Getter @Setter
+  private String backupTimeElapse;
+  @Getter @Setter
   private int sectionIndex;
   @Getter @Setter
   private boolean previous;
@@ -259,6 +261,8 @@ public class DeliveryBean implements Serializable {
   private String courseName;
   @Getter @Setter
   private String timeLimit;
+  @Getter @Setter
+  private String backupTimeLimit;
   @Getter @Setter
   private int timeLimit_hour;
   @Getter @Setter
@@ -546,6 +550,12 @@ public class DeliveryBean implements Serializable {
     try{
       if (timeElapse!=null && !("").equals(timeElapse)
           && getTimeLimit()!=null && !("").equals(getTimeLimit())){
+        if (!"0".equals(timeElapse)) {
+          setBackupTimeElapse(timeElapse);
+        }
+        if (!"0".equals(getTimeLimit())) {
+          setBackupTimeLimit(getTimeLimit());
+        }
         double limit = (new Double(getTimeLimit()));
         double elapsed = (new Double(timeElapse));
         if (limit > elapsed)
