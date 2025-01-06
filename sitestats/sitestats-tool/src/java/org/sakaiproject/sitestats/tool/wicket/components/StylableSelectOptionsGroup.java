34b4df83c655f0270c73c707521aed50f58eaaf7
Sam Ottenhoff <ottenhoff@longsight.com>
2025-01-06 21:07:37 +0100

    SAK-50803 Sitestats "Reports Add" page shows detach warnings (#13147)

--- sitestats/sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/components/StylableSelectOptionsGroup.java
+++ sitestats/sitestats-tool/src/java/org/sakaiproject/sitestats/tool/wicket/components/StylableSelectOptionsGroup.java
@@ -19,16 +19,22 @@
 package org.sakaiproject.sitestats.tool.wicket.components;
 
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.border.Border;
 import org.apache.wicket.model.IModel;
 
 public class StylableSelectOptionsGroup extends Border {
 	private static final long	serialVersionUID	= 1L;
+	private WebMarkupContainer optgroup;
 
 	public StylableSelectOptionsGroup(String id, IModel model, IModel style, IModel<String> hclass) {
 		super(id);
-		WebMarkupContainer optgroup = new WebMarkupContainer("optgroup");
+		// Add the body container first
+		addToBorder(getBodyContainer());
+
+		optgroup = new WebMarkupContainer("optgroup");
 		optgroup.add(new AttributeModifier("label", model));
 		if(style != null && !"null".equals((String) style.getObject())) {
 			optgroup.add(new AttributeModifier("style", style));
@@ -37,7 +43,28 @@ public class StylableSelectOptionsGroup extends Border {
 			optgroup.add(new AttributeModifier("class", hclass));
 		}
 		addToBorder(optgroup);
-		optgroup.add(getBodyContainer());
 	}
 
+	@Override
+	public void onComponentTagBody(MarkupStream markupStream, ComponentTag openTag) {
+		super.onComponentTagBody(markupStream, openTag);
+	}
+
+	@Override
+	protected void onDetach() {
+		super.onDetach();
+
+		// Detach the optgroup
+		if (optgroup != null) {
+			optgroup.detach();
+		}
+		// Detach the body container
+		detachBodyContainer();
+	}
+
+	private void detachBodyContainer() {
+		if (getBodyContainer() != null) {
+			getBodyContainer().detach();
+		}
+	}
 }
