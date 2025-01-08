dcb4ca36c7edd862dd10b428c7c90050bc2394c7
Earle Nietzel <earle@longsight.com>
2025-01-08 13:37:49 -0500

    SAK-50687 fixed merge issue for 23.x

--- webcomponents/tool/src/main/frontend/packages/sakai-grader/src/sakai-grader-rendering-mixin.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-grader/src/sakai-grader-rendering-mixin.js
@@ -203,7 +203,7 @@ export const graderRenderingMixin = Base => class extends Base {
                       <div class="fw-bold mb-2">${this.i18n.reviewer_attachments}</div>
                       ${pr.attachmentUrlList.map((url, i) => html`
                         <div class="feedback-attachment">
-                          <a href="${url}" title="${this.i18n.feedback_attachment_tooltip}" target="_blank">${this._i18n.attachment} ${i + 1}</a>
+                          <a href="${url}" title="${this.i18n.feedback_attachment_tooltip}" target="_blank">${this.i18n.attachment} ${i + 1}</a>
                         </div>
                       `)}
                     ` : nothing}
