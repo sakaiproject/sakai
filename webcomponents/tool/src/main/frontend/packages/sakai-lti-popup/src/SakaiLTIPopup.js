31edf98bddb5559d816757a29ff2d58c75d298db
Adrian Fish <adrian.r.fish@gmail.com>
2025-01-23 10:12:58 +0000

    Cleaned up some unecessary code. Try before you buy :)

--- webcomponents/tool/src/main/frontend/packages/sakai-lti-popup/src/SakaiLTIPopup.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-lti-popup/src/SakaiLTIPopup.js
@@ -9,57 +9,43 @@ export class SakaiLTIPopup extends SakaiElement {
     postLaunchText: { attribute: "post-launch-text", type: String },
     launchUrl: { attribute: "launch-url", type: String },
     auto: { attribute: "auto-launch", type: Boolean },
+    popped: { state: true },
   };
 
-  constructor() {
+  connectedCallback() {
 
-    super();
+    super.connectedCallback();
 
-    const randomId = Math.floor(Math.random() * 1000000);
-    this.randomId = randomId;
-    this.preLaunchText = null;
-    this.postLaunchText = null;
-    this.auto = false;
-    this.popped = false;
     this.loadTranslations("lti").then(t => {
 
       this._i18n = t;
-      if ( this.preLaunchText == null ) this.preLaunchText = this._i18n.pre_launch_text;
-      if ( this.postLaunchText == null ) this.postLaunchText = this._i18n.post_launch_text;
+      this.preLaunchText = this.preLaunchText ?? this._i18n.pre_launch_text;
+      this.postLaunchText = this.postLaunchText ?? this._i18n.post_launch_text;
     });
+
+    this.auto && this.launchPopup();
   }
 
   launchPopup() {
+
     window.open(this.launchUrl, "_blank");
-    document.getElementById("sakai-lti-popup-" + this.randomId).style.display = "none";
-    document.getElementById("sakai-lti-popup-hidden-" + this.randomId).style.display = "block";
-    return false;
+    this.popped = true;
   }
 
-  shouldUpdate(changedProperties) {
-    return changedProperties.has("preLaunchText") || changedProperties.has("postLaunchText") ||
-          changedProperties.has("launchUrl") || changedProperties.has("auto");
-  }
-
-  firstUpdated() {
-    if ( this.auto && ! this.popped ) {
-      this.popped = true;
-      setTimeout(this.launchPopup(), 1000);
-    }
+  shouldUpdate() {
+    return this._i18n;
   }
 
   render() {
 
     return html`
-      <div>
-        <button id="sakai-lti-popup-${this.randomId}"
-            class="btn btn-primary"
+      <div class="${this.auto ? "d-none" : "d-block"}">
+        <button class="btn btn-primary ${this.popped ? "d-none" : "d-block"}"
             @click=${this.launchPopup}>
           ${this.preLaunchText}
         </button>
-        <button id="sakai-lti-popup-hidden-${this.randomId}"
-            class="btn btn-primary"
-            disabled style="display:none;">
+        <button class="btn btn-primary ${this.popped ? "d-block" : "d-none"}"
+            disabled>
           ${this.postLaunchText}
         </button>
       </div>
