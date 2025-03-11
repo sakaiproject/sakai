cbf72902a4c8b9b7f40ebf7dabcbe3cc3f6036d1
Earle Nietzel <earle@longsight.com>
2025-03-11 12:37:31 -0400

    update log message

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/evaluation/QuestionScoreListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/evaluation/QuestionScoreListener.java
@@ -228,7 +228,7 @@ import org.sakaiproject.tool.assessment.data.ifc.shared.TypeIfc;
 			}
 			// build a hashMap (publishedItemId, publishedItem)
 			Map<Long, ItemDataIfc> publishedItemHash = pubService.preparePublishedItemHash(publishedAssessment);
-			log.debug("questionScores(): publishedItemHash.size = {}", publishedItemHash.size());
+			log.debug("publishedItemHash.size = {}", publishedItemHash.size());
 			// build a hashMap (publishedItemTextId, publishedItemText)
 			Map<Long, ItemTextIfc> publishedItemTextHash = pubService.preparePublishedItemTextHash(publishedAssessment);
 			log.debug("publishedItemTextHash.size = {}", publishedItemTextHash.size());
