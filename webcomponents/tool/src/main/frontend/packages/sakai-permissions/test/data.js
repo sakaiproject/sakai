f7a1653ed388edb6493b611dd4f7282f61106d09
Adrian Fish <adrian.r.fish@gmail.com>
2025-03-21 14:29:30 +0000

    SAK-50984 Content reinstate permission inheritance (#13464)

--- webcomponents/tool/src/main/frontend/packages/sakai-permissions/test/data.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-permissions/test/data.js
@@ -1,32 +1,8 @@
 export const siteId = "xyz";
 
-export const i18nUrl = /.*i18n.*resourcebundle=permissions-wc.*/;
+export { i18nUrl, i18n, toolI18nUrl, toolI18n } from "./i18n.js";
 
-export const i18n = `
-per.alrgra=already granted
-per.lis.head=Permission
-per.lis.head.title=Toggle all permissions for all roles
-per.lis.role.title=Toggle all permissions for this role
-per.lis.perm.title=Toggle all roles for this permission
-per.lis.title=Permissions
-per.lis=List of roles and permissions that can be applied to this folder. Layout: each row lists the permissions of a role. Layout: column 1 lists the roles, the other columns list the permissions, checkboxes permit enabling a permission for a role.
-per.rol=Role
-per.alert=Alert: no roles are defined.
-per.lis.clearall=Clear all
-per.lis.restoredef=Undo changes
-per.lis.selectgrp=Set permissions for
-gen.can=Cancel
-gen.sav=Save
-`;
-
-export const toolI18nUrl = /.*i18n.*resourcebundle=tool.*/;
-export const toolI18n = `
-perm-tool.create=Create
-perm-tool.delete=Delete
-perm-tool.read=Read
-perm-tool.update=Update
-`;
-export const permsUrl = /.*permissions.*tool\.json.*/;
+export const permsUrl = /\/api\/sites.*permissions.*/;
 export const perms = {
   available: [ "tool.read", "tool.create", "tool.delete" ],
   on: {
