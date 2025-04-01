1ee18cff843bb89449eff227c2446061b90f033f
Jesus Maria Mendez Perez <jesus.mendez@ticarum.es>
2025-04-01 20:57:19 +0200

    SAK-51267 Gradebook - ORACLE Not possible to enter a grade on a Gradebook item

--- gradebookng/api/src/main/java/org/sakaiproject/grading/api/model/AbstractGradeRecord.java
+++ gradebookng/api/src/main/java/org/sakaiproject/grading/api/model/AbstractGradeRecord.java
@@ -25,10 +25,12 @@ import javax.persistence.DiscriminatorType;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -57,8 +59,9 @@ import lombok.ToString;
 public abstract class AbstractGradeRecord implements PersistableEntity<Long>, Serializable {
 
     @Id
-    @GeneratedValue
     @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "gb_grade_record_id_sequence")
+    @SequenceGenerator(name = "gb_grade_record_id_sequence", sequenceName = "GB_GRADE_RECORD_S")
     @ToString.Include
     protected Long id;
 
