a90cf3d23aea135e9abefb318f3cc917150a31bd
Jesus Maria Mendez Perez <jesus.mendez@ticarum.es>
2025-09-15 09:50:50 +0200

    SAK-51765 Error - reorderPinnedSites(null:-1)

--- library/src/webapp/js/portal/portal.all.sites.js
+++ library/src/webapp/js/portal/portal.all.sites.js
@@ -284,4 +284,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     allsites.setup();
   });
+
+  const organizeTab = document.getElementById('allsites-organize-favourites-tab');
+  if (organizeTab) {
+    organizeTab.addEventListener("click", e => {
+      const refreshNotification = document.getElementById('allsites-refresh-notification');
+      if (refreshNotification) {
+        const computedStyle = window.getComputedStyle(refreshNotification);
+        const displayValue = computedStyle.getPropertyValue('display');
+        if (displayValue === 'block') {
+          location.reload();
+        }
+      }
+    });
+  }
+
 });
