6cd1dd36455fdff6455e2b6d7eb342689d73495b
JulianJanetschek <54806745+jkjanetschek@users.noreply.github.com>
2025-10-23 20:08:03 +0200

    SAK-51755 Assignments Download All functionality accounts for similarly names files (#13954)
    (cherry picked from commit c54b898932045591b0b16414083cbbc04a63c628)

--- assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
+++ assignment/impl/src/java/org/sakaiproject/assignment/impl/AssignmentServiceImpl.java
@@ -4051,12 +4051,11 @@ public class AssignmentServiceImpl implements AssignmentService, EntityTransferr
                         realName = candidateName;
                         done.put(candidateName, 1);
                     } else {
-                        String fileName = FilenameUtils.removeExtension(candidateName);
+                        String fileName = org.springframework.util.StringUtils.stripFilenameExtension(candidateName);
                         String fileExt = org.springframework.util.StringUtils.getFilenameExtension(candidateName);
-                        if (!"".equals(fileExt.trim())) {
-                            fileExt = "." + fileExt;
-                        }
-                        realName = fileName + "+" + already + fileExt;
+                        realName = org.springframework.util.StringUtils.hasText(fileExt)
+                                ?  fileName + "+" + already + "." + fileExt
+                                :  fileName + "+" + already;
                         done.put(candidateName, already + 1);
                     }
 
