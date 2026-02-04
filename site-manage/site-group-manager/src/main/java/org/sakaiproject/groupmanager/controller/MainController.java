f3eebbd1f3eeb8765122b72f5e14cd984cbba14e
manu •• <mfuster99@outlook.es>
2026-02-04 09:50:41 +0100

    SAK-52325 Site Info Joinable Set Group restrictions

--- site-manage/site-group-manager/src/main/java/org/sakaiproject/groupmanager/controller/MainController.java
+++ site-manage/site-group-manager/src/main/java/org/sakaiproject/groupmanager/controller/MainController.java
@@ -16,11 +16,13 @@
 package org.sakaiproject.groupmanager.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
@@ -29,8 +31,17 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import lombok.extern.slf4j.Slf4j;
-
+import org.sakaiproject.authz.api.AuthzGroup.RealmLockMode;
+import org.sakaiproject.authz.api.AuthzRealmLockException;
+import org.sakaiproject.groupmanager.constants.GroupManagerConstants;
+import org.sakaiproject.groupmanager.form.MainForm;
+import org.sakaiproject.groupmanager.service.SakaiService;
+import org.sakaiproject.samigo.util.SamigoConstants;
+import org.sakaiproject.site.api.Group;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.util.comparator.GroupTitleComparator;
+import org.sakaiproject.util.comparator.UserSortNameComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
@@ -41,18 +52,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
-import org.sakaiproject.authz.api.AuthzGroup.RealmLockMode;
-import org.sakaiproject.authz.api.AuthzRealmLockException;
-import org.sakaiproject.groupmanager.constants.GroupManagerConstants;
-import org.sakaiproject.groupmanager.form.MainForm;
-import org.sakaiproject.groupmanager.service.SakaiService;
-import org.sakaiproject.site.api.Group;
-import org.sakaiproject.site.api.Site;
-import org.sakaiproject.user.api.User;
-import org.sakaiproject.util.comparator.GroupTitleComparator;
-import org.sakaiproject.util.comparator.UserSortNameComparator;
-import org.sakaiproject.samigo.util.SamigoConstants;
-
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
@@ -91,6 +91,8 @@ public class MainController {
         boolean anyJoinableSetDate = false;
         // Joinable sets size for each group
         Map<String, String> groupJoinableSetSizeMap = new HashMap<String, String>();
+        // Group restrictions for each group
+        Map<String, String> groupRestrictionsMap = new HashMap<String, String>();
         // List of groups of the site, excluding the ones which GROUP_PROP_WSETUP_CREATED property is false.
         List<Group> groupList = site.getGroups().stream().filter(group -> group.getProperties().getProperty(Group.GROUP_PROP_WSETUP_CREATED) != null && Boolean.valueOf(group.getProperties().getProperty(Group.GROUP_PROP_WSETUP_CREATED)).booleanValue()).collect(Collectors.toList());
         // Sort the group list by title.
@@ -135,6 +137,27 @@ public class MainController {
             }
             // Get the max number of users who can join each joinable set group
             groupJoinableSetSizeMap.put(group.getId(), group.getProperties().getProperty(Group.GROUP_PROP_JOINABLE_SET_MAX));
+            
+            // Get group restrictions if this group is part of a joinable set
+            String joinableSetRestrictions = "";
+            if (joinableSetName != null) {
+                String allowedGroups = group.getProperties().getProperty(GroupManagerConstants.GROUP_PROP_JOINABLE_ALLOWED_GROUPS);
+                if (allowedGroups != null && !allowedGroups.trim().isEmpty()) {
+                    // Find the names of the restricted groups
+                    List<String> restrictedGroupNames = new ArrayList<>();
+                    String[] restrictedGroupIds = allowedGroups.split(",");
+                    for (String restrictedGroupId : restrictedGroupIds) {
+                        Optional<Group> restrictedGroup = site.getGroups().stream()
+                            .filter(g -> g.getId().equals(restrictedGroupId.trim()))
+                            .findFirst();
+                        if (restrictedGroup.isPresent()) {
+                            restrictedGroupNames.add(restrictedGroup.get().getTitle());
+                        }
+                    }
+                    joinableSetRestrictions = String.join(", ", restrictedGroupNames);
+                }
+            }
+            groupRestrictionsMap.put(group.getId(), joinableSetRestrictions);
 
             // Check if the group is locked for modify or all
             if (RealmLockMode.ALL.equals(group.getRealmLock()) || RealmLockMode.MODIFY.equals(group.getRealmLock())) {
@@ -151,9 +174,61 @@ public class MainController {
             // If the group is locked, provide information about the entities that are locking the group.
             if (groupLocked) {
                 anyGroupLocked = true;
-                lockedGroupsEntityMap.put(group.getId(), sakaiService.getGroupLockingEntities(group));
+                Map<String, List<String>> entityMap = sakaiService.getGroupLockingEntities(group);
+
+                if (!entityMap.containsKey("assignments")) {
+                    entityMap.put("assignments", new ArrayList<>());
+                }
+                if (!entityMap.containsKey("assessments")) {
+                    entityMap.put("assessments", new ArrayList<>());
+                }
+                if (!entityMap.containsKey("joinableSets")) {
+                    entityMap.put("joinableSets", new ArrayList<>());
+                }
+
+                lockedGroupsEntityMap.put(group.getId(), entityMap);
             }
 
+        }
+
+        // Check for groups that are being used as restrictions in joinable sets
+        for (Group group : groupList) {
+            String groupId = group.getId();
+
+            Map<String, List<String>> entityMap = lockedGroupsEntityMap.getOrDefault(groupId, new HashMap<>());
+
+            if (!entityMap.containsKey("joinableSets")) {
+                entityMap.put("joinableSets", new ArrayList<>());
+            }
+
+            boolean isUsedInJoinableSet = site.getGroups().stream()
+                .anyMatch(g -> {
+                    String allowedGroups = g.getProperties().getProperty(GroupManagerConstants.GROUP_PROP_JOINABLE_ALLOWED_GROUPS);
+                    return allowedGroups != null && Arrays.asList(allowedGroups.split(",")).contains(groupId);
+                });
+
+            if (isUsedInJoinableSet) {
+                // Mark as locked for deletion
+                if (!lockedForDeletionGroupList.contains(groupId)) {
+                    lockedForDeletionGroupList.add(groupId);
+                }
+
+                anyGroupLocked = true;
+                List<String> joinableSetTitles = entityMap.get("joinableSets");
+
+                // Find which joinable sets are using this group as restriction
+                site.getGroups().stream()
+                    .filter(g -> {
+                        String allowedGroups = g.getProperties().getProperty(GroupManagerConstants.GROUP_PROP_JOINABLE_ALLOWED_GROUPS);
+                        return allowedGroups != null && Arrays.asList(allowedGroups.split(",")).contains(groupId);
+                    })
+                    .map(g -> g.getProperties().getProperty(Group.GROUP_PROP_JOINABLE_SET))
+                    .filter(Objects::nonNull)
+                    .distinct()
+                    .forEach(joinableSetTitles::add);
+            }
+
+            lockedGroupsEntityMap.put(groupId, entityMap);
         }
 
         // Add attributes to the model
@@ -168,6 +243,7 @@ public class MainController {
         model.addAttribute("joinableSetCloseDateMap", joinableSetCloseDateMap);
         model.addAttribute("anyJoinableSetDate", anyJoinableSetDate);
         model.addAttribute("groupJoinableSetSizeMap", groupJoinableSetSizeMap);
+        model.addAttribute("groupRestrictionsMap", groupRestrictionsMap);
         model.addAttribute("mainForm", new MainForm());
         log.debug("Listing {} groups for the site {}.", groupList.size(), site.getId());
 
@@ -187,6 +263,28 @@ public class MainController {
 
         // Control if any group has been deleted
         boolean anyGroupDeleted = false;
+
+        // Check if any groups to delete are being used as restrictions in joinable sets
+        List<Group> groupsUsedInJoinableSets = new ArrayList<>();
+        for (String deletedGroupId : deleteGroupsForm.getDeletedGroupList()) {
+            // Check if this group is referenced in any joinable set's allowed groups property
+            boolean isUsedInJoinableSet = site.getGroups().stream()
+                .anyMatch(group -> {
+                    String allowedGroups = group.getProperties().getProperty(GroupManagerConstants.GROUP_PROP_JOINABLE_ALLOWED_GROUPS);
+                    return allowedGroups != null && Arrays.asList(allowedGroups.split(",")).contains(deletedGroupId);
+                });
+
+            if (isUsedInJoinableSet) {
+                sakaiService.findGroupById(deletedGroupId).ifPresent(groupsUsedInJoinableSets::add);
+            }
+        }
+
+        // If groups are being used in joinable sets, show error and return
+        if (!groupsUsedInJoinableSets.isEmpty()) {
+            String groups = String.join(", ", groupsUsedInJoinableSets.stream().map(g -> g.getTitle()).collect(Collectors.toList()));
+            model.addAttribute("errorMessage", messageSource.getMessage("index.error.cantDeleteGroupsUsedInJoinableSets", new Object[] {groups}, sakaiService.getCurrentUserLocale()));
+            return showIndex(model, request, response);
+        }
 
         // For each group, try to delete it from the site
         List<Group> lockedGroups = new ArrayList<>(deleteGroupsForm.getDeletedGroupList().size());
