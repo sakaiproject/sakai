f3df394d10d9ee6049696ad345dbeaf2e4de96a2
Sam Ottenhoff <ottenhoff@longsight.com>
2026-03-06 14:13:20 -0500

    SAK-51799 SiteInfo Fix URL replacement during site import (#14425)
    (cherry picked from commit 5d671ebe37074bed8a8fd845a858c5029cf75b12)
    
     Conflicts:
    	site-manage/site-manage-impl/impl/pom.xml

---
+++ site-manage/site-manage-impl/impl/src/test/java/org/sakaiproject/sitemanage/impl/SiteManageServiceImplImportToolContentTest.java
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2003-2026 The Apereo Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://opensource.org/licenses/ecl2
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sakaiproject.sitemanage.impl;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.sakaiproject.authz.api.AuthzGroup;
+import org.sakaiproject.authz.api.AuthzGroupService;
+import org.sakaiproject.authz.api.FunctionManager;
+import org.sakaiproject.site.api.Site;
+import org.sakaiproject.site.api.SitePage;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.site.api.ToolConfiguration;
+import org.sakaiproject.sitemanage.api.SiteManageConstants;
+import org.sakaiproject.tool.api.Tool;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Focused unit tests for SiteManageServiceImpl#importToolContent site info URL behavior.
+ */
+public class SiteManageServiceImplImportToolContentTest {
+
+    private SiteManageServiceImpl siteManageService;
+    private SiteService siteService;
+    private AuthzGroupService authzGroupService;
+    private FunctionManager functionManager;
+
+    @Before
+    public void setUp() {
+        siteManageService = spy(new SiteManageServiceImpl());
+        siteService = mock(SiteService.class);
+        authzGroupService = mock(AuthzGroupService.class);
+        functionManager = mock(FunctionManager.class);
+
+        siteManageService.setSiteService(siteService);
+        siteManageService.setAuthzGroupService(authzGroupService);
+        siteManageService.setFunctionManager(functionManager);
+    }
+
+    @Test
+    public void importToolContentUsesSourceSiteInfoUrl() throws Exception {
+
+        final String oldSiteId = "site-old";
+        final String newSiteId = "site-new";
+        final String sourceSiteInfoUrl = "https://sakai.example.edu/portal/site/site-old";
+        final String destinationSiteInfoUrl = "https://sakai.example.edu/portal/site/should-not-be-used";
+        final String transferResultSiteInfoUrl = "https://sakai.example.edu/access/content/group/site-new/site-info?from-transfer=true";
+        final String expectedSiteInfoUrl = transferResultSiteInfoUrl;
+
+        Site sourceSite = mock(Site.class);
+        Site destinationSite = mock(Site.class);
+        mockImportContext(oldSiteId, newSiteId, sourceSiteInfoUrl, destinationSiteInfoUrl, sourceSite, destinationSite);
+
+        doReturn(transferResultSiteInfoUrl).when(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+
+        siteManageService.importToolContent(oldSiteId, destinationSite, false);
+
+        verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+        verify(siteManageService, never()).transferSiteResource(oldSiteId, newSiteId, destinationSiteInfoUrl);
+        verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
+        verify(siteService).save(destinationSite);
+    }
+
+    @Test
+    public void importToolContentFallsBackToSiteIdReplacementWhenUrlIsNotAResource() throws Exception {
+
+        final String oldSiteId = "site-old";
+        final String newSiteId = "site-new";
+        final String sourceSiteInfoUrl = "https://sakai.example.edu/portal/site/site-old";
+        final String destinationSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+        final String expectedSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+
+        Site sourceSite = mock(Site.class);
+        Site destinationSite = mock(Site.class);
+        mockImportContext(oldSiteId, newSiteId, sourceSiteInfoUrl, destinationSiteInfoUrl, sourceSite, destinationSite);
+
+        // Simulate no update from transferSiteResource (non-resource URL case).
+        doReturn(sourceSiteInfoUrl).when(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+
+        siteManageService.importToolContent(oldSiteId, destinationSite, false);
+
+        InOrder inOrder = inOrder(siteManageService, destinationSite, siteService);
+        inOrder.verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+        inOrder.verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
+        inOrder.verify(siteService).save(destinationSite);
+    }
+
+    private SitePage mockSiteInfoPage() {
+        SitePage page = mock(SitePage.class);
+        ToolConfiguration toolConfiguration = mock(ToolConfiguration.class);
+        Tool tool = mock(Tool.class);
+
+        when(tool.getId()).thenReturn(SiteManageConstants.SITE_INFO_TOOL_ID);
+        when(toolConfiguration.getTool()).thenReturn(tool);
+        when(page.getTools()).thenReturn(List.of(toolConfiguration));
+
+        return page;
+    }
+
+    private void mockImportContext(String oldSiteId, String newSiteId, String sourceSiteInfoUrl, String destinationSiteInfoUrl, Site sourceSite, Site destinationSite) throws Exception {
+        SitePage siteInfoPage = mockSiteInfoPage();
+        String destinationSiteRef = "/site/" + newSiteId;
+
+        when(sourceSite.getInfoUrl()).thenReturn(sourceSiteInfoUrl);
+        when(sourceSite.getRoles()).thenReturn(Collections.emptySet());
+
+        when(destinationSite.getId()).thenReturn(newSiteId);
+        when(destinationSite.getInfoUrl()).thenReturn(destinationSiteInfoUrl);
+        when(destinationSite.getPages()).thenReturn(List.of(siteInfoPage));
+
+        when(siteService.getSite(oldSiteId)).thenReturn(sourceSite);
+        when(siteService.siteReference(newSiteId)).thenReturn(destinationSiteRef);
+
+        AuthzGroup destinationRealm = mock(AuthzGroup.class);
+        when(functionManager.getRegisteredFunctions()).thenReturn(Collections.emptyList());
+        when(authzGroupService.getAuthzGroup(destinationSiteRef)).thenReturn(destinationRealm);
+    }
+}
