2c31bb457d03223db4814e50ef8111525934ce6b
Sam Ottenhoff <ottenhoff@longsight.com>
2026-03-10 14:09:32 -0400

    SAK-50564 Dashboard fix calendar widget pinned sites message (#14441)
    (cherry picked from commit fd7e5036928bd2f558e42c88f86fc9d4ef275f46)
    
     Conflicts:
    	webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/sakai-calendar.test.js

--- webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/data.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/data.js
@@ -1,10 +1,12 @@
 export const i18nUrl = /.*getI18nProperties.*calendar-wc/;
+export const pinnedSitesMessage = "Showing events for the next year, for pinned sites only";
 
 export const i18n = `
 widget_title=Calendar
 today=Today
 days_message=Showing events for the next {} days
 events_for=Events for
+pinned_sites_message=${pinnedSitesMessage}
 `;
 
 export const userId = "adrian";
@@ -17,6 +19,7 @@ export const vavavoomSite = "Vavavoom Site";
 export const selectedDate = new Date(1677269640000);
 
 export const userCalendarUrl= "/api/users/current/calendar";
+export const siteCalendarUrl = "/api/sites/xyz/calendar";
 
 export const userCalendarEvents = {
   events: [
--- webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/sakai-calendar.test.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/sakai-calendar.test.js
@@ -10,6 +10,7 @@ describe("sakai-calendar tests", () => {
     .get(data.i18nUrl, data.i18n, { overwriteRoutes: true })
     .get(sitePickerData.i18nUrl, sitePickerData.i18n, { overwriteRoutes: true })
     .get(data.userCalendarUrl, { "events": data.userCalendarEvents.events, "sites": sitePickerData.sites }, { overwriteRoutes: true })
+    .get(data.siteCalendarUrl, { "events": data.userCalendarEvents.events })
     .get("*", 500, { overwriteRoutes: true });
 
   it ("renders in user mode correctly", async () => {
@@ -21,10 +22,16 @@ describe("sakai-calendar tests", () => {
 
     expect(el.shadowRoot.getElementById("container")).to.exist;
 
-    waitUntil(() => el._events);
+    await waitUntil(() => {
+      const calendarMessage = el.shadowRoot.querySelector(".calendar-msg");
+      return el._events && calendarMessage && calendarMessage.textContent.includes(data.pinnedSitesMessage);
+    });
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
+
+    expect(el.shadowRoot.querySelector(".calendar-msg")).to.exist;
+    expect(el.shadowRoot.querySelector(".calendar-msg").textContent).to.contain(data.pinnedSitesMessage);
 
     el.dispatchEvent(new CustomEvent("user-selected-date-changed", { detail: { selectedDate: data.selectedDate } }));
 
@@ -34,5 +41,18 @@ describe("sakai-calendar tests", () => {
     expect(el.shadowRoot.querySelector("#days-events a")).to.exist;
     expect(el.shadowRoot.querySelectorAll("#days-events a span").item(0).innerHTML).to.contain(data.userCalendarEvents.events[0].title);
     expect(el.shadowRoot.querySelectorAll("#days-events a span").item(1).innerHTML).to.contain(data.userCalendarEvents.events[0].siteTitle);
+  });
+
+  it ("does not render the pinned sites message in site mode", async () => {
+
+    const el = await fixture(html`
+      <sakai-calendar site-id="${data.siteId}"></sakai-calendar>
+    `);
+
+    await waitUntil(() => el._events && el._i18n && !el.shadowRoot.querySelector(".calendar-msg"));
+    await elementUpdated(el);
+    await expect(el).to.be.accessible();
+
+    expect(el.shadowRoot.querySelector(".calendar-msg")).to.not.exist;
   });
 });
