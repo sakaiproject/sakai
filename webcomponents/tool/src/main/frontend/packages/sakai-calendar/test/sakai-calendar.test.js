fd7e5036928bd2f558e42c88f86fc9d4ef275f46
Sam Ottenhoff <ottenhoff@longsight.com>
2026-03-10 14:09:32 -0400

    SAK-50564 Dashboard fix calendar widget pinned sites message (#14441)

--- webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/sakai-calendar.test.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-calendar/test/sakai-calendar.test.js
@@ -11,6 +11,7 @@ describe("sakai-calendar tests", () => {
       .get(data.i18nUrl, data.i18n)
       .get(sitePickerData.i18nUrl, sitePickerData.i18n)
       .get(data.userCalendarUrl, { "events": data.userCalendarEvents.events, "sites": sitePickerData.sites })
+      .get(data.siteCalendarUrl, { "events": data.userCalendarEvents.events })
       .get("*", 500);
   });
 
@@ -27,10 +28,16 @@ describe("sakai-calendar tests", () => {
 
     expect(el.shadowRoot.getElementById("container")).to.exist;
 
-    waitUntil(() => el._events);
+    await waitUntil(() => {
+      const calendarMessage = el.shadowRoot.querySelector(".calendar-msg");
+      return el._events && calendarMessage && calendarMessage.textContent.includes(data.pinnedSitesMessage);
+    });
 
     await elementUpdated(el);
     await expect(el).to.be.accessible();
+
+    expect(el.shadowRoot.querySelector(".calendar-msg")).to.exist;
+    expect(el.shadowRoot.querySelector(".calendar-msg").textContent).to.contain(data.pinnedSitesMessage);
 
     el.dispatchEvent(new CustomEvent("user-selected-date-changed", { detail: { selectedDate: data.selectedDate } }));
 
@@ -40,5 +47,18 @@ describe("sakai-calendar tests", () => {
     expect(el.shadowRoot.querySelector("#days-events a")).to.exist;
     expect(el.shadowRoot.querySelectorAll("#days-events a span").item(0).innerHTML).to.contain(data.userCalendarEvents.events[0].title);
     expect(el.shadowRoot.querySelectorAll("#days-events a span").item(1).innerHTML).to.contain(data.userCalendarEvents.events[0].siteTitle);
+  });
+
+  it ("does not render the pinned sites message in site mode", async () => {
+
+    const el = await fixture(html`
+      <sakai-calendar site-id="${data.siteId}"></sakai-calendar>
+    `);
+
+    await waitUntil(() => el._events && el._i18n && !el.shadowRoot.querySelector(".calendar-msg"));
+    await elementUpdated(el);
+    await expect(el).to.be.accessible();
+
+    expect(el.shadowRoot.querySelector(".calendar-msg")).to.not.exist;
   });
 });
