8333cd4e0336bb1920148233c69201845fada73e
Sam Ottenhoff <ottenhoff@longsight.com>
2026-03-11 10:00:41 -0400

    SAK-51799 SiteInfo rewrite info url on import (#14464)

--- site-manage/site-manage-impl/impl/src/test/java/org/sakaiproject/sitemanage/impl/SiteManageServiceImplImportToolContentTest.java
+++ site-manage/site-manage-impl/impl/src/test/java/org/sakaiproject/sitemanage/impl/SiteManageServiceImplImportToolContentTest.java
@@ -15,8 +15,11 @@
  */
 package org.sakaiproject.sitemanage.impl;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,13 +27,19 @@ import org.mockito.InOrder;
 import org.sakaiproject.authz.api.AuthzGroup;
 import org.sakaiproject.authz.api.AuthzGroupService;
 import org.sakaiproject.authz.api.FunctionManager;
+import org.sakaiproject.component.api.ServerConfigurationService;
+import org.sakaiproject.entity.api.EntityManager;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.entity.api.ResourcePropertiesEdit;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SitePage;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.site.api.ToolConfiguration;
 import org.sakaiproject.sitemanage.api.SiteManageConstants;
 import org.sakaiproject.tool.api.Tool;
+import org.tsugi.lti13.LTICustomVars;
 
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
@@ -48,6 +57,8 @@ public class SiteManageServiceImplImportToolContentTest {
     private SiteService siteService;
     private AuthzGroupService authzGroupService;
     private FunctionManager functionManager;
+    private ServerConfigurationService serverConfigurationService;
+    private EntityManager entityManager;
 
     @Before
     public void setUp() {
@@ -55,10 +66,18 @@ public class SiteManageServiceImplImportToolContentTest {
         siteService = mock(SiteService.class);
         authzGroupService = mock(AuthzGroupService.class);
         functionManager = mock(FunctionManager.class);
+        serverConfigurationService = mock(ServerConfigurationService.class);
+        entityManager = mock(EntityManager.class);
 
         siteManageService.setSiteService(siteService);
         siteManageService.setAuthzGroupService(authzGroupService);
         siteManageService.setFunctionManager(functionManager);
+        siteManageService.setServerConfigurationService(serverConfigurationService);
+        siteManageService.setEntityManager(entityManager);
+
+        // Keep this test class focused on site-info URL behavior.
+        doReturn(false).when(siteManageService).isAddMissingToolsOnImportEnabled();
+        when(entityManager.getEntityProducers()).thenReturn(Collections.emptyList());
     }
 
     @Test
@@ -107,6 +126,124 @@ public class SiteManageServiceImplImportToolContentTest {
         inOrder.verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
         inOrder.verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
         inOrder.verify(siteService).save(destinationSite);
+    }
+
+    @Test
+    public void importToolContentFallsBackToSiteIdReplacementWhenTransferReturnsBlank() throws Exception {
+
+        final String oldSiteId = "site-old";
+        final String newSiteId = "site-new";
+        final String sourceSiteInfoUrl = "https://sakai.example.edu/portal/site/site-old";
+        final String destinationSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+        final String expectedSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+
+        Site sourceSite = mock(Site.class);
+        Site destinationSite = mock(Site.class);
+        mockImportContext(oldSiteId, newSiteId, sourceSiteInfoUrl, destinationSiteInfoUrl, sourceSite, destinationSite);
+
+        // Simulate transfer failure after resolving a resource.
+        doReturn("").when(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+
+        siteManageService.importToolContent(oldSiteId, destinationSite, false);
+
+        verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+        verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
+        verify(destinationSite, never()).setInfoUrl("");
+        verify(siteService).save(destinationSite);
+    }
+
+    @Test
+    public void importToolsIntoSiteUpdatesSiteInfoUrlDuringOverviewImport() throws Exception {
+
+        final String oldSiteId = "site-old";
+        final String newSiteId = "site-new";
+        final String sourceSiteInfoUrl = "https://sakai.example.edu/portal/site/site-old";
+        final String sourceSiteDescription = "Imported site description";
+        final String expectedSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+
+        Site sourceSite = mock(Site.class);
+        Site destinationSite = mock(Site.class);
+        ResourceProperties sourceSiteProperties = mock(ResourceProperties.class);
+        ResourcePropertiesEdit destinationSiteProperties = mock(ResourcePropertiesEdit.class);
+
+        when(sourceSite.getDescription()).thenReturn(sourceSiteDescription);
+        when(sourceSite.getInfoUrl()).thenReturn(sourceSiteInfoUrl);
+        when(sourceSite.getProperties()).thenReturn(sourceSiteProperties);
+        when(sourceSiteProperties.getProperty(LTICustomVars.CONTEXT_ID_HISTORY)).thenReturn(null);
+
+        when(destinationSite.getId()).thenReturn(newSiteId);
+        when(destinationSite.getPropertiesEdit()).thenReturn(destinationSiteProperties);
+
+        when(siteService.getSite(oldSiteId)).thenReturn(sourceSite);
+        when(siteService.getSite(newSiteId)).thenReturn(destinationSite);
+
+        // Simulate no update from transferSiteResource (non-resource URL case).
+        doReturn(sourceSiteInfoUrl).when(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+
+        Map<String, List<String>> importTools = new HashMap<>();
+        importTools.put(SiteManageConstants.SITE_INFO_TOOL_ID, List.of(oldSiteId));
+
+        siteManageService.importToolsIntoSite(
+            destinationSite,
+            new ArrayList<>(List.of(SiteManageConstants.SITE_INFO_TOOL_ID)),
+            importTools,
+            Collections.emptyMap(),
+            Collections.emptyMap(),
+            false
+        );
+
+        verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+        verify(destinationSite).setDescription(sourceSiteDescription);
+        verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
+        verify(destinationSite, never()).setInfoUrl("");
+        verify(siteService, atLeastOnce()).save(destinationSite);
+    }
+
+    @Test
+    public void importToolsIntoSiteFallsBackToSiteIdReplacementWhenTransferReturnsBlank() throws Exception {
+
+        final String oldSiteId = "site-old";
+        final String newSiteId = "site-new";
+        final String sourceSiteInfoUrl = "https://sakai.example.edu/portal/site/site-old";
+        final String sourceSiteDescription = "Imported site description";
+        final String expectedSiteInfoUrl = "https://sakai.example.edu/portal/site/site-new";
+
+        Site sourceSite = mock(Site.class);
+        Site destinationSite = mock(Site.class);
+        ResourceProperties sourceSiteProperties = mock(ResourceProperties.class);
+        ResourcePropertiesEdit destinationSiteProperties = mock(ResourcePropertiesEdit.class);
+
+        when(sourceSite.getDescription()).thenReturn(sourceSiteDescription);
+        when(sourceSite.getInfoUrl()).thenReturn(sourceSiteInfoUrl);
+        when(sourceSite.getProperties()).thenReturn(sourceSiteProperties);
+        when(sourceSiteProperties.getProperty(LTICustomVars.CONTEXT_ID_HISTORY)).thenReturn(null);
+
+        when(destinationSite.getId()).thenReturn(newSiteId);
+        when(destinationSite.getPropertiesEdit()).thenReturn(destinationSiteProperties);
+
+        when(siteService.getSite(oldSiteId)).thenReturn(sourceSite);
+        when(siteService.getSite(newSiteId)).thenReturn(destinationSite);
+
+        // Simulate transfer failure after resolving a resource.
+        doReturn("").when(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+
+        Map<String, List<String>> importTools = new HashMap<>();
+        importTools.put(SiteManageConstants.SITE_INFO_TOOL_ID, List.of(oldSiteId));
+
+        siteManageService.importToolsIntoSite(
+            destinationSite,
+            new ArrayList<>(List.of(SiteManageConstants.SITE_INFO_TOOL_ID)),
+            importTools,
+            Collections.emptyMap(),
+            Collections.emptyMap(),
+            false
+        );
+
+        verify(siteManageService).transferSiteResource(oldSiteId, newSiteId, sourceSiteInfoUrl);
+        verify(destinationSite).setDescription(sourceSiteDescription);
+        verify(destinationSite).setInfoUrl(expectedSiteInfoUrl);
+        verify(destinationSite, never()).setInfoUrl("");
+        verify(siteService, atLeastOnce()).save(destinationSite);
     }
 
     private SitePage mockSiteInfoPage() {
