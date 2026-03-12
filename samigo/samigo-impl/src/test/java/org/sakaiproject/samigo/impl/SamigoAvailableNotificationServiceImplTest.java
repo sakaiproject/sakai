65f1b0c09c0e6cb75e48b9fc7b78307223765ec1
Sam Ottenhoff <ottenhoff@longsight.com>
2026-03-11 20:11:22 -0400

    test

---
+++ samigo/samigo-impl/src/test/java/org/sakaiproject/samigo/impl/SamigoAvailableNotificationServiceImplTest.java
@@ -0,0 +1,56 @@
+package org.sakaiproject.samigo.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.sakaiproject.entity.api.EntityPropertyTypeException;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.event.api.NotificationService;
+import org.sakaiproject.samigo.util.SamigoConstants;
+import org.sakaiproject.user.api.Preferences;
+import org.sakaiproject.user.api.PreferencesService;
+import org.sakaiproject.user.api.User;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SamigoAvailableNotificationServiceImplTest {
+
+    private static final String OPTIONAL_NOTIFICATION_PREF_KEY = Integer.toString(NotificationService.NOTI_OPTIONAL);
+
+    @Mock private PreferencesService preferencesService;
+    @Mock private Preferences preferences;
+    @Mock private ResourceProperties resourceProperties;
+    @Mock private User user;
+    private final SamigoAvailableNotificationServiceImpl service = new SamigoAvailableNotificationServiceImpl();
+
+    @Test
+    public void getOpenNotificationPreferenceDefaultsInvalidPreferenceToImmediate() throws Exception {
+        service.setPreferencesService(preferencesService);
+        when(user.getId()).thenReturn("user-1");
+        when(user.getEid()).thenReturn("user1");
+        when(preferencesService.getPreferences("user-1")).thenReturn(preferences);
+        when(preferences.getProperties(NotificationService.PREFS_TYPE + SamigoConstants.NOTI_PREFS_TYPE_SAMIGO_OPEN))
+                .thenReturn(resourceProperties);
+        when(resourceProperties.getLongProperty(OPTIONAL_NOTIFICATION_PREF_KEY))
+                .thenThrow(new EntityPropertyTypeException(OPTIONAL_NOTIFICATION_PREF_KEY));
+        when(resourceProperties.getProperty(OPTIONAL_NOTIFICATION_PREF_KEY)).thenReturn("");
+
+        assertEquals(NotificationService.PREF_IMMEDIATE, service.getOpenNotificationPreference(user));
+    }
+
+    @Test
+    public void getOpenNotificationPreferenceHonorsIgnorePreference() throws Exception {
+        service.setPreferencesService(preferencesService);
+        when(user.getId()).thenReturn("user-1");
+        when(preferencesService.getPreferences("user-1")).thenReturn(preferences);
+        when(preferences.getProperties(NotificationService.PREFS_TYPE + SamigoConstants.NOTI_PREFS_TYPE_SAMIGO_OPEN))
+                .thenReturn(resourceProperties);
+        when(resourceProperties.getLongProperty(OPTIONAL_NOTIFICATION_PREF_KEY))
+                .thenReturn((long) NotificationService.PREF_IGNORE);
+
+        assertEquals(NotificationService.PREF_IGNORE, service.getOpenNotificationPreference(user));
+    }
+}
