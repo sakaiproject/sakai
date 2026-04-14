5bf45258a8893e3a7619e4c86f12f78096348d82
David Horwitz <dhorwitz@gmail.com>
2026-04-14 09:06:12 +0200

    SAK-52498 Update Passay 2.0.0

--- kernel/kernel-impl/src/main/java/org/sakaiproject/util/impl/PasswordFactoryImpl.java
+++ kernel/kernel-impl/src/main/java/org/sakaiproject/util/impl/PasswordFactoryImpl.java
@@ -14,27 +14,13 @@
  * limitations under the License.
  */
 package org.sakaiproject.util.impl;
-/**
- * Copyright (c) 2003-2017 The Apereo Foundation
- *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *             http://opensource.org/licenses/ecl2
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-import org.passay.CharacterCharacteristicsRule;
-import org.passay.CharacterData;
-import org.passay.CharacterRule;
-import org.passay.EnglishCharacterData;
-import org.passay.PasswordGenerator;
+
+import org.passay.data.CharacterData;
+import org.passay.data.EnglishCharacterData;
+import org.passay.generate.PasswordGenerator;
+import org.passay.rule.CharacterCharacteristicsRule;
+import org.passay.rule.CharacterRule;
 import org.sakaiproject.util.api.PasswordFactory;
 
 import lombok.extern.slf4j.Slf4j;
@@ -44,18 +30,15 @@ public class PasswordFactoryImpl implements PasswordFactory {
 
   @Override
   public String generatePassword() {
-    PasswordGenerator gen = new PasswordGenerator();
+    
     CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
-    CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
-    lowerCaseRule.setNumberOfCharacters(2);
+    CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars, 2);
      
     CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
-    CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
-    upperCaseRule.setNumberOfCharacters(2);
+    CharacterRule upperCaseRule = new CharacterRule(upperCaseChars, 2);
      
     CharacterData digitChars = EnglishCharacterData.Digit;
-    CharacterRule digitRule = new CharacterRule(digitChars);
-    digitRule.setNumberOfCharacters(2);
+    CharacterRule digitRule = new CharacterRule(digitChars, 2);
      
     CharacterData specialChars = new CharacterData() {
       public String getErrorCode() {
@@ -66,11 +49,10 @@ public class PasswordFactoryImpl implements PasswordFactory {
         return "!@#$%^&*()_+";
       }
     };
-    CharacterRule splCharRule = new CharacterRule(specialChars);
-    splCharRule.setNumberOfCharacters(2);
-     
-    String password = gen.generatePassword(10, splCharRule, lowerCaseRule,
-                      upperCaseRule, digitRule);
+    CharacterRule splCharRule = new CharacterRule(specialChars, 2);
+    PasswordGenerator gen = new PasswordGenerator(10, splCharRule, lowerCaseRule,
+            upperCaseRule, digitRule);
+    String password = gen.toString();
     log.debug("password: {}", password);
     return password;
   }
