4cb175af57e21291d2548b1c6d50aa797c7d516a
manu •• <mfuster99@outlook.es>
2026-06-03 17:58:39 +0200

    copyright

--- polls/tool/src/main/java/org/sakaiproject/poll/tool/mvc/PollExportController.java
+++ polls/tool/src/main/java/org/sakaiproject/poll/tool/mvc/PollExportController.java
@@ -1,3 +1,19 @@
+/**********************************************************************************
+ * Copyright (c) 2026 The Apereo Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://opensource.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************************/
+
 package org.sakaiproject.poll.tool.mvc;
 
 import java.io.ByteArrayOutputStream;
@@ -13,7 +29,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
-import com.opencsv.CSVWriter;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
@@ -43,6 +58,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.opencsv.CSVWriter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
