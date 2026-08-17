be7e5b9ea7a478fa07c0c68ca38ca09100bb605e
Sam Ottenhoff <ottenhoff@longsight.com>
2026-08-17 09:37:00 +0200

    SAK-52808 Portal register tasks component

--- webcomponents/tool/src/main/frontend/bundle-entry-points/body-scripts.js
+++ webcomponents/tool/src/main/frontend/bundle-entry-points/body-scripts.js
@@ -2,6 +2,7 @@ import "@sakai-ui/sakai-permissions/sakai-permissions.js"
 import "@sakai-ui/sakai-search/sakai-search.js";
 import "@sakai-ui/sakai-calendar/sakai-calendar.js";
 import "@sakai-ui/sakai-grades/sakai-grades.js";
+import "@sakai-ui/sakai-tasks/sakai-tasks.js";
 import "get-browser-fingerprint";
 import { init as initJumpToTop } from "@sakai-ui/sakai-jump-to-top/sakai-jump-to-top.js";
 initJumpToTop();
