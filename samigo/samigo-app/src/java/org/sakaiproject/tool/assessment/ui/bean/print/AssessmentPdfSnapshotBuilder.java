8bc2f0943d97ea13e17c10ad15133d9d167a7465
Earle Nietzel <earle.nietzel@getstellarcode.com>
2026-08-26 15:22:00 -0400

    Build fix unresolvable dependencies from the Jakarta migration
    Conflicts:
    	master/pom.xml

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/print/AssessmentPdfSnapshotBuilder.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/bean/print/AssessmentPdfSnapshotBuilder.java
@@ -22,7 +22,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.faces.model.SelectItem;
+import jakarta.faces.model.SelectItem;
 
 import org.apache.commons.lang3.StringUtils;
 import org.sakaiproject.component.cover.ComponentManager;
