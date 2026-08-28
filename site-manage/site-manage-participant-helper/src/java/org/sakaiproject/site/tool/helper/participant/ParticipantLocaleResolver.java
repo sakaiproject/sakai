77560b62cf21d33724954939e5e7a6b9081e918b
Jesús María Méndez Pérez <jesus.mendez@ticarum.es>
2026-08-28 07:44:17 +0200

    SAK-52757 Migrate to Jakarta (#14861)

--- site-manage/site-manage-participant-helper/src/java/org/sakaiproject/site/tool/helper/participant/ParticipantLocaleResolver.java
+++ site-manage/site-manage-participant-helper/src/java/org/sakaiproject/site/tool/helper/participant/ParticipantLocaleResolver.java
@@ -9,8 +9,8 @@ package org.sakaiproject.site.tool.helper.participant;
 import java.util.Locale;
 import java.util.Objects;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.sakaiproject.util.api.LocaleService;
 import org.springframework.web.servlet.LocaleResolver;
