823d5420a0607db7e70f29297c4330adc71a92c3
Earle Nietzel <earle.nietzel@getstellarcode.com>
2026-08-31 21:20:31 -0400

    SAK-52757 Fix Hibernate 6 test failures: drop HibernateDaoSupport, remove invalid @Bean @Autowired, add jackson-databind for JSON format mapper

--- jobscheduler/scheduler-component-shared/src/test/java/org/sakaiproject/component/app/scheduler/ContextMappingConfiguration.java
+++ jobscheduler/scheduler-component-shared/src/test/java/org/sakaiproject/component/app/scheduler/ContextMappingConfiguration.java
@@ -80,7 +80,6 @@ public class ContextMappingConfiguration {
     }
 
     @Bean
-    @Autowired
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
         HibernateTransactionManager txManager =  new HibernateTransactionManager();
         txManager.setSessionFactory(sessionFactory);
