5c6a7272098b7f8aa347fd61d8e2b0ddabd080dc
Sam Ottenhoff <ottenhoff@longsight.com>
2026-09-02 11:18:10 -0400

    SAK-52840 SiteStats move report chart title above chart

--- webcomponents/tool/src/main/frontend/packages/sakai-sitestats/test/sakai-sitestats-chart.test.js
+++ webcomponents/tool/src/main/frontend/packages/sakai-sitestats/test/sakai-sitestats-chart.test.js
@@ -47,6 +47,8 @@ describe("sakai-sitestats-chart tests", () => {
     const plugins = el._chartInstance.config.plugins || [];
 
     expect(el.shadowRoot.querySelector(".chart-frame.depth")).to.exist;
+    const figure = el.shadowRoot.querySelector("figure");
+    expect(figure.firstElementChild).to.equal(figure.querySelector("figcaption"));
     expect(dataset.backgroundColor).to.contain("0.26");
     expect(dataset.borderWidth).to.equal(3);
     expect(el._chartInstance.config.options.layout.padding.top).to.equal(18);
