98d7ee407981776f56bcb5eb2a5d6bc3c414ba62
Sam Ottenhoff <ottenhoff@longsight.com>
2026-09-11 13:44:08 -0400

    SAK-52092 Tags retain original association repository license

--- tags/tags-impl/impl/src/java/org/sakaiproject/tags/impl/TagAssociationRepositoryImpl.java
+++ tags/tags-impl/impl/src/java/org/sakaiproject/tags/impl/TagAssociationRepositoryImpl.java
@@ -1,25 +1,16 @@
-/**********************************************************************************
+/******************************************************************************
+ * Copyright 2023 sakaiproject.org Licensed under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- * Copyright (c) 2016 The Sakai Foundation
+ * http://opensource.org/licenses/ECL-2.0
  *
- * Original developers:
- *
- *   Unicon
- *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.osedu.org/licenses/ECL-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- **********************************************************************************/
-
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 package org.sakaiproject.tags.impl;
 
 import java.util.List;
