9c001040bf3f0d3a3dabf55435f73d820250dcb3
Jesús María Méndez Pérez <jesus.mendez@ticarum.es>
2026-09-14 19:06:42 +0200

    SAK-52757 Thymelef6-2 (#14961)

--- plus/tool/src/main/java/org/sakaiproject/plus/tool/WebMvcConfiguration.java
+++ plus/tool/src/main/java/org/sakaiproject/plus/tool/WebMvcConfiguration.java
@@ -25,10 +25,10 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring6.ISpringTemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
